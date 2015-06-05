2c4002fad6ec48a752e7539c7293fc7b1c68786c
codyopel <codyopel@gmail.com>
2015-06-05 06:08:01 -0400

    p7zip: 0.38 -> 0.38.1 - disable unfree rar support by default - add meta.license - remove use of makeFlagsArray - use force flag when copying makefile

--- pkgs/tools/archivers/p7zip/default.nix
+++ pkgs/tools/archivers/p7zip/default.nix
@@ -1,30 +1,49 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl
+, rarSupport ? false
+}:
 
 let
-  version = "9.38";
+  inherit (stdenv) isDarwin;
+  inherit (stdenv.lib) optionalString;
 in
+
 stdenv.mkDerivation rec {
   name = "p7zip-${version}";
+  version = "9.38.1";
 
   src = fetchurl {
     url = "mirror://sourceforge/p7zip/p7zip_${version}_src_all.tar.bz2";
-    sha256 = "0mxribb9a3lz3bifz6002hg7vyy8h9piinypian533hw8qvswfx7";
+    sha256 = "15drzp2xi9zv7w55yd2hzx8q3biyhnk3fz95sd5g66wskh81jl7x";
   };
 
+  patchPhase = optionalString (!rarSupport) ''
+    sed -e '/Rar/d' -i makefile* CPP/7zip/Bundles/Format7zFree/makefile
+    sed -e '/RAR/d' -i makefile* CPP/7zip/Bundles/Format7zFree/makefile
+    rm -rf CPP/7zip/Compress/Rar
+  '';
+
+  makeFlags = [
+    "DEST_HOME=$(out)"
+  ];
+
   preConfigure = ''
-    makeFlagsArray=(DEST_HOME=$out)
     buildFlags=all3
-  '' + stdenv.lib.optionalString stdenv.isDarwin ''
-    cp makefile.macosx_64bits makefile.machine
+  '' + optionalString isDarwin ''
+    cp -f makefile.macosx_64bits makefile.machine
   '';
 
   enableParallelBuilding = true;
 
-  meta = {
+  meta = with stdenv.lib; {
+    description = "A port of the 7-zip archiver for Unix";
     homepage = http://p7zip.sourceforge.net/;
-    description = "A port of the 7-zip archiver";
-    # license = stdenv.lib.licenses.lgpl21Plus; + "unRAR restriction"
-    platforms = stdenv.lib.platforms.unix;
-    maintainers = [ stdenv.lib.maintainers.raskin ];
+    license = (
+      if rarSupport then
+        licenses.unfree
+      else
+        licenses.lgpl21Plus
+    );
+    maintainers = with maintainers; [ raskin ];
+    platforms = platforms.unix;
   };
 }

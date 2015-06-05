cf0cc7fd963c214a858943b5f45030db860edefd
codyopel <codyopel@gmail.com>
2015-06-05 06:38:23 -0400

    gptfdisk: refactor - add support for Darwin & FreeBSD (untested) - install components explicitly instead of with a loop

--- pkgs/tools/system/gptfdisk/default.nix
+++ pkgs/tools/system/gptfdisk/default.nix
@@ -1,5 +1,15 @@
 { fetchurl, stdenv, libuuid, popt, icu, ncurses }:
 
+let
+  inherit (stdenv) isDarwin isFreeBSD isLinux system;
+  inherit (stdenv.lib) optionalString;
+in
+
+# Make sure platform is supported
+assert (!isDarwin && !isFreeBSD && !isLinux) -> throw "gptfdisk does not support the `${system}' platform";
+
+# TODO: add Cygwin support
+
 stdenv.mkDerivation rec {
   name = "gptfdisk-1.0.0";
 
@@ -8,27 +18,36 @@ stdenv.mkDerivation rec {
     sha256 = "0v0xl0mzwabdf9yisgsvkhpyi48kbik35c6df42gr6d78dkrarjv";
   };
 
-  buildInputs = [ libuuid popt icu ncurses ];
-
-  installPhase = ''
-    mkdir -p $out/sbin
-    mkdir -p $out/share/man/man8
-    for prog in gdisk sgdisk fixparts cgdisk
-    do
-        install -v -m755 $prog $out/sbin
-        install -v -m644 $prog.8 $out/share/man/man8
-    done
+  # Use the correct makefile on FreeBSD & Darwin
+  patchPhase = optionalString (isDarwin || isFreeBSD) ''
+    rm -f Makefile
+  '' + optionalString isDarwin ''
+    mv Makefile.mac Makefile
+  '' + optionalString isFreeBSD ''
+    mv Makefile.freebsd Makefile
   '';
 
-  meta = {
+  buildInputs = [ icu libuuid ncurses popt ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    install -v -m755 cgdisk $out/bin
+    install -v -m755 fixparts $out/bin
+    install -v -m755 gdisk $out/bin
+    install -v -m755 sgdisk $out/bin
+
+    mkdir -p $out/share/man/man8
+    install -v -m644 cgdisk.8 $out/share/man/man8
+    install -v -m644 fixparts.8 $out/share/man/man8
+    install -v -m644 gdisk.8 $out/share/man/man8
+    install -v -m644 sgdisk.8 $out/share/man/man8
+  '';
+
+  meta = with stdenv.lib; {
     description = "A set of text-mode partitioning tools for Globally Unique Identifier (GUID) Partition Table (GPT) disks";
-
-    license = stdenv.lib.licenses.gpl2;
-
     homepage = http://www.rodsbooks.com/gdisk/;
-
-    maintainers = [ stdenv.lib.maintainers.shlevy ];
-
-    platforms = stdenv.lib.platforms.linux;
+    license = licenses.gpl2;
+    maintainers = [ maintainers.shlevy ];
+    platforms = platforms.unix;
   };
 }

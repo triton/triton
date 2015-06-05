70b4920b7c1bb96cb93cbe2b8f26cfb2e372036e
William A. Kennington III <william@wkennington.com>
2015-06-04 23:51:43 -0700

    Revert "libgcrypt: Revert to old style"
    This reverts commit 330d198be1b06c482f46545830e65bbeab3a35d1.

--- pkgs/development/libraries/libgcrypt/default.nix
+++ pkgs/development/libraries/libgcrypt/default.nix
@@ -1,7 +1,24 @@
-{ lib, stdenv, fetchurl, libgpgerror, enableCapabilities ? false, libcap }:
+{ stdenv, fetchurl
+, libgpgerror
 
-assert enableCapabilities -> stdenv.isLinux;
+# Optional Dependencies
+, libcap ? null, pth ? null
+}:
 
+let
+  mkFlag = trueStr: falseStr: cond: name: val:
+    if cond == null then null else
+      "--${if cond != false then trueStr else falseStr}${name}${if val != null && cond != false then "=${val}" else ""}";
+  mkEnable = mkFlag "enable-" "disable-";
+  mkWith = mkFlag "with-" "without-";
+  mkOther = mkFlag "" "" true;
+
+  shouldUsePkg = pkg: if pkg != null && stdenv.lib.any (x: x == stdenv.system) pkg.meta.platforms then pkg else null;
+
+  optLibcap = shouldUsePkg libcap;
+  #optPth = shouldUsePkg pth;
+  optPth = null; # Broken as of 1.6.3
+in
 stdenv.mkDerivation rec {
   name = "libgcrypt-1.6.3";
 
@@ -10,26 +27,29 @@ stdenv.mkDerivation rec {
     sha256 = "0pq2nwfqgggrsh8rk84659d80vfnlkbphwqjwahccd5fjdxr3d21";
   };
 
-  buildInputs =
-    [ libgpgerror ]
-    ++ lib.optional enableCapabilities libcap;
+  buildInputs = [ libgpgerror optLibcap optPth ];
+
+  configureFlags = [
+    (mkWith   (optLibcap != null) "capabilities"  null)
+    (mkEnable (optPth != null)    "random-daemon" null)
+  ];
 
   # Make sure libraries are correct for .pc and .la files
   # Also make sure includes are fixed for callers who don't use libgpgcrypt-config
   postInstall = ''
     sed -i 's,#include <gpg-error.h>,#include "${libgpgerror}/include/gpg-error.h",g' $out/include/gcrypt.h
-  '' + stdenv.lib.optionalString enableCapabilities ''
-    sed -i 's,\(-lcap\),-L${libcap}/lib \1,' $out/lib/libgcrypt.la
+  '' + stdenv.lib.optionalString (!stdenv.isDarwin && optLibcap != null) ''
+    sed -i 's,\(-lcap\),-L${optLibcap}/lib \1,' $out/lib/libgcrypt.la
   '';
 
   doCheck = true;
 
-  meta = {
+  meta = with stdenv.lib; {
     homepage = https://www.gnu.org/software/libgcrypt/;
     description = "General-pupose cryptographic library";
-    license = lib.licenses.lgpl2Plus;
-    platforms = lib.platforms.all;
-    maintainers = [ lib.maintainers.wkennington ];
+    license = licenses.lgpl2Plus;
+    platforms = platforms.all;
+    maintainers = with maintainers; [ wkennington ];
     repositories.git = git://git.gnupg.org/libgcrypt.git;
   };
 }

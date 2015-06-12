f27b7d7c4a9548a424c0aa6144a7b4df276ba972
codyopel <codyopel@gmail.com>
2015-06-12 15:40:33 -0400

    wayland: 1.7.0 -> 1.8.0

--- pkgs/development/libraries/wayland/default.nix
+++ pkgs/development/libraries/wayland/default.nix
@@ -2,23 +2,27 @@
 
 # Optional Dependencies
 , libffi ? null, docbook_xsl ? null, doxygen ? null, graphviz ? null, libxslt ? null, xmlto ? null
-, expat ? null # Build wayland-scanner (currently cannot be disabled as of 1.7.0)
+, expat ? null # Build wayland-scanner (currently cannot be disabled as of 1.8.0)
 
 # Extra Arguments
 , enableDocumentation ? false
 }:
 
+
+let
+  inherit (stdenv.lib) mkEnable optionals;
+in
+
 # Require the optional to be enabled until upstream fixes or removes the configure flag
 assert expat != null;
 
-with stdenv.lib;
 stdenv.mkDerivation rec {
   name = "wayland-${version}";
-  version = "1.7.0";
+  version = "1.8.0";
 
   src = fetchurl {
     url = "http://wayland.freedesktop.org/releases/${name}.tar.xz";
-    sha256 = "173w0pqzk2m7hjlg15bymrx7ynxgq1ciadg03hzybxwnvfi4gsmx";
+    sha256 = "0h61bzm9mig9ajqw2sxdr16f69qs2l5bn3g9szdhvsh8f9mk9s1i";
   };
 
   configureFlags = [
@@ -31,13 +35,15 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ libffi expat ];
 
-  meta = {
-    description = "Reference implementation of the wayland protocol";
-    homepage    = http://wayland.freedesktop.org/;
-    license     = licenses.mit;
-    platforms   = platforms.linux;
-    maintainers = with maintainers; [ codyopel wkennington ];
+  passthru = {
+    inherit version;
   };
 
-  passthru.version = version;
+  meta = with stdenv.lib; {
+    description = "Reference implementation of the wayland protocol";
+    homepage = http://wayland.freedesktop.org/;
+    license = licenses.mit;
+    maintainers = with maintainers; [ codyopel wkennington ];
+    platforms = platforms.linux;
+  };
 }

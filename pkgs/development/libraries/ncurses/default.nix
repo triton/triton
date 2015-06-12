efe715ef3d6e2878b767c3d9237c8f21801bbfe9
William A. Kennington III <william@wkennington.com>
2015-06-12 15:27:20 -0700

    ncurses: More factoring

--- pkgs/development/libraries/ncurses/default.nix
+++ pkgs/development/libraries/ncurses/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl
+{ stdenv, fetchurl, pkgconfig, libtool
 
 # Optional Dependencies
 , gpm ? null
@@ -10,8 +10,6 @@
 
 with stdenv.lib;
 let
-  buildShared = !stdenv.isDarwin;
-
   optGpm = stdenv.shouldUsePkg gpm;
 in
 stdenv.mkDerivation rec {
@@ -25,40 +23,91 @@ stdenv.mkDerivation rec {
   # gcc-5.patch should be removed after 5.9
   patches = [ ./clang.patch ./gcc-5.patch ];
 
+  nativeBuildInputs = [ pkgconfig libtool ];
   buildInputs = [ optGpm ];
 
   configureFlags = [
-    (mkWith   true        "abi-version" abiVersion)
-    (mkWith   true        "cxx"         null)
-    (mkWith   true        "cxx-binding" null)
-    (mkWith   false       "ada"         null)
-    (mkWith   true        "manpages"    null)
-    (mkWith   true        "progs"       null)
-    (mkWith   doCheck     "tests"       null)
-    (mkWith   true        "curses-h"    null)
-    (mkEnable true        "pc-files"    null)
-    (mkWith   buildShared "shared"      null)
-    (mkWith   true        "normal"      null)
-    (mkWith   false       "debug"       null)
-    (mkWith   false       "termlib"     null)
-    (mkWith   false       "ticlib"      null)
-    (mkWith   optGpm      "gpm"         null)
-    (mkEnable true        "overwrite"   null)
-    (mkEnable true        "database"    null)
-    (mkWith   true        "xterm-new"   null)
-    (mkEnable true        "symlinks"    null)
-    (mkEnable unicode     "widec"       null)
-    (mkEnable true        "ext-colors"  null)
-    (mkEnable true        "ext-mouse"   null)
-  ] ++ stdenv.lib.optionals stdenv.isCygwin [
-    "--enable-sp-funcs"
-    "--enable-term-driver"
-    "--enable-const"
-    "--enable-ext-colors"
-    "--enable-ext-mouse"
-    "--enable-reentrant"
-    "--enable-colorfgbg"
-    "--enable-tcap-names"
+    (mkWith   true        "abi-version"   abiVersion)
+    (mkWith   true        "cxx"           null)
+    (mkWith   true        "cxx-binding"   null)
+    (mkWith   false       "ada"           null)
+    (mkWith   true        "manpages"      null)
+    (mkWith   true        "progs"         null)
+    (mkWith   doCheck     "tests"         null)
+    (mkWith   true        "curses-h"      null)
+    (mkEnable true        "pc-files"      null)
+    (mkEnable true        "mixed-case"    "auto")
+    (mkWith   true        "libtool"       null)
+    (mkWith   true        "shared"        null)
+    (mkWith   true        "normal"        null)
+    (mkWith   false       "debug"         null)
+    (mkWith   false       "profile"       null)
+    (mkWith   false       "termlib"       null)
+    (mkWith   false       "ticlib"        null)
+    (mkWith   optGpm      "gpm"           null)
+    (mkWith   true        "dlsym"         null)
+    (mkWith   true        "sysmouse"      "maybe")
+    (mkEnable true        "relink"        null)
+    (mkEnable true        "overwrite"     null)
+    (mkEnable true        "database"      null)
+    (mkWith   false       "hashed-db"     null)
+    (mkWith   true        "fallbacks"     "")
+    (mkWith   true        "xterm-new"     null)
+    # With terminfo-dirs
+    # With default-terminfo-dir
+    # Enable big-core: Autodetected
+    # Enable big-strings: Autodetected
+    (mkEnable false       "termcap"       null)
+    (mkWith   true        "termpath"      "\${out}/share/misc/termcap")
+    (mkEnable true        "getcap"        null)
+    (mkEnable false       "getcap-cache"  null)
+    (mkEnable true        "home-terminfo" null)
+    (mkEnable true        "root-environ"  null)
+    (mkEnable true        "symlinks"      null)
+    (mkEnable false       "broken-linker" null)
+    (mkEnable false       "bsdpad"        null)
+    (mkEnable unicode     "widec"         null)
+    (mkEnable true        "lp64"          null)
+    (mkEnable true        "tparm-varargs" null)
+    (mkWith   true        "tic-depends"   null)
+    (mkWith   true        "bool"          null)
+    # With caps
+    # With chtype
+    # With ospeed
+    # With mmask-t
+    # With ccharw-max
+    (mkWith   false       "rcs-ids"       null)
+    (mkEnable true        "ext-funcs"     null)
+    (mkEnable true        "sp-funcs"      null)
+    (mkEnable true        "term-driver"   null)
+    (mkEnable true        "const"         null)
+    (mkEnable true        "ext-colors"    null)
+    (mkEnable true        "ext-mouse"     null)
+    (mkEnable true        "no-padding"    null)
+    (mkEnable false       "signed-char"   null)
+    (mkEnable true        "sigwinch"      null)
+    (mkEnable true        "tcap-names"    null)
+    (mkWith   true        "devlop"        null)
+    (mkEnable true        "hard-tabs"     null)
+    (mkEnable true        "xmc-glitch"    null)
+    (mkWith   true        "assumed-color" null)
+    (mkEnable true        "hashmap"       null)
+    (mkEnable true        "colorfgbg"      null)
+    (mkEnable true        "interop"        null)
+    (mkWith   true        "pthread"        null)
+    (mkEnable false       "pthreads-eintr" null)
+    (mkEnable false       "weak-symbols"   null)
+    (mkEnable true        "reentrant"      null)
+    # With wrap-prefix
+    (mkEnable true        "safe-sprintf"   null)
+    (mkEnable true        "scroll-hints"   null)
+    (mkEnable false       "wgetch-events"  null)
+    (mkEnable true        "echo"           null)
+    (mkEnable true        "warnings"       null)
+    (mkEnable false       "assertions"     null)
+    (mkEnable false       "expanded"       null)
+    (mkEnable true        "macros"         null)
+    (mkWith   false       "trace"          null)
   ];
 
   # PKG_CONFIG_LIBDIR is where the *.pc files will be installed. If this
@@ -93,24 +142,27 @@ stdenv.mkDerivation rec {
     ln -svf . $out/include/ncursesw
     ln -svf . $out/include/ncurses
 
-    # Create non-unicode compatability
-    libs="$(find $out/lib -name \*w.a | sed 's,.*lib\(.*\)w.a.*,\1,g')"
+    suffix="$(awk -F': ' 'f{print $3; f=0} /default library suffix/{f=1}' config.log)"
+    libs="$(ls $out/lib/pkgconfig | tr ' ' '\n' | sed "s,\(.*\)$suffix\.pc,\1,g")"
+  '' + optionalString stdenv.isLinux ''
     for lib in $libs; do
-      if [ -e "$out/lib/lib''${lib}w.so" ]; then
-        ln -svf lib''${lib}w.so $out/lib/lib$lib.so
-        ln -svf lib''${lib}w.so.${abiVersion} $out/lib/lib$lib.so.${abiVersion}
+      if [ -e "$out/lib/lib''${lib}$suffix.so" ]; then
+        ln -svf lib''${lib}$suffix.so $out/lib/lib$lib.so
+        ln -svf lib''${lib}$suffix.so.${abiVersion} $out/lib/lib$lib.so.${abiVersion}
       fi
-      ln -svf lib''${lib}w.a $out/lib/lib$lib.a
-      ln -svf ''${lib}w.pc $out/lib/pkgconfig/$lib.pc
+      if [ -e "$out/lib/lib''${lib}$suffix.a" ]; then
+        ln -svf lib''${lib}$suffix.a $out/lib/lib$lib.a
+      fi
+      ln -svf lib''${lib}$suffix.la $out/lib/lib$lib.la
+      ln -svf ''${lib}$suffix.pc $out/lib/pkgconfig/$lib.pc
     done
 
     # Create curses compatability
-    ln -svf libncursesw.so $out/lib/libcursesw.so
-    ln -svf libncursesw.so $out/lib/libcurses.so
+    ln -svf libncurses.so $out/lib/libcurses.so
   '' + stdenv.lib.optionalString stdenv.isCygwin ''
     for lib in $libs; do
-      if test -e $out/lib/lib''${lib}w.dll.a; then
-          ln -svf lib''${lib}w.dll.a $out/lib/lib$lib.dll.a
+      if test -e $out/lib/lib''${lib}$suffix.dll.a; then
+        ln -svf lib''${lib}$suffix.dll.a $out/lib/lib$lib.dll.a
       fi
     done
   '') else ''

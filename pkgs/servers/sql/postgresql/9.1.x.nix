0fa53eb4f8ada8752019aec374690ddeb86bd9bb
William A. Kennington III <william@wkennington.com>
2015-06-18 23:20:27 -0700

    postgres: 9.1.16 -> 9.1.18

--- pkgs/servers/sql/postgresql/9.1.x.nix
+++ pkgs/servers/sql/postgresql/9.1.x.nix
@@ -2,10 +2,10 @@
 
 callPackage ./generic.nix (args // rec {
   psqlSchema = "9.1";
-  version = "${psqlSchema}.16";
+  version = "${psqlSchema}.18";
 
   src = fetchurl {
     url = "mirror://postgresql/source/v${version}/postgresql-${version}.tar.bz2";
-    sha256 = "0mllj1r1648iwm0qj3cj9qxizhlyhqmz94iydnwhf48psvvy4r9b";
+    sha256 = "1a44hmcvfaa8j169ladsibmvjakw6maaxqkzz1ab8139cqkda9i7";
   };
 })

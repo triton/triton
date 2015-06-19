153c2b9b6b45bd8788084acac630913503260367
William A. Kennington III <william@wkennington.com>
2015-06-18 23:16:37 -0700

    postgres: 9.0.20 -> 9.0.22

--- pkgs/servers/sql/postgresql/9.0.x.nix
+++ pkgs/servers/sql/postgresql/9.0.x.nix
@@ -2,10 +2,10 @@
 
 callPackage ./generic.nix (args // rec {
   psqlSchema = "9.0";
-  version = "${psqlSchema}.20";
+  version = "${psqlSchema}.22";
 
   src = fetchurl {
     url = "mirror://postgresql/source/v${version}/postgresql-${version}.tar.bz2";
-    sha256 = "0vxa90d1ghv6vg4c6kxvm2skypahvlq4sd968q7l9ff3dl145z02";
+    sha256 = "19gq6axjhvlr5zlrzwnll1fbrvai4xh0nb1jki6gmmschl6v5m4l";
   };
 })

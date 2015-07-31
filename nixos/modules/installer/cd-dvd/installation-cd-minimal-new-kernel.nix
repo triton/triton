471e0f1430605d9ef51776ffe566e14d359a32fa
William A. Kennington III <william@wkennington.com>
2015-07-30 23:01:13 -0700

    iso

--- nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel.nix
+++ nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel.nix
@@ -1,7 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ./installation-cd-minimal.nix ];
+  imports = [
+    /conf/nixos/common/base.nix
+    /conf/nixos/common/sshd.nix
+    ./installation-cd-minimal.nix
+  ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  fileSystems."/iso".options = "defaults,x-systemd.device-timeout=0";
+
+  systemd.services.sshd.wantedBy = pkgs.lib.mkOverride 0 [ "multi-user.target" ];
+  systemd.services.systemd-udev-settle.serviceConfig.TimeoutSec = 0;
 }

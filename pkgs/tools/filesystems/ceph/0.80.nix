393aab57d62f7095d988072ba9e33fa79e70e13a
William A. Kennington III <william@wkennington.com>
2015-11-26 23:33:47 -0800

    ceph: 0.80.10 -> 0.80.11

--- pkgs/tools/filesystems/ceph/0.80.nix
+++ pkgs/tools/filesystems/ceph/0.80.nix
@@ -1,12 +1,12 @@
 { callPackage, fetchgit, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "0.80.10";
+  version = "0.80.11";
 
   src = fetchgit {
     url = "git://github.com/ceph/ceph.git";
     rev = "refs/tags/v${version}";
-    sha256 = "1arajccczjdqp7igs17569xlq5cj4azcm5wwixg6ryypjr2grcbl";
+    sha256 = "1qa6bxbyzm7gn2xg28qmrai3ws4chkhcfcavrkbc128mf8i52kb8";
   };
 
   patches = [

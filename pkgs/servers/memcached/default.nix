b2cd5898d4fad526781852885ea1d41dbc12b5fc
William A. Kennington III <william@wkennington.com>
2015-12-03 12:44:35 -0800

    memcached: 1.4.20 -> 1.4.25

--- pkgs/servers/memcached/default.nix
+++ pkgs/servers/memcached/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, cyrus_sasl, libevent}:
 
 stdenv.mkDerivation rec {
-  name = "memcached-1.4.20";
+  name = "memcached-1.4.25";
 
   src = fetchurl {
     url = "http://memcached.org/files/${name}.tar.gz";
-    sha256 = "0620llasj8xgffk6hk2ml15z0c5i34455wwg60i1a2zdir023l95";
+    sha256 = "1njxg6mh8bzzhzqcmh0qswp33cpd7r7bp9m934ck4k927ixl6n7h";
   };
 
   buildInputs = [cyrus_sasl libevent];

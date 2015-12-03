4ff7e63ba29d819a92328394e1963b66c1e5cc81
William A. Kennington III <william@wkennington.com>
2015-12-03 12:54:20 -0800

    librsvg: 2.40.9 -> 2.40.12

--- pkgs/development/libraries/librsvg/default.nix
+++ pkgs/development/libraries/librsvg/default.nix
@@ -6,11 +6,11 @@
 # no introspection by default, it's too big
 
 stdenv.mkDerivation rec {
-  name = "librsvg-2.40.9";
+  name = "librsvg-2.40.12";
 
   src = fetchurl {
     url    = "mirror://gnome/sources/librsvg/2.40/${name}.tar.xz";
-    sha256 = "0fplymmqqr28y24vcnb01szn62pfbqhk8p1ngns54x9m6mflr5hk";
+    sha256 = "0l5mzwlw6k20hvndvk5xllks20xbddr7b93rsvs9jf5zg11hrr7z";
   };
 
   NIX_LDFLAGS = if stdenv.isDarwin then "-lintl" else null;

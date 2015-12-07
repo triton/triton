05655d63414caf1e417dfa79eb641ec031a7c980
William A. Kennington III <william@wkennington.com>
2015-12-07 10:48:47 -0800

    libbsd: 0.7.0 -> 0.8.0

--- pkgs/development/libraries/libbsd/default.nix
+++ pkgs/development/libraries/libbsd/default.nix
@@ -1,13 +1,14 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl, openssl }:
 
-let name = "libbsd-0.7.0";
-in stdenv.mkDerivation {
-  inherit name;
+stdenv.mkDerivation rec {
+  name = "libbsd-0.8.0";
 
   src = fetchurl {
     url = "http://libbsd.freedesktop.org/releases/${name}.tar.xz";
-    sha256 = "1fqhbi0vd6xjxazf633x388cc8qyn58l78704s0h6k63wlbhwfqg";
+    sha256 = "1373fdj6m57fz0pjnipyqq62k3p9bf3sr2k8ig3y8q6r9c435dzv";
   };
+
+  buildInputs = [ openssl ];
 
   patchPhase = ''
     substituteInPlace Makefile \
@@ -15,9 +16,10 @@ in stdenv.mkDerivation {
       --replace "{exec_prefix}" "{prefix}"
   '';
 
-  meta = { 
+  meta = with stdenv.lib; {
     description = "Common functions found on BSD systems";
     homepage = http://libbsd.freedesktop.org/;
-    license = stdenv.lib.licenses.bsd3;
+    license = licenses.bsd3;
+    platforms = platforms.linux;
   };
 }

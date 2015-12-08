b8ab818dd5b7ad2a06e96e2f242fae973981e654
William A. Kennington III <william@wkennington.com>
2015-12-08 09:19:26 -0800

    flashplugin: 11.2.202.540 -> 11.2.202.548

--- pkgs/applications/networking/browsers/mozilla-plugins/flashplayer-11/default.nix
+++ pkgs/applications/networking/browsers/mozilla-plugins/flashplayer-11/default.nix
@@ -36,7 +36,7 @@
 
 let
   # -> http://get.adobe.com/flashplayer/
-  version = "11.2.202.540";
+  version = "11.2.202.548";
 
   src =
     if stdenv.system == "x86_64-linux" then
@@ -47,7 +47,7 @@ let
       else rec {
         inherit version;
         url = "http://fpdownload.adobe.com/get/flashplayer/pdc/${version}/install_flash_player_11_linux.x86_64.tar.gz";
-        sha256 = "0zya9n5h669wbna182ig6dl4yf5sv4lvqk19rqhcwv3i718b0ai6";
+        sha256 = "0iz8317cjrg6lb5jn79ina5bik77waxpfg0xyhz26yg82shivv32";
       }
     else if stdenv.system == "i686-linux" then
       if debug then
@@ -60,7 +60,7 @@ let
       else rec {
         inherit version;
         url = "http://fpdownload.adobe.com/get/flashplayer/pdc/${version}/install_flash_player_11_linux.i386.tar.gz";
-        sha256 = "1n8ik5f257s388ql7gkmfh1iqil0g4kzxh3zsv2x8r6ssrvpq1by";
+        sha256 = "150j545pq5jzs55zamv5qnmx5ajy5n68riqmm21l76b0syzdzvw8";
       }
     else throw "Flash Player is not supported on this platform";
 

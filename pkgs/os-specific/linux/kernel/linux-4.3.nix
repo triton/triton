3f2782f79710a2e1fac58fc8717c90974016e3c6
William A. Kennington III <william@wkennington.com>
2015-12-11 09:28:41 -0800

    kernel: 4.3.1 -> 4.3.2

--- pkgs/os-specific/linux/kernel/linux-4.3.nix
+++ pkgs/os-specific/linux/kernel/linux-4.3.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.3.1";
+  version = "4.3.2";
   extraMeta.branch = "4.3";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0whvh6qxxa132y9pvlyzkyz6f6ss5bscs97md2w3hq2lwzvazyhi";
+    sha256 = "17df6476g80wxvmiwmh0pzd7xaids3xqjn6l6i51pwj376crqs17";
   };
 
   features.iwlwifi = true;

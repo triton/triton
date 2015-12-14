f65c31cc4fce1791fe73cc911657d8410f6e181d
William A. Kennington III <william@wkennington.com>
2015-12-14 09:34:57 -0800

    libsigc++: 2.3.1 -> 2.6.2

--- pkgs/development/libraries/libsigcxx/default.nix
+++ pkgs/development/libraries/libsigcxx/default.nix
@@ -1,19 +1,20 @@
 { stdenv, fetchurl, pkgconfig, gnum4 }:
 
 stdenv.mkDerivation rec {
-  name = "libsigc++-2.3.1";
+  name = "libsigc++-2.6.2";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/libsigc++/2.3/${name}.tar.xz";
-    sha256 = "14q3sq6d43f6wfcmwhw4v1aal4ba0h5x9v6wkxy2dnqznd95il37";
+    url = "mirror://gnome/sources/libsigc++/2.6/${name}.tar.xz";
+    sha256 = "0ds4wlys149gi320xiy452dr0mq6r94r03sp2wn7kpii9h9ygb7x";
   };
 
-  buildInputs = [ pkgconfig gnum4 ];
+  nativeBuildInputs = [ pkgconfig gnum4 ];
 
   doCheck = true;
 
-  meta = {
+  meta = with stdenv.lib; {
     homepage = http://libsigc.sourceforge.net/;
     description = "A typesafe callback system for standard C++";
+    platforms = platforms.all;
   };
 }

f3b00bfe3642e7fe6c2afd732d1762be103d202e
William A. Kennington III <william@wkennington.com>
2015-12-15 20:42:04 -0800

    libcroco: 0.6.8 -> 0.6.10

--- pkgs/desktops/gnome-3/3.18/core/libcroco/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/libcroco/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, pkgconfig, libxml2, glib }:
 
 stdenv.mkDerivation rec {
-  name = "libcroco-0.6.8";
+  name = "libcroco-0.6.10";
 
   src = fetchurl {
     url = "mirror://gnome/sources/libcroco/0.6/${name}.tar.xz";
-    sha256 = "0w453f3nnkbkrly7spx5lx5pf6mwynzmd5qhszprq8amij2invpa";
+    sha256 = "0v14ajhjdhsnlwp2q65629r53hgc0rrzr31653xw9xbpvw8nc1kj";
   };
 
   outputs = [ "out" "doc" ];

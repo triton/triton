686ce04466cec50618eb08e9c6d7efa8014c73a7
William A. Kennington III <william@wkennington.com>
2015-12-15 20:54:09 -0800

    libbsd: 0.8.0 -> 0.8.1

--- pkgs/development/libraries/libbsd/default.nix
+++ pkgs/development/libraries/libbsd/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, openssl }:
 
 stdenv.mkDerivation rec {
-  name = "libbsd-0.8.0";
+  name = "libbsd-0.8.1";
 
   src = fetchurl {
     url = "http://libbsd.freedesktop.org/releases/${name}.tar.xz";
-    sha256 = "1373fdj6m57fz0pjnipyqq62k3p9bf3sr2k8ig3y8q6r9c435dzv";
+    sha256 = "1c7r58y3jz0251y7l54jcyik4xr4y2lki7v8kf9ww2vjmn0qgg5d";
   };
 
   buildInputs = [ openssl ];

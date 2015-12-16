6f2b33a48448911eecbc41fa302813e966d416d7
William A. Kennington III <william@wkennington.com>
2015-12-15 20:50:59 -0800

    gegl: 0.2.0 -> 0.3.4

--- pkgs/development/libraries/gegl/default.nix
+++ pkgs/development/libraries/gegl/default.nix
@@ -1,27 +1,24 @@
 { stdenv, fetchurl, pkgconfig, glib, babl, libpng, cairo, libjpeg
-, librsvg, pango, gtk, bzip2, intltool }:
+, librsvg, pango, bzip2, intltool, gdk_pixbuf, openexr, SDL
+, jasper, exiv2 }:
 
 stdenv.mkDerivation rec {
-  name = "gegl-0.2.0";
+  name = "gegl-0.3.4";
 
   src = fetchurl {
-    url = "ftp://ftp.gtk.org/pub/gegl/0.2/${name}.tar.bz2";
+    url = "https://download.gimp.org/pub/gegl/0.3/${name}.tar.bz2";
     sha256 = "df2e6a0d9499afcbc4f9029c18d9d1e0dd5e8710a75e17c9b1d9a6480dd8d426";
   };
 
-  patches = [( fetchurl {
-    url = "https://projects.archlinux.org/svntogit/packages.git/plain/trunk/"
-      + "gegl-0.2.0-CVE-2012-4433.patch?h=packages/gegl&id=57a60fbda5d7bbbd1cc4767cb0724baa80c5e3e9";
-    sha256 = "0p8mxj3w09nn1cc6cbxrd9hx742c5y27903i608wx6ja3kdjis59";
-    name = "CVE-2012-4433.patch";
-  })];
-
   # needs fonts otherwise  don't know how to pass them
-  configureFlags = "--disable-docs";
+  configureFlags = [ "--disable-docs" ];
 
   NIX_LDFLAGS = if stdenv.isDarwin then "-lintl" else null;
 
-  buildInputs = [ babl libpng cairo libjpeg librsvg pango gtk bzip2 intltool ];
+  buildInputs = [
+    babl glib intltool cairo pango gdk_pixbuf libjpeg openexr librsvg SDL
+    jasper exiv2
+  ];
 
   nativeBuildInputs = [ pkgconfig ];
 

5fa9a012684c38a2b5808da75f0f417872c77f8b
William A. Kennington III <william@wkennington.com>
2015-12-15 21:04:09 -0800

    alsa-plugins: 1.0.29 -> 1.1.0

--- pkgs/os-specific/linux/alsa-plugins/default.nix
+++ pkgs/os-specific/linux/alsa-plugins/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, lib, pkgconfig, alsaLib, libogg, libpulseaudio ? null, libjack2 ? null }:
 
 stdenv.mkDerivation rec {
-  name = "alsa-plugins-1.0.29";
+  name = "alsa-plugins-1.1.0";
 
   src = fetchurl {
     urls = [
       "ftp://ftp.alsa-project.org/pub/plugins/${name}.tar.bz2"
       "http://alsa.cybermirror.org/plugins/${name}.tar.bz2"
     ];
-    sha256 = "0ck5xa0vnjhn5w23gf87y30h7bcb6hzsx4817sw35xl5qb58ap9j";
+    sha256 = "1vn9n996d5i1addb9wrr958ycps59bn09bi5zvsrkvrvjllw70rv";
   };
 
   # ToDo: a52, etc.?

ca6fafeea88186f4f892fab9acbf7666e940e7c7
William A. Kennington III <william@wkennington.com>
2015-12-15 20:48:45 -0800

    atkmm: 2.22.7 -> 2.24.2

--- pkgs/development/libraries/atkmm/default.nix
+++ pkgs/development/libraries/atkmm/default.nix
@@ -1,14 +1,13 @@
 { stdenv, fetchurl, atk, glibmm, pkgconfig }:
 
 stdenv.mkDerivation rec {
-  name = "atkmm-2.22.7";
+  name = "atkmm-2.24.2";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/atkmm/2.22/${name}.tar.xz";
-    sha256 = "06zrf2ymml2dzp53sss0d4ch4dk9v09jm8rglnrmwk4v81mq9gxz";
+    url = "mirror://gnome/sources/atkmm/2.24/${name}.tar.xz";
+    sha256 = "ff95385759e2af23828d4056356f25376cfabc41e690ac1df055371537e458bd";
   };
 
-  propagatedBuildInputs = [ atk glibmm ];
-
   nativeBuildInputs = [ pkgconfig ];
+  propagatedBuildInputs = [ atk glibmm ];
 }

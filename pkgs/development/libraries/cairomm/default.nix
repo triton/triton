5f7c089e6553f43b11c6a055abcdf7c9b31ce7ae
William A. Kennington III <william@wkennington.com>
2015-12-15 20:49:11 -0800

    cairomm: 1.11.2 -> 1.12.0

--- pkgs/development/libraries/cairomm/default.nix
+++ pkgs/development/libraries/cairomm/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv, pkgconfig, cairo, xlibsWrapper, fontconfig, freetype, libsigcxx }:
 
 stdenv.mkDerivation rec {
-  name = "cairomm-1.11.2";
+  name = "cairomm-1.12.0";
 
   src = fetchurl {
     url = "http://cairographics.org/releases/${name}.tar.gz";
-    sha1 = "35e190a03f760924bece5dc1204cc36b3583c806";
+    sha256 = "1k3lb3jwnk5nm4s5cfm5kk8kl4b066chis4inws6k5yxdzn5lhsh";
   };
 
   buildInputs = [ pkgconfig ];

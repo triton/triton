c59fa28d342e0c64cd80d1de3ab1179206ecdcc4
William A. Kennington III <william@wkennington.com>
2015-12-15 20:50:31 -0800

    gd: 2.0.35 -> 2.1.1

--- pkgs/development/libraries/gd/default.nix
+++ pkgs/development/libraries/gd/default.nix
@@ -1,18 +1,17 @@
 {stdenv, fetchurl, zlib, libpng, freetype, libjpeg, fontconfig}:
 
-stdenv.mkDerivation {
-  name = "gd-2.0.35";
+stdenv.mkDerivation rec {
+  name = "gd-2.1.1";
   
   src = fetchurl {
-    url = http://www.libgd.org/releases/gd-2.0.35.tar.bz2;
-    sha256 = "1y80lcmb8qbzf0a28841zxhq9ndfapmh2fsrqfd9lalxfj8288mz";
+    url = "https://github.com/libgd/libgd/releases/download/${name}/lib${name}.tar.xz";
+    sha256 = "11djy9flzxczphigqgp7fbbblbq35gqwwhn9xfcckawlapa1xnls";
   };
   
-  buildInputs = [zlib libpng freetype];
+  buildInputs = [ zlib libpng freetype ];
+  propagatedBuildInputs = [ libjpeg fontconfig ];
 
-  propagatedBuildInputs = [libjpeg fontconfig]; # urgh
-
-  configureFlags = "--without-x";
+  configureFlags = [ "--without-x" ];
 
   meta = {
     homepage = http://www.libgd.org/;

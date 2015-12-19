47ba1bc1a217c33dbf771a7d4b9dd2f65063e9af
William A. Kennington III <william@wkennington.com>
2015-12-19 00:23:06 -0800

    Add a setup-hook for absolute package config paths

--- pkgs/stdenv/linux/default.nix
+++ pkgs/stdenv/linux/default.nix
@@ -301,7 +301,7 @@ rec {
     allowedRequisites = with stage4.pkgs;
       [ gzip bzip2 xz bash binutils coreutils diffutils findutils gawk
         glibc gnumake gnused gnutar gnugrep gnupatch patchelf attr acl
-        paxctl zlib pcre linuxHeaders ed gcc gcc.cc libsigsegv
+        paxctl zlib pcre linuxHeaders ed gcc gcc.cc libsigsegv pkgconfig
       ];
 
     overrides = pkgs: {

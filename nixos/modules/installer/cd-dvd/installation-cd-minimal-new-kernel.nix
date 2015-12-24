98f858398abb7b18064dffb67e80de8e0503303e
William A. Kennington III <william@wkennington.com>
2015-12-23 17:41:00 -0800

    Revert "iso"
    This reverts commit 471e0f1430605d9ef51776ffe566e14d359a32fa.

--- nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel.nix
+++ nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel.nix
@@ -1,14 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    /conf/nixos/common/base.nix
-    /conf/nixos/common/sshd.nix
-    ./installation-cd-minimal.nix
-  ];
+  imports = [ ./installation-cd-minimal.nix ];
 
-  fileSystems."/iso".options = "defaults,x-systemd.device-timeout=0";
-
-  systemd.services.sshd.wantedBy = pkgs.lib.mkOverride 0 [ "multi-user.target" ];
-  systemd.services.systemd-udev-settle.serviceConfig.TimeoutSec = 0;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 }

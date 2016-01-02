ead0a0473b1b0c774a5c4680b4c2c421a09a44d4
William A. Kennington III <william@wkennington.com>
2016-01-02 15:01:45 -0800

    libwpd: 0.10.0 -> 0.10.1

--- pkgs/development/libraries/libwpd/default.nix
+++ pkgs/development/libraries/libwpd/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, zlib, pkgconfig, glib, libgsf, libxml2, librevenge }:
 
 stdenv.mkDerivation rec {
-  name = "libwpd-0.10.0";
+  name = "libwpd-0.10.1";
   
   src = fetchurl {
     url = "mirror://sourceforge/libwpd/${name}.tar.xz";
-    sha256 = "0b6krzr6kxzm89g6bapn805kdayq70hn16n5b5wfs2lwrf0ag2wx";
+    sha256 = "1xl457xl0zddb2dy3zp35lra67kp271bai45vkpdkybhqw5l3lq9";
   };
   
   buildInputs = [ glib libgsf libxml2 zlib librevenge ];

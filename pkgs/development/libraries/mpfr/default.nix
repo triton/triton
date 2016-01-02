06c93d6c95971b42f7f5178897fd7fb676250d6a
William A. Kennington III <william@wkennington.com>
2016-01-02 15:02:04 -0800

    mpfr: 3.1.3-p3 -> 3.1.3-p5

--- pkgs/development/libraries/mpfr/default.nix
+++ pkgs/development/libraries/mpfr/default.nix
@@ -1,21 +1,36 @@
 { stdenv, fetchurl, gmp }:
 
+let
+  patchSha256s = {
+    "patch01" = "1hhpl0wg3gmv5m5kxc5r3n7vzvjnil4jx6rvk99gmfwylsjqx8lf";
+    "patch02" = "19gvq283yspz7yajm4q2ys03hpxj0m5l8ahrkmccjzaqd07jwq53";
+    "patch03" = "0p1y7mwbnshbj81hg5nfprzwh4aakxszf7n8bn4kh66jka4cw79c";
+    "patch04" = "0vp2ywg8p0a2g8wai4fw29mbx1nq5yjzd361h2mr4cn6vac4a3sz";
+    "patch05" = "1y0izl4y17xrdjidl2p1m1vapmgci5qv6shk0aqd187nvrs72jgw";
+  };
+in
+with stdenv.lib;
 stdenv.mkDerivation rec {
-  name = "mpfr-3.1.3";
+  name = "mpfr-${version}-p${toString (length patches)}";
+  version = "3.1.3";
 
   src = fetchurl {
-    url = "mirror://gnu/mpfr/${name}.tar.bz2";
+    url = "mirror://gnu/mpfr/mpfr-${version}.tar.bz2";
     sha256 = "1z8akfw9wbmq91vrx04bw86mmnxw2sw5qm5cr8ix5b3w2mcv8fzn";
   };
 
-  patches = [ ./upstream.patch ];
+  patches = flip mapAttrsToList patchSha256s (n: sha256: fetchurl {
+    name = "mpfr-${version}-${n}";
+    url = "http://www.mpfr.org/mpfr-${version}/${n}";
+    inherit sha256;
+  });
 
   # mpfr.h requires gmp.h
   propagatedBuildInputs = [ gmp ];
 
   configureFlags =
-    stdenv.lib.optional stdenv.isSunOS "--disable-thread-safe" ++
-    stdenv.lib.optional stdenv.is64bit "--with-pic";
+    optional stdenv.isSunOS "--disable-thread-safe" ++
+    optional stdenv.is64bit "--with-pic";
 
   doCheck = true;
 
@@ -37,9 +52,9 @@ stdenv.mkDerivation rec {
       floating-point arithmetic (53-bit mantissa).
     '';
 
-    license = stdenv.lib.licenses.lgpl2Plus;
+    license = licenses.lgpl2Plus;
 
     maintainers = [ ];
-    platforms = stdenv.lib.platforms.all;
+    platforms = platforms.all;
   };
 }

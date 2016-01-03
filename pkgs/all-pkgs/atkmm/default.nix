8859b89a8e01c5909c9cf7e6152fa471e7bb9438
Cody Opel <codyopel@gmail.com>
2016-01-03 06:45:27 -0500

    atkmm: refactor

--- pkgs/all-pkgs/atkmm/default.nix
+++ pkgs/all-pkgs/atkmm/default.nix
@@ -1,13 +1,46 @@
-{ stdenv, fetchurl, atk, glibmm, pkgconfig }:
+{ stdenv
+, fetchurl
+
+, atk
+, glibmm
+}:
 
 stdenv.mkDerivation rec {
-  name = "atkmm-2.24.2";
+  name = "atkmm-${version}";
+  versionMajor = "2.24";
+  versionMinor = "2";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/atkmm/2.24/${name}.tar.xz";
-    sha256 = "ff95385759e2af23828d4056356f25376cfabc41e690ac1df055371537e458bd";
+    url = "mirror://gnome/sources/atkmm/${versionMajor}/${name}.tar.xz";
+    sha256 = "1gaqwhviadsmy0fsr47686yglv1p4mpkamj0in127bz2b5bki5gz";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
-  propagatedBuildInputs = [ atk glibmm ];
+  configureFlags = [
+    "--enable-deprecated-api"
+    "--disable-documentation"
+    "--without-libstdc-doc"
+    "--without-libsigc-doc"
+    "--without-glibmm-doc"
+  ];
+
+  propagatedBuildInputs = [
+    atk
+    glibmm
+  ];
+
+  enableParallelBuilding = true;
+
+  meta = with stdenv.lib; {
+    description = "C++ interface for the ATK library";
+    homepage = http://www.gtkmm.org;
+    license = licenses.lgpl21Plus;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
+  };
 }

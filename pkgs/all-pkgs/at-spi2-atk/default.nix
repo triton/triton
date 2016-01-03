a6567ca06055bde5021e703e9a151a2c35eea324
Cody Opel <codyopel@gmail.com>
2016-01-03 05:36:30 -0500

    at-spi2-atk: 2.18.0 -> 2.18.1

--- pkgs/all-pkgs/at-spi2-atk/default.nix
+++ pkgs/all-pkgs/at-spi2-atk/default.nix
@@ -1,21 +1,75 @@
-{ stdenv, fetchurl, python, popt, atk, xorg
-, intltool, dbus_glib, at_spi2_core }:
+{ stdenv
+, fetchurl
+, intltool
+, pkgconfig
+, python
+
+, atk
+, at-spi2-core
+, dbus_glib
+, glib
+, popt
+, xorg
+
+, libxml2
+}:
+
+with {
+  inherit (stdenv.lib)
+    optionals
+    wtFlag;
+};
 
 stdenv.mkDerivation rec {
+  name = "at-spi2-atk-${version}";
   versionMajor = "2.18";
-  versionMinor = "0";
-  moduleName   = "at-spi2-atk";
-  name = "${moduleName}-${versionMajor}.${versionMinor}";
+  versionMinor = "1";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/${moduleName}/${versionMajor}/${name}.tar.xz";
-    sha256 = "0xgkrnx04vaklbkzc7bzym9s0qhj8aiz4knqlxgx3vxnacsb6vaa";
+    url = "mirror://gnome/sources/at-spi2-atk/${versionMajor}/${name}.tar.xz";
+    sha256 = "0bf1g5cj84rmx7p1q547vwbc0hlpcs2wrxnmv96lckfkhs9mzcf4";
   };
 
-  buildInputs = [ python popt atk xorg.libX11 xorg.libICE xorg.libXtst xorg.libXi
-                  intltool dbus_glib at_spi2_core xorg.libSM ];
+  configureFlags = [
+    "--enable-schemas-compile"
+    "--enable-p2p"
+    (wtFlag "tests" doCheck null)
+  ];
+
+  nativeBuildInputs = [
+    intltool
+    python
+  ];
+
+  buildInputs = [
+    atk
+    dbus_glib
+    glib
+    popt
+    at-spi2-core
+    xorg.libICE
+    xorg.libSM
+    xorg.libX11
+    xorg.libXi
+    xorg.libXtst
+  ] ++ optionals doCheck [
+    libxml2
+  ];
+
+  doCheck = false;
+  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
-    platforms = platforms.unix;
+    description = "Gtk module for bridging AT-SPI to Atk";
+    homepage = https://wiki.gnome.org/Accessibility;
+    license = licenses.lgpl2Plus;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }

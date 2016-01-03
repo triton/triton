40d726c7fb974b2213571d69120c21bc2481d6f0
Cody Opel <codyopel@gmail.com>
2016-01-03 05:37:07 -0500

    atk: refactor

--- pkgs/all-pkgs/atk/default.nix
+++ pkgs/all-pkgs/atk/default.nix
@@ -1,44 +1,57 @@
-{ stdenv, fetchurl, pkgconfig, perl, glib, libintlOrEmpty, gobjectIntrospection }:
+{ stdenv
+, gettext
+, fetchurl
+, pkgconfig
+, perl
 
-let
-  ver_maj = "2.18";
-  ver_min = "0";
-in
+, glib
+, gobject-introspection
+}:
+
 stdenv.mkDerivation rec {
-  name = "atk-${ver_maj}.${ver_min}";
+  name = "atk-${version}";
+  versionMajor = "2.18";
+  versionMinor = "0";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/atk/${ver_maj}/${name}.tar.xz";
-    sha256 = "ce6c48d77bf951083029d5a396dd552d836fff3c1715d3a7022e917e46d0c92b";
+    url = "mirror://gnome/sources/atk/${versionMajor}/${name}.tar.xz";
+    sha256 = "0ay9s137x49f0akx658p7kznz0rdapfrd8ym54q0hlgrggblhv6f";
   };
 
-  buildInputs = libintlOrEmpty;
+  configureFlags = [
+    "--enable-rebuilds"
+    "--enable-glibtest"
+    "--enable-introspection"
+    "--disable-gtk-doc"
+    "--disable-gtk-doc-html"
+    "--disable-gtk-doc-pdf"
+  ];
 
-  nativeBuildInputs = [ pkgconfig perl ];
+  nativeBuildInputs = [
+    gettext
+    perl
+  ];
 
-  propagatedBuildInputs = [ glib gobjectIntrospection /*ToDo: why propagate*/ ];
+  buildInputs = [
+    glib
+    gobject-introspection
+  ];
 
-  #doCheck = true; # no checks in there (2.10.0)
+  postInstall = "rm -rvf $out/share/gtk-doc";
 
-  postInstall = "rm -rf $out/share/gtk-doc";
+  enableParallelBuilding = true;
 
-  meta = {
-    description = "Accessibility toolkit";
-
-    longDescription = ''
-      ATK is the Accessibility Toolkit.  It provides a set of generic
-      interfaces allowing accessibility technologies such as screen
-      readers to interact with a graphical user interface.  Using the
-      ATK interfaces, accessibility tools have full access to view and
-      control running applications.
-    '';
-
+  meta = with stdenv.lib; {
+    description = "GTK+ & GNOME Accessibility Toolkit";
     homepage = http://library.gnome.org/devel/atk/;
-
-    license = stdenv.lib.licenses.lgpl2Plus;
-
-    maintainers = with stdenv.lib.maintainers; [ raskin urkud ];
-    platforms = stdenv.lib.platforms.linux ++ stdenv.lib.platforms.darwin;
+    license = licenses.lgpl2Plus;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
-
 }

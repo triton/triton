bea0d2ff7c22a2aff96542fb7db34cca23702db2
Cody Opel <codyopel@gmail.com>
2016-01-03 05:37:52 -0500

    at-spi2-core: 2.18.0 -> 2.18.3

--- pkgs/all-pkgs/at-spi2-core/default.nix
+++ pkgs/all-pkgs/at-spi2-core/default.nix
@@ -1,31 +1,69 @@
-{ stdenv, fetchurl, python, popt, intltool, dbus_glib, xorg, gobjectIntrospection }:
+{ stdenv
+, fetchurl
+, intltool
+, pkgconfig
+, python
+
+, dbus_glib
+, glib
+, gobject-introspection
+, popt
+, xorg
+}:
 
 stdenv.mkDerivation rec {
+  name = "at-spi2-core-${version}";
   versionMajor = "2.18";
-  versionMinor = "0";
-  moduleName   = "at-spi2-core";
-  name = "${moduleName}-${versionMajor}.${versionMinor}";
+  versionMinor = "3";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/${moduleName}/${versionMajor}/${name}.tar.xz";
-    sha256 = "0xna0gnlqvzy6209klirywcm7ianazshg6pkk828g07bnrywgvhs";
+    url = "mirror://gnome/sources/at-spi2-core/${versionMajor}/${name}.tar.xz";
+    sha256 = "0afn4x04j5l352vj0dccb2hkpzg3l2vhr8h1yv89fpqmjkfnm8md";
   };
 
-  outputs = [ "out" "doc" ];
-
-  buildInputs = [
-    python popt intltool dbus_glib
-    xorg.libX11 xorg.xextproto xorg.libSM xorg.libICE xorg.libXtst xorg.libXi
-    gobjectIntrospection
+  configureFlags = [
+    "--enable-nls"
+    "--enable-x11"
+    "--disable-xevie"
+    "--enable-introspection"
+    "--disable-gtk-doc"
+    "--disable-gtk-doc-html"
+    "--disable-gtk-doc-pdf"
+    "--with-x"
+    "--with-dbus-daemondir=/run/current-system/sw/bin/"
   ];
 
-  # ToDo: on non-NixOS we create a symlink from there?
-  configureFlags = "--with-dbus-daemondir=/run/current-system/sw/bin/";
+  nativeBuildInputs = [
+    intltool
+    python
+  ];
 
-  NIX_LDFLAGS = with stdenv; lib.optionalString isDarwin "-lintl";
+  buildInputs = [
+    dbus_glib
+    glib
+    popt
+    gobject-introspection
+    xorg.libICE
+    xorg.libSM
+    xorg.libX11
+    xorg.libXi
+    xorg.libXtst
+    xorg.xextproto
+  ];
+
+  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
-    platforms = platforms.unix;
+    description = "D-Bus accessibility specifications and registration daemon";
+    homepage = https://wiki.gnome.org/Accessibility;
+    license = licenses.lgpl2Plus;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }
-

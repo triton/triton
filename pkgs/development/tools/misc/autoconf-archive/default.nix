51292f8829ba3e46a7ef238c893df249ac65868e
William A. Kennington III <william@wkennington.com>
2016-01-04 14:26:48 -0800

    autoconf-archive: Cleanup

--- pkgs/development/tools/misc/autoconf-archive/default.nix
+++ pkgs/development/tools/misc/autoconf-archive/default.nix
@@ -1,4 +1,5 @@
-{ stdenv, fetchurl, xz }:
+{ stdenv, fetchurl }:
+
 stdenv.mkDerivation rec {
   name = "autoconf-archive-${version}";
   version = "2015.09.25";
@@ -7,7 +8,6 @@ stdenv.mkDerivation rec {
     url = "mirror://gnu/autoconf-archive/autoconf-archive-${version}.tar.xz";
     sha256 = "02im1jn0igzn2qpxkgiwxvcm3jgvjaypg955pi9h2d6jvfjnf13w";
   };
-  buildInputs = [ xz ];
 
   meta = with stdenv.lib; {
     description = "Archive of autoconf m4 macros";

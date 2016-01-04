68400936fbbb78ba489324349c66fd55873ff627
Cody Opel <codyopel@gmail.com>
2016-01-04 04:20:01 -0500

    pango: refactor

--- pkgs/all-pkgs/pango/default.nix
+++ pkgs/all-pkgs/pango/default.nix
@@ -1,51 +1,80 @@
-{ stdenv, fetchurl, pkgconfig, xlibsWrapper, glib, cairo, libpng, harfbuzz
-, fontconfig, freetype, libintlOrEmpty, gobjectIntrospection
+{ stdenv
+, fetchurl
+
+, cairo
+, fontconfig
+, freetype
+, glib
+, gobject-introspection
+, harfbuzz
+, libpng
+, xorg
+#, LibThai
 }:
 
-let
-  ver_maj = "1.38";
-  ver_min = "1";
-in
+with {
+  inherit (stdenv.lib)
+    optionalString
+    wtFlag;
+};
+
 stdenv.mkDerivation rec {
-  name = "pango-${ver_maj}.${ver_min}";
+  name = "pango-${version}";
+  versionMajor = "1.38";
+  versionMinor = "1";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/pango/${ver_maj}/${name}.tar.xz";
-    sha256 = "1320569f6c6d75d6b66172b2d28e59c56ee864ee9df202b76799c4506a214eb7";
+    url = "mirror://gnome/sources/pango/${versionMajor}/${name}.tar.xz";
+    sha256 = "1dsf45m51i4rcyvh5wlxxrjfhvn5b67d5ckjc6vdcxbddjgmc80k";
   };
 
-  buildInputs = with stdenv.lib; [ gobjectIntrospection ]
-    ++ optional stdenv.isDarwin fontconfig;
-  nativeBuildInputs = [ pkgconfig ];
+  configureFlags = [
+    "--enable-rebuilds"
+    "--enable-introspection"
+    "--disable-gtk-doc"
+    "--disable-gtk-doc-html"
+    "--disable-gtk-doc-pdf"
+    "--disable-doc-cross-reference"
+    "--enable-Bsymbolic"
+    "--enable-installed-tests"
+    (wtFlag "xft" (xorg.libXft != null) null)
+    (wtFlag "cairo" (cairo != null) null)
+  ];
 
-  propagatedBuildInputs = [ xlibsWrapper glib cairo libpng fontconfig freetype harfbuzz ] ++ libintlOrEmpty;
+  buildInputs = [
+    cairo
+    fontconfig
+    freetype
+    glib
+    gobject-introspection
+    harfbuzz
+    libpng
+    xorg.libX11
+    xorg.libXft
+    xorg.libXrender
+  ];
 
+  postInstall = "rm -rvf $out/share/gtk-doc";
+
+  preCheck = optionalString doCheck ''
+    # Fontconfig fails to load default config in test
+    export FONTCONFIG_FILE="${fontconfig}/etc/fonts/fonts.conf"
+  '';
+
+  doCheck = true;
   enableParallelBuilding = true;
 
-  doCheck = false; # test-layout fails on 1.38.0
-  # jww (2014-05-05): The tests currently fail on Darwin:
-  #
-  # ERROR:testiter.c:139:iter_char_test: assertion failed: (extents.width == x1 - x0)
-  # .../bin/sh: line 5: 14823 Abort trap: 6 srcdir=. PANGO_RC_FILE=./pangorc ${dir}$tst
-  # FAIL: testiter
-
-  postInstall = "rm -rf $out/share/gtk-doc";
-
   meta = with stdenv.lib; {
-    description = "A library for laying out and rendering of text, with an emphasis on internationalization";
-
-    longDescription = ''
-      Pango is a library for laying out and rendering of text, with an
-      emphasis on internationalization.  Pango can be used anywhere
-      that text layout is needed, though most of the work on Pango so
-      far has been done in the context of the GTK+ widget toolkit.
-      Pango forms the core of text and font handling for GTK+-2.x.
-    '';
-
+    description = "A library for laying out and rendering of text";
     homepage = http://www.pango.org/;
     license = licenses.lgpl2Plus;
-
-    maintainers = with maintainers; [ raskin urkud ];
-    platforms = with platforms; linux ++ darwin;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }

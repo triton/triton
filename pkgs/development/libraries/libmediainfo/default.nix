18908284aca6b39a281ba6205f14f63288fcdd14
William A. Kennington III <william@wkennington.com>
2016-01-04 14:27:41 -0800

    libmediainfo: 0.7.80 -> 0.7.81

--- pkgs/development/libraries/libmediainfo/default.nix
+++ pkgs/development/libraries/libmediainfo/default.nix
@@ -1,19 +1,29 @@
-{ stdenv, fetchurl, automake, autoconf, libtool, pkgconfig, libzen, zlib }:
+{ stdenv, fetchurl, automake, autoconf, libtool, libzen, zlib, curl, libmms }:
 
 stdenv.mkDerivation rec {
-  version = "0.7.80";
+  version = "0.7.81";
   name = "libmediainfo-${version}";
+
   src = fetchurl {
     url = "http://mediaarea.net/download/source/libmediainfo/${version}/libmediainfo_${version}.tar.xz";
-    sha256 = "0v9px37qx0dkx67gqwi1rd9x4m7zm1ml8sdj5fx0isj6qymbd1z5";
+    sha256 = "0hzfrg7n7wlnwq28hmpxczis1k8x73wbwlsmfkshvqcwi7lva0cs";
   };
 
-  buildInputs = [ automake autoconf libtool pkgconfig libzen zlib ];
+  nativeBuildInputs = [ automake autoconf libtool ];
+  buildInputs = [ libzen zlib curl libmms ];
 
   sourceRoot = "./MediaInfoLib/Project/GNU/Library/";
 
-  configureFlags = [ "--enable-shared" ];
-  preConfigure = "sh autogen.sh";
+  preConfigure = ''
+    sh autogen.sh
+    cat configure.ac
+  '';
+
+  configureFlags = [
+    "--enable-shared"
+    "--with-libcurl"
+    "--with-libmms"
+  ];
 
   postInstall = ''
     install -vD -m 644 libmediainfo.pc "$out/lib/pkgconfig/libmediainfo.pc"

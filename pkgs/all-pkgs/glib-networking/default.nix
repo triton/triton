19c9208fa432f3270900185b437fe0f0fb4b578c
Cody Opel <codyopel@gmail.com>
2016-01-03 22:34:21 -0500

    glib-networking: 2.44.0 -> 2.46.1

--- pkgs/all-pkgs/glib-networking/default.nix
+++ pkgs/all-pkgs/glib-networking/default.nix
@@ -1,33 +1,73 @@
-{ stdenv, fetchurl, pkgconfig, glib, intltool, gnutls, libproxy
-, gsettings_desktop_schemas }:
+{ stdenv
+, fetchurl
+, intltool
 
-let
-  ver_maj = "2.44";
-  ver_min = "0";
-in
+, glib
+, gnutls
+, gsettings_desktop_schemas
+, libproxy
+, p11_kit
+}:
+
+with {
+  inherit (stdenv.lib)
+    wtFlag;
+};
+
 stdenv.mkDerivation rec {
-  name = "glib-networking-${ver_maj}.${ver_min}";
+  name = "glib-networking-${version}";
+  versionMajor = "2.46";
+  versionMinor = "1";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/glib-networking/${ver_maj}/${name}.tar.xz";
-    sha256 = "8f8a340d3ba99bfdef38b653da929652ea6640e27969d29f7ac51fbbe11a4346";
+    url = "mirror://gnome/sources/glib-networking/${versionMajor}/${name}.tar.xz";
+    sha256 = "1cchmi08jpjypgmm9i7xzh5qfg2q5k61kry9ns8mhw3z44a440ym";
   };
 
-  configureFlags = "--with-ca-certificates=/etc/ssl/certs/ca-certificates.crt";
+  configureFlags = [
+    "--enable-nls"
+    "--disable-glibtest"
+    "--disable-installed-tests"
+    "--disable-always-build-tests"
+    "--disable-gcov"
+    (wtFlag "libproxy" (libproxy != null) null)
+    "--with-gnome-proxy"
+    (wtFlag "gnutls" (gnutls != null) null)
+    "--with-ca-certificates=/etc/ssl/certs/ca-certificates.crt"
+    (wtFlag "pkcs11" (p11_kit != null) null)
+  ];
+
+  nativeBuildInputs = [
+    intltool
+  ];
+
+  buildInputs = [
+    glib
+    gnutls
+    gsettings_desktop_schemas
+    libproxy
+    p11_kit
+  ];
 
   preBuild = ''
-    sed -e "s@${glib}/lib/gio/modules@$out/lib/gio/modules@g" -i $(find . -name Makefile)
+    sed -e "s|${glib}/lib/gio/modules|$out/lib/gio/modules|g" \
+      -i $(find . -name Makefile)
   '';
 
-  nativeBuildInputs = [ pkgconfig intltool ];
-  propagatedBuildInputs = [ glib gnutls libproxy gsettings_desktop_schemas ];
-
   doCheck = false; # tests need to access the certificates (among other things)
+  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     description = "Network-related giomodules for glib";
+    homepage = https://git.gnome.org/browse/glib-networking/;
     license = licenses.lgpl2Plus;
-    platforms = platforms.unix;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }
-

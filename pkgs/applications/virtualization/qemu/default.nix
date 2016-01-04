fbff25cde7b8ba7538dd71eea1fd69c7136436ff
William A. Kennington III <william@wkennington.com>
2016-01-04 14:27:15 -0800

    qemu: 2.4.1 -> 2.5.0

--- pkgs/applications/virtualization/qemu/default.nix
+++ pkgs/applications/virtualization/qemu/default.nix
@@ -1,18 +1,59 @@
-{ stdenv, fetchurl, pkgconfig, libtool, python, perl, texinfo, flex, bison
-, gettext, makeWrapper, glib, zlib, pixman
+{ stdenv, fetchurl
+, libtool
+, python
+, perl
+, texinfo
+, flex
+, bison
+, gettext
+, makeWrapper
+, glib
+, zlib
+, pixman
 
 # Optional Arguments
-, SDL2 ? null, gtk ? null, vte ? null, nettle ? null, gnutls ? null
-, cyrus_sasl ? null, libjpeg ? null, libpng ? null, ncurses ? null, curl ? null
-, libcap ? null, attr ? null, bluez ? null, libibverbs ? null, librdmacm ? null
-, libuuid ? null, vde2 ? null, libaio ? null, libcap_ng ? null, spice ? null
-, spice_protocol ? null, libceph ? null, libxfs ? null, nss ? null, nspr ? null
-, libusb ? null, usbredir ? null, mesa ? null, epoxy ? null, lzo ? null
-, snappy ? null, bzip2 ? null, libseccomp ? null, glusterfs ? null
-, libssh2 ? null, numactl ? null, gperftools ? null
+, SDL2 ? null
+, gtk ? null
+, vte ? null
+, virglrenderer ? null
+, nettle ? null
+, gnutls ? null
+, cyrus_sasl ? null
+, libjpeg ? null
+, libpng ? null
+, ncurses ? null
+, curl ? null
+, libcap ? null
+, attr ? null
+, bluez ? null
+, libibverbs ? null
+, librdmacm ? null
+, libuuid ? null
+, vde2 ? null
+, libaio ? null
+, libcap_ng ? null
+, spice ? null
+, spice_protocol ? null
+, libceph ? null
+, libxfs ? null
+, libcacard ? null
+, libusb ? null
+, usbredir ? null
+, mesa ? null
+, epoxy ? null
+, lzo ? null
+, snappy ? null
+, bzip2 ? null
+, libseccomp ? null
+, glusterfs ? null
+, libssh2 ? null
+, numactl ? null
+, gperftools ? null
+, jemalloc ? null
 
 # Audio libraries
-, libpulseaudio ? null, alsaLib ? null
+, libpulseaudio ? null
+, alsaLib ? null
 
 # Extra options
 , type ? ""
@@ -21,7 +62,7 @@
 with stdenv;
 with stdenv.lib;
 let
-  version = "2.4.1";
+  version = "2.5.0";
 
   isKvmOnly = type == "kvm-only";
   isNix = type == "nix";
@@ -29,6 +70,7 @@ let
   optSDL2 = if isNix then null else shouldUsePkg SDL2;
   optGtk = if isNix then null else shouldUsePkg gtk;
   optVte = if isNix then null else shouldUsePkg vte;
+  optVirglrenderer = if isNix then null else shouldUsePkg virglrenderer;
   optLibcap = if isNix then null else shouldUsePkg libcap;
   optAttr = if isNix then null else shouldUsePkg attr;
   optNettle = if isNix then null else shouldUsePkg nettle;
@@ -49,8 +91,7 @@ let
   optSpice_protocol = if isNix then null else shouldUsePkg spice_protocol;
   optLibceph = if isNix then null else shouldUsePkg libceph;
   optLibxfs = if isNix then null else shouldUsePkg libxfs;
-  optNss = if isNix then null else shouldUsePkg nss;
-  optNspr = if isNix then null else shouldUsePkg nspr;
+  optLibcacard = if isNix then null else shouldUsePkg libcacard;
   optLibusb = if isNix then null else shouldUsePkg libusb;
   optUsbredir = if isNix then null else shouldUsePkg usbredir;
   optMesa = if isNix then null else shouldUsePkg mesa;
@@ -63,6 +104,7 @@ let
   optLibssh2 = if isNix then null else shouldUsePkg libssh2;
   optNumactl = if isNix then null else shouldUsePkg numactl;
   optGperftools = if isNix then null else shouldUsePkg gperftools;
+  optJemalloc = if isNix then null else shouldUsePkg jemalloc;
 
   hasSDLAbi = if optSDL2 != null then true else null;
 
@@ -82,8 +124,6 @@ let
 
   hasSpice = optSpice != null && optSpice_protocol != null;
 
-  hasNss = optNss != null && optNspr != null;
-
   hasOpengl = optMesa != null && optEpoxy != null;
 
   optLibpulseaudio = if isNix then null else shouldUsePkg libpulseaudio;
@@ -102,7 +142,10 @@ let
     else if stdenv.system == "i686-linux" then "i386-softmmu"
     else null;
 
-  hasModules = if isNix then null else true;
+  hasModules = !isNix;
+
+  malloc = if optJemalloc != null then "jemalloc" else
+    if optGperfTools != null then "tcmalloc" else null;
 in
 
 stdenv.mkDerivation rec {
@@ -110,18 +153,44 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "http://wiki.qemu.org/download/qemu-${version}.tar.bz2";
-    sha256 = "0xx1wc7lj5m3r2ab7f0axlfknszvbd8rlclpqz4jk48zid6czmg3";
+    sha256 = "1m3j6xl7msrniidkvr5pw9d44yba5m7hm42xz8xy77v105s8hhrl";
   };
 
-  nativeBuildInputs = [ pkgconfig libtool perl texinfo flex bison gettext makeWrapper ];
+  nativeBuildInputs = [ libtool perl texinfo flex bison gettext makeWrapper ];
 
   buildInputs = [
-    python glib zlib pixman optSDL2 optGtk optVte optNcurses optCurl optBluez
-    optVde2 optLibcap_ng optAttr optLibuuid optLibceph optLibxfs optLibusb
-    optUsbredir optLzo optSnappy optBzip2 optLibseccomp optGlusterfs optLibssh2
-    optNumactl optGperftools optLibpulseaudio optAlsaLib
+    python
+    glib
+    zlib
+    pixman
+    optSDL2
+    optGtk
+    optVte
+    optVirglrenderer
+    optNcurses
+    optCurl
+    optBluez
+    optVde2
+    optLibcap_ng
+    optAttr
+    optLibuuid
+    optLibceph
+    optLibxfs
+    optLibcacard
+    optLibusb
+    optUsbredir
+    optLzo
+    optSnappy
+    optBzip2
+    optLibseccomp
+    optGlusterfs
+    optLibssh2
+    optNumactl
+    optLibpulseaudio
+    optAlsaLib
   ] ++ optionals (optGnutls != null) [
-    optNettle optGnutls
+    optNettle
+    optGnutls
   ] ++ optionals hasVncSasl [
     optCyrus_sasl
   ] ++ optionals hasVncJpeg [
@@ -131,28 +200,33 @@ stdenv.mkDerivation rec {
   ] ++ optionals hasVirtfs [
     optLibcap
   ] ++ optionals hasRdma [
-    optLibibverbs optLibrdmacm
+    optLibibverbs
+    optLibrdmacm
   ] ++ optionals hasLinuxAio [
     optLibaio
   ] ++ optionals hasSpice [
-    optSpice optSpice_protocol
-  ] ++ optionals hasNss [
-    optNss optNspr
+    optSpice
+    optSpice_protocol
   ] ++ optionals hasOpengl [
-    optMesa optEpoxy
+    optMesa
+    optEpoxy
+  ] ++ optionals (malloc == "jemalloc") [
+    optJemalloc
+  ] ++ optionals (malloc == "tcmalloc") [
+    optGperfTools
   ];
 
   enableParallelBuilding = true;
 
   configureFlags = [
-    (mkOther                          "smbd"                "smbd")
-    (mkOther                          "sysconfdir"          "/etc")
-    (mkOther                          "localstatedir"       "/var")
+    (mkOther                             "smbd"                "smbd")
+    (mkOther                             "sysconfdir"          "/etc")
+    (mkOther                             "localstatedir"       "/var")
     # Enable  debug
     # Disable strip
     # Disable werror
     # Disable stack-protector
-    (mkOther                          "audio-drv-list"      audio)
+    (mkOther                             "audio-drv-list"      audio)
     # block-drv-whitelist
     # block-drv-ro-whitelist
     # Enable  trace-backend
@@ -167,73 +241,75 @@ stdenv.mkDerivation rec {
     # With    vss-sdk
     # With    win-sdk
 
-    (mkEnable (!isNix)                "system"              null)
-    (mkEnable (!isKvmOnly)            "user"                null)
-    (mkEnable (!isKvmOnly)            "guest-base"          null)
-    (mkEnable (!isNix)                "docs"                null)
-    (mkEnable true                    "guest-agent"         null)
+    (mkEnable (!isNix)                   "system"              null)
+    (mkEnable (!isKvmOnly)               "user"                null)
+    (mkEnable (!isNix)                   "docs"                null)
+    (mkEnable true                       "guest-agent"         null)
     # Enable  guest-agent-msi
-    (mkEnable (!isNix)                "pie"                 null)
-    (mkEnable hasModules              "modules"             null)
-    (mkEnable false                   "debug-tcg"           null)
-    (mkEnable false                   "debug-info"          null)
-    (mkEnable false                   "sparse"              null)
+    (mkEnable (!isNix)                   "pie"                 null)
+    (mkEnable hasModules                 "modules"             null)
+    (mkEnable false                      "debug-tcg"           null)
+    (mkEnable false                      "debug-info"          null)
+    (mkEnable false                      "sparse"              null)
 
-    (mkEnable (optGnutls != null)     "gnutls"              null)
-    (mkEnable (optSDL2 != null)       "sdl"                 null)
-    (mkWith   hasSDLAbi               "sdlabi"              "2.0")
-    (mkEnable (optGtk != null)        "gtk"                 null)
+    (mkEnable (optGnutls != null)        "gnutls"              null)
+    (mkEnable (optNettle != null)        "nettle"              null)
+    (mkEnable false                      "gcrypt"              null)  # We are using nettle
+    (mkEnable (optSDL2 != null)          "sdl"                 null)
+    (mkWith   hasSDLAbi                  "sdlabi"              "2.0")
+    (mkEnable (optGtk != null)           "gtk"                 null)
     # With    gtk-abi
-    (mkEnable (optVte != null)        "vte"                 null)
-    (mkEnable (optNcurses != null)    "curses"              null)
-    (mkEnable hasVnc                  "vnc"                 null)
-    (mkEnable hasVncTls               "vnc-tls"             null)
-    (mkEnable hasVncSasl              "vnc-sasl"            null)
-    (mkEnable hasVncJpeg              "vnc-jpeg"            null)
-    (mkEnable hasVncPng               "vnc-png"             null)
-    (mkEnable stdenv.isDarwin         "cocoa"               null)
-    (mkEnable hasVirtfs               "virtfs"              null)
-    (mkEnable false                   "xen"                 null)
-    (mkEnable false                   "xen-pci-passthrough" null)
-    (mkEnable false                   "brlapi"              null)
-    (mkEnable (optCurl != null)       "curl"                null)
-    (mkEnable hasFdt                  "fdt"                 null)
-    (mkEnable (optBluez != null)      "bluez"               null)
-    (mkEnable stdenv.isLinux          "kvm"                 null)
-    (mkEnable hasRdma                 "rdma"                null)
-    (mkEnable (optLibuuid != null)    "uuid"                null)
-    (mkEnable (optVde2 != null)       "vde"                 null)
-    (mkEnable false                   "netmap"              null)  # TODO(wkennington): Add Support
-    (mkEnable hasLinuxAio             "linux-aio"           null)
-    (mkEnable (optLibcap_ng != null)  "cap-ng"              null)
-    (mkEnable (optAttr != null)       "attr"                null)
-    (mkEnable stdenv.isLinux          "vhost-net"           null)
-    (mkEnable hasSpice                "spice"               null)
-    (mkEnable (optLibceph != null)    "rbd"                 null)
-    (mkEnable false                   "libiscsi"            null)  # TODO(wkennington): Add support
-    (mkEnable false                   "libnfs"              null)  # TODO(wkennington): Add support
-    (mkEnable hasNss                  "smartcard-nss"       null)
-    (mkEnable (optLibusb != null)     "libusb"              null)
-    (mkEnable (optUsbredir != null)   "usb-redir"           null)
-    (mkEnable (optLzo != null)        "lzo"                 null)
-    (mkEnable (optSnappy != null)     "snappy"              null)
-    (mkEnable (optBzip2 != null)      "bzip2"               null)
-    (mkEnable (optLibseccomp != null) "seccomp"             null)
-    (mkEnable true                    "coroutine-pool"      null)
-    (mkEnable (optGlusterfs != null)  "glusterfs"           null)
-    (mkEnable false                   "archipelago"         null)
-    (mkEnable true                    "tpm"                 null)
-    (mkEnable (optLibssh2 != null)    "libssh2"             null)
-    (mkEnable (optLibuuid != null)    "vhdx"                null)
-    (mkEnable (optNumactl != null)    "numa"                null)
-    (mkEnable (optGperftools != null) "tcmalloc"            null)
+    (mkEnable (optVte != null)           "vte"                 null)
+    (mkEnable (optVirglrenderer != null) "virglrenderer"       null)
+    (mkEnable (optNcurses != null)       "curses"              null)
+    (mkEnable hasVnc                     "vnc"                 null)
+    (mkEnable hasVncSasl                 "vnc-sasl"            null)
+    (mkEnable hasVncJpeg                 "vnc-jpeg"            null)
+    (mkEnable hasVncPng                  "vnc-png"             null)
+    (mkEnable stdenv.isDarwin            "cocoa"               null)
+    (mkEnable hasVirtfs                  "virtfs"              null)
+    (mkEnable false                      "xen"                 null)
+    (mkEnable false                      "xen-pci-passthrough" null)
+    (mkEnable false                      "brlapi"              null)
+    (mkEnable (optCurl != null)          "curl"                null)
+    (mkEnable hasFdt                     "fdt"                 null)
+    (mkEnable (optBluez != null)         "bluez"               null)
+    (mkEnable stdenv.isLinux             "kvm"                 null)
+    (mkEnable hasRdma                    "rdma"                null)
+    (mkEnable (optLibuuid != null)       "uuid"                null)
+    (mkEnable (optVde2 != null)          "vde"                 null)
+    (mkEnable false                      "netmap"              null)  # TODO(wkennington): Add Support
+    (mkEnable hasLinuxAio                "linux-aio"           null)
+    (mkEnable (optLibcap_ng != null)     "cap-ng"              null)
+    (mkEnable (optAttr != null)          "attr"                null)
+    (mkEnable stdenv.isLinux             "vhost-net"           null)
+    (mkEnable hasSpice                   "spice"               null)
+    (mkEnable (optLibceph != null)       "rbd"                 null)
+    (mkEnable false                      "libiscsi"            null)  # TODO(wkennington): Add support
+    (mkEnable false                      "libnfs"              null)  # TODO(wkennington): Add support
+    (mkEnable (optLibcacard != null)     "smartcard"           null)
+    (mkEnable (optLibusb != null)        "libusb"              null)
+    (mkEnable (optUsbredir != null)      "usb-redir"           null)
+    (mkEnable (optLzo != null)           "lzo"                 null)
+    (mkEnable (optSnappy != null)        "snappy"              null)
+    (mkEnable (optBzip2 != null)         "bzip2"               null)
+    (mkEnable (optLibseccomp != null)    "seccomp"             null)
+    (mkEnable true                       "coroutine-pool"      null)
+    (mkEnable (optGlusterfs != null)     "glusterfs"           null)
+    (mkEnable false                      "archipelago"         null)
+    (mkEnable true                       "tpm"                 null)
+    (mkEnable (optLibssh2 != null)       "libssh2"             null)
+    (mkEnable (optLibuuid != null)       "vhdx"                null)
+    (mkEnable (optNumactl != null)       "numa"                null)
+    (mkEnable (malloc == "tcmalloc")     "tcmalloc"            null)
+    (mkEnable (malloc == "jemalloc")     "jemalloc"            null)
 
-    (mkEnable hasOpengl               "opengl"              null)
-    (mkEnable (optLibxfs != null)     "xfsctl"              null)
+    (mkEnable hasOpengl                  "opengl"              null)
+    (mkEnable (optLibxfs != null)        "xfsctl"              null)
   ] ++ optionals isKvmOnly [
-    (mkOther                          "target-list"         targetList)
+    (mkOther                             "target-list"         targetList)
   ] ++ optionals isNix [
-    (mkOther                          "static"              null)
+    (mkOther                             "static"              null)
   ];
 
   installFlags = [

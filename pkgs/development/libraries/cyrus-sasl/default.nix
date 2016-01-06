7ed9b59c58e1a076607bcac1160226d34777bf28
William A. Kennington III <william@wkennington.com>
2016-01-06 01:03:18 -0800

    cyrus-sasl: Unbreak

--- pkgs/development/libraries/cyrus-sasl/default.nix
+++ pkgs/development/libraries/cyrus-sasl/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, openssl, kerberos, db, gettext, pam, fixDarwinDylibNames, autoreconfHook }:
+{ lib, stdenv, fetchurl, openssl, kerberos, db, gettext, pam, fixDarwinDylibNames }:
 
 with stdenv.lib;
 stdenv.mkDerivation rec {
@@ -9,32 +9,27 @@ stdenv.mkDerivation rec {
     sha256 = "1hvvbcsg21nlncbgs0cgn3iwlnb3vannzwsp6rwvnn9ba4v53g4g";
   };
 
+  nativeBuildInputs = lib.optional stdenv.isDarwin fixDarwinDylibNames;
   buildInputs =
-    [ openssl db gettext kerberos autoreconfHook ]
-    ++ lib.optional stdenv.isLinux pam
-    ++ lib.optional stdenv.isDarwin fixDarwinDylibNames;
+    [ openssl db gettext kerberos ]
+    ++ lib.optional stdenv.isLinux pam;
 
-  patches = [
-    ./missing-size_t.patch # https://bugzilla.redhat.com/show_bug.cgi?id=906519
-    (
-      fetchurl {
-        url = "http://www.linuxfromscratch.org/patches/blfs/svn/cyrus-sasl-2.1.26-fixes-3.patch";
-        sha256 = "1vh4pc2rxxm6yvykx0b7kg09jbcwcxwv5rs6yq2ag3y8p6a9x86w";
-      }
-    )
-  ];
+  patches = [ ./missing-size_t.patch ]; # https://bugzilla.redhat.com/show_bug.cgi?id=906519
 
   configureFlags = [
     "--with-openssl=${openssl}"
+    "--enable-auth-sasldb"
   ];
 
   # Set this variable at build-time to make sure $out can be evaluated.
   preConfigure = ''
-    configureFlagsArray=( --with-plugindir=$out/lib/sasl2
-                          --with-configdir=$out/lib/sasl2
-                          --with-saslauthd=/run/saslauthd
-                          --enable-login
-                        )
+    configureFlagsArray+=("--with-plugindir=$out/lib/sasl2")
+    configureFlagsArray+=("--with-configdir=$out/lib/sasl2")
+    configureFlagsArray+=("--with-saslauthd=/run/saslauthd")
+  '';
+
+  preBuild = ''
+    cat sasldb/Makefile
   '';
 
   installFlags = lib.optional stdenv.isDarwin [ "framedir=$(out)/Library/Frameworks/SASL2.framework" ];

d69bf58d0cfdf3db7e2f3e2f1fd2975aa03c5c94
Cody Opel <codyopel@gmail.com>
2016-01-06 00:14:03 -0500

    gsm: refactor

--- pkgs/all-pkgs/gsm/default.nix
+++ pkgs/all-pkgs/gsm/default.nix
@@ -1,11 +1,13 @@
-{ stdenv, fetchurl
-, staticSupport ? false # Compile statically (support for packages that look for the static object)
+{ stdenv
+, fetchurl
+, staticSupport ? false
 }:
 
-let
-  inherit (stdenv) isDarwin;
-  inherit (stdenv.lib) optional optionalString;
-in
+with {
+  inherit (stdenv.lib)
+    optional
+    optionalString;
+};
 
 stdenv.mkDerivation rec {
   name = "gsm-${version}";
@@ -16,40 +18,38 @@ stdenv.mkDerivation rec {
     sha256 = "0b1mx69jq88wva3wk0hi6fcl5a52qhnq2f9p3f3jdh5k61ma252q";
   };
 
-  patchPhase = ''
+  postPhase = ''
     # Fix include directory
     sed -e 's,$(GSM_INSTALL_ROOT)/inc,$(GSM_INSTALL_ROOT)/include/gsm,' -i Makefile
-  '' + optionalString (!staticSupport) (
-    (if isDarwin then  ''
-      # Build dylib on Darwin
-      sed -e 's,libgsm.a,libgsm.dylib,' -i Makefile
-      sed -e 's,$(AR) $(ARFLAGS) $(LIBGSM) $(GSM_OBJECTS),$(LD) -o $(LIBGSM) -dynamiclib -install_name $(GSM_INSTALL_ROOT)/$(LIBGSM) $(GSM_OBJECTS) -lc,' -i Makefile
-    '' else ''
-      # Build ELF shared object by default
-      sed -e 's,libgsm.a,libgsm.so,' -i Makefile
-      sed -e 's/$(AR) $(ARFLAGS) $(LIBGSM) $(GSM_OBJECTS)/$(LD) -shared -Wl,-soname,libgsm.so -o $(LIBGSM) $(GSM_OBJECTS) -lc/' -i Makefile
-    '') + ''
-      # Remove line that is unused when building shared libraries
-      sed -e 's,$(RANLIB) $(LIBGSM),,' -i Makefile
-    ''
-  );
+  '' + optionalString (!staticSupport) ''
+    # Build ELF shared object
+    sed -i Makefile \
+      -e 's,libgsm.a,libgsm.so,' \
+      -e 's/$(AR) $(ARFLAGS) $(LIBGSM) $(GSM_OBJECTS)/$(LD) -shared -Wl,-soname,libgsm.so -o $(LIBGSM) $(GSM_OBJECTS) -lc/' \
+      -e '/$(RANLIB) $(LIBGSM)/d'
+  '';
 
   makeFlags = [
     "SHELL=${stdenv.shell}"
     "INSTALL_ROOT=$(out)"
   ] ++ optional stdenv.cc.isClang "CC=clang";
 
-  preInstall = "mkdir -p $out/{bin,lib,man/man1,man/man3,include/gsm}";
-
   NIX_CFLAGS_COMPILE = optional (!staticSupport) "-fPIC";
 
-  parallelBuild = false;
+  preInstall = "mkdir -pv $out/{bin,lib,man/man1,man/man3,include/gsm}";
+
+  enableParallelBuilding = false;
 
   meta = with stdenv.lib; {
     description = "Lossy speech compression codec";
-    homepage    = http://www.quut.com/gsm/;
-    license     = licenses.bsd2;
-    maintainers = with maintainers; [ codyopel raskin ];
-    platforms   = platforms.unix;
+    homepage = http://www.quut.com/gsm/;
+    license = licenses.bsd2;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }

b77469d572dc8a1087e7eb65b309cc8393e246d1
Cody Opel <codyopel@gmail.com>
2016-01-07 15:51:22 -0500

    libglade: add missing build inputs

--- pkgs/desktops/gnome-2/platform/libglade/default.nix
+++ pkgs/desktops/gnome-2/platform/libglade/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, pkgconfig, gtk2, libxml2, python, gettext}:
+{stdenv, fetchurl, pkgconfig, gtk2, libxml2, python, gettext, atk, glib}:
 
 stdenv.mkDerivation {
   name = "libglade-2.6.4";
@@ -8,7 +8,7 @@ stdenv.mkDerivation {
     sha256 = "1v2x2s04jry4gpabws92i0wq2ghd47yr5n9nhgnkd7c38xv1wdk4";
   };
 
-  buildInputs = [ pkgconfig gtk2 python gettext ];
+  buildInputs = [ pkgconfig gtk2 python gettext atk glib ];
 
   propagatedBuildInputs = [ libxml2 ];
 

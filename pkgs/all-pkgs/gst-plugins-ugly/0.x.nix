05d292921d33816dcf9cd88e99db91e0837aea04
Cody Opel <codyopel@gmail.com>
2016-01-10 13:40:31 -0500

    gst-plugins-ugly: refactor & 1.6.1 -> 1.6.2

---
+++ pkgs/all-pkgs/gst-plugins-ugly/0.x.nix
@@ -0,0 +1,106 @@
+{ stdenv
+, fetchurl
+, gettext
+
+, a52dec
+, amrnb
+, amrwb
+, glib
+, gst-plugins-base_0
+, gstreamer_0
+, lame
+#, libcdio
+, libdvdread
+, libmad
+, mpeg2dec
+, orc
+, x264
+}:
+
+with {
+  inherit (stdenv.lib)
+    enFlag;
+};
+
+stdenv.mkDerivation rec {
+  name = "gst-plugins-ugly-0.10.19";
+
+  src = fetchurl {
+    url = "http://gstreamer.freedesktop.org/src/gst-plugins-ugly/${name}.tar.xz";
+    sha256 = "0wx8dr3sqfkar106yw6h57jdv2cifwsydkziz9z7wqwjz1gzcd29";
+  };
+
+  configureFlags = [
+    "--disable-maintainer-mode"
+    "--enable-nls"
+    "--enable-rpath"
+    "--disable-debug"
+    "--disable-profiling"
+    "--disable-valgrind"
+    "--disable-gcov"
+    "--disable-examples"
+    "--enable-external"
+    "--enable-experimental"
+    "--disable-gtk-doc"
+    "--enable-gobject-cast-checks"
+    "--enable-glib-asserts"
+    (enFlag "orc" (orc != null) null)
+    # Internal plugins
+    "--enable-asfdemux"
+    "--enable-dvdlpcmdec"
+    "--enable-dvdsub"
+    "--enable-iec958"
+    "--enable-mpegaudioparse"
+    "--enable-mpegstream"
+    "--enable-realmedia"
+    "--enable-synaesthesia"
+    # External plugins
+    (enFlag "a52dec" (a52dec != null) null)
+    (enFlag "amrnb" (amrnb != null) null)
+    (enFlag "amrwb" (amrwb != null) null)
+    #(enFlag "cdio" (libcdio != null) null)
+    (enFlag "dvdread" (libdvdread != null) null)
+    (enFlag "lame" (lame != null) null)
+    (enFlag "mad" (libmad != null) null)
+    (enFlag "mpeg2dec" (mpeg2dec != null) null)
+    #(enFlag "sidplay" (sidplay != null) null)
+    #(enFlag "twolame" (twolame != null) null)
+    (enFlag "x264" (x264 != null) null)
+
+  ];
+
+  nativeBuildInputs = [
+    gettext
+  ];
+
+  buildInputs = [
+    a52dec
+    amrnb
+    amrwb
+    glib
+    gst-plugins-base_0
+    gstreamer_0
+    lame
+    #libcdio
+    libdvdread
+    libmad
+    mpeg2dec
+    orc
+    x264
+  ];
+
+  enableParallelBuilding = true;
+
+  meta = with stdenv.lib; {
+    description = "Basepack of plugins for gstreamer";
+    homepage = http://gstreamer.freedesktop.org;
+    license = licenses.gpl2;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
+  };
+}

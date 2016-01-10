8f9605043b44f1f4472b7ef76b8b98f92bcf0bfd
Cody Opel <codyopel@gmail.com>
2016-01-10 10:45:27 -0500

    gst-plugins-good: refactor & 1.6.1 -> 1.6.2

---
+++ pkgs/all-pkgs/gst-plugins-good/0.x.nix
@@ -0,0 +1,195 @@
+{ stdenv
+, fetchurl
+, gettext
+
+, aalib
+, bzip2
+, cairo
+, flac
+, gdk-pixbuf
+, glib
+, gst-plugins-base_0
+, gstreamer_0
+, libcaca
+, libgudev
+, libjack2
+, libjpeg
+, libpng
+, libpulseaudio
+, libsoup
+, libv4l
+, orc
+, speex
+, taglib
+, wavpack
+, xorg
+, zlib
+}:
+
+with {
+  inherit (stdenv.lib)
+    enFlag
+    wtFlag;
+};
+
+stdenv.mkDerivation rec {
+  name = "gst-plugins-good-0.10.31";
+
+  src = fetchurl {
+    url = "http://gstreamer.freedesktop.org/src/gst-plugins-good/${name}.tar.xz";
+    sha256 = "0r1b54yixn8v2l1dlwmgpkr0v2a6a21id5njp9vgh58agim47a3p";
+  };
+
+  patches = [
+    ./gstreamer-0.10-v4l.patch
+    ./gstreamer-0.10-linux-headers-3.9.patch
+  ];
+
+  configureFlags = [
+    "--disable-maintainer-mode"
+    "--enable-nls"
+    "--enable-rpath"
+    "--disable-debug"
+    "--disable-profiling"
+    "--disable-valgrind"
+    "--disable-gcov"
+    "--disable-examples"
+    "--enable-external"
+    "--enable-experimental"
+    "--enable-schemas-install"
+    "--disable-gtk-doc"
+    "--enable-gobject-cast-checks"
+    "--enable-glib-asserts"
+    (enFlag "orc" (orc != null) null)
+    "--enable-gconftool"
+    "--enable-videofilter"
+    "--enable-alpha"
+    "--enable-apetag"
+    "--enable-audiofx"
+    "--enable-audioparsers"
+    "--enable-auparse"
+    "--enable-autodetect"
+    "--enable-avi"
+    "--enable-cutter"
+    "--enable-debugutils"
+    "--enable-deinterlace"
+    "--enable-effectv"
+    "--enable-equalizer"
+    "--enable-flv"
+    "--enable-id3demux"
+    "--enable-icydemux"
+    "--enable-interleave"
+    "--enable-flx"
+    "--enable-goom"
+    "--enable-goom2k1"
+    "--enable-imagefreeze"
+    "--enable-isomp4"
+    "--enable-law"
+    "--enable-level"
+    "--enable-matroska"
+    "--enable-monoscope"
+    "--enable-multifile"
+    "--enable-multipart"
+    "--enable-replaygain"
+    "--enable-rtp"
+    "--enable-rtpmanager"
+    "--enable-rtsp"
+    "--enable-shapewipe"
+    "--enable-smpte"
+    "--enable-spectrum"
+    "--enable-udp"
+    "--enable-videobox"
+    "--enable-videocrop"
+    "--enable-videomixer"
+    "--enable-wavenc"
+    "--enable-wavparse"
+    "--enable-y4m"
+    # External plugins
+    "--disable-directsound"
+    "--disable-oss"
+    "--disable-oss4"
+    "--disable-sunaudio"
+    "--disable-osx_audio"
+    "--disable-osx_video"
+    (enFlag "gst_v4l2" (libv4l != null) null)
+    (enFlag "x" (xorg.libX11 != null) null)
+    (enFlag "xshm" (xorg.libXext != null) null)
+    (enFlag "xvideo" (xorg.libXv != null) null)
+    (enFlag "aalib" (aalib != null) null)
+    "--disable-aalibtest"
+    "--disable-annodex"
+    (enFlag "cairo" (cairo != null) null)
+    "--enable-cairo_gobject"
+    "--disable-esd"
+    "--disable-esdtest"
+    (enFlag "flac" (flac != null) null)
+    "--enable-gconf"
+    (enFlag "gdk_pixbuf" (gdk-pixbuf != null) null)
+    "--disable-hal"
+    (enFlag "jack" (libjack2 != null) null)
+    (enFlag "jpeg" (libjpeg != null) null)
+    (enFlag "libcaca" (libcaca != null) null)
+    "--disable-libdv"
+    (enFlag "libpng" (libpng != null) null)
+    (enFlag "pulse" (libpulseaudio != null) null)
+    "--enable-dv1394"
+    "--disable-shout2"
+    (enFlag "soup" (libsoup != null) null)
+    (enFlag "speex" (speex != null) null)
+    (enFlag "taglib" (taglib != null) null)
+    (enFlag "wavpack" (wavpack != null) null)
+    (enFlag "zlib" (zlib != null) null)
+    (enFlag "bz2" (bzip2 != null) null)
+    "--without-gtk"
+    (wtFlag "gudev" (libgudev != null) null)
+    (wtFlag "libv4l2" (libv4l != null) null)
+    "--with-x"
+    #"--with-jpeg-mmx"
+  ];
+
+  nativeBuildInputs = [
+    gettext
+  ];
+
+  buildInputs = [
+    aalib
+    bzip2
+    cairo
+    flac
+    gdk-pixbuf
+    glib
+    gst-plugins-base_0
+    gstreamer_0
+    libcaca
+    libgudev
+    libjack2
+    libjpeg
+    libpng
+    libpulseaudio
+    libsoup
+    libv4l
+    orc
+    speex
+    taglib
+    wavpack
+    xorg.libX11
+    xorg.libXext
+    xorg.libXv
+    zlib
+  ];
+
+  enableParallelBuilding = true;
+
+  meta = with stdenv.lib; {
+    description = "Basepack of plugins for GStreamer";
+    homepage = http://gstreamer.freedesktop.org;
+    license = licenses.lgpl2plus;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
+  };
+}
---
+++ pkgs/all-pkgs/gst-plugins-good/1.x.nix
@@ -0,0 +1,186 @@
+{ stdenv
+, fetchurl
+, python
+
+, aalib
+, bzip2
+, cairo
+, flac
+, gdk-pixbuf
+, gst-plugins-base
+, gstreamer
+, libcaca
+, libgudev
+, libjack2
+, libjpeg
+, libpng
+, libpulseaudio
+, libshout
+, libsoup
+, libv4l
+, libvpx
+, orc
+, speex
+, taglib
+, wavpack
+, xorg
+, zlib
+}:
+
+with {
+  inherit (stdenv.lib)
+    enFlag
+    wtFlag;
+};
+
+stdenv.mkDerivation rec {
+  name = "gst-plugins-good-1.6.2";
+
+  src = fetchurl {
+    url = "http://gstreamer.freedesktop.org/src/gst-plugins-good/${name}.tar.xz";
+    sha256 = "14kaxgv31ibnwrlh00grs54lvyimiljm7wr4w2c4n9wkrvgm8vl7";
+  };
+
+  configureFlags = [
+    "--disable-maintainer-mode"
+    "--enable-nls"
+    "--enable-rpath"
+    "--disable-fatal-warnings"
+    "--disable-debug"
+    "--disable-profiling"
+    "--disable-valgrind"
+    "--disable-gcov"
+    "--disable-examples"
+    "--enable-external"
+    "--enable-experimental"
+    "--disable-gtk-doc"
+    "--disable-gtk-doc-html"
+    "--disable-gtk-doc-pdf"
+    "--enable-gobject-cast-checks"
+    "--enable-glib-asserts"
+    (enFlag "orc" (orc != null) null)
+    "--disable-static-plugins"
+    # Internal plugins
+    "--enable-alpha"
+    "--enable-apetag"
+    "--enable-audiofx"
+    "--enable-audioparsers"
+    "--enable-auparse"
+    "--enable-autodetect"
+    "--enable-avi"
+    "--enable-cutter"
+    "--disable-debugutils"
+    "--enable-deinterlace"
+    "--enable-dtmf"
+    "--enable-effectv"
+    "--enable-equalizer"
+    "--enable-flv"
+    "--enable-flx"
+    "--enable-goom"
+    "--enable-goom2k1"
+    "--enable-icydemux"
+    "--enable-id3demux"
+    "--enable-imagefreeze"
+    "--enable-interleave"
+    "--enable-isomp4"
+    "--enable-law"
+    "--enable-level"
+    "--enable-matroska"
+    "--enable-monoscope"
+    "--enable-multifile"
+    "--enable-multipart"
+    "--enable-replaygain"
+    "--enable-rtp"
+    "--enable-rtpmanager"
+    "--enable-rtsp"
+    "--enable-shapewipe"
+    "--enable-smpte"
+    "--enable-spectrum"
+    "--enable-udp"
+    "--enable-videobox"
+    "--enable-videocrop"
+    "--enable-videofilter"
+    "--enable-videomixer"
+    "--enable-wavenc"
+    "--enable-wavparse"
+    "--enable-y4m"
+    # External plugins
+    "--disable-directsound"
+    "--enable-waveform"
+    "--disable-oss"
+    "--disable-oss4"
+    "--disable-sunaudio"
+    "--disable-osx_audio"
+    "--disable-osx_video"
+    (enFlag "gst_v4l2" (libv4l != null) null)
+    (enFlag "x" (xorg.libX11 != null) null)
+    (enFlag "aalib" (aalib != null) null)
+    "--disable-aalibtest"
+    (enFlag "cairo" (cairo != null) null)
+    (enFlag "flac" (flac != null) null)
+    (enFlag "gdk_pixbuf" (gdk-pixbuf != null) null)
+    (enFlag "jack" (libjack2 != null) null)
+    (enFlag "jpeg" (libjpeg != null) null)
+    (enFlag "libcaca" (libcaca != null) null)
+    "--disable-libdv"
+    (enFlag "libpng" (libpng != null) null)
+    (enFlag "pulse" (libpulseaudio != null) null)
+    "--disable-dv1394"
+    (enFlag "shout2" (libshout != null) null)
+    (enFlag "soup" (libsoup != null) null)
+    (enFlag "speex" (speex != null) null)
+    (enFlag "taglib" (taglib != null) null)
+    (enFlag "vpx" (libvpx != null) null)
+    (enFlag "wavpack" (wavpack != null) null)
+    (enFlag "zlib" (zlib != null) null)
+    (enFlag "bz2" (bzip2 != null) null)
+    (wtFlag "--with-gudev" (libgudev != null) null)
+    (wtFlag "--with-libv4l2" (libv4l != null) null)
+  ];
+
+  nativeBuildInputs = [
+    python
+  ];
+
+  buildInputs = [
+    aalib
+    bzip2
+    cairo
+    flac
+    gdk-pixbuf
+    gst-plugins-base
+    gstreamer
+    libcaca
+    libgudev
+    libjack2
+    libjpeg
+    libpng
+    libpulseaudio
+    libshout
+    libsoup
+    libv4l
+    libvpx
+    orc
+    speex
+    taglib
+    wavpack
+    zlib
+    xorg.libX11
+    xorg.libXext
+  ];
+
+  enableParallelBuilding = true;
+
+  meta = with stdenv.lib; {
+    description = "Basepack of plugins for GStreamer";
+    homepage = http://gstreamer.freedesktop.org;
+    license = licenses.lgpl2Plus;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
+  };
+}

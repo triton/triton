62dce45cc2e6b13ab12ef20af9d0fa9cdd4baf05
William A. Kennington III <william@wkennington.com>
2016-01-11 11:17:41 -0800

    Add cairo to xorg args

--- pkgs/servers/x11/xorg/default.nix
+++ pkgs/servers/x11/xorg/default.nix
@@ -2,7 +2,7 @@
 args @ { clangStdenv, fetchurl, fetchgit, fetchpatch, stdenv, pkgconfig, intltool, freetype, fontconfig
 , libxslt, expat, libpng, zlib, perl, mesa_drivers, spice_protocol
 , dbus, libuuid, openssl, gperf, m4, libevdev, tradcpp, libinput, mcpp, makeWrapper, autoreconfHook
-, autoconf, automake, libtool, xmlto, asciidoc, flex, bison, python, mtdev, pixman, ... }: with args;
+, autoconf, automake, libtool, xmlto, asciidoc, flex, bison, python, mtdev, pixman, cairo, ... }: with args;
 
 let
 
--- pkgs/servers/x11/xorg/generate-expr-from-tarballs.pl
+++ pkgs/servers/x11/xorg/generate-expr-from-tarballs.pl
@@ -232,7 +232,7 @@ print OUT <<EOF;
 args @ { clangStdenv, fetchurl, fetchgit, fetchpatch, stdenv, pkgconfig, intltool, freetype, fontconfig
 , libxslt, expat, libpng, zlib, perl, mesa_drivers, spice_protocol
 , dbus, libuuid, openssl, gperf, m4, libevdev, tradcpp, libinput, mcpp, makeWrapper, autoreconfHook
-, autoconf, automake, libtool, xmlto, asciidoc, flex, bison, python, mtdev, pixman, ... }: with args;
+, autoconf, automake, libtool, xmlto, asciidoc, flex, bison, python, mtdev, pixman, cairo, ... }: with args;
 
 let
 

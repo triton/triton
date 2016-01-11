f1e58d8e41957146eb79b73ef05c06c527ad8c1c
William A. Kennington III <william@wkennington.com>
2016-01-11 13:32:39 -0800

    cjdns: 16 -> 17.1

--- pkgs/tools/networking/cjdns/default.nix
+++ pkgs/tools/networking/cjdns/default.nix
@@ -1,32 +1,27 @@
-{ stdenv, fetchFromGitHub, nodejs, which, python27, utillinux }:
+{ stdenv, fetchFromGitHub, nodejs, which, python2, utillinux }:
 
 let
-  version = "16"; # see ${src}/util/version/Version.h
-  date = "20150509";
+  version = "17.1"; # see ${src}/util/version/Version.h
 in
 stdenv.mkDerivation {
-  name = "cjdns-${version}-${date}";
+  name = "cjdns-${version}";
 
   src = fetchFromGitHub {
     owner = "cjdelisle";
     repo = "cjdns";
-    rev = "a05ade40dc31caebaf3aa770aac3ab2ecb02d867";
-    sha256 = "07vwsw5d0sdxypl187cyzzdrv0chf4yyjxcymf847afkfr249n29";
+    rev = "cjdns-v${version}";
+    sha256 = "0q9d7ll912lvv0z67mhw83vxkbv9kbajh9ngqdj937my08i9yq6c";
   };
 
-  buildInputs = [ which python27 nodejs ] ++
-    # for flock
-    stdenv.lib.optional stdenv.isLinux [ utillinux ];
+  nativeBuildInputs = [ which python2 nodejs ];
+  buildInputs = stdenv.lib.optional stdenv.isLinux [ utillinux ];
 
-  buildPhase =
-    stdenv.lib.optionalString stdenv.isArm "Seccomp_NO=1 "
-    + "bash do";
+  buildPhase = ''
+    bash do
+  '';
+
   installPhase = ''
     installBin cjdroute makekeys privatetopublic publictoip6
-    sed -i 's,/usr/bin/env node,'$(type -P node), \
-      $(find contrib -name "*.js")
-    sed -i 's,/usr/bin/env python,'$(type -P python), \
-      $(find contrib -type f)
     mkdir -p $out/share/cjdns
     cp -R contrib tools node_build node_modules $out/share/cjdns/
   '';

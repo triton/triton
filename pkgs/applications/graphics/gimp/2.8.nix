788077df16a7d1780fa19d861381dd378025d25e
William A. Kennington III <william@wkennington.com>
2016-01-11 01:38:08 -0800

    gimp: Fix dependencies

--- pkgs/applications/graphics/gimp/2.8.nix
+++ pkgs/applications/graphics/gimp/2.8.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, pkgconfig, intltool, babl, gegl, gtk2, glib, gdk_pixbuf
 , pango, cairo, freetype, fontconfig, lcms, libpng, libjpeg, poppler, libtiff
 , webkit, libmng, librsvg, libwmf, zlib, libzip, ghostscript, aalib, jasper
-, python, pygtk, libart_lgpl, libexif, gettext, xorg, wrapPython }:
+, python, pygtk, libart_lgpl, libexif, gettext, xorg, wrapPython, atk }:
 
 stdenv.mkDerivation rec {
   name = "gimp-2.8.16";
@@ -11,23 +11,20 @@ stdenv.mkDerivation rec {
     sha256 = "1dsgazia9hmab8cw3iis7s69dvqyfj5wga7ds7w2q5mms1xqbqwm";
   };
 
-  buildInputs =
-    [ pkgconfig intltool babl gegl gtk2 glib gdk_pixbuf pango cairo
-      freetype fontconfig lcms libpng libjpeg poppler libtiff webkit
-      libmng librsvg libwmf zlib libzip ghostscript aalib jasper
-      python pygtk libart_lgpl libexif gettext xorg.libXpm
-      wrapPython
-    ];
+  nativeBuildInputs = [ pkgconfig intltool gettext wrapPython ];
+
+  buildInputs = [
+    babl gegl gtk2 glib gdk_pixbuf pango cairo
+    freetype fontconfig lcms libpng libjpeg poppler libtiff webkit
+    libmng librsvg libwmf zlib libzip ghostscript aalib jasper
+    python pygtk libart_lgpl libexif xorg.libXpm atk
+  ];
 
   pythonPath = [ pygtk ];
 
-  postInstall = ''wrapPythonPrograms'';
-
-  #configureFlags = [ "--disable-print" ];
-
-  # "screenshot" needs this.
-  NIX_LDFLAGS = "-rpath ${xorg.libX11}/lib"
-    + stdenv.lib.optionalString stdenv.isDarwin " -lintl";
+  postInstall = ''
+    wrapPythonPrograms
+  '';
 
   meta = {
     description = "The GNU Image Manipulation Program";

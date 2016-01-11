a661e7b55824b6062432d40238e0e6ac330aeeda
Cody Opel <codyopel@gmail.com>
2016-01-10 20:35:47 -0500

    kernel: add 4.4

---
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -0,0 +1,17 @@
+{ stdenv, fetchurl, perl, buildLinux, ... } @ args:
+
+import ./generic.nix (args // rec {
+  version = "4.4";
+  extraMeta.branch = "4.4";
+
+  src = fetchurl {
+    url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
+    sha256 = "1chqahb0ssvla7n9arzi4882x72fm72p436ic2j9jjarxy7pq7a0";
+  };
+
+  features.iwlwifi = true;
+  features.efiBootStub = true;
+  features.needsCifsUtils = true;
+  features.canDisableNetfilterConntrackHelpers = true;
+  features.netfilterRPFilter = true;
+} // (args.argsOverride or {}))

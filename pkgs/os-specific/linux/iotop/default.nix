4c2f822020e3922b111f55335876c553d4954e33
Cody Opel <codyopel@gmail.com>
2016-01-14 18:12:25 -0500

    iotop: remove python module reference

--- pkgs/os-specific/linux/iotop/default.nix
+++ pkgs/os-specific/linux/iotop/default.nix
@@ -1,4 +1,7 @@
-{ stdenv, fetchurl, buildPythonPackage, pythonPackages }:
+{ stdenv
+, buildPythonPackage
+, fetchurl
+}:
 
 buildPythonPackage rec {
   name = "iotop-0.6";
@@ -9,13 +12,11 @@ buildPythonPackage rec {
     sha256 = "0nzprs6zqax0cwq8h7hnszdl3d2m4c2d4vjfxfxbnjfs9sia5pis";
   };
 
-  pythonPath = [ pythonPackages.curses ];
-
   doCheck = false;
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "A tool to find out the processes doing the most IO";
-    maintainers = [ stdenv.lib.maintainers.raskin ];
-    platforms = stdenv.lib.platforms.linux;
+    maintainers = with maintainers; [ ];
+    platforms = platforms.linux;
   };
 }

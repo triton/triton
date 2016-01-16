70ca255be76b8704dd9f7ee828fce10bb8f2d7b5
William A. Kennington III <william@wkennington.com>
2016-01-16 01:34:47 -0800

    mutt: Move patches to triton-patches

--- pkgs/applications/networking/mailreaders/mutt/default.nix
+++ pkgs/applications/networking/mailreaders/mutt/default.nix
@@ -60,14 +60,46 @@ stdenv.mkDerivation rec {
 
   # Adding the sidebar
   patches = stdenv.lib.optional withSidebar [
-    ./trash-folder.patch
-    ./sidebar.patch
-    ./sidebar-dotpathsep.patch
-    ./sidebar-utf8.patch
-    ./sidebar-newonly.patch
-    ./sidebar-delimnullwide.patch
-    ./sidebar-compose.patch
-    ./sidebar-new.patch
+    (fetchTritonPatch {
+      rev = "e12fe48a1f06e9d8cecd18c79ec0f06bd6dc3fe7";
+      file = "mutt/sidebar-compose.patch";
+      sha256 = "9d08dc2ca9aab3697273d6f77bce9d0ed701e066aeb42e1e4f15ded3b888c20b";
+    })
+    (fetchTritonPatch {
+      rev = "e12fe48a1f06e9d8cecd18c79ec0f06bd6dc3fe7";
+      file = "mutt/sidebar-delimnullwide.patch";
+      sha256 = "3fdae2e6db9b223fd30d24faf52dc0f9a9377cf75291533ee1e36c46fdcb314c";
+    })
+    (fetchTritonPatch {
+      rev = "e12fe48a1f06e9d8cecd18c79ec0f06bd6dc3fe7";
+      file = "mutt/sidebar-dotpathsep.patch";
+      sha256 = "e31c6be587087314f59d11235b95e03f3aa095f70c64d33b47d7d510466c0581";
+    })
+    (fetchTritonPatch {
+      rev = "e12fe48a1f06e9d8cecd18c79ec0f06bd6dc3fe7";
+      file = "mutt/sidebar-new.patch";
+      sha256 = "6c8345d67696145fa93d414ef6e514f577d5edbd2ba12369cde34198bdeb2edb";
+    })
+    (fetchTritonPatch {
+      rev = "e12fe48a1f06e9d8cecd18c79ec0f06bd6dc3fe7";
+      file = "mutt/sidebar-newonly.patch";
+      sha256 = "afd43eb47b27dd820c2195ac18cfe6421dcf0bb74188881521adbe9a8216efee";
+    })
+    (fetchTritonPatch {
+      rev = "e12fe48a1f06e9d8cecd18c79ec0f06bd6dc3fe7";
+      file = "mutt/sidebar-utf8.patch";
+      sha256 = "998fa2c73aa50c495071c019c2616f4b7fd1f8ef7bc44ee08bad5af76a69d776";
+    })
+    (fetchTritonPatch {
+      rev = "e12fe48a1f06e9d8cecd18c79ec0f06bd6dc3fe7";
+      file = "mutt/sidebar.patch";
+      sha256 = "fefc1d12ac6006c4ad53a7a3893170ae312ae8dabbc913bd15a8906a9f9396bf";
+    })
+    (fetchTritonPatch {
+      rev = "e12fe48a1f06e9d8cecd18c79ec0f06bd6dc3fe7";
+      file = "mutt/trash-folder.patch";
+      sha256 = "bec56cd374735dfd2ccd05f606f3b0e4fa70ac0403bdc24d802cb3c98c1ddf40";
+    })
   ];
 
   meta = with stdenv.lib; {

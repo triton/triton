63baa1e3dbc9751bbaae6451d7c6b31cfda12187
Cody Opel <codyopel@gmail.com>
2016-01-16 17:57:30 -0500

    pixman: refactor

--- pkgs/all-pkgs/pixman/default.nix
+++ pkgs/all-pkgs/pixman/default.nix
@@ -1,4 +1,20 @@
-{ fetchurl, stdenv, libpng, glib /*just passthru*/ }:
+{ fetchurl
+, stdenv
+
+, glib
+, libpng
+}:
+
+with {
+  inherit (stdenv)
+    cc
+    isArm
+    isi686
+    isx86_64;
+  inherit (stdenv.lib)
+    enFlag
+    optionals;
+};
 
 stdenv.mkDerivation rec {
   name = "pixman-0.32.8";
@@ -8,20 +24,52 @@ stdenv.mkDerivation rec {
     sha1 = "5c57045622265b877c9bf02d531973eadf942140";
   };
 
-  patches = stdenv.lib.optional stdenv.isDarwin ./fix-clang36.patch;
+  patches = optionals stdenv.cc.isClang [
+    (fetchTritonPatch {
+      rev = "d3fc5e59bd2b4b465c2652aae5e7428b24eb5669";
+      file = "pixman/pixman-fix-clang36.patch";
+      sha256 = "4267d50a561ce07a1d8b3c5127ba3428f3470a1ba9ee3c781d0d2323d9a6e5f6";
+    })
+  ];
 
-  buildInputs = stdenv.lib.optional doCheck libpng;
+  buildInputs = optionals doCheck [
+    libpng
+  ];
 
-  configureFlags = stdenv.lib.optional stdenv.isArm "--disable-arm-iwmmxt";
-
-  doCheck = true;
+  configureFlags = [
+    "--enable-openmp"
+    "--disable-loongson-mmi" # mips
+    (enFlag "mmx" (isi686 || isx86_64) null)
+    (enFlag "sse2" (isi686 || isx86_64) null)
+    (enFlag "sse3" (isi686 || isx86_64) null)
+    "--disable-vmx"
+    (enFlag "arm-simd" isArm null)
+    (enFlag "arm-neon" isArm null)
+    "--disable-arm-iwmmxt"
+    (enFlag "arm-iwmmxt2" isArm null)
+    "--disable-mis-dspr2"
+    (enFlag "gcc-inline-asm" cc.isGNU null)
+    "--disable-static-testprogs"
+    "--enable-timers"
+    "--disable-gtk"
+    (enFlag "libpng" (libpng != null) null)
+  ];
 
   postInstall = glib.flattenInclude;
 
+  doCheck = true;
+  enableParallelBuilding = true;
+
   meta = with stdenv.lib; {
-    homepage = http://pixman.org;
     description = "A low-level library for pixel manipulation";
+    homepage = http://pixman.org;
     license = licenses.mit;
-    platforms = platforms.all;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }

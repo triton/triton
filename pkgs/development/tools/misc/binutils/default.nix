e58bbb95a5e12da374376b30458db139247025fb
William A. Kennington III <william@wkennington.com>
2016-01-15 16:13:50 -0800

    binutils: Move patches

--- pkgs/development/tools/misc/binutils/default.nix
+++ pkgs/development/tools/misc/binutils/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, noSysDirs, zlib
+{ stdenv, fetchurl, fetchTritonPatch, noSysDirs, zlib
 , cross ? null, gold ? true, bison ? null
 }:
 
@@ -18,20 +18,36 @@ stdenv.mkDerivation rec {
     # Turn on --enable-new-dtags by default to make the linker set
     # RUNPATH instead of RPATH on binaries.  This is important because
     # RUNPATH can be overriden using LD_LIBRARY_PATH at runtime.
-    ./new-dtags.patch
+    (fetchTritonPatch {
+      rev = "a998b3cf816a979c701495a9cb4379d5fae83eee";
+      file = "binutils/new-dtags.patch";
+      sha256 = "c2c3e8ce7ec166bfaea20db66d7358942757214bcc7c4b75f005f78ea5b2ba03";
+    })
 
     # Since binutils 2.22, DT_NEEDED flags aren't copied for dynamic outputs.
     # That requires upstream changes for things to work. So we can patch it to
     # get the old behaviour by now.
-    ./dtneeded.patch
+    (fetchTritonPatch {
+      rev = "a998b3cf816a979c701495a9cb4379d5fae83eee";
+      file = "binutils/dtneeded.patch";
+      sha256 = "16dca5a36a94de8f677450b6b83abd96a692be322b4a49e3d0cd2e2cdc3d11b7";
+    })
 
     # Make binutils output deterministic by default.
-    ./deterministic.patch
+    (fetchTritonPatch {
+      rev = "a998b3cf816a979c701495a9cb4379d5fae83eee";
+      file = "binutils/deterministic.patch";
+      sha256 = "912815241d4f64f971b22deb456217b8288b44b2f96ae0e50289a911dbecddc8";
+    })
 
     # Always add PaX flags section to ELF files.
     # This is needed, for instance, so that running "ldd" on a binary that is
     # PaX-marked to disable mprotect doesn't fail with permission denied.
-    ./pt-pax-flags-20121023.patch
+    (fetchTritonPatch {
+      rev = "a998b3cf816a979c701495a9cb4379d5fae83eee";
+      file = "binutils/pt-pax-flags-20121023.patch";
+      sha256 = "6f01fa85cbc2428ac40e2325bafcc01dcbd3dde6ec9f0dc7367b8474049021a2";
+    })
   ];
 
   nativeBuildInputs = optional gold bison;

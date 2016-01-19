95c51055cb8762698884802c5c68e65f5b4636fb
William A. Kennington III <william@wkennington.com>
2016-01-18 22:28:16 -0800

    libbson: 1.3.0 -> 1.3.1

--- pkgs/development/libraries/libbson/default.nix
+++ pkgs/development/libraries/libbson/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "libbson-${version}";
-  version = "1.3.0";
+  version = "1.3.1";
 
   src = fetchurl {
     url = "https://github.com/mongodb/libbson/releases/download/${version}/${name}.tar.gz";
-    sha256 = "1b9hbxfljbqpjslmsvmvlgmll57l9d2ylgcrc7mi5yyf9r92i1k3";
+    sha256 = "0gckh0w05ap3pnlf9cnh3hdrhanz2bn1i5z4inylyirr2aqywz38";
   };
 
   nativeBuildInputs = [ perl ];

bb314e891142b9e0bfcea64a74871c93afb002e0
Cody Opel <codyopel@gmail.com>
2016-01-19 18:56:12 -0500

    rtmpdump: remove inline double single quotes

--- pkgs/all-pkgs/rtmpdump/default.nix
+++ pkgs/all-pkgs/rtmpdump/default.nix
@@ -16,13 +16,14 @@ stdenv.mkDerivation rec {
 
   src = fetchgit {
     url = git://git.ffmpeg.org/rtmpdump;
-    # Currently the latest commit is used (a release has not been made since 2011, i.e. '2.4')
+    # Currently the latest commit is used (a release has not
+    # been made since 2011, i.e. '2.4')
     rev = "fa8646daeb19dfd12c181f7d19de708d623704c0";
     sha256 = "06c137sm6g6av4kazv7qrhz5k8bbyz31x8id7gdwgpppa640m6kc";
   };
 
   makeFlags = [
-    ''prefix=$(out)''
+    "prefix=$(out)"
     "CRYPTO=OPENSSL"
   ] ++ optional stdenv.cc.isClang "CC=clang";
 

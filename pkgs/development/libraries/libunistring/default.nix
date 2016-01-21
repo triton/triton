f9cd95c601dab7f4760194a3893cd6edf1c559de
William A. Kennington III <william@wkennington.com>
2016-01-21 12:48:00 -0800

    libunistring: Disable parallel check

--- pkgs/development/libraries/libunistring/default.nix
+++ pkgs/development/libraries/libunistring/default.nix
@@ -20,6 +20,9 @@ stdenv.mkDerivation rec {
   # http://lists.gnu.org/archive/html/bug-libunistring/2010-02/msg00004.html .
   doCheck = stdenv.isLinux;
 
+  # One of the tests fails to compile for 0.9.6 when run in parallel
+  parallelCheck = false;
+
   meta = {
     homepage = http://www.gnu.org/software/libunistring/;
 

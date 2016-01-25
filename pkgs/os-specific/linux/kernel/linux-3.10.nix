27e6299052cc46e6377ab948a2756cb87fb9fe68
William A. Kennington III <william@wkennington.com>
2016-01-24 22:09:13 -0800

    kernel: 3.10.92 -> 3.10.95

--- pkgs/os-specific/linux/kernel/linux-3.10.nix
+++ pkgs/os-specific/linux/kernel/linux-3.10.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "3.10.92";
+  version = "3.10.95";
   extraMeta.branch = "3.10";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
-    sha256 = "0z0jdix1mfpnnc8cxw7rzpnhxdayckpnrasvxi1qf0dwhcqgk92d";
+    sha256 = "09mxi7v6cljb9gaav8y3xvn2c7scr1l1lrsab1r5gnkv2gb8cs7m";
   };
 
   kernelPatches = args.kernelPatches ++ [ { name = "cve-2016-0728"; patch = ./cve-2016-0728.patch; } ];

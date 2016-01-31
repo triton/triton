cb4bbebf960cde7406488cb8815e795d21825734
Cody Opel <codyopel@gmail.com>
2016-01-31 07:46:02 -0500

    vala: refactor

--- pkgs/all-pkgs/vala/default.nix
+++ pkgs/all-pkgs/vala/default.nix
@@ -4,10 +4,10 @@
 , fetchurl
 , flex
 , gettext
-, gobject-introspection
 , libxslt
 
 , glib
+, gobject-introspection
 , libiconv
 }:
 
@@ -22,8 +22,22 @@ stdenv.mkDerivation rec {
     sha256 = "1pyyhfw3zzbhxfscbn8xz70dg6vx0kh8gshzikpxczhg01xk7w31";
   };
 
+  nativeBuildInputs = [
+    autoreconfHook
+    bison
+    flex
+    gettext
+    libxslt
+  ];
+
+  buildInputs = [
+    glib
+    gobject-introspection
+    libiconv
+  ];
+
   postPatch = ''
-    patchShebangs tests/testrunner.sh
+    patchShebangs ./tests/testrunner.sh
   '' +
   /* dbus tests require machine-id */ ''
     sed -i tests/Makefile.am \
@@ -36,27 +50,15 @@ stdenv.mkDerivation rec {
     "--disable-coverage"
   ];
 
-  nativeBuildInputs = [
-    autoreconfHook
-    bison
-    flex
-    gettext
-    gobject-introspection
-    libxslt
-  ];
-
-  buildInputs = [
-    glib
-    libiconv
-  ];
-
   doCheck = true;
 
   meta = with stdenv.lib; {
     description = "Compiler for GObject type system";
     homepage = "http://live.gnome.org/Vala";
     license = licenses.lgpl21Plus;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [
+      codyopel
+    ];
     platforms = [
       "i686-linux"
       "x86_64-linux"

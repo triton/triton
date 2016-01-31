fb941d9fef3845dc7626761fe92e2a6ab9225023
Cody Opel <codyopel@gmail.com>
2016-01-31 10:07:43 -0500

    vino: refactor

--- pkgs/all-pkgs/vino/default.nix
+++ pkgs/all-pkgs/vino/default.nix
@@ -19,6 +19,15 @@
 , zlib
 }:
 
+with {
+  inherit (stdenv.lib)
+    wtFlag;
+};
+
+assert xorg != null ->
+  xorg.libSM != null &&
+  xorg.libX11 != null;
+
 stdenv.mkDerivation rec {
   name = "vino-${version}";
   versionMajor = "3.18";
@@ -29,23 +38,6 @@ stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/vino/${versionMajor}/${name}.tar.xz";
     sha256 = "0npyzabbk0v4qdxd22dv89v9rnpx69lv4gl7rqzyxm7cpdw6xv07";
   };
-
-  configureFlags = [
-    "--disable-maintainer-mode"
-    "--enable-compile-warnings"
-    "--disable-debug"
-    "--enable-nls"
-    "--enable-ipv6"
-    "--enable-schemas-compile"
-    "--with-telepathy"
-    "--with-secret"
-    "--with-x"
-    "--with-gnutls"
-    "--with-gcrypt"
-    "--with-avahi"
-    "--with-zlib"
-    "--with-jpeg"
-  ];
 
   nativeBuildInputs = [
     intltool
@@ -70,13 +62,35 @@ stdenv.mkDerivation rec {
     zlib
   ];
 
+  configureFlags = [
+    "--disable-maintainer-mode"
+    "--enable-compile-warnings"
+    "--disable-debug"
+    "--enable-nls"
+    "--enable-ipv6"
+    "--enable-schemas-compile"
+    (wtFlag "telepathy" (telepathy_glib != null) null)
+    (wtFlag "secret" (libsecret != null) null)
+    (wtFlag "x" (xorg != null) null)
+    (wtFlag "gnutls" (gnutls != null) null)
+    (wtFlag "gcrypt" (libgcrypt != null) null)
+    (wtFlag "avahi" (avahi != null) null)
+    (wtFlag "zlib" (zlib != null) null)
+    (wtFlag "jpeg" (libjpeg != null) null)
+  ];
+
   doCheck = true;
 
   meta = with stdenv.lib; {
     description = "GNOME desktop sharing server";
     homepage = https://wiki.gnome.org/action/show/Projects/Vino;
     license = licenses.gpl2;
-    maintainers = with maintainers; [ ];
-    platforms = platforms.linux;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }

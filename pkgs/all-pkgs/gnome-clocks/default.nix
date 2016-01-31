da1f5262caca94e828b4e965c4b872a09e63edec
Cody Opel <codyopel@gmail.com>
2016-01-31 16:45:47 -0500

    gnome-clocks: refactor

--- pkgs/all-pkgs/gnome-clocks/default.nix
+++ pkgs/all-pkgs/gnome-clocks/default.nix
@@ -1,35 +1,73 @@
-{ stdenv, intltool, fetchurl, libgweather, libnotify
-, pkgconfig, gtk3, glib, gsound
-, makeWrapper, itstool, libcanberra, libtool
-, librsvg, gdk-pixbuf, geoclue2, wrapGAppsHook
-, gsettings-desktop-schemas, adwaita-icon-theme
-, gnome-desktop, geocode-glib, libxml2
+{ stdenv
+, fetchurl
+, intltool
+, itstool
+, libtool
+
+, adwaita-icon-theme
+, gdk-pixbuf
+, geoclue2
+, geocode-glib
+, glib
+, gnome-desktop
+, gsettings-desktop-schemas
+, gsound
+, gtk3
+, libcanberra
+, libgweather
+, libnotify
+, librsvg
+, libxml2
+, vala
 }:
 
 stdenv.mkDerivation rec {
-  name = "gnome-clocks-3.18.0";
+  name = "gnome-clocks-${version}";
+  versionMajor = "3.18";
+  versionMinor = "0";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = mirror://gnome/sources/gnome-clocks/3.18/gnome-clocks-3.18.0.tar.xz;
+    url = "mirror://gnome/sources/gnome-clocks/${versionMajor}/${name}.tar.xz";
     sha256 = "ca0818ec89e3539201da6b5388365e3d66df815198beccc90e2be44c7822baa0";
   };
 
+  nativeBuildInputs = [
+    intltool
+    itstool
+    libtool
+  ];
+
+  buildInputs = [
+    geoclue2
+    geocode-glib
+    glib
+    gnome-desktop
+    gsound
+    gtk3
+    libgweather
+    libxml2
+    vala
+  ];
+
+  configureFlags = [
+    "--enable-nls"
+    "--enable-rpath"
+    "--enable-schemas-compile"
+  ];
+
   doCheck = true;
 
-  buildInputs = [ pkgconfig gtk3 glib intltool itstool libcanberra
-                  gsettings-desktop-schemas makeWrapper
-                  gdk-pixbuf librsvg adwaita-icon-theme
-                  gnome-desktop geocode-glib geoclue2
-                  libgweather libnotify libtool gsound libxml2
-                ];
-
-  enableParallelBuilding = true;
-
   meta = with stdenv.lib; {
-    homepage = https://wiki.gnome.org/Apps/Clocks;
     description = "Clock application designed for GNOME 3";
-    maintainers = gnome3.maintainers;
+    homepage = https://wiki.gnome.org/Apps/Clocks;
     license = licenses.gpl2;
-    platforms = platforms.linux;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }

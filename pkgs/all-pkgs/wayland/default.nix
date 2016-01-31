ca7dd41310f2e0794701d5db1b1b492835e96209
Cody Opel <codyopel@gmail.com>
2016-01-31 07:32:17 -0500

    wayland: refactor

--- pkgs/all-pkgs/wayland/default.nix
+++ pkgs/all-pkgs/wayland/default.nix
@@ -1,20 +1,22 @@
-{ stdenv, fetchurl
+{ stdenv
+, docbook_xsl
+, doxygen
+, fetchurl
+, graphviz
+, libxslt
+, xmlto
 
-# Optional Dependencies
-, libffi ? null, docbook_xsl ? null, doxygen ? null, graphviz ? null, libxslt ? null, xmlto ? null
-, expat ? null # Build wayland-scanner (currently cannot be disabled as of 1.8.0)
+, libffi
+, expat
 
-# Extra Arguments
 , enableDocumentation ? false
 }:
 
-
-let
-  inherit (stdenv.lib) mkEnable optionals;
-in
-
-# Require the optional to be enabled until upstream fixes or removes the configure flag
-assert expat != null;
+with {
+  inherit (stdenv.lib)
+    enFlag
+    optionals;
+};
 
 stdenv.mkDerivation rec {
   name = "wayland-${version}";
@@ -25,20 +27,24 @@ stdenv.mkDerivation rec {
     sha256 = "1yhy62vkbq8j8c9zaa6yzvn75cd99kfa8n2zfdwl80x019r711ww";
   };
 
+  nativeBuildInputs = [ ]
+    ++ optionals enableDocumentation [
+      docbook_xsl
+      doxygen
+      graphviz
+      libxslt
+      xmlto
+    ];
+
+  buildInputs = [
+    expat
+    libffi
+  ];
+
   configureFlags = [
-    (mkEnable (expat != null)     "scanner"       null)
-    (mkEnable enableDocumentation "documentation" null)
+    "--enable-libraries"
+    (enFlag "documentation" enableDocumentation null)
   ];
-
-  nativeBuildInputs = optionals enableDocumentation [
-    docbook_xsl
-    doxygen
-    graphviz
-    libxslt
-    xmlto
-  ];
-
-  buildInputs = [ libffi expat ];
 
   passthru = {
     inherit version;
@@ -48,7 +54,13 @@ stdenv.mkDerivation rec {
     description = "Reference implementation of the wayland protocol";
     homepage = http://wayland.freedesktop.org/;
     license = licenses.mit;
-    maintainers = with maintainers; [ codyopel wkennington ];
-    platforms = platforms.linux;
+    maintainers = with maintainers; [
+      codyopel
+      wkennington
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }

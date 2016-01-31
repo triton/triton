ba32d6c40e01b8612ddcc805e7275d71b057814b
Cody Opel <codyopel@gmail.com>
2016-01-31 18:12:34 -0500

    gnome-keyring: refactor

--- pkgs/all-pkgs/gnome-keyring/default.nix
+++ pkgs/all-pkgs/gnome-keyring/default.nix
@@ -1,6 +1,22 @@
-{ stdenv, fetchurl, pkgconfig, dbus, libgcrypt, libtasn1, pam, python, glib, libxslt
-, intltool, pango, gcr, gdk_pixbuf, atk, p11_kit, makeWrapper
-, docbook_xsl_ns, docbook_xsl, gtk3, gconf, libgnome_keyring }:
+{ stdenv
+, fetchurl
+, gettext
+, intltool
+, libxslt
+
+, gcr
+, glib
+, libcap_ng
+, libgcrypt
+, p11_kit
+, pam
+}:
+
+with {
+  inherit (stdenv.lib)
+    enFlag
+    wtFlag;
+};
 
 stdenv.mkDerivation rec {
   name = "gnome-keyring-${version}";
@@ -13,23 +29,52 @@ stdenv.mkDerivation rec {
     sha256 = "167dq1yvm080g5s38hqjl0xx5cgpkcl1xqy9p5sxmgc92zb0srrz";
   };
 
-  buildInputs = [
-    dbus libgcrypt pam python gtk3 gconf libgnome_keyring
-    pango gcr gdk_pixbuf atk p11_kit makeWrapper
+  nativeBuildInputs = [
+    gettext
+    intltool
   ];
 
-  propagatedBuildInputs = [ glib libtasn1 libxslt ];
-
-  nativeBuildInputs = [ pkgconfig intltool docbook_xsl_ns docbook_xsl ];
+  buildInputs = [
+    gcr
+    glib
+    libcap_ng
+    libgcrypt
+    p11_kit
+    pam
+  ];
 
   configureFlags = [
-    "--with-ca-certificates=/etc/ssl/certs/ca-certificates.crt" # NixOS hardcoded path
-    "--with-pkcs11-config=$$out/etc/pkcs11/" # installation directories
-    "--with-pkcs11-modules=$$out/lib/pkcs11/"
+    "--disable-maintainer-mode"
+    "--enable-nls"
+    "--enable-schemas-compile"
+    (enFlag "pam" (pam != null) null)
+    "--enable-ssh-agent"
+    "--disable-selinux"
+    "--disable-p11-tests"
+    "--disable-doc"
+    "--disable-debug"
+    "--disable-coverage"
+    "--disable-valgrind"
+    #"--with-dbus-services="
+    #"--with-pam-dir="
+    "--with-pkcs11-config=\${out}/etc/pkcs11/"
+    "--with-pkcs11-modules=\${out}/lib/pkcs11/"
+    (wtFlag "libcap-ng" (libcap_ng != null) null)
   ];
 
   meta = with stdenv.lib; {
-    platforms = platforms.linux;
-    #maintainers = gnome3.maintainers;
+    description = "Password and keyring managing daemon";
+    homepage = https://wiki.gnome.org/Projects/GnomeKeyring;
+    license = with licenses; [
+      gpl2Plus
+      lgpl2Plus
+    ];
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }

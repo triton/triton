5660496d23e787eef6ee21ef45148f4591729504
Cody Opel <codyopel@gmail.com>
2016-01-31 12:24:42 -0500

    gsound: 1.0.1 -> 1.0.2

--- pkgs/all-pkgs/gsound/default.nix
+++ pkgs/all-pkgs/gsound/default.nix
@@ -1,23 +1,61 @@
-{ stdenv, fetchurl, pkgconfig, glib, libcanberra, gobjectIntrospection, libtool
+{ stdenv
+, fetchurl
+, libtool
+
+, glib
+, gobject-introspection
+, libcanberra
+, vala
 }:
 
-let
-  majVer = "1.0";
-in stdenv.mkDerivation rec {
-  name = "gsound-${majVer}.1";
+with {
+  inherit (stdenv.lib)
+    enFlag;
+};
+
+stdenv.mkDerivation rec {
+  name = "gsound-${version}";
+  versionMajor = "1.0";
+  versionMinor = "2";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gsound/${majVer}/${name}.tar.xz";
-    sha256 = "ea0dd94429c0645f2f98824274ef04543fe459dd83a5449a68910acc3ba67f29";
+    url = "mirror://gnome/sources/gsound/${versionMajor}/${name}.tar.xz";
+    sha256 = "0lwfwx2c99qrp08pfaj59pks5dphsnxjgrxyadz065d8xqqgza5v";
   };
 
-  buildInputs = [ pkgconfig glib libcanberra gobjectIntrospection libtool ];
+  nativeBuildInputs = [
+    libtool
+  ];
+
+  buildInputs = [
+    glib
+    gobject-introspection
+    libcanberra
+    vala
+  ];
+
+  configureFlags = [
+    "--disable-maintainer-mode"
+    "--enable-compile-warnings"
+    "--disable-Werror"
+    "--disable-gtk-doc"
+    "--disable-gtk-doc-html"
+    "--disable-gtk-doc-pdf"
+    (enFlag "introspection" (gobject-introspection != null) null)
+    (enFlag "vala" (vala != null) null)
+  ];
 
   meta = with stdenv.lib; {
+    description = "GObject wrapper around the libcanberra sound event library";
     homepage = https://wiki.gnome.org/Projects/GSound;
-    description = "Small library for playing system sounds";
-    #maintainers = gnome3.maintainers;
-    license = licenses.gpl2;
-    platforms = platforms.linux;
+    license = licenses.lgpl21Plus;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }

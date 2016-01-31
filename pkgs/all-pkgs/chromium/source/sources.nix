cf1b88eb793687f1fb9f044b59feaca9f634341a
William A. Kennington III <william@wkennington.com>
2016-01-30 16:11:31 -0800

    chromium: Updates

--- pkgs/all-pkgs/chromium/source/sources.nix
+++ pkgs/all-pkgs/chromium/source/sources.nix
@@ -7,15 +7,15 @@
     sha256bin64 = "0zbgjx2k1q7kn5a7sw1l2527xw33ngaffakkfiajfx45lazmp48g";
   };
   beta = {
-    version = "48.0.2564.82";
-    sha256 = "1z8l2wzgsap4a9vprh4f4dv3393zrpni4f468mysw6yh4zs4p9nd";
-    sha256bin32 = "0bh2hv2mdsx3x1jjpysfz8h52s2wd01vijwb7902zz31jz7spz07";
-    sha256bin64 = "0963hnj1br03zsjl6188nifdxacm55h9x2222s994hss540q9wha";
+    version = "49.0.2623.28";
+    sha256 = "1cpgfcw6kgw70k14wa1m5qzj4rnvkzrw1am30d34516mbwlmmqc8";
+    sha256bin32 = "0nwhjav1x325h1drqknrjl5glivn4fdpahs1nlaqfk8kbjh1q113";
+    sha256bin64 = "0wj6j0v3rngsclzbb8rc8l90wdi98j2sb60r2ypfihaxq2gd138y";
   };
   stable = {
-    version = "48.0.2564.82";
-    sha256 = "1z8l2wzgsap4a9vprh4f4dv3393zrpni4f468mysw6yh4zs4p9nd";
-    sha256bin32 = "0sk0clhkrbvnnwc74hnsy872hkabsb9wfzfs4jzi4yqfwb9ca92x";
-    sha256bin64 = "0ds6z1ll3s6d9wmz03wdzi560fyzs4aqi9cw7x2zkgnghx078ris";
+    version = "48.0.2564.97";
+    sha256 = "1nsjn8zlqljizy5dmg2rxwkw4k5j4yi15ywxi9ca3acv68dbhd3p";
+    sha256bin32 = "0gdd9krsfsixw5b03msm7nf7zl7xh4chzkh9srb19c1kzygl2j2l";
+    sha256bin64 = "1wmw17gdq1j6vxfdc8s94jc8z30pza8nipk4dh590xnb7faihr52";
   };
 }

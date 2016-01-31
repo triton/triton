bd7dd8205b699c23de6766a3b4bae33c002496d5
Cody Opel <codyopel@gmail.com>
2016-01-31 13:40:50 -0500

    grilo: 0.2.12 -> 0.2.15

--- pkgs/all-pkgs/grilo/default.nix
+++ pkgs/all-pkgs/grilo/default.nix
@@ -1,33 +1,80 @@
-{ stdenv, fetchurl, pkgconfig, file, intltool, glib
-, libxml2, gobjectIntrospection, libsoup, totem-pl-parser }:
+{ stdenv
+, fetchurl
+, file
+, gettext
+, intltool
+
+, glib
+, gobject-introspection
+, liboauth
+, libsoup
+, libxml2
+, totem-pl-parser
+, vala
+}:
+
+with {
+  inherit (stdenv.lib)
+    enFlag;
+};
 
 stdenv.mkDerivation rec {
-  name = "grilo-0.2.12";
+  name = "grilo-${version}";
+  versionMajor = "0.2";
+  versionMinor = "15";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/grilo/0.2/${name}.tar.xz";
-    sha256 = "11bvc7rsrjjwz8hp67p3fn8zmywrpawrcbi3vgw8b0dwa0sndd2m";
+    url = "mirror://gnome/sources/grilo/${versionMajor}/${name}.tar.xz";
+    sha256 = "05b8sqfmywg45b9frya6xmw5l3c8vf5a1nhy51nyfs0a4n1japbg";
   };
+
+  nativeBuildInputs = [
+    file
+    gettext
+    intltool
+  ];
+
+  buildInputs = [
+    glib
+    gobject-introspection
+    liboauth
+    libsoup
+    libxml2
+    totem-pl-parser
+    vala
+  ];
 
   setupHook = ./setup-hook.sh;
 
-  configureFlags = [ "--enable-grl-pls" "--enable-grl-net" ];
-
-  preConfigure = ''
-    for f in src/Makefile.in libs/pls/Makefile.in libs/net/Makefile.in; do
-       substituteInPlace $f --replace @INTROSPECTION_GIRDIR@ "$out/share/gir-1.0/"
-       substituteInPlace $f --replace @INTROSPECTION_TYPELIBDIR@ "$out/lib/girepository-1.0"
-    done
-  '';
-
-  buildInputs = [ pkgconfig file intltool glib libxml2 libsoup
-                  totem-pl-parser gobjectIntrospection ];
+  configureFlags = [
+    "--enable-compile-warnings"
+    "--disable-iso-c"
+    "--disable-maintainer-mode"
+    "--disable-test-ui"
+    "--enable-grl-net"
+    (enFlag "grl-pls" (totem-pl-parser != null) null)
+    "--disable-debug"
+    # Flag is not a boolean
+    #"--disable-tests"
+    "--disable-gtk-doc"
+    "--disable-gtk-doc-html"
+    "--disable-gtk-doc-pdf"
+    (enFlag "introspection" (gobject-introspection != null) null)
+    (enFlag "vala" (vala != null) null)
+    "--enable-nls"
+  ];
 
   meta = with stdenv.lib; {
-    homepage = https://wiki.gnome.org/action/show/Projects/Grilo;
-    description = "Framework that provides access to various sources of multimedia content, using a pluggable system";
-    maintainers = gnome3.maintainers;
-    license = licenses.lgpl2;
-    platforms = platforms.linux;
+    description = "A framework for easy media discovery and browsing";
+    homepage = https://wiki.gnome.org/Projects/Grilo;
+    license = licenses.lgpl21Plus;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }

e53e7a6d524787339c7bf253e73d31d98dc83ed2
Cody Opel <codyopel@gmail.com>
2016-02-01 06:09:44 -0500

    gnome-menus: add patches

--- pkgs/all-pkgs/gnome-menus/default.nix
+++ pkgs/all-pkgs/gnome-menus/default.nix
@@ -1,4 +1,5 @@
 { stdenv
+, fetchTritonPatch
 , fetchurl
 , gettext
 , intltool
@@ -31,6 +32,19 @@ stdenv.mkDerivation rec {
   buildInputs = [
     glib
     gobject-introspection
+  ];
+
+  patches = [
+    (fetchTritonPatch {
+      rev = "e7b7c5f328fda18bc43aa8bbf73520441295850b";
+      file = "gnome-menus/gnome-menus-3.13.3-multiple-desktop.patch";
+      sha256 = "f22defd228de353d0c45ff69570c9dc7bdf20831a87f54aeb5c9e5ab3df0d232";
+    })
+    (fetchTritonPatch {
+      rev = "e7b7c5f328fda18bc43aa8bbf73520441295850b";
+      file = "gnome-menus/gnome-menus-3.13.3-multiple-desktop2.patch";
+      sha256 = "2693e4f411e6b9a5f1f93bf3aaae70e88e23ae5139c00084e1154d489b1a48de";
+    })
   ];
 
   configureFlags = [

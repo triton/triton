d93c80860dc93b3858cbf4cfbedb30bdb07ca20c
Cody Opel <codyopel@gmail.com>
2016-02-01 06:56:01 -0500

    gnome-screenshot: refactor

--- pkgs/all-pkgs/gnome-screenshot/default.nix
+++ pkgs/all-pkgs/gnome-screenshot/default.nix
@@ -2,17 +2,12 @@
 , fetchurl
 , intltool
 , itstool
-, makeWrapper
 
-, adwaita-icon-theme
-, bash
 , gdk-pixbuf
 , glib
-, gnome-themes-standard
 , gsettings-desktop-schemas
 , gtk3
 , libcanberra
-, librsvg
 , xorg
 }:
 
@@ -28,25 +23,17 @@ stdenv.mkDerivation rec {
     sha256 = "eba64dbf4acf0ab8222fec549d0a4f2dd7dbd51c255e7978dedf1f5c06a98841";
   };
 
-  propagatedUserEnvPkgs = [
-    gnome-themes-standard
-  ];
-
   nativeBuildInputs = [
     intltool
     itstool
-    makeWrapper
   ];
 
   buildInputs = [
-    adwaita-icon-theme
-    bash
     gdk-pixbuf
     glib
     gsettings-desktop-schemas
     gtk3
     libcanberra
-    librsvg
     xorg.libX11
     xorg.libXext
   ];
@@ -55,20 +42,6 @@ stdenv.mkDerivation rec {
     "--enable-nls"
     "--enable-schemas-compile"
   ];
-
-  NIX_CFLAGS_COMPILE = [
-    "-I${glib}/include/gio-unix-2.0"
-  ];
-
-  preFixup = ''
-    wrapProgram "$out/bin/gnome-screenshot" \
-      --set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE" \
-      --prefix XDG_DATA_DIRS : "${gtk3}/share" \
-      --prefix XDG_DATA_DIRS : "${gnome-themes-standard}/share" \
-      --prefix XDG_DATA_DIRS : "$out/share" \
-      --prefix XDG_DATA_DIRS : "$XDG_ICON_DIRS" \
-      --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH"
-  '';
 
   doCheck = true;
 

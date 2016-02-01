a4a32c8e8f6b036f376ca0ae8aef6df0e1c1634a
Cody Opel <codyopel@gmail.com>
2016-02-01 08:05:25 -0500

    net-tools: use mirror until fetchFromSourceforge is fixed

--- pkgs/all-pkgs/net-tools/default.nix
+++ pkgs/all-pkgs/net-tools/default.nix
@@ -1,26 +1,50 @@
-{ stdenv, fetchFromSourceforge, bluez }:
+{ stdenv
+#, fetchFromSourceforge
+, fetchurl
+, unzip
+
+, bluez
+}:
 
 stdenv.mkDerivation rec {
   name = "net-tools-2016-01-27";
 
-  src = fetchFromSourceforge {
+  /*src = fetchFromSourceforge {
     repo = "net-tools";
     rev = "3f170bff115303e92319791cbd56371e33dcbf6d";
     sha256 = "0sj2mqp74p25ijlbnf8x7xyfb74j09w0vd7ya7i24zadcbzh103r";
+  };*/
+
+  src = fetchurl {
+    url = "https://dl.dropboxusercontent.com/s/jhfbl23megweaj9/" +
+          "net-tools-code-3f170bff115303e92319791cbd56371e33dcbf6d.zip";
+    sha256 = "07gl1myln86mnrgk6j0ba2li9bywd2nbz1rhn5vfdwc2645dqnjr";
   };
 
-  buildInputs = [ bluez ];
+  nativeBuildInputs = [
+    unzip
+  ];
+
+  buildInputs = [
+    bluez
+  ];
 
   preBuild = ''
     cp ${./config.h} config.h
-    makeFlagsArray+=("BASEDIR=$out")
-    makeFlagsArray+=("mandir=/share/man")
+    makeFlagsArray+=(
+      "BASEDIR=$out"
+      "mandir=/share/man"
+    )
   '';
 
-  meta = {
+  meta = with stdenv.lib; {
+    description = "Tools for controlling the network subsystem in Linux";
     homepage = http://net-tools.sourceforge.net/;
-    description = "A set of tools for controlling the network subsystem in Linux";
-    license = stdenv.lib.licenses.gpl2Plus;
-    platforms = stdenv.lib.platforms.linux;
+    license = licenses.gpl2Plus;
+    maintainers = with maintainers; [ ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }

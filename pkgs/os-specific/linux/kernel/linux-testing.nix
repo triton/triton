09c578bd89064e1d9c88e1cf24ba58b17a5b6b58
William A. Kennington III <william@wkennington.com>
2016-02-08 12:56:47 -0800

    linux-testing: 4.5-rc2 -> 4.5-rc3

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.5-rc2";
+  version = "4.5-rc3";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/testing/linux-${version}.tar.xz";
-    sha256 = "1nq61nimgvl7m7rrimr95ixwkc5sd473m5kvaf5qdyhfnh7m4br3";
+    sha256 = "11j966f4pgrsa0cvl2f51lry54a07vgyhir95ca7vjnn32rv7rgs";
   };
 
   features.iwlwifi = true;

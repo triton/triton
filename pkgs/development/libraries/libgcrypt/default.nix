227617d5719635b0ae98945de67dd798cff7ff51
William A. Kennington III <william@wkennington.com>
2016-02-09 15:01:05 -0800

    libgcrypt: 1.6.4 -> 1.6.5

--- pkgs/development/libraries/libgcrypt/default.nix
+++ pkgs/development/libraries/libgcrypt/default.nix
@@ -10,14 +10,14 @@ with stdenv.lib;
 let
   optLibcap = shouldUsePkg libcap;
   #optPth = shouldUsePkg pth;
-  optPth = null; # Broken as of 1.6.3
+  optPth = null; # Broken as of 1.6.5
 in
 stdenv.mkDerivation rec {
-  name = "libgcrypt-1.6.4";
+  name = "libgcrypt-1.6.5";
 
   src = fetchurl {
     url = "mirror://gnupg/libgcrypt/${name}.tar.bz2";
-    sha256 = "09k06gs27gxfha07sa9rpf4xh6mvphj9sky7n09ymx75w9zjrg69";
+    sha256 = "0959mwfzsxhallxdqlw359xg180ll2skxwyy35qawmfl89cbr7pl";
   };
 
   buildInputs = [ libgpgerror optLibcap optPth ];
@@ -27,19 +27,9 @@ stdenv.mkDerivation rec {
     (mkEnable (optPth != null)    "random-daemon" null)
   ];
 
-  # Make sure libraries are correct for .pc and .la files
-  # Also make sure includes are fixed for callers who don't use libgpgcrypt-config
+  # Make sure includes are fixed for callers who don't use libgpgcrypt-config
   postInstall = ''
     sed -i 's,#include <gpg-error.h>,#include "${libgpgerror}/include/gpg-error.h",g' $out/include/gcrypt.h
-  '' + optionalString (!stdenv.isDarwin && optLibcap != null) ''
-    sed -i 's,\(-lcap\),-L${optLibcap}/lib \1,' $out/lib/libgcrypt.la
-  '';
-
-  # TODO: figure out why this is even necessary and why the missing dylib only crashes
-  # random instead of every test
-  preCheck = stdenv.lib.optionalString stdenv.isDarwin ''
-    mkdir -p $out/lib
-    cp src/.libs/libgcrypt.20.dylib $out/lib
   '';
 
   doCheck = true;

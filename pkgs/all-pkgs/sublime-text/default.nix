425a4717edcbbe973dfaf4ff63a864262b348812
Cody Opel <codyopel@gmail.com>
2016-02-09 09:46:11 -0500

    sublime-text: 3083 -> 3103

--- pkgs/all-pkgs/sublime-text/default.nix
+++ pkgs/all-pkgs/sublime-text/default.nix
@@ -4,57 +4,41 @@
 
 , bzip2
 , cairo
+, gksu
 , glib
 , gtk2
+, libredirect
 , openssl
 , pango
 , xorg
+
+, pkexecPath ? "/var/setuid-wrappers/pkexec"
 }:
 
 with {
   inherit (stdenv)
-    system
-    is64bit;
+    system;
   inherit (stdenv.lib)
     makeLibraryPath
     optionalString;
 };
 
 let
-  version = "3083";
+  version = "3103";
 in
+
+assert stdenv.system != "x86_64-linux" ->
+  throw "Sublime Text is not supported on the `${system}` platform";
 
 let
   sublime-text-bin = stdenv.mkDerivation rec {
     name = "sublime-text-bin-${version}";
 
     src = fetchurl {
-      url =
-        "http://c758482.r82.cf2.rackcdn.com/sublime_text_3_build_${version}" +
-        "${if system == "x86_64-linux" then
-            "_x64"
-          else
-            null
-          }.tar.bz2";
-      sha256 =
-        if system == "i686-linux" then
-          "0r9irk2gdwdx0dk7lgssr4krfvf3lf71pzaz5hyjc704zaxf5s49"
-        else if system == "x86_64-linux" then
-          "1vhlrqz7xscmjnxpz60mdpvflanl26d7673ml7psd75n0zvcfra5"
-        else
-          throw "Sublime Text is not supported on the `${system}` platform";
+      url = "https://download.sublimetext.com/"
+          + "sublime_text_3_build_${version}_x64.tar.bz2";
+      sha256 = "1x8kb3prs6wa5s5rj0gfq96zx6k5q3s168yhfsa36x2szi6x6y4x";
     };
-
-    patchPhase = ''
-      # Fix paths
-      sed \
-        -e 's,/opt/sublime_text/,,' \
-        -e 's,sublime-text,sublime_text,' \
-        -i sublime_text.desktop
-
-      # Rename icon file
-      mv -v Icon/256x256/sublime-text.png Icon/256x256/sublime_text.png
-    '';
 
     nativeBuildInputs = [
       makeWrapper
@@ -68,28 +52,52 @@ let
       xorg.libX11
     ];
 
+    redirects = [
+      "/usr/bin/gksudo=${gksu}/bin/gksudo"
+      "/usr/bin/pkexec=${pkexecPath}"
+    ];
+
+    postPhase =
+      /* Fix paths */ ''
+        sed -i sublime_text.desktop \
+          -e 's,/opt/sublime_text/,,' \
+          -e 's,sublime-text,sublime_text,'
+      '' +
+      /* Rename icon file */ ''
+        mv -v Icon/256x256/sublime-text.png Icon/256x256/sublime_text.png
+      '';
+
     buildPhase = ''
-      for i in sublime_text plugin_host crash_reporter ; do
+      for i in 'sublime_text' 'plugin_host' 'crash_reporter' ; do
         patchelf \
           --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-          --set-rpath ${libPath}:${stdenv.cc.cc}/lib${optionalString is64bit "64"} \
+          --set-rpath ${libPath}:${stdenv.cc.cc}/lib64 \
           $i
       done
+    '' +
+    /* Rewrite gksudo/pkexec argument. Note that we can't delete
+       bytes in binary. */ ''
+      sed -i sublime_text \
+        -e 's,/bin/cp\x00,cp\x00\x00\x00\x00\x00\x00,g'
     '';
 
     installPhase = ''
-      mkdir -p $out/
+      mkdir -pv $out/
       cp -prvd * $out/
-
-      # Without this, plugin_host crashes, even though it has the rpath
-      wrapProgram \
-        $out/plugin_host \
-        --prefix LD_PRELOAD : ${stdenv.cc.cc}/lib${optionalString is64bit "64"}/libgcc_s.so.1:${openssl}/lib/libssl.so:${bzip2}/lib/libbz2.so
+    '' + ''
+      wrapProgram $out/sublime_text \
+        --set LD_PRELOAD '${libredirect}/lib/libredirect.so' \
+        --set NIX_REDIRECTS ${builtins.concatStringsSep ":" redirects}
+    '' +
+    /* Without this, plugin_host crashes, even though it has the rpath */ ''
+      wrapProgram $out/plugin_host \
+        --prefix LD_PRELOAD : '${stdenv.cc.cc}/li64/libgcc_s.so.1' \
+        --prefix LD_PRELOAD : '${openssl}/lib/libssl.so' \
+        --prefix LD_PRELOAD : '${bzip2}/lib/libbz2.so'
     '';
 
     dontStrip = true;
     dontPatchELF = true;
-    enableParallelBuilding = true;
   };
 in
 
@@ -101,17 +109,15 @@ stdenv.mkDerivation rec {
   ];
 
   installPhase = ''
-    mkdir -p $out/bin
+    mkdir -pv $out/bin
     ln -sv ${sublime-text-bin}/sublime_text $out/bin/sublime_text
     ln -sv ${sublime-text-bin}/sublime_text $out/bin/subl
 
-    mkdir -p $out/share/applications
+    mkdir -pv $out/share/applications
     ln -sv ${sublime-text-bin}/sublime_text.desktop $out/share/applications
-    mkdir -p $out/share/icons
+    mkdir -pv $out/share/icons
     ln -sv ${sublime-text-bin}/Icon/256x256/sublime_text.png $out/share/icons
   '';
-
-  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     description = "Sophisticated text editor for code, markup and prose";
@@ -121,7 +127,6 @@ stdenv.mkDerivation rec {
     ];
     license = licenses.unfreeRedistributable;
     platforms = [
-      "i686-linux"
       "x86_64-linux"
     ];
   };

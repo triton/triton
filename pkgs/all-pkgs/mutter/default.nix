734a315fb232308f15f19d245c755db251994313
Cody Opel <codyopel@gmail.com>
2016-02-10 15:33:00 -0500

    mutter: fix wrapper

--- pkgs/all-pkgs/mutter/default.nix
+++ pkgs/all-pkgs/mutter/default.nix
@@ -48,44 +48,6 @@ stdenv.mkDerivation rec {
     sha256 = "108i4qvwhipdlyvd75fykhskp6y4rywkhffmdknpaxbc45pk4sca";
   };
 
-  patches = [
-    (fetchTritonPatch {
-      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
-      file = "mutter/math.patch";
-      sha256 = "8c29cc1d5e414583d9a27884dda09a5bbab7b76cf8598145c2c818b3cf95a273";
-    })
-    (fetchTritonPatch {
-      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
-      file = "mutter/x86.patch";
-      sha256 = "0f7438b60b8c32b9f788245273081c4181eb529610ca804c5ba46d12338b1475";
-    })
-    (fetchTritonPatch {
-      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
-      file = "mutter/mutter-3.18.2-bypass-hint.patch";
-      sha256 = "5743a64e088d61706f9ab70ae8bc0b97bd6633825bfda7bc68fc6c593bd43353";
-    })
-    (fetchTritonPatch {
-      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
-      file = "mutter/mutter-3.18.2-cursor-renderer.patch";
-      sha256 = "0264b85181a489615984d5b3098b5b1747214c0d05fb9d725b8b2b85819a0c06";
-    })
-    (fetchTritonPatch {
-      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
-      file = "mutter/mutter-3.18.2-logical-monitors.patch";
-      sha256 = "8446922fe20b8c35cfdd891e393d936316530d02ac9b00e61abc0b6e465cf61f";
-    })
-    (fetchTritonPatch {
-      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
-      file = "mutter/mutter-3.18.2-wayland-crash.patch";
-      sha256 = "5ad07d72ef05954a53b3e7679ad607621c6076749af431b4ae3c58698731910a";
-    })
-    (fetchTritonPatch {
-      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
-      file = "mutter/mutter-3.18.2-configure-notify.patch";
-      sha256 = "474b01c8bd0c14fe04645c8e0cb7e45b9fb4b37c876c11a8545a6ed1c7a7bd59";
-    })
-  ];
-
   nativeBuildInputs = [
     gettext
     intltool
@@ -133,6 +95,44 @@ stdenv.mkDerivation rec {
     zenity
   ];
 
+  patches = [
+    (fetchTritonPatch {
+      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
+      file = "mutter/math.patch";
+      sha256 = "8c29cc1d5e414583d9a27884dda09a5bbab7b76cf8598145c2c818b3cf95a273";
+    })
+    (fetchTritonPatch {
+      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
+      file = "mutter/x86.patch";
+      sha256 = "0f7438b60b8c32b9f788245273081c4181eb529610ca804c5ba46d12338b1475";
+    })
+    (fetchTritonPatch {
+      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
+      file = "mutter/mutter-3.18.2-bypass-hint.patch";
+      sha256 = "5743a64e088d61706f9ab70ae8bc0b97bd6633825bfda7bc68fc6c593bd43353";
+    })
+    (fetchTritonPatch {
+      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
+      file = "mutter/mutter-3.18.2-cursor-renderer.patch";
+      sha256 = "0264b85181a489615984d5b3098b5b1747214c0d05fb9d725b8b2b85819a0c06";
+    })
+    (fetchTritonPatch {
+      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
+      file = "mutter/mutter-3.18.2-logical-monitors.patch";
+      sha256 = "8446922fe20b8c35cfdd891e393d936316530d02ac9b00e61abc0b6e465cf61f";
+    })
+    (fetchTritonPatch {
+      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
+      file = "mutter/mutter-3.18.2-wayland-crash.patch";
+      sha256 = "5ad07d72ef05954a53b3e7679ad607621c6076749af431b4ae3c58698731910a";
+    })
+    (fetchTritonPatch {
+      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
+      file = "mutter/mutter-3.18.2-configure-notify.patch";
+      sha256 = "474b01c8bd0c14fe04645c8e0cb7e45b9fb4b37c876c11a8545a6ed1c7a7bd59";
+    })
+  ];
+
   configureFlags = [
     "--enable-nls"
     "--enable-glibtest"
@@ -158,6 +158,11 @@ stdenv.mkDerivation rec {
           $out/lib/mutter \
           $out/lib/girepository-1.0
       fi
+
+      wrapProgram $out/bin/mutter \
+        --set 'GI_TYPELIB_PATH' "$GI_TYPELIB_PATH" \
+        --prefix 'XDG_DATA_DIRS' : "$out/share" \
+        --prefix 'XDG_DATA_DIRS' : "$GSETTINGS_SCHEMAS_PATH"
     '';
 
   meta = with stdenv.lib; {

bdef0d89ae0e6d55b5cdb0aae4c42fb83c737ae2
Cody Opel <codyopel@gmail.com>
2016-02-11 00:02:34 -0500

    eog: fix wrapper

--- pkgs/all-pkgs/eog/default.nix
+++ pkgs/all-pkgs/eog/default.nix
@@ -3,12 +3,16 @@
 , gettext
 , intltool
 , itstool
+, makeWrapper
 
 , adwaita-icon-theme
+, atk
+, dconf
 , exempi
 , gdk-pixbuf
 , glib
 , gnome-desktop
+, gobject-introspection
 , gsettings-desktop-schemas
 , gtk3
 , lcms2
@@ -17,6 +21,7 @@
 , libpeas
 , librsvg
 , libxml2
+, pango
 , shared_mime_info
 , xorg
 }:
@@ -43,14 +48,17 @@ stdenv.mkDerivation rec {
     gettext
     intltool
     itstool
+    makeWrapper
   ];
 
   buildInputs = [
     adwaita-icon-theme
+    atk
     exempi
     gdk-pixbuf
     glib
     gnome-desktop
+    gobject-introspection
     gsettings-desktop-schemas
     gtk3
     lcms2
@@ -59,6 +67,7 @@ stdenv.mkDerivation rec {
     libpeas
     librsvg
     libxml2
+    pango
     shared_mime_info
     xorg.libX11
   ];
@@ -81,6 +90,17 @@ stdenv.mkDerivation rec {
     (wtFlag "librsvg" (librsvg != null) null)
     (wtFlag "x" (xorg != null) null)
   ];
+
+  preFixup = ''
+    wrapProgram $out/bin/eog \
+      --set 'GDK_PIXBUF_MODULE_FILE' "$GDK_PIXBUF_MODULE_FILE" \
+      --set 'GSETTINGS_BACKEND' 'dconf' \
+      --prefix 'GIO_EXTRA_MODULES' : "${dconf}/lib/gio/modules" \
+      --prefix 'GI_TYPELIB_PATH' : "$GI_TYPELIB_PATH" \
+      --prefix 'XDG_DATA_DIRS' : "$GSETTINGS_SCHEMAS_PATH" \
+      --prefix 'XDG_DATA_DIRS' : "$out/share" \
+      --prefix 'XDG_DATA_DIRS' : "$XDG_ICON_DIRS"
+  '';
 
   meta = with stdenv.lib; {
     description = "The Eye of GNOME image viewer";

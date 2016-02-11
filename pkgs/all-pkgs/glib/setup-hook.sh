983fab8008f1910ec39194ba385b3ee5ed69e664
Cody Opel <codyopel@gmail.com>
2016-02-10 21:28:16 -0500

    glib: install schemas in the standard system directory

--- pkgs/all-pkgs/glib/setup-hook.sh
+++ pkgs/all-pkgs/glib/setup-hook.sh
@@ -1,27 +1,28 @@
-# Install gschemas, if any, in a package-specific directory
+# Install gschemas in a package-specific directory
 
 make_glib_find_gsettings_schemas() {
 
-  # For packages that need gschemas of other packages (e.g. empathy)
-  if [[ -d "${1}/share/gsettings-schemas/"*"/glib-2.0/schemas" ]] ; then
-    addToSearchPath GSETTINGS_SCHEMAS_PATH "${1}/share/gsettings-schemas/"*
+  # Add glib schemas to GSETTINGS_SCHEMAS_PATH
+  if [[ -d "${1}/share/glib-2.0/schemas" ]] ; then
+    addToSearchPath 'GSETTINGS_SCHEMAS_PATH' "${1}/share/glib-2.0/schemas"
   fi
 
 }
 
-
 glibPreFixupPhase() {
 
-  # Move gschemas in case the install flag didn't help
-  if [[ -d "${out}/share/glib-2.0/schemas" ]] ; then
-    mkdir -pv "${out}/share/gsettings-schemas/${name}/glib-2.0"
-    mv -v "${out}/share/glib-2.0/schemas" "${out}/share/gsettings-schemas/${name}/glib-2.0/"
+  # Make sure schemas are installed in $out/share/glib-2.0/schemas
+  if [[ -d "${out}/share/gsettings-schemas/${name}/glib-2.0/schemas" ]] ; then
+    mkdir -pv "${out}/share/glib-2.0/schemas"
+    mv -v \
+      "${out}/share/gsettings-schemas/${name}/glib-2.0/schemas" \
+      "${out}/share/glib-2.0/schemas"
   fi
 
-  addToSearchPath GSETTINGS_SCHEMAS_PATH "${out}/share/gsettings-schemas/${name}"
+  addToSearchPath 'GSETTINGS_SCHEMAS_PATH' "${out}/share/glib-2.0/schemas"
 
 }
 
 envHooks+=('make_glib_find_gsettings_schemas')
-installFlagsArray+=("gsettingsschemadir=${out}/share/gsettings-schemas/$name/glib-2.0/schemas/")
-preFixupPhases+=" glibPreFixupPhase"
+installFlagsArray+=("gsettingsschemadir=${out}/share/glib-2.0/schemas/")
+preFixupPhases+=('glibPreFixupPhase')

986fa200593adc853e445076e1087e9b2d1ab90e
Cody Opel <codyopel@gmail.com>
2016-02-12 04:07:23 -0500

    webkitgtk: refactor

--- pkgs/all-pkgs/webkitgtk/2.4.x.nix
+++ pkgs/all-pkgs/webkitgtk/2.4.x.nix
@@ -63,79 +63,6 @@ stdenv.mkDerivation rec {
     sha256 = "0r651ar3p0f8zwl7764kyimxk5hy88cwy116pv8cl5l8hbkjkpxg";
   };
 
-  CC = "cc";
-
-  patches = [
-    # https://bugs.webkit.org/show_bug.cgi?id=113397
-    (fetchTritonPatch {
-      rev = "6abc19d8cdde923ac47c84223bfa7c784b9b5b94";
-      file = "webkitgtk/webkit-gtk-1.11.90-gtk-docize-fix.patch";
-      sha256 = "1e70b12e8b90a35229f53c74e68d150555c8290e656fa1763fb2b9c1af352884";
-    })
-    # Fix build with recent libjpeg
-    # https://bugs.webkit.org/show_bug.cgi?id=122412
-    (fetchTritonPatch {
-      rev = "6cc6ef88e3830b254059e869fe8e97153794c836";
-      file = "webkitgtk/webkit-gtk-2.4.9-jpeg-9a.patch";
-      sha256 = "b442a8021ec5bfc751708fe6409baff6ae8576d7fb49da95245b6ba2c1557536";
-    })
-    # Fix building with --disable-webgl
-    # https://bugs.webkit.org/show_bug.cgi?id=131267
-    (fetchTritonPatch {
-      rev = "b03892aa1ca4dcc06e7c56bbecff3b65a778472c";
-      file = "webkitgtk/webkit-gtk-2.4.7-disable-webgl.patch";
-      sha256 = "6fb78d5f94806ddb976ceb076e623a2fd67e80111d624a2b3c01fde2fbbcd64e";
-    })
-    (fetchTritonPatch {
-      rev = "d3fc5e59bd2b4b465c2652aae5e7428b24eb5669";
-      file = "webkitgtk/webkit-gtk-2.4.9-webcore-svg-libxml-cflags.patch";
-      sha256 = "4d1938540903afa8ecc2ad84cb680687bc7ab724d15df953d8fe390e3914134a";
-    })
-  ];
-
-  prePatch = ''
-    patchShebangs ./Tools/gtk
-  '';
-
-  configureFlags = [
-    "--enable-largefile"
-    "--enable-webkit1"
-    "--disable-webkit2"
-    "--disable-debug"
-    "--disable-developer-mode"
-    "--enable-optimizations"
-    "--enable-x11-target"
-    "--disable-wayland-target"
-    "--disable-win32-target"
-    "--disable-quartz-target"
-    "--disable-directfb-target"
-    (enFlag "spellcheck" (enchant != null) null)
-    (enFlag "credential-storage" (libsecret != null) null)
-    "--enable-glx"
-    "--enable-egl"
-    "--disable-gles2"
-    "--disable-gamepad"
-    "--enable-video"
-    "--enable-geolocation"
-    "--enable-svg"
-    "--enable-svg-fonts"
-    (enFlag "web-audio" (gstreamer != null && gst-plugins-base != null) null)
-    "--disable-battery-status"
-    "--disable-coverage"
-    "--enable-fast-malloc"
-    "--disable-debug-symbols"
-    "--enable-webgl"
-    "--enable-accelerated-compositing"
-    "--enable-jit"
-    "--disable-ftl-jit" # llvm
-    "--disable-opcode-stats"
-    (enFlag "introspection" (gobject-introspection != null) null)
-    "--enable-glibtest"
-    "--enable-schemas-compile"
-    "--disable-maintainer-mode"
-    "--with-gtk=${gtkVer}.0"
-  ];
-
   nativeBuildInputs = [
     bison
     flex
@@ -182,7 +109,81 @@ stdenv.mkDerivation rec {
     zlib
   ];
 
-  enableParallelBuilding = true;
+  CC = "cc";
+
+  patches = [
+    # https://bugs.webkit.org/show_bug.cgi?id=113397
+    (fetchTritonPatch {
+      rev = "6abc19d8cdde923ac47c84223bfa7c784b9b5b94";
+      file = "webkitgtk/webkit-gtk-1.11.90-gtk-docize-fix.patch";
+      sha256 = "1e70b12e8b90a35229f53c74e68d150555c8290e656fa1763fb2b9c1af352884";
+    })
+    # Fix build with recent libjpeg
+    # https://bugs.webkit.org/show_bug.cgi?id=122412
+    (fetchTritonPatch {
+      rev = "6cc6ef88e3830b254059e869fe8e97153794c836";
+      file = "webkitgtk/webkit-gtk-2.4.9-jpeg-9a.patch";
+      sha256 = "b442a8021ec5bfc751708fe6409baff6ae8576d7fb49da95245b6ba2c1557536";
+    })
+    # Fix building with --disable-webgl
+    # https://bugs.webkit.org/show_bug.cgi?id=131267
+    (fetchTritonPatch {
+      rev = "b03892aa1ca4dcc06e7c56bbecff3b65a778472c";
+      file = "webkitgtk/webkit-gtk-2.4.7-disable-webgl.patch";
+      sha256 = "6fb78d5f94806ddb976ceb076e623a2fd67e80111d624a2b3c01fde2fbbcd64e";
+    })
+    (fetchTritonPatch {
+      rev = "d3fc5e59bd2b4b465c2652aae5e7428b24eb5669";
+      file = "webkitgtk/webkit-gtk-2.4.9-webcore-svg-libxml-cflags.patch";
+      sha256 = "4d1938540903afa8ecc2ad84cb680687bc7ab724d15df953d8fe390e3914134a";
+    })
+  ];
+
+  postPatch = ''
+    patchShebangs ./Tools/gtk
+  '';
+
+  configureFlags = [
+    "--enable-largefile"
+    "--enable-webkit1"
+    "--disable-webkit2"
+    "--disable-debug"
+    "--disable-developer-mode"
+    "--enable-optimizations"
+    "--enable-x11-target"
+    "--disable-wayland-target"
+    "--disable-win32-target"
+    "--disable-quartz-target"
+    "--disable-directfb-target"
+    (enFlag "spellcheck" (enchant != null) null)
+    (enFlag "credential-storage" (libsecret != null) null)
+    "--enable-glx"
+    "--enable-egl"
+    "--disable-gles2"
+    "--disable-gamepad"
+    "--enable-video"
+    "--enable-geolocation"
+    "--enable-svg"
+    "--enable-svg-fonts"
+    (enFlag "web-audio" (
+      gstreamer != null
+      && gst-plugins-base != null) null)
+    "--disable-battery-status"
+    "--disable-coverage"
+    "--enable-fast-malloc"
+    "--disable-debug-symbols"
+    "--enable-webgl"
+    "--enable-accelerated-compositing"
+    "--enable-jit"
+    "--disable-ftl-jit" # llvm
+    "--disable-opcode-stats"
+    (enFlag "introspection" (gobject-introspection != null) null)
+    "--enable-glibtest"
+    "--enable-schemas-compile"
+    "--disable-maintainer-mode"
+    "--with-gtk=${gtkVer}.0"
+  ];
+
   dontAddDisableDepTrack = true;
 
   meta = with stdenv.lib; {

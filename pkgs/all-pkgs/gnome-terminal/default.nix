fc99a2b4a393c47725f42ee2d64756cad92c5109
Cody Opel <codyopel@gmail.com>
2016-02-12 04:10:51 -0500

    gnome-terminal: refactor

--- pkgs/all-pkgs/gnome-terminal/default.nix
+++ pkgs/all-pkgs/gnome-terminal/default.nix
@@ -6,10 +6,13 @@
 , itstool
 , libuuid
 , libxml2
+, makeWrapper
 , which
 
+, adwaita-icon-theme
 , appdata-tools
 , dconf
+, gdk-pixbuf
 , glib
 , gsettings-desktop-schemas
 , gtk3
@@ -37,12 +40,15 @@ stdenv.mkDerivation rec {
     itstool
     libuuid
     libxml2
+    makeWrapper
     which
   ];
 
   buildInputs = [
+    adwaita-icon-theme
     appdata-tools
     dconf
+    gdk-pixbuf
     glib
     gsettings-desktop-schemas
     gtk3
@@ -58,9 +64,13 @@ stdenv.mkDerivation rec {
   ];
 
   preFixup = ''
-    gnomeWrapperArgs+=(
-      "--prefix GIO_EXTRA_MODULES : ${dconf}/lib/gio/modules"
-    )
+    wrapProgram $out/bin/gnome-terminal \
+      --set 'GDK_PIXBUF_MODULE_FILE' "$GDK_PIXBUF_MODULE_FILE" \
+      --set 'GSETTINGS_BACKEND' 'dconf' \
+      --prefix 'GIO_EXTRA_MODULES' : "$GIO_EXTRA_MODULES" \
+      --prefix 'XDG_DATA_DIRS' : "$GSETTINGS_SCHEMAS_PATH" \
+      --prefix 'XDG_DATA_DIRS' : "$out/share" \
+      --prefix 'XDG_DATA_DIRS' : "$XDG_ICON_DIRS"
   '';
 
   meta = with stdenv.lib; {

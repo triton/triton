515e8f9af5ae046a1f8376f0917cb5699d7943b0
Cody Opel <codyopel@gmail.com>
2016-02-13 12:43:38 -0500

    librsvg: refactor

--- pkgs/all-pkgs/librsvg/default.nix
+++ pkgs/all-pkgs/librsvg/default.nix
@@ -29,6 +29,19 @@ stdenv.mkDerivation rec {
     sha256 = "014q7gz6mgfa7pfn0lr13qqv568ad8j1sw9d4vksnpazq0zajvjd";
   };
 
+  buildInputs = [
+    bzip2
+    cairo
+    gdk-pixbuf-core
+    glib
+    gobject-introspection
+    libcroco
+    libgsf
+    libxml2
+    pango
+    vala
+  ];
+
   configureFlags = [
     "--disable-maintainer-mode"
     "--enable-pixbuf-loader"
@@ -47,28 +60,11 @@ stdenv.mkDerivation rec {
   postConfigure = ''
     GDK_PIXBUF=$out/lib/gdk-pixbuf-2.0/2.10.0
     mkdir -p $GDK_PIXBUF/loaders
-    sed -e "s#gdk_pixbuf_moduledir = .*#gdk_pixbuf_moduledir = $GDK_PIXBUF/loaders#" \
-        -i gdk-pixbuf-loader/Makefile
-    sed -e "s#gdk_pixbuf_cache_file = .*#gdk_pixbuf_cache_file = $GDK_PIXBUF/loaders.cache#" \
-        -i gdk-pixbuf-loader/Makefile
-    sed -e "s#\$(GDK_PIXBUF_QUERYLOADERS)#GDK_PIXBUF_MODULEDIR=$GDK_PIXBUF/loaders \$(GDK_PIXBUF_QUERYLOADERS)#" \
-         -i gdk-pixbuf-loader/Makefile
+    sed -i gdk-pixbuf-loader/Makefile \
+      -e "s#gdk_pixbuf_moduledir = .*#gdk_pixbuf_moduledir = $GDK_PIXBUF/loaders#" \
+      -e "s#gdk_pixbuf_cache_file = .*#gdk_pixbuf_cache_file = $GDK_PIXBUF/loaders.cache#" \
+      -e "s#\$(GDK_PIXBUF_QUERYLOADERS)#GDK_PIXBUF_MODULEDIR=$GDK_PIXBUF/loaders \$(GDK_PIXBUF_QUERYLOADERS)#"
   '';
-
-  buildInputs = [
-    bzip2
-    cairo
-    gdk-pixbuf-core
-    glib
-    gobject-introspection
-    libcroco
-    libgsf
-    libxml2
-    pango
-    vala
-  ];
-
-  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     description = "Scalable Vector Graphics (SVG) rendering library";

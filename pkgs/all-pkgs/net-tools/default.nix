1dec84c77c44a6da5ceaf8b3347fba46c89ec00a
Cody Opel <codyopel@gmail.com>
2016-02-13 18:43:05 -0500

    net-tools: use fetchgit

--- pkgs/all-pkgs/net-tools/default.nix
+++ pkgs/all-pkgs/net-tools/default.nix
@@ -1,5 +1,6 @@
 { stdenv
-, fetchFromSourceforge
+#, fetchFromSourceforge
+, fetchgit
 
 , bluez
 }:
@@ -7,10 +8,16 @@
 stdenv.mkDerivation rec {
   name = "net-tools-2016-01-27";
 
-  src = fetchFromSourceforge {
+  /*src = fetchFromSourceforge {
     repo = "net-tools";
     rev = "3f170bff115303e92319791cbd56371e33dcbf6d";
     sha256 = "0sj2mqp74p25ijlbnf8x7xyfb74j09w0vd7ya7i24zadcbzh103r";
+  };*/
+
+  src = fetchgit {
+    url = "http://git.code.sf.net/p/net-tools/code";
+    rev = "3f170bff115303e92319791cbd56371e33dcbf6d";
+    sha256 = "0qarpgc3lnzg2wkmaq3rf70mrydnk2cc3qjxaykv625xm6bdyxy2";
   };
 
   buildInputs = [

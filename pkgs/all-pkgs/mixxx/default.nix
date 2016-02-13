0290e476fb2292bb3ef351f99798b432bc105364
Cody Opel <codyopel@gmail.com>
2016-02-13 12:24:16 -0500

    mixxx: refactor

--- pkgs/all-pkgs/mixxx/default.nix
+++ pkgs/all-pkgs/mixxx/default.nix
@@ -45,46 +45,6 @@ stdenv.mkDerivation rec {
     sha256 = "0vb71w1yq0xwwsclrn2jj9bk8w4n14rfv5c0aw46c11mp8xz7f71";
   };
 
-  postPatch = ''
-    sed -e 's/"which /"type -P /' -i build/depends.py
-  '';
-
-  sconsFlags = [
-    "build=release"
-    "qt5=1"
-    "qtdir=${qt5.qtbase}"
-
-    "opengles=0"
-    "hss1394=0"
-    (scFlag "hid" (libusb != null))
-    (scFlag "bulk" (libusb != null))
-    (scFlag "mad" (libmad != null))
-    "coreaudio=0"
-    "mediafoundation=0"
-    "ipod=0"
-    "vinylcontrol=1"
-    (scFlag "vamp" (vampSDK != null))
-    (scFlag "modplug" (libmodplug != null))
-    (scFlag "faad" (faad2 != null))
-    (scFlag "wv" (wavpack != null))
-    "color=0"
-    "asan=0"
-    "perftools=0"
-    "asmlib=0"
-    "buildtime=0"
-    "qtdebug=0"
-    "verbose=0"
-    "profiling=0"
-    "test=0"
-    (scFlag "shoutcast" (libshout != null))
-    (scFlag "opus" (libopus != null))
-    (scFlag "ffmpeg" (ffmpeg != null))
-    "optimize=portable"
-    "autodjcrates=1"
-    "macappstore=0"
-    "localecompare=1"
-  ];
-
   nativeBuildInputs = [
     scons
   ];
@@ -123,19 +83,60 @@ stdenv.mkDerivation rec {
     xorg.libX11
   ];
 
+  postPatch = ''
+    sed -i build/depends.py \
+      -e 's/"which /"type -P /'
+  '';
+
+  sconsFlags = [
+    "build=release"
+    "qt5=1"
+    "qtdir=${qt5.qtbase}"
+
+    "opengles=0"
+    "hss1394=0"
+    (scFlag "hid" (libusb != null))
+    (scFlag "bulk" (libusb != null))
+    (scFlag "mad" (libmad != null))
+    "coreaudio=0"
+    "mediafoundation=0"
+    "ipod=0"
+    "vinylcontrol=1"
+    (scFlag "vamp" (vampSDK != null))
+    (scFlag "modplug" (libmodplug != null))
+    (scFlag "faad" (faad2 != null))
+    (scFlag "wv" (wavpack != null))
+    "color=0"
+    "asan=0"
+    "perftools=0"
+    "asmlib=0"
+    "buildtime=0"
+    "qtdebug=0"
+    "verbose=0"
+    "profiling=0"
+    "test=0"
+    (scFlag "shoutcast" (libshout != null))
+    (scFlag "opus" (libopus != null))
+    (scFlag "ffmpeg" (ffmpeg != null))
+    "optimize=portable"
+    "autodjcrates=1"
+    "macappstore=0"
+    "localecompare=1"
+  ];
+
   buildPhase = ''
-    runHook preBuild
+    runHook 'preBuild'
     mkdir -p "$out"
     scons \
       -j$NIX_BUILD_CORES -l$NIX_BUILD_CORES \
       $sconsFlags "prefix=$out"
-    runHook postBuild
+    runHook 'postBuild'
   '';
 
   installPhase = ''
-    runHook preInstall
+    runHook 'preInstall'
     scons $sconsFlags "prefix=$out" install
-    runHook postInstall
+    runHook 'postInstall'
   '';
 
   meta = with stdenv.lib; {

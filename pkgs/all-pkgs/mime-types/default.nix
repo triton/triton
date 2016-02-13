77530e0c37a7e5d3246de4ed4909ab32f032cf86
Cody Opel <codyopel@gmail.com>
2016-02-13 12:30:58 -0500

    mime-types: refactor

--- pkgs/all-pkgs/mime-types/default.nix
+++ pkgs/all-pkgs/mime-types/default.nix
@@ -6,22 +6,26 @@ stdenv.mkDerivation {
   name = "mime-types-2016-01-12";
 
   src = fetchurl {
-    url = "http://anonscm.debian.org/cgit/collab-maint/mime-support.git/plain/mime.types?id=fe8d90a379338a4a9dac4ca791ed7aca52fa0423";
+    url = "http://anonscm.debian.org/cgit/collab-maint/mime-support.git/plain/"
+        + "mime.types?id=fe8d90a379338a4a9dac4ca791ed7aca52fa0423";
     sha256 = "1z6q4w90id3g8kmfrxxbkgj3sxqqwfka5cnkvn556l0nbc7zr4wi";
   };
 
   buildCommand = ''
-    mkdir -p $out/etc
-    cp $src $out/etc/mime.types
+    mkdir -pv $out/etc
+    cp -v $src $out/etc/mime.types
   '';
 
   meta = with stdenv.lib; {
-    platforms = [
-      "x86_64-linux"
-      "i686-linux"
-    ];
+    description = "Provides /etc/mime.types file";
+    homepage = http://anonscm.debian.org/cgit/collab-maint/mime-support.git;
+    license = licenses.free;
     maintainers = with maintainers; [
       wkennington
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
     ];
   };
 }

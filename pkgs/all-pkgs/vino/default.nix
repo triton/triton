57d8c14e668e627bc65829165afe9008377420d2
Cody Opel <codyopel@gmail.com>
2016-02-13 11:27:07 -0500

    vino: refactor

--- pkgs/all-pkgs/vino/default.nix
+++ pkgs/all-pkgs/vino/default.nix
@@ -86,7 +86,7 @@ stdenv.mkDerivation rec {
   preFixup = ''
     wrapProgram $out/libexec/vino-server \
       --set 'GSETTINGS_BACKEND' 'dconf' \
-      --prefix 'GIO_EXTRA_MODULES' : "${dconf}/lib/gio/modules" \
+      --prefix 'GIO_EXTRA_MODULES' : "$GIO_EXTRA_MODULES" \
       --prefix 'XDG_DATA_DIRS' : "$GSETTINGS_SCHEMAS_PATH" \
       --prefix 'XDG_DATA_DIRS' : "$out/share" \
       --prefix 'XDG_DATA_DIRS' : "$XDG_ICON_DIRS"

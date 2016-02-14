c0502721b52ee608d680cc0439075d56162df15e
William A. Kennington III <william@wkennington.com>
2016-02-14 15:14:36 -0800

    lmdb: 0.9.16 -> 0.9.18

--- pkgs/development/libraries/lmdb/default.nix
+++ pkgs/development/libraries/lmdb/default.nix
@@ -1,36 +1,30 @@
-{ stdenv, fetchzip }:
+{ stdenv
+, fetchFromGitHub
+}:
 
-let optional = stdenv.lib.optional;
-in stdenv.mkDerivation rec {
+stdenv.mkDerivation rec {
   name = "lmdb-${version}";
-  version = "0.9.16";
+  version = "0.9.18";
 
-  src = fetchzip {
-    url = "https://github.com/LMDB/lmdb/archive/LMDB_${version}.tar.gz";
-    sha256 = "1lkmngscijwiz09gdkqygdp87x55vp8gb4fh4vq7s34k4jv0327l";
+  src = fetchFromGitHub {
+    owner = "LMDB";
+    repo = "lmdb";
+    rev = "LMDB_${version}";
+    sha256 = "01j384kxg36kym060pybr5p6mjw0xv33bqbb8arncdkdq57xk8wg";
   };
 
-  postUnpack = "sourceRoot=\${sourceRoot}/libraries/liblmdb";
-
-  makeFlags = ["prefix=$(out)"]
-              ++ optional stdenv.cc.isClang "CC=clang";
+  prePatch = ''
+    while ! [ -f Makefile ]; do
+      cd *
+    done
+    makeFlagsArray+=("prefix=$out")
+  '';
 
   doCheck = true;
-  checkPhase = "make test";
-
-  preInstall = ''
-    mkdir -p $out/{man/man1,bin,lib,include}
-  '';
+  checkTarget = "test";
 
   meta = with stdenv.lib; {
     description = "Lightning memory-mapped database";
-    longDescription = ''
-      LMDB is an ultra-fast, ultra-compact key-value embedded data store
-      developed by Symas for the OpenLDAP Project. It uses memory-mapped files,
-      so it has the read performance of a pure in-memory database while still
-      offering the persistence of standard disk-based databases, and is only
-      limited to the size of the virtual address space.
-    '';
     homepage = http://symas.com/mdb/;
     maintainers = with maintainers; [ jb55 ];
     license = licenses.openldap;

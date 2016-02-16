d19bd535e6398c3a716416bb1c65ed59f0f98506
William A. Kennington III <william@wkennington.com>
2016-02-15 23:02:21 -0800

    libxslt: Fix

--- pkgs/development/libraries/libxslt/default.nix
+++ pkgs/development/libraries/libxslt/default.nix
@@ -8,14 +8,13 @@ stdenv.mkDerivation rec {
     sha256 = "13029baw9kkyjgr7q3jccw2mz38amq7mmpr5p3bh775qawd1bisz";
   };
 
-  patches = stdenv.lib.optional stdenv.isSunOS ./patch-ah.patch
-    ++ [
-      (fetchpatch {
-        name = "CVE-2015-7995.patch";
-        url = "http://git.gnome.org/browse/libxslt/patch/?id=7ca19df892ca22";
-        sha256 = "1xzg0q94dzbih9nvqp7g9ihz0a3qb0w23l1158m360z9smbi8zbd";
-      })
-    ];
+  patches = [
+    (fetchpatch {
+      name = "CVE-2015-7995.patch";
+      url = "http://git.gnome.org/browse/libxslt/patch/?id=7ca19df892ca22";
+      sha256 = "1xzg0q94dzbih9nvqp7g9ihz0a3qb0w23l1158m360z9smbi8zbd";
+    })
+  ];
 
   outputs = [ "out" "doc" ];
 

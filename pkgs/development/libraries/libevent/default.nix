02ebbd02559840b1e782f54b09fd265bd080e93b
William A. Kennington III <william@wkennington.com>
2016-02-16 02:49:32 -0800

    libevent: Cleanup

--- pkgs/development/libraries/libevent/default.nix
+++ pkgs/development/libraries/libevent/default.nix
@@ -1,8 +1,8 @@
 { stdenv, fetchurl, autoreconfHook, openssl, python, findutils }:
 
-let version = "2.0.22"; in
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "libevent-${version}";
+  version = "2.0.22";
 
   src = fetchurl {
     url = "mirror://sourceforge/levent/libevent-${version}-stable.tar.gz";
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
   };
 
   nativeBuildInputs = [ autoreconfHook ];
-  buildInputs = [ openssl python ] ++ stdenv.lib.optional stdenv.isCygwin findutils;
+  buildInputs = [ openssl python ];
 
   patchPhase = ''
     patchShebangs event_rpcgen.py
@@ -18,19 +18,6 @@ stdenv.mkDerivation {
 
   meta = with stdenv.lib; {
     description = "Event notification library";
-
-    longDescription =
-      '' The libevent API provides a mechanism to execute a callback function
-         when a specific event occurs on a file descriptor or after a timeout
-         has been reached.  Furthermore, libevent also support callbacks due
-         to signals or regular timeouts.
-
-         libevent is meant to replace the event loop found in event driven
-         network servers.  An application just needs to call event_dispatch()
-         and then add or remove events dynamically without having to change
-         the event loop.
-      '';
-
     license = licenses.bsd3;
     platforms = platforms.all;
     maintainers = with maintainers; [ wkennington ];

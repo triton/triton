6819f6f698c07f4c375b7e67a74b6e8014ab5ae3
Cody Opel <codyopel@gmail.com>
2016-02-16 16:55:13 -0500

    fix typo

--- pkgs/development/libraries/popt/default.nix
+++ pkgs/development/libraries/popt/default.nix
@@ -6,6 +6,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "http://rpm5.org/files/popt/${name}.tar.gz";
     sha256 = "1j2c61nn2n351nhj4d25mnf3vpiddcykq005w2h6kw79dwlysa77";
+  };
 
   meta = {
     description = "command line option parsing library";

a4177ed1b2329266ba19afcd49c91a227e38c6ed
William A. Kennington III <william@wkennington.com>
2016-02-15 18:31:27 -0800

    help2man: Refactor

--- pkgs/development/tools/misc/help2man/default.nix
+++ pkgs/development/tools/misc/help2man/default.nix
@@ -8,30 +8,18 @@ stdenv.mkDerivation rec {
     sha256 = "0miqq77ssk5rgsc9xlv7k5n2wk2c5wv2m1kh4zhbwrggfmjaycn2";
   };
 
-  buildInputs = [ makeWrapper perl gettext ];
+  nativeBuildInputs = [ makeWrapper perl gettext ];
 
-  doCheck = false;                                # target `check' is missing
-
-  patches = if stdenv.isCygwin then [ ./1.40.4-cygwin-nls.patch ] else null;
-
-  postInstall =
-    '' wrapProgram "$out/bin/help2man" \
-         --prefix PERL5LIB : "$(echo ${perlPackages.LocaleGettext}/lib/perl*/site_perl)"
-    '';
+  postInstall = ''
+    wrapProgram "$out/bin/help2man" \
+      --prefix PERL5LIB : "$(echo ${perlPackages.LocaleGettext}/lib/perl*/site_perl)"
+  '';
 
 
   meta = with stdenv.lib; {
     description = "Generate man pages from `--help' output";
-
-    longDescription =
-      '' help2man produces simple manual pages from the ‘--help’ and
-         ‘--version’ output of other commands.
-      '';
-
     homepage = http://www.gnu.org/software/help2man/;
-
     license = licenses.gpl3Plus;
     platforms = platforms.all;
-    maintainers = with maintainers; [ pSub ];
   };
 }

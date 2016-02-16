73fd7d1a1ed90fe2fb0ab44890cb7cdc4e36c5bf
William A. Kennington III <william@wkennington.com>
2016-02-16 13:09:05 -0800

    libgsf: Cleanup

--- pkgs/development/libraries/libgsf/default.nix
+++ pkgs/development/libraries/libgsf/default.nix
@@ -1,5 +1,5 @@
-{ fetchurl, stdenv, pkgconfig, intltool, gettext, glib, libxml2, zlib, bzip2
-, python, perl, gdk-pixbuf-core, libiconv }:
+{ fetchurl, stdenv, intltool, gettext, glib, libxml2, zlib, bzip2
+, python, perl, gdk-pixbuf-core }:
 
 with { inherit (stdenv.lib) optionals; };
 
@@ -11,12 +11,12 @@ stdenv.mkDerivation rec {
     sha256 = "f0fea447e0374a73df45b498fd1701393f8e6acb39746119f8a292fb4a0cb528";
   };
 
-  nativeBuildInputs = [ pkgconfig intltool ];
+  nativeBuildInputs = [ intltool ];
 
   buildInputs = [ gettext bzip2 zlib python ]
     ++ stdenv.lib.optional doCheck perl;
 
-  propagatedBuildInputs = [ libxml2 glib gdk-pixbuf-core libiconv ];
+  propagatedBuildInputs = [ libxml2 glib gdk-pixbuf-core ];
 
   doCheck = true;
   preCheck = "patchShebangs ./tests/";
@@ -27,10 +27,5 @@ stdenv.mkDerivation rec {
     license     = licenses.lgpl2Plus;
     maintainers = with maintainers; [ lovek323 ];
     platforms   = stdenv.lib.platforms.unix;
-
-    longDescription = ''
-      Libgsf aims to provide an efficient extensible I/O abstraction for
-      dealing with different structured file formats.
-    '';
   };
 }

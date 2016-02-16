c2a29bf789e3c1f2d2e7eaa1cc1f91d7b8ea483e
William A. Kennington III <william@wkennington.com>
2016-02-15 18:31:39 -0800

    flex: Refactor

--- pkgs/development/tools/parsing/flex/default.nix
+++ pkgs/development/tools/parsing/flex/default.nix
@@ -8,23 +8,14 @@ stdenv.mkDerivation rec {
     sha256 = "1sdqx63yadindzafrq1w31ajblf9gl1c301g068s20s7bbpi3ri4";
   };
 
-  buildInputs = [ bison ];
-
-  propagatedNativeBuildInputs = [ m4 ];
-
-  postConfigure = stdenv.lib.optionalString (stdenv.isDarwin || stdenv.isCygwin) ''
-    sed -i Makefile -e 's/-no-undefined//;'
-  '';
-
-  crossAttrs = {
-    preConfigure = ''
-      export ac_cv_func_malloc_0_nonnull=yes
-      export ac_cv_func_realloc_0_nonnull=yes
-    '';
-  };
+  nativeBuildInputs = [ m4 bison ];
 
   meta = {
     homepage = http://flex.sourceforge.net/;
     description = "A fast lexical analyser generator";
+    platforms = [
+      "x86_64-linux"
+      "i686-linux"
+    ];
   };
 }

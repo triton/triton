afb29a045bdf3ef69d0fd65b5bf562003e5ddc31
William A. Kennington III <william@wkennington.com>
2016-02-16 02:42:15 -0800

    libidn: Cleanup

--- pkgs/development/libraries/libidn/default.nix
+++ pkgs/development/libraries/libidn/default.nix
@@ -1,4 +1,4 @@
-{ fetchurl, stdenv, libiconv }:
+{ fetchurl, stdenv }:
 
 stdenv.mkDerivation rec {
   name = "libidn-1.32";
@@ -8,33 +8,12 @@ stdenv.mkDerivation rec {
     sha256 = "1xf4hphhahcjm2xwx147lfpsavjwv9l4c2gf6hx71zxywbz5lpds";
   };
 
-  doCheck = ! stdenv.isDarwin;
-
-  buildInputs = stdenv.lib.optional stdenv.isDarwin libiconv;
+  doCheck = true;
 
   meta = {
     homepage = http://www.gnu.org/software/libidn/;
     description = "Library for internationalized domain names";
-
-    longDescription = ''
-      GNU Libidn is a fully documented implementation of the
-      Stringprep, Punycode and IDNA specifications.  Libidn's purpose
-      is to encode and decode internationalized domain names.  The
-      native C, C\# and Java libraries are available under the GNU
-      Lesser General Public License version 2.1 or later.
-
-      The library contains a generic Stringprep implementation.
-      Profiles for Nameprep, iSCSI, SASL, XMPP and Kerberos V5 are
-      included.  Punycode and ASCII Compatible Encoding (ACE) via IDNA
-      are supported.  A mechanism to define Top-Level Domain (TLD)
-      specific validation tables, and to compare strings against those
-      tables, is included.  Default tables for some TLDs are also
-      included.
-    '';
-
-    repositories.git = git://git.savannah.gnu.org/libidn.git;
     license = stdenv.lib.licenses.lgpl2Plus;
     platforms = stdenv.lib.platforms.all;
-    maintainers = [ ];
   };
 }

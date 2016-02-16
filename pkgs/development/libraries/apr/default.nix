290728c4445d108d426c74eb38631326024670a8
Cody Opel <codyopel@gmail.com>
2016-02-16 17:35:45 -0500

    apr: remove darwin

--- pkgs/development/libraries/apr/default.nix
+++ pkgs/development/libraries/apr/default.nix
@@ -8,15 +8,6 @@ stdenv.mkDerivation rec {
     sha256 = "0ypn51xblix5ys9xy7da3ngdydip0qqh9rdq8nz54w9aq8lys0vx";
   };
 
-  patches = stdenv.lib.optionals stdenv.isDarwin [ ./is-this-a-compiler-bug.patch ];
-
-  configureFlags =
-    # Including the Windows headers breaks unistd.h.
-    # Based on ftp://sourceware.org/pub/cygwin/release/libapr1/libapr1-1.3.8-2-src.tar.bz2
-    stdenv.lib.optional (stdenv.system == "i686-cygwin") "ac_cv_header_windows_h=no";
-
-  enableParallelBuilding = true;
-
   meta = {
     homepage = http://apr.apache.org/;
     description = "The Apache Portable Runtime library";

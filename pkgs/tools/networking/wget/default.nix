79729d438ec419897c3adc2bcec342652dfb76fc
Cody Opel <codyopel@gmail.com>
2016-02-16 16:59:04 -0500

    wget: remove darwin & libiconv

--- pkgs/tools/networking/wget/default.nix
+++ pkgs/tools/networking/wget/default.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchurl, gettext, libidn, pkgconfig
 , perl, perlPackages, LWP, python3
-, libiconv, libpsl ? null, openssl ? null }:
+, libpsl ? null, openssl ? null }:
 
 stdenv.mkDerivation rec {
   name = "wget-1.17.1";
@@ -20,15 +20,12 @@ stdenv.mkDerivation rec {
     do
       sed -i "$i" -e's/localhost/127.0.0.1/g'
     done
-  '' + stdenv.lib.optionalString stdenv.isDarwin ''
-    export LIBS="-liconv -lintl"
   '';
 
   nativeBuildInputs = [ gettext pkgconfig ];
-  buildInputs = [ libidn libiconv libpsl ]
+  buildInputs = [ libidn libpsl ]
     ++ stdenv.lib.optionals doCheck [ perl perlPackages.IOSocketSSL LWP python3 ]
-    ++ stdenv.lib.optional (openssl != null) openssl
-    ++ stdenv.lib.optional stdenv.isDarwin perl;
+    ++ stdenv.lib.optional (openssl != null) openssl;
 
   configureFlags =
     if openssl != null then "--with-ssl=openssl" else "--without-ssl";
@@ -37,14 +34,6 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "Tool for retrieving files using HTTP, HTTPS, and FTP";
-
-    longDescription =
-      '' GNU Wget is a free software package for retrieving files using HTTP,
-         HTTPS and FTP, the most widely-used Internet protocols.  It is a
-         non-interactive commandline tool, so it may easily be called from
-         scripts, cron jobs, terminals without X-Windows support, etc.
-      '';
-
     license = licenses.gpl3Plus;
 
     homepage = http://www.gnu.org/software/wget/;

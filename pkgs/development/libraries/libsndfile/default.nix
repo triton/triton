1e82212a342ced6c5550649ed8e17f7177609f92
Cody Opel <codyopel@gmail.com>
2016-02-15 20:25:59 -0500

    libsndfile: purge darwin

--- pkgs/development/libraries/libsndfile/default.nix
+++ pkgs/development/libraries/libsndfile/default.nix
@@ -1,6 +1,4 @@
-{ stdenv, fetchurl, flac, libogg, libvorbis, pkgconfig
-, Carbon
-}:
+{ stdenv, fetchurl, flac, libogg, libvorbis, pkgconfig }:
 
 stdenv.mkDerivation rec {
   name = "libsndfile-1.0.26";
@@ -10,18 +8,7 @@ stdenv.mkDerivation rec {
     sha256 = "14jhla289cj45946h0hq2an0a9g4wkwb3v4571bla6ixfvn20rfd";
   };
 
-  buildInputs = [ pkgconfig flac libogg libvorbis ]
-    ++ stdenv.lib.optional stdenv.isDarwin Carbon;
-
-  # need headers from the Carbon.framework in /System/Library/Frameworks to
-  # compile this on darwin -- not sure how to handle
-  preConfigure = stdenv.lib.optionalString stdenv.isDarwin
-    ''
-      NIX_CFLAGS_COMPILE+=" -I$SDKROOT/System/Library/Frameworks/Carbon.framework/Versions/A/Headers"
-    '';
-
-  # Needed on Darwin.
-  NIX_CFLAGS_LINK = "-logg -lvorbis";
+  buildInputs = [ pkgconfig flac libogg libvorbis ] ;
 
   meta = with stdenv.lib; {
     description = "A C library for reading and writing files containing sampled sound";
@@ -29,25 +16,5 @@ stdenv.mkDerivation rec {
     license     = licenses.lgpl2Plus;
     maintainers = with maintainers; [ lovek323 ];
     platforms   = platforms.unix;
-
-    longDescription = ''
-      Libsndfile is a C library for reading and writing files containing
-      sampled sound (such as MS Windows WAV and the Apple/SGI AIFF format)
-      through one standard library interface.  It is released in source
-      code format under the GNU Lesser General Public License.
-
-      The library was written to compile and run on a Linux system but
-      should compile and run on just about any Unix (including MacOS X).
-      There are also pre-compiled binaries available for 32 and 64 bit
-      windows.
-
-      It was designed to handle both little-endian (such as WAV) and
-      big-endian (such as AIFF) data, and to compile and run correctly on
-      little-endian (such as Intel and DEC/Compaq Alpha) processor systems
-      as well as big-endian processor systems such as Motorola 68k, Power
-      PC, MIPS and SPARC.  Hopefully the design of the library will also
-      make it easy to extend for reading and writing new sound file
-      formats.
-    '';
   };
 }

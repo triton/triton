9ab23f17192f84004fc596068f12e7621646e4f0
William A. Kennington III <william@wkennington.com>
2016-02-16 02:52:52 -0800

    lz4: Cleanup

--- pkgs/tools/compression/lz4/default.nix
+++ pkgs/tools/compression/lz4/default.nix
@@ -13,24 +13,13 @@ stdenv.mkDerivation rec {
 
   buildInputs = stdenv.lib.optional doCheck valgrind;
 
-  enableParallelBuilding = true;
-
   makeFlags = [ "PREFIX=$(out)" ];
 
   doCheck = false; # tests take a very long time
   checkTarget = "test";
 
-  patches = [ ./install-on-freebsd.patch ] ;
-
   meta = with stdenv.lib; {
     description = "Extremely fast compression algorithm";
-    longDescription = ''
-      Very fast lossless compression algorithm, providing compression speed
-      at 400 MB/s per core, with near-linear scalability for multi-threaded
-      applications. It also features an extremely fast decoder, with speed in
-      multiple GB/s per core, typically reaching RAM speed limits on
-      multi-core systems.
-    '';
     homepage = https://code.google.com/p/lz4/;
     license = with licenses; [ bsd2 gpl2Plus ];
     platforms = platforms.unix;

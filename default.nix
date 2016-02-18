2f2c97d7476f680855b8b1c8c4978ba7200eb116
William A. Kennington III <william@wkennington.com>
2016-02-18 15:17:22 -0800

    default.nix: Make argument passing more obvious

--- default.nix
+++ default.nix
@@ -1,9 +1,8 @@
-let requiredVersion = import ./lib/minver.nix; in
-
-if ! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.nixVersion == 1 then
-
-  abort "This version of Triton requires Nix >= ${requiredVersion}, please upgrade! See https://nixos.org/wiki/How_to_update_when_Nix_is_too_old_to_evaluate_Nixpkgs"
-
-else
-
-  import ./pkgs/top-level/all-packages.nix
+{ ... } @ args:
+let
+  requiredVersion = import ./lib/minver.nix;
+in
+  if ! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.nixVersion == 1 then
+    abort "This version of Triton requires Nix >= ${requiredVersion}, please upgrade! See https://nixos.org/wiki/How_to_update_when_Nix_is_too_old_to_evaluate_Nixpkgs"
+  else
+    import ./pkgs/top-level/all-packages.nix args

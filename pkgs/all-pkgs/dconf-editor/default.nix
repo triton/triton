d9308574021804609992c9f133b64aa835bd66dc
Cody Opel <codyopel@gmail.com>
2016-02-19 07:11:21 -0500

    dconf-editor: fix gio module wrapper

--- pkgs/all-pkgs/dconf-editor/default.nix
+++ pkgs/all-pkgs/dconf-editor/default.nix
@@ -47,7 +47,7 @@ stdenv.mkDerivation rec {
     wrapProgram $out/bin/dconf-editor \
       --set 'GDK_PIXBUF_MODULE_FILE' "$GDK_PIXBUF_MODULE_FILE" \
       --set 'GSETTINGS_BACKEND' 'dconf' \
-      --prefix 'GIO_EXTRA_MODULES' : "${dconf}/lib/gio/modules" \
+      --prefix 'GIO_EXTRA_MODULES' : "$GIO_EXTRA_MODULES" \
       --prefix 'XDG_DATA_DIRS' : "$GSETTINGS_SCHEMAS_PATH" \
       --prefix 'XDG_DATA_DIRS' : "$out/share" \
       --prefix 'XDG_DATA_DIRS' : "$XDG_ICON_DIRS"
@@ -61,7 +61,6 @@ stdenv.mkDerivation rec {
       codyopel
     ];
     platforms = with platforms;
-      i686-linux
-      ++ x86_64-linux;
+      x86_64-linux;
   };
 }

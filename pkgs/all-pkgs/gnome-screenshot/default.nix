4544852537121eb7e3fcd262cbdb1b1675d8a176
Cody Opel <codyopel@gmail.com>
2016-02-19 09:43:44 -0500

    gnome-screenshot: fix gio modules wrapper

--- pkgs/all-pkgs/gnome-screenshot/default.nix
+++ pkgs/all-pkgs/gnome-screenshot/default.nix
@@ -34,6 +34,7 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     adwaita-icon-theme
+    dconf
     gdk-pixbuf
     glib
     gsettings-desktop-schemas
@@ -52,7 +53,7 @@ stdenv.mkDerivation rec {
     wrapProgram $out/bin/gnome-screenshot \
       --set 'GDK_PIXBUF_MODULE_FILE' "$GDK_PIXBUF_MODULE_FILE" \
       --set 'GSETTINGS_BACKEND' 'dconf' \
-      --prefix 'GIO_EXTRA_MODULES' : "${dconf}/lib/gio/modules" \
+      --prefix 'GIO_EXTRA_MODULES' : "$GIO_EXTRA_MODULES" \
       --prefix 'XDG_DATA_DIRS' : "$GSETTINGS_SCHEMAS_PATH" \
       --prefix 'XDG_DATA_DIRS' : "$out/share" \
       --prefix 'XDG_DATA_DIRS' : "$XDG_ICON_DIRS"
@@ -68,7 +69,6 @@ stdenv.mkDerivation rec {
       codyopel
     ];
     platforms = with platforms;
-      i686-linux
-      ++ x86_64-linux;
+      x86_64-linux;
   };
 }

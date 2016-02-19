25b8c11d209354880e429970a55740bf2117b5a7
Cody Opel <codyopel@gmail.com>
2016-02-19 00:20:45 -0500

    pth: remove arm

--- pkgs/development/libraries/pth/default.nix
+++ pkgs/development/libraries/pth/default.nix
@@ -2,15 +2,15 @@
 
 stdenv.mkDerivation rec {
   name = "pth-2.0.7";
-  
+
   src = fetchurl {
     url = "mirror://gnu/pth/${name}.tar.gz";
     sha256 = "0ckjqw5kz5m30srqi87idj7xhpw6bpki43mj07bazjm2qmh3cdbj";
   };
 
-  preConfigure = stdenv.lib.optionalString stdenv.isArm ''
-    configureFlagsArray=("CFLAGS=-DJB_SP=8 -DJB_PC=9")
-  '';
+  #preConfigure = stdenv.lib.optionalString stdenv.isArm ''
+  #  configureFlagsArray=("CFLAGS=-DJB_SP=8 -DJB_PC=9")
+  #'';
 
   # Fails with -> pth_uctx.c:31:19: fatal error: pth_p.h: No such file or directory
   parallelBuild = false;

b32d2bd5b78d05484aada8118996df752a0c4489
Cody Opel <codyopel@gmail.com>
2016-02-19 02:15:52 -0500

    libshout: 2.3.1 -> 2.4.1

--- pkgs/development/libraries/libshout/default.nix
+++ pkgs/development/libraries/libshout/default.nix
@@ -1,31 +1,30 @@
-{ stdenv, fetchurl, pkgconfig
-, libvorbis, libtheora, speex }:
+{ stdenv
+, fetchurl
 
-# need pkgconfig so that libshout installs ${out}/lib/pkgconfig/shout.pc
+, libvorbis
+, libtheora
+, speex
+}:
 
 stdenv.mkDerivation rec {
-  name = "libshout-2.3.1";
+  name = "libshout-2.4.1";
 
   src = fetchurl {
     url = "http://downloads.xiph.org/releases/libshout/${name}.tar.gz";
-    sha256 = "cf3c5f6b4a5e3fcfbe09fb7024aa88ad4099a9945f7cb037ec06bcee7a23926e";
+    sha256 = "0kgjpf8jkgyclw11nilxi8vyjk4s8878x23qyxnvybbgqbgbib7k";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
-  propagatedBuildInputs = [ libvorbis libtheora speex ];
+  buildInputs = [
+    libvorbis
+    libtheora
+    speex
+  ];
 
   meta = {
     description = "icecast 'c' language bindings";
-
-    longDescription = ''
-      Libshout is a library for communicating with and sending data to an icecast
-      server.  It handles the socket connection, the timing of the data, and prevents
-      bad data from getting to the icecast server.
-    '';
-
     homepage = http://www.icecast.org;
     license = stdenv.lib.licenses.gpl2;
-    maintainers = with stdenv.lib.maintainers; [ jcumming ];
- 
+    maintainers = with stdenv.lib.maintainers; [ ];
+
   };
 }

04123602f2a1c0620179f211cd8d26f7aebb87a3
Cody Opel <codyopel@gmail.com>
2016-02-20 03:57:23 -0500

    ed: remove darwin

--- pkgs/applications/editors/ed/default.nix
+++ pkgs/applications/editors/ed/default.nix
@@ -22,7 +22,7 @@ stdenv.mkDerivation rec {
        make: *** [check] Error 127
 
     */
-  doCheck = !stdenv.isDarwin;
+  doCheck = true;
 
   crossAttrs = {
     compileFlags = [ "CC=${stdenv.cross.config}-gcc" ];
@@ -30,22 +30,8 @@ stdenv.mkDerivation rec {
 
   meta = {
     description = "An implementation of the standard Unix editor";
-
-    longDescription = ''
-      GNU ed is a line-oriented text editor.  It is used to create,
-      display, modify and otherwise manipulate text files, both
-      interactively and via shell scripts.  A restricted version of ed,
-      red, can only edit files in the current directory and cannot
-      execute shell commands.  Ed is the "standard" text editor in the
-      sense that it is the original editor for Unix, and thus widely
-      available.  For most purposes, however, it is superseded by
-      full-screen editors such as GNU Emacs or GNU Moe.
-    '';
-
     license = stdenv.lib.licenses.gpl3Plus;
-
     homepage = http://www.gnu.org/software/ed/;
-
     maintainers = [ ];
   };
 }

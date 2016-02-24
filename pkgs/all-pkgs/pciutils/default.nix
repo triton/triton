4128844d0247edfae93b939e9804f60129ab3555
William A. Kennington III <william@wkennington.com>
2016-02-24 15:50:56 -0800

    pciutils: Move to all-pkgs

--- pkgs/all-pkgs/pciutils/default.nix
+++ pkgs/all-pkgs/pciutils/default.nix
@@ -1,4 +1,10 @@
-{ stdenv, fetchurl, pkgconfig, zlib, kmod, which }:
+{ stdenv
+, fetchurl
+, which
+
+, kmod
+, zlib
+}:
 
 stdenv.mkDerivation rec {
   name = "pciutils-3.4.1"; # with database from 2016-01
@@ -8,21 +14,42 @@ stdenv.mkDerivation rec {
     sha256 = "0am8hiv435h2dayclnkdk8qjlpj08m4djf6sv15n9l84av658mc6";
   };
 
-  buildInputs = [ pkgconfig zlib kmod which ];
+  nativeBuildInputs = [
+    which
+  ];
 
-  makeFlags = "SHARED=yes PREFIX=\${out}";
+  buildInputs = [
+    kmod
+    zlib
+  ];
 
-  installTargets = "install install-lib";
+  preBuild = ''
+    makeFlagsArray+=(
+      "SHARED=yes"
+      "PREFIX=$out"
+    )
+  '';
+
+  installTargets = [
+    "install"
+    "install-lib"
+  ];
 
   # Get rid of update-pciids as it won't work.
-  postInstall = "rm $out/sbin/update-pciids $out/man/man8/update-pciids.8";
+  postInstall = ''
+    rm $out/sbin/update-pciids $out/man/man8/update-pciids.8
+  '';
 
   meta = with stdenv.lib; {
     homepage = http://mj.ucw.cz/pciutils.html;
     description = "A collection of programs for inspecting and manipulating configuration of PCI devices";
     license = licenses.gpl2Plus;
-    platforms = platforms.unix;
-    maintainers = [ maintainers.vcunat ]; # not really, but someone should watch it
+    maintainers = with maintainers; [
+      wkennington
+    ];
+    platforms = with platforms;
+      i686-linux
+      ++ x86_64-linux;
   };
 }
 

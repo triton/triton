0cfd8de35cb085d77990ea63d17092b157b5d2d3
William A. Kennington III <william@wkennington.com>
2016-02-26 14:54:14 -0800

    subversion: Refactor

--- pkgs/applications/version-management/subversion/default.nix
+++ pkgs/applications/version-management/subversion/default.nix
@@ -1,19 +1,8 @@
-{ bdbSupport ? false # build support for Berkeley DB repositories
-, httpServer ? false # build Apache DAV module
-, httpSupport ? false # client must support http
-, pythonBindings ? false
-, perlBindings ? false
-, javahlBindings ? false
-, saslSupport ? false
-, stdenv, fetchurl, apr, aprutil, zlib, sqlite
-, apacheHttpd ? null, expat, swig ? null, jdk ? null, python ? null, perl ? null
-, sasl ? null, serf ? null
+{ stdenv
+, fetchurl, apr, aprutil, zlib, sqlite
+, expat, swig, python, perl
+, cyrus-sasl, serf
 }:
-
-assert bdbSupport -> aprutil.bdbSupport;
-assert httpServer -> apacheHttpd != null;
-assert pythonBindings -> swig != null && python != null;
-assert javahlBindings -> jdk != null && perl != null;
 
 let
   common = { version, sha1 }: stdenv.mkDerivation rec {
@@ -25,48 +14,36 @@ let
       inherit sha1;
     };
 
-    buildInputs = [ zlib apr aprutil sqlite ]
-      ++ stdenv.lib.optional httpSupport serf
-      ++ stdenv.lib.optional pythonBindings python
-      ++ stdenv.lib.optional perlBindings perl
-      ++ stdenv.lib.optional saslSupport sasl;
+    buildInputs = [ zlib apr aprutil sqlite serf python perl cyrus-sasl ];
 
-    configureFlags = ''
-      ${if bdbSupport then "--with-berkeley-db" else "--without-berkeley-db"}
-      ${if httpServer then "--with-apxs=${apacheHttpd}/bin/apxs" else "--without-apxs"}
-      ${if pythonBindings || perlBindings then "--with-swig=${swig}" else "--without-swig"}
-      ${if javahlBindings then "--enable-javahl --with-jdk=${jdk}" else ""}
-      --disable-keychain
-      ${if saslSupport then "--with-sasl=${sasl}" else "--without-sasl"}
-      ${if httpSupport then "--with-serf=${serf}" else "--without-serf"}
-      --with-zlib=${zlib}
-      --with-sqlite=${sqlite}
-    '';
+    configureFlags = [
+      "--with-berkeley-db"
+      "--with-swig=${swig}"
+      "--disable-keychain"
+      "--with-sasl=${cyrus-sasl}"
+      "--with-serf=${serf}"
+      "--with-zlib=${zlib}"
+      "--with-sqlite=${sqlite}"
+    ];
 
     preBuild = ''
       makeFlagsArray=(APACHE_LIBEXECDIR=$out/modules)
     '';
 
     postInstall = ''
-      if test -n "$pythonBindings"; then
-          make swig-py swig_pydir=$(toPythonPath $out)/libsvn swig_pydir_extra=$(toPythonPath $out)/svn
-          make install-swig-py swig_pydir=$(toPythonPath $out)/libsvn swig_pydir_extra=$(toPythonPath $out)/svn
-      fi
+      make swig-py swig_pydir=$(toPythonPath $out)/libsvn swig_pydir_extra=$(toPythonPath $out)/svn
+      make install-swig-py swig_pydir=$(toPythonPath $out)/libsvn swig_pydir_extra=$(toPythonPath $out)/svn
 
-      if test -n "$perlBindings"; then
-          make swig-pl-lib
-          make install-swig-pl-lib
-          cd subversion/bindings/swig/perl/native
-          perl Makefile.PL PREFIX=$out
-          make install
-          cd -
-      fi
+      make swig-pl-lib
+      make install-swig-pl-lib
+      pushd subversion/bindings/swig/perl/native
+      perl Makefile.PL PREFIX=$out
+      make install
+      popd
 
       mkdir -p $out/share/bash-completion/completions
       cp tools/client-side/bash_completion $out/share/bash-completion/completions/subversion
     '';
-
-    inherit perlBindings pythonBindings;
 
     # Parallel Building works fine but Parallel Install fails
     parallelInstall = false;

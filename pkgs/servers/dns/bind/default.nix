51ceb00ce5a8c4f414842ad9604ce47f2622e576
William A. Kennington III <william@wkennington.com>
2016-02-26 10:49:10 -0800

    dnsutils: Refactor

--- pkgs/servers/dns/bind/default.nix
+++ pkgs/servers/dns/bind/default.nix
@@ -1,113 +1,119 @@
-{ stdenv, fetchurl, libtool, docbook5_xsl
+{ stdenv
+, fetchurl
+, libtool
+, docbook5_xsl
 
-# Optional Dependencies
-, libseccomp ? null, python ? null, kerberos ? null, openssl ? null
-, libxml2 ? null, json_c ? null, readline ? null, libcap ? null, idnkit ? null
-, libiconv ? null
+, db
+, idnkit
+, json-c
+, kerberos
+, libcap
+, libseccomp
+, libxml2
+, mysql_lib
+, ncurses
+, openldap
+, openssl
+, postgresql_lib
+, python
+, readline
 
-# Optional DLZ Modules
-, postgresql ? null, libmysql ? null, db ? null, openldap ? null
-
-# Extra arguments
 , suffix ? ""
 }:
 
-with stdenv;
 let
-  version = "9.10.3-P3";
-
   toolsOnly = suffix == "tools";
 
-  optLibseccomp = shouldUsePkg libseccomp;
-  optPython = if toolsOnly then null else shouldUsePkg python;
-  optKerberos = shouldUsePkg kerberos;
-  optOpenssl = shouldUsePkg openssl;
-  optLibxml2 = shouldUsePkg libxml2;
-  optJson_c = shouldUsePkg json_c;
-  optReadline = shouldUsePkg readline;
-  optLibcap = if !stdenv.isLinux then null else shouldUsePkg libcap;
-  optIdnkit = shouldUsePkg idnkit;
-  optLibiconv = shouldUsePkg libiconv;
-
-  optPostgresql = if toolsOnly then null else shouldUsePkg postgresql;
-  optLibmysql = if toolsOnly then null else shouldUsePkg libmysql;
-  optDb = if toolsOnly then null else shouldUsePkg db;
-  optOpenldap = if toolsOnly then null else shouldUsePkg openldap;
-
-  pythonBin = if optPython == null then null else "${optPython}/bin/python";
+  inherit (stdenv.lib)
+    optionals
+    optionalString;
 in
-with stdenv.lib;
 stdenv.mkDerivation rec {
   name = "bind${optionalString (suffix != "") "-${suffix}"}-${version}";
+  version = "9.10.3-P3";
 
   src = fetchurl {
     url = "http://ftp.isc.org/isc/bind9/${version}/bind-${version}.tar.gz";
     sha256 = "10yblk8qbb85qxakzdjy5qmqvqj4rlcqsqvlkriglampzg8i0239";
   };
 
-  patches = [ ./libressl.patch ./remove-mkdir-var.patch ];
+  nativeBuildInputs = [
+    docbook5_xsl
+    libtool
+  ];
 
-  nativeBuildInputs = [ optPython libtool docbook5_xsl ];
   buildInputs = [
-    optLibseccomp optPython optKerberos optOpenssl optLibxml2 optJson_c
-    optReadline optLibcap optIdnkit optLibiconv optPostgresql optLibmysql
-    optDb optOpenldap
+    idnkit
+    json-c
+    kerberos
+    libcap
+    libseccomp
+    libxml2
+    ncurses
+    openssl
+    python
+    readline
+  ] ++ optionals (!toolsOnly) [
+    db
+    openldap
+    mysql_lib
+    postgresql_lib
   ];
 
   configureFlags = [
-    (mkOther                          "localstatedir"       "/var")
-    (mkOther                          "sysconfdir"          "/etc")
-    (mkEnable (optLibseccomp != null) "seccomp"             null)
-    (mkWith   (optPython != null)     "python"              pythonBin)
-    (mkEnable true                    "kqueue"              null)
-    (mkEnable true                    "epoll"               null)
-    (mkEnable true                    "devpoll"             null)
-    (mkWith   false                   "geoip"               null)  # TODO(wkennington): GeoDNS support
-    (mkWith   (optKerberos != null)   "gssapi"              optKerberos)
-    (mkWith   true                    "libtool"             null)
-    (mkEnable (optOpenssl == null)    "native-pkcs11"       null)
-    (mkWith   (optOpenssl != null)    "openssl"             optOpenssl)
-    (mkWith   true                    "pkcs11"              null)
-    (mkWith   true                    "ecdsa"               null)
-    (mkWith   false                   "gost"                null)  # Insecure cipher
-    (mkWith   true                    "aes"                 null)
-    (mkEnable (optOpenssl != null)    "openssl-hash"        null)
-    (mkEnable true                    "sit"                 null)
-    (mkWith   true                    "sit-alg"             "aes")
-    (mkWith   (optLibxml2 != null)    "libxml2"             optLibxml2)
-    (mkWith   (optJson_c != null)     "libjson"             optJson_c)
-    (mkEnable true                    "largefile"           null)
-    (mkWith   false                   "purify"              null)
-    (mkWith   false                   "gperftools-profiler" null)
-    (mkEnable false                   "backtrace"           null)
-    (mkEnable false                   "symtable"            null)
-    (mkEnable true                    "ipv6"                null)
-    (mkWith   false                   "kame"                null)
-    (mkWith   (optReadline != null)   "readline"            null)
-    (mkEnable (optKerberos == null)   "isc-spnego"          null)
-    (mkEnable true                    "chroot"              null)
-    (mkEnable (optLibcap != null)     "linux-caps"          null)
-    (mkEnable true                    "atomic"              null)
-    (mkEnable false                   "fixed-rrset"         null)
-    (mkEnable true                    "rpz-nsip"            null)
-    (mkEnable true                    "rpz-nsdname"         null)
-    (mkEnable true                    "filter-aaaa"         null)
-    (mkWith   true                    "docbook-xsl"         "${docbook5_xsl}/share/xsl/docbook")
-    (mkWith   (optIdnkit != null)     "idn"                 optIdnkit)
-    (mkWith   (optLibiconv != null)   "libiconv"            optLibiconv)
-    (mkWith   false                   "atf"                 null)
-    (mkWith   true                    "tuning"              "large")
-    (mkEnable true                    "querytrace"          null)
-    (mkWith   true                    "dlopen"              null)
-    (mkWith   false                   "make-clean"          null)
-    (mkEnable true                    "full-report"         null)
-    (mkWith   (optPostgresql != null) "dlz-postgres"        optPostgresql)
-    (mkWith   (optLibmysql != null)   "dlz-mysql"           optLibmysql)
-    (mkWith   (optDb != null)         "dlz-bdb"             optDb)
-    (mkWith   true                    "dlz-filesystem"      null)
-    (mkWith   (optOpenldap != null)   "dlz-ldap"            optOpenldap)
-    (mkWith   false                   "dlz-odbc"            null)
-    (mkWith   true                    "dlz-stub"            null)
+    "--localstatedir=/var"
+    "--sysconfdir=/etc"
+    "--enable-seccomp"
+    "--with-python=${python.interpreter}"
+    "--enable-kqueue"
+    "--enable-epoll"
+    "--enable-devpoll"
+    "--without-geoip"  # TODO(wkennington): GeoDNS support
+    "--with-gssapi=${kerberos}"
+    "--with-libtool"
+    "--disable-native-pkcs11"
+    "--with-openssl=${openssl}"
+    "--with-pkcs11"
+    "--with-ecdsa"
+    "--without-gost"  # Insecure cipher
+    "--with-aes"
+    "--enable-openssl-hash"
+    "--enable-sit"
+    "--with-sit-alg=aes"
+    "--with-libxml2=${libxml2}"
+    "--with-libjson=${json-c}"
+    "--enable-largefile"
+    "--without-purify"
+    "--without-gperftools-profiler"
+    "--disable-backtrace"
+    "--disable-symtable"
+    "--enable-ipv6"
+    "--without-kame"
+    "--with-readline"
+    "--disable-isc-spnego"
+    "--enable-chroot"
+    "--enable-linux-caps"
+    "--enable-atomic"
+    "--disable-fixed-rrset"
+    "--enable-rpz-nsip"
+    "--enable-rpz-nsdname"
+    "--enable-filter-aaaa"
+    "--with-docbook-xsl=${docbook5_xsl}/share/xsl/docbook"
+    "--with-idn=${idnkit}"
+    "--without-atf"
+    "--with-tuning=large"
+    "--enable-querytrace"
+    "--with-dlopen"
+    "--without-make-clean"
+    "--enable-full-report"
+  ] ++ optionals (!toolsOnly) [
+    "--with-dlz-postgres=${postgresql_lib}"
+    "--with-dlz-mysql=${mysql_lib}"
+    "--with-dlz-bdb=${db}"
+    "--with-dlz-filesystem"
+    "--with-dlz-ldap=${openldap}"
+    "--without-dlz-odbc"
+    "--with-dlz-stub"
   ];
 
   installFlags = [
@@ -125,13 +131,15 @@ stdenv.mkDerivation rec {
     install -m 0644 $TMPDIR/$out/share/man/man1/{dig,host,nslookup,nsupdate}.1 $out/share/man/man1
   '';
 
-  enableParallelBuilding = true;
-
-  meta = {
+  meta = with stdenv.lib; {
     homepage = "http://www.isc.org/software/bind";
     description = "Domain name server";
     license = licenses.isc;
-    maintainers = with maintainers; [ viric simons wkennington ];
-    platforms = platforms.unix;
+    maintainers = with maintainers; [
+      wkennington
+    ];
+    platforms = with platforms;
+      i686-linux
+      ++ x86_64-linux;
   };
 }

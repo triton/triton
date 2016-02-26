4f7375d9b8907584c2c07361e6b201dfd992f3cf
William A. Kennington III <william@wkennington.com>
2016-02-26 14:55:02 -0800

    libopus: Refactor

--- pkgs/development/libraries/libopus/default.nix
+++ pkgs/development/libraries/libopus/default.nix
@@ -1,20 +1,17 @@
-{ stdenv, fetchurl, fixedPoint ? false, withCustomModes ? true }:
+{ stdenv, fetchurl }:
 
-let
-  version = "1.1.2";
-in
 stdenv.mkDerivation rec {
   name = "libopus-${version}";
+  version = "1.1.2";
 
   src = fetchurl {
     url = "http://downloads.xiph.org/releases/opus/opus-${version}.tar.gz";
     sha256 = "1z87x5c5x951lhnm70iqr2gqn15wns5cqsw8nnkvl48jwdw00a8f";
   };
 
-  configureFlags = stdenv.lib.optional fixedPoint "--enable-fixed-point"
-                ++ stdenv.lib.optional withCustomModes "--enable-custom-modes";
-
-  doCheck = true;
+  configureFlags = [
+    "--enable-custom-modes"
+  ];
 
   meta = with stdenv.lib; {
     description = "Open, royalty-free, highly versatile audio codec";

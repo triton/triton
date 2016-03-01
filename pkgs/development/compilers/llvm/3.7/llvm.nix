3768cbcb21a7770cc9ad3ca4b56351bcb2eadff6
William A. Kennington III <william@wkennington.com>
2016-02-29 20:09:37 -0800

    llvm: Go back to building statically

--- pkgs/development/compilers/llvm/3.7/llvm.nix
+++ pkgs/development/compilers/llvm/3.7/llvm.nix
@@ -36,6 +36,13 @@ in stdenv.mkDerivation rec {
       file = "llvm/fix-llvm-config.patch";
       sha256 = "059655c0e6ea5dd248785ffc1b2e6402eeb66544ffe36ff15d76543dd7abb413";
     })
+
+    # Part of the shared library TODO
+    #(fetchTritonPatch {
+    #  rev = "b8eb01f3c7b7ad707c8877e9e577b04a68e15707";
+    #  file = "llvm/llvm-config-shared-link.patch";
+    #  sha256 = "95c1f0d83f5195260f6acca60acc2605bb2ec1a5b106c138239e36a84e15557e";
+    #})
   ];
 
   nativeBuildInputs = [ perl groff cmake ninja python ];
@@ -49,12 +56,16 @@ in stdenv.mkDerivation rec {
 
   cmakeFlags = with stdenv; [
     "-DCMAKE_BUILD_TYPE=Release"
-    "-DLLVM_INSTALL_UTILS=ON"  # Needed by rustc
+    "-DLLVM_INCLUDE_EXAMPLES=OFF"
     "-DLLVM_BUILD_TESTS=ON"
-    "-DLLVM_ENABLE_FFI=ON"
     "-DLLVM_ENABLE_RTTI=ON"
-    "-DBUILD_SHARED_LIBS=ON"
-    "-DLLVM_BINUTILS_INCDIR=${binutils}/include"
+    "-DLLVM_INSTALL_UTILS=ON"  # Needed by rustc
+    "-DLLVM_ENABLE_FFI=ON"
+    "-DLLVM_ENABLE_LTO=Full"
+
+    # TODO: Figure out how to make the single shared library work
+    #"-DLLVM_BUILD_LLVM_DYLIB=ON"
+    #"-DLLVM_LINK_LLVM_DYLIB=ON"
   ];
 
   doCheck = true;

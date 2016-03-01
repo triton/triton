07d9e35bebd6d12da08d04f6b80d14dace9c1e7e
Cody Opel <codyopel@gmail.com>
2016-03-01 10:49:14 -0500

    libmusicbrainz: fix cmake hook

--- pkgs/development/libraries/libmusicbrainz/5.x.nix
+++ pkgs/development/libraries/libmusicbrainz/5.x.nix
@@ -1,16 +1,16 @@
-{ stdenv, fetchurl, cmake, neon, libdiscid }:
+{ stdenv, fetchurl, cmake, perl, neon, libdiscid, libxml2 }:
 
 stdenv.mkDerivation rec {
   name = "libmusicbrainz-5.0.1";
 
-  buildInputs = [ cmake neon libdiscid ];
+  buildInputs = [ cmake perl neon libdiscid libxml2 ];
 
   src = fetchurl {
     url = "https://github.com/downloads/metabrainz/libmusicbrainz/${name}.tar.gz";
     md5 = "a0406b94c341c2b52ec0fe98f57cadf3";
   };
 
-  dontUseCmakeBuildDir=true;
+  createCmakeBuildDir = false;
 
   meta = {
     homepage = http://musicbrainz.org/doc/libmusicbrainz;

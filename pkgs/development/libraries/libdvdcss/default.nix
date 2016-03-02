a674761f3b5fccd85e22faf4d11ddaa91eafafa2
William A. Kennington III <william@wkennington.com>
2016-03-01 20:39:01 -0800

    libdvdcss: 1.3.0 -> 1.4.0

--- pkgs/development/libraries/libdvdcss/default.nix
+++ pkgs/development/libraries/libdvdcss/default.nix
@@ -1,16 +1,24 @@
-{stdenv, fetchurl}:
+{ stdenv
+, fetchurl
+}:
 
 stdenv.mkDerivation rec {
   name = "libdvdcss-${version}";
-  version = "1.3.0";
+  version = "1.4.0";
 
   src = fetchurl {
     url = "http://get.videolan.org/libdvdcss/${version}/${name}.tar.bz2";
-    sha256 = "158k9zagmbk5bkbz96l6lwhh7xcgfcnzflkr4vblskhcab6llhbw";
+    sha256 = "0nl45ifc4xcb196snv9d6hinfw614cqpzcqp92dg43c0hickg290";
   };
 
-  meta = {
+  meta = with stdenv.lib; {
     homepage = http://www.videolan.org/developers/libdvdcss.html;
     description = "A library for decrypting DVDs";
+    maintainers = with maintainers; [
+      wkennington
+    ];
+    platforms = with platforms;
+      i686-linux
+      ++ x86_64-linux;
   };
 }

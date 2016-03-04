9cd4fe7978339a428a3cbb886c1f12a83d68bf9a
William A. Kennington III <william@wkennington.com>
2016-03-03 22:19:12 -0800

    filezilla: Refactor

--- pkgs/all-pkgs/filezilla/default.nix
+++ pkgs/all-pkgs/filezilla/default.nix
@@ -1,34 +1,51 @@
-{ stdenv, fetchurl, dbus, gnutls, wxGTK30, libidn, tinyxml2, gettext
-, pkgconfig, xdg-utils, gtk2, sqlite, pugixml }:
+{ stdenv
+, fetchurl
+, gettext
 
-let version = "3.14.1"; in
-stdenv.mkDerivation {
+, dbus
+, gnutls
+, libfilezilla
+, pugixml
+, sqlite
+, wxGTK
+, xdg-utils
+}:
+
+stdenv.mkDerivation rec {
   name = "filezilla-${version}";
+  version = "3.16.0";
 
   src = fetchurl {
     url = "mirror://sourceforge/project/filezilla/FileZilla_Client/${version}/FileZilla_${version}_src.tar.bz2";
-    sha256 = "0v6lb7miy6jbnswii816na8818xqxlvs1vadnii21xfmrsv7225i";
+    sha256 = "0ilv4xhgav4srx6iqn0v0kv8rifgkysyx1hb9bnm45dc0skmbgbx";
   };
+
+  nativeBuildInputs = [
+    gettext
+  ];
+
+  buildInputs = [
+    dbus
+    gnutls
+    libfilezilla
+    pugixml
+    sqlite
+    wxGTK
+    xdg-utils
+  ];
 
   configureFlags = [
     "--disable-manualupdatecheck"
   ];
 
-  buildInputs = [
-    dbus gnutls wxGTK30 libidn tinyxml2 gettext pkgconfig xdg-utils gtk2 sqlite
-    pugixml ];
-
   meta = with stdenv.lib; {
     homepage = http://filezilla-project.org/;
     description = "Graphical FTP, FTPS and SFTP client";
     license = licenses.gpl2;
-    longDescription = ''
-      FileZilla Client is a free, open source FTP client. It supports
-      FTP, SFTP, and FTPS (FTP over SSL/TLS). The client is available
-      under many platforms, binaries for Windows, Linux and Mac OS X are
-      provided.
-    '';
-    platforms = platforms.linux;
-    maintainers = with maintainers; [ pSub ];
+    maintainers = with maintainers; [
+      wkennington
+    ];
+    platforms = with platforms;
+      x86_64-linux;
   };
 }

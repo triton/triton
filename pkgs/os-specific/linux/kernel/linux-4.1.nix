5aa2f7c870316c8f8a31eaf78faa4e8917c5f608
William A. Kennington III <william@wkennington.com>
2016-03-05 14:59:13 -0800

    kernel: 4.1.18 -> 4.1.19

--- pkgs/os-specific/linux/kernel/linux-4.1.nix
+++ pkgs/os-specific/linux/kernel/linux-4.1.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.1.18";
+  version = "4.1.19";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1n838dg058knmx4n5mfqxh2ai9d3x6w9zs1apkwzm89rpisc1ijb";
+    sha256 = "07i16hw18jhchcqr4qdsilkmr03iklnjp0g4pv9jz73gphf091p8";
   };
 
   features.iwlwifi = true;

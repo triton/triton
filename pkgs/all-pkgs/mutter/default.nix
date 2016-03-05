bd4af9faf73d806d905a0040bed6f1388e648628
Cody Opel <codyopel@gmail.com>
2016-03-05 03:22:25 -0500

    mutter: remove backports for 3.18.2

--- pkgs/all-pkgs/mutter/default.nix
+++ pkgs/all-pkgs/mutter/default.nix
@@ -111,31 +111,6 @@ stdenv.mkDerivation rec {
       file = "mutter/x86.patch";
       sha256 = "0f7438b60b8c32b9f788245273081c4181eb529610ca804c5ba46d12338b1475";
     })
-    (fetchTritonPatch {
-      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
-      file = "mutter/mutter-3.18.2-bypass-hint.patch";
-      sha256 = "5743a64e088d61706f9ab70ae8bc0b97bd6633825bfda7bc68fc6c593bd43353";
-    })
-    (fetchTritonPatch {
-      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
-      file = "mutter/mutter-3.18.2-cursor-renderer.patch";
-      sha256 = "0264b85181a489615984d5b3098b5b1747214c0d05fb9d725b8b2b85819a0c06";
-    })
-    (fetchTritonPatch {
-      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
-      file = "mutter/mutter-3.18.2-logical-monitors.patch";
-      sha256 = "8446922fe20b8c35cfdd891e393d936316530d02ac9b00e61abc0b6e465cf61f";
-    })
-    (fetchTritonPatch {
-      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
-      file = "mutter/mutter-3.18.2-wayland-crash.patch";
-      sha256 = "5ad07d72ef05954a53b3e7679ad607621c6076749af431b4ae3c58698731910a";
-    })
-    (fetchTritonPatch {
-      rev = "9e67dfb8cbcb8c314fee112e2b751dd907cec544";
-      file = "mutter/mutter-3.18.2-configure-notify.patch";
-      sha256 = "474b01c8bd0c14fe04645c8e0cb7e45b9fb4b37c876c11a8545a6ed1c7a7bd59";
-    })
   ];
 
   configureFlags = [

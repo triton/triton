ebe082c6f79bf253ac0c612ea9155bbb9f85f9a3
William A. Kennington III <william@wkennington.com>
2016-03-07 00:40:41 -0800

    lcms: Fix

--- pkgs/development/libraries/lcms/default.nix
+++ pkgs/development/libraries/lcms/default.nix
@@ -1,16 +1,23 @@
-{stdenv, fetchurl}:
+{ stdenv
+, fetchurl
+}:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "lcms-1.19";
 
   src = fetchurl {
-    url = http://www.littlecms.com/lcms-1.19.tar.gz;
+    url = "mirror://sourceforge/lcms/${name}.tar.gz";
     sha256 = "1abkf8iphwyfs3z305z3qczm3z1i9idc1lz4gvfg92jnkz5k5bl0";
   };
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "Color management engine";
     homepage = http://www.littlecms.com/;
-    license = stdenv.lib.licenses.mit;
+    license = licenses.mit;
+    maintainers = with maintainers; [
+      wkennington
+    ];
+    platforms = with platforms;
+      x86_64-linux;
   };
 }

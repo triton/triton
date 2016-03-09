cc0dd1619d7e0e9add80dce6f2db9705f8fc4cba
Cody Opel <codyopel@gmail.com>
2016-03-09 13:20:40 -0500

    libtorrent-rasterbar: 1.0.8 -> 1.0.9

--- pkgs/all-pkgs/libtorrent-rasterbar/1.x.nix
+++ pkgs/all-pkgs/libtorrent-rasterbar/1.x.nix
@@ -1,6 +1,6 @@
 { callPackage, ... } @ args:
 
 callPackage ./generic.nix (args // {
-  version = "1.0.8";
-  sha256 = "0ylsbzpdjk8z7pc1fr0bba0gwh3235kbd7vqd4i0pf26v4jqcj94";
+  version = "1.0.9";
+  sha256 = "1kfydlvmx4pgi5lpbhqr4p3jr78p3f61ic32046mkp4yfyydrspl";
 })

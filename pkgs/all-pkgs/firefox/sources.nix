4bf58248f01add47333098793490cd7c05ce5106
Cody Opel <codyopel@gmail.com>
2016-03-08 19:52:41 -0500

    firefox-esr: 38.6.1 -> 38.7.0

--- pkgs/all-pkgs/firefox/sources.nix
+++ pkgs/all-pkgs/firefox/sources.nix
@@ -4,7 +4,7 @@
     sha512 = "2sw9qrg4mfclc4l0czpzra3cn9hwz6v02b3zwixjcj7scwkp7dm701s6idk9bvmfnbsfxgf2h8a9nmplr2rifbpgcivg0q1yzqpnp6p";
   };
   esr = {
-    version = "38.6.1esr";
-    sha512 = "e80161a8cf95ab46cf49018c1105c986a412405e3bdb7535fc7ad5e68195f8ebe799624ed482576538a785ff2a4fdad5d1cf594566180035c2bfc9a4eb9b790c";
+    version = "38.7.0esr";
+    sha512 = "356agppaji4q9j75hrh6000mvv6jx9vkkzpxx14nxgh4kr8cy90jgr8h6r4jwl5qgmz7jwpmyy23yqavyj04r11r8yqr9c2dnqcvf7d";
   };
 }

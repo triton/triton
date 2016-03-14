23d256d01d3a66bacf57a3bf3d3a9d378184afdb
William A. Kennington III <william@wkennington.com>
2016-03-14 01:50:28 -0700

    spl: 0.6.5.4 -> 0.6.5.5

--- pkgs/os-specific/linux/spl/default.nix
+++ pkgs/os-specific/linux/spl/default.nix
@@ -1,13 +1,13 @@
 { callPackage, fetchFromGitHub, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "0.6.5.4";
+  version = "0.6.5.5";
 
   src = fetchFromGitHub {
     owner = "zfsonlinux";
     repo = "spl";
     rev = "spl-${version}";
-    sha256 = "0k80xvl15ahbs0mylfl2bd5widxhngpf7dl6zq46s21wk0795jl4";
+    sha256 = "1f49qv648klg2sn1v1wzwd6ls1njjj0hrazz7msd74ayhwm0zcw7";
   };
 
   patches = [ ./patches.patch ];

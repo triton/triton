7c645752abbf7a8115cc0526fca84386c0c59d68
William A. Kennington III <william@wkennington.com>
2016-03-17 01:48:52 -0700

    net-tools: 2016-01-16 -> 2016-02-15

--- pkgs/all-pkgs/net-tools/default.nix
+++ pkgs/all-pkgs/net-tools/default.nix
@@ -1,23 +1,21 @@
 { stdenv
-#, fetchFromSourceforge
+, fetchurl
 , fetchgit
 
 , bluez
 }:
 
+let
+  date = "2016-02-15";
+  gitRev = "bd8bceaed2311651710331a7f8990c3e31be9840";
+in
 stdenv.mkDerivation rec {
-  name = "net-tools-2016-01-27";
+  name = "net-tools-${date}";
 
-  /*src = fetchFromSourceforge {
-    repo = "net-tools";
-    rev = "3f170bff115303e92319791cbd56371e33dcbf6d";
-    sha256 = "0sj2mqp74p25ijlbnf8x7xyfb74j09w0vd7ya7i24zadcbzh103r";
-  };*/
-
-  src = fetchgit {
-    url = "http://git.code.sf.net/p/net-tools/code";
-    rev = "3f170bff115303e92319791cbd56371e33dcbf6d";
-    sha256 = "0qarpgc3lnzg2wkmaq3rf70mrydnk2cc3qjxaykv625xm6bdyxy2";
+  src = fetchurl {
+    name = "${name}.tar.xz";
+    multihash = "QmQo77UM6kGXvznbkDFYcDeJZV2aLLUzcw8zGiPepRSrZZ";
+    sha256 = "ff6f8986a56cdab0f012084def538357fba117ad81660ed6f7130f6c48b8e963";
   };
 
   buildInputs = [
@@ -32,13 +30,36 @@ stdenv.mkDerivation rec {
     )
   '';
 
+  passthru = {
+    sourceTarball = stdenv.mkDerivation rec {
+      name = "net-tools-tarball-${date}";
+
+      src = fetchgit {
+        url = "http://git.code.sf.net/p/net-tools/code";
+        rev = gitRev;
+        sha256 = "1h8iryf5qc4svyz2ri1v4a8117w4r79rw6iqn8n7qavg0w45b30k";
+      };
+
+      buildPhase = ''
+        cp -r ${src} ${name}
+        tar Jcf ${name}.tar.xz ${name}
+      '';
+
+      installPhase = ''
+        mkdir -p $out
+        cp ${name}.tar.xz $out
+      '';
+    };
+  };
+
   meta = with stdenv.lib; {
     description = "Tools for controlling the network subsystem in Linux";
     homepage = http://net-tools.sourceforge.net/;
     license = licenses.gpl2Plus;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [
+      wkennington
+    ];
     platforms = with platforms;
-      i686-linux
-      ++ x86_64-linux;
+      x86_64-linux;
   };
 }

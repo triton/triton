4dd09c480769631d18d9475bd177ce34e2c95aed
William A. Kennington III <william@wkennington.com>
2016-03-21 15:20:03 -0700

    minisign: Allow updates to verify minisign hash

--- pkgs/all-pkgs/minisign/default.nix
+++ pkgs/all-pkgs/minisign/default.nix
@@ -24,6 +24,16 @@ stdenv.mkDerivation rec {
     libsodium
   ];
 
+  passthru = {
+    srcUpdate = fetchurl {
+      url = "https://github.com/jedisct1/minisign/archive/${version}.tar.gz";
+      minisignUrl = "https://github.com/jedisct1/minisign/releases/download/${version}/minisign-${version}.tar.gz.minisig";
+      minisignPub = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      sha256 = "f2267a07bece923d4d174ccacccc56eff9c05b28c4d971e601de896355442f0a";
+      showURLs = true;
+    };
+  };
+
   meta = with stdenv.lib; {
     maintainers = with maintainers; [
       wkennington

f79c971642e8e8dbd8e4d08da856d1cb86175b4f
Cody Opel <codyopel@gmail.com>
2016-03-21 05:46:29 -0400

    fcgi: upstream mirror no longer exists

--- pkgs/development/libraries/fcgi/default.nix
+++ pkgs/development/libraries/fcgi/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   name = "fcgi-2.4.0";
 
   src = fetchurl {
-    url = "http://www.fastcgi.com/dist/${name}.tar.gz";
+    url = "mirror://gentoo/distfiles/${name}.tar.gz";
     sha256 = "1f857wnl1d6jfrgfgfpz3zdaj8fch3vr13mnpcpvy8bang34bz36";
   };
 

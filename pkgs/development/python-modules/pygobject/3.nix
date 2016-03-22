83a9f6b4a4bb825cd921c440a5b18ec4a17e5b8c
Cody Opel <codyopel@gmail.com>
2016-03-22 15:28:33 -0400

    pygobject: 3.18.2 -> 3.20.0

--- pkgs/development/python-modules/pygobject/3.nix
+++ pkgs/development/python-modules/pygobject/3.nix
@@ -1,18 +1,58 @@
-{ stdenv, fetchurl, python, pkgconfig, glib, gobjectIntrospection, pycairo, cairo, libffi }:
+{ stdenv
+, fetchurl
+
+, python
+, pkgconfig
+, glib
+, gobject-introspection
+, pycairo
+, cairo
+, libffi
+}:
+
+with {
+  inherit (stdenv.lib)
+    enFlag;
+};
 
 stdenv.mkDerivation rec {
-  name = "pygobject-3.18.2";
+  name = "pygobject-${version}";
+  versionMajor = "3.20";
+  versionMinor = "0";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/pygobject/3.18/${name}.tar.xz";
-    sha256 = "0prc3ky7g50ixmfxbc7zf43fw6in4hw2q07667hp8swi2wassg1a";
+    url = "mirror://gnome/sources/pygobject/${versionMajor}/${name}.tar.xz";
+    sha256 = "31ab4701f40490082aa98af537ccddba889577abe66d242582f28577e8807f46";
   };
 
-  buildInputs = [ python pkgconfig glib gobjectIntrospection libffi ];
-  propagatedBuildInputs = [ pycairo cairo ];
+  buildInputs = [
+    cairo
+    glib
+    gobject-introspection
+    libffi
+    pycairo
+    python
+  ];
 
-  meta = {
-    homepage = http://live.gnome.org/PyGObject;
+  configureFlags = [
+    "--enable-thread"
+    "--enable-glibtest"
+    (enFlag "cairo" (cairo != null) null)
+    "--enable-compile-warnings"
+    "--disable-iso-c"
+    "--disable-code-coverage"
+    "--with-common"
+  ];
+
+  meta = with stdenv.lib; {
     description = "Python bindings for Glib";
+    homepage = http://live.gnome.org/PyGObject;
+    license = licenses.free;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = with platforms;
+      x86_64-linux;
   };
 }

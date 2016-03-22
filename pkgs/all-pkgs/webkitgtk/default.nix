bf51a056d949479536e268663076279037d5b873
Cody Opel <codyopel@gmail.com>
2016-03-22 10:06:42 -0400

    webkitgtk: 2.10.8 -> 2.12.0

--- pkgs/all-pkgs/webkitgtk/default.nix
+++ pkgs/all-pkgs/webkitgtk/default.nix
@@ -22,7 +22,6 @@
 , gnutls
 , gst-plugins-base
 , gstreamer
-, gtk2
 , gtk3
 , harfbuzz
 , icu
@@ -50,118 +49,20 @@
 
 with {
   inherit (stdenv.lib)
-    cmFlag;
+    cmFlag
+    optionals;
 };
 
+assert xorg != null ->
+  xorg.libX11 != null;
+
 stdenv.mkDerivation rec {
-  name = "webkitgtk-2.10.8";
+  name = "webkitgtk-2.12.0";
 
   src = fetchurl {
     url = "http://webkitgtk.org/releases/${name}.tar.xz";
-    sha256 = "1a98z7fa8vxk7y2hlbnkl767908anyyxbwkyiar5gi037yr84dii";
+    sha256 = "a68604f8fd1bcb247d647709290226289b90c16acb1f3730538b688db8df5ea6";
   };
-
-  patches = [
-    (fetchTritonPatch {
-      rev = "d3fc5e59bd2b4b465c2652aae5e7428b24eb5669";
-      file = "webkitgtk/webkit-gtk-2.10.x-finding-harfbuzz-icu.patch";
-      sha256 = "8eb3f4844b06c3c060233396045248a883177e9a09c491ddfaf9897d8e8ca2c4";
-    })
-    (fetchTritonPatch {
-      rev = "0f1cfb237c758ea9c79fe44d6f5b5b20bcf8900f";
-      file = "webkitgtk/webkit-gtk-2.10.7-fix-compatibility-with-glibc-2.23.patch";
-      sha256 = "b66396c7aeddaf90360d4c37301099734925197fa664bfd7d8c6b4532624b432";
-    })
-  ];
-
-  postPatch = ''
-    patchShebangs ./Tools
-  '';
-
-  cmakeFlags = [
-    (cmFlag "CMAKE_BUILD_TYPE" "Release")
-    (cmFlag "PORT" "GTK")
-    (cmFlag "ENABLE_PLUGIN_PROCESS_GTK2" (gtk2 != null))
-
-    (cmFlag "ENABLE_WEBKIT" true)
-    (cmFlag "ENABLE_WEBKIT2" true)
-    (cmFlag "ENABLE_TOOLS" true)
-    # Shared library causes linker failure
-    (cmFlag "SHARED_CORE" false)
-    (cmFlag "ENABLE_API_TESTS" false)
-
-    (cmFlag "ENABLE_GRAPHICS_CONTEXT_3D" true)
-    (cmFlag "ENABLE_GTKDOC" false)
-    (cmFlag "ENABLE_INTROSPECTION" (gobject-introspection != null))
-    (cmFlag "USE_REDIRECTED_XCOMPOSITE_WINDOW" true)
-
-    (cmFlag "ENABLE_DEVELOPER_MODE" false)
-
-    # Optional libraries
-    (cmFlag "ENABLE_CREDENTIAL_STORAGE" (libsecret != null))
-    (cmFlag "ENABLE_JIT" true)
-    (cmFlag "ENABLE_FTL_JIT" false) # llvm
-    # TODO: add gudev support
-    (cmFlag "ENABLE_GAMEPAD_DEPRECATED" false)
-    (cmFlag "ENABLE_GEOLOCATION" (geoclue2 != null))
-    # TODO: add openwebrtc support
-    (cmFlag "ENABLE_MEDIA_STREAM" false)
-    (cmFlag "ENABLE_OPENGL" (mesa_noglu != null))
-    (cmFlag "ENABLE_GLES2" (mesa_noglu != null))
-    (cmFlag "ENABLE_PLUGIN_PROCESS_GTK2" (gtk2 != null))
-    (cmFlag "ENABLE_SECCOMP_FILTERS" false)
-    (cmFlag "ENABLE_SPELLCHECK" (enchant != null))
-    (cmFlag "ENABLE_SUBTLE_CRYPTO" (gnutls != null))
-    (cmFlag "ENABLE_VIDEO" (gstreamer != null && gst-plugins-base != null))
-    (cmFlag "ENABLE_WEB_AUDIO" (gstreamer != null && gst-plugins-base != null))
-    # TODO: add gstreamer mpeg-ts support
-    (cmFlag "USE_GSTREAMER_MPEGTS" false)
-    # TODO: add gstreamer GL support
-    (cmFlag "USE_GSTREAMER_GL" false)
-    (cmFlag "ENABLE_X11_TARGET" (xorg.libX11 != null))
-    # TODO: add wayland support (linker errors)
-    (cmFlag "ENABLE_WAYLAND_TARGET" (wayland != null))
-    (cmFlag "USE_LIBNOTIFY" (libnotify != null))
-    # TODO: add libhyphen support
-    (cmFlag "USE_LIBHYPHEN" false)
-
-    #ENABLE_PLUGIN_PROCESS
-    #ENABLE_NETWORK_PROCESS
-    #ENABLE_DATABASE_PROCESS
-    #ENABLE_ICONDATABASE
-    #ENABLE_THREADED_COMPOSITOR
-    #ENABLE_BATTERY_STATUS
-    #ENABLE_ACCESSIBILITY
-    #ENABLE_ALLINONE_BUILD
-    #ENABLE_ENCRYPTED_MEDIA
-    #ENABLE_WEBGL
-    #ENABLE_VIDEO_TRACK
-    #ENABLE_QUOTA
-    #USE_EGL
-    #ENABLE_WEB_REPLAY
-    #ENABLE_USER_MESSAGE_HANDLERS
-    #WebCore_USER_AGENT_SCRIPTS
-    #ENABLE_SVG_FONTS
-    #ENABLE_GEOLOCATION
-    #USE_GEOCLUE2
-    #USE_TEXTURE_MAPPER
-    #USE_OPENGL_ES_2
-    #USE_OPENGL
-    #ENABLE_SMOOTH_SCROLLING
-    #ENABLE_INDEXED_DATABASE
-    #USE_LD_GOLD
-    #ENABLE_ECORE_X
-    #ENABLE_ACCESSIBILITY
-    #ENABLE_NETSCAPE_PLUGIN_API
-    #ENABLE_EGL
-    #ENABLE_SPEECH_SYNTHESIS
-  ];
-
-  # WebKit2 missing include path for gst-plugins-base.
-  # https://bugs.webkit.org/show_bug.cgi?id=148894
-  NIX_CFLAGS_COMPILE = [
-    "-I${gst-plugins-base}/include/gstreamer-1.0"
-  ];
 
   nativeBuildInputs = [
     bison
@@ -187,7 +88,6 @@ stdenv.mkDerivation rec {
     gnutls
     gst-plugins-base
     gstreamer
-    gtk2
     gtk3
     harfbuzz
     icu
@@ -203,6 +103,7 @@ stdenv.mkDerivation rec {
     pango
     sqlite
     wayland
+  ] ++ optionals (xorg != null) [
     xorg.libX11
     xorg.libXau
     xorg.libXcomposite
@@ -217,6 +118,72 @@ stdenv.mkDerivation rec {
     zlib
   ];
 
+  patches = [
+    (fetchTritonPatch {
+      rev = "d3fc5e59bd2b4b465c2652aae5e7428b24eb5669";
+      file = "webkitgtk/webkit-gtk-2.10.x-finding-harfbuzz-icu.patch";
+      sha256 = "8eb3f4844b06c3c060233396045248a883177e9a09c491ddfaf9897d8e8ca2c4";
+    })
+  ];
+
+  postPatch = ''
+    patchShebangs ./Tools
+  '';
+
+  cmakeFlags = [
+    (cmFlag "CMAKE_BUILD_TYPE" "Release")
+    (cmFlag "PORT" "GTK")
+    (cmFlag "ENABLE_PLUGIN_PROCESS_GTK2" false)
+
+    (cmFlag "ENABLE_WEBKIT" false)
+    (cmFlag "ENABLE_WEBKIT2" true)
+    (cmFlag "ENABLE_TOOLS" true)
+    # Shared library causes linker failures
+    (cmFlag "SHARED_CORE" false)
+    (cmFlag "ENABLE_API_TESTS" false)
+
+    (cmFlag "ENABLE_GRAPHICS_CONTEXT_3D" true)
+    (cmFlag "ENABLE_GTKDOC" false)
+    (cmFlag "ENABLE_INTROSPECTION" (gobject-introspection != null))
+    (cmFlag "USE_REDIRECTED_XCOMPOSITE_WINDOW" true)
+
+    (cmFlag "ENABLE_DEVELOPER_MODE" false)
+
+    # Optional libraries
+    (cmFlag "ENABLE_CREDENTIAL_STORAGE" (libsecret != null))
+    (cmFlag "ENABLE_JIT" true)
+    (cmFlag "ENABLE_FTL_JIT" false) # llvm
+    # TODO: add gudev support
+    (cmFlag "ENABLE_GAMEPAD_DEPRECATED" false)
+    (cmFlag "ENABLE_GEOLOCATION" (geoclue2 != null))
+    # TODO: add openwebrtc support
+    (cmFlag "ENABLE_MEDIA_STREAM" false)
+    (cmFlag "ENABLE_OPENGL" (mesa_noglu != null))
+    (cmFlag "ENABLE_GLES2" (mesa_noglu != null))
+    (cmFlag "ENABLE_PLUGIN_PROCESS_GTK2" false)
+    (cmFlag "ENABLE_SECCOMP_FILTERS" false)
+    (cmFlag "ENABLE_SPELLCHECK" (enchant != null))
+    (cmFlag "ENABLE_SUBTLE_CRYPTO" (gnutls != null))
+    (cmFlag "ENABLE_VIDEO" (gstreamer != null && gst-plugins-base != null))
+    (cmFlag "ENABLE_WEB_AUDIO" (gstreamer != null && gst-plugins-base != null))
+    # TODO: add gstreamer mpeg-ts support
+    (cmFlag "USE_GSTREAMER_MPEGTS" false)
+    # TODO: add gstreamer GL support
+    (cmFlag "USE_GSTREAMER_GL" false)
+    (cmFlag "ENABLE_X11_TARGET" (xorg != null))
+    # TODO: add wayland support (linker errors)
+    (cmFlag "ENABLE_WAYLAND_TARGET" (wayland != null))
+    (cmFlag "USE_LIBNOTIFY" (libnotify != null))
+    # TODO: add libhyphen support
+    (cmFlag "USE_LIBHYPHEN" false)
+  ];
+
+  # WebKit2 missing include path for gst-plugins-base.
+  # https://bugs.webkit.org/show_bug.cgi?id=148894
+  NIX_CFLAGS_COMPILE = [
+    "-I${gst-plugins-base}/include/gstreamer-1.0"
+  ];
+
   meta = with stdenv.lib; {
     description = "Web content rendering engine, GTK+ port";
     homepage = "http://webkitgtk.org/";
@@ -225,7 +192,6 @@ stdenv.mkDerivation rec {
       codyopel
     ];
     platforms = with platforms;
-      i686-linux
-      ++ x86_64-linux;
+      x86_64-linux;
   };
 }

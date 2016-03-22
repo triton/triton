ba7304d486462341f863f9d20df2d799e3cb1fc0
Cody Opel <codyopel@gmail.com>
2016-03-22 05:02:49 -0400

    at-spi2-atk: 2.18.1 -> 2.20.0

--- pkgs/all-pkgs/at-spi2-atk/default.nix
+++ pkgs/all-pkgs/at-spi2-atk/default.nix
@@ -21,13 +21,13 @@ with {
 
 stdenv.mkDerivation rec {
   name = "at-spi2-atk-${version}";
-  versionMajor = "2.18";
-  versionMinor = "1";
+  versionMajor = "2.20";
+  versionMinor = "0";
   version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/at-spi2-atk/${versionMajor}/${name}.tar.xz";
-    sha256 = "0bf1g5cj84rmx7p1q547vwbc0hlpcs2wrxnmv96lckfkhs9mzcf4";
+    sha256 = "a24b142b6e8f1dd2d57a657447dde3e0ae29df481968c88673a58d4ce44f3ad2";
   };
 
   nativeBuildInputs = [

45edf35d899bf564bbe8f8a8694b4d1ceeb621d4
Cody Opel <codyopel@gmail.com>
2016-03-22 11:11:17 -0400

    gdk-pixbuf: 2.32.3 -> 2.33.2

--- pkgs/all-pkgs/gdk-pixbuf-core/default.nix
+++ pkgs/all-pkgs/gdk-pixbuf-core/default.nix
@@ -23,51 +23,15 @@ with {
 
 stdenv.mkDerivation rec {
   name = "gdk-pixbuf-core-${version}";
-  versionMajor = "2.32";
-  versionMinor = "3";
+  versionMajor = "2.33";
+  versionMinor = "2";
   version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gdk-pixbuf/${versionMajor}/" +
           "gdk-pixbuf-${version}.tar.xz";
-    sha256 = "0cfh87aqyqbfcwpbv1ihgmgfcn66il5q2n8yjyl8gxkjmkqp2rrb";
+    sha256 = "d91ec3ab7d0fcf998d2c38b6725655666e0767e2c462598aea003caee2455929";
   };
-
-  postPatch =
-  /* The configure script only tests glib for mimetype detection
-     support if --enable-gio-sniffing=auto, this patches it to
-     run the test and explicitly fail if glib isn't configured
-     correctly. */ ''
-    sed -i configure.ac \
-      -e '/x$enable_gio_sniffing/ s/xauto/xyes/' \
-      -e 's|\[gio_can_sniff=no\]|\[gio_can_sniff=no, AC_MSG_ERROR(gio cannot determine mimetype)\]|'
-  '';
-
-  configureFlags = [
-    # TODO: fix glib to support gio sniffing
-    "--disable-gio-sniffing"
-    "--enable-rebuilds"
-    "--enable-nls"
-    "--enable-rpath"
-    "--enable-glibtest"
-    "--enable-modules"
-    (enFlag "introspection" (gobject-introspection != null) null)
-    "--disable-gtk-doc"
-    "--disable-gtk-doc-html"
-    "--disable-gtk-doc-pdf"
-    "--disable-man"
-    "--enable-Bsymbolic"
-    "--enable-installed-tests"
-    "--enable-always-build-tests"
-    "--disable-coverage"
-    # Enabling relocations breaks setting loaders.cache path
-    "--disable-relocations"
-    (wtFlag "libpng" (libpng != null) null)
-    (wtFlag "libjpeg" (libjpeg != null) null)
-    (wtFlag "libjasper" (jasper != null) null)
-    "--with-gdiplus"
-    (wtFlag "x11" (xorg != null) null)
-  ];
 
   nativeBuildInputs = [
     autoreconfHook
@@ -86,19 +50,58 @@ stdenv.mkDerivation rec {
     xorg.libX11
   ];
 
+  postPatch =
+  /* The configure script only tests glib for mimetype detection
+     support if --enable-gio-sniffing=auto, this patches it to
+     run the test and explicitly fail if glib isn't configured
+     correctly. */ ''
+    sed -i configure.ac \
+      -e '/x$enable_gio_sniffing/ s/xauto/xyes/' \
+      -e 's|\[gio_can_sniff=no\]|\[gio_can_sniff=no, AC_MSG_ERROR(gio cannot determine mimetype)\]|'
+  '';
+
+  configureFlags = [
+    "--disable-maintainer-mode"
+    # TODO: fix glib to support gio sniffing
+    "--disable-gio-sniffing"
+    "--enable-largefile"
+    "--disable-debug"
+    "--enable-rebuilds"
+    "--enable-nls"
+    "--enable-rpath"
+    "--enable-glibtest"
+    "--enable-modules"
+    (enFlag "introspection" (gobject-introspection != null) null)
+    "--disable-gtk-doc"
+    "--disable-gtk-doc-html"
+    "--disable-gtk-doc-pdf"
+    "--disable-man"
+    "--enable-Bsymbolic"
+    "--disable-installed-tests"
+    "--disable-always-build-tests"
+    "--disable-coverage"
+    # Enabling relocations breaks setting loaders.cache path
+    "--disable-relocations"
+    (wtFlag "libpng" (libpng != null) null)
+    (wtFlag "libjpeg" (libjpeg != null) null)
+    (wtFlag "libtiff" (libtiff != null) null)
+    (wtFlag "libjasper" (jasper != null) null)
+    "--without-gdiplus"
+    (wtFlag "x11" (xorg != null) null)
+  ];
+
   postInstall = "rm -rvf $out/share/gtk-doc";
 
   doCheck = false;
-  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     description = "A library for image loading and manipulation";
     homepage = http://library.gnome.org/devel/gdk-pixbuf/;
+    license = licenses.free;
     maintainers = [
       codyopel
     ];
     platforms = with platforms;
-      i686-linux
-      ++ x86_64-linux;
+      x86_64-linux;
   };
 }

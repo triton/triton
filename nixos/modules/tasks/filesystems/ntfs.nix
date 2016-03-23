8864ed467f6fe239d18a34982b439022438482fe
Cody Opel <codyopel@gmail.com>
2016-03-22 20:43:57 -0400

    ntfs-3g: fix reference

--- nixos/modules/tasks/filesystems/ntfs.nix
+++ nixos/modules/tasks/filesystems/ntfs.nix
@@ -5,7 +5,7 @@ with lib;
 {
   config = mkIf (any (fs: fs == "ntfs" || fs == "ntfs-3g") config.boot.supportedFilesystems) {
 
-    system.fsPackages = [ pkgs.ntfs3g ];
+    system.fsPackages = [ pkgs.ntfs-3g ];
 
   };
 }

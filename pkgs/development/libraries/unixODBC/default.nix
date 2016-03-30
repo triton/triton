e866ce979f93b05056320639dc3954df98b64a07
William A. Kennington III <william@wkennington.com>
2016-03-30 16:04:52 -0700

    unixODBC: Cleanup

--- pkgs/development/libraries/unixODBC/default.nix
+++ pkgs/development/libraries/unixODBC/default.nix
@@ -1,10 +1,35 @@
-{stdenv, fetchurl}:
+{ stdenv
+, fetchurl
+
+, ncurses
+, readline
+}:
 
 stdenv.mkDerivation rec {
-  name = "unixODBC-2.3.2";
+  name = "unixODBC-2.3.4";
+
   src = fetchurl {
     url = "ftp://ftp.unixodbc.org/pub/unixODBC/${name}.tar.gz";
-    sha256 = "16jw5fq7wgfky6ak1h2j2pqx99jivsdl4q8aq6immpr55xs5jd4w";
+    md5Confirm = "bd25d261ca1808c947cb687e2034be81";
+    sha256 = "2e1509a96bb18d248bf08ead0d74804957304ff7c6f8b2e5965309c632421e39";
   };
-  configureFlags = "--disable-gui --sysconfdir=/etc";
+
+  buildInputs = [
+    ncurses
+    readline
+  ];
+
+  configureFlags = [
+    "--disable-gui"
+    "--sysconfdir=/etc"
+    "--localstatedir=/var"
+  ];
+
+  meta = with stdenv.lib; {
+    maintainers = with maintainers; [
+      wkennington
+    ];
+    platforms = with platforms;
+      x86_64-linux;
+  };
 }

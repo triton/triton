1572305365b5d0459ae6a5a8d6a9acb4dadb2b29
Cody Opel <codyopel@gmail.com>
2016-03-29 23:52:50 -0400

    clutter-gtk: 1.6.6 -> 1.8.0

--- pkgs/all-pkgs/clutter-gtk/default.nix
+++ pkgs/all-pkgs/clutter-gtk/default.nix
@@ -20,26 +20,14 @@ with {
 
 stdenv.mkDerivation rec {
   name = "clutter-gtk-${version}";
-  versionMajor = "1.6";
-  versionMinor = "6";
+  versionMajor = "1.8";
+  versionMinor = "0";
   version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/clutter-gtk/${versionMajor}/${name}.tar.xz";
-    sha256 = "0a2a8ci6in82l43zak3zj3cyms23i5rq6lzk1bz013gm023ach4l";
+    sha256 = "742ef9d68ece36cbb1b2e1a4a6fbdad932f6645360be7e6de75abbb140dfbf1d";
   };
-
-  configureFlags = [
-    "--disable-maintainer-flags"
-    "--enable-deprecated"
-    "--disable-debug"
-    "--enable-nls"
-    "--enable-rpath"
-    "--disable-gtk-doc"
-    "--disable-gtk-doc-html"
-    "--disable-gtk-doc-pdf"
-    (enFlag "introspection" (gobject-introspection != null) null)
-  ];
 
   nativeBuildInputs = [
     gettext
@@ -57,15 +45,28 @@ stdenv.mkDerivation rec {
     pango
   ];
 
-  postBuild = "rm -rvf $out/share/gtk-doc";
+  configureFlags = [
+    "--disable-deprecated"
+    "--disable-debug"
+    "--disable-maintainer-flags"
+    "--enable-nls"
+    "--enable-rpath"
+    "--disable-gtk-doc"
+    "--disable-gtk-doc-html"
+    "--disable-gtk-doc-pdf"
+    (enFlag "introspection" (gobject-introspection != null) null)
+  ];
+
+  postBuild = "rm -frv $out/share/gtk-doc";
 
   meta = with stdenv.lib; {
     description = "Library for embedding a Clutter canvas (stage) in GTK+";
     homepage = http://www.clutter-project.org/;
     license = licenses.lgpl2Plus;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [
+      codyopel
+    ];
     platforms = with platforms;
-      i686-linux
-      ++ x86_64-linux;
+      x86_64-linux;
   };
 }

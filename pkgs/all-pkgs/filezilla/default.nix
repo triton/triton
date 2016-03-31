e274fd285de6d5167ccf81ff60b470103b8f9063
William A. Kennington III <william@wkennington.com>
2016-03-31 15:39:04 -0700

    filezilla: Use the given hash files

--- pkgs/all-pkgs/filezilla/default.nix
+++ pkgs/all-pkgs/filezilla/default.nix
@@ -11,12 +11,16 @@
 , xdg-utils
 }:
 
+let
+  version = "3.16.1";
+  baseFileUrl = "mirror://sourceforge/project/filezilla/FileZilla_Client/${version}/FileZilla_${version}";
+in
 stdenv.mkDerivation rec {
   name = "filezilla-${version}";
-  version = "3.16.1";
 
   src = fetchurl {
-    url = "mirror://sourceforge/project/filezilla/FileZilla_Client/${version}/FileZilla_${version}_src.tar.bz2";
+    url = "${baseFileUrl}_src.tar.bz2";
+    sha512Url = "${baseFileUrl}.sha512";
     sha256 = "3b046f854cea17f9c14d6a12aa5d2e027908c6535f44a068cfed5da9eddddda8";
   };
 

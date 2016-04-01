0fb1a57c2c04635132488c28e827554fe6b566ef
William A. Kennington III <william@wkennington.com>
2016-03-31 18:47:34 -0700

    libmsgpack: 1.4.0 -> 1.4.1

--- pkgs/all-pkgs/libmsgpack/default.nix
+++ pkgs/all-pkgs/libmsgpack/default.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   name = "libmsgpack-${version}";
-  version = "1.4.0";
+  version = "1.4.1";
 
   src = fetchFromGitHub {
     owner = "msgpack";
     repo = "msgpack-c";
     rev = "cpp-${version}";
-    sha256 = "0knm1vrybah767wh36qiqc82crgnd359c7w9vsg06f19hd8a30w8";
+    sha256 = "16b50d9e48a9ba32877255f229ae5a0fc5bd0f73100c1d15c030157653ff8bc0";
   };
 
   nativeBuildInputs = [

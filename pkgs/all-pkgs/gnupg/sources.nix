dbefdb1c5932fd92d9060c9fcb6a8a06ad5112e0
William A. Kennington III <william@wkennington.com>
2016-04-01 22:32:54 -0700

    gnupg: 2.0.29 -> 2.0.30

--- pkgs/all-pkgs/gnupg/sources.nix
+++ pkgs/all-pkgs/gnupg/sources.nix
@@ -1,7 +1,7 @@
 {
   "2.0" = {
-    version = "2.0.29";
-    sha256 = "1jaakn0mi6pi2b3g3imxj3qzxw2zg0ifxs30baq2b157dcw6pvb8";
+    version = "2.0.30";
+    sha256 = "e329785a4f366ba5d72c2c678a7e388b0892ac8440c2f4e6810042123c235d71";
   };
 
   "2.1" = {

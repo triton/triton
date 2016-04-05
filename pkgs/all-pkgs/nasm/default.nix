0748279188fd22271c951ac0b739d3b0abc243e7
Cody Opel <codyopel@gmail.com>
2016-04-05 00:23:25 -0400

    nasm: 2.12 -> 2.12.01

--- pkgs/all-pkgs/nasm/default.nix
+++ pkgs/all-pkgs/nasm/default.nix
@@ -1,18 +1,21 @@
-{ stdenv, fetchurl }:
+{ stdenv
+, fetchurl
+}:
 
 stdenv.mkDerivation rec {
   name = "nasm-${version}";
-  version = "2.12";
+  version = "2.12.01";
 
   src = fetchurl {
     url = "http://www.nasm.us/pub/nasm/releasebuilds/${version}/" +
           "${name}.tar.bz2";
-    sha256 = "07l3cx88lnr57xi2q46xzh5cwqyswimaqgnms69h0m41614hb80f";
+    sha256 = "f8bebee8107a42f6661526cf9e0bd92fcd33ff0df01ea05093f7650ec60d902b";
   };
 
   meta = with stdenv.lib; {
     description = "An assembler for x86 and x86_64 instruction sets";
     homepage = http://www.nasm.us/;
+    license = licenses.bsd2;
     maintainers = with maintainers; [ ];
     platforms = with platforms;
       i686-linux

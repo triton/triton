90b56a9ff4acfb88aefab6d9b9593fd4d0170de2
William A. Kennington III <william@wkennington.com>
2016-04-06 14:46:32 -0700

    ipfs-hasher: Add release tarball

--- pkgs/all-pkgs/ipfs-hasher/default.nix
+++ pkgs/all-pkgs/ipfs-hasher/default.nix
@@ -3,10 +3,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "ipfs-hasher-0.0.0";
+  name = "ipfs-hasher-${version}";
+  version = "0.0.0";
   
   src = fetchurl {
-    name = "${name}.tar.xz";
+    url = "https://github.com/triton/ipfs-hasher/releases/download/${version}/${name}.tar.xz";
     multihash = "QmXCsrxUoJpZENQ17T798n9X7YiCAjc5T3bLDHgLKShQaU";
     sha256 = "5488b02404c3879b974a14bfd2b028c6238489260107bc324883dae9e90c0b28";
   };
@@ -16,6 +17,7 @@ stdenv.mkDerivation rec {
       wkennington
     ];
     platforms = with platforms;
-      x86_64-linux;
+      i686-linux
+      ++ x86_64-linux;
   };
 }

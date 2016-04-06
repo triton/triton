bb859e0f8955adfb3a95ab41fe1849d42764a974
William A. Kennington III <william@wkennington.com>
2016-04-06 11:32:24 -0700

    vim: 7.4.1585 -> 7.4.1714

--- pkgs/all-pkgs/vim/default.nix
+++ pkgs/all-pkgs/vim/default.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation rec {
   name = "vim-${version}";
-  version = "7.4.1585";
+  version = "7.4.1714";
 
   src = fetchFromGitHub {
     owner = "vim";
     repo = "vim";
     rev = "v${version}";
-    sha256 = "704712cb9d3b3bacbe10afb36d10c0564cfd1e17119b57b93aa75dec2ddb8401";
+    sha256 = "ecdd2b84b2bd3154d57e7acdf57d0263feeaf3507c4402a9fd0bafd6bcb1c753";
   };
 
   nativeBuildInputs = [

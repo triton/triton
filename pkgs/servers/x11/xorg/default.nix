934c6ea1de912af387c9410b25e0a7db29a22f77
William A. Kennington III <william@wkennington.com>
2016-04-07 10:44:03 -0700

    xorg: Updates

--- pkgs/servers/x11/xorg/default.nix
+++ pkgs/servers/x11/xorg/default.nix
@@ -3584,11 +3584,11 @@ let
   }) // {inherit fontsproto libpciaccess randrproto renderproto videoproto xextproto xf86dgaproto xorgserver xproto ;};
 
   xf86videoamdgpu = (mkDerivation "xf86videoamdgpu" {
-    name = "xf86-video-amdgpu-1.0.1";
+    name = "xf86-video-amdgpu-1.1.0";
     builder = ./builder.sh;
     src = fetchurl {
-      url = mirror://xorg/individual/driver/xf86-video-amdgpu-1.0.1.tar.bz2;
-      sha256 = "0plinff7r99plmr120pyzncar52scjxh1i8dy4yn95kszicvj3vk";
+      url = mirror://xorg/individual/driver/xf86-video-amdgpu-1.1.0.tar.bz2;
+      sha256 = "0cbrqpmi1hgbsi0i93v0yp7lv3wf4s0vbdlrj19cxmglv7gd1xb9";
     };
     nativeBuildInputs = [ ];
     buildInputs = [ fontsproto mesa glamoregl libdrm systemd_lib randrproto renderproto videoproto xextproto xf86driproto xorgserver xproto ];

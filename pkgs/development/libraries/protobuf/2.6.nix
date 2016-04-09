ab7d8efce18f2a5e356fcb815e6a482babe97965
Cody W. Opel <codyopel@gmail.com>
2016-04-09 04:08:34 -0400

    protobuf: fix hash

--- pkgs/development/libraries/protobuf/2.6.nix
+++ pkgs/development/libraries/protobuf/2.6.nix
@@ -7,6 +7,6 @@ callPackage ./generic.nix (args // rec {
     owner = "google";
     repo = "protobuf";
     rev = "v${version}";
-    sha256 = "3ea7b7edd5148c93dce1023e4a39bb2a9be3c1e11222d81708dfe70aea1d0e4f";
+    sha256 = "ab9ffee509fa047a4d00f5901e2556d022f4780879321219c781ca0e06ca46c1";
   };
 })

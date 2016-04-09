8fca28f7498475ba7213d3523edc1d1f8874415c
Cody W. Opel <codyopel@gmail.com>
2016-04-09 00:14:25 -0400

    aria2: refactor

--- pkgs/all-pkgs/aria2/default.nix
+++ pkgs/all-pkgs/aria2/default.nix
@@ -14,7 +14,8 @@ stdenv.mkDerivation rec {
   version = "1.21.0";
 
   src = fetchurl {
-    url = "https://github.com/tatsuhiro-t/aria2/releases/download/release-${version}/${name}.tar.xz";
+    url = "https://github.com/tatsuhiro-t/aria2/releases/download/"
+      + "release-${version}/${name}.tar.xz";
     sha256 = "225c5f2c8acc899e0a802cdf198f82bd0d3282218e80cdce251b1f9ffacf6580";
   };
 
@@ -32,14 +33,13 @@ stdenv.mkDerivation rec {
   ];
 
   meta = with stdenv.lib; {
+    description = "A multi-protocol/source, command-line download utility";
     homepage = https://github.com/tatsuhiro-t/aria2;
-    description = "A lightweight, multi-protocol, multi-source, command-line download utility";
     license = licenses.gpl2Plus;
     maintainers = with maintainers; [
       wkennington
     ];
     platforms = with platforms;
-      i686-linux
-      ++ x86_64-linux;
+      x86_64-linux;
   };
 }

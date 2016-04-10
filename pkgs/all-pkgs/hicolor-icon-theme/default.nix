2abe7fc757b8590c0e57975804145804921e547d
Cody W. Opel <codyopel@gmail.com>
2016-04-10 05:02:25 -0400

    hicolor-icon-theme: fix hash

--- pkgs/all-pkgs/hicolor-icon-theme/default.nix
+++ pkgs/all-pkgs/hicolor-icon-theme/default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://icon-theme.freedesktop.org/releases/${name}.tar.xz";
-    sha256 = "1k9fj0lb9b44inb5q5m04910x5nfkzrxl3ys9ckihqrixzk0dvbv";
+    sha256 = "9cc45ac3318c31212ea2d8cb99e64020732393ee7630fa6c1810af5f987033cc";
   };
 
   setupHook = ./setup-hook.sh;

781570263ca9ae85cc4a07a4af4ba1143d4df533
William A. Kennington III <william@wkennington.com>
2016-04-10 17:19:59 -0700

    tinc: 2016-02-28 -> 2016-04-10

--- pkgs/tools/networking/tinc/pre.nix
+++ pkgs/tools/networking/tinc/pre.nix
@@ -11,12 +11,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "tinc-1.1pre-2016-02-28";
+  name = "tinc-1.1pre-2016-04-10";
 
   src = fetchgit {
     url = "git://tinc-vpn.org/tinc";
-    rev = "bf50b3502a022b406424d0d03aaf7670133452b2";
-    sha256 = "0v04xyyyrqjf0wiswqgzwyysfpxparxx70v22i1ybkmgj4gcxx5j";
+    rev = "2a7871990bc401921b8bb9accbc6a8206d564f72";
+    sha256 = "0nj5zv0n4ghl2jvm6a2ldlpsl46l1mr20xbp2kv34a4333yy1ihc";
   };
 
   nativeBuildInputs = [

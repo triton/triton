43d8910d6a8dd5d6ba26c5704cd45513729bf8db
William A. Kennington III <william@wkennington.com>
2016-04-11 15:08:54 -0700

    feh: 2.14 -> 2.15

--- pkgs/applications/graphics/feh/default.nix
+++ pkgs/applications/graphics/feh/default.nix
@@ -1,31 +1,69 @@
-{ stdenv, makeWrapper, fetchurl, xlibsWrapper, imlib2, libjpeg, libpng
-, xorg, curl, libexif }:
+{ stdenv
+, makeWrapper
+, fetchurl
+
+, curl
+, imlib2
+, libexif
+, libjpeg
+, libpng
+, xorg
+}:
 
 stdenv.mkDerivation rec {
-  name = "feh-2.14";
+  name = "feh-2.15";
 
   src = fetchurl {
     url = "http://feh.finalrewind.org/${name}.tar.bz2";
-    sha256 = "0j5wxpqccnd0hl74z2vwv25n7qnik1n2mcm2jn0c0z7cjn4wsa9q";
+    allowHashOutput = false;
+    sha256 = "42baa20bc5044288de6f782f5298566ff74ccbfee157264dfc0f8ebebebbab39";
   };
 
-  nativeBuildInputs = [ makeWrapper ];
-  buildInputs = [ xlibsWrapper imlib2 libjpeg libpng xorg.libXinerama curl libexif ];
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
+  buildInputs = [
+    curl
+    imlib2
+    libexif
+    libpng
+    xorg.libX11
+    xorg.libXinerama
+    xorg.libXt
+  ];
 
   preBuild = ''
-    makeFlags="PREFIX=$out exif=1"
+    makeFlagsArray+=(
+      "PREFIX=$out"
+      "exif=1"
+    )
   '';
 
   postInstall = ''
-    wrapProgram "$out/bin/feh" --prefix PATH : "${libjpeg}/bin" \
-                               --add-flags '--theme=feh'
+    wrapProgram "$out/bin/feh" \
+      --prefix PATH : "${libjpeg}/bin" \
+      --add-flags '--theme=feh'
   '';
 
-  meta = {
+  passthru = {
+    srcVerified = fetchurl {
+      failEarly = true;
+      pgpsigUrl = map (n: "${n}.asc") src.urls;
+      pgpKeyId = "5166E005";
+      pgpKeyFingerprint = "781B B707 1C6B F648 EAEB  08A1 100D 5BFB 5166 E005";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
+
+  meta = with stdenv.lib; {
     description = "A light-weight image viewer";
     homepage = https://derf.homelinux.org/projects/feh/;
-    license = stdenv.lib.licenses.mit;
-    maintainers = with stdenv.lib.maintainers; [viric];
-    platforms = with stdenv.lib.platforms; all;
+    license = licenses.mit;
+    maintainers = with maintainers; [
+      wkennington
+    ];
+    platforms = with platforms;
+      x86_64-linux;
   };
 }

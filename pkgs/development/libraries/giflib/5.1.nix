38c266b96416f3e35423fc2e2ad59b8420a98e8e
William A. Kennington III <william@wkennington.com>
2016-04-12 14:38:40 -0700

    giflib: 5.1.2 -> 5.1.4

--- pkgs/development/libraries/giflib/5.1.nix
+++ pkgs/development/libraries/giflib/5.1.nix
@@ -1,18 +1,23 @@
-{ stdenv, fetchurl }:
+{ stdenv
+, fetchurl
+}:
 
 stdenv.mkDerivation rec {
-  name = "giflib-5.1.2";
+  name = "giflib-5.1.4";
 
   src = fetchurl {
     url = "mirror://sourceforge/giflib/${name}.tar.bz2";
-    sha256 = "0z1adsza46q84chkxwr6x8ph11k117k8nywkzwar6bxhqf2a1h3n";
+    sha256 = "df27ec3ff24671f80b29e6ab1c4971059c14ac3db95406884fc26574631ba8d5";
   };
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "A library for reading and writing gif images";
-    platforms = stdenv.lib.platforms.all;
-    license = stdenv.lib.licenses.mit;
-    maintainers = with stdenv.lib.maintainers; [ fuuzetsu ];
+    license = licenses.mit;
+    maintainers = with stdenv.lib.maintainers; [
+      wkennington
+    ];
+    platforms = with platforms;
+      x86_64-linux;
     branch = "5.1";
   };
 }

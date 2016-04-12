9b7552e8b8648c17f1e590ca7472e3cf0f1bd57b
William A. Kennington III <william@wkennington.com>
2016-04-12 12:03:44 -0700

    libinput: 1.2.2 -> 1.2.3

--- pkgs/all-pkgs/libinput/default.nix
+++ pkgs/all-pkgs/libinput/default.nix
@@ -37,11 +37,12 @@ assert testsSupport ->
   && valgrind != null;
 
 stdenv.mkDerivation rec {
-  name = "libinput-1.2.2";
+  name = "libinput-1.2.3";
 
   src = fetchurl {
     url = "https://www.freedesktop.org/software/libinput/${name}.tar.xz";
-    sha256 = "08ac5b3620d86df1cdb4970c4137c8746c9f4b9f0c8e7dfab49858dbceb8f367";
+    allowHashOutput = false;
+    sha256 = "09b2a9d3a0cc973a9d3fdc7af40162cbef0b16be0a2e415786f83761f519e9f2";
   };
 
   buildInputs = [
@@ -66,6 +67,16 @@ stdenv.mkDerivation rec {
     (enFlag "event-gui" eventGUISupport null)
     (enFlag "tests" testsSupport null)
   ];
+
+  passthru = {
+    srcVerified = fetchurl {
+      failEarly = false;
+      pgpsigUrls = map (n: "${n}.sig") src.urls;
+      pgpKeyId = "B467F0BF";
+      pgpKeyFingerprint = "3C2C 43D9 447D 5938 EF45  51EB E23B 7E70 B467 F0BF";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "Library to handle input devices";

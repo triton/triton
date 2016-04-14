a299eff1a296c331c5da647e0ec38e4762c44490
Cody W. Opel <codyopel@gmail.com>
2016-04-14 12:19:34 -0400

    glibmm: 2.48.0 -> 2.48.1

--- pkgs/all-pkgs/glibmm/default.nix
+++ pkgs/all-pkgs/glibmm/default.nix
@@ -8,12 +8,13 @@
 stdenv.mkDerivation rec {
   name = "glibmm-${version}";
   versionMajor = "2.48";
-  versionMinor = "0";
+  versionMinor = "1";
   version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/glibmm/${versionMajor}/${name}.tar.xz";
-    sha256 = "b531392b03a34b8d0bd86d8a70510e7812fccfad47bd43eeff9e8a16d8438b85";
+    sha256Url = "mirror://gnome/sources/glibmm/${versionMajor}/${name}.sha256sum";
+    sha256 = "dc225f7d2f466479766332483ea78f82dc349d59399d30c00de50e5073157cdf";
   };
 
   buildInputs = [

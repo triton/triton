83bc548604fac8a31b81645a9e44831f3efd6d9e
Cody W. Opel <codyopel@gmail.com>
2016-04-14 12:15:37 -0400

    gtksourceview: 3.20.0 -> 3.20.1

--- pkgs/all-pkgs/gtksourceview/default.nix
+++ pkgs/all-pkgs/gtksourceview/default.nix
@@ -24,21 +24,14 @@ in
 stdenv.mkDerivation rec {
   name = "gtksourceview-${version}";
   versionMajor = "3.20";
-  versionMinor = "0";
+  versionMinor = "1";
   version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gtksourceview/${versionMajor}/${name}.tar.xz";
-    sha256 = "0b753cbd49cdfb0fb8bb347e4669dcc233d9c05c1d321429efe93c4e885262fd";
+    sha256Url = "mirror://gnome/sources/gtksourceview/${versionMajor}/${name}.sha256sum";
+    sha256 = "322d95c0eaba768a47956ccf1af732ea9377ab7a8afc145b1a34a04012ae8c4c";
   };
-
-  patches = [
-    (fetchTritonPatch {
-      rev = "453aedffa95d1c459a15a6f1fb8cb9d0ce810803";
-      file = "gtksourceview/nix_share_path.patch";
-      sha256 = "522655ce1664afef805040f2068094fdf57e3283a4cc2bdf52b33cb6de9fbe00";
-    })
-  ];
 
   nativeBuildInputs = [
     gettext
@@ -56,6 +49,14 @@ stdenv.mkDerivation rec {
     perl
     gobject-introspection
     vala
+  ];
+
+  patches = [
+    (fetchTritonPatch {
+      rev = "453aedffa95d1c459a15a6f1fb8cb9d0ce810803";
+      file = "gtksourceview/nix_share_path.patch";
+      sha256 = "522655ce1664afef805040f2068094fdf57e3283a4cc2bdf52b33cb6de9fbe00";
+    })
   ];
 
   configureFlags = [

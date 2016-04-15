61b0423014910586c852a70f396c48e32e65e59b
Cody W. Opel <codyopel@gmail.com>
2016-04-15 08:54:53 -0400

    revert 'atom: 1.6.2 -> 1.7.0' & refactor

--- pkgs/all-pkgs/atom/default.nix
+++ pkgs/all-pkgs/atom/default.nix
@@ -5,6 +5,7 @@
 
 , alsa-lib
 , atk
+, bzip2
 , cairo
 , cups
 , dbus
@@ -19,6 +20,8 @@
 , libcap
 , libgnome-keyring
 , libgpg-error
+, libnotify
+, libpng
 , nspr
 , nss
 , pango
@@ -28,51 +31,17 @@
 }:
 
 let
-  atomEnv = buildEnv {
-    name = "env-atom";
-    paths = [
-      alsa-lib
-      atk
-      cairo
-      cups
-      dbus
-      expat
-      fontconfig
-      freetype
-      gconf
-      gdk-pixbuf-core
-      glib
-      gtk2
-      libcap
-      libgnome-keyring
-      libgpg-error
-      nspr
-      nss
-      pango
-      xorg.libX11
-      xorg.libXcomposite
-      xorg.libXcursor
-      xorg.libXdamage
-      xorg.libXext
-      xorg.libXfixes
-      xorg.libXi
-      xorg.libXrandr
-      xorg.libXrender
-      xorg.libXtst
-      stdenv.cc.cc
-      systemd_lib
-      zlib
-    ];
-  };
+  inherit (stdenv.lib)
+    makeLibraryPath;
 in
 
 stdenv.mkDerivation rec {
   name = "atom-${version}";
-  version = "1.7.0";
+  version = "1.6.2";
 
   src = fetchurl {
     url = "https://github.com/atom/atom/releases/download/v${version}/atom-amd64.deb";
-    sha256 = "09f1f1725e8d76d9dbf9886b34a00cb1521077d663fa7ae8447e44ce482d60a1";
+    sha256 = "36b1a5ff2fe30e82eed351ed610e58a8e601785494f3321f2596a9aa01bb82ce";
     name = "${name}.deb";
   };
 
@@ -80,9 +49,45 @@ stdenv.mkDerivation rec {
     makeWrapper
   ];
 
-  buildInputs = [
-    atomEnv
-    gvfs
+  atomLibs = makeLibraryPath [
+    alsa-lib
+    atk
+    bzip2
+    cairo
+    cups
+    dbus
+    expat
+    fontconfig
+    freetype
+    gconf
+    gdk-pixbuf-core
+    glib
+    gtk2
+    libcap
+    libgnome-keyring
+    libgpg-error
+    libnotify
+    libpng
+    nspr
+    nss
+    pango
+    xorg.libX11
+    xorg.libXau
+    xorg.libxcb
+    xorg.libXcomposite
+    xorg.libXcursor
+    xorg.libXdamage
+    xorg.libXdmcp
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXi
+    xorg.libXrandr
+    xorg.libXrender
+    xorg.libXtst
+    xorg.xcbutil
+    stdenv.cc.cc
+    systemd_lib
+    zlib
   ];
 
   unpackPhase = "true";
@@ -94,25 +99,33 @@ stdenv.mkDerivation rec {
   installPhase = ''
     mkdir -pv $out
 
-    ar p $src data.tar.gz | tar -C $out -xz ./usr
-
-    substituteInPlace $out/usr/share/applications/atom.desktop \
-      --replace /usr/share/atom $out/bin
-
+    ar p $src data.tar.gz | tar -C $out -xz
     mv -v $out/usr/* $out/
-    rm -rv $out/share/lintian
     rm -rv $out/usr/
 
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+    sed -i $out/share/applications/atom.desktop \
+      -e 's,/usr/share/atom,$out/share/atom,'
+
+    rm -rv $out/share/lintian
+  '';
+
+  preFixup = ''
+    patchelf \
+      --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+      --set-rpath "$out/share/atom:${atomLibs}" \
       $out/share/atom/atom
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+
+    patchelf \
+      --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+      --set-rpath "$out/share/atom:${atomLibs}" \
       $out/share/atom/resources/app/apm/bin/node
 
-    wrapProgram $out/bin/atom \
-      --prefix "LD_LIBRARY_PATH" : "${atomEnv}/lib:${atomEnv}/lib64" \
+    wrapProgram $out/share/atom/atom \
+      --prefix "LD_LIBRARY_PATH" : "${atomLibs}" \
       --prefix "PATH" : "${gvfs}/bin"
+
     wrapProgram $out/bin/apm \
-      --prefix "LD_LIBRARY_PATH" : "${atomEnv}/lib:${atomEnv}/lib64"
+      --prefix "LD_LIBRARY_PATH" : "${atomLibs}"
   '';
 
   meta = with stdenv.lib; {

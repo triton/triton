556a1d35888dcbe307ec88b3e9b3eb91e8bff9f3
Cody W. Opel <codyopel@gmail.com>
2016-04-16 04:02:13 -0400

    btsync: 2.3.1 -> 2.3.6

--- pkgs/all-pkgs/btsync/default.nix
+++ pkgs/all-pkgs/btsync/default.nix
@@ -4,64 +4,38 @@
 }:
 
 let
-  inherit (stdenv)
-    isFreeBSD
-    isLinux
-    system;
-  inherit (stdenv.lib.platforms)
-    x86_64-freebsd
-    x86_64-linux;
-in
-
-let
-  arch =
-    if (system == "x86_64-freebsd"
-       || system == "x86_64-linux") then
-      "x64"
-    else
-      null;
-  platform =
-    if isFreeBSD then
-      "freebsd"
-    else if isLinux then
-      "linux"
-    else
-      null;
-  libPath = stdenv.lib.makeLibraryPath [
-    stdenv.cc.libc
-  ];
+  inherit (stdenv.lib)
+    makeLibraryPath;
 in
 
 stdenv.mkDerivation rec {
   name = "btsync-${version}";
-  version = "2.3.1";
+  version = "2.3.6";
 
   src  = fetchurl {
-    url  = "https://download-cdn.getsyncapp.com/${version}/"
-         + "${platform}-${arch}/BitTorrent-Sync_${arch}.tar.gz";
-    sha256 =
-      if system == "x86_64-freebsd" then
-        "1ldhi0ydpxdbpd0ak5c3zv93wif5sqsgfj4ggav2b0djm76alxgb"
-      else if system == "x86_64-linux" then
-        "17dzyfy6jjzrsf923pmrxmg09nkbdhnq4afcia2d0c1yk6did610"
-      else
-        null;
+    url  = "https://download-cdn.getsync.com/${version}/"
+      + "linux-x64/BitTorrent-Sync_x64.tar.gz";
+    sha256 = "fac80d415aa44d9f2e027b56cb4fea1aa8770489cffc037b64fd05135fa4d907";
   };
 
   nativeBuildInputs = [
     patchelf
   ];
 
-  sourceRoot  = ".";
+  libPath = makeLibraryPath [
+    stdenv.cc.libc
+  ];
+
+  sourceRoot = ".";
 
   installPhase = ''
-    install -vD btsync $out/bin/btsync
+    install -D -m755 -v btsync $out/bin/btsync
   '';
 
   preFixup = ''
     patchelf \
       --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-      --set-rpath ${libPath} \
+      --set-rpath "${libPath}" \
       "$out/bin/btsync"
   '';
 
@@ -71,9 +45,10 @@ stdenv.mkDerivation rec {
     description = "Automatically sync files via secure, distributed technology";
     homepage = "http://www.bittorrent.com/sync";
     license = licenses.unfreeRedistributable;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [
+      codyopel
+    ];
     platforms = with platforms;
-      x86_64-freebsd
-      ++ x86_64-linux;
+      x86_64-linux;
   };
 }

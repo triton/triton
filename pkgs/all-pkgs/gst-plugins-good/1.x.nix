05a4bd7e59f150dbabee3fb45aee45c350aa3b93
Cody W. Opel <codyopel@gmail.com>
2016-04-21 14:03:03 -0400

    gst-plugins-good: 1.8.0 -> 1.8.1

--- pkgs/all-pkgs/gst-plugins-good/1.x.nix
+++ pkgs/all-pkgs/gst-plugins-good/1.x.nix
@@ -35,12 +35,12 @@ let
 in
 
 stdenv.mkDerivation rec {
-  name = "gst-plugins-good-1.8.0";
+  name = "gst-plugins-good-1.8.1";
 
   src = fetchurl rec {
     url = "https://gstreamer.freedesktop.org/src/gst-plugins-good/${name}.tar.xz";
     sha256Url = "${url}.sha256sum";
-    sha256 = "c20c134d47dbc238d921707a3b66da709c2b4dd89f9d267cec13d1ddf16e9f4d";
+    sha256 = "2103e17921d67894e82eafdd64fb9b06518599952fd93e625bfbc83ffead0972";
   };
 
   nativeBuildInputs = [
@@ -73,6 +73,7 @@ stdenv.mkDerivation rec {
     zlib
     xorg.libX11
     xorg.libXext
+    xorg.xextproto
   ];
 
   configureFlags = [

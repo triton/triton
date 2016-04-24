74e51476add42eff4809842888aeb45998b4cffa
William A. Kennington III <william@wkennington.com>
2016-04-24 05:42:03 -0700

    go: 1.6.1 -> 1.6.2

--- pkgs/all-pkgs/go/sources.nix
+++ pkgs/all-pkgs/go/sources.nix
@@ -1,7 +1,7 @@
 {
   "1.6" = {
-    version = "1.6.1";
-    sha256 = "1d4b53cdee51b2298afcf50926a7fa44b286f0bf24ff8323ce690a66daa7193f";
+    version = "1.6.2";
+    sha256 = "787b0b750d037016a30c6ed05a8a70a91b2e9db4bd9b1a2453aa502a63f1bccc";
     sha256Bootstrap = {
       "x86_64-linux" = "5470eac05d273c74ff8bac7bef5bad0b5abbd1c4052efbdbc8db45332e836b0b";
     };

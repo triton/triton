526da5abbfd3fe1959b28f0a6df8bb254226cefc
William A. Kennington III <william@wkennington.com>
2016-04-28 17:50:49 -0700

    fuse: 2.9.5 -> 2.9.6

--- pkgs/all-pkgs/fuse/default.nix
+++ pkgs/all-pkgs/fuse/default.nix
@@ -3,11 +3,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "fuse-2.9.5";
+  name = "fuse-2.9.6";
   
   src = fetchurl {
-    url = "https://github.com/libfuse/libfuse/releases/download/${stdenv.lib.replaceStrings ["-" "."] ["_" "_"] name}/${name}.tar.gz";
-    sha256 = "579f371cc5ffc1afca7057512bf7d52988a9ede57859a7c55e5b9f72435cdbb5";
+    url = "https://github.com/libfuse/libfuse/releases/download/${name}/${name}.tar.gz";
+    allowHashOutput = false;
+    sha256 = "0cb48bbc7106632d15a0c6bb582496c9884b869e329428891fe075a9e916f16b";
   };
 
   preConfigure = ''
@@ -24,7 +25,16 @@ stdenv.mkDerivation rec {
   configureFlags = [
     "--disable-kernel-module"
   ];
-  
+
+  passthru = {
+    srcVerified = fetchurl {
+      failEarly = true;
+      pgpsigUrls = map (n: "${n}.asc") src.urls;
+      pgpKeyFingerprint = "ED31 791B 2C5C 1613 AF38  8B8A D113 FCAC 3C4E 599F";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
+
   meta = with stdenv.lib; {
     homepage = http://fuse.sourceforge.net/;
     description = "Kernel module and library that allows filesystems to be implemented in user space";

35e9b2a6d1ab3064c57cfbd77a8a36a284b3dca7
William A. Kennington III <william@wkennington.com>
2016-04-28 23:59:27 -0700

    linux-testing: 4.5-rc7 -> 4.6-rc5

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.5-rc7";
+  version = "4.6-rc5";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/testing/linux-${version}.tar.xz";
-    sha256 = "0z43s7ccikmqigv4insjvizs3bkx2lgjvzsz5rmmpcga28dz44kq";
+    sha256 = "6df3dcaa5a673cb0cb65e4e4abb32bb84be67aa3332325d677db47ba02f6259d";
   };
 
   features.iwlwifi = true;

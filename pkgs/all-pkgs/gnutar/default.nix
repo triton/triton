b227bdf79215a420e3f193aa560313ee06dbd4cd
William A. Kennington III <william@wkennington.com>
2016-04-30 18:48:48 -0700

    tar: Support clamping mtime

--- pkgs/all-pkgs/gnutar/default.nix
+++ pkgs/all-pkgs/gnutar/default.nix
@@ -1,4 +1,5 @@
 { stdenv
+, fetchTritonPatch
 , fetchurl
 }:
 
@@ -10,6 +11,14 @@ stdenv.mkDerivation rec {
     url = "mirror://gnu/tar/tar-${version}.tar.bz2";
     sha256 = "0qkm2k9w8z91hwj8rffpjj9v1vhpiriwz4cdj36k9vrgc3hbzr30";
   };
+
+  patches = [
+    (fetchTritonPatch {
+      rev = "58bea0bf6cf5a05014ad13bb8c914b137f87422f";
+      file = "gnutar/add-clamp-mtime.patch";
+      sha256 = "6bcde813d36fed0a0d5cfffb2d715d53b85d4876c050aae672cda2e190c25c87";
+    })
+  ];
 
   meta = with stdenv.lib; {
     homepage = http://www.gnu.org/software/tar/;

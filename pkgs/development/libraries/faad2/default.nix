c46a1c2fd2ada4908bd96820513efdb12a16ea62
William A. Kennington III <william@wkennington.com>
2016-05-06 14:44:32 -0700

    faad2: Refactor

--- pkgs/development/libraries/faad2/default.nix
+++ pkgs/development/libraries/faad2/default.nix
@@ -1,25 +1,28 @@
-{stdenv, fetchurl
+{ stdenv
+, fetchurl
 , drmSupport ? false # Digital Radio Mondiale
 }:
 
-with stdenv.lib;
 stdenv.mkDerivation rec {
-  name = "faad2-${version}";
-  version = "2.7";
+  name = "faad2-2.7";
 
   src = fetchurl {
     url = "mirror://sourceforge/faac/${name}.tar.bz2";
     sha256 = "1db37ydb6mxhshbayvirm5vz6j361bjim4nkpwjyhmy4ddfinmhl";
   };
 
-  configureFlags = []
-    ++ optional drmSupport "--with-drm";
+  configureFlags = [
+    (if drmSupport then "--with-drm" else null)
+  ];
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "An open source MPEG-4 and MPEG-2 AAC decoder";
     homepage    = http://www.audiocoding.com/faad2.html;
     license     = licenses.gpl2;
-    maintainers = with maintainers; [ codyopel ];
-    platforms   = platforms.all;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms   = with platforms;
+      x86_64-linux;
   };
 }

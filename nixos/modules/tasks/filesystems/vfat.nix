97b73f4dc325232b1176945c2118a23942b314f7
William A. Kennington III <william@wkennington.com>
2016-05-10 00:58:36 -0700

    vfat: Fix

--- nixos/modules/tasks/filesystems/vfat.nix
+++ nixos/modules/tasks/filesystems/vfat.nix
@@ -11,15 +11,19 @@ in
 {
   config = mkIf (any (fs: fs == "vfat") config.boot.supportedFilesystems) {
 
-    system.fsPackages = [ pkgs.dosfstools ];
+    system.fsPackages = [
+      pkgs.dosfstools
+    ];
 
-    boot.initrd.kernelModules = mkIf inInitrd [ "vfat" "nls_cp437" "nls_iso8859-1" ];
+    boot.initrd.kernelModules = mkIf inInitrd [
+      "nls_cp437"
+      "nls_iso8859-1"
+      "vfat"
+    ];
 
-    boot.initrd.extraUtilsCommands = mkIf inInitrd
-      ''
-        copy_bin_and_libs ${pkgs.dosfstools}/sbin/dosfsck
-        ln -sv dosfsck $out/bin/fsck.vfat
-      '';
-
+    boot.initrd.extraUtilsCommands = mkIf inInitrd ''
+      copy_bin_and_libs ${pkgs.dosfstools}/bin/fsck.fat
+      ln -sv fsck.fat $out/bin/fsck.vfat
+    '';
   };
 }

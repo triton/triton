f5061cec07fca3c90097f0237d897c862d701c5c
William A. Kennington III <william@wkennington.com>
2016-05-13 13:59:55 -0700

    Fix default error message

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ let
   requiredVersion = import ./lib/minver.nix;
 in
   if ! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.nixVersion == 1 then
-    abort "This version of Triton requires Nix >= ${requiredVersion}, please upgrade! See https://nixos.org/wiki/How_to_update_when_Nix_is_too_old_to_evaluate_Nixpkgs"
+    abort "This version of Triton requires Nix >= ${requiredVersion}, please upgrade!"
   else
     import ./pkgs/top-level/all-packages.nix {
       inherit targetSystem hostSystem config;

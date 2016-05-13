cc8dbe66ac1e0324a86effba8c3206d41f92ce82
William A. Kennington III <william@wkennington.com>
2016-05-13 13:22:05 -0700

    ibfilezilla: 0.5.0 -> 0.5.1

--- pkgs/all-pkgs/libfilezilla/default.nix
+++ pkgs/all-pkgs/libfilezilla/default.nix
@@ -4,11 +4,12 @@
 
 stdenv.mkDerivation rec {
   name = "libfilezilla-${version}";
-  version = "0.5.0";
+  version = "0.5.1";
 
   src = fetchurl {
     url = "mirror://sourceforge/project/filezilla/libfilezilla/${version}/${name}.tar.bz2";
-    sha256 = "8c6a1af13113bbb78e1c66ebbbffa84c0f0ee243c0789e9b92f8e11fcb84c51d";
+    multihash = "QmfK7LkjptmaAEqKX6UgFFhHRKb5hSgh11rFR97Tjkk5GV";
+    sha256 = "585b58eecc43f3803a3f1b1ee69dce7b57bae5a49d85514044a7c95da299b7f9";
   };
 
   meta = with stdenv.lib; {

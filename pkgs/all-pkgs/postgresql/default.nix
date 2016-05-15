a24eb22a3fe9f70db065361bc4d7b937754bfe9e
William A. Kennington III <william@wkennington.com>
2016-05-15 02:08:40 -0700

    postgresql: Updates

--- pkgs/all-pkgs/postgresql/default.nix
+++ pkgs/all-pkgs/postgresql/default.nix
@@ -31,28 +31,28 @@ in
 let
   sources = {
     "9.1" = {
-      version = "9.1.21";
-      sha256 = "d66ee9076f3149a4cab4be3c6f47e13bc047138d17dfcb531ad01f78cfdeb393";
+      version = "9.1.22";
+      sha256 = "f619664b0dde4e1a75fdc00c35afb4517002984a462d70967ffcdedfeee5e16e";
     };
 
     "9.2" = {
-      version = "9.2.16";
-      sha256 = "d2164cb8706bf828161871c195299841c5be1fbd9bc85d7866704e54f0741b11";
+      version = "9.2.17";
+      sha256 = "c660cc0ee42c221ebedc2c75ad0d4b30ec8da488a954df9987a3fc83bcb7363f";
     };
 
     "9.3" = {
-      version = "9.3.12";
-      sha256 = "f3339ea23f86d07f3730adc878b2e5d433087ff44aad65a5ec9c22c32b112e67";
+      version = "9.3.13";
+      sha256 = "5544e1d29bfdb9a815d3533400ae242b8763c399285e5d4020ffdb49c362a72b";
     };
 
     "9.4" = {
-      version = "9.4.7";
-      sha256 = "cc795e6c35b30e697e5891e3056376af685f848a4e67fab1702e74a2385f81e0";
+      version = "9.4.8";
+      sha256 = "4a10640e180e0d9adb587bc25a82dcce6bf507b033637e7fb9d4eeffa33a6b4c";
     };
 
     "9.5" = {
-      version = "9.5.2";
-      sha256 = "f8d132e464506b551ef498719f18cfe9d777709c7a1589dc360afc0b20e47c41";
+      version = "9.5.3";
+      sha256 = "7385c01dc58acba8d7ac4e6ad42782bd7c0b59272862a3a3d5fe378d4503a0b4";
     };
   };
 

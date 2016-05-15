61dc0e1e06973f9d07f5e9f01e3924b65cc886ef
William A. Kennington III <william@wkennington.com>
2016-05-15 01:58:49 -0700

    libevdev: 1.4.5 -> 1.5.0

--- pkgs/development/libraries/libevdev/default.nix
+++ pkgs/development/libraries/libevdev/default.nix
@@ -1,20 +1,43 @@
-{ stdenv, fetchurl, python }:
+{ stdenv
+, fetchurl
+
+, python
+}:
 
 stdenv.mkDerivation rec {
-  name = "libevdev-1.4.5";
+  name = "libevdev-1.5.0";
 
   src = fetchurl {
-    url = "http://www.freedesktop.org/software/libevdev/${name}.tar.xz";
-    sha256 = "07faqb47c7sjl25rc788cbslyiv5ijky0jc4g6312qz0hv55h779";
+    url = "https://www.freedesktop.org/software/libevdev/${name}.tar.xz";
+    allowHashOutput = false;
+    sha256 = "8c80cbc4b0a0b0c347867c6d03a5ef58a64b0dec52d0c725e279226c9ab442fc";
   };
 
-  buildInputs = [ python ];
+  buildInputs = [
+    python
+  ];
+
+  configureFlags = [
+    "--disable-silent"
+  ];
+
+  passthru = {
+    srcVerified = fetchurl {
+      failEarly = true;
+      pgpsigUrls = map (n: "${n}.sig") src.urls;
+      pgpKeyFingerprint = "3C2C 43D9 447D 5938 EF45  51EB E23B 7E70 B467 F0BF";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "Wrapper library for evdev devices";
     homepage = http://www.freedesktop.org/software/libevdev/doc/latest/index.html;
     license = licenses.mit;
-    platforms = platforms.linux;
-    maintainers = [ maintainers.amorsillo ];
+    maintainers = with stdenv.lib; [
+      wkennington
+    ];
+    platforms = with platforms;
+      x86_64-linux;
   };
 }

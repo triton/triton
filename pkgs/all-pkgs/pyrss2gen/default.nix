2a4563942a8d632e569295a57c9a534379675753
Cody W. Opel <codyopel@gmail.com>
2016-05-15 18:35:23 -0400

    pyrss2gen: enable check phase

--- pkgs/all-pkgs/pyrss2gen/default.nix
+++ pkgs/all-pkgs/pyrss2gen/default.nix
@@ -13,6 +13,8 @@ buildPythonPackage rec {
     sha256 = "7960aed7e998d2482bf58716c316509786f596426f879b05f8d84e98b82c6ee7";
   };
 
+  doCheck = true;
+
   meta = with stdenv.lib; {
     description = "Generate RSS2 using a Python data structure";
     homepage = http://www.dalkescientific.om/Python/PyRSS2Gen.html;

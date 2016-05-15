9bbc616e0b94b31b43c39b4eb894737943707c82
Cody W. Opel <codyopel@gmail.com>
2016-05-15 18:35:47 -0400

    pynzb: enable check phase

--- pkgs/all-pkgs/pynzb/default.nix
+++ pkgs/all-pkgs/pynzb/default.nix
@@ -13,6 +13,8 @@ buildPythonPackage rec {
     sha256 = "0735b3889a1174bbb65418ee503629d3f5e4a63f04b16f46ffba18253ec3ef17";
   };
 
+  doCheck = true;
+
   meta = with stdenv.lib; {
     description = "Unified API for parsing NZB files";
     homepage = http://github.com/ericflo/pynzb;

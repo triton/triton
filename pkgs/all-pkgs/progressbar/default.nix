38bc1ee7cce523bca6603e076db75f016b86ce16
Cody W. Opel <codyopel@gmail.com>
2016-05-16 15:56:51 -0400

    progressbar: fix python 3 compatibility

--- pkgs/all-pkgs/progressbar/default.nix
+++ pkgs/all-pkgs/progressbar/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , buildPythonPackage
 , fetchPyPi
+, fetchTritonPatch
 }:
 
 buildPythonPackage rec {
@@ -12,6 +13,15 @@ buildPythonPackage rec {
     inherit version;
     sha256 = "b2d38a729785149e65323381d2e6fca0a5e9615a6d8bcf10bfa8adedfc481254";
   };
+
+  patches = [
+    # Remove format as a slot attribute, not compatible with python 3.3+
+    (fetchTritonPatch {
+      rev = "1c4e4c62aaf32cfaf7164c34c805e8d550fa8020";
+      file = "progressbar/progressbar-2.3-python3.3.patch";
+      sha256 = "0f414aeb9605c892472c49af588079e2cbc922f985f0d6a5dd6a3a77c4c7e837";
+    })
+  ];
 
   doCheck = true;
 

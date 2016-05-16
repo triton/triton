f65ee1552cc7a87b78932f1d40cc94179af1b253
William A. Kennington III <william@wkennington.com>
2016-05-16 11:51:28 -0700

    iotop: Add multihash

--- pkgs/all-pkgs/iotop/default.nix
+++ pkgs/all-pkgs/iotop/default.nix
@@ -8,6 +8,7 @@ buildPythonPackage rec {
 
   src = fetchurl {
     url = "http://guichaz.free.fr/iotop/files/${name}.tar.bz2";
+    multihash = "QmPSxAB17WKR9qcgg4jvYpMfaDUoJwaytHuJ7bbpsNGyJc";
     sha256 = "0nzprs6zqax0cwq8h7hnszdl3d2m4c2d4vjfxfxbnjfs9sia5pis";
   };
 

1bf7e1576ee16d101ae3f35348c075e6abe02997
Cody W. Opel <codyopel@gmail.com>
2016-05-16 01:30:27 -0400

    zope-event: enable check phase

--- pkgs/all-pkgs/zope-event/default.nix
+++ pkgs/all-pkgs/zope-event/default.nix
@@ -13,6 +13,8 @@ buildPythonPackage rec {
     sha256 = "ce11004217863a4827ea1a67a31730bddab9073832bdb3b9be85869259118758";
   };
 
+  doCheck = true;
+
   meta = with stdenv.lib; {
     description = "An event publishing system";
     homepage = https://pypi.python.org/pypi/zope.event;

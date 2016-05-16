451c1cc77fac49c5bf99f09513ece362bb453a16
Cody W. Opel <codyopel@gmail.com>
2016-05-16 01:16:56 -0400

    backports-abc: enable check phase

--- pkgs/all-pkgs/backports-abc/default.nix
+++ pkgs/all-pkgs/backports-abc/default.nix
@@ -13,6 +13,8 @@ buildPythonPackage rec {
     sha256 = "8b3e4092ba3d541c7a2f9b7d0d9c0275b21c6a01c53a61c731eba6686939d0a5";
   };
 
+  doCheck = true;
+
   meta = with stdenv.lib; {
     description = "Backport of recent additions to the 'collections.abc' module";
     homepage = https://github.com/cython/backports_abc;

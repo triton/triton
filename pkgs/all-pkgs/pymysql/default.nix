f972fb4a9149e1511e99edf49aca3be6b3c92282
Cody W. Opel <codyopel@gmail.com>
2016-05-16 01:23:45 -0400

    pymysql: enable check phase

--- pkgs/all-pkgs/pymysql/default.nix
+++ pkgs/all-pkgs/pymysql/default.nix
@@ -13,6 +13,8 @@ buildPythonPackage rec {
     sha256 = "bd7acb4990dbf097fae3417641f93e25c690e01ed25c3ed32ea638d6c3ac04ba";
   };
 
+  doCheck = true;
+
   meta = with stdenv.lib; {
     description = "Pure Python MySQL Driver";
     homepage = https://github.com/PyMySQL/PyMySQL/;

8c53cf9c0faaa5433bc3cb3cd95e36054f0813f9
Cody W. Opel <codyopel@gmail.com>
2016-05-16 01:16:21 -0400

    babelfish: enable check phase

--- pkgs/all-pkgs/babelfish/default.nix
+++ pkgs/all-pkgs/babelfish/default.nix
@@ -13,6 +13,8 @@ buildPythonPackage rec {
     sha256 = "8380879fa51164ac54a3e393f83c4551a275f03617f54a99d70151358e444104";
   };
 
+  doCheck = true;
+
   meta = with stdenv.lib; {
     description = "A module to work with countries and languages";
     homepage = https://github.com/Diaoul/babelfish;

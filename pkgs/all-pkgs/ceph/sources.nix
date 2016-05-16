656d7ebbd2ec20e41c650ee75388371a26c15858
William A. Kennington III <william@wkennington.com>
2016-05-16 13:19:02 -0700

    ceph: Updates

--- pkgs/all-pkgs/ceph/sources.nix
+++ pkgs/all-pkgs/ceph/sources.nix
@@ -1,8 +1,8 @@
 {
   "0.94" = rec {
-    version = "0.94.6";
+    version = "0.94.7";
     rev = "refs/tags/v${version}";
-    sha256 = "1w7skpzds7mqx8zaxvwhq51w70pif6dwppaarippw320sar3yg68";
+    sha256 = "0ciidr7s0r2ig6i68y7qi3s2ypvn5spkdxh559k87yawp8yxcf5i";
   };
 
   "9" = rec {
@@ -12,20 +12,20 @@
   };
 
   "10" = rec {
-    version = "10.2.0";
+    version = "10.2.1";
     rev = "refs/tags/v${version}";
-    sha256 = "08yjgskn8rm556kkrn25h5di87yrqswpliihv2yzrsa6v2pbkwvi";
+    sha256 = "03874gbd3sc0pmc6jmqkpvw4ls0jy7x4dh6zk21852jnrqrknw35";
   };
 
   "dev" = rec {
-    version = "10.2.0";
+    version = "10.2.1";
     rev = "refs/tags/v${version}";
-    sha256 = "08yjgskn8rm556kkrn25h5di87yrqswpliihv2yzrsa6v2pbkwvi";
+    sha256 = "03874gbd3sc0pmc6jmqkpvw4ls0jy7x4dh6zk21852jnrqrknw35";
   };
 
   "git" = rec {
     version = "2016-05-06";
-    rev = "646a6a28c31c379ecd7a37eaa0e0641d1a93c728";
-    sha256 = "0if89lq59kn8yjbxbch0ar90kmjv47hb3f4riwfhdfp1z3si2ysm";
+    rev = "8980f840780445fbb6ed28944147811fd0d8a05c";
+    sha256 = "1viqlxb7fsrgkzy4axsddpsbbmrcnjlq46b9p74cx7gjl099cjyi";
   };
 }

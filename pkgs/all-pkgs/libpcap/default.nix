0726ddea0de4aed56bcd6de6a65fea1fdc46df1e
William A. Kennington III <william@wkennington.com>
2016-05-16 17:44:47 -0700

    libpcap: Add signature verification

--- pkgs/all-pkgs/libpcap/default.nix
+++ pkgs/all-pkgs/libpcap/default.nix
@@ -13,7 +13,8 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "http://www.tcpdump.org/release/${name}.tar.gz";
-    sha256 = "1c28ykkizd7jqgzrfkg7ivqjlqs9p6lygp26bsw2i0z8hwhi3lvs";
+    allowHashOutput = false;
+    sha256 = "7ad3112187e88328b85e46dce7a9b949632af18ee74d97ffc3f2b41fe7f448b0";
   };
 
   nativeBuildInputs = [
@@ -42,6 +43,15 @@ stdenv.mkDerivation rec {
   preBuild = ''
     export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I$(echo "${libnl}"/include/*)"
   '';
+
+  passthru = {
+    srcVerified = fetchurl rec {
+      failEarly = true;
+      pgpsigUrl = map (n: "${n}.sig") src.urls;
+      pgpKeyFingerprint = "1F16 6A57 42AB B9E0 249A  8D30 E089 DEF1 D9C1 5D0D";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
 
   meta = with stdenv.lib; {
     homepage = http://www.tcpdump.org;

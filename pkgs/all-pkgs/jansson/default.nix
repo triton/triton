57d19ea7bb49a3cba1885e791d6dea416dbf3cff
William A. Kennington III <william@wkennington.com>
2016-05-16 17:20:32 -0700

    jansson: Fix CVE-2016-4425

--- pkgs/all-pkgs/jansson/default.nix
+++ pkgs/all-pkgs/jansson/default.nix
@@ -1,4 +1,5 @@
 { stdenv
+, fetchTritonPatch
 , fetchurl
 }:
 
@@ -9,6 +10,14 @@ stdenv.mkDerivation rec {
     url = "http://www.digip.org/jansson/releases/${name}.tar.gz";
     sha256 = "1mvq9p85khsl818i4vbszyfab0fd45mdrwrxjkzw05mk1xcyc1br";
   };
+
+  patches = [
+    (fetchTritonPatch {
+      rev = "5bb60ada3e49df19daba41242380880a0ff718c5";
+      file = "jansson/CVE-2016-4425.patch";
+      sha256 = "99207e08752ee7627b33a0dda608b8e342794f51314838839102f8f331224a9b";
+    })
+  ];
 
   meta = with stdenv.lib; {
     homepage = "http://www.digip.org/jansson/";

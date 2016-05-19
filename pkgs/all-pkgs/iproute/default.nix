4eeab85af4fa1280567c0863cf04c18aaf387920
William A. Kennington III <william@wkennington.com>
2016-05-18 17:11:07 -0700

    iproute: 4.5.0 -> 4.6.0

--- pkgs/all-pkgs/iproute/default.nix
+++ pkgs/all-pkgs/iproute/default.nix
@@ -7,12 +7,19 @@
 , iptables
 }:
 
+let
+  version = "4.6.0";
+  tarballUrls = [
+    "mirror://kernel/linux/utils/net/iproute2/iproute2-${version}.tar"
+  ];
+in
 stdenv.mkDerivation rec {
-  name = "iproute2-4.5.0";
+  name = "iproute2-${version}";
 
   src = fetchurl {
-    url = "mirror://kernel/linux/utils/net/iproute2/${name}.tar.xz";
-    sha256 = "0jj9phsi8m2sbnz7bbh9cf9vckm67hs62ab5srdwnrg4acpjj59z";
+    urls = map (n: "${n}.xz") tarballUrls;
+    allowHashOutput = false;
+    sha256 = "74fc6a8ad085be095a159f8158bbaf385b42af9e101619f233f1ae466829d406";
   };
 
   nativeBuildInputs = [
@@ -33,9 +40,8 @@ stdenv.mkDerivation rec {
   preBuild = ''
     makeFlagsArray+=(
       "DESTDIR="
-      "LIBDIR=$out/lib"
+      "PREFIX=$out"
       "SBINDIR=$out/bin"
-      "MANDIR=$out/share/man"
     )
     buildFlagsArray+=(
       "CONFDIR=/etc/iproute"
@@ -46,6 +52,16 @@ stdenv.mkDerivation rec {
       "DOCDIR=$TMPDIR"
     )
   '';
+
+  passthru = {
+    srcVerified = fetchurl {
+      failEarly = true;
+      pgpsigUrls = map (n: "${n}.sign") tarballUrls;
+      pgpDecompress = true;
+      pgpKeyFingerprint = "9F6F C345 B05B E7E7 66B8  3C8F 80A7 7F60 95CD E47E";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
 
   meta = with stdenv.lib; {
     homepage = http://www.linuxfoundation.org/collaborate/workgroups/networking/iproute2;

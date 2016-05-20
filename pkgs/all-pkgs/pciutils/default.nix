3c9c9d3b99c7ea18b33dd04198a66f5f6d638a71
William A. Kennington III <william@wkennington.com>
2016-05-20 12:48:51 -0700

    pciutils: 3.4.1 -> 3.5.0

--- pkgs/all-pkgs/pciutils/default.nix
+++ pkgs/all-pkgs/pciutils/default.nix
@@ -6,12 +6,20 @@
 , zlib
 }:
 
+let
+  name = "pciutils-3.5.0"; # with database from 2016-01
+  
+  tarballUrls = [
+    "mirror://kernel/software/utils/pciutils/${name}.tar"
+  ];
+in
 stdenv.mkDerivation rec {
-  name = "pciutils-3.4.1"; # with database from 2016-01
+  inherit name;
 
   src = fetchurl {
-    url = "mirror://kernel/software/utils/pciutils/${name}.tar.xz";
-    sha256 = "0am8hiv435h2dayclnkdk8qjlpj08m4djf6sv15n9l84av658mc6";
+    urls = map (n: "${n}.xz") tarballUrls;
+    allowHashOutput = true;
+    sha256 = "0ee5e2b4727bede6873b12000ed7e10e3e1273b6fc414152148c694a3ca0ce85";
   };
 
   nativeBuildInputs = [
@@ -40,6 +48,16 @@ stdenv.mkDerivation rec {
     rm $out/sbin/update-pciids $out/man/man8/update-pciids.8
   '';
 
+  passthru = {
+    srcVerified = fetchurl {
+      failEarly = true;
+      pgpsigUrls = map (n: "${n}.sign") tarballUrls;
+      pgpDecompress = true;
+      pgpKeyFingerprint = "5558 F939 9CD7 8368 5055  3C6E C28E 7847 ED70 F82D";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
+
   meta = with stdenv.lib; {
     homepage = http://mj.ucw.cz/pciutils.html;
     description = "A collection of programs for inspecting and manipulating configuration of PCI devices";
@@ -48,8 +66,7 @@ stdenv.mkDerivation rec {
       wkennington
     ];
     platforms = with platforms;
-      i686-linux
-      ++ x86_64-linux;
+      x86_64-linux;
   };
 }
 

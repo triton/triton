e060bed8ea4fd7edde81dd127e41cc4c2e2a556c
William A. Kennington III <william@wkennington.com>
2016-05-23 01:52:41 -0700

    gnutar: Add check

--- pkgs/all-pkgs/gnutar/default.nix
+++ pkgs/all-pkgs/gnutar/default.nix
@@ -5,12 +5,17 @@
 
 let
   version = "1.29";
+
+  tarballUrls = version: [
+    "mirror://gnu/tar/tar-${version}.tar.bz2"
+  ];
 in
 stdenv.mkDerivation rec {
   name = "gnutar-${version}";
 
   src = fetchurl {
-    url = "mirror://gnu/tar/tar-${version}.tar.bz2";
+    urls = tarballUrls version;
+    allowHashOutput = false;
     sha256 = "236b11190c0a3a6885bdb8d61424f2b36a5872869aa3f7f695dea4b4843ae2f2";
   };
 
@@ -21,6 +26,17 @@ stdenv.mkDerivation rec {
       sha256 = "5b8a6c325cdaf83588fb87778328b47978db33230c44f24b4a909bc9306d2d86";
     })
   ];
+
+  passthru = {
+    srcVerified = fetchurl rec {
+      failEarly = true;
+      urls = tarballUrls "1.29";
+      pgpsigUrls = map (n: "${n}.sig") urls;
+      pgpKeyFingerprint = "325F 650C 4C2B 6AD5 8807  327A 3602 B07F 55D0 C732";
+      inherit (src) outputHashAlgo;
+      outputHash = "236b11190c0a3a6885bdb8d61424f2b36a5872869aa3f7f695dea4b4843ae2f2";
+    };
+  };
 
   meta = with stdenv.lib; {
     homepage = http://www.gnu.org/software/tar/;

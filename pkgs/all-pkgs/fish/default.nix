8c6612684e7b357bc340cdd7290599e2e75dab5d
William A. Kennington III <william@wkennington.com>
2016-05-24 13:22:02 -0700

    fish: 2.2.0 -> 2.3.0

--- pkgs/all-pkgs/fish/default.nix
+++ pkgs/all-pkgs/fish/default.nix
@@ -1,62 +1,43 @@
-{ stdenv, fetchurl, ncurses, net-tools, python, which, groff, gettext, man_db,
-  bc, coreutils, gnused, kbd }:
+{ stdenv
+, fetchurl
+, gettext
 
+, ncurses
+, pcre2
+}:
+
+let
+  version = "2.3.0";
+in
 stdenv.mkDerivation rec {
   name = "fish-${version}";
-  version = "2.2.0";
-
-  patches = [ ./etc_config.patch ./builtin_status.patch ./command-not-found.patch ];
 
   src = fetchurl {
     url = "http://fishshell.com/files/${version}/${name}.tar.gz";
-    sha256 = "0ympqz7llmf0hafxwglykplw6j5cz82yhlrw50lw4bnf2kykjqx7";
+    sha256 = "1ralmp7lavdl0plc09ppm232aqsn0crxx6m3hgaa06ibam3sqawi";
   };
 
-  buildInputs = [ ncurses ];
+  nativeBuildInputs = [
+    gettext
+  ];
 
-  # Required binaries during execution
-  # Python: Autocompletion generated from manpages and config editing
-  propagatedBuildInputs = [ python which groff gettext ]
-                          ++ stdenv.lib.optional true man_db
-                          ++ [ bc coreutils ];
+  buildInputs = [
+    ncurses
+    pcre2
+  ];
 
-  postInstall = ''
-    sed -e "s|expr|${coreutils}/bin/expr|" \
-        -e "s|if which unicode_start|if true|" \
-        -e "s|unicode_start|${kbd}/bin/unicode_start|" \
-        -i "$out/etc/fish/config.fish"
-    sed -e "s|bc|${bc}/bin/bc|" \
-        -e "s|/usr/bin/seq|${coreutils}/bin/seq|" \
-        -i "$out/share/fish/functions/seq.fish" \
-           "$out/share/fish/functions/math.fish"
-    sed -i "s|which |${which}/bin/which |" "$out/share/fish/functions/type.fish"
-    sed -e "s|\|cut|\|${coreutils}/bin/cut|" -i "$out/share/fish/functions/fish_prompt.fish"
-    sed -i "s|nroff |${groff}/bin/nroff |" "$out/share/fish/functions/__fish_print_help.fish"
-    sed -e "s|gettext |${gettext}/bin/gettext |" \
-        -e "s|which |${which}/bin/which |" \
-        -i "$out/share/fish/functions/_.fish"
-    sed -e "s|uname|${coreutils}/bin/uname|" \
-        -i "$out/share/fish/functions/__fish_pwd.fish" \
-           "$out/share/fish/functions/prompt_pwd.fish"
-    sed -e "s|sed |${gnused}/bin/sed |" \
-        -i "$out/share/fish/functions/alias.fish" \
-           "$out/share/fish/functions/prompt_pwd.fish"
-    substituteInPlace "$out/share/fish/functions/fish_default_key_bindings.fish" \
-      --replace "clear;" "${ncurses}/bin/clear;"
-  '' + stdenv.lib.optionalString true ''
-    sed -i "s|(hostname\||(${net-tools}/bin/hostname\||" "$out/share/fish/functions/fish_prompt.fish"
-    sed -i "s|Popen(\['manpath'|Popen(\['${man_db}/bin/manpath'|" "$out/share/fish/tools/create_manpage_completions.py"
-    sed -i "s|command manpath|command ${man_db}/bin/manpath|" "$out/share/fish/functions/man.fish"
-  '' + ''
-    sed -i "s|/sbin /usr/sbin||" \
-           "$out/share/fish/functions/__fish_complete_subcommand_root.fish"
-  '';
+  configureFlags = [
+    "--without-included-pcre2"
+  ];
 
   meta = with stdenv.lib; {
     description = "Smart and user-friendly command line shell";
     homepage = "http://fishshell.com/";
     license = licenses.gpl2;
-    platforms = platforms.all;
-    maintainers = with maintainers; [ ocharles ];
+    maintainers = with maintainers; [
+      wkennington
+    ];
+    platforms = with platforms;
+      x86_64-linux;
   };
 }

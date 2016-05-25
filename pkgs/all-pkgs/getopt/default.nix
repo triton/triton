c91c7441208bdb054e0ceeb9be76bde586913f1d
William A. Kennington III <william@wkennington.com>
2016-05-25 03:08:49 -0700

    getopt: Add multihash

--- pkgs/all-pkgs/getopt/default.nix
+++ pkgs/all-pkgs/getopt/default.nix
@@ -8,6 +8,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "http://frodo.looijaard.name/system/files/software/getopt/${name}.tar.gz";
+    multihash = "Qmck3cYDdP3V66u59i6ioYLBwCiS3uB3gR6PJd8i48Gw2N";
     sha256 = "1zn5kp8ar853rin0ay2j3p17blxy16agpp8wi8wfg4x98b31vgyh";
   };
 

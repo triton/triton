9b20de2b3166e2bf904f53dccc3a1161dfaa16eb
William A. Kennington III <william@wkennington.com>
2016-05-27 03:09:04 -0700

    zip: Fix hosting

--- pkgs/all-pkgs/zip/default.nix
+++ pkgs/all-pkgs/zip/default.nix
@@ -6,10 +6,8 @@ stdenv.mkDerivation {
   name = "zip-3.0";
 
   src = fetchurl {
-    urls = [
-      ftp://ftp.info-zip.org/pub/infozip/src/zip30.tgz
-      http://pkgs.fedoraproject.org/repo/pkgs/zip/zip30.tar.gz/7b74551e63f8ee6aab6fbc86676c0d37/zip30.tar.gz
-    ];
+    name = "zip30.tgz";
+    multihash = "QmdEzR5wvn7fEm5GGeksoxgitrNdo6pkwK68cjzmomqCmL";
     sha256 = "0sb3h3067pzf3a7mlxn1hikpcjrsvycjcnj9hl9b1c3ykcgvps7h";
   };
 

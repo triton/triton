77c80db55b67af31cb273290d3786895d6156b5c
William A. Kennington III <william@wkennington.com>
2016-05-27 03:25:52 -0700

    SDL1: Fix hashes

--- pkgs/all-pkgs/SDL_1/default.nix
+++ pkgs/all-pkgs/SDL_1/default.nix
@@ -61,33 +61,33 @@ stdenv.mkDerivation rec {
     (fetchpatch {
       name = "fix_window_resizing.diff";
       url = "https://bugs.debian.org/cgi-bin/bugreport.cgi?msg=10;filename=fix_window_resizing.diff;att=2;bug=665779";
-      sha256 = "1z35azc73vvi19pzi6byck31132a8w1vzrghp1x3hy4a4f9z4gc6";
+      sha256 = "a96e0d86e47388a7b4166bcd93f11a52952d4e1fede164c4c4431698cfd68f7e";
     })
     # Fix drops of keyboard events for SDL_EnableUNICODE
     (fetchpatch {
-      url = "http://hg.libsdl.org/SDL/raw-rev/0aade9c0203f";
-      sha256 = "1y9izncjlqvk1mkz1pkl9lrk9s452cmg2izjjlqqrhbn8279xy50";
+      url = "https://hg.libsdl.org/SDL/raw-rev/0aade9c0203f";
+      sha256 = "4a6f7c2a10bcbf4170ecfb2c05759a40f1a2da25a2e2c242d78f02cf14feace4";
     })
     # Ignore insane joystick axis events
     (fetchpatch {
-      url = "http://hg.libsdl.org/SDL/raw-rev/95abff7adcc2";
-      sha256 = "0i8x0kx0pw12ld5bfxhyzs466y3c0n9dscw1ijhq1b96r72xyhqq";
+      url = "https://hg.libsdl.org/SDL/raw-rev/95abff7adcc2";
+      sha256 = "7fd95da12539c7ecd467c4dff56e2729dbd223c2f6708b9ba952710bf6f9c6e7";
     })
     # Workaround X11 bug to allow changing gamma
     # Ticket: https://bugs.freedesktop.org/show_bug.cgi?id=27222
     (fetchpatch {
       url = "http://pkgs.fedoraproject.org/cgit/rpms/SDL.git/plain/SDL-1.2.15-x11-Bypass-SetGammaRamp-when-changing-gamma.patch?id=04a3a7b1bd88c2d5502292fad27e0e02d084698d";
-      sha256 = "0x52s4328kilyq43i7psqkqg7chsfwh0aawr50j566nzd7j51dlv";
+      sha256 = "df238d51889a722c5a51469bd545ec05e51fc130db1df0ea61526e801c9ab6f7";
     })
     # Fix a build failure on OS X Mavericks
     # Ticket: https://bugzilla.libsdl.org/show_bug.cgi?id=2085
     (fetchpatch {
-      url = "http://hg.libsdl.org/SDL/raw-rev/e9466ead70e5";
-      sha256 = "0mpwdi09h89df2wxqw87m1rdz7pr46k0w6alk691k8kwv970z6pl";
+      url = "https://hg.libsdl.org/SDL/raw-rev/e9466ead70e5";
+      sha256 = "2d6be18730e013ec3ebf3088f5bb8289a837dc3d7b11b14a1fa737d4710cd26c";
     })
     (fetchpatch {
-      url = "http://hg.libsdl.org/SDL/raw-rev/bbfb41c13a87";
-      sha256 = "1336g7waaf1c8yhkz11xbs500h8bmvabh4h437ax8l1xdwcppfxv";
+      url = "https://hg.libsdl.org/SDL/raw-rev/bbfb41c13a87";
+      sha256 = "45a554b10913960364da1724c28dd8355afaaa3248a2d33664d5d7e28220f3a5";
     })
   ];
 

1fd0269d2800147a88ddd10bf8652835ac5cbf6b
William A. Kennington III <william@wkennington.com>
2016-05-28 19:32:13 -0700

    postgresql: Refactor out sources

---
+++ pkgs/all-pkgs/postgresql/sources.nix
@@ -0,0 +1,26 @@
+{
+  "9.1" = {
+    version = "9.1.22";
+    sha256 = "f619664b0dde4e1a75fdc00c35afb4517002984a462d70967ffcdedfeee5e16e";
+  };
+
+  "9.2" = {
+    version = "9.2.17";
+    sha256 = "c660cc0ee42c221ebedc2c75ad0d4b30ec8da488a954df9987a3fc83bcb7363f";
+  };
+
+  "9.3" = {
+    version = "9.3.13";
+    sha256 = "5544e1d29bfdb9a815d3533400ae242b8763c399285e5d4020ffdb49c362a72b";
+  };
+
+  "9.4" = {
+    version = "9.4.8";
+    sha256 = "4a10640e180e0d9adb587bc25a82dcce6bf507b033637e7fb9d4eeffa33a6b4c";
+  };
+
+  "9.5" = {
+    version = "9.5.3";
+    sha256 = "7385c01dc58acba8d7ac4e6ad42782bd7c0b59272862a3a3d5fe378d4503a0b4";
+  };
+}

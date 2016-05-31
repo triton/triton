04e82798705d935e3b40db802d95b88fd272f3d3
William A. Kennington III <william@wkennington.com>
2016-05-31 14:43:21 -0700

    nginx: 1.10.0 -> 1.10.1 && 1.10.0 -> 1.11.1

--- pkgs/all-pkgs/nginx/sources.nix
+++ pkgs/all-pkgs/nginx/sources.nix
@@ -1,11 +1,11 @@
 {
   "stable" = {
-    version = "1.10.0";
-    sha256 = "8ed647c3dd65bc4ced03b0e0f6bf9e633eff6b01bac772bcf97077d58bc2be4d";
+    version = "1.10.1";
+    sha256 = "1fd35846566485e03c0e318989561c135c598323ff349c503a6c14826487a801";
   };
 
   "unstable" = {
-    version = "1.10.0";
-    sha256 = "8ed647c3dd65bc4ced03b0e0f6bf9e633eff6b01bac772bcf97077d58bc2be4d";
+    version = "1.11.1";
+    sha256 = "5d8dd0197e3ffeb427729c045382182fb28db8e045c635221b2e0e6722821ad0";
   };
 }

e7301f1ea8c3ee900e853dd96ecb72944766ebb9
William A. Kennington III <william@wkennington.com>
2016-06-03 02:23:58 -0700

    enum34: 1.1.5 ->1.1.6

--- pkgs/all-pkgs/enum34/default.nix
+++ pkgs/all-pkgs/enum34/default.nix
@@ -12,12 +12,12 @@ in
 
 buildPythonPackage rec {
   name = "enum34-${version}";
-  version = "1.1.5";
+  version = "1.1.6";
 
   src = fetchPyPi {
     package = "enum34";
     inherit version;
-    sha256 = "35cf09a65db802269a76b7df60f548940575579a0e8a00ec45294995d28b0862";
+    sha256 = "8ad8c4783bf61ded74527bffb48ed9b54166685e4230386a9ed9b1279e2df5b1";
   };
 
   # Python 3.4 Enum backported to 2.4 through 3.3

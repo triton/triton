b29457f00ef96b637c79e1ea630fa97a946a301e
William A. Kennington III <william@wkennington.com>
2016-06-04 15:05:56 -0700

    bleach: Move to all-pkgs

---
+++ pkgs/all-pkgs/bleach/default.nix
@@ -0,0 +1,33 @@
+{ stdenv
+, buildPythonPackage
+, fetchPyPi
+
+, html5lib
+, six
+}:
+
+let
+  version = "1.4.3";
+in
+buildPythonPackage {
+  name = "bleach-${version}";
+
+  src = fetchPyPi {
+    package = "bleach";
+    inherit version;
+    sha256 = "1293061adb5a9eebb7b260516e691785ac08cc1646c8976aeda7db9dbb1c6f4b";
+  };
+
+  buildInputs = [
+    html5lib
+    six
+  ];
+
+  meta = with stdenv.lib; {
+    maintainers = with maintainers; [
+      wkennington
+    ];
+    platforms = with platforms;
+      x86_64-linux;
+  };
+}

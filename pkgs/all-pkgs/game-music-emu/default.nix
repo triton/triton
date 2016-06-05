62ce665156b6d41420d8570449c3caab4e87d3f3
Cody W. Opel <codyopel@gmail.com>
2016-06-05 00:02:08 -0400

    game-music-emu: add a working mirror

--- pkgs/all-pkgs/game-music-emu/default.nix
+++ pkgs/all-pkgs/game-music-emu/default.nix
@@ -8,8 +8,10 @@ stdenv.mkDerivation rec {
   name = "game-music-emu-0.6.0";
 
   src = fetchurl {
-    url = "https://bitbucket.org/mpyne/game-music-emu/downloads/"
-        + "${name}.tar.bz2";
+    urls = [
+      "https://bitbucket.org/mpyne/game-music-emu/downloads/${name}.tar.bz2"
+      "mirror://gentoo/distfiles/${name}.tar.bz2"
+    ];
     sha256 = "11s9l938nxbrk7qb2k1ppfgizcz00cakbxgv0gajc6hyqv882vjh";
   };
 

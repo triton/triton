cd331c8cebee316c3e12ea916ede1085e7ad672d
William A. Kennington III <william@wkennington.com>
2016-06-06 12:36:21 -0700

    ntp: 4.2.8p7 -> 4.2.8p8

--- pkgs/all-pkgs/ntp/default.nix
+++ pkgs/all-pkgs/ntp/default.nix
@@ -14,7 +14,7 @@
 let
   major = "4";
   minor = "2";
-  patch = "8p7";
+  patch = "8p8";
 in
 stdenv.mkDerivation rec {
   name = "ntp-${major}.${minor}.${patch}";
@@ -22,7 +22,7 @@ stdenv.mkDerivation rec {
   src = fetchurl rec {
     url = "http://archive.ntp.org/ntp${major}/ntp-${major}.${minor}/${name}.tar.gz";
     md5Url = "${url}.md5";
-    sha256 = "81d20c06a0b01abe3b84fac092185bf014252d38fe5e7b2758f604680a0220dc";
+    sha256 = "2ab3d0b5f0456e6311dda1cc27ab75da108762773a19e46abd938bd9407b97ee";
   };
 
   nativeBuildInputs = [

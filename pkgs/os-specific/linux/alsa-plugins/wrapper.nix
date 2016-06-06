0775a07cb01d2bdc2cabbca2cda14d03eca36a03
Cody W. Opel <codyopel@gmail.com>
2016-06-06 00:19:25 -0400

    alsa-plugins-wrapper: fix references

--- pkgs/os-specific/linux/alsa-plugins/wrapper.nix
+++ pkgs/os-specific/linux/alsa-plugins/wrapper.nix
@@ -1,5 +1,5 @@
-{ writeScriptBin, stdenv, alsaPlugins }:
+{ writeScriptBin, stdenv, alsa-plugins }:
 writeScriptBin "ap${if stdenv.system == "i686-linux" then "32" else "64"}" ''
   #/bin/sh
-  ALSA_PLUGIN_DIRS=${alsaPlugins}/lib/alsa-lib "$@"
+  ALSA_PLUGIN_DIRS=${alsa-plugins}/lib/alsa-lib "$@"
 ''

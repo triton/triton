9bb0b31b7acd0cc5f288f314699f92b2d08b3585
William A. Kennington III <william@wkennington.com>
2016-06-10 07:21:28 -0700

    libjpeg-turbo: Add multihashes

--- pkgs/all-pkgs/libjpeg-turbo/sources.nix
+++ pkgs/all-pkgs/libjpeg-turbo/sources.nix
@@ -1,10 +1,12 @@
 {
   "1.4" = {
     versionPatch = "2";
+    multihash = "QmQ3xFNuKUC3agZbqH18S36vSMhRMR5Wa5EAXTyBd242qb;
     sha256 = "521bb5d3043e7ac063ce3026d9a59cc2ab2e9636c655a2515af5f4706122233e";
   };
   "1.5" = {
     versionPatch = "0";
+    multihash = "QmSxCbhUxJAjxjXSqQiuqtP3MX6TAD5VjSdraXVfk1WqGW";
     sha256 = "9f397c31a67d2b00ee37597da25898b03eb282ccd87b135a50a69993b6a2035f";
   };
 }

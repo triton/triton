df58ec49750ab7104cf911612dbf14e536743529
Cody W. Opel <codyopel@gmail.com>
2016-06-12 01:57:34 -0400

    jemalloc: 4.2.0 -> 4.2.1

--- pkgs/all-pkgs/jemalloc/default.nix
+++ pkgs/all-pkgs/jemalloc/default.nix
@@ -3,14 +3,15 @@
 }:
 
 let
-  version = "4.2.0";
+  version = "4.2.1";
 in
 stdenv.mkDerivation rec {
   name = "jemalloc-${version}";
 
   src = fetchurl {
-    url = "https://github.com/jemalloc/jemalloc/releases/download/${version}/${name}.tar.bz2";
-    sha256 = "b216ddaeb901697fe38bd30ea02d7505a4b60e8979092009f95cfda860d46acb";
+    url = "https://github.com/jemalloc/jemalloc/releases/download/${version}/"
+      + "${name}.tar.bz2";
+    sha256 = "5630650d5c1caab95d2f0898de4fe5ab8519dc680b04963b38bb425ef6a42d57";
   };
 
   dontDisableStatic = true;

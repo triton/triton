a47b3b64c1473f0cd6a996e2d40f5aa779f7903e
William A. Kennington III <william@wkennington.com>
2016-06-13 13:55:09 -0700

    expat: Fix CVE's

--- pkgs/all-pkgs/expat/default.nix
+++ pkgs/all-pkgs/expat/default.nix
@@ -13,13 +13,18 @@ stdenv.mkDerivation rec {
 
   patches = [
     (fetchTritonPatch {
-      rev = "182777a92c9373d4679204b64b72f7d2176345ec";
+      rev = "c1ae48fae1345bbe69e123f206602014d00db3fc";
       file = "expat/CVE-2015-1283-refix.patch";
       sha256 = "ccbb54a31b49864316f65c3dbe5b571efd1c6e003826bdcc4f55976bebc6f5a9";
     })
     (fetchTritonPatch {
-      rev = "c24fdfce944f8ff9651503fdaa267f938f546368";
-      file = "expat/CVE-2016-0718.patch";
+      rev = "c1ae48fae1345bbe69e123f206602014d00db3fc";
+      file = "expat/CVE-2012-6702-plus-cve-2016-5300-v1.patch";
+      sha256 = "85c2a24a722b11ebb6e0d01a97a21e763a4db5b8c0f23a5682540af5ae4d6733";
+    })
+    (fetchTritonPatch {
+      rev = "c1ae48fae1345bbe69e123f206602014d00db3fc";
+      file = "expat/CVE-2016-0718-v2-2-1.patch";
       sha256 = "3f5dab75aa8e947bcddc6ccb57ecf129341ba6aaac3511acd7842adb0c1f7ed6";
     })
 	];

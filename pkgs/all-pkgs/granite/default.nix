a1f94e79cda369f283a71dde545dafa2380173d5
Cody W. Opel <codyopel@gmail.com>
2016-06-13 05:22:26 -0400

    granite: 0.3.1 -> 0.4.0

--- pkgs/all-pkgs/granite/default.nix
+++ pkgs/all-pkgs/granite/default.nix
@@ -16,16 +16,18 @@
 , vala
 }:
 
+let
+  channel = "0.4";
+  version = "${channel}";
+in
+
 stdenv.mkDerivation rec {
   name = "granite-${version}";
-  versionMajor = "0.3";
-  versionMinor = "1";
-  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "https://code.launchpad.net/granite/${versionMajor}/${version}/" +
-           "+download/${name}.tar.xz";
-    sha256 = "1inyq9qhayzg1kl7nc6i275k9yqdicl23rs5lyrz2xdsk8gxdhcf";
+    url = "https://launchpad.net/granite/${channel}/loki-alpha1/"
+      + "+download/${name}.tar.xz";
+    sha256 = "73066f393d4c0aa637046e6fa0d6130a2612f277b9e493366f2f99267d85c5d9";
   };
 
   nativeBuildInputs = [
@@ -47,10 +49,12 @@ stdenv.mkDerivation rec {
     vala
   ];
 
-  cmakeFlags = [
-    "-DINTROSPECTION_GIRDIR=\${out}/share/gir-1.0/"
-    "-DINTROSPECTION_TYPELIBDIR=\${out}/lib/girepository-1.0"
-  ];
+  preConfigure = ''
+    cmakeFlagsArray=(
+      "-DINTROSPECTION_GIRDIR=$out/share/gir-1.0/"
+      "-DINTROSPECTION_TYPELIBDIR=$out/lib/girepository-1.0"
+    )
+  '';
 
   meta = with stdenv.lib; {
     description = "An extension to GTK+ used by elementary OS";
@@ -60,7 +64,6 @@ stdenv.mkDerivation rec {
       codyopel
     ];
     platforms = with platforms;
-      i686-linux
-      ++ x86_64-linux;
+      x86_64-linux;
   };
 }

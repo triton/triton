f70e529f03d49fab9fb4384d0ea68f8b712bd546
William A. Kennington III <william@wkennington.com>
2016-06-20 14:30:20 -0700

    dhcpcd: 6.11.0 -> 6.11.1

--- pkgs/all-pkgs/dhcpcd/default.nix
+++ pkgs/all-pkgs/dhcpcd/default.nix
@@ -5,11 +5,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "dhcpcd-6.11.0";
+  name = "dhcpcd-6.11.1";
 
   src = fetchurl {
     url = "mirror://roy/dhcpcd/${name}.tar.xz";
-    sha256 = "0ncrk7a3ypxjyjmwwhzsa02ffhmrqms4y175gx11rwapfqrcvhii";
+    multihash = "Qmd4kPu25tjbTZ4U1ScnTAMYpaaNj84g9rW2dWnpHHdYua";
+    sha256 = "1vn6m242ss4plhrs62wyi4j16zv5457calgc2qym3myigrh3v0jw";
   };
 
   buildInputs = [

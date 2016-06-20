07406c43433134bf838bbc5c621c322ba0a30c27
William A. Kennington III <william@wkennington.com>
2016-06-19 17:26:03 -0700

    nano: 2.5.3 -> 2.6.0

--- pkgs/all-pkgs/nano/default.nix
+++ pkgs/all-pkgs/nano/default.nix
@@ -5,12 +5,22 @@
 , ncurses
 }:
 
+let
+  major = "2.6";
+  patch = "0";
+
+  version = "${major}.${patch}";
+in
 stdenv.mkDerivation rec {
-  name = "nano-2.5.3";
+  name = "nano-${version}";
 
   src = fetchurl {
-    url = "mirror://gnu/nano/${name}.tar.gz";
-    sha256 = "1vhjrcydcfxqq1719vcsvqqnbjbq2523m00dhzag5vwzkc961c5j";
+    urls = [
+      "https://www.nano-editor.org/dist/v${major}/${name}.tar.gz"
+      "mirror://gnu/nano/${name}.tar.gz"
+    ];
+    allowHashOutput = false;
+    sha256 = "d517a5fcaa9fa6ceda8a63e22be1d83dd7f4af13703c73b10a48e95a6e1178d3";
   };
 
   nativeBuildInputs = [
@@ -26,6 +36,15 @@ stdenv.mkDerivation rec {
     "--enable-nls"
     "--disable-tiny"
   ];
+
+  passthru = {
+    srcVerified = fetchurl {
+      failEarly = true;
+      pgpsigUrls = map (n: "${n}.asc") src.urls;
+      pgpKeyFingerprint = "A7F6 A64A 67DA 09EF 9278  2DD7 9DF4 862A F117 5C5B ";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
 
   meta = with stdenv.lib; {
     homepage = http://www.nano-editor.org/;

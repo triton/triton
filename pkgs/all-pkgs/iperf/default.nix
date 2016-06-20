0893a339ff384014278f15a46ae3fd09886988e0
William A. Kennington III <william@wkennington.com>
2016-06-20 14:31:56 -0700

    iperf: Add multihashes

--- pkgs/all-pkgs/iperf/default.nix
+++ pkgs/all-pkgs/iperf/default.nix
@@ -8,10 +8,12 @@ let
   sources = {
     "2" = {
       version = "2.0.9";
+      multihash = "QmVEsHmgJrz1vJM9oGaM5oStWsxYC19JyCW4QAUVQWvVKM";
       sha256 = "a5350777b191e910334d3a107b5e5219b72ffa393da4186da1e0a4552aeeded6";
     };
     "3" = {
       version = "3.1.3";
+      multihash = "QmPLdT2JXCwsVPv6Fhr2ruSFfRPB5FBydjUWJ63EsabNBT";
       sha256 = "e34cf60cffc80aa1322d2c3a9b81e662c2576d2b03e53ddf1079615634e6f553";
     };
   };
@@ -30,7 +32,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     name = "${name}.tar.gz";
     url = "https://iperf.fr/download/source/${name}-source.tar.gz";
-    inherit sha256;
+    inherit multihash sha256;
   };
 
   postInstall = optionalString (channel == "3") ''

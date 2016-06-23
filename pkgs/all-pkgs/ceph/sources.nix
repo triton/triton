3f98defef6cf29d6046faabf2eefd7010a1927cd
William A. Kennington III <william@wkennington.com>
2016-06-22 19:08:53 -0700

    ceph: Hash fixes

--- pkgs/all-pkgs/ceph/sources.nix
+++ pkgs/all-pkgs/ceph/sources.nix
@@ -2,25 +2,25 @@
   "0.94" = rec {
     version = "0.94.7";
     rev = "refs/tags/v${version}";
-    sha256 = "0ciidr7s0r2ig6i68y7qi3s2ypvn5spkdxh559k87yawp8yxcf5i";
+    sha256 = "0p17c23v5y26ixm4b2ijifqn8d2rg6k301ng2vw81x6va9v6kg00";
   };
 
   "9" = rec {
     version = "9.2.1";
     rev = "refs/tags/v${version}";
-    sha256 = "1bfn0f9923q77lgydg1zg9i6s2y8r0m9l66jacfppgdfbgn2swv5";
+    sha256 = "09nhvm50sk22pawdq9vf43xigpyqp7wl694la0bx2081dinskj71";
   };
 
   "10" = rec {
     version = "10.2.2";
     rev = "refs/tags/v${version}";
-    sha256 = "1h837z7cp7l5957959m7nnmq33rb06i722a7wigrpp2r1d5kaihp";
+    sha256 = "0kzjsahpv55hh0mk2vf81bjhpkyfcq1wbmzc0cf79mn8ps4244sm";
   };
 
   "dev" = rec {
     version = "10.2.2";
     rev = "refs/tags/v${version}";
-    sha256 = "1h837z7cp7l5957959m7nnmq33rb06i722a7wigrpp2r1d5kaihp";
+    sha256 = "0kzjsahpv55hh0mk2vf81bjhpkyfcq1wbmzc0cf79mn8ps4244sm";
   };
 
   "git" = rec {

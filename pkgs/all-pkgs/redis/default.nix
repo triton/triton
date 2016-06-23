1ec43dd74194a954c05c40320a885d2426283574
William A. Kennington III <william@wkennington.com>
2016-06-22 22:27:53 -0700

    redis: 3.2.0 -> 3.2.1

--- pkgs/all-pkgs/redis/default.nix
+++ pkgs/all-pkgs/redis/default.nix
@@ -5,15 +5,15 @@
 }:
 
 let
-  version = "3.2.0";
+  version = "3.2.1";
 in
 stdenv.mkDerivation rec {
   name = "redis-${version}";
 
   src = fetchurl {
     url = "http://download.redis.io/releases/${name}.tar.gz";
-    sha1Confirm = "0c1820931094369c8cc19fc1be62f598bc5961ca";
-    sha256 = "989f1af3dc0ac1828fdac48cd6c608f5a32a235046dddf823226f760c0fd8762";
+    sha1Confirm = "26c0fc282369121b4e278523fce122910b65fbbf";
+    sha256 = "df7bfb7b527d99981eba3912ae22703764eb19adda1357818188b22fdd09d5c9";
   };
 
   preBuild = ''

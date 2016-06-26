fa9101fcfde01036c44c5980d6c36ed6e820b0fa
Cody W. Opel <codyopel@gmail.com>
2016-06-26 00:36:30 -0400

    mpv: 0.17.0 -> 0.18.0

--- pkgs/all-pkgs/mpv/default.nix
+++ pkgs/all-pkgs/mpv/default.nix
@@ -50,11 +50,11 @@ in
 
 stdenv.mkDerivation rec {
   name = "mpv-${version}";
-  version = "0.17.0";
+  version = "0.18.0";
 
   src = fetchurl {
     url = "https://github.com/mpv-player/mpv/archive/v${version}.tar.gz";
-    sha256 = "602cd2b0f5fc7e43473234fbb96e3f7bbb6418f15eb8fa720d9433cce31eba6e";
+    sha256 = "b656638d4f6bce2621baaacb60d8be384aa492fcd86dfd43996aaa2c16fee02b";
   };
 
   nativeBuildInputs = [

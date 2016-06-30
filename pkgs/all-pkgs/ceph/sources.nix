60bba0312d684126f40149380d71e40bf94e8c8d
William A. Kennington III <william@wkennington.com>
2016-06-30 15:43:11 -0700

    ceph-dev: 10.2.2 -> 11.0.0 and 2016-06-27 -> 2016-06-30

--- pkgs/all-pkgs/ceph/sources.nix
+++ pkgs/all-pkgs/ceph/sources.nix
@@ -18,14 +18,14 @@
   };
 
   "dev" = rec {
-    version = "10.2.2";
+    version = "11.0.0";
     rev = "refs/tags/v${version}";
-    sha256 = "0kzjsahpv55hh0mk2vf81bjhpkyfcq1wbmzc0cf79mn8ps4244sm";
+    sha256 = "1is9jdd9clb6cvdxcqbx9fxkyd0kjjgvnm0irw7yzzsb6zpryb4m";
   };
 
   "git" = rec {
-    version = "2016-06-27";
-    rev = "2cd0475ff94171f0b4d187638873802af2fabc4b";
-    sha256 = "1qs5jxii406fgm40l39js1hiik789vqzrrxi97ivcd5n5xxs62ks";
+    version = "2016-06-30";
+    rev = "85bb43e111692989d2296a389ce45377d2297d6f";
+    sha256 = "1gg854z35jw7kn5v04wsxidgwzjbsvcv6wkh231axn3k9ys8c96m";
   };
 }

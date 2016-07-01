83cfb27ff2594b1b625b126aaf336d7a9438d48c
William A. Kennington III <william@wkennington.com>
2016-06-30 17:48:46 -0700

    zfs: 2016-05-12 -> 2016-06-29

--- pkgs/os-specific/linux/zfs/git.nix
+++ pkgs/os-specific/linux/zfs/git.nix
@@ -1,13 +1,13 @@
 { callPackage, stdenv, fetchFromGitHub, spl_git, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "2016-05-12";
+  version = "2016-06-29";
 
   src = fetchFromGitHub {
     owner = "zfsonlinux";
     repo = "zfs";
-    rev = "a9bb2b682785d48b4bcdaca9d382fc87bbf6e2fb";
-    sha256 = "e0c8bee918f48841a960ffa1456a1f1ec04d6e1ba802abb271cf7c47c9e0861e";
+    rev = "5c27b296055301f13103ca0aa98c2ded01dcd9a0";
+    sha256 = "f6602aa6ed752cb7ca819453af5995c1bf6cf4c26d8330c6dc8a9e49d58ccbd7";
   };
 
   patches = [ ./nix-build.patch ];

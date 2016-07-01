fdca2b0ed67f72110a796658df8e8f904e4d34a5
William A. Kennington III <william@wkennington.com>
2016-06-30 17:48:35 -0700

    spl: 2016-05-12 -> 2016-06-21

--- pkgs/os-specific/linux/spl/git.nix
+++ pkgs/os-specific/linux/spl/git.nix
@@ -1,13 +1,13 @@
 { callPackage, fetchFromGitHub, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "2016-05-12";
+  version = "2016-06-21";
 
   src = fetchFromGitHub {
     owner = "zfsonlinux";
     repo = "spl";
-    rev = "39cd90ef08bb6817dd57ac08e9de5c87af2681ed";
-    sha256 = "03cce697b12fb472475cd019bc41c67b99a606fefd1195f5e614b15f35d39bae";
+    rev = "5ad98ad0978d43b41180018536ce5efdaa4ea546";
+    sha256 = "4ee328238bbb410fb2c12da8c37cbf89d25edb11d1e5e55e5e2d5154f7e0f142";
   };
 
   patches = [ ./patches.patch ];

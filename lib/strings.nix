16e477e05bfbe7d6fe3ba3c4c81ecaf45a41207a
Cody W. Opel <codyopel@gmail.com>
2016-07-09 22:43:22 -0400

    lib/strings.nix: fix formatting

--- lib/strings.nix
+++ lib/strings.nix
@@ -1,15 +1,20 @@
 /* String manipulation functions. */
 
-let lib = import ./default.nix;
-
-inherit (builtins) length;
-
+let
+  lib = import ./default.nix;
+  inherit (builtins)
+    length;
 in
 
 rec {
 
-  inherit (builtins) stringLength substring head tail isString replaceStrings;
-
+  inherit (builtins)
+    head
+    isString
+    replaceStrings
+    stringLength
+    substring
+    tail;
 
   # Concatenate a list of strings.
   concatStrings =
@@ -18,19 +23,17 @@ rec {
     else
       lib.foldl' (x: y: x + y) "";
 
-
   # Map a function over a list and concatenate the resulting strings.
   concatMapStrings = f: list: concatStrings (map f list);
   concatImapStrings = f: list: concatStrings (lib.imap f list);
 
-
   # Place an element between each element of a list, e.g.,
   # `intersperse "," ["a" "b" "c"]' returns ["a" "," "b" "," "c"].
   intersperse = separator: list:
-    if list == [] || length list == 1
-    then list
-    else tail (lib.concatMap (x: [separator x]) list);
-
+    if list == [] || length list == 1 then
+      list
+    else
+      tail (lib.concatMap (x: [ separator x ]) list);
 
   # Concatenate a list of strings with a separator between each element, e.g.
   # concatStringsSep " " ["foo" "bar" "xyzzy"] == "foo bar xyzzy"
@@ -39,7 +42,6 @@ rec {
 
   concatMapStringsSep = sep: f: list: concatStringsSep sep (map f list);
   concatImapStringsSep = sep: f: list: concatStringsSep sep (lib.imap f list);
-
 
   # Construct a Unix-style search path consisting of each `subDir"
   # directory of the given list of packages.  For example,
@@ -59,9 +61,9 @@ rec {
     let
       lenStr = stringLength str;
       lenSuff = stringLength suff;
-    in lenStr >= lenSuff &&
-       substring (lenStr - lenSuff) lenStr str == suff;
-
+    in
+    lenStr >= lenSuff
+    && substring (lenStr - lenSuff) lenStr str == suff;
 
   # Convert a string to a list of characters (i.e. singleton strings).
   # For instance, "abc" becomes ["a" "b" "c"].  This allows you to,
@@ -72,7 +74,6 @@ rec {
   stringToCharacters = s:
     map (p: substring p 1 s) (lib.range 0 (stringLength s - 1));
 
-
   # Manipulate a string charactter by character and replace them by
   # strings before concatenating the results.
   stringAsChars = f: s:
@@ -80,16 +81,13 @@ rec {
       map f (stringToCharacters s)
     );
 
-
   # Escape occurrence of the elements of ‘list’ in ‘string’ by
   # prefixing it with a backslash. For example, ‘escape ["(" ")"]
   # "(foo)"’ returns the string ‘\(foo\)’.
   escape = list: replaceChars list (map (c: "\\${c}") list);
 
-
   # Escape all characters that have special meaning in the Bourne shell.
   escapeShellArg = lib.escape (stringToCharacters "\\ ';$`()|<>\t*[]");
-
 
   # Obsolete - use replaceStrings instead.
   replaceChars = builtins.replaceStrings or (
@@ -97,14 +95,15 @@ rec {
     let
       substList = lib.zipLists del new;
       subst = c:
-        let found = lib.findFirst (sub: sub.fst == c) null substList; in
+        let
+          found = lib.findFirst (sub: sub.fst == c) null substList;
+        in
         if found == null then
           c
         else
           found.snd;
     in
-      stringAsChars subst s);
-
+    stringAsChars subst s);
 
   # Case conversion utilities.
   lowerChars = stringToCharacters "abcdefghijklmnopqrstuvwxyz";
@@ -112,10 +111,8 @@ rec {
   toLower = replaceChars upperChars lowerChars;
   toUpper = replaceChars lowerChars upperChars;
 
-
   # Appends string context from another string.
   addContextFrom = a: b: substring 0 0 a + b;
-
 
   # Cut a string with a separator and produces a list of strings which
   # were separated by this separator; e.g., `splitString "."
@@ -131,18 +128,21 @@ rec {
         substring startAt sepLen s == sep;
 
       recurse = index: startAt:
-        let cutUntil = i: [(substring startAt (i - startAt) s)]; in
+        let
+          cutUntil = i: [ (substring startAt (i - startAt) s) ];
+        in
         if index < lastSearch then
           if startWithSep index then
-            let restartAt = index + sepLen; in
+            let
+              restartAt = index + sepLen;
+            in
             cutUntil index ++ recurse restartAt restartAt
           else
             recurse (index + 1) startAt
         else
           cutUntil sLen;
     in
-      recurse 0 0;
-
+    recurse 0 0;
 
   # return the suffix of the second argument if the first argument match its
   # prefix. e.g.,
@@ -152,28 +152,26 @@ rec {
       preLen = stringLength pre;
       sLen = stringLength s;
     in
-      if hasPrefix pre s then
-        substring preLen (sLen - preLen) s
-      else
-        s;
+    if hasPrefix pre s then
+      substring preLen (sLen - preLen) s
+    else
+      s;
 
   removeSuffix = suf: s:
     let
       sufLen = stringLength suf;
       sLen = stringLength s;
     in
-      if sufLen <= sLen && suf == substring (sLen - sufLen) sufLen s then
-        substring 0 (sLen - sufLen) s
-      else
-        s;
+    if sufLen <= sLen && suf == substring (sLen - sufLen) sufLen s then
+      substring 0 (sLen - sufLen) s
+    else
+      s;
 
   # Return true iff string v1 denotes a version older than v2.
   versionOlder = v1: v2: builtins.compareVersions v2 v1 == 1;
 
-
   # Return true iff string v1 denotes a version equal to or newer than v2.
   versionAtLeast = v1: v2: !versionOlder v1 v2;
-
 
   # This function takes an argument that's either a derivation or a
   # derivation's "name" attribute and extracts the version part from that
@@ -183,7 +181,6 @@ rec {
   #    lib.getVersion pkgs.youtube-dl         ==> "2016.01.01"
   getVersion = x: (builtins.parseDrvName (x.name or x)).version;
 
-
   # Extract name with version from URL. Ask for separator which is
   # supposed to start extension.
   nameFromURL = url: sep:
@@ -191,7 +188,9 @@ rec {
       components = splitString "/" url;
       filename = lib.last components;
       name = builtins.head (splitString sep filename);
-    in assert name !=  filename; name;
+    in
+    assert name !=  filename;
+    name;
 
   # Create a fixed width string with additional prefix to match
   # required width.
@@ -200,16 +199,19 @@ rec {
       strw = lib.stringLength str;
       reqWidth = width - (lib.stringLength filler);
     in
-      assert strw <= width;
-      if strw == width then str else filler + fixedWidthString reqWidth filler str;
-
+    assert strw <= width;
+    if strw == width then
+      str
+    else
+      filler + fixedWidthString reqWidth filler str;
 
   # Format a number adding leading zeroes up to fixed width.
   fixedWidthNumber = width: n: fixedWidthString width "0" (toString n);
 
-
   # Check whether a value is a store path.
-  isStorePath = x: builtins.substring 0 1 (toString x) == "/" && dirOf (builtins.toPath x) == builtins.storeDir;
+  isStorePath = x:
+    builtins.substring 0 1 (toString x) == "/"
+    && dirOf (builtins.toPath x) == builtins.storeDir;
 
   # Convert string to int
   # Obviously, it is a bit hacky to use fromJSON that way.
@@ -233,7 +235,8 @@ rec {
         (lib.splitString "\n" (builtins.readFile file));
       removeComments = lib.filter (line: !(lib.hasPrefix "#" line));
       relativePaths = removeComments lines;
-      absolutePaths = builtins.map (path: builtins.toPath (root + "/" + path)) relativePaths;
+      absolutePaths =
+        builtins.map (path: builtins.toPath (root + "/" + path)) relativePaths;
     in
       absolutePaths;
 

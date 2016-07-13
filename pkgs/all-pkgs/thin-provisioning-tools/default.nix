04b9d44e6fbdff83c9a895ffb32a24192b16d4dc
William A. Kennington III <william@wkennington.com>
2016-07-13 15:43:04 -0700

    thin-provisioning-tools: 0.6.2-rc8 -> 0.6.2

--- pkgs/all-pkgs/thin-provisioning-tools/default.nix
+++ pkgs/all-pkgs/thin-provisioning-tools/default.nix
@@ -8,7 +8,7 @@
 }:
 
 let
-  version = "0.6.2-rc8";
+  version = "0.6.2";
 in
 stdenv.mkDerivation {
   name = "thin-provisioning-tools-${version}";
@@ -17,7 +17,7 @@ stdenv.mkDerivation {
     owner = "jthornber";
     repo = "thin-provisioning-tools";
     rev = "v${version}";
-    sha256 = "25de728013ed4b64fe616bb7af79f6e432a9b90fa34682f0ae31cfcca2dd1e89";
+    sha256 = "bce9e5b91ee777e78e293347764df0118091e6016a97f03f2223f62438ef477c";
   };
 
   nativeBuildInputs = [

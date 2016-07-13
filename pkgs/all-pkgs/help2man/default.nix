bccdee467cadef264cb932a5828deb3594799e9c
William A. Kennington III <william@wkennington.com>
2016-07-13 14:53:00 -0700

    help2man: 1.47.3 -> 1.47.4

--- pkgs/all-pkgs/help2man/default.nix
+++ pkgs/all-pkgs/help2man/default.nix
@@ -5,12 +5,20 @@
 , makeWrapper
 }:
 
+let
+  version = "1.47.4";
+
+  tarballUrls = version: [
+    "mirror://gnu/help2man/help2man-${version}.tar.xz"
+  ];
+in
 stdenv.mkDerivation rec {
-  name = "help2man-1.47.3";
+  name = "help2man-${version}";
 
   src = fetchurl {
-    url = "mirror://gnu/help2man/${name}.tar.xz";
-    sha256 = "0miqq77ssk5rgsc9xlv7k5n2wk2c5wv2m1kh4zhbwrggfmjaycn2";
+    urls = tarballUrls version;
+    allowHashOutput = false;
+    sha256 = "d4ecf697d13f14dd1a78c5995f06459bff706fd1ce593d1c02d81667c0207753";
   };
 
   nativeBuildInputs = [
@@ -24,6 +32,17 @@ stdenv.mkDerivation rec {
       --prefix PERL5LIB : "$(echo ${perlPackages.LocaleGettext}/${perlPackages.perl.libPrefix})"
   '';
 
+  passthru = {
+    srcVerification = fetchurl rec {
+      failEarly = true;
+      urls = tarballUrls "1.47.4";
+      pgpsigUrls = map (n: "${n}.sig") urls;
+      pgpKeyFingerprint = "87EA 44D1 50D8 9615 E39A  3FEE F0DC 8E00 B28C 5995";
+      inherit (src) outputHashAlgo;
+      outputHash = "d4ecf697d13f14dd1a78c5995f06459bff706fd1ce593d1c02d81667c0207753";
+    };
+  };
+
   meta = with stdenv.lib; {
     description = "Generate man pages from `--help' output";
     homepage = http://www.gnu.org/software/help2man/;
@@ -32,7 +51,6 @@ stdenv.mkDerivation rec {
       wkennington
     ];
     platforms = with platforms;
-      i686-linux
-      ++ x86_64-linux;
+      x86_64-linux;
   };
 }

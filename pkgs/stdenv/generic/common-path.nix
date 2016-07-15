e2db20b147dfd98e24ea9283d36c87e95485f3ad
William A. Kennington III <william@wkennington.com>
2016-07-15 13:50:27 -0700

    common-path: Fix curl path to not include curl-config

--- pkgs/stdenv/generic/common-path.nix
+++ pkgs/stdenv/generic/common-path.nix
@@ -5,7 +5,6 @@
     brotli
     bzip2
     coreutils
-    curl
     diffutils
     findutils
     gawk
@@ -17,4 +16,14 @@
     gzip
     pkgconfig
     xz;
+
+  # We don't want to expose curl-config
+  curl = pkgs.stdenv.mkDerivation {
+    name = "curl-path";
+
+    buildCommand = ''
+      mkdir -p "$out/bin"
+      ln -sv "${pkgs.curl}/bin/curl" "$out/bin"
+    '';
+  };
 }

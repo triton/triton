a82a961b9ae55424de42fa45687764f8cfdaf54d
William A. Kennington III <william@wkennington.com>
2016-07-22 00:30:37 -0700

    uefi-shell: Actually add source

---
+++ pkgs/all-pkgs/uefi-shell/default.nix
@@ -0,0 +1,31 @@
+{ stdenv
+, fetchFromGitHub
+}:
+
+let
+self = stdenv.mkDerivation {
+  name = "uefi-shell-2016-07-11";
+
+  src = fetchFromGitHub {
+    owner = "tianocore";
+    repo = "edk2";
+    rev = "0f65154396df0cae940b779f715da127c7d0b28f";
+    sha256 = "84abc5a491aac61f9b85b1146cbd73efe5297c648966f9556feb0aabd12b35fa";
+  };
+
+  installPhase = ''
+    mkdir -p "$out/share/efi"
+    cp ShellBinPkg/UefiShell/X64/Shell.efi "$out/share/efi/Shell.efi"
+  '';
+
+  meta = with stdenv.lib; {
+    maintainers = with maintainers; [
+      wkennington
+    ];
+    platforms = with platforms;
+      x86_64-linux;
+  };
+};
+in self // {
+  shell = "${self}/share/efi/Shell.efi";
+}

ffc57ba3f36a295f72051ed952fe6e538f0ca37a
William A. Kennington III <william@wkennington.com>
2016-07-25 13:58:29 -0700

    libebml: 1.3.3 -> 1.3.4

--- pkgs/all-pkgs/libebml/default.nix
+++ pkgs/all-pkgs/libebml/default.nix
@@ -1,18 +1,22 @@
-{ stdenv, fetchurl }:
+{ stdenv
+, fetchurl
+}:
 
 stdenv.mkDerivation rec {
-  name = "libebml-1.3.3";
+  name = "libebml-1.3.4";
 
   src = fetchurl {
-    url = "http://dl.matroska.org/downloads/libebml/${name}.tar.bz2";
-    sha256 = "16alhwd1yz5bv3765xfn5azwk37805lg1f61195gjq8rlkd49yrm";
+    url = "https://dl.matroska.org/downloads/libebml/${name}.tar.bz2";
+    sha256 = "c50d3ecf133742c6549c0669c3873f968e11a365a5ba17b2f4dc339bbe51f387";
   };
 
   meta = with stdenv.lib; {
     description = "Extensible Binary Meta Language library";
     license = licenses.lgpl21;
     homepage = http://dl.matroska.org/downloads/libebml/;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [
+      wkennington
+    ];
     platforms = with platforms;
       x86_64-linux;
   };

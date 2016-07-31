dcc1f403b0b2eeaed6bc57250f729b1eb61a8a6c
Cody W. Opel <codyopel@gmail.com>
2016-07-30 23:10:57 -0400

    tcl: 8.6.5 -> 8.6.6

--- pkgs/all-pkgs/tcl/sources.nix
+++ pkgs/all-pkgs/tcl/sources.nix
@@ -5,8 +5,8 @@
     sha256 = "d3f04456da873d17f02efc30734b0300fb6c3b85028d445fe284b83253a6db18";
   };
   "8.6" = {
-    version = "8.6.5";
+    version = "8.6.6";
     multihash = "QmbJqmSWDkRoiNs68uvsifDFAqLYBTHpXkhhujGt8xhyXb";
-    sha256 = "ce26d5b9c7504fc25d2f10ef0b82b14cf117315445b5afa9e673ed331830fb53";
+    sha256 = "a265409781e4b3edcc4ef822533071b34c3dc6790b893963809b9fe221befe07";
   };
 }

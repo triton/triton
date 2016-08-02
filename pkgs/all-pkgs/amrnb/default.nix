6fd0f944d9c3b0856df05a4fda9baecc55925b19
Cody W. Opel <codyopel@gmail.com>
2016-08-02 03:14:45 -0400

    amrnb: amrnb-3gpp: 11.0.0 -> 13.0.0

--- pkgs/all-pkgs/amrnb/default.nix
+++ pkgs/all-pkgs/amrnb/default.nix
@@ -1,4 +1,5 @@
 { stdenv
+, autoreconfHook
 , fetchurl
 , unzip
 }:
@@ -6,26 +7,38 @@
 stdenv.mkDerivation rec{
   name = "amrnb-11.0.0.0";
 
-  srcAmr = fetchurl {
-    url = http://www.3gpp.org/ftp/Specs/latest/Rel-11/26_series/26104-b00.zip;
-    sha256 = "1wf8ih0hk7w20vdlnw7jb7w73v15hbxgbvmq4wq7h2ghn0j8ppr3";
+  # http://www.3gpp.org/DynaReport/26104.htm
+  # NOTE: When updating amrnb-3gpp, update every instance of 26104-d00 to the
+  #       updated file name.
+  amrnb_3gpp = fetchurl {
+    # Rel-13
+    url = "http://www.3gpp.org/ftp/Specs/archive/26_series/26.104/26104-d00.zip";
+    sha256 = "d5447f8a672243dd9db32ba64fd69181da725cac1f7b347c0af95d9949a47619";
   };
 
   src = fetchurl {
     url = "http://www.penguin.cz/~utx/ftp/amr/${name}.tar.bz2";
-    sha256 = "1qgiw02n2a6r32pimnd97v2jkvnw449xrqmaxiivjy2jcr5h141q";
+    sha256 = "3890004b665278b963ecaae2dc1321dcee29c53ea9d91aaf18d9286105e0f1e1";
   };
 
   nativeBuildInputs = [
+    autoreconfHook
     unzip
   ];
+
+  postPatch = /* Fix hardcoded 3GPP source version */ ''
+    sed -i Makefile.{in,am} \
+      -i configure{,.ac} \
+      -i prepare_sources.sh.in \
+      -e 's/26104-b00/26104-d00/g'
+  '';
 
   configureFlags = [
     "--without-downloader"
   ];
 
-  postConfigure = ''
-    cp -v $srcAmr 26104-b00.zip
+  preConfigure = ''
+    cp -v $amrnb_3gpp 26104-d00.zip
   '';
 
   meta = with stdenv.lib; {

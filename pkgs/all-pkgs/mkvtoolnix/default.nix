a0dc9019ef640f153b24692aa649876ac6e287a8
Cody W. Opel <codyopel@gmail.com>
2016-08-07 05:21:01 -0400

    mkvtoolnix: 9.0.1 -> 9.3.1

--- pkgs/all-pkgs/mkvtoolnix/default.nix
+++ pkgs/all-pkgs/mkvtoolnix/default.nix
@@ -24,15 +24,13 @@ let
     wtFlag;
 in
 
-assert qt5 != null -> qt5.qtbase != null;
-
 stdenv.mkDerivation rec {
   name = "mkvtoolnix-${version}";
-  version = "9.0.1";
+  version = "9.3.1";
 
   src = fetchurl {
     url = "https://mkvtoolnix.download/sources/${name}.tar.xz";
-    sha256 = "292504633d714c42f73f08474137e462827f6d8d570292005bbaebb8fee8e52e";
+    sha256 = "f3695761bf0a5fdcd6144cfb0a624094c10c9d66d43a340ebb917b7c6a8b39a2";
   };
 
   nativeBuildInputs = [
@@ -50,10 +48,9 @@ stdenv.mkDerivation rec {
     libogg
     libvorbis
     pugixml
+    qt5
     xdg-utils
     zlib
-  ] ++ optionals (qt5 != null) [
-    qt5.qtbase
   ];
 
   postPatch = ''
@@ -84,6 +81,18 @@ stdenv.mkDerivation rec {
   installPhase = ''
     ./drake install -j $NIX_BUILD_CORES
   '';
+
+  passthru = {
+    srcVerification = fetchurl {
+      inherit (src)
+        outputHash
+        outputHashAlgo
+        urls;
+      failEarly = true;
+      pgpsigUrls = map (n: "${n}.sig") src.urls;
+      pgpKeyFingerprint = "D919 9745 B054 5F2E 8197  062B 0F92 290A 445B 9007";
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "Cross-platform tools for Matroska";

3afa76f5758f510fc98c64e434568753c1765242
Cody W. Opel <codyopel@gmail.com>
2016-08-08 06:30:33 -0400

    dnscrypt: fix formatting

--- pkgs/all-pkgs/dnscrypt-wrapper/default.nix
+++ pkgs/all-pkgs/dnscrypt-wrapper/default.nix
@@ -12,8 +12,8 @@ stdenv.mkDerivation rec {
   name = "dnscrypt-wrapper-${version}";
 
   src = fetchurl {
-    url = "https://github.com/Cofyc/dnscrypt-wrapper/releases/download/v${version}"
-      + "/dnscrypt-wrapper-v${version}.tar.bz2";
+    url = "https://github.com/Cofyc/dnscrypt-wrapper/releases/download/"
+      + "v${version}/dnscrypt-wrapper-v${version}.tar.bz2";
     sha256 = "02f52859ec766e85b2825dabdb89a34c8d126c538b5550efe2349ecae2aeb266";
   };
 

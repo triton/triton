c5a104ad5194c97c1fc96e732a53f39cba90c008
Cody W. Opel <codyopel@gmail.com>
2016-08-08 16:03:50 -0400

    glfw(3): fix formatting

--- pkgs/all-pkgs/glfw/3.x.nix
+++ pkgs/all-pkgs/glfw/3.x.nix
@@ -15,7 +15,8 @@ stdenv.mkDerivation rec {
   name = "glfw-${version}";
 
   src = fetchurl {
-    url = "https://github.com/glfw/glfw/releases/download/${version}/${name}.zip";
+    url = "https://github.com/glfw/glfw/releases/download/"
+      + "${version}/${name}.zip";
     sha256 = "d9983a129732bd400869dd26c9ef2ed253b1da0cfb79585ab7af63a175d0f652";
   };
 
@@ -42,8 +43,8 @@ stdenv.mkDerivation rec {
     "-DBUILD_SHARED_LIBS=ON"
   ];
 
-  meta = with stdenv.lib; { 
-    description = "Multi-platform library for creating OpenGL contexts and managing input, including keyboard, mouse, joystick and time";
+  meta = with stdenv.lib; {
+    description = "Library for creating OpenGL contexts and managing input";
     homepage = "http://www.glfw.org/";
     license = licenses.zlib;
     maintainers = with maintainers; [

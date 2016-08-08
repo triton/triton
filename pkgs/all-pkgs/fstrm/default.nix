96b8cc9b561170bd2322ac324ff33024416cdb4b
Cody W. Opel <codyopel@gmail.com>
2016-08-08 09:33:43 -0400

    fstrm: fix formatting

--- pkgs/all-pkgs/fstrm/default.nix
+++ pkgs/all-pkgs/fstrm/default.nix
@@ -11,7 +11,8 @@ stdenv.mkDerivation rec {
   name = "fstrm-${version}";
 
   src = fetchurl {
-    url = "https://github.com/farsightsec/fstrm/releases/download/v${version}/${name}.tar.gz";
+    url = "https://github.com/farsightsec/fstrm/releases/download/v${version}/"
+      + "${name}.tar.gz";
     sha256 = "ad5d39957a4b334a6c7fcc94f308dc7ac75e1997cc642e9bb91a18fc0f42a98a";
   };
 

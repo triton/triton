d9bb16ea5b8333cc7d70841fae213425568b6236
Cody W. Opel <codyopel@gmail.com>
2016-08-08 21:38:32 -0400

    clutter-gst: add sha256Url

--- pkgs/all-pkgs/clutter-gst/3.x.nix
+++ pkgs/all-pkgs/clutter-gst/3.x.nix
@@ -27,6 +27,8 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://gnome/sources/clutter-gst/${versionMajor}/${name}.tar.xz";
+    sha256Url = "mirror://gnome/sources/clutter-gst/${versionMajor}/"
+      + "${name}.sha256sum";
     sha256 = "0aec0d0c6020cd19a5bb0dab1165a92748f81a9a3acdfabb0f966d5f53bc8093";
   };
 

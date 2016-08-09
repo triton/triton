ffef7ca07d0b12d27c5248df4bf40473b7fc0b68
Cody W. Opel <codyopel@gmail.com>
2016-08-09 05:23:01 -0400

    xdg-user-dirs: fix references to dockbook-xsl

--- pkgs/tools/X11/xdg-user-dirs/default.nix
+++ pkgs/tools/X11/xdg-user-dirs/default.nix
@@ -1,14 +1,14 @@
-{ stdenv, fetchurl, libxslt, docbook_xsl, makeWrapper }:
+{ stdenv, fetchurl, libxslt, docbook-xsl, makeWrapper }:
 
 stdenv.mkDerivation rec {
   name = "xdg-user-dirs-0.15";
-  
+
   src = fetchurl {
     url = "http://user-dirs.freedesktop.org/releases/${name}.tar.gz";
     sha256 = "20b4a751f41d0554bce3e0ce5e8d934be98cc62d48f0b90a894c3e1916552786";
   };
 
-  buildInputs = [ libxslt docbook_xsl makeWrapper ];
+  buildInputs = [ libxslt docbook-xsl makeWrapper ];
 
   preFixup = ''
     wrapProgram "$out/bin/xdg-user-dirs-update" \

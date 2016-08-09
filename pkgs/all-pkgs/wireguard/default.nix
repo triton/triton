84bff6dbb3cca86400f7f09e3709e2bf262c813a
William A. Kennington III <william@wkennington.com>
2016-08-08 20:33:36 -0700

    wireguard: 2016-07-28 -> 2016-08-09

--- pkgs/all-pkgs/wireguard/default.nix
+++ pkgs/all-pkgs/wireguard/default.nix
@@ -10,15 +10,15 @@ let
     optionals
     optionalString;
 
-  rev = "ded0e645cfa45130e42c4d5bfba8f7d54e1855a9";
-  date = "2016-07-28";
+  rev = "609696b05dac48e37e960f43ef458b16bcae9310";
+  date = "2016-08-09";
 in
 stdenv.mkDerivation {
   name = "wireguard-${date}";
 
   src = fetchzip {
     url = "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-${rev}.tar.xz";
-    sha256 = "7375d93708ccce68f7e36cec83f63cad7b09623a21e270412e55673411b4b6dd";
+    sha256 = "9e05663cc28f85a5c5593cb4c7069711f8b3139a7a1e07b37729a5db6aa30c31";
   };
 
   buildInputs = optionals (kernel == null) [

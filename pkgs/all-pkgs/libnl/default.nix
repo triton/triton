b41c159afa83a124fdf570f91c480e745f56352f
William A. Kennington III <william@wkennington.com>
2016-08-09 17:14:03 -0700

    libnl: 3.2.27 -> 3.2.28

--- pkgs/all-pkgs/libnl/default.nix
+++ pkgs/all-pkgs/libnl/default.nix
@@ -5,22 +5,31 @@
 }:
 
 let
-  version = "3.2.27";
+  version = "3.2.28";
   version' = stdenv.lib.replaceStrings ["."] ["_"] version;
 in
-
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "libnl-${version}";
 
   src = fetchurl {
     url = "https://github.com/thom311/libnl/releases/download/libnl${version'}/libnl-${version}.tar.gz";
-    sha256 = "1ilfynang98p282lgvfcp1fvfa4s86zn0qpr7i10zabq7hmzkfsb";
+    allowHashOutput = false;
+    sha256 = "cd608992c656e8f6e3ab6c1391b162a5a51c49336b9219f7f390e61fc5437c41";
   };
 
   nativeBuildInputs = [
     bison
     flex
   ];
+
+  passthru = {
+    srcVerification = fetchurl {
+      failEarly = true;
+      pgpsigUrls = map (n: "${n}.sig") src.urls;
+      pgpKeyFingerprint = "49EA 7C67 0E08 50E7 4195  14F6 29C2 366E 4DFC 5728";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
 
   meta = with stdenv.lib; {
     homepage = "http://www.infradead.org/~tgr/libnl/";

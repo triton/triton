2b268eb5ab9412eb560107bbbedc591feed169ae
William A. Kennington III <william@wkennington.com>
2016-08-09 17:13:54 -0700

    diffutils: 3.3 -> 3.4

--- pkgs/all-pkgs/diffutils/default.nix
+++ pkgs/all-pkgs/diffutils/default.nix
@@ -3,12 +3,20 @@
 , coreutils
 }:
 
+let
+  version = "3.4";
+
+  tarballUrls = version: [
+    "mirror://gnu/diffutils/diffutils-${version}.tar.xz"
+  ];
+in
 stdenv.mkDerivation rec {
-  name = "diffutils-3.3";
+  name = "diffutils-${version}";
 
   src = fetchurl {
-    url = "mirror://gnu/diffutils/${name}.tar.xz";
-    sha256 = "1761vymxbp4wb5rzjvabhdkskk95pghnn67464byvzb5mfl8jpm2";
+    urls = tarballUrls version;
+    allowHashOutput = false;
+    sha256 = "db53c025f2ac3d217bcf753dad6dee7b410b33d0948495ff015aaf8b91189ce2";
   };
 
   # We need to directly reference coreutils, otherwise the
@@ -16,6 +24,17 @@ stdenv.mkDerivation rec {
   buildInputs = [
     coreutils
   ];
+
+  passthru = {
+    srcVerification = fetchurl rec {
+      failEarly = true;
+      urls = tarballUrls "3.4";
+      pgpsigUrls = map (n: "${n}.sig") urls;
+      pgpKeyFingerprint = "155D 3FC5 00C8 3448 6D1E  EA67 7FD9 FCCB 000B EEEE";
+      inherit (src) outputHashAlgo;
+      outputHash = "db53c025f2ac3d217bcf753dad6dee7b410b33d0948495ff015aaf8b91189ce2";
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "Commands for showing the differences (diff) between files";

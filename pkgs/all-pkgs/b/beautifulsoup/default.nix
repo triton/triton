ccfc565d3cd4470508203c48d5487eecbbb7cb75
Cody W. Opel <codyopel@gmail.com>
2016-08-11 08:11:34 -0400

    beautifulsoup: add missing dependencies

--- pkgs/all-pkgs/b/beautifulsoup/default.nix
+++ pkgs/all-pkgs/b/beautifulsoup/default.nix
@@ -1,8 +1,16 @@
 { stdenv
 , buildPythonPackage
 , fetchPyPi
+, isPy3k
+
+, html5lib
+, lxml
 }:
 
+let
+  inherit (stdenv.lib)
+    optionals;
+in
 buildPythonPackage rec {
   name = "beautifulsoup-${version}";
   version = "4.5.1";
@@ -13,7 +21,13 @@ buildPythonPackage rec {
     sha256 = "3c9474036afda9136aac6463def733f81017bf9ef3510d25634f335b0c87f5e1";
   };
 
-  doCheck = true;
+  propagatedBuildInputs = [
+    html5lib
+    lxml
+  ];
+
+  # Not all tests have been converted to Python 3
+  doCheck = !isPy3k;
 
   meta = with stdenv.lib; {
     description = "HTML/XML parser";

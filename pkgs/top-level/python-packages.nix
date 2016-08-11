85b403088b19fe564bab89953042aa7358ede2b2
Cody W. Opel <codyopel@gmail.com>
2016-08-11 06:25:46 -0400

    testscenarios: 0.4 -> 0.5.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -18537,11 +18537,12 @@ zope-interface = callPackage ../all-pkgs/z/zope-interface { };
 
    testscenarios = buildPythonPackage rec {
      name = "testscenarios-${version}";
-     version = "0.4";
+     version = "0.5.0";
 
-     src = pkgs.fetchurl {
-       url = "https://pypi.python.org/packages/source/t/testscenarios/${name}.tar.gz";
-       sha256 = "1671jvrvqlmbnc42j7pc5y6vc37q44aiwrq0zic652pxyy2fxvjg";
+     src = fetchPyPi {
+       package = "testscenarios";
+       inherit version;
+       sha256 = "c257cb6b90ea7e6f8fef3158121d430543412c9a87df30b5dde6ec8b9b57a2b6";
      };
 
      propagatedBuildInputs = with self; [ testtools ];

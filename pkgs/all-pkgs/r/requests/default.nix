82e7346ef1d8154748e8e667bfc0421692e91396
Cody W. Opel <codyopel@gmail.com>
2016-08-11 05:05:05 -0400

    requests: 2.10.0 -> 2.11.0

--- pkgs/all-pkgs/r/requests/default.nix
+++ pkgs/all-pkgs/r/requests/default.nix
@@ -4,7 +4,7 @@
 }:
 
 let
-  version = "2.10.0";
+  version = "2.11.0";
 in
 buildPythonPackage {
   name = "requests-${version}";
@@ -12,7 +12,7 @@ buildPythonPackage {
   src = fetchPyPi {
     package = "requests";
     inherit version;
-    sha256 = "63f1815788157130cee16a933b2ee184038e975f0017306d723ac326b5525b54";
+    sha256 = "b2ff053e93ef11ea08b0e596a1618487c4e4c5f1006d7a1706e3671c57dea385";
   };
 
   meta = with stdenv.lib; {

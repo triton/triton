83029dc84ea0acae30fa11e519d815e3c6ac4495
William A. Kennington III <william@wkennington.com>
2016-08-12 12:26:42 -0700

    libid3tag: Add multihash

--- pkgs/development/libraries/libid3tag/default.nix
+++ pkgs/development/libraries/libid3tag/default.nix
@@ -7,6 +7,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = mirror://sourceforge/mad/libid3tag-0.15.1b.tar.gz;
+    multihash = "QmVvjo484Tz6o9FHbyJf5bFxR13ZEkoaknPSWtaqrCbNzF";
     sha256 = "63da4f6e7997278f8a3fef4c6a372d342f705051d1eeb6a46a86b03610e26151";
   };
 

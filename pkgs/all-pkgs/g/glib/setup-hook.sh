89397d562a2775ba45700568d0fd432adf026460
Cody W. Opel <codyopel@gmail.com>
2016-08-13 07:40:29 -0400

    glib(setup-hook.sh): Revert use $1 instead of $out & $name

--- pkgs/all-pkgs/g/glib/setup-hook.sh
+++ pkgs/all-pkgs/g/glib/setup-hook.sh
@@ -1,21 +1,21 @@
 # Add gio modules to GIO_EXTRA_MODULES
 find_gio_modules() {
-  if [ -d "${1}/lib/gio-modules/$(basename "${1}")/gio/modules" ] ; then
+  if [ -d "${out}/lib/gio-modules/${name}/gio/modules" ] ; then
     addToSearchPath GIO_EXTRA_MODULES \
-      "${1}/lib/gio-modules/$(basename "${1}")/gio/modules"
+      "${out}/lib/gio-modules/${name}/gio/modules"
   fi
 }
 
 # Add gsettings schemas to GSETTINGS_SCHEMAS_PATH
 find_gsettings_schemas() {
-  if [ -d "${1}/share/gsettings-schemas/$(basename "${1}")/glib-2.0/schemas" ] ; then
+  if [ -d "${out}/share/gschemas/${name}/glib-2.0/schemas" ] ; then
     addToSearchPath GSETTINGS_SCHEMAS_PATH \
-      "${1}/share/gsettings-schemas/$(basename "${1}")"
+      "${out}/share/gschemas/${name}"
   fi
 }
 
 # Make sure gio modules are installed in
-#   $out/lib/gio-modules/$(basename "${1}")/gio/modules/
+#   $out/lib/gio-modules/${name}/gio/modules/
 fix_gio_modules_install_path() {
   # If gio modules are all installed in $out/lib/gio/modules, it will
   # result in filename collisions with giomodule.cache when trying to
@@ -24,19 +24,20 @@ fix_gio_modules_install_path() {
   # At runtime, glib looks for gio/modules in GIO_EXTRA_MODULES,
   # so we must place these directories in a unique directory.
 
-  if [ -d "${1}/lib/gio/modules" ] ; then
-    mkdir -pv "${1}/lib/gio-modules/$(basename "${1}")"
+  if [ -d "${out}/lib/gio/modules" ] ; then
+    mkdir -pv "${out}/lib/gio-modules/${name}"
     mv -v \
-      "${1}/lib/gio" \
-      "${1}/lib/gio-modules/$(basename "${1}")/"
+      "${out}/lib/gio" \
+      "${out}/lib/gio-modules/${name}/"
   fi
 
-  addToSearchPath GIO_EXTRA_MODULES "${1}/lib/gio-modules/$(basename "${1}")"
+  addToSearchPath GIO_EXTRA_MODULES \
+    "${out}/lib/gio-modules/${name}"
 }
 
 # Make sure gsettings schemas are installed in
-#   $out/share/gsettings-schemas/$(basename "${1}")/glib-2.0/schemas/
-fix_gsettings_schemas_install_path() {
+#   $out/share/gschemas/${name}/glib-2.0/schemas/
+fix_gschemas_install_path() {
   # If gsettings schemas are all installed in $out/glib-2.0/schemas, it
   # will result in filename collisions with gschemas.compiled when trying
   # to add more than one package conatining this file to a given profile.
@@ -44,36 +45,46 @@ fix_gsettings_schemas_install_path() {
   # At runtime, gsettings looks for glib-2.0/schemas in XDG_DATA_DIRS, so
   # we must place these directories in a unique directory.
 
-  if [ -d "${1}/share/glib-2.0/schemas" ] ; then
-    mkdir -pv "${1}/share/gsettings-schemas/$(basename "${1}")/glib-2.0"
+  if [ -d "${out}/share/glib-2.0/schemas" ] ; then
+    mkdir -pv "${out}/share/gschemas/${name}/glib-2.0"
     mv -v \
-      "${1}/share/glib-2.0/schemas" \
-      "${1}/share/gsettings-schemas/$(basename "${1}")/glib-2.0/"
+      "${out}/share/glib-2.0/schemas" \
+      "${out}/share/gschemas/${name}/glib-2.0/"
   fi
 
   addToSearchPath GSETTINGS_SCHEMAS_PATH \
-    "${1}/share/gsettings-schemas/$(basename "${1}")"
+    "${out}/share/gschemas/${name}"
 }
 
 # Recompile schemas to make sure all schemas are compiled and to
 # ensure gschemas.compiled exists.
-fix_compiled_gsettings_schemas() {
-  if [ -d "${1}/share/gsettings-schemas/$(basename "${1}")/glib-2.0/schemas" ] ; then
-    if type -P glib-compile-schemas ; then
-      # Remove existing gschemas.compiled
-      if [ -f "${1}/share/gsettings-schemas/$(basename "${1}")/glib-2.0/schemas/gschemas.compiled" ] ; then
-        rm -fv "${1}/share/gsettings-schemas/$(basename "${1}")/glib-2.0/schemas/gschemas.compiled"
-      fi
-      glib-compile-schemas \
-        "${1}/share/gsettings-schemas/$(basename "${1}")/glib-2.0/schemas/"
-    elif [ -x "${1}/bin/glib-compile-schemas" ] ; then
-      # Do not try to compile schemas in glib itself
-      :
-    else
-      echo 'ERROR: fix_compiled_gsettings_schemas failed, glib-compile-schemas'
-      echo '       not found in PATH or $out/bin/glib-compile-schemas'
-      return 1
+compile_gschemas() {
+  local gschemas_dir
+
+  if [ -d "${out}/share/gschemas/${name}/glib-2.0/schemas" ] ; then
+    gschemas_dir="${out}/share/gschemas/${name}/glib-2.0/schemas"
+  # Incase fix_gschemas_install_path and compile_gschemas are run out-of-order.
+  elif [ -d "${out}/share/glib-2.0/schemas" ] ; then
+    gschemas_dir="${out}/share/glib-2.0/schemas"
+  # If no gschemas directories exists exit hook.
+  else
+    return 0
+  fi
+
+  echo "Compiling gschemas in: ${gschemas_dir}"
+  if [ -x "${out}/bin/glib-compile-schemas" ] ; then
+    # Do not try to compile schemas in glib itself
+    :
+  elif type -P glib-compile-schemas ; then
+    # Remove existing gschemas.compiled
+    if [ -f "${gschemas_dir}/gschemas.compiled" ] ; then
+      rm -fv "${gschemas_dir}/gschemas.compiled"
     fi
+    glib-compile-schemas "${gschemas_dir}/"
+  else
+    echo 'ERROR: fix_compiled_gsettings_schemas failed, glib-compile-schemas'
+    echo '       not found in PATH or $out/bin/glib-compile-schemas'
+    return out
   fi
 }
 
@@ -81,9 +92,9 @@ envHooks+=('find_gio_modules')
 envHooks+=('find_gsettings_schemas')
 
 installFlagsArray+=(
-  "gsettingsschemadir=${out}/share/gsettings-schemas/${name}/glib-2.0/schemas/"
+  "gsettingsschemadir=${out}/share/gschemas/${name}/glib-2.0/schemas/"
 )
 
 preFixupPhases+=('fix_gio_modules_install_path')
-preFixupPhases+=('fix_gsettings_schemas_install_path')
-preFixupPhases+=('fix_compiled_gsettings_schemas')
+preFixupPhases+=('fix_gschemas_install_path')
+preFixupPhases+=('compile_gschemas')

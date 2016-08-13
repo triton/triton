e05592cd17f3c321b4785b7ce58df40868fb2ebc
Cody W. Opel <codyopel@gmail.com>
2016-08-13 04:20:16 -0400

    glib(setup-hook.sh): use value passed as  instead of inheriting any variable named  &

--- pkgs/all-pkgs/g/glib/setup-hook.sh
+++ pkgs/all-pkgs/g/glib/setup-hook.sh
@@ -1,19 +1,21 @@
 # Add gio modules to GIO_EXTRA_MODULES
 find_gio_modules() {
-  if [ -d "${1}"/lib/gio-modules/*/gio/modules ] ; then
-    addToSearchPath GIO_EXTRA_MODULES "${1}"/lib/gio-modules/*/gio/modules
+  if [ -d "${1}/lib/gio-modules/$(basename "${1}")/gio/modules" ] ; then
+    addToSearchPath GIO_EXTRA_MODULES \
+      "${1}/lib/gio-modules/$(basename "${1}")/gio/modules"
   fi
 }
 
 # Add gsettings schemas to GSETTINGS_SCHEMAS_PATH
 find_gsettings_schemas() {
-  if [ -d "${1}"/share/gsettings-schemas/*/glib-2.0/schemas ] ; then
-    addToSearchPath GSETTINGS_SCHEMAS_PATH "${1}"/share/gsettings-schemas/*
+  if [ -d "${1}/share/gsettings-schemas/$(basename "${1}")/glib-2.0/schemas" ] ; then
+    addToSearchPath GSETTINGS_SCHEMAS_PATH \
+      "${1}/share/gsettings-schemas/$(basename "${1}")"
   fi
 }
 
 # Make sure gio modules are installed in
-#   $out/lib/gio-modules/${name}/gio/modules/
+#   $out/lib/gio-modules/$(basename "${1}")/gio/modules/
 fix_gio_modules_install_path() {
   # If gio modules are all installed in $out/lib/gio/modules, it will
   # result in filename collisions with giomodule.cache when trying to
@@ -22,18 +24,18 @@ fix_gio_modules_install_path() {
   # At runtime, glib looks for gio/modules in GIO_EXTRA_MODULES,
   # so we must place these directories in a unique directory.
 
-  if [ -d "${out}/lib/gio/modules" ] ; then
-    mkdir -pv "${out}/lib/gio-modules/${name}"
+  if [ -d "${1}/lib/gio/modules" ] ; then
+    mkdir -pv "${1}/lib/gio-modules/$(basename "${1}")"
     mv -v \
-      "${out}/lib/gio" \
-      "${out}/lib/gio-modules/${name}/"
+      "${1}/lib/gio" \
+      "${1}/lib/gio-modules/$(basename "${1}")/"
   fi
 
-  addToSearchPath GIO_EXTRA_MODULES "${out}/lib/gio-modules/${name}"
+  addToSearchPath GIO_EXTRA_MODULES "${1}/lib/gio-modules/$(basename "${1}")"
 }
 
 # Make sure gsettings schemas are installed in
-#   $out/share/gsettings-schemas/${name}/glib-2.0/schemas/
+#   $out/share/gsettings-schemas/$(basename "${1}")/glib-2.0/schemas/
 fix_gsettings_schemas_install_path() {
   # If gsettings schemas are all installed in $out/glib-2.0/schemas, it
   # will result in filename collisions with gschemas.compiled when trying
@@ -42,14 +44,15 @@ fix_gsettings_schemas_install_path() {
   # At runtime, gsettings looks for glib-2.0/schemas in XDG_DATA_DIRS, so
   # we must place these directories in a unique directory.
 
-  if [ -d "${out}/share/glib-2.0/schemas" ] ; then
-    mkdir -pv "${out}/share/gsettings-schemas/${name}/glib-2.0"
+  if [ -d "${1}/share/glib-2.0/schemas" ] ; then
+    mkdir -pv "${1}/share/gsettings-schemas/$(basename "${1}")/glib-2.0"
     mv -v \
-      "${out}/share/glib-2.0/schemas" \
-      "${out}/share/gsettings-schemas/${name}/glib-2.0/"
+      "${1}/share/glib-2.0/schemas" \
+      "${1}/share/gsettings-schemas/$(basename "${1}")/glib-2.0/"
   fi
 
-  addToSearchPath GSETTINGS_SCHEMAS_PATH "${out}/share/gsettings-schemas/${name}"
+  addToSearchPath GSETTINGS_SCHEMAS_PATH \
+    "${1}/share/gsettings-schemas/$(basename "${1}")"
 }
 
 envHooks+=(

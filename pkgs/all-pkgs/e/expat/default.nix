2c2a301fcd1c25b6ff4239fffd79476a1b19e16b
William A. Kennington III <william@wkennington.com>
2016-08-16 12:55:24 -0700

    expat: Fix CVE-2016-0718

--- pkgs/all-pkgs/e/expat/default.nix
+++ pkgs/all-pkgs/e/expat/default.nix
@@ -1,5 +1,6 @@
 { stdenv
 , fetchurl
+, fetchTritonPatch
 }:
 
 stdenv.mkDerivation rec {
@@ -10,6 +11,18 @@ stdenv.mkDerivation rec {
     multihash = "QmXyog231KWB6xwMbPjpv51akeJZj1jHsdkVrWNkMxzVgX";
     sha256 = "d9e50ff2d19b3538bd2127902a89987474e1a4db8e43a66a4d1a712ab9a504ff";
   };
+
+  patchFlags = [
+    "-p2"
+  ];
+
+  patches = [
+    (fetchTritonPatch {
+      rev = "7968def6dcf2836760c1015f62b9afdde149217d";
+      file = "expat/2016-0718-regression.patch";
+      sha256 = "e64ff17753e601f23a6825beeb930aef1bec17b7eec7dce4e8c465b3c0cd66ff";
+    })
+  ];
 
   meta = with stdenv.lib; {
     description = "A stream-oriented XML parser library written in C";

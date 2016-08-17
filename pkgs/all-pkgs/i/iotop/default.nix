7fcdcb437bf2a54d86dd33f8327369a0343ab995
Cody W. Opel <codyopel@gmail.com>
2016-08-17 15:52:20 -0400

    iotop: fix python 3 support

--- pkgs/all-pkgs/i/iotop/default.nix
+++ pkgs/all-pkgs/i/iotop/default.nix
@@ -12,6 +12,13 @@ buildPythonPackage rec {
     sha256 = "0nzprs6zqax0cwq8h7hnszdl3d2m4c2d4vjfxfxbnjfs9sia5pis";
   };
 
+  postPatch =
+    /* XXX: Fixed in >0.6
+       Fix build error with Python 3 caused by itervalues() in setup.py */ ''
+      sed -i setup.py \
+        -e 's/itervalues()/values()/'
+    '';
+
   meta = with stdenv.lib; {
     description = "A tool to find out the processes doing the most IO";
     maintainers = with maintainers; [

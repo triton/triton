9063d7f36df7d5cd0f65202f9ccf87b1574fbb3a
Cody W. Opel <codyopel@gmail.com>
2016-08-18 23:22:00 -0400

    zeromq: refactor

--- pkgs/all-pkgs/z/zeromq/default.nix
+++ pkgs/all-pkgs/z/zeromq/default.nix
@@ -7,13 +7,21 @@
 }:
 
 let
+  inherit (builtins)
+    replaceStrings;
+  inherit (stdenv.lib)
+    strings;
+
   version = "4.1.5";
+
+  versionMajorMinor = replaceStrings ["."] ["-"] (strings.substring 0 3 version);
 in
 stdenv.mkDerivation rec {
   name = "zeromq-${version}";
 
   src = fetchurl {
-    url = "https://github.com/zeromq/zeromq4-1/releases/download/v${version}/${name}.tar.gz";
+    url = "https://github.com/zeromq/zeromq${versionMajorMinor}/releases/"
+      + "download/v${version}/${name}.tar.gz";
     sha256 = "04aac57f081ffa3a2ee5ed04887be9e205df3a7ddade0027460b8042432bdbcf";
   };
 
@@ -30,8 +38,8 @@ stdenv.mkDerivation rec {
   ];
 
   meta = with stdenv.lib; {
-    homepage = "http://www.zeromq.org";
     description = "The Intelligent Transport Layer";
+    homepage = "http://www.zeromq.org";
     license = licenses.gpl3;
     maintainers = with maintainers; [
       wkennington

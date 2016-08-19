635508a8db48ffd243cf77998b46db9eaf8e2ef6
Cody W. Opel <codyopel@gmail.com>
2016-08-18 21:59:12 -0400

    sg3_utils: 1.31 -> 1.42

--- pkgs/tools/system/sg3_utils/default.nix
+++ pkgs/tools/system/sg3_utils/default.nix
@@ -1,17 +1,22 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "sg3_utils-1.31";
+  name = "sg3_utils-1.43";
 
   src = fetchurl {
     url = "http://sg.danny.cz/sg/p/${name}.tgz";
+    multihash = "QmfAZiUCRW79h4wRoyjxdnMJmd7XZNhAtxoygomd4YcifG";
     sha256 = "190hhkhl096fxkspkr93lrq1n79xz5c5i2n4n4g998qc3yv3hjyq";
   };
 
-  meta = {
-    homepage = http://sg.danny.cz/sg/;
+  meta = with stdenv.lib; {
     description = "Utilities that send SCSI commands to devices";
-    platforms = stdenv.lib.platforms.all;
-    maintainers = [ stdenv.lib.maintainers.urkud ];
+    homepage = http://sg.danny.cz/sg/;
+    license = licenses.gpl2;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = with platforms;
+      x86_64-linux;
   };
 }

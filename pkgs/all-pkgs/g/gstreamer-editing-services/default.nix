08357ec2efcb69ae4a360b726cbea4ea7ec145e3
Cody W. Opel <codyopel@gmail.com>
2016-08-20 20:55:43 -0400

    gstreamer-editing-services: 1.8.2 -> 1.8.3

--- pkgs/all-pkgs/g/gstreamer-editing-services/default.nix
+++ pkgs/all-pkgs/g/gstreamer-editing-services/default.nix
@@ -16,15 +16,14 @@ let
   inherit (stdenv.lib)
     enFlag;
 in
-
 stdenv.mkDerivation rec {
-  name = "gstreamer-editing-services-1.8.2";
+  name = "gstreamer-editing-services-1.8.3";
 
   src = fetchurl rec {
-    url = "https://gstreamer.freedesktop.org/src/gstreamer-editing-services/" +
-          "${name}.tar.xz";
-    sha256Url = "${url}.sha256sum";
-    sha256 = "a1d57ff9461407cca1f6e7a9f31a5bdb73f73f33c488a3e3318b27e10a4332ae";
+    url = "https://gstreamer.freedesktop.org/src/gstreamer-editing-services/"
+      + "${name}.tar.xz";
+    sha256Url = url + ".sha256sum";
+    sha256 = "c48a75ab2a3b72ed33f69d8279c56c0f3a2d0881255f8b169a7a13518eaa13cd";
   };
 
   nativeBuildInputs = [

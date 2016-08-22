38e6d49c00751cfdcae918ccebc75669a72ef09c
William A. Kennington III <william@wkennington.com>
2016-08-22 15:04:54 -0700

    libnetfilter_conntrack: 1.0.5 -> 1.0.6

--- pkgs/all-pkgs/l/libnetfilter_conntrack/default.nix
+++ pkgs/all-pkgs/l/libnetfilter_conntrack/default.nix
@@ -6,19 +6,28 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "libnetfilter_conntrack-${version}";
-  version = "1.0.5";
+  name = "libnetfilter_conntrack-1.0.6";
 
   src = fetchurl {
     url = "http://netfilter.org/projects/libnetfilter_conntrack/files/${name}.tar.bz2";
-    md5Confirm = "6aa1bd3c1d0723235ac897087b4cd4e5";
-    sha256 = "0fnpja3g8s38cp7ipija5pvhfgna1gybn0z2bl276nk08fppv7gw";
+    allowHashOutput = false;
+    multihash = "QmdyNB8BoHa5cg2rTyCASLK3mwoGPwRZriJRwtb6XPpt9N";
+    sha256 = "efcc08021284e75f4d96d3581c5155a11f08fd63316b1938cbcb269c87f37feb";
   };
 
   buildInputs = [
     libmnl
     libnfnetlink
   ];
+
+  passthru = {
+    srcVerification = fetchurl {
+      failEarly = true;
+      pgpsigUrl = map (n: "${n}.sig") src.urls;
+      pgpKeyFingerprint = "C09D B206 3F1D 7034 BA61  52AD AB46 55A1 26D2 92E4";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "Userspace library providing an API to the in-kernel connection tracking state table";

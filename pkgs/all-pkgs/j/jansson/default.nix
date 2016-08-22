c01d010e32d36d5dce818e999627d92eada40316
William A. Kennington III <william@wkennington.com>
2016-08-22 15:04:34 -0700

    jansson: Add verification and multihash

--- pkgs/all-pkgs/j/jansson/default.nix
+++ pkgs/all-pkgs/j/jansson/default.nix
@@ -8,6 +8,8 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "http://www.digip.org/jansson/releases/${name}.tar.gz";
+    allowHashOutput = false;
+    multihash = "QmUqTpZSco2wWXPDsCrxkgjEBG2Qn21HuEU3c1dztVXf9K";
     sha256 = "1mvq9p85khsl818i4vbszyfab0fd45mdrwrxjkzw05mk1xcyc1br";
   };
 
@@ -18,6 +20,15 @@ stdenv.mkDerivation rec {
       sha256 = "99207e08752ee7627b33a0dda608b8e342794f51314838839102f8f331224a9b";
     })
   ];
+
+  passthru = {
+    srcVerification = fetchurl {
+      failEarly = true;
+      pgpsigUrls = map (n: "${n}.asc") src.urls;
+      pgpKeyFingerprint = "B5D6 953E 6D50 59ED 7ADA  0F2F D365 7D24 D058 434C";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
 
   meta = with stdenv.lib; {
     homepage = "http://www.digip.org/jansson/";

8ae16da6fdcc0ed31e718aabb9434da220759191
Cody W. Opel <codyopel@gmail.com>
2016-08-27 20:01:06 -0400

    xf86-input-wacom: add multihash & srcVerification

--- pkgs/all-pkgs/x/xf86-input-wacom/default.nix
+++ pkgs/all-pkgs/x/xf86-input-wacom/default.nix
@@ -12,6 +12,8 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://sourceforge/linuxwacom/${name}.tar.bz2";
+    multihash = "QmWVD3PFeRbH9dYbhWDzkWiSM58tt5V9fJwbUYVBdtPAKK";
+    allowHashOutput = false;
     sha256 = "24eef830744a388795a318ef743f19c451e394d9ef1332e98e2d54810a70f8e0";
   };
 
@@ -40,6 +42,18 @@ stdenv.mkDerivation rec {
       "--with-xorg-conf-dir=$out/share/X11/xorg.conf.d"
     )
   '';
+
+  passthru = {
+    srcVerification = fetchurl {
+      inherit (src)
+        outputHash
+        outputHashAlgo
+        urls;
+      failEarly = true;
+      pgpsigUrls = map (n: "${n}.sig") src.urls;
+      pgpKeyFingerprint = "9A12 ECCC 5383 CA2A F5B4  2CDC A6DC 6691 1B21 27D5";
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "Wacom digitizer driver for X11";

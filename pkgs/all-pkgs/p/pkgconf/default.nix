b773cb1ab3002cfd2acc53d4c7e6c9a9b0b4c9c4
Cody W. Opel <codyopel@gmail.com>
2016-08-28 21:38:39 -0400

    pkgconf: 2016-01-21 -> 1.0.1

--- pkgs/all-pkgs/p/pkgconf/default.nix
+++ pkgs/all-pkgs/p/pkgconf/default.nix
@@ -1,30 +1,26 @@
 { stdenv
-, autoreconfHook
 , fetchurl
 }:
 
 stdenv.mkDerivation rec {
-  name = "pkgconf-2016-01-21";
+  name = "pkgconf-1.0.1";
 
   src = fetchurl {
-    url = "https://github.com/pkgconf/pkgconf/archive/7e6fa325eb668c3462981a16fb4c36270832e00f.tar.gz";
-    sha256 = "0kqxbgfapg5x26308jq5d9qk35acj989skx4xc7nwkgffs948lyl";
+    url = "https://github.com/pkgconf/pkgconf/releases/download/${name}/"
+      + "${name}.tar.xz";
+    sha256 = "37db912cf060ed0a3113114515597f3c566d3ab796973c5c408691e20ec1fe20";
   };
-
-  nativeBuildInputs = [
-    autoreconfHook
-  ];
 
   postInstall = ''
     # The install is broken and is missing libpkgconfig/config.h
-    cp libpkgconf/config.h $out/include/pkgconf
+    cp -v libpkgconf/config.h $out/include/pkgconf
 
     # The header files expect themselves to be in libpkgconf
     # however they are installed to pkgconf
-    ln -s pkgconf $out/include/libpkgconf
+    ln -sv pkgconf $out/include/libpkgconf
 
     # We want compatability with pkg-config
-    ln -s pkgconf $out/bin/pkg-config
+    ln -sv pkgconf $out/bin/pkg-config
   '';
 
   meta = with stdenv.lib; {

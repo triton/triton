b936940ad95ab46fdaf69de4e85491c3977d2b98
Cody W. Opel <codyopel@gmail.com>
2016-08-28 22:46:07 -0400

    unifont: 9.0.01 -> 9.0.02

--- pkgs/data/fonts/unifont/default.nix
+++ pkgs/data/fonts/unifont/default.nix
@@ -1,28 +1,34 @@
 { stdenv, fetchurl, xorg }:
 
 stdenv.mkDerivation rec {
-  name = "unifont-${version}";
-  version = "9.0.01";
+  name = "unifont-9.0.02";
 
-  ttf = fetchurl {
-    url = "http://fossies.org/linux/unifont/font/precompiled/${name}.ttf";
-    sha256 = "0n2vdzrp86bjxfyqgmryrqckmjiiz4jvsfz9amgg3dv2p42y0dhd";
-  };
+  buildInputs = [
+    xorg.mkfontdir
+    xorg.mkfontscale
+  ];
 
-  pcf = fetchurl {
-    url = "http://fossies.org/linux/unifont/font/precompiled/${name}.pcf.gz";
-    sha256 = "1n3zff46pk6s2x5y7h76aq7h9wfq2acv77gpmxkhz5iwvbpxgb4z";
-  };
-
-  buildInputs = [ xorg.mkfontscale xorg.mkfontdir ];
-
-  phases = "installPhase";
+  phases = [
+    "installPhase"
+  ];
 
   installPhase =
-    ''
-      mkdir -p $out/share/fonts $out/share/fonts/truetype
-      cp -v ${pcf} $out/share/fonts/unifont.pcf.gz
-      cp -v ${ttf} $out/share/fonts/truetype/unifont.ttf
+    let
+      unifont_pcf = fetchurl {
+        url = "http://unifoundry.com/pub/${name}/font-builds/${name}.pcf.gz";
+        multihash = "Qmf7skRRpT8UXnEBSKGGahRKZDMRxecey4noUEtV9vzwQu";
+        sha256 = "29ac5e89b6f40537a8bd5f1460d3927cff8cfeb37975eddf8f26b85e975ca025";
+      };
+
+      unifont_ttf = fetchurl {
+        url = "http://unifoundry.com/pub/${name}/font-builds/${name}.ttf";
+        multihash = "QmU8DRxEN2SqwR8Vki5RNPdX4Wt9B5KkV28T1DCtpFFhLf";
+        sha256 = "753a9cb25da2f4e52df96f8b6ee9246c1b130cdc8ee3beaad6227b771f294291";
+      };
+    in ''
+      mkdir -pv $out/share/fonts $out/share/fonts/truetype
+      cp -v ${unifont_pcf} $out/share/fonts/unifont.pcf.gz
+      cp -v ${unifont_ttf} $out/share/fonts/truetype/unifont.ttf
       cd $out/share/fonts
       mkfontdir
       mkfontscale
@@ -31,10 +37,9 @@ stdenv.mkDerivation rec {
   meta = with stdenv.lib; {
     description = "Unicode font for Base Multilingual Plane";
     homepage = http://unifoundry.com/unifont.html;
-
     # Basically GPL2+ with font exception.
     license = http://unifoundry.com/LICENSE.txt;
-    maintainers = [ maintainers.rycee ];
+    maintainers = with maintainers; [ ];
     platforms = platforms.all;
   };
 }

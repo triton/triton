08dd1db5a624146764dfcc8ae6650c78e1e25171
Cody W. Opel <codyopel@gmail.com>
2016-08-30 21:41:53 -0400

    libyaml: 0.1.6 -> 0.1.7

--- pkgs/all-pkgs/l/libyaml/default.nix
+++ pkgs/all-pkgs/l/libyaml/default.nix
@@ -3,26 +3,21 @@
 , fetchurl
 }:
 
+let
+  version = "0.1.7";
+in
 stdenv.mkDerivation rec {
-  name = "libyaml-${version}";
-  version = "0.1.6";
+  name = "yaml-${version}";
 
   src = fetchurl {
     url = "http://pyyaml.org/download/libyaml/yaml-${version}.tar.gz";
-    sha256 = "0j9731s5zjb8mjx7wzf6vh7bsqi38ay564x6s9nri2nh9cdrg9kx";
+    multihash = "QmeyiLzTi5ujorYnG5LhNHLA2o4unnicBzKQabmBmn2Z4S";
+    sha256 = "8088e457264a98ba451a90b8661fcb4f9d6f478f7265d48322a196cec2480729";
   };
 
-  patches = [
-    (fetchTritonPatch {
-      rev = "5f0bea0839c25b76893bdfccb73df7646a5198ab";
-      file = "libyaml/CVE-2014-9130.patch";
-      sha256 = "30546a280c4f9764a93ff5f4f88671a02222e9886e7f63ee19aebf1b2086a7fe";
-    })
-  ];
-
   meta = with stdenv.lib; {
-    homepage = http://pyyaml.org/;
     description = "A YAML 1.1 parser and emitter written in C";
+    homepage = http://pyyaml.org/;
     license = licenses.mit;
     maintainers = with maintainers; [
       wkennington

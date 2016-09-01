f2d7becdd0e1a8dba5a5a5578963d28e991f72eb
Cody W. Opel <codyopel@gmail.com>
2016-09-01 06:49:27 -0400

    zope-interface: 4.2.0 -> 4.3.1

--- pkgs/all-pkgs/z/zope-interface/default.nix
+++ pkgs/all-pkgs/z/zope-interface/default.nix
@@ -6,7 +6,7 @@
 }:
 
 let
-  version = "4.2.0";
+  version = "4.3.1";
 in
 buildPythonPackage rec {
   name = "zope.interface-${version}";
@@ -14,7 +14,7 @@ buildPythonPackage rec {
   src = fetchPyPi {
     package = "zope.interface";
     inherit version;
-    sha256 = "36762743940a075283e1fb22a2ec9e8231871dace2aa00599511ddc4edf0f8f9";
+    sha256 = "320920cedb07666fd4022f6a0fcd4a44551133a8415c98eac0873b753bb5a70c";
   };
 
   buildInputs = [

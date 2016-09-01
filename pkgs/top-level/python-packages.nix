be7a417ea3ac59ad1fd11c1542abc2de6942afb6
Cody W. Opel <codyopel@gmail.com>
2016-09-01 06:57:27 -0400

    ply: 3.8 -> 3.9

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1127,11 +1127,13 @@ zope-interface = callPackage ../all-pkgs/z/zope-interface { };
    };
 
    ply = buildPythonPackage (rec {
-     name = "ply-3.8";
+     name = "ply-${version}";
+     version = "3.9";
 
-     src = pkgs.fetchurl {
-       url = "https://pypi.python.org/packages/source/p/ply/${name}.tar.gz";
-       sha256 = "e7d1bdff026beb159c9942f7a17e102c375638d9478a7ecd4cc0c76afd8de0b8";
+     src = fetchPyPi {
+       package = "ply";
+       inherit version;
+       sha256 = "0d7e2940b9c57151392fceaa62b0865c45e06ce1e36687fd8d03f011a907f43e";
      };
 
      checkPhase = ''
@@ -1143,25 +1145,8 @@ zope-interface = callPackage ../all-pkgs/z/zope-interface { };
 
      meta = {
        homepage = http://www.dabeaz.com/ply/;
-
-       description = "PLY (Python Lex-Yacc), an implementation of the lex and yacc parsing tools for Python";
-
-       longDescription = ''
-         PLY is an implementation of lex and yacc parsing tools for Python.
-         In a nutshell, PLY is nothing more than a straightforward lex/yacc
-         implementation.  Here is a list of its essential features: It's
-         implemented entirely in Python; It uses LR-parsing which is
-         reasonably efficient and well suited for larger grammars; PLY
-        provides most of the standard lex/yacc features including support for
-         empty productions, precedence rules, error recovery, and support for
-         ambiguous grammars; PLY is straightforward to use and provides very
-         extensive error checking; PLY doesn't try to do anything more or less
-         than provide the basic lex/yacc functionality.  In other words, it's
-         not a large parsing framework or a component of some larger system.
-       '';
-
+       description = "Python implementation of the lex & yacc parsing tools";
        license = licenses.bsd3;
-
        maintainers = [ ];
      };
    });

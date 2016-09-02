318c58d76f72d8f51d9accbdd14a54b4c1db2498
William A. Kennington III <william@wkennington.com>
2016-09-02 15:51:20 -0700

    swig: Fix multihash

--- pkgs/all-pkgs/s/swig/sources.nix
+++ pkgs/all-pkgs/s/swig/sources.nix
@@ -1,12 +1,12 @@
 {
   "2" = {
     version = "2.0.12";
-    multihash = "QmTS4GA6yAUYFDQhV27FZ131qMrN7aMs6NKuFVZfa4gFNp";
+    multihash = "QmdV5MEJsdj69uNgE3WJBEBNMtTaYBcT2rK1gfUQhSRa4c";
     sha256 = "65e13f22a60cecd7279c59882ff8ebe1ffe34078e85c602821a541817a4317f7";
   };
   "3" = {
     version = "3.0.10";
-    multihash = "QmTS4GA6yAUYFDQhV27FZ131qMrN7aMs6NKuFVZfa4gFNp";
+    multihash = "QmRR4RtPpAunX5fQK5ooUygZmHDE35hqQE8YuxwS1eiWiM";
     sha256 = "2939aae39dec06095462f1b95ce1c958ac80d07b926e48871046d17c0094f44c";
   };
 }

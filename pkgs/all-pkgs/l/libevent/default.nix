5048777e16ba6983bf30fc8165cd8b275028fe5d
William A. Kennington III <william@wkennington.com>
2016-09-04 15:18:44 -0700

    libevent: Cleanup sources

--- pkgs/all-pkgs/l/libevent/default.nix
+++ pkgs/all-pkgs/l/libevent/default.nix
@@ -1,22 +1,27 @@
 { stdenv
-, autoreconfHook
 , fetchurl
 , python
 
 , openssl
 }:
 
-stdenv.mkDerivation rec {
-  name = "libevent-${version}";
+let
   version = "2.0.22";
 
+  tarballUrls = version: [
+    "https://github.com/libevent/libevent/releases/download/release-${version}-stable/libevent-${version}-stable.tar.gz"
+  ];
+in
+stdenv.mkDerivation rec {
+  name = "libevent-${version}";
+
   src = fetchurl {
-    url = "mirror://sourceforge/levent/libevent-${version}-stable.tar.gz";
+    urls = tarballUrls version;
+    hashOutput = false;
     sha256 = "18qz9qfwrkakmazdlwxvjmw8p76g70n3faikwvdwznns1agw9hki";
   };
 
   nativeBuildInputs = [
-    autoreconfHook
     python
   ];
 
@@ -27,6 +32,17 @@ stdenv.mkDerivation rec {
   patchPhase = ''
     patchShebangs event_rpcgen.py
   '';
+
+  passthru = {
+    srcVerification = fetchurl rec {
+      failEarly = true;
+      urls = tarballUrls "2.0.22";
+      pgpsigUrls = map (n: "${n}.asc") urls;
+      pgpKeyFingerprint = "B35B F85B F194 89D0 4E28  C33C 2119 4EBB 1657 33EA";
+      inherit (src) outputHashAlgo;
+      outputHash = "18qz9qfwrkakmazdlwxvjmw8p76g70n3faikwvdwznns1agw9hki";
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "Event notification library";

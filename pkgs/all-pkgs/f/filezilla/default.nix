f9041cf1c7d70b3c23227458a7477bd99cbde381
William A. Kennington III <william@wkennington.com>
2016-09-06 15:17:45 -0700

    filezilla: Fix url

--- pkgs/all-pkgs/f/filezilla/default.nix
+++ pkgs/all-pkgs/f/filezilla/default.nix
@@ -15,14 +15,14 @@
 let
   version = "3.21.0";
 
-  baseFileUrl = "mirror://sourceforge/project/filezilla/FileZilla_Client/${version}/FileZilla_${version}";
+  baseFileUrl = "mirror://sourceforge/filezilla/FileZilla_Client/${version}/FileZilla_${version}";
 in
 stdenv.mkDerivation rec {
   name = "filezilla-${version}";
 
   src = fetchurl {
     url = "${baseFileUrl}_src.tar.bz2";
-    multihash = "QmW8kEaW4bwN9bAKDLmDW7VCZcXiCv9T65ha9GiJGvUJYw";
+    #multihash = "QmW8kEaW4bwN9bAKDLmDW7VCZcXiCv9T65ha9GiJGvUJYw";
     sha256 = "209bcdfcd60ae2278fa2fa8d99421682e0db146add9e96cb1e8455c3378c80e4";
   };
 

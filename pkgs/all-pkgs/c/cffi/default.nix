700115e69c66e824e9333c3f5819ba0050988b37
Cody W. Opel <codyopel@gmail.com>
2016-09-07 06:36:15 -0400

    cffi: 1.7.0 -> 1.8.2

--- pkgs/all-pkgs/c/cffi/default.nix
+++ pkgs/all-pkgs/c/cffi/default.nix
@@ -4,18 +4,27 @@
 
 , libffi
 , pycparser
+, python
+
+, docs ? false
+  , sphinx
+
+, pytest
 }:
 
 let
-  version = "1.7.0";
+  inherit (stdenv.lib)
+    optionals;
+
+  version = "1.8.2";
 in
-buildPythonPackage {
+buildPythonPackage rec {
   name = "cffi-${version}";
 
   src = fetchPyPi {
     package = "cffi";
     inherit version;
-    sha256 = "6ed5dd6afd8361f34819c68aaebf9e8fc12b5a5893f91f50c9e50c8886bb60df";
+    sha256 = "2b636db1a179439d73ae0a090479e179a43df5d4eddc7e4c4067f960d4038530";
   };
 
   propagatedBuildInputs = [
@@ -24,10 +33,28 @@ buildPythonPackage {
 
   buildInputs = [
     libffi
+  ] ++ optionals docs [
+    sphinx
+  ] ++ optionals doCheck [
+    pytest
   ];
 
+  preFixup = /* Simple test to make sure module was built */ ''
+    ${python.interpreter} -c "import _cffi_backend as backend"
+  '';
+
+  checkPhase = ''
+    py.test
+  '';
+
+  doCheck = false;
+
   meta = with stdenv.lib; {
+    description = "Foreign Function Interface for Python calling C code";
+    homepage = http://cffi.readthedocs.org/;
+    license = licenses.mit;
     maintainers = with maintainers; [
+      codyopel
       wkennington
     ];
     platforms = with platforms;

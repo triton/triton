bc2471ac1c1e88cba6f318cbea89246c2aac86ed
Cody W. Opel <codyopel@gmail.com>
2016-09-08 06:53:31 -0400

    clutter-gst: 3.0.18 -> 3.0.20

--- pkgs/all-pkgs/c/clutter-gst/sources.nix
+++ pkgs/all-pkgs/c/clutter-gst/sources.nix
@@ -5,7 +5,7 @@
     sha256 = "0f90fkywwn9ww6a8kfjiy4xx65b09yaj771jlsmj2w4khr0zhi59";
   };
   "3.0" = {
-    version = "3.0.18";
-    sha256 = "0aec0d0c6020cd19a5bb0dab1165a92748f81a9a3acdfabb0f966d5f53bc8093";
+    version = "3.0.20";
+    sha256 = "a0011787ed2a2dafe914f973c1ede283d40b8eb75123f4a7b608ae6d1cc066c9";
   };
 }

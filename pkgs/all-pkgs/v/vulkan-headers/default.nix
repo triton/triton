5f97e12e0b2f02da1bbff84a9a8d94238d5f90b8
Cody W. Opel <codyopel@gmail.com>
2016-09-09 17:44:07 -0400

    vulkan-headers: 1.0.25 -> 1.0.26

--- pkgs/all-pkgs/v/vulkan-headers/default.nix
+++ pkgs/all-pkgs/v/vulkan-headers/default.nix
@@ -3,14 +3,14 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "vulkan-headers-1.0.25";
+  name = "vulkan-headers-1.0.26";
 
   src = fetchFromGitHub {
     version = 1;
     owner = "KhronosGroup";
     repo = "Vulkan-Docs";
-    rev = "85184f305afe0f2e9e99cc9525e8ce25c32e74e0";
-    sha256 = "8aee8851c41bb7e2a20201ccbf9184dbed29dec3c2408b49221a41d004135eef";
+    rev = "eaea7d27099cf7deca4848f9536c9f41269fbd90";
+    sha256 = "78235ba3c61878b7e401317e08957b3210558d9c56846e32d8ff1d8c76506bf0";
   };
 
   installPhase = ''

e9d0d16d6e6644e8730ecc3c4e10b6ca75b12402
Cody W. Opel <codyopel@gmail.com>
2016-09-10 19:30:21 -0400

    grilo: 0.3.1 -> 0.3.2

--- pkgs/all-pkgs/g/grilo/default.nix
+++ pkgs/all-pkgs/g/grilo/default.nix
@@ -15,20 +15,18 @@
 
 let
   inherit (stdenv.lib)
-    enFlag;
-in
+    boolEn;
 
+  channel = "0.3";
+  version = "0.3.2";
+in
 stdenv.mkDerivation rec {
   name = "grilo-${version}";
-  versionMajor = "0.3";
-  versionMinor = "1";
-  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/grilo/${versionMajor}/${name}.tar.xz";
-    sha256Url = "mirror://gnome/sources/grilo/${versionMajor}/"
-      + "${name}.sha256sum";
-    sha256 = "ebbdc61dc7920a8cac436895e8625a0ee64d6a4b352987fb5d361ef87243cd4c";
+    url = "mirror://gnome/sources/grilo/${channel}/${name}.tar.xz";
+    hashOutput = false;
+    sha256 = "f26f684a5d76aea8dbce136750bc67d2170b36575f109292fbb78ae99ec87f5b";
   };
 
   nativeBuildInputs = [
@@ -55,17 +53,29 @@ stdenv.mkDerivation rec {
     "--disable-maintainer-mode"
     "--disable-test-ui"
     "--enable-grl-net"
-    (enFlag "grl-pls" (totem-pl-parser != null) null)
+    "--${boolEn (totem-pl-parser != null)}-grl-pls"
     "--disable-debug"
     # Flag is not a boolean
     #"--disable-tests"
     "--disable-gtk-doc"
     "--disable-gtk-doc-html"
     "--disable-gtk-doc-pdf"
-    (enFlag "introspection" (gobject-introspection != null) null)
-    (enFlag "vala" (vala != null) null)
+    "--${boolEn (gobject-introspection != null)}-introspection"
+    "--${boolEn (vala != null)}-vala"
     "--enable-nls"
   ];
+
+  passthru = {
+    srcVerification = fetchurl {
+      inherit (src)
+        outputHash
+        outputHashAlgo
+        urls;
+      sha256Url = "https://download.gnome.org/sources/grilo/${channel}/"
+        + "${name}.sha256sum";
+      failEarly = true;
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "A framework for easy media discovery and browsing";

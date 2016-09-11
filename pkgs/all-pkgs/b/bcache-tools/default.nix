e45aca6a97d816472e7a47c342a17c6a7d3ec105
William A. Kennington III <william@wkennington.com>
2016-09-10 17:05:26 -0700

    bcache-tools: 2016-08-30 -> 2016-09-05

--- pkgs/all-pkgs/b/bcache-tools/default.nix
+++ pkgs/all-pkgs/b/bcache-tools/default.nix
@@ -15,12 +15,12 @@ let
     "stable" = {
       version = "1.0.8";
       rev = "03bc5a32e5c14b75fb757902754480b8bcc3069e";
-      sha256 = "496ae8691eb9c5a233bd99ed7984cbe129d702c322dc540143cd4012b24a4dad";
+      sha256 = "339e97128db8a70c8da4cd51584634dda49d569314a0078361d075bf023580e6";
     };
     "dev" = {
-      version = "2016-08-30";
-      rev = "580ec0497a545bb52de204023928d4939dd14eb4";
-      sha256 = "63800e6c504a0186080870a32ff8a0e90a76d5cb95f30a541810b4cd7388f9e3";
+      version = "2016-09-05";
+      rev = "837a476cc139167fc483016f0a2635a048f7709e";
+      sha256 = "da105b85089621bdb8b69236538d1e5d6d3ea221b19a9cb727264da4f5ae2325";
     };
   };
 
@@ -37,7 +37,7 @@ stdenv.mkDerivation {
   name = "bcache-tools-${version}";
 
   src = fetchFromGitHub {
-    version = 1;
+    version = 2;
     owner = "wkennington";
     repo = "bcache-tools";
     inherit rev sha256;

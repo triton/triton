b13d93c785f453ba8eb50aed3b0577fa7cceb1dd
William A. Kennington III <william@wkennington.com>
2016-09-12 13:35:49 -0700

    mpfr: 3.1.4p3 -> 3.1.4p6

--- pkgs/all-pkgs/m/mpfr/patches.nix
+++ pkgs/all-pkgs/m/mpfr/patches.nix
@@ -2,4 +2,7 @@
   "patch01" = "0hb5rnk6rdyhml2k6h6zci3x5fsk6q7llbw3s53zgbzy0krw60ws";
   "patch02" = "1wg0pqvzqlc0899hjxirgp0fh1hgsw1h0bjk7zhkgxfm0aj3zrjv";
   "patch03" = "02s7f18l6ykhg0mjd5hzbhzkqizahimlkxks4zp9rdsgz7dvz7wi";
+  "patch04" = "1vhff0xxaq4jnyq1mn69j31qibwgdnajihmh13wn4yjwgczakx0h";
+  "patch05" = "02yvrv94c1mjdqc97vjjyfq3cifc7jj9c7cmq9gra8idf91qfcf6";
+  "patch06" = "0n5pnwizx60ji6aj0kggrkyz3w7zjvxdhiajnsvxjlngx2kx339l";
 }

4ac721de7b33982a40e605940a32f6a578dcbd6c
William A. Kennington III <william@wkennington.com>
2016-09-16 01:37:10 -0700

    vlc: Fix https

--- pkgs/all-pkgs/v/vlc/default.nix
+++ pkgs/all-pkgs/v/vlc/default.nix
@@ -88,7 +88,7 @@ stdenv.mkDerivation rec {
   version = "2.2.4";
 
   src = fetchurl {
-    url = "https://get.videolan.org/vlc/${version}/${name}.tar.xz";
+    url = "http://get.videolan.org/vlc/${version}/${name}.tar.xz";
     hashOutput = false;
     sha256 = "1632e91d2a0087e0ef4c3fb4c95c3c2890f7715a9d1d43ffd46329f428cf53be";
   };

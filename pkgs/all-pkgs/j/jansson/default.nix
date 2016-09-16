0d3797b261af7e67ae87cdc4abeb04ed9e239a2f
William A. Kennington III <william@wkennington.com>
2016-09-16 01:04:44 -0700

    jansson: 2.7 -> 2.8

--- pkgs/all-pkgs/j/jansson/default.nix
+++ pkgs/all-pkgs/j/jansson/default.nix
@@ -1,25 +1,16 @@
 { stdenv
-, fetchTritonPatch
 , fetchurl
 }:
 
 stdenv.mkDerivation rec {
-  name = "jansson-2.7";
+  name = "jansson-2.8";
 
   src = fetchurl {
     url = "http://www.digip.org/jansson/releases/${name}.tar.gz";
     hashOutput = false;
-    multihash = "QmUqTpZSco2wWXPDsCrxkgjEBG2Qn21HuEU3c1dztVXf9K";
-    sha256 = "1mvq9p85khsl818i4vbszyfab0fd45mdrwrxjkzw05mk1xcyc1br";
+    multihash = "QmWbiFgbikuSukX5ZkbxDN1ia4waEiZhENTZzHReJSriZt";
+    sha256 = "cef2e9e3190ef8b16bfa4c5ccdf131bd460cd1aaf6e6dad9cd84f4b3ab40fb6c";
   };
-
-  patches = [
-    (fetchTritonPatch {
-      rev = "5bb60ada3e49df19daba41242380880a0ff718c5";
-      file = "jansson/CVE-2016-4425.patch";
-      sha256 = "99207e08752ee7627b33a0dda608b8e342794f51314838839102f8f331224a9b";
-    })
-  ];
 
   passthru = {
     srcVerification = fetchurl {

f04021b254ac13afb3282854ea09fecea480c8d4
Cody W. Opel <codyopel@gmail.com>
2016-09-18 21:27:52 -0400

    ffmpeg(head): fix attr

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -946,7 +946,7 @@ ffmpeg_3 = callPackageAlias "ffmpeg_3-1" { };
 ffmpeg_head = pkgs.ffmpeg_generic {
   channel = "9.9";
   # Use latest dependencies
-  libvpx = libvpx_head;
+  libvpx = pkgs.libvpx_head;
 };
 ffmpeg = callPackageAlias "ffmpeg_3" { };
 

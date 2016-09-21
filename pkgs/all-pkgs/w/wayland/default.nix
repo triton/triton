3248011f722562dd797588b7eb6945a3277858fc
William A. Kennington III <william@wkennington.com>
2016-09-21 14:44:58 -0700

    wayland: 1.11.0 -> 1.12.0

--- pkgs/all-pkgs/w/wayland/default.nix
+++ pkgs/all-pkgs/w/wayland/default.nix
@@ -18,7 +18,7 @@ let
     boolEn
     optionals;
 
-  version = "1.11.0";
+  version = "1.12.0";
 in
 stdenv.mkDerivation rec {
   name = "wayland-${version}";
@@ -26,7 +26,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "https://wayland.freedesktop.org/releases/${name}.tar.xz";
     hashOutput = false;
-    sha256 = "9540925f7928becfdf5e3b84c70757f6589bf1ceef09bea78784d8e4772c0db0";
+    sha256 = "d6b4135cba0188abcb7275513c72dede751d6194f6edc5b82183a3ba8b821ab1";
   };
 
   nativeBuildInputs = [ ]

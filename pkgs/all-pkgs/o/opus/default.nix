8ec948089286dfde339c38833fcce7baf32af762
Cody W. Opel <codyopel@gmail.com>
2016-09-24 05:50:01 -0400

    opus: refactor

--- pkgs/all-pkgs/o/opus/default.nix
+++ pkgs/all-pkgs/o/opus/default.nix
@@ -8,40 +8,44 @@ let
   inherit (stdenv)
     targetSystem;
   inherit (stdenv.lib)
-    elem
-    enFlag
-    platforms;
+    boolEn;
 in
-
 stdenv.mkDerivation rec {
-  name = "opus-${version}";
-  version = "1.1.3";
+  name = "opus-1.1.3";
 
   src = fetchurl {
-    url = "http://downloads.xiph.org/releases/opus/opus-${version}.tar.gz";
-    sha256Url = "http://downloads.xiph.org/releases/opus/SHA256SUMS.txt";
+    url = "http://downloads.xiph.org/releases/opus/${name}.tar.gz";
+    hashOutput = false;
     sha256 = "58b6fe802e7e30182e95d0cde890c0ace40b6f125cffc50635f0ad2eef69b633";
   };
 
   configureFlags = [
     "--disable-maintainer-mode"
-    (enFlag "fixed-point" fixedPoint null)
-    (enFlag "float-api" (!fixedPoint) null)
+    "--${boolEn fixedPoint}-fixed-point"
+    "--${boolEn (!fixedPoint)}-float-api"
     # non-Opus modes, e.g. 44.1 kHz & 2^n frames
     "--enable-custom-modes"
     "--disable-float-approx"
     "--enable-asm"
     "--enable-rtcd"
-    # Enable intrinsics optimizations for ARM(float) X86(fixed)
-    (enFlag "intrinsics" (
-      (elem targetSystem platforms.arm-all && !fixedPoint)
-      || (elem targetSystem platforms.x86-all && fixedPoint)) null)
+    "--enable-intrinsics"
     "--disable-assertions"
     "--disable-fuzzing"
     "--enable-ambisonics"
     "--disable-doc"
     "--disable-extra-programs"
   ];
+
+  passthru = {
+    srcVerification = fetchurl {
+      inherit (src)
+        outputHash
+        outputHashAlgo
+        urls;
+      sha256Url = "http://downloads.xiph.org/releases/opus/SHA256SUMS.txt";
+      failEarly = true;
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "Versatile codec designed for speech and audio transmission";

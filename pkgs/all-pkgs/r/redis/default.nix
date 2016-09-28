2833fb73aab4533ededea246816264150b7e1db2
Cody W. Opel <codyopel@gmail.com>
2016-09-28 08:49:57 -0400

    redis: 3.2.3 -> 3.2.4

--- pkgs/all-pkgs/r/redis/default.nix
+++ pkgs/all-pkgs/r/redis/default.nix
@@ -1,19 +1,18 @@
 { stdenv
 , fetchurl
-
-, jemalloc
 }:
 
 let
-  version = "3.2.3";
+  version = "3.2.4";
 in
 stdenv.mkDerivation rec {
   name = "redis-${version}";
 
   src = fetchurl {
     url = "http://download.redis.io/releases/${name}.tar.gz";
-    sha1Confirm = "92d6d93ef2efc91e595c8bf578bf72baff397507";
-    sha256 = "674e9c38472e96491b7d4f7b42c38b71b5acbca945856e209cb428fbc6135f15";
+    # https://github.com/antirez/redis-hashes/blob/master/README
+    sha1Confirm = "f0fe685cbfdb8c2d8c74613ad8a5a5f33fba40c9";
+    sha256 = "2ad042c5a6c508223adeb9c91c6b1ae091394b4026f73997281e28914c9369f1";
   };
 
   preBuild = ''

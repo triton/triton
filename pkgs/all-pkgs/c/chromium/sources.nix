d0e9a838ce6316ca6ea1849338ad3b468130fd33
Cody W. Opel <codyopel@gmail.com>
2016-10-01 10:36:23 -0400

    chromium: add sources.nix for google-chrome

---
+++ pkgs/all-pkgs/c/chromium/sources.nix
@@ -0,0 +1,15 @@
+{ }:
+{
+  "stable" = {
+    version = "53.0.2785.143";
+    sha256 = "c52a58b79bfb27bb87e4a0a6ff213001485fbc747657b290f75d39ddce07dcc3";
+  };
+  "beta" = {
+    version = "54.0.2840.34";
+    sha256 = "1wpcl6cgiv02s3zgxvgsmrws454g81lnvyfjmhhcdckkwyljcv5l";
+  };
+  "unstable" = {
+    version = "55.0.2868.3";
+    sha256 = "1lbpj1wm35l2qf4fxfivd9drwwdrylv3knh8db889dyz5hrh3j5y";
+  };
+}

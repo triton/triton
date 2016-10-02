0327e3e32be86f34de404737626220720c872f98
Cody W. Opel <codyopel@gmail.com>
2016-10-02 16:54:02 -0400

    ffmpeg: 3.1.3 -> 3.1.4

--- pkgs/all-pkgs/f/ffmpeg/sources.nix
+++ pkgs/all-pkgs/f/ffmpeg/sources.nix
@@ -6,9 +6,9 @@
     sha256 = "97be8a6c1cab2c52015544f76d40f6fd0babcb1b40cf601b4e57fb818564157c";
   };
   "3.1" = {
-    version = "3.1.3";
-    multihash = "QmUMHcQZuNAgM4ygG1gTiVyCQHm5kxDRwoCVSrJPYEq7Tw";
-    sha256 = "f8575c071e2a64437aeb70c8c030b385cddbe0b5cde20c9b18a6def840128822";
+    version = "3.1.4";
+    multihash = "QmRguYcMCTyft1HXTnV4MkD2VLm4pEcM69PYME3QTNwiJd";
+    sha256 = "a80cb378dda5c9bbcdbd62a99bdec0e4eedbcb47f290e72845af4855c1146b5b";
   };
   "9.9" = { # Git
     version = "2016.09.23";

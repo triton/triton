c9f2b0f7646070203d2833240197cd96f7492e46
William A. Kennington III <william@wkennington.com>
2016-10-03 11:13:25 -0700

    nettle: 3.2 -> 3.3

--- pkgs/all-pkgs/n/nettle/default.nix
+++ pkgs/all-pkgs/n/nettle/default.nix
@@ -1,23 +1,27 @@
 { stdenv
-, autoreconfHook
-, fetchTritonPatch
 , fetchurl
 , gnum4
 
 , gmp
 }:
 
+let
+  tarballUrls = version: [
+    "mirror://gnu/nettle/nettle-${version}.tar.gz"
+  ];
+
+  version = "3.3";
+in
 stdenv.mkDerivation rec {
-  name = "nettle-3.2";
+  name = "nettle-${version}";
 
   src = fetchurl {
-    url = "mirror://gnu/nettle/${name}.tar.gz";
-    sha256 = "15wxhk52yc62rx0pddmry66hqm6z5brrrkx4npd3wh9nybg86hpa";
+    urls = tarballUrls version;
+    hashOutput = false;
+    sha256 = "46942627d5d0ca11720fec18d81fc38f7ef837ea4197c1f630e71ce0d470b11e";
   };
 
   nativeBuildInputs = [
-    # FIXME: remove once CVE is patched upstream
-    autoreconfHook
     gnum4
   ];
 
@@ -25,15 +29,18 @@ stdenv.mkDerivation rec {
     gmp
   ];
 
-  patches = [
-    (fetchTritonPatch {
-      rev = "553ec8a8988737c3dcacaa987746db09cca006a2";
-      file = "nettle/nettle-3.2-CVE-2016-6489.patch";
-      sha256 = "0bc6f90da2e682ebdfe1444ee83a591c4a47ba9ef70cfa955530a626e4f5888d";
-    })
-  ];
-
   doCheck = true;
+
+  passthru = {
+    srcVerification = fetchurl rec {
+      failEarly = true;
+      urls = tarballUrls "3.3";
+      pgpsigUrls = map (n: "${n}.sig") urls;
+      pgpKeyFingerprint = "343C 2FF0 FBEE 5EC2 EDBE  F399 F359 9FF8 28C6 7298";
+      inherit (src) outputHashAlgo;
+      outputHash = "46942627d5d0ca11720fec18d81fc38f7ef837ea4197c1f630e71ce0d470b11e";
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "Cryptographic library";

ca9c184ffd4c15bba839781da2e1dafe93ab862b
Cody W. Opel <codyopel@gmail.com>
2016-10-03 13:26:56 -0400

    gegl: refactor
    + Add x86 simd
    + Disable (g)exiv, requires mrg
    + Use autoreconf to fix dependency detection
    + Use gdk-pixbif_unwrapped

--- pkgs/all-pkgs/g/gegl/default.nix
+++ pkgs/all-pkgs/g/gegl/default.nix
@@ -1,12 +1,14 @@
 { stdenv
+, autoreconfHook
 , fetchurl
 , intltool
+, lib
 
 , babl
 , cairo
 , exiv2
-, ffmpeg
-, gdk-pixbuf
+, ffmpeg_2
+, gdk-pixbuf_unwrapped
 , gexiv2
 , glib
 , gobject-introspection
@@ -21,34 +23,44 @@
 , libwebp
 , openexr
 , pango
-#, v4l_lib
+, v4l_lib
 , vala
 }:
 
 let
-  versionMajor = "0.3";
-  versionMinor = "8";
-  version = "${versionMajor}.${versionMinor}";
+  inherit (stdenv)
+    targetSystem;
+  inherit (lib)
+    boolEn
+    boolWt
+    elem
+    platforms;
+
+  channel = "0.3";
+  version = "${channel}.8";
 in
 stdenv.mkDerivation rec {
   name = "gegl-${version}";
 
   src = fetchurl {
-    url = "https://download.gimp.org/pub/gegl/${versionMajor}/${name}.tar.bz2";
+    url = "https://download.gimp.org/pub/gegl/${channel}/${name}.tar.bz2";
     sha256 = "06ca9e67a59da026eb941b9d323269d7c19a922f1e478acdd3791a0eef8b229b";
   };
 
   nativeBuildInputs = [
+    # Pre-generated autoconf/automake files are outdated and fail
+    # to detect libv4l & ffmpeg correctly.
+    autoreconfHook
     intltool
   ];
 
   buildInputs = [
     babl
     cairo
-    exiv2
-    ffmpeg
-    gdk-pixbuf
-    gexiv2
+    #exiv2
+    ffmpeg_2
+    gdk-pixbuf_unwrapped
+    #gexiv2
     glib
     gobject-introspection
     jasper
@@ -67,22 +79,58 @@ stdenv.mkDerivation rec {
   ];
 
   configureFlags = [
+    "--disable-maintainer-mode"
+    "--enable-largefile"
+    "--disable-debug"
+    "--disable-profile"
     "--disable-docs"
+    "--${boolEn (elem targetSystem platforms.x86-all)}-mmx"
+    "--${boolEn (elem targetSystem platforms.x86-all)}-sse"
+    "--${boolEn (gobject-introspection != null)}-introspection"
+    "--disable-glibtest"
+    "--enable-nls"
+    "--disable-gtk-doc"
+    "--disable-gtk-doc-html"
+    "--disable-gtk-doc-pdf"
+    "--disable-workshop"
+    "--${boolEn (vala != null)}-vala"
     "--without-mrg"
+    # Requres --with-mrg
+    #"--${boolWt (gexiv2 != null && mrg != null)}-gexiv2"
+    "--without-gexiv2"
+    "--${boolWt (cairo != null)}-cairo"
+    "--${boolWt (pango != null)}-pango"
+    "--${boolWt (pango != null)}-pangocairo"
+    "--${boolWt (gdk-pixbuf_unwrapped != null)}-gdk-pixbuf"
     "--without-lensfun"
+    "--${boolWt (librsvg != null)}-librsvg"
+    # Requires --with-libv4l
+    #"--${boolWt (v4l_lib != null)}-libv4l2"
+    "--without-libv4l2"
+    "--${boolWt (openexr != null)}-openexr"
     "--without-sdl"
+    "--${boolWt (libraw != null)}-libraw"
+    "--${boolWt (jasper != null)}-jasper"
     "--without-graphviz"
     "--without-lua"
+    "--${boolWt (ffmpeg_2 != null)}-libavformat"
+    #"--${boolWt (v4l_lib != null)}-libv4l"
+    "--without-libv4l"
+    "--${boolWt (lcms2 != null)}-lcms"
     "--without-libspiro"
+    #"--${boolWt (exiv2 != null)}-exiv2"
+    "--without-exit2"
     "--without-umfpack"
-    "--without-libv4l"  # This is currently broken
+    "--${boolWt (libtiff != null)}-libtiff"
+    "--${boolWt (libwebp != null)}-webp"
   ];
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Graph-based image processing framework";
     homepage = http://www.gegl.org;
     license = licenses.gpl3;
     maintainers = with maintainers; [
+      codyopel
       wkennington
     ];
     platforms = with platforms;

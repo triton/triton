e11e63e769c5c65dfe90aa1dfba014ef32bb916a
Cody W. Opel <codyopel@gmail.com>
2016-10-04 10:54:00 -0400

    gstreamer: refactor setup-hook.sh

--- pkgs/all-pkgs/g/gstreamer/setup-hook.sh
+++ pkgs/all-pkgs/g/gstreamer/setup-hook.sh
@@ -1,9 +1,7 @@
 addGstreamer1LibPath() {
-
-  if [[ -d "${1}/lib/gstreamer-1.0" ]] ; then
-    export GST_PLUGIN_PATH="${GST_PLUGIN_PATH}${GST_PLUGIN_PATH:+:}${1}/lib/gstreamer-1.0"
+  if [ -d "${1}/lib/gstreamer-1.0" ] ; then
+    GST_PLUGIN_PATH="${GST_PLUGIN_PATH}${GST_PLUGIN_PATH:+:}${1}/lib/gstreamer-1.0"
   fi
-
 }
 
 envHooks+=('addGstreamer1LibPath')

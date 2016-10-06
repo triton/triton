142fbdb1bf3e3219e001733cd8c32ef75fbd3834
Cody W. Opel <codyopel@gmail.com>
2016-10-06 16:41:25 -0400

    gnome-themes-standard: 3.22.1 -> 3.22.2

--- pkgs/all-pkgs/g/gnome-themes-standard/sources.nix
+++ pkgs/all-pkgs/g/gnome-themes-standard/sources.nix
@@ -1,7 +1,7 @@
 { }:
 {
   "3.22" = {
-    version = "3.22.1";
-    sha256 = "90f6f4e79eaa42e424fa35144cdbcb5db93db56e73200ac045742ba320febb54";
+    version = "3.22.2";
+    sha256 = "b34516cd59b873c187c1897c25bac3b9ce2d30a472f1fd7ae9d7105d93e17da5";
   };
 }

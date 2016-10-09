251fce3dbab289648627038f59216b6067475154
William A. Kennington III <william@wkennington.com>
2016-10-09 00:54:55 -0700

    ipmitool: 1.8.17 -> 1.8.18

--- pkgs/all-pkgs/i/ipmitool/default.nix
+++ pkgs/all-pkgs/i/ipmitool/default.nix
@@ -4,13 +4,15 @@
 , openssl
 }:
 
+let
+  version = "1.8.18";
+in
 stdenv.mkDerivation rec {
-  name = "ipmitool-1.8.17";
+  name = "ipmitool-${version}";
 
   src = fetchurl {
-    url = "mirror://sourceforge/ipmitool/${name}.tar.bz2";
-    multihash = "QmdofyZF3hzpf3NSwzG4NfH4qU1K6cG6qYKjnrpErXyGTZ";
-    sha256 = "97fa20efd9c87111455b174858544becae7fcc03a3cb7bf5c19b09065c842d02";
+    url = "mirror://sourceforge/ipmitool/ipmitool/${version}/${name}.tar.bz2";
+    sha256 = "0c1ba3b1555edefb7c32ae8cd6a3e04322056bc087918f07189eeedfc8b81e01";
   };
 
   buildInputs = [

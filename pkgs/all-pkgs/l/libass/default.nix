81c68308b30d8a0d6d4974245575ab650104aa84
Cody W. Opel <codyopel@gmail.com>
2016-10-09 00:51:43 -0400

    libass: 0.13.3 -> 0.13.4

--- pkgs/all-pkgs/l/libass/default.nix
+++ pkgs/all-pkgs/l/libass/default.nix
@@ -15,7 +15,7 @@ let
   inherit (stdenv.lib)
     boolEn;
 
-  version = "0.13.3";
+  version = "0.13.4";
 in
 stdenv.mkDerivation rec {
   name = "libass-${version}";
@@ -23,7 +23,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "https://github.com/libass/libass/releases/download/${version}/"
       + "${name}.tar.xz";
-    sha256 = "a641b653d7c9f2f3b9d6a5e5a906a004ac3e110487ad485d9dd029e944bb3f6d";
+    sha256 = "d84a2fc89011b99d87fc47af91906622707c165d1860e9f774825ebbbc9c9fb6";
   };
 
   nativeBuildInputs = [

ba9572bf9aed70d85c86a041060c85d2e5e92fff
Cody W. Opel <codyopel@gmail.com>
2016-10-14 16:04:52 -0400

    libnotify: 0.7.6 -> 0.7.7

--- pkgs/development/libraries/libnotify/default.nix
+++ pkgs/development/libraries/libnotify/default.nix
@@ -1,24 +1,17 @@
-{ stdenv, fetchurl, pkgconfig, automake, autoconf, libtool
-, glib, gdk-pixbuf, gobject-introspection, autoreconfHook }:
+{ stdenv, fetchurl
+, glib, gdk-pixbuf, gobject-introspection, gtk_3 }:
 
 stdenv.mkDerivation rec {
   ver_maj = "0.7";
-  ver_min = "6";
+  ver_min = "7";
   name = "libnotify-${ver_maj}.${ver_min}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/libnotify/${ver_maj}/${name}.tar.xz";
-    sha256 = "0dyq8zgjnnzcah31axnx6afb21kl7bks1gvrg4hjh3nk02j1rxhf";
+    sha256 = "9cb4ce315b2655860c524d46b56010874214ec27e854086c1a1d0260137efc04";
   };
 
-  # see Gentoo ebuild - we don't need to depend on gtk+(2/3)
-  preAutoreconf = ''
-    sed -i -e 's:noinst_PROG:check_PROG:' tests/Makefile.am || die
-    sed -i -e '/PKG_CHECK_MODULES(TESTS/d' configure.ac || die
-  '';
-
-  buildInputs = [ pkgconfig automake autoconf autoreconfHook
-                  libtool glib gdk-pixbuf gobject-introspection ];
+  buildInputs = [ glib gdk-pixbuf gobject-introspection gtk_3 ];
 
   meta = {
     homepage = http://galago-project.org/; # very obsolete but found no better

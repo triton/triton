b6c578dca637ad0f780c80fad0b5bc8d0ff5b2f4
Cody W. Opel <codyopel@gmail.com>
2016-10-15 23:10:27 -0400

    nautilus: 3.22.0 -> 3.22.1

--- pkgs/all-pkgs/n/nautilus/sources.nix
+++ pkgs/all-pkgs/n/nautilus/sources.nix
@@ -1,7 +1,7 @@
 { }:
 {
   "3.22" = {
-    version = "3.22.0";
-    sha256 = "7671d9cf9df0321f5ad03abce03ab35ab6d643e1b3392a84f3c1d7c23ce29816";
+    version = "3.22.1";
+    sha256 = "17e37ffecebb2cf29e6bfb52111a951f0c67534b9dabf0210c12d15abb7dbddc";
   };
 }

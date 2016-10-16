b2029152f7bd4e5f8f041471ff656a680b61925c
Cody W. Opel <codyopel@gmail.com>
2016-10-15 23:36:52 -0400

    lensfun: refactor

--- pkgs/all-pkgs/l/lensfun/default.nix
+++ pkgs/all-pkgs/l/lensfun/default.nix
@@ -1,17 +1,28 @@
 { stdenv
 , cmake
 , fetchurl
+, lib
 
 , glib
 , libpng
 , zlib
 }:
 
+let
+  inherit (stdenv)
+    targetSystem;
+  inherit (lib)
+    boolOn
+    elem
+    platforms;
+
+  version = "0.3.2";
+in
 stdenv.mkDerivation rec {
-  name = "lensfun-0.3.2";
+  name = "lensfun-${version}";
 
   src = fetchurl {
-    url = "mirror://sourceforge/lensfun/${name}.tar.gz";
+    url = "mirror://sourceforge/lensfun/${version}/${name}.tar.gz";
     sha256 = "ae8bcad46614ca47f5bda65b00af4a257a9564a61725df9c74cb260da544d331";
   };
 
@@ -26,17 +37,16 @@ stdenv.mkDerivation rec {
   ];
 
   cmakeFlags = [
-    "-L"
     "-DBUILD_DOC=OFF"
-    "-DBUILD_FOR_SSE=ON"
-    "-DBUILD_FOR_SSE2=ON"
+    "-DBUILD_FOR_SSE=${boolOn (elem targetSystem platforms.x86-all)}"
+    "-DBUILD_FOR_SSE2=${boolOn (elem targetSystem platforms.x86-all)}"
     "-DLENSTOOL=ON"
     "-DBUILD_STATIC=OFF"
     "-DBUILD_TESTS=OFF"
     "-DBUILD_AUXFUN=ON"
   ];
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "A database of photographic lenses & their characteristics";
     homepage = http://lensfun.sourceforge.net/;
     license = licenses.lgpl3; # CC-BY-SA-3.0

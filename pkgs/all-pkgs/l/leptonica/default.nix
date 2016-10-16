37a9993b74411ae92a499c008cffa8b6c517a726
Cody W. Opel <codyopel@gmail.com>
2016-10-15 23:44:05 -0400

    leptonica: refactor

--- pkgs/all-pkgs/l/leptonica/default.nix
+++ pkgs/all-pkgs/l/leptonica/default.nix
@@ -1,5 +1,6 @@
 { stdenv
 , fetchurl
+, lib
 
 , giflib
 , libjpeg
@@ -13,16 +14,16 @@
 }:
 
 let
-  inherit (stdenv.lib)
-    enFlag
-    wtFlag;
+  inherit (lib)
+    boolEn
+    boolWt;
 in
-
 stdenv.mkDerivation rec {
   name = "leptonica-1.73";
 
   src = fetchurl {
     url = "http://www.leptonica.org/source/${name}.tar.gz";
+    multihash = "QmdsTL6sLfcSxH1ZPSbhwt6KNcYVDrQRbcqCT3rsGhbKgp";
     sha256 = "19e4335c674e7b78af9338d5382cc5266f34a62d4ce533d860af48eaa859afc1";
   };
 
@@ -37,17 +38,17 @@ stdenv.mkDerivation rec {
   ];
 
   configureFlags = [
-    (enFlag "programs" programs null)
-    (wtFlag "zlib" (zlib != null) null)
-    (wtFlag "libpng" (libpng != null) null)
-    (wtFlag "jpeg" (openjpeg != null) null)
-    (wtFlag "giflib" (giflib != null) null)
-    (wtFlag "libtiff" (libtiff != null) null)
-    (wtFlag "libwebp" (libwebp != null) null)
-    (wtFlag "libopenjpeg" (openjpeg != null) null)
+    "--${boolEn programs}-programs"
+    "--${boolWt (zlib != null)}-zlib"
+    "--${boolWt (libpng != null)}-libpng"
+    "--${boolWt (openjpeg != null)}-jpeg"
+    "--${boolWt (giflib != null)}-giflib"
+    "--${boolWt (libtiff != null)}-libtiff"
+    "--${boolWt (libwebp != null)}-libwebp"
+    "--${boolWt (openjpeg != null)}-libopenjpeg"
   ];
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Image processing and analysis library";
     homepage = http://www.leptonica.org/;
     # Its own license: http://www.leptonica.org/about-the-license.html

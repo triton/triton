d9bf329621c0162d9dde80158e1aaa98b700c240
Cody W. Opel <codyopel@gmail.com>
2016-10-16 03:59:11 -0400

    python(3.6): 3.6.0b1 -> 3.6.0b2

--- pkgs/all-pkgs/p/python/sources.nix
+++ pkgs/all-pkgs/p/python/sources.nix
@@ -20,8 +20,8 @@
     pgpKeyFingerprint = "97FC 712E 4C02 4BBE A48A  61ED 3A5C A953 F73C 700D";
   };
   "3.6" = {
-    versionPatch = "0b1";
-    sha256 = "a83b094a8abf8a1fba7c548a5e8dd0aabe87a87a6ebd87c97f4a5a2527a74d42";
+    versionPatch = "0b2";
+    sha256 = "3afd4544b99933275c513f7c3d3a23e183de8e209709ed8785bbd42e2c4869ea";
     pgpKeyFingerprint = "0D96 DF4D 4110 E5C4 3FBF  B17F 2D34 7EA6 AA65 421D";
   };
 }

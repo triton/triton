c217786002f53bb1a07c2bec39c7b14a3d206958
William A. Kennington III <william@wkennington.com>
2016-10-18 11:02:38 -0700

    Make updating all go packages easier

---
+++ maintainers/scripts/update-all-go-packages.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+updater="$(dirname "$(readlink -f "$0")")/update-go-package.sh"
+
+# Find the top git level
+while ! [ -d "pkgs/top-level" ]; do
+  cd ..
+done
+
+exec "$updater" $(cat pkgs/top-level/all-packages.nix | grep pkgs.goPackages | awk -F. '{print $3}' | tr '\n' ' ')

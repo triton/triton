67b2d035c737d654776f8dca8a1068616a03eade
William A. Kennington III <william@wkennington.com>
2016-10-17 19:15:48 -0700

    grpc: Fix 1.0.0 -> 2016-10-17

--- pkgs/all-pkgs/g/grpc/default.nix
+++ pkgs/all-pkgs/g/grpc/default.nix
@@ -5,19 +5,22 @@
 , ninja
 , perl
 
+, openssl
+, protobuf-cpp
 , zlib
 }:
 
 let
-  version = "1.0.0";
+  version = "2016-10-17";
 in
 stdenv.mkDerivation {
   name = "grpc-${version}";
 
   src = fetchgit {
+    version = 2;
     url = "https://github.com/grpc/grpc.git";
-    rev = "refs/tags/v${version}";
-    sha256 = "0a5h7ydvyyywpf5ifxxma8h9i0nsh2fg3k7dlsg57486mjmpbzgd";
+    rev = "15586f27fa21a497cb3a4ba4d3ca5d3a381127f0";
+    sha256 = "1yw8f46r6zg2664qryz8ijki1lqmyp0v019sym8a8z3ja7gk5mqx";
   };
 
   nativeBuildInputs = [
@@ -28,13 +31,20 @@ stdenv.mkDerivation {
   ];
 
   buildInputs = [
+    openssl
+    protobuf-cpp
     zlib
   ];
 
-  cmakeFlags = [
-    "-DgRPC_ZLIB_PROVIDER=package"
-    "-DgRPC_PROTOBUF_PROVIDER=package"
-    "-DgRPC_SSL_PROVIDER=package"
+  # Don't vendor packages we have
+  postPatch = ''
+    sed -i 's,PROVIDER "module",PROVIDER "package",g' CMakeLists.txt
+  '';
+
+  NIX_CFLAGS_LINK = [
+    "-pthread"
+    "-lprotobuf"
+    "-lprotoc"
   ];
 
   meta = with stdenv.lib; {

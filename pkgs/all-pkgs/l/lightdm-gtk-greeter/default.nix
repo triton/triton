c6850075ed488658f516c6079c7ce62cc9bb0cc4
Cody W. Opel <codyopel@gmail.com>
2016-10-20 07:41:48 -0400

    lightdm-gtk-greeter: fix missing dependencies from update

--- pkgs/all-pkgs/l/lightdm-gtk-greeter/default.nix
+++ pkgs/all-pkgs/l/lightdm-gtk-greeter/default.nix
@@ -2,25 +2,33 @@
 , fetchTritonPatch
 , fetchurl
 , intltool
+, lib
 , makeWrapper
 
+, exo
 , glib
-, gtk3
+, gobject-introspection
+, gtk_3
 , hicolor-icon-theme
 , lightdm
+, libxklavier
 , xorg
 }:
 
 let
-  ver_branch = "2.0";
-  version = "2.0.2";
-in
+  inherit (lib)
+    boolEn
+    boolWt;
 
+  channel = "2.0";
+  version = "${channel}.2";
+in
 stdenv.mkDerivation rec {
   name = "lightdm-gtk-greeter-${version}";
 
   src = fetchurl {
-    url = "https://launchpad.net/lightdm-gtk-greeter/${ver_branch}/${version}/+download/${name}.tar.gz";
+    url = "https://launchpad.net/lightdm-gtk-greeter/${channel}/${version}/"
+      + "+download/${name}.tar.gz";
     sha256 = "8ee6d93d1d6837b3590f64ac4d5bac5db888a8861dff1cb2ef10f7816ad36690";
   };
 
@@ -30,29 +38,29 @@ stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
+    exo
     glib
-    gtk3
+    gobject-introspection
+    gtk_3
     lightdm
+    libxklavier
     xorg.libX11
-  ];
-
-  # Disable -Werror as there are issues with 2.0.1 on gcc 6.1.0
-  prePatch = ''
-    sed -i 's,-Werror[^ "]*,,g' configure
-  '';
-
-  patches = [
-    # Remove after 2.0.1
-    (fetchTritonPatch {
-      rev = "77e91ff8ea4b2a4d01c6618c7d98649e0a2db66b";
-      file = "lightdm-gtk-greeter/lightdm-gtk-greeter-2.0.1-r339-fix-deprecated-gdk-cursor.patch";
-      sha256 = "2de9eb1f182260cc36cde1edb4deae215f1396c263bc365f0869e3447774b84d";
-    })
   ];
 
   configureFlags = [
     "--localstatedir=/var"
     "--sysconfdir=/etc"
+    "--disable-maintainer-mode"
+    "--${boolEn (gobject-introspection != null)}-introspection"
+    "--enable-compile-warnings"
+    "--disable-iso-c"
+    "--disable-libindicator"
+    "--disable-libido"
+    #"--enable-at-spi-command"
+    "--disable-indicator-services-command"
+    #"--enable-kill-on-sigterm"
+    "--enable-nls"
+    "--${boolWt (libxklavier != null)}-libxklavier"
   ];
 
   preInstall = ''
@@ -63,13 +71,11 @@ stdenv.mkDerivation rec {
   '';
 
   postInstall = ''
-    substituteInPlace "$out/share/xgreeters/lightdm-gtk-greeter.desktop" \
-      --replace "Exec=lightdm-gtk-greeter" "Exec=$out/sbin/lightdm-gtk-greeter"
+    sed -i "$out/share/xgreeters/lightdm-gtk-greeter.desktop" \
+      -e "s,Exec=lightdm-gtk-greeter,Exec=$out/sbin/lightdm-gtk-greeter,"
     wrapProgram "$out/sbin/lightdm-gtk-greeter" \
       --prefix XDG_DATA_DIRS ":" "${hicolor-icon-theme}/share"
   '';
-
-  parallelBuild = false;
 
   meta = with stdenv.lib; {
     homepage = http://launchpad.net/lightdm-gtk-greeter;

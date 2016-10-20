f31098a3617c3fb15578f26aee1585ccab5325a8
Cody W. Opel <codyopel@gmail.com>
2016-10-20 12:47:25 -0400

    ncdc: refactor

--- pkgs/all-pkgs/n/ncdc/default.nix
+++ pkgs/all-pkgs/n/ncdc/default.nix
@@ -1,5 +1,6 @@
 { stdenv
 , fetchurl
+, lib
 
 , bzip2
 , glib
@@ -10,12 +11,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "ncdc-${version}";
-  version = "1.19.1";
+  name = "ncdc-1.19.1";
 
   src = fetchurl {
-    url = "http://dev.yorhel.nl/download/ncdc-${version}.tar.gz";
-    sha256 = "0iwx4b3x207sw11qqjfynpwnhryhixjzbgcy9l9zfisa8f0k7cm6";
+    url = "https://dev.yorhel.nl/download/${name}.tar.gz";
+    sha256 = "a6b23381434a47f7134d9ebdf5658fd06768f9b5de498c43e0fa00d1c7229d47";
   };
 
   buildInputs = [
@@ -27,7 +27,12 @@ stdenv.mkDerivation rec {
     zlib
   ];
 
-  meta = with stdenv.lib; {
+  configureFlags = [
+    "--disable-git-version"
+    "--without-geoip"
+  ];
+
+  meta = with lib; {
     description = "ncurses direct connect client";
     homepage = http://dev.yorhel.nl/ncdc;
     license = licenses.mit;

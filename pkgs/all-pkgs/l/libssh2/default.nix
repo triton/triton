c37808437e2f643c7cc5eec900d14d2d8c23fdec
William A. Kennington III <william@wkennington.com>
2016-10-25 17:56:40 -0700

    libssh2: 1.7.0 -> 1.8.0

--- pkgs/all-pkgs/l/libssh2/default.nix
+++ pkgs/all-pkgs/l/libssh2/default.nix
@@ -6,11 +6,13 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "libssh2-1.7.0";
+  name = "libssh2-1.8.0";
 
   src = fetchurl {
     url = "https://www.libssh2.org/download/${name}.tar.gz";
-    sha256 = "116mh112w48vv9k3f15ggp5kxw5sj4b88dzb5j69llsh7ba1ymp4";
+    multihash = "QmY6PpE6gRK4vY5i277niCrMvev4pxqpxoqBNbXwXkUrzH";
+    hashOutput = false;
+    sha256 = "39f34e2f6835f4b992cafe8625073a88e5a28ba78f83e8099610a7b3af4676d4";
   };
 
   buildInputs = [
@@ -30,6 +32,15 @@ stdenv.mkDerivation rec {
     "--disable-debug"
     "--disable-examples-build"
   ];
+
+  passthru = {
+    srcVerification = fetchurl {
+      failEarly = true;
+      pgpsigUrls = map (n: "${n}.asc") src.urls;
+      pgpKeyFingerprint = "27ED EAF2 2F3A BCEB 50DB  9A12 5CC9 08FD B71E 12C2";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "A client-side C library implementing the SSH2 protocol";

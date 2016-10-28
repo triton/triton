7374ba0e73fdca2fcc8ff3eec89929b71f5d7f52
Cody W. Opel <codyopel@gmail.com>
2016-10-28 15:54:00 -0400

    mpg123: 1.22.2 -> 1.23.8

--- pkgs/applications/audio/mpg123/default.nix
+++ pkgs/applications/audio/mpg123/default.nix
@@ -1,24 +1,22 @@
 {stdenv, fetchurl, alsa-lib }:
 
+let
+  version = "1.23.8";
+in
 stdenv.mkDerivation rec {
-  name = "mpg123-1.22.2";
+  name = "mpg123-${version}";
 
   src = fetchurl {
-    url = "mirror://sourceforge/mpg123/${name}.tar.bz2";
-    sha256 = "0i1phi6fdjas37y00h3j8rb0b8ngr9az6hy5ff5bl53ify3j87kd";
+    url = "mirror://sourceforge/mpg123/mpg123/${version}/${name}.tar.bz2";
+    sha256 = "de2303c8ecb65593e39815c0a2f2f2d91f708c43b85a55fdd1934c82e677cf8e";
   };
 
   buildInputs = [ alsa-lib ];
-
-  crossAttrs = {
-    configureFlags = if stdenv.cross ? mpg123 then
-      "--with-cpu=${stdenv.cross.mpg123.cpu}" else "";
-  };
 
   meta = {
     description = "Fast console MPEG Audio Player and decoder library";
     homepage = http://mpg123.org;
     license = stdenv.lib.licenses.lgpl21;
-    maintainers = [ stdenv.lib.maintainers.ftrvxmtrx ];
+    maintainers = [ ];
   };
 }

1f2e6f59392000f1ef50aaff79d5df6de7ee0a1a
Cody W. Opel <codyopel@gmail.com>
2016-10-28 14:39:28 -0400

    gtk+(3): 3.22.1 -> 3.22.2

--- pkgs/all-pkgs/g/gtk/3-sources.nix
+++ pkgs/all-pkgs/g/gtk/3-sources.nix
@@ -1,7 +1,7 @@
 { }:
 {
   "3.22" = {
-    version = "3.22.1";
-    sha256 = "127c8c5cfc32681f9ab3cb542eb0d5c16c1c02faba68bf8fcac9a3cf278ef471";
+    version = "3.22.2";
+    sha256 = "2343dcaed2fc237ef718136ff4110fef7eb1ebfe178a9b064aa21cd74ecb6946";
   };
 }

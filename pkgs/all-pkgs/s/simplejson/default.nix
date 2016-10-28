59228dc482604d82dfb789e7be6eac137d130229
Cody W. Opel <codyopel@gmail.com>
2016-10-28 15:58:41 -0400

    simplejson: 3.9.0 -> 3.10.0

--- pkgs/all-pkgs/s/simplejson/default.nix
+++ pkgs/all-pkgs/s/simplejson/default.nix
@@ -4,7 +4,7 @@
 }:
 
 let
-  version = "3.9.0";
+  version = "3.10.0";
 in
 buildPythonPackage {
   name = "simplejson-${version}";
@@ -12,7 +12,7 @@ buildPythonPackage {
   src = fetchPyPi {
     package = "simplejson";
     inherit version;
-    sha256 = "e9abeee37424f4bfcd27d001d943582fb8c729ffc0b74b72bd0e9b626ed0d1b6";
+    sha256 = "953be622e88323c6f43fad61ffd05bebe73b9fd9863a46d68b052d2aa7d71ce2";
   };
 
   meta = with stdenv.lib; {

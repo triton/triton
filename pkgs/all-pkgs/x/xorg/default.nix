97ea49b75811633ee78bf53d46eefc877519e6ee
Cody W. Opel <codyopel@gmail.com>
2016-10-28 17:20:58 -0400

    xf86-input-libinput: 0.20.0 -> 0.22.0

--- pkgs/all-pkgs/x/xorg/default.nix
+++ pkgs/all-pkgs/x/xorg/default.nix
@@ -2292,10 +2292,10 @@ let
   }) // {inherit inputproto xorgserver xproto ;};
 
   xf86inputlibinput = (mkDerivation "xf86inputlibinput" {
-    name = "xf86-input-libinput-0.20.0";
+    name = "xf86-input-libinput-0.22.0";
     src = fetchurl {
-      url = mirror://xorg/individual/driver/xf86-input-libinput-0.20.0.tar.bz2;
-      sha256 = "1c1r5axm43kcdgllv40sl1chk4z4qhkv4yyym6xml3mss1aq2kn5";
+      url = mirror://xorg/individual/driver/xf86-input-libinput-0.22.0.tar.bz2;
+      sha256 = "1qpaffl21xxjqcn4cqdfnd1gvxwdxizvvkimkypf4j6l5q3v8qn7";
     };
     nativeBuildInputs = [ utilmacros ];
     buildInputs = [ inputproto libinput xorgserver xproto ];

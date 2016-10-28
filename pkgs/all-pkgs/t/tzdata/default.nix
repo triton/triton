b0d65c50b9a2df8c2f67f08b6fc4baaefc9414ba
William A. Kennington III <william@wkennington.com>
2016-10-28 15:20:15 -0700

    tzdata: 2016f -> 2016h

--- pkgs/all-pkgs/t/tzdata/default.nix
+++ pkgs/all-pkgs/t/tzdata/default.nix
@@ -2,18 +2,20 @@
 , fetchurl
 }:
 
+let
+  version = "2016h";
+in
 stdenv.mkDerivation rec {
   name = "tzdata-${version}";
-  version = "2016f";
 
   srcs = [
     (fetchurl {
       url = "https://www.iana.org/time-zones/repository/releases/tzdata${version}.tar.gz";
-      sha256 = "ed8c951008d12f1db55a11e96fc055718c6571233327d9de16a7f8475e2502b0";
+      sha256 = "da1b74fc2dec2ce8b64948dafb0bfc2f923c830d421a7ae4d016226135697a64";
     })
     (fetchurl {
       url = "https://www.iana.org/time-zones/repository/releases/tzcode${version}.tar.gz";
-      sha256 = "72325f384490a310eeb2ea0fab7e6f011a5be19adab2ff9d83bf9d1993b066ed";
+      sha256 = "30e62f0b86a78fb020d378b950930da023ca31b1a58f08d8fb2066627c4d6566";
     })
   ];
 
@@ -23,24 +25,11 @@ stdenv.mkDerivation rec {
     makeFlagsArray+=(
       "TOPDIR=$out"
       "TZDIR=$out/share/zoneinfo"
-      "ETCDIR=$TMPDIR/etc"
-      "LIBDIR=$out/lib"
-      "MANDIR=$TMPDIR/man"
-      "AWK=awk"
-      "CFLAGS=-DHAVE_LINK=0"
+      "MANDIR=$TMPDIR/share/man"
+      "LIBDIR=$TMPDIR/lib"
+      "ETCDIR=$TMPDIR/bin"
     )
   '';
-
-  postInstall = ''
-    rm $out/share/zoneinfo-posix
-    ln -s . $out/share/zoneinfo/posix
-    mv $out/share/zoneinfo-leaps $out/share/zoneinfo/right
-
-    mkdir -p "$out/include"
-    cp tzfile.h "$out/include/tzfile.h"
-  '';
-
-  preferLocalBuild = true;
 
   meta = with stdenv.lib; {
     homepage = http://www.iana.org/time-zones;

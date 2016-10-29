ca8a35b33666339802b5945a434e140035b13791
Cody W. Opel <codyopel@gmail.com>
2016-10-29 01:19:24 -0400

    x265: fix missing file

---
+++ pkgs/all-pkgs/x/x265/sources.nix
@@ -0,0 +1,14 @@
+{ }:
+{
+  "stable" = {
+    version = "2.1";
+    multihash = "QmW92vUhh1nXAkV5GkdB7aXYhVFi4P7NY4SUDP8NXeEZZT";
+    sha256 = "b3bc83754e91ed5655c8cba5a2ed48e6b9ab39699c9ed6554c670211d5870f9c";
+  };
+  "head" = {
+    fetchzipversion = 2;
+    version = "2016-10-27";
+    rev = "d216cb9b3b4777fd768311cb3c7281b0fcf1213d";
+    sha256 = "c39682a8f0a700c295e513a796edf04eb44fcee398637101c39208b01db91be0";
+  };
+}

78a684f521cffecf78acedf9f1b4f6b447d03c03
William A. Kennington III <william@wkennington.com>
2016-10-29 14:29:09 -0700

    dnssec-root: Make more deterministic

--- pkgs/all-pkgs/d/dnssec-root/default.nix
+++ pkgs/all-pkgs/d/dnssec-root/default.nix
@@ -1,107 +1,144 @@
 { stdenv
 , fetchurl
 , ldns
+, openssl
 }:
 
 # This is purposely not versioned to force the user to keep it up to date
 let
   filePath = "share/dnssec/iana-root.txt";
 
-  pkg = fetchurl {
+  pkg = fetchurl rec {
     name = "dnssec-root-2010-07-15";
-    url = "https://data.iana.org/root-anchors/root-anchors.xml";
+    multihash = "QmQUgFAv46gdDFsgp9N7xCDjHnYewKpEuEgkyqbkD1vjgy";
     sha256 = "14mlj3pv1nw60nhs289inrm3lqxi780kf0x146h7id2xhin56k3r";
     downloadToTemp = true;
     recursiveHash = true;
 
     postFetch = ''
-      echo ""
-      echo -n "##########################"
-      echo -n " root-anchors.xml "
-      echo -n "##########################"
-      echo ""
-      cat $downloadedFile
-      echo -n "##########################"
-      echo -n " root-anchors.xml "
-      echo -n "##########################"
-      echo ""
-      echo ""
-      echo -n "##########################"
-      echo -n " drill-query "
-      echo -n "##########################"
-      echo ""
-      ${ldns}/bin/drill -z -t -s DNSKEY . | tee drill-out
-      echo -n "##########################"
-      echo -n " drill-query "
-      echo -n "##########################"
-      echo ""
-
-      awk '
-      BEGIN { FS = "[<>]"; }
-      { if (/<Digest>/) {print $3;} }
-      ' $downloadedFile > hash
-      [ -n "$(cat hash)" ]
-
-      awk '
-      BEGIN { FS = "[<>]"; }
-      { if (/<DigestType>/) {print $3;} }
-      ' $downloadedFile > hash-type
-      [ -n "$(cat hash-type)" ]
-
-      awk '
-      BEGIN { FS = "[<>]"; }
-      { if (/<KeyTag>/) {print $3;} }
-      ' $downloadedFile > xml-id
-      [ -n "$(cat xml-id)" ]
-
-      awk '
-      BEGIN { getline hash<"hash"; }
-      { if (match($0, tolower(hash))) {print $7;} }
-      ' drill-out > drill-id
-      [ -n "$(cat drill-id)" ]
-
-      if [ "$(cat xml-id)" != "$(cat drill-id)" ]; then
-        echo "Drill and XML Id's don't match" >&2
+      if [ "$(openssl ${passthru.srcVerification.outputHashAlgo} -r -hex "$downloadedFile" 2>/dev/null | tail -n 1 | awk '{print $1}')" != "${passthru.srcVerification.outputHash}" ]; then
+        echo "Hash does not match the source file" >&2
         exit 1
       fi
-
-      awk '
-      BEGIN { getline id<"drill-id"; }
-      { if (match($0, "id = " id)) {print $0;} }
-      ' drill-out > key-line
-
-      awk '
-      {
-        getline id<"xml-id";
-        getline hash<"hash";
-        getline hashtype<"hash-type";
-        print "Domain: " $1;
-        print "Id: " id;
-        print "Hash: " hash;
-        print "HashType: " hashtype;
-        print "Flags: " $5;
-        print "Protocol: " $6;
-        print "Algorithm: " $7;
-        print "Key: " $8;
-      }
-      ' key-line > iana-root.txt
-
-      echo ""
-      echo -n "##########################"
-      echo -n " IANA Root Parsed "
-      echo -n "##########################"
-      echo ""
-      cat iana-root.txt
-      echo -n "##########################"
-      echo -n " IANA Root Parsed "
-      echo -n "##########################"
-      echo ""
-
-      echo ""
-      echo "The key is probably okay since the root-anchors and dns match."
-
-      install -Dm644 iana-root.txt "$out/${filePath}"
+      install -Dm644 "$downloadedFile" "$out/${filePath}"
     '';
+
+    passthru = rec {
+      srcFile = fetchurl rec {
+        failEarly = true;
+        url = "https://data.iana.org/root-anchors/root-anchors.xml";
+        pgpsigUrl = "https://data.iana.org/root-anchors/root-anchors.asc";
+        pgpKeyFingerprint = "2FBB 91BC AAEE 0ABE 1F80  31C7 D1AF BCE0 0F6C 91D2";
+        sha256 = "dfb281b771dc854c18d1cff9d2eecaf184cf7a9668606aaa33e8f01bf4b4d8e4";
+      };
+
+      srcVerification = stdenv.mkDerivation {
+        name = "iana-root.txt";
+        outputHash = "56f00d17c194332ab51f9dc524bb0407b487922c8598e8ae1bcf2b1dabd00707";
+        outputHashAlgo = "sha256";
+        outputHashMode = "flat";
+
+        nativeBuildInputs = [
+          openssl
+        ];
+
+        buildCommand = ''
+          echo ""
+          echo -n "##########################"
+          echo -n " root-anchors.xml "
+          echo -n "##########################"
+          echo ""
+          cat "${srcFile}"
+          echo -n "##########################"
+          echo -n " root-anchors.xml "
+          echo -n "##########################"
+          echo ""
+          echo ""
+          echo -n "##########################"
+          echo -n " drill-query "
+          echo -n "##########################"
+          echo ""
+          ${ldns}/bin/drill -z -t -s DNSKEY . | tee drill-out
+          echo -n "##########################"
+          echo -n " drill-query "
+          echo -n "##########################"
+          echo ""
+
+          awk '
+          BEGIN { FS = "[<>]"; }
+          { if (/<Digest>/) {print $3;} }
+          ' "${srcFile}" > hash
+          [ -n "$(cat hash)" ]
+
+          awk '
+          BEGIN { FS = "[<>]"; }
+          { if (/<DigestType>/) {print $3;} }
+          ' "${srcFile}" > hash-type
+          [ -n "$(cat hash-type)" ]
+
+          awk '
+          BEGIN { FS = "[<>]"; }
+          { if (/<KeyTag>/) {print $3;} }
+          ' "${srcFile}" > xml-id
+          [ -n "$(cat xml-id)" ]
+
+          awk '
+          BEGIN { getline hash<"hash"; }
+          { if (match($0, tolower(hash))) {print $7;} }
+          ' drill-out > drill-id
+          [ -n "$(cat drill-id)" ]
+
+          if [ "$(cat xml-id)" != "$(cat drill-id)" ]; then
+            echo "Drill and XML Id's don't match" >&2
+            exit 1
+          fi
+
+          awk '
+          BEGIN { getline id<"drill-id"; }
+          { if (match($0, "id = " id)) {print $0;} }
+          ' drill-out > key-line
+
+          awk '
+          {
+            getline id<"xml-id";
+            getline hash<"hash";
+            getline hashtype<"hash-type";
+            print "Domain: " $1;
+            print "Id: " id;
+            print "Hash: " hash;
+            print "HashType: " hashtype;
+            print "Flags: " $5;
+            print "Protocol: " $6;
+            print "Algorithm: " $7;
+            print "Key: " $8;
+          }
+          ' key-line > iana-root.txt
+
+          echo ""
+          echo -n "##########################"
+          echo -n " IANA Root Parsed "
+          echo -n "##########################"
+          echo ""
+          cat iana-root.txt
+          echo -n "##########################"
+          echo -n " IANA Root Parsed "
+          echo -n "##########################"
+          echo ""
+
+          echo ""
+          echo "The key is probably okay since the root-anchors and dns match."
+
+          install -Dm644 iana-root.txt "$out"
+          hash="$(openssl "$outputHashAlgo" -r -hex "$out" | head -n 1 | awk '{print $1}')"
+          if [ "$hash" != "$outputHash" ]; then
+            echo "Got a bad hash:" >&2
+            echo "  File: $out" >&2
+            echo "  Hash: $hash" >&2
+            exit 1
+          fi
+        '';
+      };
+    };
 
     meta = with stdenv.lib; {
       maintainers = with maintainers; [

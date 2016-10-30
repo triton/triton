84dcd687e2cc70610815ff33ccc582dd35f1f848
William A. Kennington III <william@wkennington.com>
2016-10-30 16:27:35 -0700

    pycparser: 2.14 -> 2.17

--- pkgs/all-pkgs/p/pycparser/default.nix
+++ pkgs/all-pkgs/p/pycparser/default.nix
@@ -4,7 +4,7 @@
 }:
 
 let
-  version = "2.14";
+  version = "2.17";
 in
 buildPythonPackage {
   name = "pycparser-${version}";
@@ -12,7 +12,7 @@ buildPythonPackage {
   src = fetchPyPi {
     package = "pycparser";
     inherit version;
-    sha256 = "7959b4a74abdc27b312fed1c21e6caf9309ce0b29ea86b591fd2e99ecdf27f73";
+    sha256 = "0aac31e917c24cb3357f5a4d5566f2cc91a19ca41862f6c3c22dc60a629673b6";
   };
 
   meta = with stdenv.lib; {

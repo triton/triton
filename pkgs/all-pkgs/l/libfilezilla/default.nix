0d74e52774729ca9d59915d7e58289836b75437d
William A. Kennington III <william@wkennington.com>
2016-11-02 12:57:33 -0700

    libfilezilla: 0.7.1 -> 0.8.0

--- pkgs/all-pkgs/l/libfilezilla/default.nix
+++ pkgs/all-pkgs/l/libfilezilla/default.nix
@@ -3,14 +3,17 @@
 }:
 
 let
-  version = "0.7.1";
+  version = "0.8.0";
 in
 stdenv.mkDerivation rec {
   name = "libfilezilla-${version}";
 
   src = fetchurl {
-    url = "mirror://sourceforge/filezilla/libfilezilla/${version}/${name}.tar.bz2";
-    sha256 = "d95d2db75e523462c3f4b72b663b395dfe988cb71c3abef609f794a155a6ddd3";
+    urls = [
+      "https://download.filezilla-project.org/libfilezilla/${name}.tar.bz2"
+      "mirror://sourceforge/filezilla/libfilezilla/${version}/${name}.tar.bz2"
+    ];
+    sha256 = "0bee16be8d68d3b393a914458a586b4c684e781f2dcc9d287ce60129dc20015f";
   };
 
   meta = with stdenv.lib; {

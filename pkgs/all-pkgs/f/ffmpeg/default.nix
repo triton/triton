ec2fc136f8109fa683f3abf23c504676751782cd
Cody W. Opel <codyopel@gmail.com>
2016-11-05 17:23:49 -0400

    Revert "ffmpeg: fix missing speex dependency"
    This reverts commit 76920cdb665d4351ce39af2b3116ff30de5b674b.

--- pkgs/all-pkgs/f/ffmpeg/default.nix
+++ pkgs/all-pkgs/f/ffmpeg/default.nix
@@ -136,7 +136,6 @@
 , snappy
 , soxr
 , speex
-, speexdsp
 , tesseract
 #, twolame
 #, utvideo
@@ -367,7 +366,6 @@ stdenv.mkDerivation rec {
     soxr
     snappy
     speex
-    speexdsp
     tesseract
     v4l_lib
     vid-stab
@@ -551,7 +549,7 @@ stdenv.mkDerivation rec {
     "--${boolEn (samba_client != null)}-libsmbclient"
     "--${boolEn (snappy != null)}-libsnappy"
     "--${boolEn (soxr != null)}-libsoxr"
-    "--${boolEn (speex != null && speexdsp != null)}-libspeex"
+    "--${boolEn (speex != null)}-libspeex"
     "--${boolEn (libssh != null)}-libssh"
     #(fflag "--${boolEn (tesseract != null)}-libtesseract" "3.0")
     /**/(fflag "--disable-libtesseract" "3.0")

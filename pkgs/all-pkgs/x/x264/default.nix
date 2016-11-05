197503ec2d4cc1edcb81d6ebd58b0111bd57bc1b
Cody W. Opel <codyopel@gmail.com>
2016-11-05 09:32:50 -0400

    x264: 20160916 -> 20161104

--- pkgs/all-pkgs/x/x264/default.nix
+++ pkgs/all-pkgs/x/x264/default.nix
@@ -6,7 +6,7 @@
 }:
 
 let
-  version = "20160916";
+  version = "20161104";
 in
 stdenv.mkDerivation rec {
   name = "x264-${version}";
@@ -14,7 +14,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "https://ftp.videolan.org/pub/videolan/x264/snapshots/"
       + "x264-snapshot-${version}-2245-stable.tar.bz2";
-    sha256 = "1286dd6ca57605022d5922cff4d8a83c8b35f543f0c309d3906e1bf9209deb93";
+    sha256 = "a6ee16e1ba8943f8ba7b4bc36488489146d5556f4fa11280efd406782889ce76";
   };
 
   nativeBuildInputs = [
@@ -27,12 +27,7 @@ stdenv.mkDerivation rec {
   '';
 
   configureFlags = [
-    "--enable-cli"
-    # Only a static executable is built if --enable-shared is not passed
     "--enable-shared"
-    #"--enable-opencl"
-    "--enable-gpl"
-    "--enable-thread"
     "--disable-win32thread"
     "--disable-interlaced"
     "--bit-depth=${
@@ -42,9 +37,6 @@ stdenv.mkDerivation rec {
         "8"
     }"
     "--chroma-format=all"
-    "--enable-asm"
-    "--disable-debug"
-    "--disable-gprof"
     "--enable-pic"
     "--disable-avs"
     "--disable-swscale"

310b02e98a66db18185851748f000ccd42b73722
Cody W. Opel <codyopel@gmail.com>
2016-11-09 08:06:12 -0500

    gvfs: 1.30.1.1 -> 1.30.2

--- pkgs/all-pkgs/g/gvfs/sources.nix
+++ pkgs/all-pkgs/g/gvfs/sources.nix
@@ -1,7 +1,7 @@
 { }:
 {
   "1.30" = {
-    version = "1.30.1.1";
-    sha256 = "585b5de66eb99a09c020f34955db31e4764c8260a8afbb761ae264a958152ecc";
+    version = "1.30.2";
+    sha256 = "dd897a13af0728765baead44426e2cf968cd2c9caad3bee4d589970f7612c5d7";
   };
 }

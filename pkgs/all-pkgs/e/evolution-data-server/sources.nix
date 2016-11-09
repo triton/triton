da0c5e5df04e05e4af37726010c589af6e580e4b
Cody W. Opel <codyopel@gmail.com>
2016-11-09 07:49:39 -0500

    evolution-data-server: 3.22.1 -> 3.22.2

--- pkgs/all-pkgs/e/evolution-data-server/sources.nix
+++ pkgs/all-pkgs/e/evolution-data-server/sources.nix
@@ -1,7 +1,7 @@
 { }:
 {
   "3.22" = {
-    version = "3.22.1";
-    sha256 = "5ea1a25c26c2b5587b912661fc0eadf0ef8bc89ec01be9bb23cfffdf30ab41d9";
+    version = "3.22.2";
+    sha256 = "c0498e0968a60c3e42d5f72416c6e36752bced73bb530aa028dbd0008efd2167";
   };
 }

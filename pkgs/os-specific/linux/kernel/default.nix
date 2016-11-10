877f832539351f92be7f3d3faba1991081895f6b
William A. Kennington III <william@wkennington.com>
2016-11-10 11:18:56 -0800

    linux: 4.8.6 -> 4.8.7

--- pkgs/os-specific/linux/kernel/default.nix
+++ pkgs/os-specific/linux/kernel/default.nix
@@ -23,9 +23,9 @@ let
 
   sources = {
     "4.8" = {
-      version = "4.8.6";
+      version = "4.8.7";
       baseSha256 = "3e9150065f193d3d94bcf46a1fe9f033c7ef7122ab71d75a7fb5a2f0c9a7e11a";
-      patchSha256 = "3ac0ea443ac8a7aa40f8d5ce8ec33b84abbad4dbfc567c7699df728c2c21df37";
+      patchSha256 = "94213e7557d192d1054e352aec18e93275ed5a84abe190d43fd43847d1d86efe";
     };
     "testing" = {
       version = "4.9-rc4";

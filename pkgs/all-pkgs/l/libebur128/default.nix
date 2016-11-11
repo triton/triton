b33191f4e39d2378fc786661f10048f41de4d8a4
Cody W. Opel <codyopel@gmail.com>
2016-11-11 04:59:28 -0500

    libebur128: 1.1.0 -> 1.2.0

--- pkgs/all-pkgs/l/libebur128/default.nix
+++ pkgs/all-pkgs/l/libebur128/default.nix
@@ -6,14 +6,14 @@
 }:
 
 let
-  version = "1.1.0";
+  version = "1.2.0";
 in
 stdenv.mkDerivation rec {
   name = "libebur128-${version}";
 
   src = fetchurl {
     url = "https://github.com/jiixyj/libebur128/archive/v${version}.tar.gz";
-    sha256 = "c60e78f4bfda387a0895c64a4fc9850445e3a4425cc98f9140885966ce17c1d1";
+    sha256 = "f4c4ce732ae085214bcc47349f89b61ed53c13721c097e01cb966533ee6b1e5b";
   };
 
   nativeBuildInputs = [

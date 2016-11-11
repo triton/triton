fd30694c2b34629c99af22aad3125f8daf15798d
Cody W. Opel <codyopel@gmail.com>
2016-11-11 17:47:51 -0500

    fdk-aac: fix missing file

---
+++ pkgs/all-pkgs/f/fdk-aac/sources.nix
@@ -0,0 +1,13 @@
+{ }:
+{
+  "stable" = {
+    version = "0.1.4";
+    sha256 = "1aqmzxri23q83wfmwbbashs27mq1mapvfirz5r9i7jkphrwgw42r";
+  };
+  "head" = {
+    fetchzipverion = 2;
+    version = "2016-10-28";
+    rev = "5fd7e6537d1f37fb1bec8c6aa2f8e22030ab10de";
+    sha256 = "8657ba29d707f705f6c37076e1e9cf950aa4291298004f774b079a0eef4246ff";
+  };
+}

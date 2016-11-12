602beea33eeec7b9efc0ca041322321e5fd72b17
Cody W. Opel <codyopel@gmail.com>
2016-11-11 20:27:39 -0500

    pycountry: 1.2.0 -> 16.11.8

--- pkgs/all-pkgs/p/pycountry/default.nix
+++ pkgs/all-pkgs/p/pycountry/default.nix
@@ -3,20 +3,22 @@
 , fetchPyPi
 }:
 
+let
+  version = "16.11.8";
+in
 buildPythonPackage rec {
   name = "pycountry-${version}";
-  version = "1.20";
 
   src = fetchPyPi {
     package = "pycountry";
     inherit version;
-    sha256 = "0588efa3171e1d5e4cc96fce569ac865964285fdc8dbdc0860844f74598d1f98";
+    sha256 = "c9a0536699dfb46fb43ae1449999a921a79361030773bc3d35e00abfecb437c2";
   };
 
   doCheck = true;
 
   meta = with stdenv.lib; {
-    description = "ISO country, subdivision, language, currency and script definitions";
+    description = "ISO country, subdivision, language, currency & script definitions";
     homepage = https://bitbucket.org/flyingcircus/pycountry;
     license = licenses.lgpl21;
     maintainers = with maintainers; [

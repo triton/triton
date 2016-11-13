b4208ed8a334eb6da4af5a4ab3a66e40f15bb00c
Cody W. Opel <codyopel@gmail.com>
2016-11-13 08:01:05 -0500

    amrnb: add multihashes

--- pkgs/all-pkgs/a/amrnb/default.nix
+++ pkgs/all-pkgs/a/amrnb/default.nix
@@ -12,11 +12,13 @@ stdenv.mkDerivation rec{
   amrnb_3gpp = fetchurl {
     # Rel-13
     url = "http://www.3gpp.org/ftp/Specs/archive/26_series/26.104/26104-d00.zip";
+    multihash = "QmWFJTCdcERKsPwkkWLGhRhz54QEosYWmVJpxTkKw3CFuh";
     sha256 = "d5447f8a672243dd9db32ba64fd69181da725cac1f7b347c0af95d9949a47619";
   };
 
   src = fetchurl {
     url = "http://www.penguin.cz/~utx/ftp/amr/${name}.tar.bz2";
+    multihash = "QmbeaQgStCwmNCUDfiX6LKPVxpC14SETBAvVr4P7k3t6m1";
     sha256 = "3890004b665278b963ecaae2dc1321dcee29c53ea9d91aaf18d9286105e0f1e1";
   };
 

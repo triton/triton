283327811d186f94bccfce8479f8183de48cd2fb
Cody W. Opel <codyopel@gmail.com>
2016-11-14 15:50:11 -0500

    libdrm: 2.4.71 -> 2.4.73

--- pkgs/all-pkgs/l/libdrm/default.nix
+++ pkgs/all-pkgs/l/libdrm/default.nix
@@ -5,13 +5,13 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "libdrm-2.4.71";
+  name = "libdrm-2.4.73";
 
   src = fetchurl {
     url = "https://dri.freedesktop.org/libdrm/${name}.tar.bz2";
     hashOutput = false;
-    multihash = "QmWZznUmnCeyirF3Wk1MhG1mKDExh59ds9shZnLSVJYam2";
-    sha256 = "c66287ddeee5f46ea8f8880b94b80acb3bbc33ba6321d17767eef145046df9b8";
+    multihash = "QmZesngVTk8w5TVRrKcHRy8J6yBS4zD5oN3bp1knBgXWFD";
+    sha256 = "96bfd39242fe168017d95f22e141645a35591f5902a7d98c2fa4ca8c31df5e4d";
   };
 
   buildInputs = [
@@ -49,6 +49,8 @@ stdenv.mkDerivation rec {
       failEarly = true;
       pgpsigUrls = map (n: "${n}.sig") src.urls;
       pgpKeyFingerprints = [
+        # Emil Velikov
+        "8703 B670 0E7E E06D 7A39  B8D6 EDAE 37B0 2CEB 490D"
         "E8EB 5B34 081C E1EE A26E  FE19 5B5B DA07 1D49 CC38"
         "FC9B AE14 35A9 F7F6 64B8  2057 B5D6 2936 D1FC 9EE8"
         "D628 5B5E 8992 99F3 DA74  6184 191C 9B90 5522 B045"

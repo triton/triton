8429cabd7a1e3039a52dad6c31e9a258ef9c5c8e
Cody W. Opel <codyopel@gmail.com>
2016-11-19 13:12:14 -0500

    python: comment pgp keys

--- pkgs/all-pkgs/p/python/sources.nix
+++ pkgs/all-pkgs/p/python/sources.nix
@@ -2,26 +2,31 @@
   "2.7" = {
     versionPatch = "12";
     sha256 = "d7837121dd5652a05fef807c361909d255d173280c4e1a4ded94d73d80a1f978";
+    # Benjamin Peterson
     pgpKeyFingerprint = "C01E 1CAD 5EA2 C4F0 B8E3  5715 04C3 67C2 18AD D4FF";
   };
   "3.3" = {
     versionPatch = "6";
     sha256 = "0gsxpgd5p4mwd01gw501vsyahncyw3h9836ypkr3y32kgazy89jj";
+    # Georg Brandl
     pgpKeyFingerprint = "26DE A9D4 6133 91EF 3E25  C9FF 0A5B 1018 3658 0288";
   };
   "3.4" = {
     versionPatch = "5";
     sha256 = "ee64b1c8a47461009abf25252332d29a4e587cb4f0c1c96aa793a3732e9d898a";
+    # Larry Hastings
     pgpKeyFingerprint = "97FC 712E 4C02 4BBE A48A  61ED 3A5C A953 F73C 700D";
   };
   "3.5" = {
     versionPatch = "2";
     sha256 = "0010f56100b9b74259ebcd5d4b295a32324b58b517403a10d1a2aa7cb22bca40";
+    # Larry Hastings
     pgpKeyFingerprint = "97FC 712E 4C02 4BBE A48A  61ED 3A5C A953 F73C 700D";
   };
   "3.6" = {
     versionPatch = "0b3";
     sha256 = "b417a9024c8e5221c8b6bc154dd8e776653aed9d650f8e0d85d8ec82bf5c8fa5";
+    # Ned Deily
     pgpKeyFingerprint = "0D96 DF4D 4110 E5C4 3FBF  B17F 2D34 7EA6 AA65 421D";
   };
 }

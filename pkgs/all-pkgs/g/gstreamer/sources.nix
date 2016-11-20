e66b04fa51d98ccb6557fe02a7964c096c863763
Cody W. Opel <codyopel@gmail.com>
2016-11-19 22:13:02 -0500

    gstreamer(1.10): 1.10.0 -> 1.10.1

--- pkgs/all-pkgs/g/gstreamer/sources.nix
+++ pkgs/all-pkgs/g/gstreamer/sources.nix
@@ -5,7 +5,7 @@
     sha256 = "66b37762d4fdcd63bce5a2bec57e055f92420e95037361609900278c0db7c53f";
   };
   "1.10" = {
-    version = "1.10.0";
-    sha256 = "1e7778fabe480185bfc4ce552f0860de1443f6618a68a86ccdd65854fed37207";
+    version = "1.10.1";
+    sha256 = "f68df996e0e699382b935bb4783dd402c301377df18f57e28e0318c4b3bff6da";
   };
 }

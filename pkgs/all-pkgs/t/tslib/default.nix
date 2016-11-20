56c248292e7cd757e7fc7d798b1b7b335a944728
Cody W. Opel <codyopel@gmail.com>
2016-11-19 19:57:19 -0500

    tslib: 2015-08-20 -> 1.2

--- pkgs/all-pkgs/t/tslib/default.nix
+++ pkgs/all-pkgs/t/tslib/default.nix
@@ -1,17 +1,18 @@
 { stdenv
 , autoreconfHook
-, fetchFromGitHub
+, fetchurl
 }:
 
+let
+  version = "1.2";
+in
 stdenv.mkDerivation rec {
-  name = "tslib-2015-08-20";
-  
-  src = fetchFromGitHub {
-    version = 1;
-    owner = "kergoth";
-    repo = "tslib";
-    rev = "0a11148eff4111afc8b241b59fdca541fcfa69c1";
-    sha256 = "c484a8ba9479c846082b2a84ca8104320ce69e763d5078f61ba64518c28369c4";
+  name = "tslib-${version}";
+
+  src = fetchurl {
+    url = "https://github.com/kergoth/tslib/releases/download/${version}/"
+      + "${name}.tar.xz";
+    sha256 = "ae9d3f0ad615f2a1938466742db673bda8a0e8ab294c51d9b2264361401ddb26";
   };
 
   nativeBuildInputs = [
@@ -19,7 +20,11 @@ stdenv.mkDerivation rec {
   ];
 
   meta = with stdenv.lib; {
+    description = "Touchscreen access library";
+    homepage = https://github.com/kergoth/tslib/;
+    license = licenses.lgpl2;
     maintainers = with maintainers; [
+      codyopel
       wkennington
     ];
     platforms = with platforms;

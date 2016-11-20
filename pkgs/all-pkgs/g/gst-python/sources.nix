4c022bd74a76042a200d5ee386db4ea6f75094d9
Cody W. Opel <codyopel@gmail.com>
2016-11-19 22:11:52 -0500

    gst-python(1.10): 1.10.0 -> 1.10.1

--- pkgs/all-pkgs/g/gst-python/sources.nix
+++ pkgs/all-pkgs/g/gst-python/sources.nix
@@ -5,7 +5,7 @@
     sha256 = "149e7b9c2c361832bc765d39bce004d1ffe1b330c09c42dc902ca48867e804ce";
   };
   "1.10" = {
-    version = "1.10.0";
-    sha256 = "de59b493e47bf0221421873f6c80811dd7ff7b1929521264d3a559bc410dc741";
+    version = "1.10.1";
+    sha256 = "5305b34af74a182fd72de0e6093eb6f206ea4d26dad53d0f564603063e80b013";
   };
 }

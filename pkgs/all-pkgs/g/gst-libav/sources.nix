a7b6f7a6200a5945387efc662a9811a1af34ccaf
Cody W. Opel <codyopel@gmail.com>
2016-11-19 22:09:05 -0500

    gst-libav(1.10): 1.10.0 -> 1.10.1

--- pkgs/all-pkgs/g/gst-libav/sources.nix
+++ pkgs/all-pkgs/g/gst-libav/sources.nix
@@ -5,7 +5,7 @@
     sha256 = "9006a05990089f7155ee0e848042f6bb24e52ab1d0a59ff8d1b5d7e33001a495";
   };
   "1.10" = {
-    version = "1.10.0";
-    sha256 = "af98204411c78abb98233c1858f2886be6401304d24be218752bbbcede9bd85b";
+    version = "1.10.1";
+    sha256 = "27b28b8de0e6dff1e3952428e8ed8ba4a12f452f789ac0ae9bbe50f00a5c72c7";
   };
 }

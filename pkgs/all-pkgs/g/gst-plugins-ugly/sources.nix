9278433b198a07c6767f2e534a041917844b7b3d
Cody W. Opel <codyopel@gmail.com>
2016-11-19 22:10:59 -0500

    gst-plugins-ugly(1.10): 1.10.0 -> 1.10.1

--- pkgs/all-pkgs/g/gst-plugins-ugly/sources.nix
+++ pkgs/all-pkgs/g/gst-plugins-ugly/sources.nix
@@ -5,7 +5,7 @@
     sha256 = "6fa2599fdd072d31fbaf50c34af406e2be944a010b1f4eab67a5fe32a0310693";
   };
   "1.10" = {
-    version = "1.10.0";
-    sha256 = "97de8d1903abb54305732e6b8781c8c42f5cc5a2a548a8f31b38c189e81a55e0";
+    version = "1.10.1";
+    sha256 = "a5ecd59fc2091eeb52368de81cc6a91c1a1c19dc5bdde85ce90e1eed5d4183c2";
   };
 }

856104624f4ead35602dcc54ae7e8d80036591f6
William A. Kennington III <william@wkennington.com>
2016-11-22 16:19:00 -0800

    glfw: 3.2 -> 3.2.1

--- pkgs/all-pkgs/g/glfw/default.nix
+++ pkgs/all-pkgs/g/glfw/default.nix
@@ -9,7 +9,7 @@
 }:
 
 let
-  version = "3.2";
+  version = "3.2.1";
 in
 stdenv.mkDerivation rec {
   name = "glfw-${version}";
@@ -17,7 +17,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "https://github.com/glfw/glfw/releases/download/"
       + "${version}/${name}.zip";
-    sha256 = "d9983a129732bd400869dd26c9ef2ed253b1da0cfb79585ab7af63a175d0f652";
+    sha256 = "b7d55e13e07095119e7d5f6792586dd0849c9fcdd867d49a4a5ac31f982f7326";
   };
 
   nativeBuildInputs = [

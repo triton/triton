d1a6de6f9f829ea372fd3f7ea0295e880efc0b23
William A. Kennington III <william@wkennington.com>
2016-11-25 14:34:51 -0800

    openjdk: 8u122b03 -> 8u122b04

--- pkgs/development/compilers/openjdk/8.nix
+++ pkgs/development/compilers/openjdk/8.nix
@@ -41,7 +41,7 @@ let
       throw "openjdk requires i686-linux or x86_64-linux";
 
   update = "122";
-  build = "03";
+  build = "04";
 
   baseurl = "http://hg.openjdk.java.net/jdk8u/jdk8u";
   repover = "jdk8u${update}-b${build}";
@@ -57,14 +57,14 @@ let
     name = "openjdk-8u${update}b${build}";
 
     srcs = [
-      (fetchjava "" "d5689617e8701e3db3ec93525642dc273715ab876edf7c11be32e971627c7168")
-      (fetchjava "langtools" "9da834a96a5bb8ab53df9168c1393286b81a2adba1bda611d86cea0ac21f2ff5")
-      (fetchjava "hotspot" "c9de908ec5c609ba3767ddacd4834e24b001cf3bb8c69ed9aba7709c1f99e0ff")
-      (fetchjava "corba" "d07eb405155e05f35f15f49f3ac28b94ad8c0ebb1ff11144b7eed3cb4df422f6")
-      (fetchjava "jdk" "523044ec3522584d4ae420b7569f44eeadeae202560efa22268335642a672907")
-      (fetchjava "jaxws" "4bf372da731135d01b3c593596ce0728ca125a1e7131d70d5856fe84ede67f10")
-      (fetchjava "jaxp" "9bc0c44d04031e39f01788fc6d90a49ecc319de6511d455fbb16894dd4e5abf9")
-      (fetchjava "nashorn" "30aabd07070ac7eb65224f713c3ffc82eeaae99c35cfa369b198c600b95b7705")
+      (fetchjava "" "02e49c79d1d0beace7d53386652e018bde40e4564dde22d120eef4fc5ff118ff")
+      (fetchjava "langtools" "fd916811fa5a5afa110d67d6d59146680e20f42915d22e81ab5dee5e87a81a42")
+      (fetchjava "hotspot" "a1fd4e347b17860ea3dccaa8246b936c7bc249fb142dc19a683d3c9d97288ae4")
+      (fetchjava "corba" "34fe1ffaaad65d8c4cbe03e610d48e8910718e8fd5377c021a08c789db34aa47")
+      (fetchjava "jdk" "70702e20e42228d01398f84ef31c3c7381545bef44c20de0a776b391e48084cf")
+      (fetchjava "jaxws" "278bd0721280999422dc5c1fad55062b9cab064b113cd7efa842a4b1bd1c3273")
+      (fetchjava "jaxp" "2d957c3f47e778a6e6a53db41b8986b10da00c1d0b259a4a6b5bb5ff7c1de42c")
+      (fetchjava "nashorn" "cd4608561c4e35b34a86f71b11fb5a8eb882229fab8d124fdfbc04f921d49383")
     ];
 
     sourceRoot = ".";

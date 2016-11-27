0d2b5a07c0bb4d0a7e983ef68b2cab33a425c668
Cody W. Opel <codyopel@gmail.com>
2016-11-26 23:19:21 -0500

    ntp: 4.2.8p8 -> 4.2.8p9

--- pkgs/all-pkgs/n/ntp/default.nix
+++ pkgs/all-pkgs/n/ntp/default.nix
@@ -14,7 +14,7 @@
 let
   major = "4";
   minor = "2";
-  patch = "8p8";
+  patch = "8p9";
 in
 stdenv.mkDerivation rec {
   name = "ntp-${major}.${minor}.${patch}";
@@ -22,7 +22,7 @@ stdenv.mkDerivation rec {
   src = fetchurl rec {
     url = "http://archive.ntp.org/ntp${major}/ntp-${major}.${minor}/${name}.tar.gz";
     md5Url = "${url}.md5";
-    sha256 = "2ab3d0b5f0456e6311dda1cc27ab75da108762773a19e46abd938bd9407b97ee";
+    sha256 = "b724287778e1bac625b447327c9851eedef020517a3545625e9f652a90f30b72";
   };
 
   nativeBuildInputs = [

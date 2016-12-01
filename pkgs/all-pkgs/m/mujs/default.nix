8bcc452b60144a79e3a53b3c077e5f77bf27b8e5
William A. Kennington III <william@wkennington.com>
2016-12-01 01:19:09 -0800

    mujs: 2016-05-04 -> 2016-10-31

--- pkgs/all-pkgs/m/mujs/default.nix
+++ pkgs/all-pkgs/m/mujs/default.nix
@@ -3,14 +3,14 @@
 }:
 
 stdenv.mkDerivation {
-  name = "mujs-2016-05-04";
+  name = "mujs-2016-10-31";
 
   src = fetchFromGitHub {
-    version = 1;
+    version = 2;
     owner = "ccxvii";
     repo = "mujs";
-    rev = "1930f35933654d02234249b8c9b8c0d1c8c9fb6b";
-    sha256 = "007ef0382e1f67547556afae93629a9e90e3e9df7df14591b47da65c497b2da0";
+    rev = "a0ceaf5050faf419401fe1b83acfa950ec8a8a89";
+    sha256 = "023a927441a75e335c4899d44a64a0fd62117564dcbe45b112cd661df9e9f6c3";
   };
 
   preInstall = ''

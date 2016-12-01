31b7d84431720e3bfa4e25a009a7d947635d6660
William A. Kennington III <william@wkennington.com>
2016-11-30 16:28:41 -0800

    collectd: 5.6.1 -> 5.6.2

--- pkgs/all-pkgs/c/collectd/default.nix
+++ pkgs/all-pkgs/c/collectd/default.nix
@@ -4,63 +4,85 @@
 , curl
 , cyrus-sasl
 , gdk-pixbuf
-, jdk
+, gpsd
+, grpc
+, hiredis
+, i2c-tools
 , iptables
+, jdk
 , libatasmart
-#, libcredis
-#, libdbi
+, libcap
+, libdbi
 , libgcrypt
-#, libmemcached
-#, libmodbus
+, libmnl
 , libnotify
-#, liboping
+, liboping
 , libpcap
-#, libsigrok
-#, libvirt
 , libxml2
 , lm-sensors
+, lua
 , lvm2
 , mysql_lib
 , net-snmp
+, openldap
+, perl
 , postgresql
 , protobuf-c
+, protobuf-cpp
 , python2
-#, rabbitmq-c
-#, riemann
-#, rrdtool
+, python3
+, rrdtool
 , systemd_lib
-#, varnish
+, xfsprogs_lib
 , yajl
 }:
 
 stdenv.mkDerivation rec {
-  name = "collectd-5.6.1";
+  name = "collectd-5.6.2";
 
   src = fetchurl {
-    url = "https://github.com/collectd/collectd/releases/download/"
-      + "${name}/${name}.tar.bz2";
-    sha256 = "c30ff644f91407b4dc2d99787b99cc45ec00e538bd1cc269429d3c5e8a4aee2c";
+    urls = [
+      "https://storage.googleapis.com/collectd-tarballs/${name}.tar.bz2"
+      "https://github.com/collectd/collectd/releases/download/${name}/${name}.tar.bz2"
+    ];
+    hashOutput = false;  # Hashes at: https://collectd.org/download.shtml
+    sha256 = "cc0b4118a91e5369409ced22d1d8a85c1a400098419414160c1839268ecad0c6";
   };
 
   buildInputs = [
     curl
     cyrus-sasl
     gdk-pixbuf
-    jdk
+    gpsd
+    grpc
+    hiredis
+    i2c-tools
     iptables
+    jdk
     libatasmart
+    libcap
+    libdbi
     libgcrypt
+    libmnl
     libnotify
+    liboping
     libpcap
     libxml2
     lm-sensors
+    lua
     lvm2
     mysql_lib
     net-snmp
+    openldap
+    perl
     postgresql
     protobuf-c
+    protobuf-cpp
     python2
+    python3
+    rrdtool
     systemd_lib
+    xfsprogs_lib
     yajl
   ];
 

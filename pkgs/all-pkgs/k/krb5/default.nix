93eeab9819a65fc2c31ae4504cfc0ab0b7c2d685
William A. Kennington III <william@wkennington.com>
2016-12-02 17:27:54 -0800

    krb5: 1.14.4 -> 1.15

--- pkgs/all-pkgs/k/krb5/default.nix
+++ pkgs/all-pkgs/k/krb5/default.nix
@@ -19,19 +19,21 @@ let
   libOnly = type == "lib";
 
   tarballUrls = major: patch: [
-    "https://web.mit.edu/kerberos/dist/krb5/${major}/krb5-${major}.${patch}.tar.gz"
+    "https://web.mit.edu/kerberos/dist/krb5/${major}/krb5-${version major patch}.tar.gz"
   ];
 
-  major = "1.14";
-  patch = "4";
+  version = major: patch: "${major}${optionalString (patch != null) ".${patch}"}";
+
+  major = "1.15";
+  patch = null;
 in
 stdenv.mkDerivation rec {
-  name = "${type}krb5-${major}.${patch}";
+  name = "${type}krb5-${version major patch}";
 
   src = fetchurl {
     urls = tarballUrls major patch;
     hashOutput = false;
-    sha256 = "03a61a4280c9161771fb39019085dbe6a57aa602080515ff93b43cd6137e0b95";
+    sha256 = "fd34752774c808ab4f6f864f935c49945f5a56b62240b1ad4ab1af7b4ded127c";
   };
 
   prePatch= ''
@@ -61,6 +63,7 @@ stdenv.mkDerivation rec {
     "--with-crypto-impl=openssl"
     "--with-pkinit-crypto-impl=openssl"
     "--with-tls-impl=openssl"
+    "--enable-asan"
     "--enable-aesni"
     "--enable-kdc-lookaside-cache"
     "--enable-pkinit"
@@ -92,10 +95,10 @@ stdenv.mkDerivation rec {
   passthru = rec {
     srcVerification = fetchurl rec {
       failEarly = true;
-      urls = tarballUrls "1.14" "4";
+      urls = tarballUrls "1.15" null;
       pgpsigUrls = map (n: "${n}.asc") urls;
       pgpKeyFingerprint = "2C73 2B1C 0DBE F678 AB3A  F606 A32F 17FD 0055 C305";
-      sha256 = "03a61a4280c9161771fb39019085dbe6a57aa602080515ff93b43cd6137e0b95";
+      sha256 = "fd34752774c808ab4f6f864f935c49945f5a56b62240b1ad4ab1af7b4ded127c";
     };
   };
 

d0f53a45d54fb385a0483e3a652be684dbd07176
Cody W. Opel <codyopel@gmail.com>
2016-12-03 22:15:43 -0500

    gstreamer-editing-services: 1.10.1 -> 1.10.2

--- pkgs/all-pkgs/g/gstreamer-editing-services/sources.nix
+++ pkgs/all-pkgs/g/gstreamer-editing-services/sources.nix
@@ -1,7 +1,7 @@
 { }:
 {
   "1.10" = {
-    version = "1.10.1";
-    sha256 = "e9c60f454726aaa0a1dbc7b6e192f9a0a23733ea5680b474a0abc5fad7ed0d11";
+    version = "1.10.2";
+    sha256 = "db159aeb9df5959f7b246d4b6fcbc8d01cec2ced6d539d00c608458a245fa743";
   };
 }

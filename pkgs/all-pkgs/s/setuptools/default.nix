4079772164bd9222e6be503ef590db49923f72e0
Cody W. Opel <codyopel@gmail.com>
2016-12-03 22:30:50 -0500

    setuptools: 28.8.0 -> 30.1.0

--- pkgs/all-pkgs/s/setuptools/default.nix
+++ pkgs/all-pkgs/s/setuptools/default.nix
@@ -8,12 +8,12 @@
 stdenv.mkDerivation rec {
   name = "${python.executable}-setuptools-${version}";
   # Make sure to update pkgs/p/pip/bootstrap.nix setuptools hash when updating
-  version = "28.8.0";
+  version = "30.1.0";
 
   src = fetchPyPi {
     package = "setuptools";
     inherit version;
-    sha256 = "432a1ad4044338c34c2d09b0ff75d509b9849df8cf329f4c1c7706d9c2ba3c61";
+    sha256 = "73c7f183260cec2ef870128c77106ba7a978649b8c4cddc320ec3547615e885f";
   };
 
   buildInputs = [
@@ -31,7 +31,7 @@ stdenv.mkDerivation rec {
 
   passthru = {
     # Hash for pip bootstrap, see pkgs/p/pip/bootstrap.nix
-    bootstrapSha256 = "2e59d06ac798efce29c567ee0e0687514efca529a665b8f364cf497cd10d21b2";
+    bootstrapSha256 = "3861f9b31fb4c6e9bdf6e485adcaf982d12dbd45a9108cea7b08817d6fdea18d";
   };
 
   meta = with stdenv.lib; {

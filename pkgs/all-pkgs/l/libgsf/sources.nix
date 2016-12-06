8cf922e52624f5e27f8c0f7bf9b03a3877bba2f1
Cody W. Opel <codyopel@gmail.com>
2016-12-06 03:43:44 -0500

    libgsf: 1.14.40 -> 1.14.41

--- pkgs/all-pkgs/l/libgsf/sources.nix
+++ pkgs/all-pkgs/l/libgsf/sources.nix
@@ -1,7 +1,7 @@
 { }:
 {
   "1.14" = {
-    version = "1.14.40";
-    sha256 = "350638894b5e324d8880b50e4bc43daabaf9f3f43a66010af080ffa2d32d51e0";
+    version = "1.14.41";
+    sha256 = "150b98586a1021d5c49b3d4d065d0aa3e3674ae31db131af5372499d2d3f08d3";
   };
 }

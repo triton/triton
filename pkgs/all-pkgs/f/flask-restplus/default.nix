b06ea56f12dbd51dafc46f35a96f4c8d62c5f290
Cody W. Opel <codyopel@gmail.com>
2016-12-08 07:49:56 -0500

    flask-restplus: add 0.8 channel

--- pkgs/all-pkgs/f/flask-restplus/default.nix
+++ pkgs/all-pkgs/f/flask-restplus/default.nix
@@ -9,24 +9,39 @@
 , six
 
 , blinker
+, flask-restful
 , mock
 , nose
 #, rednose
 , tzlocal
+
+, channel ? "0.9"
 }:
 
 let
   inherit (stdenv.lib)
-    optionals;
+    optionals
+    versionOlder;
+
+  sources = {
+    "0.8" = {
+      version = "0.8.6";
+      sha256 = "3bb76cc156b9a09da62396d82b29fa31e4f27cccf79528538fe7155cf2785593";
+    };
+    "0.9" = {
+      version = "0.9.2";
+      sha256 = "c4313097a673ef2cffabceb44b6fdd03132ee5e7ab34d0289c37af12a3d11186";
+    };
+  };
+
+  source = sources."${channel}";
 in
 buildPythonPackage rec {
-  name = "flask-restplus-${version}";
-  version = "0.9.2";
+  name = "flask-restplus-${source.version}";
 
   src = fetchPyPi {
     package = "flask-restplus";
-    inherit version;
-    sha256 = "c4313097a673ef2cffabceb44b6fdd03132ee5e7ab34d0289c37af12a3d11186";
+    inherit (source) sha256 version;
   };
 
   propagatedBuildInputs = [
@@ -35,6 +50,8 @@ buildPythonPackage rec {
     jsonschema
     pytz
     six
+  ] ++ optionals (versionOlder source.version "0.9.0") [
+    flask-restful
   ] ++ optionals doCheck [
     blinker
     mock

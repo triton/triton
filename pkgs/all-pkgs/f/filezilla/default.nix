e2926155e773743c64e3d61fa5da597539769d2f
William A. Kennington III <william@wkennington.com>
2016-12-11 15:22:04 -0800

    filezilla: 3.22.2.2 -> 3.23.0.2

--- pkgs/all-pkgs/f/filezilla/default.nix
+++ pkgs/all-pkgs/f/filezilla/default.nix
@@ -13,7 +13,7 @@
 }:
 
 let
-  version = "3.22.2.2";
+  version = "3.23.0.2";
 
   file = "FileZilla_${version}_src.tar.bz2";
 in
@@ -24,7 +24,7 @@ stdenv.mkDerivation rec {
     urls = [
       "mirror://sourceforge/filezilla/FileZilla_Client/${version}/${file}"
     ];
-    sha256 = "bb7117e587ff05544c24eb7925e89c27afdd43ef6c0ce2e623c021d4479802c0";
+    sha256 = "3f464a7ad5f0e49085e455a023ec08130b663d392ea6aa6be9608527b015022f";
   };
 
   nativeBuildInputs = [

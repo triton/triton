8c21c75dd89d3921d344e1b6acceb8f98342acc5
Cody W. Opel <codyopel@gmail.com>
2016-12-11 20:04:12 -0500

    wavpack: 4.80.0 -> 5.0.0

--- pkgs/all-pkgs/w/wavpack/default.nix
+++ pkgs/all-pkgs/w/wavpack/default.nix
@@ -3,18 +3,13 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "wavpack-4.80.0";
+  name = "wavpack-5.0.0";
 
   src = fetchurl {
     url = "http://www.wavpack.com/${name}.tar.bz2";
-    multihash = "QmSj1A89S3tnTSWEF2iSqVdPKiT1aaq3kCekuStgk3BkCU";
-    sha256 = "79182ea75f7bd1ca931ed230062b435fde4a4c2e0dbcad048007bd1ef1e66be9";
+    multihash = "QmRXZ8PkXwe6pUUgdfY7bNE9MQVvUv4i1pu8LmEFFzpN9S";
+    sha256 = "918d7e32a19598df543b17fff840b10a0880f87296f9e32af454d256b6a64049";
   };
-
-  postPatch = ''
-    sed -i wavpack.pc.in \
-      -e '2iexec_prefix=@exec_prefix@'
-  '';
 
   meta = with stdenv.lib; {
     description = "Hybrid audio compression format";

9d71b75880e1ca8b8d7bdc587410fd56adccd03e
Cody W. Opel <codyopel@gmail.com>
2016-12-15 22:26:49 -0500

    game-music-emu: 0.6.0 -> 0.6.1

--- pkgs/all-pkgs/g/game-music-emu/default.nix
+++ pkgs/all-pkgs/g/game-music-emu/default.nix
@@ -5,14 +5,14 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "game-music-emu-0.6.0";
+  name = "game-music-emu-0.6.1";
 
   src = fetchurl {
     urls = [
       "https://bitbucket.org/mpyne/game-music-emu/downloads/${name}.tar.bz2"
       "mirror://gentoo/distfiles/${name}.tar.bz2"
     ];
-    sha256 = "11s9l938nxbrk7qb2k1ppfgizcz00cakbxgv0gajc6hyqv882vjh";
+    sha256 = "dc11bea098072d540d4d52dfb252e76fc3d3af67ee2807da48fbd8dbda3fd321";
   };
 
   nativeBuildInputs = [

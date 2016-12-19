71fc314cb504c2b509e3665169f820ee8095ea7b
William A. Kennington III <william@wkennington.com>
2016-12-19 09:43:39 -0800

    afflib: 3.7.4 -> 3.7.13

--- pkgs/all-pkgs/a/afflib/default.nix
+++ pkgs/all-pkgs/a/afflib/default.nix
@@ -12,17 +12,17 @@
 }:
 
 let
-  version = "3.7.4";
+  version = "3.7.13";
 in
 stdenv.mkDerivation {
   name = "afflib-${version}";
 
   src = fetchFromGitHub {
     version = 2;
-    owner = "simsong";
+    owner = "sshock";
     repo = "AFFLIBv3";
     rev = "v${version}";
-    sha256 = "4d917bec1749c7e587e57a5ebf41561546481b5b82f501e28c861bd192756fe4";
+    sha256 = "0a39e479f8daa17f2df490df7f5624b15edfa8b3aa0188a7850d8370035f793b";
   };
 
   nativeBuildInputs = [

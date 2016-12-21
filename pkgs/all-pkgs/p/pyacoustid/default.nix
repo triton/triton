7afa34b97eea8ec012e29f2e497216918f5ea191
Cody W. Opel <codyopel@gmail.com>
2016-12-21 01:36:41 -0500

    pyacoustid: 1.1.3 -> 1.1.4

--- pkgs/all-pkgs/p/pyacoustid/default.nix
+++ pkgs/all-pkgs/p/pyacoustid/default.nix
@@ -8,7 +8,7 @@
 }:
 
 let
-  version = "1.1.3";
+  version = "1.1.4";
 in
 buildPythonPackage rec {
   name = "pyacoustid-${version}";
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   src = fetchPyPi {
    package = "pyacoustid";
    inherit version;
-   sha256 = "6e303cb34ad10a3a3b50f6b969ef3269a0b6f0fbe713b8e8ead800d47621c06f";
+   sha256 = "b54bc803e936e49170f01febcf89621dda4a1ebb3d407e04e9ead290fa3a6cf3";
   };
 
   buildInputs = [
@@ -30,7 +30,7 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Bindings for Chromaprint acoustic fingerprinting";
-    homepage = "https://github.com/sampsyo/pyacoustid";
+    homepage = https://github.com/sampsyo/pyacoustid;
     license = licenses.mit;
     maintainers = with maintainers; [
       codyopel

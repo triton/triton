8de511fadcb60a2e1890d188ad1e10a53bf21c17
William A. Kennington III <william@wkennington.com>
2016-12-22 13:04:59 -0800

    opensmtpd: Add libbsd

--- pkgs/all-pkgs/o/opensmtpd/default.nix
+++ pkgs/all-pkgs/o/opensmtpd/default.nix
@@ -5,6 +5,7 @@
 
 , db
 , libasr
+, libbsd
 , libevent
 , openssl
 , pam
@@ -36,6 +37,7 @@ stdenv.mkDerivation rec {
   buildInputs = [
     db
     libasr
+    libbsd
     libevent
     openssl
     pam

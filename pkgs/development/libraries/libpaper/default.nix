1655c4748658ed3c40e8f91c622469e4dcb5127e
William A. Kennington III <william@wkennington.com>
2016-12-30 12:35:36 -0800

    libpaper: 1.1.24 -> 1.1.24+nmu5

--- pkgs/development/libraries/libpaper/default.nix
+++ pkgs/development/libraries/libpaper/default.nix
@@ -1,17 +1,31 @@
-{ stdenv, fetchurl }:
+{ stdenv
+, autoreconfHook
+, fetchurl
+}:
 
+let
+  version = "1.1.24+nmu5";
+in
 stdenv.mkDerivation rec {
-  version = "1.1.24";
   name = "libpaper-${version}";
 
   src = fetchurl {
     url = "mirror://debian/pool/main/libp/libpaper/libpaper_${version}.tar.gz";
-    sha256 = "0zhcx67afb6b5r936w5jmaydj3ks8zh83n9rm5sv3m3k8q8jib1q";
+    sha256 = "e29deda4cd7350189c71af0925cbf4a4473f9841d1419a922e1e8ff1954db1f2";
   };
 
-  meta = {
+  nativeBuildInputs = [
+    autoreconfHook
+  ];
+
+  meta = with stdenv.lib; {
     description = "Library for handling paper characteristics";
     homepage = "http://packages.debian.org/unstable/source/libpaper";
-    license = stdenv.lib.licenses.gpl2;
+    license = licenses.gpl2;
+    maintainers = with maintainers; [
+      wkennington
+    ];
+    platforms = with platforms;
+      x86_64-linux;
   };
 }

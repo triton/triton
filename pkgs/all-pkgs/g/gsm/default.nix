fc3cc280bd9cb46cbddde6e02ddc451b70de38a2
Cody W. Opel <codyopel@gmail.com>
2017-01-01 18:44:13 -0500

    gsm: 1.0.14 -> 1.0.16

--- pkgs/all-pkgs/g/gsm/default.nix
+++ pkgs/all-pkgs/g/gsm/default.nix
@@ -1,5 +1,6 @@
 { stdenv
 , fetchurl
+, lib
 
 , staticSupport ? false
 }:
@@ -11,25 +12,23 @@ let
 in
 
 stdenv.mkDerivation rec {
-  name = "gsm-${version}";
-  version = "1.0.14";
+  name = "gsm-1.0.16";
 
   src = fetchurl {
     url = "http://www.quut.com/gsm/${name}.tar.gz";
-    sha256 = "0b1mx69jq88wva3wk0hi6fcl5a52qhnq2f9p3f3jdh5k61ma252q";
+    multihash = "QmRfJYdFiqexpN9ob1CScLZFE2mQit1JaTvR3VSPr31iqj";
+    sha256 = "725a3768a1e23ab8648b4df9d470aed38eb1635af3cbc8d0b64fef077236f4ce";
   };
 
-  postPatch =
-    /* Fix include directory */ ''
-      sed -i Makefile \
-        -e 's,$(GSM_INSTALL_ROOT)/inc,$(GSM_INSTALL_ROOT)/include/gsm,'
-    '' + optionalString (!staticSupport)
-    /* Build ELF shared object */ ''
-      sed -i Makefile \
-        -e 's,libgsm.a,libgsm.so,' \
-        -e 's/$(AR) $(ARFLAGS) $(LIBGSM) $(GSM_OBJECTS)/$(LD) -shared -Wl,-soname,libgsm.so -o $(LIBGSM) $(GSM_OBJECTS) -lc/' \
-        -e '/$(RANLIB) $(LIBGSM)/d'
-    '';
+  postPatch = /* Fix include directory */ ''
+    sed -i Makefile \
+      -e 's,$(GSM_INSTALL_ROOT)/inc,$(GSM_INSTALL_ROOT)/include/gsm,'
+  '' + optionalString (!staticSupport) /* Build ELF shared object */ ''
+    sed -i Makefile \
+      -e 's,libgsm.a,libgsm.so,' \
+      -e 's/$(AR) $(ARFLAGS) $(LIBGSM) $(GSM_OBJECTS)/$(LD) -shared -Wl,-soname,libgsm.so -o $(LIBGSM) $(GSM_OBJECTS) -lc/' \
+      -e '/$(RANLIB) $(LIBGSM)/d'
+  '';
 
   makeFlags = [
     "SHELL=${stdenv.shell}"
@@ -38,9 +37,11 @@ stdenv.mkDerivation rec {
 
   NIX_CFLAGS_COMPILE = optional (!staticSupport) "-fPIC";
 
-  preInstall = "mkdir -pv $out/{bin,lib,man/man1,man/man3,include/gsm}";
+  preInstall = ''
+    mkdir -pv "$out"/{bin,lib,man/man1,man/man3,include/gsm}
+  '';
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Lossy speech compression codec";
     homepage = http://www.quut.com/gsm/;
     license = licenses.bsd2;

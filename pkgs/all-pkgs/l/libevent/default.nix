6b6b723b496a696468e506c1fcf59ede5f439842
William A. Kennington III <william@wkennington.com>
2017-01-02 01:20:40 -0800

    libevent: 2.0.22 -> 2.1.7-rc

--- pkgs/all-pkgs/l/libevent/default.nix
+++ pkgs/all-pkgs/l/libevent/default.nix
@@ -1,12 +1,13 @@
 { stdenv
-, fetchurl
+, autoreconfHook
+, fetchFromGitHub
 , python
 
 , openssl
 }:
 
 let
-  version = "2.0.22";
+  version = "2.1.7-rc";
 
   tarballUrls = version: [
     "https://github.com/libevent/libevent/releases/download/release-${version}-stable/libevent-${version}-stable.tar.gz"
@@ -15,13 +16,22 @@ in
 stdenv.mkDerivation rec {
   name = "libevent-${version}";
 
-  src = fetchurl {
+  /*src = fetchurl {
     urls = tarballUrls version;
     hashOutput = false;
     sha256 = "18qz9qfwrkakmazdlwxvjmw8p76g70n3faikwvdwznns1agw9hki";
+  };*/
+
+  src = fetchFromGitHub {
+    version = 2;
+    owner = "libevent";
+    repo = "libevent";
+    rev = "release-${version}";
+    sha256 = "70a1e1042a1035f2e0d0d2edcb5eae84ca718b69a26803bebf6115e72dd5db41";
   };
 
   nativeBuildInputs = [
+    autoreconfHook
     python
   ];
 
@@ -33,7 +43,11 @@ stdenv.mkDerivation rec {
     patchShebangs event_rpcgen.py
   '';
 
-  passthru = {
+  configureFlags = [
+    "--disable-samples"
+  ];
+
+  /*passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
       urls = tarballUrls "2.0.22";
@@ -42,7 +56,7 @@ stdenv.mkDerivation rec {
       inherit (src) outputHashAlgo;
       outputHash = "18qz9qfwrkakmazdlwxvjmw8p76g70n3faikwvdwznns1agw9hki";
     };
-  };
+  };*/
 
   meta = with stdenv.lib; {
     description = "Event notification library";

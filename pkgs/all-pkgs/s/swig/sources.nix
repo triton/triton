e5c61ebab82c39656c5c0d73bbd4b68c2e091457
William A. Kennington III <william@wkennington.com>
2017-01-07 15:26:44 -0800

    swig: 3.0.10 -> 3.0.11

--- pkgs/all-pkgs/s/swig/sources.nix
+++ pkgs/all-pkgs/s/swig/sources.nix
@@ -5,8 +5,8 @@
     sha256 = "65e13f22a60cecd7279c59882ff8ebe1ffe34078e85c602821a541817a4317f7";
   };
   "3" = {
-    version = "3.0.10";
-    multihash = "QmRR4RtPpAunX5fQK5ooUygZmHDE35hqQE8YuxwS1eiWiM";
-    sha256 = "2939aae39dec06095462f1b95ce1c958ac80d07b926e48871046d17c0094f44c";
+    version = "3.0.11";
+    multihash = "QmQyibwJnsJ1QncZ7AXtB3BpuNyjihcY4GktFiEUCmuCEn";
+    sha256 = "d9031d531d7418829a54d0d51c4ed9007016b213657ec70be44031951810566e";
   };
 }

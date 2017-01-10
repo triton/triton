f7b68a4d63d5f135aceeabfd150d52834bb2ad93
William A. Kennington III <william@wkennington.com>
2017-01-10 00:17:14 -0800

    unifont: 9.0.02 -> 9.0.06

--- pkgs/data/fonts/unifont/default.nix
+++ pkgs/data/fonts/unifont/default.nix
@@ -1,7 +1,11 @@
-{ stdenv, fetchurl, xorg }:
+{ stdenv
+, fetchurl
+
+, xorg
+}:
 
 stdenv.mkDerivation rec {
-  name = "unifont-9.0.02";
+  name = "unifont-9.0.06";
 
   buildInputs = [
     xorg.mkfontdir
@@ -16,14 +20,14 @@ stdenv.mkDerivation rec {
     let
       unifont_pcf = fetchurl {
         url = "http://unifoundry.com/pub/${name}/font-builds/${name}.pcf.gz";
-        multihash = "Qmf7skRRpT8UXnEBSKGGahRKZDMRxecey4noUEtV9vzwQu";
-        sha256 = "29ac5e89b6f40537a8bd5f1460d3927cff8cfeb37975eddf8f26b85e975ca025";
+        multihash = "QmQ8rcmpvnSHTgrFLq8VhvL88i1jQfBEczZHbX1aPPapsU";
+        sha256 = "6d23e82ea3fd3d79849d675c0c30129b62a3973a83b4cdc05f9994efef773b86";
       };
 
       unifont_ttf = fetchurl {
         url = "http://unifoundry.com/pub/${name}/font-builds/${name}.ttf";
-        multihash = "QmU8DRxEN2SqwR8Vki5RNPdX4Wt9B5KkV28T1DCtpFFhLf";
-        sha256 = "753a9cb25da2f4e52df96f8b6ee9246c1b130cdc8ee3beaad6227b771f294291";
+        multihash = "QmbKttQAQBgfAdWHKgXjXT6kKgDLrzr4sK3BDH94qM62oY";
+        sha256 = "e217fbf24ac1ba3f028ed937b30b6c27f31fd1288857ca0fb0b11100637c2665";
       };
     in ''
       mkdir -pv $out/share/fonts $out/share/fonts/truetype

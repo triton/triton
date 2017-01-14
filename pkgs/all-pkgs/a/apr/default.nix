f021b69652ac7eeba6ea3cb8864c101f90b460cf
Cody W. Opel <codyopel@gmail.com>
2017-01-13 21:43:53 -0500

    apr: disable buildDirCheck

--- pkgs/all-pkgs/a/apr/default.nix
+++ pkgs/all-pkgs/a/apr/default.nix
@@ -11,6 +11,9 @@ stdenv.mkDerivation rec {
     sha256 = "0ypn51xblix5ys9xy7da3ngdydip0qqh9rdq8nz54w9aq8lys0vx";
   };
 
+  # FIXME
+  buildDirCheck = false;
+
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;

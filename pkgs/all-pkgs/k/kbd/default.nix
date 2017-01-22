bd5375340f8acedab3219a8b5f903551abe3e63e
William A. Kennington III <william@wkennington.com>
2017-01-21 22:30:00 -0800

    kbd: 2.0.3 -> 2.0.4

--- pkgs/all-pkgs/k/kbd/default.nix
+++ pkgs/all-pkgs/k/kbd/default.nix
@@ -2,35 +2,24 @@
 , autoreconfHook
 , fetchurl
 
-, bzip2
 , check
-, gzip
 , pam
-
-, neoSupport ? false
 }:
 
 let
-  /* Get the dvorak programmer keymap (present in X but not in kbd) */
-  dvpSrc = fetchurl {
-    url = "http://kaufmann.no/downloads/linux/dvp-1_2_1.map.gz";
-    sha256 = "0e859211cfe16a18a3b9cbf2ca3e280a23a79b4e40b60d8d01d0fde7336b6d50";
-  };
+  tarballUrls = version: [
+    "mirror://kernel/linux/utils/kbd/kbd-${version}.tar"
+  ];
 
-  # German keyboard layout
-  neoSrc = fetchurl {
-    name = "neo.map";
-    url = "https://svn.neo-layout.org/linux/console/neo.map?r=2455";
-    sha256 = "1wlgp09wq84hml60hi4ls6d4zna7vhycyg40iipyh1279i91hsx7";
-  };
+  version = "2.0.4";
 in
-
 stdenv.mkDerivation rec {
-  name = "kbd-2.0.3";
+  name = "kbd-${version}";
 
   src = fetchurl {
-    url = "mirror://kernel/linux/utils/kbd/${name}.tar.xz";
-    sha256 = "0ppv953gn2zylcagr4z6zg5y2x93dxrml29plypg6xgbq3hrv2bs";
+    urls = map (n: "${n}.xz") (tarballUrls version);
+    hashOutput = false;
+    sha256 = "5fd90af6beb225a9bb9b9fb414c090fba53c9a55793e172f508cd43652e59a88";
   };
 
   nativeBuildInputs = [
@@ -42,24 +31,6 @@ stdenv.mkDerivation rec {
     pam
   ];
 
-  patches = [
-    ./console-fix.patch
-  ];
-
-  postPatch = stdenv.lib.optionalString neoSupport ''
-    mkdir -p data/keymaps/i386/neo
-    cat "${neoSrc}" > data/keymaps/i386/neo/neo.map
-    sed -i -e 's,^KEYMAPSUBDIRS *= *,&i386/neo ,' data/Makefile.am
-  '' + ''
-    # Add the dvp keyboard in the dvorak folder
-    ${gzip}/bin/gzip -c -d ${dvpSrc} > data/keymaps/i386/dvorak/dvp.map
-
-    # Fix the path to gzip/bzip2.
-    substituteInPlace src/libkeymap/findfile.c \
-      --replace gzip ${gzip}/bin/gzip \
-      --replace bzip2 ${bzip2}/bin/bzip2 \
-  '';
-
   configureFlags = [
     "--enable-optional-progs"
     "--enable-libkeymap"
@@ -69,6 +40,16 @@ stdenv.mkDerivation rec {
   makeFlags = [
     "setowner="
   ];
+
+  passthru = {
+    srcVerification = fetchurl {
+      failEarly = true;
+      pgpsigUrls = map (n: "${n}.sign") (tarballUrls version);
+      pgpDecompress = true;
+      pgpKeyFingerprint = "7F2A 3D07 2981 49A0 793C  9A4E A45A BA54 4CFF D434";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
 
   meta = with stdenv.lib; {
     homepage = ftp://ftp.altlinux.org/pub/people/legion/kbd/;

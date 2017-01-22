e57d1c23bf16dc2977a3709521f11398b1527603
William A. Kennington III <william@wkennington.com>
2017-01-21 18:04:03 -0800

    bash: 4.4p05 -> 4.4p011

--- pkgs/all-pkgs/b/bash/patches.nix
+++ pkgs/all-pkgs/b/bash/patches.nix
@@ -4,4 +4,10 @@
   "bash44-003" = "1chqww2rj6g42b8s60q5zlzy0jzp684jkpsbrbfy1vzxja8mmpsi";
   "bash44-004" = "1cy8abf96hkrjhw921ndr0shlcnc52bg45rn6xri4v5clhq0l25d";
   "bash44-005" = "0a8515kyk4zsgmvlqvlganjfr7pq0j6kzpr4d6xx02kpbdr4n7i2";
+  "bash44-006" = "1f24wgqngmj2mrj9yibwvc2zvlmn5xi53mnw777g3l40c4m2x3ka";
+  "bash44-007" = "1bzdsnqaf05gdbqpsixhan8vygjxpcxlz1dd8d9f5jdznw3wq76y";
+  "bash44-008" = "1firw915mjm03hbbw9a70ch3cpgrgnvqjpllgdnn6csr8q04f546";
+  "bash44-009" = "0g1l56kvw61rpw7dqa9fcl9llkl693h73g631hrhxlm030ddssqb";
+  "bash44-010" = "01lfhrkdsdkdz8ypzapr614ras23x7ckjnr60aa5bzkaqprccrc4";
+  "bash44-011" = "038p7mhnq9m65g505hi3827jkf9f35nd1cy00w8mwafpyxp44mnx";
 }

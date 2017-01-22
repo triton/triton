2f8ef04cd90208323d05cf3cccd21a483959676e
William A. Kennington III <william@wkennington.com>
2017-01-21 19:11:46 -0800

    samba: 4.5.3 -> 4.5.4

--- pkgs/all-pkgs/s/samba/default.nix
+++ pkgs/all-pkgs/s/samba/default.nix
@@ -51,7 +51,7 @@ let
     optionals
     optionalString;
 
-  version = "4.5.3";
+  version = "4.5.4";
   name = "samba${if isClient then "-client" else ""}-${version}";
 
   tarballUrls = [
@@ -66,7 +66,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     urls = map (n: "${n}.gz") tarballUrls;
     hashOutput = false;
-    sha256 = "f06dc454a1bb72b21a329e1ad75a14795886401671ec7439d69a6b824c492eca";
+    sha256 = "1800d10ea14cabef67c83cc56a0f37f4f30ba5df218b3a1e7c874069a08c0f40";
   };
 
   nativeBuildInputs = [

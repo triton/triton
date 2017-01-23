3bef15b7569b5434f351ddd8c593095dc3cdc791
William A. Kennington III <william@wkennington.com>
2017-01-23 10:42:18 -0800

    DBD-mysql: 4.033 -> 4.041

--- pkgs/development/perl-modules/DBD-mysql/default.nix
+++ pkgs/development/perl-modules/DBD-mysql/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, buildPerlPackage, DBI, mysql }:
 
 buildPerlPackage rec {
-  name = "DBD-mysql-4.033";
+  name = "DBD-mysql-4.041";
 
   src = fetchurl {
     url = "mirror://cpan/authors/id/C/CA/CAPTTOFU/${name}.tar.gz";
-    sha256 = "0769xakykps0cx368g4vaips4w3bjk383rianiavq7sq6g6bp66c";
+    sha256 = "0769xakyaps0cx368g4vaips4w3bjk383rianiavq7sq6g6bp66c";
   };
 
   buildInputs = [ mysql.lib ] ;

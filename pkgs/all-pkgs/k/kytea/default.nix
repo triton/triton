861d2313b601ac477b5eee7d54b2d440d884ce4c
William A. Kennington III <william@wkennington.com>
2017-01-23 17:31:01 -0800

    kytea: Add multihash

--- pkgs/all-pkgs/k/kytea/default.nix
+++ pkgs/all-pkgs/k/kytea/default.nix
@@ -7,6 +7,7 @@ stdenv.mkDerivation rec {
   
   src = fetchurl {
     url = "http://www.phontron.com/kytea/download/${name}.tar.gz";
+    multihash = "Qme88yCXrCeimKQmBFZXaourVyhTVL9Tv47uDYf7Cgvhvy";
     sha256 = "534a33d40c4dc5421f053c71a75695c377df737169f965573175df5d2cff9f46";
   };
 

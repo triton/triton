9186f3b5daf48290a707a1b035b8ba4cee4158c2
William A. Kennington III <william@wkennington.com>
2017-01-23 16:35:35 -0800

    iso-codes: 3.68 -> 3.74

--- pkgs/all-pkgs/i/iso-codes/default.nix
+++ pkgs/all-pkgs/i/iso-codes/default.nix
@@ -8,15 +8,13 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "iso-codes-3.68";
+  name = "iso-codes-3.74";
 
   src = fetchurl {
-    urls = [
-      "http://pkg-isocodes.alioth.debian.org/downloads/${name}.tar.xz"
-      "mirror://gentoo/distfiles/${name}.tar.xz"
-    ];
+    url = "https://pkg-isocodes.alioth.debian.org/downloads/${name}.tar.xz";
+    multihash = "QmXrvkNV4eCdD8pyDRQYd8fPStrN77dWQ7hBNUhDJcBgDC";
     hashOutput = false;
-    sha256 = "5881cf7caa5adfffb14ade99138949324c28a277babe8d07dafbff521acef9d1";
+    sha256 = "21f4f3cea8fe09f5b53784522303a0e1e7d083964ecaf1c75b1441d4d9ec6aee";
   };
 
   nativeBuildInputs = [
@@ -31,19 +29,18 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
-      url = "http://pkg-isocodes.alioth.debian.org/downloads/${name}.tar.xz";
-      pgpsigUrl = "${url}.sig";
+      pgpsigUrls = map (n: "${n}.sig") src.urls;
       pgpKeyFingerprints = [
         "D1CB 8F39 BC5D ED24 C5D2  C78C 1302 F1F0 36EB EB19"
         "F972 A168 A270 3B34 CC23  E09F D4E5 EDAC C014 3D2D"
       ];
-      inherit (src) outputHashAlgo outputHash;
+      inherit (src) urls outputHashAlgo outputHash;
     };
   };
 
   meta = with stdenv.lib; {
     description = "Various ISO codes packaged as XML files";
-    homepage = http://pkg-isocodes.alioth.debian.org/;
+    homepage = https://pkg-isocodes.alioth.debian.org/;
     license = licenses.free;
     maintainers = with maintainers; [
       codyopel

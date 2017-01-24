e7f7f25ff6b78af5eb55bbbaf2ee15543fe9b7a0
Cody W. Opel <codyopel@gmail.com>
2017-01-24 05:50:22 -0500

    nvidia-settings: 375.26 -> 378.09

--- pkgs/all-pkgs/n/nvidia-settings/default.nix
+++ pkgs/all-pkgs/n/nvidia-settings/default.nix
@@ -18,7 +18,7 @@
 }:
 
 let
-  version = "375.26";
+  version = "378.09";
 in
 stdenv.mkDerivation rec {
   name = "nvidia-settings-${version}";
@@ -26,7 +26,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "http://http.download.nvidia.com/XFree86/nvidia-settings/"
       + "nvidia-settings-${version}.tar.bz2";
-    sha256 = "beb0d88e2f63427239fd2014299ef7bf780e70800f68be5d011575e858711fe9";
+    sha256 = "1bfdb4d42f4afc83a51f63574c3ae70c5f36e4dab87fc8c23564fef62bbe53ba";
   };
 
   nativeBuildInputs = [

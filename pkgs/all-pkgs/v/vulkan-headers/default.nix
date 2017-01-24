76c5ceb825e9df0d9cda7ea6b4430d0932f4e72b
Cody W. Opel <codyopel@gmail.com>
2017-01-24 05:12:55 -0500

    vulkan-headers: 1.0.38 -> 1.0.39

--- pkgs/all-pkgs/v/vulkan-headers/default.nix
+++ pkgs/all-pkgs/v/vulkan-headers/default.nix
@@ -1,16 +1,17 @@
 { stdenv
 , fetchFromGitHub
+, lib
 }:
 
 stdenv.mkDerivation rec {
-  name = "vulkan-headers-1.0.38";
+  name = "vulkan-headers-1.0.39";
 
   src = fetchFromGitHub {
     version = 2;
     owner = "KhronosGroup";
     repo = "Vulkan-Docs";
-    rev = "70b659d28d01a75209d27afbfd764d1ef0d1dcb3";
-    sha256 = "11d00582e2ce99fff586c32c4bbe1edc70fe1efb30517c257a5547ce8f5f4834";
+    rev = "ca4abe0d34ca8ed8410c65c177f8658da183576d";
+    sha256 = "4c20be55c7b9ed06a6bffe1da172844a8621b2b2b0599380e606c366dfd0992b";
   };
 
   installPhase = ''
@@ -20,7 +21,7 @@ stdenv.mkDerivation rec {
       "$out/include/vulkan/vk_platform.h"
   '';
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "The Vulkan API Specification";
     homepage = https://github.com/KhronosGroup/Vulkan-Docs;
     license = licenses.asl20;

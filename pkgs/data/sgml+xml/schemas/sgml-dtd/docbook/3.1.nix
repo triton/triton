6da0d077e2050c5ca2f1104f4344d164b3ecba94
William A. Kennington III <william@wkennington.com>
2017-01-23 16:15:33 -0800

    docbook: Add multihashes

--- pkgs/data/sgml+xml/schemas/sgml-dtd/docbook/3.1.nix
+++ pkgs/data/sgml+xml/schemas/sgml-dtd/docbook/3.1.nix
@@ -4,11 +4,13 @@ let
 
   src = fetchurl {
     url = http://www.oasis-open.org/docbook/sgml/3.1/docbk31.zip;
+    multihash = "QmfUY5gGjunL7MzA9n5Csn4wCj2FXUNaADF852bCCX6sxZ";
     sha256 = "0f25ch7bywwhdxb1qa0hl28mgq1blqdap3rxzamm585rf4kis9i0";
   };
 
   isoents = fetchurl {
     url = http://www.oasis-open.org/cover/ISOEnts.zip;
+    multihash = "QmZZ1YvduSEzQFw6cjczYdv4UsdSTdhWgiTHjEhAARBSf6";
     sha256 = "1clrkaqnvc1ja4lj8blr0rdlphngkcda3snm7b9jzvcn76d3br6w";
   };
 

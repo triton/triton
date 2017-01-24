5c2fe2c3c0f2696b3192aac2d55f0a11f854a56b
William A. Kennington III <william@wkennington.com>
2017-01-23 16:25:17 -0800

    libogg: Add multihash

--- pkgs/all-pkgs/l/libogg/default.nix
+++ pkgs/all-pkgs/l/libogg/default.nix
@@ -7,6 +7,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "http://downloads.xiph.org/releases/ogg/${name}.tar.xz";
+    multihash = "QmPMKrTQv2CB3bkrCb38Qky3RS1jHB4wDxyGcEwn67o29A";
     sha256 = "16z74q422jmprhyvy7c9x909li8cqzmvzyr8cgbm52xcsp6pqs1z";
   };
 

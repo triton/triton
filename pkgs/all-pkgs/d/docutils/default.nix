3c00a695ba1f6c5c04f10bf784552c526be45fd0
William A. Kennington III <william@wkennington.com>
2017-01-25 14:29:42 -0800

    docutils: 0.12 -> 0.13.1

--- pkgs/all-pkgs/d/docutils/default.nix
+++ pkgs/all-pkgs/d/docutils/default.nix
@@ -4,7 +4,7 @@
 }:
 
 let
-  version = "0.12";
+  version = "0.13.1";
 in
 buildPythonPackage {
   name = "docutils-${version}";
@@ -12,7 +12,7 @@ buildPythonPackage {
   src = fetchPyPi {
     package = "docutils";
     inherit version;
-    sha256 = "c7db717810ab6965f66c8cf0398a98c9d8df982da39b4cd7f162911eb89596fa";
+    sha256 = "718c0f5fb677be0f34b781e04241c4067cbd9327b66bdd8e763201130f5175be";
   };
   
   meta = with stdenv.lib; {

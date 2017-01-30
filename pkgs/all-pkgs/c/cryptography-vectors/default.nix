0151e812b113dc075b2f649e41b64c6fa99f7dba
Cody W. Opel <codyopel@gmail.com>
2017-01-30 03:12:18 -0500

    cryptography-vectors: 1.7 -> 1.7.2

--- pkgs/all-pkgs/c/cryptography-vectors/default.nix
+++ pkgs/all-pkgs/c/cryptography-vectors/default.nix
@@ -1,10 +1,11 @@
 { stdenv
 , buildPythonPackage
 , fetchPyPi
+, lib
 }:
 
 let
-  version = "1.7";
+  version = "1.7.2";
 in
 buildPythonPackage {
   name = "cryptography_vectors-${version}";
@@ -12,10 +13,10 @@ buildPythonPackage {
   src = fetchPyPi {
     package = "cryptography_vectors";
     inherit version;
-    sha256 = "d25da535d860a6712761ae88a29ba8b1211043a468cfeafb1b4335bc530368a5";
+    sha256 = "4be4eee8a11deee5c2f00e389b49de8ce2642130282d1cd0adffb2f7dbe0acdc";
   };
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     maintainers = with maintainers; [
       wkennington
     ];

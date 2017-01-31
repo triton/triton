233613926d78c790a236da26bb0a0836e5d9d4b9
William A. Kennington III <william@wkennington.com>
2017-01-31 13:24:05 -0800

    readline: 7.0p001 -> 7.0p003

--- pkgs/all-pkgs/r/readline/patches.nix
+++ pkgs/all-pkgs/r/readline/patches.nix
@@ -1,3 +1,5 @@
 {
   "readline70-001" = "0xm3sxvwmss7ddyfb11n6pgcqd1aglnpy15g143vzcf75snb7hcs";
+  "readline70-002" = "0n1dxmqsbjgrfxb1hgk5c6lsraw4ncbnzxlsx7m35nym6lncjiw7";
+  "readline70-003" = "1027kmymniizcy0zbdlrczxfx3clxcdln5yq05q9yzlc6y9slhwy";
 }

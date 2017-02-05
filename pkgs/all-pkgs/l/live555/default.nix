045d12b7a1924ae8f1b53a2542821b57d48f6f6c
Cody W. Opel <codyopel@gmail.com>
2017-02-05 16:06:08 -0500

    live555: 2016.02.09 -> 2017.01.26

--- pkgs/all-pkgs/l/live555/default.nix
+++ pkgs/all-pkgs/l/live555/default.nix
@@ -1,44 +1,48 @@
-{ stdenv, fetchurl }:
+{ stdenv
+, fetchurl
+, lib
+}:
 
+let
+  version = "2017.01.26";
+in
 stdenv.mkDerivation rec {
   name = "live555-${version}";
-  version = "2016.02.09";
 
   src = fetchurl {
-    # upstream doesn't provide a stable URL, use vlc mirror
-    url = "https://download.videolan.org/pub/videolan/testing/contrib/live555/"
-        + "live.${version}.tar.gz";
-    sha256 = "02z2f8z5cy0ajnh9pgar40lsxdknfw5cbyw52138hxnpr6adrvak";
+    # upstream doesn't provide a stable URL, use videolan mirror
+    url = "mirror://videolan/testing/contrib/live555/live.${version}.tar.gz";
+    sha256 = "3c4d440b79e415eb3a86d30d0cd061d3edcaaa0281909e8629e9756c2c375458";
   };
 
-  postPatch =
-    /* Remove hardcodec paths */ ''
-      sed -i genMakefiles \
-        -e 's,/bin/rm,rm,g'
-    '' +
-    /* Add fPIC support */ ''
-      sed -i config.linux \
-        -e 's/$(INCLUDES) -I. -O2 -DSOCKLEN_T/$(INCLUDES) -I. -O2 -I. -fPIC -DRTSPCLIENT_SYNCHRONOUS_INTERFACE=1 -DSOCKLEN_T/g' \
-    '';
+  postPatch = /* Remove hard-coded paths */ ''
+    sed -i genMakefiles \
+      -e 's,/bin/rm,rm,g'
+  '' + /* Add fPIC support */ ''
+    sed -i config.linux \
+      -e 's/$(INCLUDES) -I. -O2 -DSOCKLEN_T/$(INCLUDES) -I. -O2 -I. -fPIC -DRTSPCLIENT_SYNCHRONOUS_INTERFACE=1 -DSOCKLEN_T/g' \
+  '';
 
   configureFlags = [
     "linux"
   ];
 
-  makeFlags = [
-    "PREFIX=$(out)"
-  ];
+  preBuild = ''
+    makeFlagsArray+=("PREFIX=$out")
+  '';
 
   configureScript = "./genMakefiles";
 
   # Not a standard configure script
   dontAddPrefix = true;
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Libraries for RTP/RTCP/RTSP/SIP multimedia streaming";
     homepage = http://www.live555.com/liveMedia/;
     license = licenses.lgpl21Plus;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [
+      codyopel
+    ];
     platforms = with platforms;
       x86_64-linux;
   };

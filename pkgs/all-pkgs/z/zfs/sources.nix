bcf1cd230bde43168a3184495168186f1ddad389
William A. Kennington III <william@wkennington.com>
2017-02-04 17:04:18 -0800

    zfs_dev: 2017-01-23 -> 2017-02-04

--- pkgs/all-pkgs/z/zfs/sources.nix
+++ pkgs/all-pkgs/z/zfs/sources.nix
@@ -6,10 +6,10 @@
     sha256 = "2eb73910e4728bc280f982361e0c0139f944d52e2a55c06e2b8f0ebe6e452e82";
   };
   "dev" = {
-    date = "2017-01-23";
-    rev = "3d43125fd465d869c813eb30a0233c0b6aae1209";
+    date = "2017-02-04";
+    rev = "298ec40b6d9edcbde7b1fbbf172bd05be0eb9b97";
     maxKernelVersion = "4.11";
     fetchzipVersion = 2;
-    sha256 = "f1d0a33e6ab22d5f952546cc63c03b2909523a3a42e16477115b5c9d183a5f6e";
+    sha256 = "98af42b1bc1cadddc750f9f12f1fee9edcaab406b955e60d758caa612338587a";
   };
 }

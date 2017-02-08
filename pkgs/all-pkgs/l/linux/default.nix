0a3f78d345ae5e6ec3147562a5ed04a92baa3a0d
William A. Kennington III <william@wkennington.com>
2017-02-07 17:02:23 -0800

    linux-bcache: 4.8.17-2017-01-17 -> 4.9.8-2017-02-07

--- pkgs/all-pkgs/l/linux/default.nix
+++ pkgs/all-pkgs/l/linux/default.nix
@@ -36,14 +36,14 @@ let
     };
     "bcache" =
       let
-        date = "2017-01-17";
+        date = "2017-02-07";
       in {
-        version = "4.8.17";
+        version = "4.9.8";
         patchUrls = [
           "https://github.com/wkennington/linux/releases/download/bcachefs-${version}-${date}/patch-bcachefs-${version}-${date}.xz"
         ];
         baseSha256 = "3e9150065f193d3d94bcf46a1fe9f033c7ef7122ab71d75a7fb5a2f0c9a7e11a";
-        patchSha256 = "ff90aaae3c294b9636fe6d18e52789cb83f32ff41d6eac8da8bbbc78c2d0aa84";
+        patchSha256 = "7bc5ca87f7c5b7f611ff277f8c279c6192e4614981628ff9c9b09653a80869d4";
         features.bcachefs = true;
       };
     "bcache-testing" =

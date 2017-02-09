be1dee6986317fce7b5e23df58af1fcdf0d4adf2
Cody W. Opel <codyopel@gmail.com>
2017-02-09 00:15:31 -0500

    ffmpeg(head): 2017.01.24 -> 2017.02.08

--- pkgs/all-pkgs/f/ffmpeg/sources.nix
+++ pkgs/all-pkgs/f/ffmpeg/sources.nix
@@ -11,8 +11,8 @@
     sha256 = "54ce502aca10b7e6059f19220ea2f68fa0c9c4c4d255ae13e615f08f0c94dcc5";
   };
   "9.9" = { # Git
-    version = "2017.01.24";
-    rev = "9e6a2427558a718be0c1fffacffd935f630a7a8d";
-    sha256 = "e8fefc8e4e067f19fcf59f1a17efb86070299f509947577aa7e1a20286400a91";
+    version = "2017.02.08";
+    rev = "53234b9ba5e6ebe23060c83d8fee2e03d9cb3e17";
+    sha256 = "c398936f4351917352a5c1e15a09eadf4f7615b02415e3dc00816d9909a4a7f7";
   };
 }

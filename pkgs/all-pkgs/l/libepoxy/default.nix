e17a5942fcb842b2b58b75fc3d0a75cec3f7e00b
Cody W. Opel <codyopel@gmail.com>
2017-02-12 14:32:58 -0500

    libepoxy: use release tarballs & refactor

--- pkgs/all-pkgs/l/libepoxy/default.nix
+++ pkgs/all-pkgs/l/libepoxy/default.nix
@@ -1,30 +1,41 @@
 { stdenv
-, autoreconfHook
-, fetchFromGitHub
+, fetchurl
 , lib
-, python
+, python3
 
 , mesa
 , xorg
 }:
 
 let
-  version = "1.4";
+  inherit (lib)
+    replaceChars;
+
+  channel = "1.4";
+  version = "${channel}.0";
+
+  versionFormatted =
+    # For initial minor releases drop the trailing zero
+    if replaceChars ["${channel}."] [""] version == "0" then
+      channel
+    else
+      version;
 in
 stdenv.mkDerivation rec {
   name = "libepoxy-${version}";
 
-  src = fetchFromGitHub {
-    version = 2;
-    owner = "anholt";
-    repo = "libepoxy";
-    rev = "v${version}";
-    sha256 = "5db036b788ac1920834fdc6dbee2fe5156d9992f5c7d869b63127a3307ef21c6";
+  src = fetchurl {
+    urls = [
+      ("https://github.com/anholt/libepoxy/releases/download/v${versionFormatted}/"
+        + "${name}.tar.xz")
+      "mirror://gnome/sources/libepoxy/${channel}/${name}.tar.xz"
+    ];
+    hashOutput = false;
+    sha256 = "25a906b14a921bc2b488cfeaa21a00486fe92630e4a9dd346e4ecabeae52ab41";
   };
 
   nativeBuildInputs = [
-    autoreconfHook
-    python
+    python3
     xorg.utilmacros
   ];
 
@@ -32,6 +43,22 @@ stdenv.mkDerivation rec {
     mesa
     xorg.libX11
   ];
+
+  configureFlags = [
+    "--enable-glx"
+  ];
+
+  passthru = {
+    srcVerification = fetchurl {
+      inherit (src)
+        outputHash
+        outputHashAlgo
+        urls;
+      sha256Url = "https://download.gnome.org/sources/libepoxy/${channel}/"
+        + "${name}.sha256sum";
+      failEarly = true;
+    };
+  };
 
   meta = with lib; {
     description = "A library for handling OpenGL function pointer management";

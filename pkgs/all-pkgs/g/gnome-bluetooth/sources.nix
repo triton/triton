1ec9da4af31aa98f80579fe7a4aef26d8007a3d0
Cody W. Opel <codyopel@gmail.com>
2017-02-13 23:56:26 -0500

    gnome-bluetooth: 3.20.0 -> 3.20.1

--- pkgs/all-pkgs/g/gnome-bluetooth/sources.nix
+++ pkgs/all-pkgs/g/gnome-bluetooth/sources.nix
@@ -1,7 +1,7 @@
 { }:
 {
   "3.20" = {
-    version = "3.20.0";
-    sha256 = "93b3ca16b348a168d044b3f777049b7dba2a9292c4adb2751a771e3bc5e4eb53";
+    version = "3.20.1";
+    sha256 = "ea9050b5f3b94ef279febae4cb789c2500a25344c784f650fe7f932fcd6798fe";
   };
 }

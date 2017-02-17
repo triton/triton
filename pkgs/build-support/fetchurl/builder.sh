7cf90307783b4c9127ee6eb9a4b3cfd8436021c9
William A. Kennington III <william@wkennington.com>
2017-02-16 16:34:01 -0800

    fetchurl: Try a new keyserver

--- pkgs/build-support/fetchurl/builder.sh
+++ pkgs/build-support/fetchurl/builder.sh
@@ -402,7 +402,7 @@ trap cleanup EXIT ERR INT QUIT PIPE TERM
 
 if [ "${#pgpKeyFingerprints[@]}" -gt "0" ]; then
   eval `dirmngr --daemon --homedir=$HOME --disable-http --disable-ldap`
-  gpg --verbose --recv-keys --keyserver "hkps://pgp.mit.edu" "${pgpKeyFingerprints[@]}"
+  gpg --verbose --recv-keys --keyserver "hkps://pgp.key-server.io" "${pgpKeyFingerprints[@]}"
 fi
 
 i=0

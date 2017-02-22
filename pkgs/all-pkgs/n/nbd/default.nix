085e187002a74ed4d6bbcaa7c593631a8c8a7fd7
William A. Kennington III <william@wkennington.com>
2017-02-22 13:38:23 -0800

    nbd: 3.15.1 -> 3.15.2

--- pkgs/all-pkgs/n/nbd/default.nix
+++ pkgs/all-pkgs/n/nbd/default.nix
@@ -5,14 +5,14 @@
 }:
 
 let
-  version = "3.15.1";
+  version = "3.15.2";
 in
 stdenv.mkDerivation rec {
   name = "nbd-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/nbd/nbd/${version}/${name}.tar.xz";
-    sha256 = "ac1108dfdaffe1cf01f5f0f34c738771184bf6ff4a503edefaf10a961f8b8745";
+    sha256 = "cf188ebdad3d317742b874fb8669faa437ee9fab4005e71b049bc301011af344";
   };
 
   buildInputs = [

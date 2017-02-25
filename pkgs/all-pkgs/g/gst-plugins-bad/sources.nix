7b0bedb1754540170f9b3b05e9e8796adf9d67ce
Cody W. Opel <codyopel@gmail.com>
2017-02-25 11:58:35 -0500

    gst-plugins-bad: 1.10.3 -> 1.10.4

--- pkgs/all-pkgs/g/gst-plugins-bad/sources.nix
+++ pkgs/all-pkgs/g/gst-plugins-bad/sources.nix
@@ -1,7 +1,7 @@
 { }:
 {
   "1.10" = {
-    version = "1.10.3";
-    sha256 = "24099351fb6984c9e7560de06e072ff2e33d0b2db38b8fcc7afefb536e5094e7";
+    version = "1.10.4";
+    sha256 = "23ddae506b3a223b94869a0d3eea3e9a12e847f94d2d0e0b97102ce13ecd6966";
   };
 }

5699502a74089d3c226878af97def05b4c89454a
Cody W. Opel <codyopel@gmail.com>
2017-02-25 12:06:47 -0500

    tracker: 1.10.4 -> 1.10.5

--- pkgs/all-pkgs/t/tracker/sources.nix
+++ pkgs/all-pkgs/t/tracker/sources.nix
@@ -1,7 +1,7 @@
 { }:
 {
   "1.10" = {
-    version = "1.10.4";
-    sha256 = "70e779e55bf580615a66db43ae45d081e1810cc11bbfcdd0a6339f506e0adf9d";
+    version = "1.10.5";
+    sha256 = "3b8488f0e372054790f2fd4207227c5b15425bf1ab829d76928ac0b32de9c5b7";
   };
 }

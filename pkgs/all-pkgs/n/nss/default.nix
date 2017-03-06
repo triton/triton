3f386805113b8017686bfb0e2a4711ee0b1a73fc
William A. Kennington III <william@wkennington.com>
2017-03-06 13:56:22 -0800

    nss: 3.29.2 -> 3.29.3

--- pkgs/all-pkgs/n/nss/default.nix
+++ pkgs/all-pkgs/n/nss/default.nix
@@ -9,7 +9,7 @@
 }:
 
 let
-  version = "3.29.2";
+  version = "3.29.3";
 
   baseUrl = "https://ftp.mozilla.org/pub/mozilla.org/security/nss/releases"
     + "/NSS_${stdenv.lib.replaceStrings ["."] ["_"] version}_RTM/src";
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "${baseUrl}/${name}.tar.gz";
     sha256Url = "${baseUrl}/SHA256SUMS";
-    sha256 = "bc44302b624faf0ac187ec4b8b4c003c0ec465aa846744a085e6ac5ff3012891";
+    sha256 = "35ddcc31251ef829994efeee925011aa1414e32be7e388236970255aa3c8e1eb";
   };
 
   buildInputs = [

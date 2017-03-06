eaf7288f8de838f99dad139a538b2e1b82cbd76a
Cody W. Opel <codyopel@gmail.com>
2017-03-05 23:08:59 -0500

    pango: 1.40.3 -> 1.40.4

--- pkgs/all-pkgs/p/pango/default.nix
+++ pkgs/all-pkgs/p/pango/default.nix
@@ -1,5 +1,6 @@
 { stdenv
 , fetchurl
+, lib
 
 , cairo
 , fontconfig
@@ -10,30 +11,29 @@
 , xorg
 }:
 
-let
-  inherit (stdenv.lib)
-    enFlag
-    optionals
-    optionalString
-    wtFlag;
-
-  versionMajor = "1.40";
-  versionMinor = "3";
-  version = "${versionMajor}.${versionMinor}";
-in
-
 assert xorg != null ->
   xorg.libX11 != null
   && xorg.libXft != null
   && xorg.libXrender != null;
 
+let
+  inherit (lib)
+    boolEn
+    boolWt
+    optionals
+    optionalString;
+
+  versionMajor = "1.40";
+  versionMinor = "4";
+  version = "${versionMajor}.${versionMinor}";
+in
 stdenv.mkDerivation rec {
   name = "pango-${version}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/pango/${versionMajor}/${name}.tar.xz";
-    sha256Url = "mirror://gnome/sources/pango/${versionMajor}/${name}.sha256sum";
-    sha256 = "abba8b5ce728520c3a0f1535eab19eac3c14aeef7faa5aded90017ceac2711d3";
+    hashOutput = false;
+    sha256 = "f8fdc5fc66356dc4edf915048cceeee065a0e0cb70b3b2598f62bda320129a3e";
   };
 
   buildInputs = [
@@ -49,37 +49,47 @@ stdenv.mkDerivation rec {
     xorg.libXrender
   ];
 
-  postPatch =
-    /* Test fails randomly */ optionalString doCheck ''
-      sed -i tests/Makefile.in \
-        -e 's,\(am__append_4 = testiter\) test-pangocairo-threads,\1,g'
-    '';
+  postPatch = /* Test fails randomly */ optionalString doCheck ''
+    sed -i tests/Makefile.in \
+      -e 's,\(am__append_4 = testiter\) test-pangocairo-threads,\1,g'
+  '';
 
   configureFlags = [
     "--enable-rebuilds"
-    (enFlag "introspection" (gobject-introspection != null) null)
+    "--${boolEn (gobject-introspection != null)}-introspection"
     "--disable-gtk-doc"
     "--disable-gtk-doc-html"
     "--disable-gtk-doc-pdf"
     "--disable-doc-cross-reference"
     "--enable-Bsymbolic"
     "--disable-installed-tests"
-    (wtFlag "xft" (xorg != null) null)
-    (wtFlag "cairo" (cairo != null) null)
+    "--${boolWt (xorg != null)}-xft"
+    "--${boolWt (cairo != null)}-cairo"
   ];
 
   # Does not respect --disable-gtk-doc
   postInstall = "rm -rvf $out/share/gtk-doc";
 
-  preCheck =
-    /* Fontconfig fails to load default config in test */
-    optionalString doCheck ''
-      export FONTCONFIG_FILE="${fontconfig}/etc/fonts/fonts.conf"
-    '';
+  preCheck = /* Fontconfig fails to load default config in test */
+      optionalString doCheck ''
+    export FONTCONFIG_FILE="${fontconfig}/etc/fonts/fonts.conf"
+  '';
 
   doCheck = true;
 
-  meta = with stdenv.lib; {
+  passthru = {
+    srcVerification = fetchurl {
+      inherit (src)
+        outputHash
+        outputHashAlgo
+        urls;
+      sha256Url = "https://download.gnome.org/sources/pango/${channel}/"
+        + "${name}.sha256sum";
+      failEarly = true;
+    };
+  };
+
+  meta = with lib; {
     description = "A library for laying out and rendering of text";
     homepage = http://www.pango.org/;
     license = licenses.lgpl2Plus;

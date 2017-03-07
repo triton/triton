fe092e5b8451a2dfe9c9d2738dc8148a2def5846
Cody W. Opel <codyopel@gmail.com>
2017-03-07 13:33:49 -0500

    libtorrent-rasterbar(1.1-head): 2017-02-28 -> 2017-03-06

--- pkgs/all-pkgs/l/libtorrent-rasterbar/sources.nix
+++ pkgs/all-pkgs/l/libtorrent-rasterbar/sources.nix
@@ -6,9 +6,9 @@
   };
   "1.1-head" = {
     fetchzipversion = 2;
-    version = "2017-02-28";
-    rev = "9a63d4696ebec5743418181ef3411371d8e31b04";
-    sha256 = "e808056038b10d1ed274354062c3d692efd0100f6ee20ddc0addb3703f6ad941";
+    version = "2017-03-06";
+    rev = "faa2029f8b1ff390b82437ff4aacbeaceeb1a69e";
+    sha256 = "14f9aba4603332052c4c78f8e5862a431d4d45633525837bdbbbfdcdd6600935";
   };
   "head" = {
     fetchzipversion = 2;

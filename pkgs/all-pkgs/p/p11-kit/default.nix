87361ec355224b2e9d5b107896a08a4b444103c8
William A. Kennington III <william@wkennington.com>
2017-03-08 14:42:48 -0800

    p11-kit: 0.23.2 -> 0.23.5

--- pkgs/all-pkgs/p/p11-kit/default.nix
+++ pkgs/all-pkgs/p/p11-kit/default.nix
@@ -5,13 +5,19 @@
 , libtasn1
 }:
 
+let
+  version = "0.23.5";
+
+  tarballUrls = version: [
+    "https://github.com/p11-glue/p11-kit/releases/download/${version}/p11-kit-${version}.tar.gz"
+  ];
+in
 stdenv.mkDerivation rec {
-  name = "p11-kit-0.23.2";
+  name = "p11-kit-${version}";
 
   src = fetchurl {
-    url = "https://p11-glue.freedesktop.org/releases/${name}.tar.gz";
-    multihash = "QmbAbYcMhxSwfMRnnBMdyVUoHu6aptC5pZdqdpuAqVRYjY";
-    sha256 = "1w7szm190phlkg7qx05ychlj2dbvkgkhx9gw6dx4d5rw62l6wwms";
+    urls = tarballUrls version;
+    sha256 = "0d8fed192563c324bb5ace3c068f06558a5569a6e8eb47eee1cd79ada3b1124f";
   };
 
   buildInputs = [
@@ -28,6 +34,20 @@ stdenv.mkDerivation rec {
   preInstall = ''
     installFlagsArray+=("exampledir=$out/etc/pkcs11")
   '';
+
+  passthru = {
+    srcVerification = fetchurl rec {
+      failEarly = true;
+      urls = tarballUrls "0.23.5";
+      pgpsigUrls = map (n: "${n}.sig") urls;
+      pgpKeyFingerprints = [
+        "AED6 E2A1 85EE B379 F174  76D2 E012 D07A D0E3 CC30"
+        "4622 25C3 B46F 3487 9FC8  496C D605 848E D7E6 9871"
+      ];
+      inherit (src) outputHashAlgo;
+      outputHash = "0d8fed192563c324bb5ace3c068f06558a5569a6e8eb47eee1cd79ada3b1124f";
+    };
+  };
 
   meta = with stdenv.lib; {
     homepage = https://p11-glue.freedesktop.org/;

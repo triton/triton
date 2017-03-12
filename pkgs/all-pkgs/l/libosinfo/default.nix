a35c6e69929bcfc3f901480da4ad62338f9d3725
Cody W. Opel <codyopel@gmail.com>
2017-03-12 04:29:48 -0400

    libosinfo: update upstream urls

--- pkgs/all-pkgs/l/libosinfo/default.nix
+++ pkgs/all-pkgs/l/libosinfo/default.nix
@@ -2,6 +2,7 @@
 , fetchurl
 , gettext
 , intltool
+, lib
 
 , check
 , glib
@@ -14,15 +15,14 @@
 }:
 
 let
-  inherit (stdenv.lib)
-    enFlag;
+  inherit (lib)
+    boolEn;
 in
-
 stdenv.mkDerivation rec {
   name = "libosinfo-1.0.0";
 
   src = fetchurl {
-    url = "https://fedorahosted.org/releases/l/i/libosinfo/${name}.tar.gz";
+    url = "https://releases.pagure.org/libosinfo/${name}.tar.gz";
     hashOutput = false;
     sha256 = "f7b425ecde5197d200820eb44401c5033771a5d114bd6390230de768aad0396b";
   };
@@ -51,8 +51,8 @@ stdenv.mkDerivation rec {
     "--disable-gtk-doc-html"
     "--disable-gtk-doc-pdf"
     "--disable-tests"
-    (enFlag "introspection" (gobject-introspection != null) null)
-    (enFlag "vala" (vala != null) null)
+    "--${boolEn (gobject-introspection != null)}-introspection"
+    "--${boolEn (vala != null)}-vala"
     "--disable-coverage"
     "--with-usb-ids-path=${hwdata}/data/hwdata/usb.ids"
     "--with-pci-ids-path=${hwdata}/data/hwdata/pci.ids"
@@ -67,9 +67,9 @@ stdenv.mkDerivation rec {
     };
   };
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "GObject library for managing information about real/virtual OSes";
-    homepage = http://libosinfo.org/;
+    homepage = https://libosinfo.org/;
     license = with licenses; [
       lgpl21
       gpl2

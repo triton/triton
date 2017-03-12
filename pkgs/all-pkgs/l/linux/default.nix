dba29ac5b872d4495898ade5c5efeb0155c9ddf3
Cody W. Opel <codyopel@gmail.com>
2017-03-12 03:30:54 -0400

    linux: 4.10.1 -> 4.10.2

--- pkgs/all-pkgs/l/linux/default.nix
+++ pkgs/all-pkgs/l/linux/default.nix
@@ -29,9 +29,9 @@ let
       patchSha256 = "87a0f07dd393e2d08850f0536417d091684535ff0c8ab8f8d9aeab1db38589bf";
     };
     "4.10" = {
-      version = "4.10.1";
+      version = "4.10.2";
       baseSha256 = "3c95d9f049bd085e5c346d2c77f063b8425f191460fcd3ae9fe7e94e0477dc4b";
-      patchSha256 = "da560125aa350f76f0e4a5b9373a0d0a1c27ccefe3b7bd9231724f3a3c4ebb9e";
+      patchSha256 = "3e2c2ba9dd2c421ea4f7e10150cc5f5fa5fdbaffef5377988fabb7d6f7d65bab";
     };
     "testing" = {
       version = "4.11-rc1";

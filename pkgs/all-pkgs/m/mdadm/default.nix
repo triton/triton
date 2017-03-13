82cee435df66b69a62787c95291b64b22315fafe
William A. Kennington III <william@wkennington.com>
2017-03-13 16:12:35 -0700

    mdadm: 3.3.7 -> 4.0

--- pkgs/all-pkgs/m/mdadm/default.nix
+++ pkgs/all-pkgs/m/mdadm/default.nix
@@ -3,12 +3,20 @@
 , groff
 }:
 
+let
+  tarballUrls = version: [
+    "mirror://kernel/linux/utils/raid/mdadm/mdadm-${version}.tar"
+  ];
+
+  version = "4.0";
+in
 stdenv.mkDerivation rec {
-  name = "mdadm-3.3.4";
+  name = "mdadm-${version}";
 
   src = fetchurl {
-    url = "mirror://kernel/linux/utils/raid/mdadm/${name}.tar.xz";
-    sha256 = "0s6a4bq7v7zxiqzv6wn06fv9f6g502dp047lj471jwxq0r9z9rca";
+    urls = map (n: "${n}.xz") (tarballUrls version);
+    hashOutput = false;
+    sha256 = "1d6ae7f24ced3a0fa7b5613b32f4a589bb4881e3946a5a2c3724056254ada3a9";
   };
 
   nativeBuildInputs = [
@@ -37,15 +45,25 @@ stdenv.mkDerivation rec {
   ];
 
   # Attempt removing if building with gcc5 when updating
-  NIX_CFLAGS_COMPILE = [
-    "-std=gnu89"
-  ];
+  #NIX_CFLAGS_COMPILE = [
+  #  "-std=gnu89"
+  #];
 
   # This is to avoid self-references, which causes the initrd to explode
   # in size and in turn prevents mdraid systems from booting.
   allowedReferences = [
     stdenv.cc.libc
   ];
+
+  passthru = {
+    srcVerification = fetchurl {
+      failEarly = true;
+      pgpDecompress = true;
+      pgpsigUrls = map (n: "${n}.sign") (tarballUrls version);
+      pgpKeyFingerprint = "6A86 B80E 1D22 F21D 0B26  BA75 397D 82E0 531A 9C91";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "Programs for managing RAID arrays under Linux";

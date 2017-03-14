188053bdad69dadef012c4fa4a7792c44250983f
Cody W. Opel <codyopel@gmail.com>
2017-03-13 20:18:21 -0400

    speedtest-cli: 1.0.0 -> 1.0.2

--- pkgs/all-pkgs/s/speedtest-cli/default.nix
+++ pkgs/all-pkgs/s/speedtest-cli/default.nix
@@ -4,7 +4,7 @@
 }:
 
 let
-  version = "1.0.0";
+  version = "1.0.2";
 in
 buildPythonPackage rec {
   name = "speedtest-cli-${version}";
@@ -12,7 +12,7 @@ buildPythonPackage rec {
   src = fetchPyPi {
     package = "speedtest-cli";
     inherit version;
-    sha256 = "6e825a6e1348cb4ae755cd28c9c640cae6fa8e8129419aea3693399b46386671";
+    sha256 = "60cd286e1a8244100f4e4faf8f79d534c7df508daaf9d77f11196878846f7bff";
   };
 
   meta = with stdenv.lib; {

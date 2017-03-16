633fcc87d7bb1b9f5b4f87a2e3178b726c6f13a1
Cody W. Opel <codyopel@gmail.com>
2017-03-15 20:26:29 -0400

    libpthread-stubs: 0.3 -> 0.4

--- pkgs/all-pkgs/l/libpthread-stubs/default.nix
+++ pkgs/all-pkgs/l/libpthread-stubs/default.nix
@@ -4,11 +4,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "libpthread-stubs-0.3";
+  name = "libpthread-stubs-0.4";
 
   src = fetchurl {
     url = "mirror://xorg/individual/xcb/${name}.tar.bz2";
-    sha256 = "35b6d54e3cc6f3ba28061da81af64b9a92b7b757319098172488a660e3d87299";
+    sha256 = "e4d05911a3165d3b18321cc067fdd2f023f06436e391c6a28dff618a78d2e733";
   };
 
   meta = with lib; {

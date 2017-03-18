92b520f49defe0b3d264567bc8af71f795d6a165
Cody W. Opel <codyopel@gmail.com>
2017-03-18 00:39:01 -0400

    rest: enFlag -> boolEn

--- pkgs/all-pkgs/r/rest/default.nix
+++ pkgs/all-pkgs/r/rest/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , fetchurl
 , intltool
+, lib
 
 , glib
 , gobject-introspection
@@ -9,16 +10,15 @@
 }:
 
 let
-  inherit (stdenv.lib)
-    enFlag
-    wtFlag;
-in
+  inherit (lib)
+    boolEn;
 
-stdenv.mkDerivation rec {
-  name = "rest-${version}";
   versionMajor = "0.8";
   versionMinor = "0";
   version = "${versionMajor}.${versionMinor}";
+in
+stdenv.mkDerivation rec {
+  name = "rest-${version}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/rest/${versionMajor}/${name}.tar.xz";
@@ -40,7 +40,7 @@ stdenv.mkDerivation rec {
     "--disable-gtk-doc"
     "--disable-gtk-doc-html"
     "--disable-gtk-doc-pdf"
-    (enFlag "introspection" (gobject-introspection != null) "yes")
+    "--${boolEn (gobject-introspection != null)}-introspection"
     "--disable-gcov"
     # gnome support only adds a dependency on obsolete libsoup-gnome
     "--without-gnome"
@@ -49,7 +49,7 @@ stdenv.mkDerivation rec {
 
   postInstall = "rm -rvf $out/share/gtk-doc";
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Helper library for RESTful services";
     homepage = https://wiki.gnome.org/Projects/Librest;
     license = licenses.lgpl21;

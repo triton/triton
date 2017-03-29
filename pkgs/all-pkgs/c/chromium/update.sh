418c01dd4cd46d2d1347af44aa6e4e71b1a1de24
Cody W. Opel <codyopel@gmail.com>
2017-03-29 09:34:02 -0400

    chromium: refactor update.sh to fallback to previous version if update fails

--- pkgs/all-pkgs/c/chromium/update.sh
+++ pkgs/all-pkgs/c/chromium/update.sh
@@ -34,8 +34,10 @@ in pkgs.buildEnv {
     coreutils
     curl
     gawk
+    gnused
     jq
     openssl
+    perl
   ];
 }"
 pushd "$TOP_LEVEL" >/dev/null
@@ -50,7 +52,9 @@ declare -r omahaproxy='https://omahaproxy.appspot.com'
 declare -r bucket_url='https://commondatastorage.googleapis.com/chromium-browser-official'
 declare -r deb_url='https://dl.google.com/linux/chrome/deb/pool/main/g'
 
-CHROMIUM_DIR="$(readlink -f "$(readlink -f "$(dirname "$(readlink -f "${0}")")")")"
+declare -r CHROMIUM_DIR="$(
+  readlink -f "$(readlink -f "$(dirname "$(readlink -f "${0}")")")"
+)"
 
 declare -ra CURL_ARGS=(
   '--continue-at -'
@@ -99,13 +103,37 @@ declare -A CHROME_EQUIVALENT_CHANNEL=(
 
 declare -a CHROMIUM_LATEST_VERSIONS
 
+# Convert sources.nix to json for parsing
+# FIXME: use nix to read attrs
+sources_to_json() {
+  local -r File="${1}"
+
+  if [ ! -f "${File}" ] ; then
+    echo "ERROR: sources_to_json requires a file: ${File}" >&2
+    return 1
+  fi
+
+  tail -n +3 "${File}" |
+    sed -e 's/ =/:/' \
+    -e 's/;/,/' \
+    -e '/sha256/ s/,//' \
+    -e 's/version/"version"/' \
+    -e 's/sha256/"sha256"/' \
+    -e 's/    },/    }/' \
+    -e '$s/,$//g' |
+    perl -00pe 's/,(?!.*,)//s'
+}
+
 version_latest() {
   local -r Channel="${1}"
 
   echo "${omahaproxy}/all.json" >&2
   curl ${CURL_ARGS[@]} "${omahaproxy}/all.json" -o "${TMPDIR}/all.json"
 
-  Version="$(jq  -r -c -M ".[]|select(.os|contains(\"linux\"))|.versions[]|select(.channel|contains(\"${Channel}\"))|.version" "${TMPDIR}/all.json")"
+  Version="$(
+    jq  -r -c -M ".[]|select(.os|contains(\"linux\"))|.versions[]|select(.channel|contains(\"${Channel}\"))|.version" \
+      "${TMPDIR}/all.json"
+  )"
 
   [ -n "${Version}" ]
 
@@ -178,11 +206,24 @@ hash_google_chrome() {
 sources_chromium() {
   local ChromiumHash
   local Version
+  local ChromiumPreviousJson
+
+  ChromiumPreviousJson="$(sources_to_json "${CHROMIUM_DIR}/sources.nix")"
 
   for i in "${CHROMIUM_CHANNELS[@]}" ; do
     Version="$(version_latest "${i}")"
-    ChromiumHash="$(hash_chromium 'sha256' "${Version}")"
-    echo "Chromium: channel=${i} version=${Version}" >&2
+    ChromiumHash="$(hash_chromium 'sha256' "${Version}")" || {
+      echo -e "\e[31mFalling back to previous channel version\e[0m" >&2
+      Version="$(
+        echo "${ChromiumPreviousJson}" | jq -r -c -M ".${i}.version"
+      )"
+      echo -e "\e[33m Version: ${Version}\e[0m" >&2
+      ChromiumHash="$(
+        echo "${ChromiumPreviousJson}" | jq -r -c -M ".${i}.sha256"
+      )"
+      echo -e "\e[33m ChromiumHash: ${ChromiumHash}\e[0m" >&2
+    }
+    echo -e "\e[33mChromium: channel=${i} version=${Version}\e[0m" >&2
     cat <<EOF
   "${i}" = {
     version = "${Version}";
@@ -195,14 +236,22 @@ EOF
 sources_google_chrome() {
   local -a ArchPlatforms
   local Channel
+  local GoogleChromeHash
+  local GoogleChromePreviousJson
   local Name
   local -a OsPlatforms
   local Version
+  local VersionChromium
+  local VersionLatest
 
   OsPlatforms=($(
     echo "${GOOGLECHROME_PLATFORMS}" |
       jq -r -c -M '.[].os'
   ))
+
+  GoogleChromePreviousJson="$(
+    sources_to_json "${CHROMIUM_DIR}/../../g/google-chrome/sources.nix"
+  )"
 
   for i in "${OsPlatforms[@]}" ; do
     ArchPlatforms=($(
@@ -224,13 +273,32 @@ sources_google_chrome() {
     fi
 
     echo "  \"${Channel}\" = {"
-    Version="$(version_latest "${i}")"
+    VersionChromium="$(
+      sources_to_json "${CHROMIUM_DIR}/sources.nix" |
+        jq -r -c -M ".${i}.version"
+    )"
+    VersionLatest="$(version_latest "${i}")"
+    if [ "${VersionLatest}" != "${VersionChromium}" ] ; then
+      Version="${VersionChromium}"
+    else
+      Version="${VersionLatest}"
+    fi
     # FIXME: translate chromium -> chrome channels
     for x in "${Platforms[@]}" ; do
-      echo "Google Chrome: channel=${Channel} version=${Version} arch=FIXME" >&2
+      echo -e "\e[33mGoogle Chrome: channel=${Channel} version=${Version} arch=FIXME\e[0m" >&2
+      if [ "${VersionLatest}" != "${VersionChromium}" ] ; then
+        GoogleChromeHash="$(
+          echo "${GoogleChromePreviousJson}" |
+            jq -r -c -M ".${Channel}.\"${x}\".sha256"
+        )"
+      else
+        GoogleChromeHash="$(
+          hash_google_chrome 'sha256' "${Channel}" "${Version}" 'amd64'
+        )"
+      fi
       cat <<EOF
     "${x}" = {
-      sha256 = "$(hash_google_chrome 'sha256' "${Channel}" "${Version}" 'amd64')";
+      sha256 = "${GoogleChromeHash}";
     };
 EOF
     done
@@ -241,6 +309,7 @@ EOF
 sources_chromium_generate() {
   local ChromiumSources
 
+  # sources_chromium must be called before sources.nix is overwritten
   ChromiumSources="$(sources_chromium)"
 
   cat > "${CHROMIUM_DIR}/sources.nix" <<EOF
@@ -254,11 +323,16 @@ EOF
 }
 
 sources_google_chrome_generate() {
+  local GoogleChromeSources
+
+  # sources_google_chrome must be called before sources.nix is overwritten
+  GoogleChromeSources="$(sources_google_chrome)"
+
   cat > "${CHROMIUM_DIR}/../../g/google-chrome/sources.nix" <<EOF
 # THIS FILE IS AUTOGENERATED, DO NOT EDIT
 { }:
 {
-$(sources_google_chrome)
+${GoogleChromeSources}
 }
 
 EOF

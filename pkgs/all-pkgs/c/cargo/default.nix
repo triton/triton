3435a0fd19ea1e8bd3d1ec900fb625f08f092acf
William A. Kennington III <william@wkennington.com>
2017-03-29 15:50:36 -0700

    cargo: Use the real rustc

--- pkgs/all-pkgs/c/cargo/default.nix
+++ pkgs/all-pkgs/c/cargo/default.nix
@@ -2,6 +2,7 @@
 , buildRustPackage
 , cargo_bootstrap
 , fetchCargo
+, rustc
 }:
 
 let
@@ -15,6 +16,10 @@ buildRustPackage {
     inherit version;
     sha256 = "02cc30ce96b2a4d6903f47bcc8a06e51daa90552bf570d3af94f798e8f2cc84f";
   };
+
+  nativeBuildInputs = [
+    rustc
+  ];
 
   passthru = {
     bootstrap = cargo_bootstrap;

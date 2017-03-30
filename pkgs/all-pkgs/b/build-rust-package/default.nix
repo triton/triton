8119bc1610ed4d821c1dd6c1594505999838948c
William A. Kennington III <william@wkennington.com>
2017-03-29 17:10:44 -0700

    build-rust-package: Attempt to fix building

--- pkgs/all-pkgs/b/build-rust-package/default.nix
+++ pkgs/all-pkgs/b/build-rust-package/default.nix
@@ -19,19 +19,26 @@ stdenv.mkDerivation ({
     cargo
   ] ++ (args.nativeBuildInputs or [ ]);
 
-  unpackPhase = ''
+  preUnpack = ''
     export CARGO_HOME="$TMPDIR/cargo"
     export CARGO_TARGET_DIR="$TMPDIR/build"
     mkdir -p "$CARGO_HOME" "$CARGO_TARGET_DIR"
   '';
 
   configurePhase = ''
-    true
+    mv registry "$CARGO_HOME"
+    sed -i "s,@CARGO_HOME,$CARGO_HOME,g" Cargo.lock
+
+    # Configure cargo to use the local registry and predefined user settings
+    sed ${../../f/fetch-cargo/config.in} \
+      -e "s,@registry@,$(echo "$CARGO_HOME/registry/crates.io"*),g" \
+      -e "s,@cores@,$NIX_BUILD_CORES,g" \
+      > "$CARGO_HOME/config"
   '';
 
   buildPhase = ''
     runHook preBuild
-    cargo build -j $NIX_BUILD_CORES --release --verbose
+    cargo build -j $NIX_BUILD_CORES --release --frozen --all-features --verbose
     runHook postBuild
   '';
 

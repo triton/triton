8e40e76916eb2163df40c62afdac980bd7060506
William A. Kennington III <william@wkennington.com>
2017-04-01 15:32:11 -0700

    nano: 2.7.5 -> 2.8.0

--- pkgs/all-pkgs/n/nano/default.nix
+++ pkgs/all-pkgs/n/nano/default.nix
@@ -6,8 +6,8 @@
 }:
 
 let
-  major = "2.7";
-  patch = "5";
+  major = "2.8";
+  patch = "0";
 
   version = "${major}.${patch}";
 in
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
       "mirror://gnu/nano/${name}.tar.xz"
     ];
     hashOutput = false;
-    sha256 = "a64d24e6bc4fc448376d038f9a755af77f8e748c9051b6f45bf85e783a7e67e4";
+    sha256 = "15c1bcf4d8888d3b56f68a0b75871cc349b81a9094f8a42d73010ffc26c85dc2";
   };
 
   nativeBuildInputs = [

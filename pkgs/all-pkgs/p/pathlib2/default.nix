3cafeba94d0408934c831eebf52ecb68c24bf2db
Cody W. Opel <codyopel@gmail.com>
2017-04-02 23:08:16 -0400

    pathlib2: 2.1.0 -> 2.2.1

--- pkgs/all-pkgs/p/pathlib2/default.nix
+++ pkgs/all-pkgs/p/pathlib2/default.nix
@@ -1,12 +1,18 @@
 { stdenv
 , buildPythonPackage
 , fetchPyPi
+, isPy3k
+, lib
 
+, scandir
 , six
 }:
 
 let
-  version = "2.1.0";
+  inherit (lib)
+    optionals;
+
+  version = "2.2.1";
 in
 buildPythonPackage {
   name = "pathlib2-${version}";
@@ -14,14 +20,20 @@ buildPythonPackage {
   src = fetchPyPi {
     package = "pathlib2";
     inherit version;
-    sha256 = "deb3a960c1d55868dfbcac98432358b92ba89d95029cddd4040db1f27405055c";
+    sha256 = "ce9007df617ef6b7bd8a31cd2089ed0c1fed1f7c23cf2bf1ba140b3dd563175d";
   };
 
   propagatedBuildInputs = [
+    scandir
     six
+  ] ++ optionals (!isPy3k) [
+    scandir
   ];
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
+    description = "Object-oriented filesystem paths";
+    homepage = https://pypi.python.org/pypi/pathlib2/;
+    license = licenses.mit;
     maintainers = with maintainers; [
       wkennington
     ];

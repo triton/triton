0691ce41485f77166d2eb0a8dd3a9bbff38f228f
William A. Kennington III <william@wkennington.com>
2017-04-05 11:42:35 -0700

    dtc: 1.4.1 -> 1.4.4

--- pkgs/all-pkgs/d/dtc/default.nix
+++ pkgs/all-pkgs/d/dtc/default.nix
@@ -1,18 +1,20 @@
 { stdenv
 , bison
-, fetchgit
+, fetchzip
 , flex
 }:
 
+let
+  version = "1.4.4";
+in
 stdenv.mkDerivation rec {
   name = "dtc-${version}";
-  version = "1.4.1";
 
-  src = fetchgit {
-    version = 1;
-    url = "git://git.kernel.org/pub/scm/utils/dtc/dtc.git";
-    rev = "refs/tags/v${version}";
-    sha256 = "0nkh1rwfsm7yvk5pqlis00a7sxag8y29r3zg1w87hkw7a5pv97gq";
+  src = fetchzip {
+    version = 2;
+    url = "https://git.kernel.org/pub/scm/utils/dtc/dtc.git/snapshot/v${version}.tar.xz";
+    multihash = "Qmc7rFTTDpbv62vAZk5eroWu7koMJpuiMGfDbtG43BZheb";
+    sha256 = "e2bc26528eb95e728c6b67d7f69d05eae468587498f6e18f5e731914ef0c3ebc";
   };
 
   nativeBuildInputs = [
@@ -20,16 +22,21 @@ stdenv.mkDerivation rec {
     bison
   ];
 
-  installFlags = [
-    "INSTALL=install"
-    "PREFIX=$(out)"
-  ];
+  preInstall = ''
+    installFlagsArray+=(
+      "INSTALL=install"
+      "PREFIX=$out"
+    )
+  '';
 
   meta = with stdenv.lib; {
     description = "Device Tree Compiler";
     homepage = https://git.kernel.org/cgit/utils/dtc/dtc.git;
     license = licenses.gpl2; # dtc itself is GPLv2, libfdt is dual GPL/BSD
-    maintainers = with maintainers; [ ];
-    platforms = platforms.all;
+    maintainers = with maintainers; [
+      wkennington
+    ];
+    platforms = with platforms;
+      x86_64-linux;
   };
 }

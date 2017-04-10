7de84c8180f6378ac6ef05d2a4e72ab49c276901
Cody W. Opel <codyopel@gmail.com>
2017-04-09 22:27:17 -0400

    mg: 2016-10-05 -> 2017-04-01

--- pkgs/all-pkgs/m/mg/default.nix
+++ pkgs/all-pkgs/m/mg/default.nix
@@ -1,12 +1,13 @@
 { stdenv
 , fetchurl
+, lib
 
 , libbsd
 , ncurses
 }:
 
 let
-  version = "2016-10-05";
+  version = "2017-04-01";
   rev = stdenv.lib.replaceStrings ["-"] [""] version;
 in
 stdenv.mkDerivation rec {
@@ -14,8 +15,8 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://homepage.boetes.org/software/mg/mg-${rev}.tar.gz";
-    multihash = "QmZGHhLQyBojc3BifQRjtoJ4r8dMyCm6Wtv7Fr6Ny1YnZU";
-    sha256 = "b7fcb5136a6783ca24c8463ab0852fc1f26bdb2bb1c24759b2c51ccfc46c5e61";
+    multihash = "QmNbDUe1yzSsKhxt11xFZDh97hfevWvG9111V5iAXYkdnj";
+    sha256 = "0a3608b17c153960cb1d954ca3b62445a77c0c1a18aa5c8c58aba9f6b8d62aab";
   };
 
   buildInputs = [
@@ -40,10 +41,13 @@ stdenv.mkDerivation rec {
     "prefix=$(out)"
   ];
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Micro GNU/emacs, an EMACS style editor";
     homepage = http://homepage.boetes.org/software/mg/;
     license = licenses.publicDomain;
+    maintainers = with maintainers; [
+      codyopel
+    ];
     platforms = with platforms;
       x86_64-linux;
   };

1ecf9810465b29ab232939934cc4ba15541f7347
Cody W. Opel <codyopel@gmail.com>
2017-04-09 23:05:31 -0400

    pytz: 2016.10 -> 2017.2

--- pkgs/all-pkgs/p/pytz/default.nix
+++ pkgs/all-pkgs/p/pytz/default.nix
@@ -4,7 +4,7 @@
 }:
 
 let
-  version = "2016.10";
+  version = "2017.2";
 in
 buildPythonPackage rec {
   name = "pytz-${version}";
@@ -12,7 +12,8 @@ buildPythonPackage rec {
   src = fetchPyPi {
     package = "pytz";
     inherit version;
-    sha256 = "9a43e20aa537cfad8fe7a1715165c91cb4a6935d40947f2d070e4c80f2dcd22b";
+    type = ".zip";
+    sha256 = "f5c056e8f62d45ba8215e5cb8f50dfccb198b4b9fbea8500674f3443e4689589";
   };
 
   meta = with stdenv.lib; {

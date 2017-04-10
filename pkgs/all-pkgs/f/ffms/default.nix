ae83aa7e0c8db585adf29c45e28081cfc4716741
Cody W. Opel <codyopel@gmail.com>
2017-04-10 00:07:52 -0400

    ffms: 2.22 -> 2.23

--- pkgs/all-pkgs/f/ffms/default.nix
+++ pkgs/all-pkgs/f/ffms/default.nix
@@ -1,13 +1,14 @@
 { stdenv
 , autoreconfHook
 , fetchzip
+, lib
 
 , ffmpeg
 , zlib
 }:
 
 let
-  version = "2.22";
+  version = "2.23";
 in
 stdenv.mkDerivation rec {
   name = "ffms-${version}";
@@ -15,7 +16,7 @@ stdenv.mkDerivation rec {
   src = fetchzip {
     version = 2;
     url = "https://github.com/FFMS/ffms2/archive/${version}.tar.gz";
-    sha256 = "42d9eeba82bd4741822553d8448399d6e5c32d21d26c22ff620bfb67830c24b4";
+    sha256 = "8a40f22a28e15c974906a222b44b6df29d9c50d34f41ded1ab1ad6edd6711a8d";
   };
 
   nativeBuildInputs = [
@@ -32,7 +33,7 @@ stdenv.mkDerivation rec {
     "--with-zlib=${zlib}"
   ];
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "FFmpeg based library and Avisynth plugin";
     homepage = https://github.com/FFMS/ffms2;
     license = licenses.mit;

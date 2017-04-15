b57616a09a472f3fc85e3f9c606ca80ebb1f70c4
William A. Kennington III <william@wkennington.com>
2017-04-15 13:51:00 -0700

    bcachefs-tools: 2017-04-10 -> 2017-04-15

--- pkgs/all-pkgs/b/bcachefs-tools/default.nix
+++ pkgs/all-pkgs/b/bcachefs-tools/default.nix
@@ -11,14 +11,18 @@
 , zlib
 }:
 
+let
+  date = "2017-04-15";
+  rev = "1b495cf9e1c75d19cb1bff9b0b13d03c9a62153c";
+in
 stdenv.mkDerivation {
-  name = "bcache-tools-2017-04-10";
+  name = "bcache-tools-2017-04-15";
 
   src = fetchzip {
     version = 2;
-    url = "https://evilpiepirate.org/git/bcachefs-tools.git/snapshot/bcachefs-tools-03bc9d71b13e6f8e879894f93ea16f1f4a8280c9.tar.xz";
-    multihash = "QmXtdmAb8k6QUpAx1keGHPqLMSYMWDnoTKEHK1ZTYQjh38";
-    sha256 = "509fc6b9d9621d9e19b68bece86533b2719c1e51dd6997baf9c2524b74529324";
+    url = "https://evilpiepirate.org/git/bcachefs-tools.git/snapshot/bcachefs-tools-${rev}.tar.xz";
+    multihash = "QmZoMB9nzB9QgjrX8o2NDBaxBE9jMPzWc4vXUZciMjAjRr";
+    sha256 = "8337500fe2f602af30231b6b009f8aeacfeec074fa4e2fbf37ef2e1603e70cf8";
   };
 
   buildInputs = [

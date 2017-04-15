52d9ea2663d1a73b7325e7c04469b09b87e98c19
William A. Kennington III <william@wkennington.com>
2017-04-15 14:00:30 -0700

    bc: 1.07 -> 1.07.1

--- pkgs/all-pkgs/b/bc/default.nix
+++ pkgs/all-pkgs/b/bc/default.nix
@@ -1,6 +1,5 @@
 { stdenv
 , ed
-, fetchTritonPatch
 , fetchurl
 , flex
 
@@ -9,12 +8,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "bc-1.07";
+  name = "bc-1.07.1";
 
   src = fetchurl {
     url = "mirror://gnu/bc/${name}.tar.gz";
     hashOutput = false;
-    sha256 = "55cf1fc33a728d7c3d386cc7b0cb556eb5bacf8e0cb5a3fcca7f109fc61205ad";
+    sha256 = "62adfca89b0a1c0164c2cdca59ca210c1d44c3ffc46daf9931cf4942664cb02a";
   };
 
   nativeBuildInputs = [
@@ -26,14 +25,6 @@ stdenv.mkDerivation rec {
     flex
     ncurses
     readline
-  ];
-
-  patches = [
-    (fetchTritonPatch {
-      rev = "f7da28f32f73a7a392b4f4bd0fbeb90e17fdbad0";
-      file = "b/bc/fix-hang.patch";
-      sha256 = "64719ac2d2a1622c241f5647973122d3ca742a28649875cd29ee957bf383dc38";
-    })
   ];
 
   configureFlags = [

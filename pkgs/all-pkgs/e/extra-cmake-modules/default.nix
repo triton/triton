cd3165f32d9b6cb73b2b632a2aa7c70e34997921
William A. Kennington III <william@wkennington.com>
2017-04-17 13:57:01 -0700

    extra-cmake-modules: 5.26.0 -> 5.33.0

--- pkgs/all-pkgs/e/extra-cmake-modules/default.nix
+++ pkgs/all-pkgs/e/extra-cmake-modules/default.nix
@@ -5,7 +5,7 @@
 }:
 
 let
-  majorVersion = "5.26";
+  majorVersion = "5.33";
   patchVersion = "0";
   version = "${majorVersion}.${patchVersion}";
 in
@@ -15,13 +15,22 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "mirror://kde/stable/frameworks/${majorVersion}/${name}.tar.xz";
     hashOutput = false;
-    sha256 = "cd529cc10cc4a4fc20a962329ffc8cc93cc200b7dc681aa4ddfc9e9cc88f79ec";
+    sha256 = "f3ff5e36c45ff579a742de700680678211cc90d8132af18f3a1c68f4f36b6a04";
   };
 
   nativeBuildInputs = [
     cmake
     ninja
   ];
+
+  passthru = {
+    srcVerification = fetchurl {
+      failEarly = true;
+      pgpsigUrls = map (n: "${n}.sig") src.urls;
+      pgpKeyFingerprint = "53E6 B47B 45CE A3E0 D5B7  4577 58D0 EE64 8A48 B3BB";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
 
   meta = with stdenv.lib; {
     maintainers = with maintainers; [

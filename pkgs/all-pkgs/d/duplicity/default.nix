d6085738fc6f7b03e1370de8af1b143efff6ae74
William A. Kennington III <william@wkennington.com>
2017-04-19 00:51:18 -0700

    duplicity: 0.7.10 -> 0.7.12

--- pkgs/all-pkgs/d/duplicity/default.nix
+++ pkgs/all-pkgs/d/duplicity/default.nix
@@ -7,7 +7,7 @@
 }:
 
 let
-  version = "0.7.10";
+  version = "0.7.12";
 in
 buildPythonPackage rec {
   name = "duplicity-${version}";
@@ -15,7 +15,8 @@ buildPythonPackage rec {
   src = fetchurl {
     url = "https://code.launchpad.net/duplicity/0.7-series/${version}/"
       + "+download/${name}.tar.gz";
-    sha256 = "485fef15526d163c061e00ec08de216cf7d652e67d41de5dc3bed9fb42214842";
+    multihash = "QmYoNVYBk3TK3cwfGwBRjwmjyGA2aR2aj1HzTGF56Epeq8";
+    sha256 = "11cbad44a90891af1bf9e294260ba7c21a1660ccd3ab2c6e736ba74ac5cf0fe6";
   };
 
   buildInputs = [

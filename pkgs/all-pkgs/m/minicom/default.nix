7e7728dbfdde0d36c29f546e4485652807aa2dff
William A. Kennington III <william@wkennington.com>
2017-04-20 01:37:39 -0700

    minicom: 2.7 -> 2.7.1

--- pkgs/all-pkgs/m/minicom/default.nix
+++ pkgs/all-pkgs/m/minicom/default.nix
@@ -4,13 +4,17 @@
 , ncurses
 }:
 
-stdenv.mkDerivation {
-  name = "minicom-2.7";
+let
+  version = "2.7.1";
+  id = "4215";
+in
+stdenv.mkDerivation rec {
+  name = "minicom-${version}";
 
   src = fetchurl {
-    url = "https://alioth.debian.org/frs/download.php/file/3977/minicom-2.7.tar.gz";
-    multihash = "QmVc4tikrQXUJZD9TCBzvRus6dCYa8DfkrsbemJCsjyiLa";
-    sha256 = "9ac3a663b82f4f5df64114b4792b9926b536c85f59de0f2d2b321c7626a904f4";
+    url = "https://alioth.debian.org/frs/download.php/file/${id}/${name}.tar.gz";
+    multihash = "QmdGxYm6sKNTjHkGW3aN8WWfJFNSuM5mH99RC6sYdHnPgR";
+    sha256 = "532f836b7a677eb0cb1dca8d70302b73729c3d30df26d58368d712e5cca041f1";
   };
 
   buildInputs = [

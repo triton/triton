73179e02a1b4530bf4cda9c9e22bea8f50e49314
William A. Kennington III <william@wkennington.com>
2017-04-22 14:29:51 -0700

    opus-tools: 0.1.9 -> 0.1.10

--- pkgs/all-pkgs/o/opus-tools/default.nix
+++ pkgs/all-pkgs/o/opus-tools/default.nix
@@ -17,11 +17,13 @@ let
 in
 
 stdenv.mkDerivation rec {
-  name = "opus-tools-0.1.9";
+  name = "opus-tools-0.1.10";
+
   src = fetchurl {
     url = "http://downloads.xiph.org/releases/opus/${name}.tar.gz";
-    sha256Url = "http://downloads.xiph.org/releases/opus/SHA256SUMS.txt";
-    sha256 = "0fk4nknvl111k89j5yckmyrh6b2wvgyhrqfncp7rig3zikbkv1xi";
+    multihash = "QmeBwLLdV5fnjXKQFpngiBPPJrA3b2g68jLYFJGiCY3JKS";
+    hashOutput = false;
+    sha256 = "a2357532d19471b70666e0e0ec17d514246d8b3cb2eb168f68bb0f6fd372b28c";
   };
 
   buildInputs = [
@@ -40,6 +42,14 @@ stdenv.mkDerivation rec {
     "--enable-pie"
     (wtFlag "flac" (flac != null) null)
   ];
+
+  passthru = {
+    srcVerification = fetchurl {
+      failEarly = true;
+      sha256Url = "https://archive.mozilla.org/pub/opus/SHA256SUMS.txt";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "Tools to work with opus encoded audio streams";

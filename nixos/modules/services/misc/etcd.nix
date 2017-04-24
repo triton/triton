7e2c3b22ace90449c72cdb74bc256f7a7f6c34a6
William A. Kennington III <william@wkennington.com>
2017-04-24 02:34:28 -0700

    nixos/etcd: Cleanup

--- nixos/modules/services/misc/etcd.nix
+++ nixos/modules/services/misc/etcd.nix
@@ -5,9 +5,34 @@ with lib;
 let
   cfg = config.services.etcd;
 
-in {
+  configFile = pkgs.writeText "etcd.conf.yaml" (''
+    name: '${cfg.name}'
+    data-dir: '${cfg.dataDir}'
+  '' + optionalString (cfg.initialAdvertisePeerUrls != null) ''
+    initial-advertise-peer-urls: ${concatStringsSep "," cfg.initialAdvertisePeerUrls}
+  '' + optionalString (cfg.advertiseClientUrls != null) ''
+    advertise-client-urls: ${concatStringsSep "," cfg.advertiseClientUrls}
+  '' + optionalString (cfg.listenClientUrls != null) ''
+    listen-client-urls: ${concatStringsSep "," cfg.listenClientUrls}
+  '' + optionalString (cfg.listenPeerUrls != null) ''
+    listen-peer-urls: ${concatStringsSep "," cfg.listenPeerUrls}
+  '' + optionalString (cfg.initialCluster != null) ''
+    initial-cluster: ${concatStringsSep "," (mapAttrsToList (n: u: "${n}=${u}") cfg.initialCluster)}
+  '' + optionalString (cfg.initialClusterState != null) ''
+    initial-cluster-state: ${cfg.initialClusterState}
+  '' + optionalString (cfg.initialClusterToken != null) ''
+    initial-cluster-token: ${cfg.initialClusterToken}
+  '' + ''
+    strict-reconfig-check: true
+    enable-v2: false
+    client-transport-security:
+    peer-transport-security:
+  '');
+in
+{
 
   options.services.etcd = {
+
     enable = mkOption {
       description = "Whether to enable etcd.";
       default = false;
@@ -20,46 +45,52 @@ in {
       type = types.str;
     };
 
+    dataDir = mkOption {
+      type = types.path;
+      default = "/var/lib/etcd";
+      description = "Etcd data directory.";
+    };
+
     advertiseClientUrls = mkOption {
       description = "Etcd list of this member's client URLs to advertise to the rest of the cluster.";
       default = cfg.listenClientUrls;
-      type = types.listOf types.str;
+      type = types.nullOr (types.listOf types.str);
     };
 
     listenClientUrls = mkOption {
       description = "Etcd list of URLs to listen on for client traffic.";
-      default = ["http://localhost:4001"];
-      type = types.listOf types.str;
+      default = null;
+      type = types.nullOr (types.listOf types.str);
     };
 
     listenPeerUrls = mkOption {
       description = "Etcd list of URLs to listen on for peer traffic.";
-      default = ["http://localhost:7001"];
-      type = types.listOf types.str;
+      default = null;
+      type = types.nullOr (types.listOf types.str);
     };
 
     initialAdvertisePeerUrls = mkOption {
       description = "Etcd list of this member's peer URLs to advertise to rest of the cluster.";
       default = cfg.listenPeerUrls;
-      type = types.listOf types.str;
+      type = types.nullOr (types.listOf types.str);
     };
 
     initialCluster = mkOption {
       description = "Etcd initial cluster configuration for bootstrapping.";
-      default = ["${cfg.name}=http://localhost:7001"];
-      type = types.listOf types.str;
+      default = null;
+      type = types.nullOr (types.attrsOf types.str);
     };
 
     initialClusterState = mkOption {
       description = "Etcd initial cluster configuration for bootstrapping.";
-      default = "new";
-      type = types.enum ["new" "existing"];
+      default = null;
+      type = types.nullOr (types.enum ["new" "existing"]);
     };
 
     initialClusterToken = mkOption {
       description = "Etcd initial cluster token for etcd cluster during bootstrap.";
-      default = "etcd-cluster";
-      type = types.str;
+      default = null;
+      type = types.nullOr types.str;
     };
 
     discovery = mkOption {
@@ -67,64 +98,31 @@ in {
       default = "";
       type = types.str;
     };
-
-    extraConf = mkOption {
-      description = ''
-        Etcd extra configuration. See
-        <link xlink:href='https://github.com/coreos/etcd/blob/master/Documentation/configuration.md#environment-variables' />
-      '';
-      type = types.attrsOf types.str;
-      default = {};
-      example = literalExample ''
-        {
-          "CORS" = "*";
-          "NAME" = "default-name";
-          "MAX_RESULT_BUFFER" = "1024";
-          "MAX_CLUSTER_SIZE" = "9";
-          "MAX_RETRY_ATTEMPTS" = "3";
-        }
-      '';
-    };
-
-    dataDir = mkOption {
-      type = types.path;
-      default = "/var/lib/etcd";
-      description = "Etcd data directory.";
-    };
   };
 
   config = mkIf cfg.enable {
+
     systemd.services.etcd = {
       description = "Etcd Daemon";
       wantedBy = [ "multi-user.target" ];
-      after = [ "network-interfaces.target" ];
-
-      environment = {
-        ETCD_NAME = cfg.name;
-        ETCD_DISCOVERY = cfg.discovery;
-        ETCD_DATA_DIR = cfg.dataDir;
-        ETCD_ADVERTISE_CLIENT_URLS = concatStringsSep "," cfg.advertiseClientUrls;
-        ETCD_LISTEN_CLIENT_URLS = concatStringsSep "," cfg.listenClientUrls;
-        ETCD_LISTEN_PEER_URLS = concatStringsSep "," cfg.listenPeerUrls;
-        ETCD_INITIAL_ADVERTISE_PEER_URLS = concatStringsSep "," cfg.initialAdvertisePeerUrls;
-      } // (optionalAttrs (cfg.discovery == ""){
-        ETCD_INITIAL_CLUSTER = concatStringsSep "," cfg.initialCluster;
-        ETCD_INITIAL_CLUSTER_STATE = cfg.initialClusterState;
-        ETCD_INITIAL_CLUSTER_TOKEN = cfg.initialClusterToken;
-      }) // (mapAttrs' (n: v: nameValuePair "ETCD_${n}" v) cfg.extraConf);
+      after = [ "network.target" ];
 
       serviceConfig = {
-        ExecStart = "${pkgs.etcd}/bin/etcd";
+        Type = "notify";
+        ExecStart = "@${pkgs.etcd}/bin/etcd etcd --config-file ${configFile}";
         User = "etcd";
         PermissionsStartOnly = true;
       };
+
       preStart = ''
         mkdir -m 0700 -p ${cfg.dataDir}
-        if [ "$(id -u)" = 0 ]; then chown etcd ${cfg.dataDir}; fi
+        chown etcd ${cfg.dataDir};
       '';
     };
 
-    environment.systemPackages = [ pkgs.etcdctl ];
+    environment.systemPackages = [
+      pkgs.etcd
+    ];
 
     users.extraUsers = singleton {
       name = "etcd";
@@ -132,5 +130,6 @@ in {
       description = "Etcd daemon user";
       home = cfg.dataDir;
     };
+
   };
 }

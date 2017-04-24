3568aa59e300ace99425f461767f5188d0c3ab21
William A. Kennington III <william@wkennington.com>
2017-04-24 00:13:02 -0700

    udisks: 2.1.8 -> 2.6.4

--- pkgs/all-pkgs/u/udisks/default.nix
+++ pkgs/all-pkgs/u/udisks/default.nix
@@ -6,28 +6,29 @@
 , libxslt
 
 , acl
-, systemd_lib
 , glib
 , gobject-introspection
 , gnused
 , libatasmart
-, polkit
-, util-linux_full
+, libblockdev
+, libconfig
 , libgudev
+, libstoragemgmt
+, lvm2
+, mdadm
+, polkit
+, systemd_lib
+, util-linux_full
 }:
 
 stdenv.mkDerivation rec {
-  name = "udisks-2.1.8";
+  name = "udisks-2.6.4";
 
   src = fetchurl {
-    url = "https://udisks.freedesktop.org/releases/${name}.tar.bz2";
-    multihash = "QmSknjMv6ad2CqSDwtVJQ5JQMCVWzo536T7dyVzKK6C1qz";
-    hashOutput = false;
-    sha256 = "da416914812a77e5f4d82b81deb8c25799fd3228d27d52f7bf89a501b1857dda";
+    url = "https://github.com/storaged-project/udisks/releases/download/${name}/${name}.tar.bz2";
+    sha256 = "d65389a861a9a475d3c8e0ad2b8603d9961977f1a00024093b5bacd711e774f2";
   };
 
-  # FIXME remove /var/run/current-system/sw/* references
-  # FIXME add references to parted, cryptsetup, etc (see the sources)
   postPatch = ''
     # We need to fix the default path inside of udisks
     grep -q '"/usr/bin:/bin:/usr/sbin:/sbin"' src/main.c
@@ -35,11 +36,16 @@ stdenv.mkDerivation rec {
 
     # We need to fix the udev rules
     grep -q '/bin/sh' data/80-udisks2.rules
-    grep -q 'sed' data/80-udisks2.rules
+    grep -q '/bin/sed' data/80-udisks2.rules
+    grep -q '/sbin/mdadm' data/80-udisks2.rules
     sed \
       -e 's,/bin/sh,${stdenv.shell},g' \
-      -e 's,sed,${gnused}/bin/sed,g' \
+      -e 's,/bin/sed,${gnused}/bin/sed,g' \
+      -e 's,/sbin/mdadm,${mdadm}/bin/mdadm,g' \
       -i data/80-udisks2.rules
+
+    # We need to fix uses of BUILD_DIR
+    find . -name \*.c -exec sed -i 's,BUILD_DIR,"/no-such-path",g' {} \;
   '';
 
   nativeBuildInputs = [
@@ -52,9 +58,12 @@ stdenv.mkDerivation rec {
   buildInputs = [
     acl
     glib
-    gobject-introspection
     libatasmart
+    libblockdev
+    libconfig
     libgudev
+    libstoragemgmt
+    lvm2
     polkit
     systemd_lib
   ];
@@ -69,26 +78,25 @@ stdenv.mkDerivation rec {
   configureFlags = [
     "--sysconfdir=/etc"
     "--localstatedir=/var"
+    "--disable-gtk-doc"
+    "--enable-man"
+    "--enable-lvm2"
+    "--enable-lvmcache"
+    #"--enable-iscsi"  # TODO: Enable
+    "--enable-btrfs"
+    "--enable-zram"
+    "--enable-lsm"
+    "--enable-bcache"
+    "--with-modloaddir=/etc/modules-load.d"
+    "--with-modprobedir=/etc/modprobe.d"
   ];
 
-  preBuild = ''
-    INTROSPECTION_TYPELIBDIR="$(grep -r 'INTROSPECTION_TYPELIBDIR = ' udisks/Makefile | awk -F'= ' '{print $2}')"
-    INTROSPECTION_GIRDIR="$(grep -r 'INTROSPECTION_GIRDIR = ' udisks/Makefile | awk -F'= ' '{print $2}')"
+  preInstall = ''
     installFlagsArray+=(
       "sysconfdir=$out/etc"
-      "INTROSPECTION_TYPELIBDIR=$(echo "$INTROSPECTION_TYPELIBDIR" | sed "s,${gobject-introspection},$out,g")"
-      "INTROSPECTION_GIRDIR=$(echo "$INTROSPECTION_GIRDIR" | sed "s,${gobject-introspection},$out,g")"
+      "localstatedir=$TMPDIR"
     )
   '';
-
-  passthru = {
-    srcVerification = fetchurl {
-      failEarly = true;
-      pgpsigUrls = map (n: "${n}.sign") src.urls;
-      pgpKeyFingerprint = "3DB4 6B55 EFA5 9D40 E623  2148 D14E F15D AFE1 1347";
-      inherit (src) urls outputHash outputHashAlgo;
-    };
-  };
 
   meta = with stdenv.lib; {
     homepage = http://www.freedesktop.org/wiki/Software/udisks;

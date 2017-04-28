49c7d44ca500a0f3b3e36d3b9361ebd8b94eea37
Cody W. Opel <codyopel@gmail.com>
2017-04-28 03:03:32 -0400

    geoclue: 2.4.4 -> 2.4.6

--- pkgs/all-pkgs/g/geoclue/default.nix
+++ pkgs/all-pkgs/g/geoclue/default.nix
@@ -2,6 +2,7 @@
 , fetchurl
 , gettext
 , intltool
+, lib
 
 , avahi
 , dbus
@@ -13,11 +14,11 @@
 }:
 
 let
-  inherit (stdenv.lib)
-    enFlag;
+  inherit (lib)
+    boolEn;
 
   versionMajor = "2.4";
-  version = "${versionMajor}.4";
+  version = "${versionMajor}.6";
 in
 stdenv.mkDerivation rec {
   name = "geoclue-${version}";
@@ -25,7 +26,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "https://www.freedesktop.org/software/geoclue/releases/"
       + "${versionMajor}/${name}.tar.xz";
-    sha256 = "9c43fb9d0c12067ea64400500abb0640194947d4c2c55e38545afe5d9c5c315c";
+    sha256 = "c57df7455c9b4b2f43b0f5d9be14d52f5ff9897236df768f6ca9044b79b6d3b6";
   };
 
   nativeBuildInputs = [
@@ -55,12 +56,12 @@ stdenv.mkDerivation rec {
     "--disable-gtk-doc"
     "--disable-gtk-doc-html"
     "--disable-gtk-doc-pdf"
-    (enFlag "introspection" (gobject-introspection != null) null)
+    "--${boolEn (gobject-introspection != null)}-introspection"
     "--enable-libgeoclue"
-    (enFlag "3g-source" (modemmanager != null) null)
-    (enFlag "cdma-source" (modemmanager != null) null)
-    (enFlag "modem-gps-source" (modemmanager != null) null)
-    (enFlag "nmea-source" (avahi != null) null)
+    "--${boolEn (modemmanager != null)}-3g-source"
+    "--${boolEn (modemmanager != null)}-cdma-source"
+    "--${boolEn (modemmanager != null)}-modem-gps-source"
+    "--${boolEn (avahi != null)}-nmea-source"
     "--enable-backend"
     "--disable-demo-agent"
     "--enable-compile-warnings"
@@ -68,7 +69,7 @@ stdenv.mkDerivation rec {
     "--disable-debug"
   ];
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Geolocation framework and some data providers";
     homepage = https://www.freedesktop.org/software/geoclue/;
     license = licenses.lgpl2;

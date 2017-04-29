28e29d09581c17d645d19e59d9eb140a6f5f40e3
William A. Kennington III <william@wkennington.com>
2017-04-29 15:57:34 -0700

    gnulib: 2017-03-17 -> 2017-04-29

--- pkgs/all-pkgs/g/gnulib/default.nix
+++ pkgs/all-pkgs/g/gnulib/default.nix
@@ -2,14 +2,18 @@
 , fetchzip
 }:
 
+let
+  date = "2017-04-29";
+  rev = "f9973ab5540a56ee89888799a6d7a12c9aa603cf";
+in
 stdenv.mkDerivation {
-  name = "gnulib-2017-03-17";
+  name = "gnulib-${date}";
 
   src = fetchzip {
-    version = 2;
-    url = "https://git.savannah.gnu.org/cgit/gnulib.git/snapshot/gnulib-e30643c2f1dccbd1b7f02e4a3748590d03b4d5dd.tar.xz";
-    multihash = "QmbKafqr4L62s3XQNm8bkVgnFJoWnpot973LWMARJX2xN8";
-    sha256 = "70708d36b77f73aa0dc59436a59e2d65d3acbcfb530148de9deb99a1d7afde81";
+    version = 3;
+    url = "https://git.savannah.gnu.org/cgit/gnulib.git/snapshot/gnulib-${rev}.tar.xz";
+    multihash = "Qmd3PwjvZKUSG8n8SgcaBHfmD9ULRQHZ39g1qmKzJCR58K";
+    sha256 = "73b60abb56e6026d4ec09e56a68785c2165f1cf50345e16f99827b70493d4c8f";
   };
 
   installPhase = ''

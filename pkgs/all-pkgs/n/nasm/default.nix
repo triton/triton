8f7ac0ae726b104d1ebc0634d908ee107746f7e1
William A. Kennington III <william@wkennington.com>
2017-05-03 13:43:56 -0700

    nasm: 2.12.02 -> 2.13.01

--- pkgs/all-pkgs/n/nasm/default.nix
+++ pkgs/all-pkgs/n/nasm/default.nix
@@ -3,16 +3,16 @@
 }:
 
 let
-  version = "2.12.02";
+  version = "2.13.01";
 in
 stdenv.mkDerivation rec {
   name = "nasm-${version}";
 
   src = fetchurl {
     url = "http://www.nasm.us/pub/nasm/releasebuilds/${version}/"
-      + "${name}.tar.bz2";
-    multihash = "QmVcsibVf9LePwrMKfmMrs7VPRApq7V56QkPohGsgEg3zd";
-    sha256 = "00b0891c678c065446ca59bcee64719d0096d54d6886e6e472aeee2e170ae324";
+      + "${name}.tar.xz";
+    multihash = "QmUadfDT8z2wGBF4vRc41YKDji2BoSt1iFF5ft3mHyTDrj";
+    sha256 = "aa0213008f0433ecbe07bb628506a5c4be8079be20fc3532a5031fd639db9a5e";
   };
 
   meta = with stdenv.lib; {

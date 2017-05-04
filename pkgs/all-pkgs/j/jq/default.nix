009187c9682e02b534487ccef936ea725aee27b1
William A. Kennington III <william@wkennington.com>
2017-05-03 17:35:09 -0700

    jq: 1.5 -> 2017-04-29

--- pkgs/all-pkgs/j/jq/default.nix
+++ pkgs/all-pkgs/j/jq/default.nix
@@ -1,37 +1,36 @@
 { stdenv
-, fetchTritonPatch
-, fetchurl
+, autoreconfHook
+, fetchFromGitHub
 
-#, oniguruma  # Broken for some reason
+, oniguruma
 }:
 
+let
+  date = "2017-04-29";
+  rev = "6d89e297febdbcbad4ecf201e56fc8ec99f67137";
+in
 stdenv.mkDerivation rec {
-  name = "jq-1.5";
+  name = "jq-${date}";
 
-  src = fetchurl {
-    url = "https://github.com/stedolan/jq/releases/download/${name}/${name}.tar.gz";
-    sha256 = "c4d2bfec6436341113419debf479d833692cc5cdab7eb0326b5a4d4fbe9f493c";
+  src = fetchFromGitHub {
+    version = 3;
+    owner = "stedolan";
+    repo = "jq";
+    inherit rev;
+    sha256 = "83eb0fe70189f848daf4cf61300825ebf439e0c99dfabab1cd3a8b0139fdf1c1";
   };
 
+  nativeBuildInputs = [
+    autoreconfHook
+  ];
+
   buildInputs = [
-    #oniguruma
+    oniguruma
   ];
 
-  patches = [
-    (fetchTritonPatch {
-      rev = "17b376c8a31a31c1b962aa6a856830cbafd13651";
-      file = "jq/jq-1.5-cve-2015-8863.patch";
-      sha256 = "c0cc0da8be2324ece805b6324173d6b12c4865c744cec38d95a51c338442ee5c";
-    })
+  configureFlags = [
+    "--disable-docs"
   ];
-
-  # For some reason jq doesn't have the rpath for libjq.so
-  preFixup = ''
-    patchelf \
-      --set-rpath "$out/lib:$(patchelf \
-      --print-rpath "$out/bin/jq")" \
-      "$out/bin/jq"
-  '';
 
   meta = with stdenv.lib; {
     description = "A lightweight and flexible command-line JSON processor";

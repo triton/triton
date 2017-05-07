75b49fc73ccfd6a92360a670a83458e2d5432e64
William A. Kennington III <william@wkennington.com>
2017-05-06 19:31:04 -0700

    libtirpc: Fixes

--- pkgs/all-pkgs/l/libtirpc/default.nix
+++ pkgs/all-pkgs/l/libtirpc/default.nix
@@ -1,4 +1,5 @@
 { stdenv
+, fetchTritonPatch
 , fetchurl
 
 , krb5_lib
@@ -18,6 +19,19 @@ stdenv.mkDerivation rec {
 
   propagatedBuildInputs = [
     krb5_lib
+  ];
+
+  patches = [
+    (fetchTritonPatch {
+      rev = "d6fa7f49c0b35ca8b9015226bdbbecc45c9f0ac4";
+      file = "l/libtirpc/add-missing-rwlocks.patch";
+      sha256 = "8bcdbd700ec6f8b5f87881251f9851174df233975d95a8dfdf7359f057fb3b80";
+    })
+    (fetchTritonPatch {
+      rev = "d6fa7f49c0b35ca8b9015226bdbbecc45c9f0ac4";
+      file = "l/libtirpc/CVE-2017-8879.patch";
+      sha256 = "091d3ff2b53a3ef9b20c61af19192434f652e528070fd57c706bce2988de0279";
+    })
   ];
 
   meta = with stdenv.lib; {

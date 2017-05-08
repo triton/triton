eb3dbe4878f0c6a40916cad3b1d94c140b4a6dbb
Cody W. Opel <codyopel@gmail.com>
2017-05-08 17:12:17 -0400

    vala(0.36): 0.36.0 -> 0.36.3

--- pkgs/all-pkgs/v/vala/sources.nix
+++ pkgs/all-pkgs/v/vala/sources.nix
@@ -5,7 +5,7 @@
     sha256 = "3fd4ba371778bc87da42827b8d23f1f42b0629759a9a1c40c9683dfb7e73fae5";
   };
   "0.36" = {
-    version = "0.36.0";
-    sha256 = "d27c9d11302ce6e521d616c89ea509f887de449fa4728d004e51d8f9646a775e";
+    version = "0.36.3";
+    sha256 = "ac8a4ecd01f62d0c5f62ba50b7290d8c5a1edb308eec772a65b8e79be68f061c";
   };
 }

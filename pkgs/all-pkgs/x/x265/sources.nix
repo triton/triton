87506e25b30d3325442399b4e1bd845903a8d269
Cody W. Opel <codyopel@gmail.com>
2017-05-08 16:11:03 -0400

    x265(head): 2017-04-12 -> 2017-05-02

--- pkgs/all-pkgs/x/x265/sources.nix
+++ pkgs/all-pkgs/x/x265/sources.nix
@@ -6,9 +6,9 @@
     sha256 = "9c2aa718d78f6fecdd783f08ab83b98d3169e5f670404da4c16439306907d729";
   };
   "head" = {
-    fetchzipversion = 2;
-    version = "2017-04-12";
-    rev = "0b7d54dbb71a87a0c6514fee04a51c4b29d5d704";
-    sha256 = "a87b2f702177bd86bcc0f813b1c508d2f99cec530bb3793abe580b64dafbe6a7";
+    fetchzipversion = 3;
+    version = "2017-05-02";
+    rev = "fd01abfc7898283922b34f7fcd9192ddf9c7818a";
+    sha256 = "395357390584eb283470306f406c0cc3ce31a88ac5cfe1eca78c3131460e199b";
   };
 }

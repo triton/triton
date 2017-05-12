73cecce7504f907b322ded54b43d4eb42c111cf3
Cody W. Opel <codyopel@gmail.com>
2017-05-12 02:50:05 -0400

    remove unnecessary file misplaced in lib/

--- default.nix
+++ default.nix
@@ -2,13 +2,15 @@
 , hostSystem ? builtins.currentSystem
 , config ? null
 } @ args:
+
 let
-  requiredVersion = import ./lib/minver.nix;
+  # Minimum required version for evaluating Nixpkgs
+  requiredVersion = "1.10";
 in
-  if ! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.nixVersion == 1 then
-    abort "This version of Triton requires Nix >= ${requiredVersion}, please upgrade!"
-  else
-    import ./pkgs/top-level/all-packages.nix {
-      inherit targetSystem hostSystem config;
-      stdenv = null;
-    }
+if ! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.nixVersion == 1 then
+  abort "This version of Triton requires Nix >= ${requiredVersion}, please upgrade!"
+else
+  import ./pkgs/top-level/all-packages.nix {
+    inherit targetSystem hostSystem config;
+    stdenv = null;
+  }

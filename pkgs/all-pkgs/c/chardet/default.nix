6757ba5aad6bfdb0a3445f758a98a7a3f83ed444
Cody W. Opel <codyopel@gmail.com>
2017-05-13 04:56:10 -0400

    chardet: 2.3.0 -> 3.0.2

--- pkgs/all-pkgs/c/chardet/default.nix
+++ pkgs/all-pkgs/c/chardet/default.nix
@@ -1,21 +1,30 @@
 { stdenv
 , buildPythonPackage
 , fetchPyPi
+, lib
+
+, pytest-runner
 }:
 
+let
+  version = "3.0.2";
+in
 buildPythonPackage rec {
   name = "chardet-${version}";
-  version = "2.3.0";
 
   src = fetchPyPi {
     package = "chardet";
     inherit version;
-    sha256 = "e53e38b3a4afe6d1132de62b7400a4ac363452dc5dfcf8d88e8e0cce663c68aa";
+    sha256 = "4f7832e7c583348a9eddd927ee8514b3bf717c061f57b21dbe7697211454d9bb";
   };
 
-  doCheck = true;
+  propagatedBuildInputs = [
+    pytest-runner
+  ];
 
-  meta = with stdenv.lib; {
+  doCheck = false;
+
+  meta = with lib; {
     description = "Universal encoding detector";
     homepage = https://github.com/chardet/chardet;
     license = licenses.lgpl2;

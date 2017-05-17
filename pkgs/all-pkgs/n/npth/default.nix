9ca9946291d5ce5e203aa41e726694b05a8036c6
William A. Kennington III <william@wkennington.com>
2017-05-17 10:00:22 -0700

    npth: 1.3 -> 1.4

--- pkgs/all-pkgs/n/npth/default.nix
+++ pkgs/all-pkgs/n/npth/default.nix
@@ -8,7 +8,7 @@ let
     "mirror://gnupg/npth/npth-${version}.tar.bz2"
   ];
 
-  version = "1.3";
+  version = "1.4";
 in
 stdenv.mkDerivation rec {
   name = "npth-${version}";
@@ -16,16 +16,16 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     urls = tarballUrls version;
     hashOutput = false;
-    sha256 = "bca81940436aed0734eb8d0ff8b179e04cc8c087f5625204419f5f45d736a82a";
+    sha256 = "8915141836a3169a502d65c1ebd785fcc6d406cae5ee84474272ebf2fa96f1f2";
   };
 
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
-      urls = tarballUrls "1.3";
+      urls = tarballUrls "1.4";
       pgpsigUrls = map (n: "${n}.sig") urls;
       inherit (gnupg.srcVerification) pgpKeyFingerprints;
-      outputHash = "bca81940436aed0734eb8d0ff8b179e04cc8c087f5625204419f5f45d736a82a";
+      outputHash = "8915141836a3169a502d65c1ebd785fcc6d406cae5ee84474272ebf2fa96f1f2";
       inherit (src) outputHashAlgo;
     };
   };

4c3a627b12ac96781fc0b392a9e1e5ec3eb9fa6b
William A. Kennington III <william@wkennington.com>
2017-05-18 12:07:52 -0700

    shadow: 4.2.1 -> 4.5

--- pkgs/all-pkgs/s/shadow/default.nix
+++ pkgs/all-pkgs/s/shadow/default.nix
@@ -2,20 +2,30 @@
 , fetchTritonPatch
 , fetchurl
 
+, acl
+, attr
+, audit_lib
+, cracklib
 , pam
 }:
 
+let
+  version = "4.5";
+in
 stdenv.mkDerivation rec {
-  name = "shadow-4.2.1";
+  name = "shadow-${version}";
 
   src = fetchurl {
-    url = "https://pkg-shadow.alioth.debian.org/releases/${name}.tar.xz";
+    url = "https://github.com/shadow-maint/shadow/releases/download/${version}/${name}.tar.xz";
     hashOutput = false;
-    multihash = "QmUM7xPj9MBJzNdNw2GNHtSJGa8j4wYy7z6qs3TfoLPZbY";
-    sha256 = "0h9x1zdbq0pqmygmc1x459jraiqw4gqz8849v268crk78z8r621v";
+    sha256 = "fc8c858381ad577a5c25ff5beb6ee60a34f8719c73e4e7c61e74188b4e54b741";
   };
 
   buildInputs = [
+    acl
+    attr
+    audit_lib
+    cracklib
     pam
   ];
 
@@ -39,19 +49,32 @@ stdenv.mkDerivation rec {
     export shadow_cv_logdir=/var/log
   '';
 
+  configureFlags = [
+    "--sysconfdir=/etc"
+    "--localstatedir=/var"
+    "--with-audit"
+    "--with-libpam"
+    "--with-acl"
+    "--with-attr"
+    "--with-libcrack"
+  ];
+
   preBuild = ''
     grep -r '/usr/sbin/nscd' .
-    substituteInPlace lib/nscd.c --replace /usr/sbin/nscd /run/current-system/sw/bin/nscd
+    sed -i 's,/usr/sbin/nscd,/run/current-system/sw/bin/nscd,g' lib/nscd.c
+  '';
+
+  preInstall = ''
+    installFlagsArray+=("sysconfdir=$out/etc")
   '';
 
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;
-      pgpsigUrls = map (n: "${n}.sig") src.urls;
+      pgpsigUrls = map (n: "${n}.asc") src.urls;
       pgpKeyFingerprints = [
-        "96F7 A8CC A87F 0EB8 3497 01B2 5A0A 399A EA7C F5AD"
-        "F972 A168 A270 3B34 CC23 E09F D4E5 EDAC C014 3D2D"
-        "D5C2 F9BF CA12 8BBA 22A7 7218 872F 702C 4D6E 25A8"
+        # Serge Hallyn
+        "F1D0 8DB7 7818 5BF7 8400  2DFF E9FE EA06 A85E 3F9D"
       ];
       inherit (src) urls outputHash outputHashAlgo;
     };

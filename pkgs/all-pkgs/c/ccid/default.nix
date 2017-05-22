97ade66961c2175c634e99126c7dbbe2896a0c76
William A. Kennington III <william@wkennington.com>
2017-05-21 18:03:41 -0700

    ccid: 1.4.26 -> 1.4.27

--- pkgs/all-pkgs/c/ccid/default.nix
+++ pkgs/all-pkgs/c/ccid/default.nix
@@ -7,7 +7,7 @@
 }:
 
 let
-  name = "ccid-1.4.26";
+  name = "ccid-1.4.27";
 
   tarballUrls = id: [
     "https://alioth.debian.org/frs/download.php/file/${id}/${name}.tar.bz2"
@@ -17,10 +17,10 @@ stdenv.mkDerivation rec {
   inherit name;
 
   src = fetchurl {
-    urls = tarballUrls "4205";
+    urls = tarballUrls "4218";
     hashOutput = false;
-    multihash = "QmUWnR9KYbRRUnsnh2jxhS6vhDGgk5mDFWhr5EWq83bG8c";
-    sha256 = "3267bf708ab780c02f01f6241b7c7277cb892d30fd1179a9926a8cc0ca40be2f";
+    multihash = "QmVhzTQMANuMwnCjkn3uwm4G5TQEGwAAFPyRfKHf8XQqWh";
+    sha256 = "a660e269606986cb94840ad5ba802ffb0cd23dd12b98f69a35035e0deb9dd137";
   };
 
   nativeBuildInputs = [
@@ -51,7 +51,7 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
-      pgpsigUrls = map (n: "${n}.asc") (tarballUrls "4206");
+      pgpsigUrls = map (n: "${n}.asc") (tarballUrls "4219");
       pgpKeyFingerprint = "F5E1 1B9F FE91 1146 F41D  953D 78A1 B4DF E8F9 C57E";
       inherit (src) urls outputHashAlgo outputHash;
     };

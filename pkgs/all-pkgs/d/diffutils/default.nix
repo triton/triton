53d6239076cad50852130cc4f795c154b0986903
William A. Kennington III <william@wkennington.com>
2017-05-22 10:46:25 -0700

    diffutils: 3.5 -> 3.6

--- pkgs/all-pkgs/d/diffutils/default.nix
+++ pkgs/all-pkgs/d/diffutils/default.nix
@@ -4,7 +4,7 @@
 }:
 
 let
-  version = "3.5";
+  version = "3.6";
 
   tarballUrls = version: [
     "mirror://gnu/diffutils/diffutils-${version}.tar.xz"
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     urls = tarballUrls version;
     hashOutput = false;
-    sha256 = "dad398ccd5b9faca6b0ab219a036453f62a602a56203ac659b43e889bec35533";
+    sha256 = "d621e8bdd4b573918c8145f7ae61817d1be9deb4c8d2328a65cea8e11d783bd6";
   };
 
   # We need to directly reference coreutils, otherwise the
@@ -28,11 +28,11 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
-      urls = tarballUrls "3.5";
+      urls = tarballUrls "3.6";
       pgpsigUrls = map (n: "${n}.sig") urls;
       pgpKeyFingerprint = "155D 3FC5 00C8 3448 6D1E  EA67 7FD9 FCCB 000B EEEE";
       inherit (src) outputHashAlgo;
-      outputHash = "dad398ccd5b9faca6b0ab219a036453f62a602a56203ac659b43e889bec35533";
+      outputHash = "d621e8bdd4b573918c8145f7ae61817d1be9deb4c8d2328a65cea8e11d783bd6";
     };
   };
 

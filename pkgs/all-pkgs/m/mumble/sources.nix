08173d1f9d82caf9b351a6239442147d58d65756
William A. Kennington III <william@wkennington.com>
2017-05-28 00:36:04 -0700

    mumble: 2017-05-03 -> 2017-05-25

--- pkgs/all-pkgs/m/mumble/sources.nix
+++ pkgs/all-pkgs/m/mumble/sources.nix
@@ -6,8 +6,8 @@
   };
   "git" = {
     fetchzipversion = 3;
-    version = "2017-05-03";
-    rev = "0cffca1200bfb3cc9c68b23c28a211cdafa2b4c9";
-    sha256 = "99c86ef14effb3b0fabfe49d061107a118998df91bb6e086d8507348f969ff94";
+    version = "2017-05-25";
+    rev = "3754898ac94ed3f1e86408114917d1b4c06f17b3";
+    sha256 = "94ff0675ac1497317e6fd9f5ccc80cd0d87118698c431e79c524e54e596755a4";
   };
 }

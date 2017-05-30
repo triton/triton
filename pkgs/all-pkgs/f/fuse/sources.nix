606ea8da205c3749036ac0e7990fd5a299d47863
William A. Kennington III <william@wkennington.com>
2017-05-30 11:27:28 -0700

    fuse: 3.0.1 -> 3.0.2

--- pkgs/all-pkgs/f/fuse/sources.nix
+++ pkgs/all-pkgs/f/fuse/sources.nix
@@ -4,7 +4,7 @@
     sha256 = "832432d1ad4f833c20e13b57cf40ce5277a9d33e483205fc63c78111b3358874";
   };
   "3" ={
-    version = "3.0.1";
-    sha256 = "ac54e0b251dcade5ac13b3286c101b95e9ed99c3309a8490a7478a1fb6cac22c";
+    version = "3.0.2";
+    sha256 = "ec632de07b79ec72a591f9878a6d090c249bdafb4e2adf47fa46dc681737b205";
   };
 }

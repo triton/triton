e60fa914202c2f339c800c0130c287a50a4c46a2
William A. Kennington III <william@wkennington.com>
2017-06-12 10:30:26 -0700

    rtmpdump: Upgrade fetchzip version

--- pkgs/all-pkgs/r/rtmpdump/default.nix
+++ pkgs/all-pkgs/r/rtmpdump/default.nix
@@ -8,19 +8,19 @@
 let
   inherit (stdenv.lib)
     optional;
-in
 
+  version = "2015-12-23";
+in
 stdenv.mkDerivation rec {
   name = "rtmpdump-${version}";
-  version = "2015-12-23";
 
   src = fetchgit {
-    version = 1;
+    version = 3;
     url = git://git.ffmpeg.org/rtmpdump;
     # Currently the latest commit is used (a release has not
     # been made since 2011, i.e. '2.4')
     rev = "fa8646daeb19dfd12c181f7d19de708d623704c0";
-    sha256 = "0fzdfbbkwbp4ikvjvfspl9ghzw13bq44bm2wf6qrpsnn5nl2wr6j";
+    sha256 = "f094114bfb2ba8f52a3cce14222a27fc1ed903f01a15d92d0867fac7f68b90ae";
   };
 
   buildInputs = [

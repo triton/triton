2e6937091a10009054a438b15bd3eefd1ffb2b21
William A. Kennington III <william@wkennington.com>
2017-06-14 21:00:11 -0700

    wayland-protocols: 1.7 -> 1.8

--- pkgs/all-pkgs/w/wayland-protocols/default.nix
+++ pkgs/all-pkgs/w/wayland-protocols/default.nix
@@ -5,12 +5,13 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "wayland-protocols-1.7";
+  name = "wayland-protocols-1.8";
 
   src = fetchurl {
     url = "https://wayland.freedesktop.org/releases/${name}.tar.xz";
+    multihash = "QmQFiCB3yMbtBJTRm5EBu43VUdtdFz8JRURhE3R861kcuw";
     hashOutput = false;
-    sha256 = "635f2a937d318f1fecb97b54074ca211486e38af943868dd0fa82ea38d091c1f";
+    sha256 = "e3fa5f2812cfec3c1c2573bd34adfe37d4d8950dba572d9ec6c52adcc5fe4b9a";
   };
 
   buildInputs = [
@@ -21,7 +22,7 @@ stdenv.mkDerivation rec {
     srcVerification = fetchurl {
       failEarly = true;
       pgpsigUrls = map (n: "${n}.sig") src.urls;
-      pgpKeyFingerprint = "8307 C0A2 24BA BDA1 BABD  0EB9 A6EE EC9E 0136 164A";
+      pgpKeyFingerprint = "A66D 805F 7C93 29B4 C5D8  2767 CCC4 F07F AC64 1EFF";
       inherit (src) urls outputHash outputHashAlgo;
     };
   };

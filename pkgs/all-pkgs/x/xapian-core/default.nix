180f4a2e8dc4ce17c166c6c998af188397998bd5
William A. Kennington III <william@wkennington.com>
2017-06-16 15:18:29 -0700

    xapian-core: 1.4.2 -> 1.4.4

--- pkgs/all-pkgs/x/xapian-core/default.nix
+++ pkgs/all-pkgs/x/xapian-core/default.nix
@@ -6,15 +6,15 @@
 }:
 
 let
-  version = "1.4.2";
+  version = "1.4.4";
 in
 stdenv.mkDerivation rec {
   name = "xapian-core-${version}";
 
   src = fetchurl {
     url = "https://oligarchy.co.uk/xapian/${version}/${name}.tar.xz";
-    multihash = "QmVB7zQeDJn5d4F6xb3kvj4d4PFeYNaCKPrTHSUnDfag6V";
-    sha256 = "aec2c4352998127a2f2316218bf70f48cef0a466a87af3939f5f547c5246e1ce";
+    multihash = "QmQjRk7UXkkbLCg57fWcVads7imyiE5EqYWz5fMkupTmvb";
+    sha256 = "a6a985a9841a452d75cf2169196b7ca6ebeef27da7c607078cd401ad041732d9";
   };
 
   buildInputs = [

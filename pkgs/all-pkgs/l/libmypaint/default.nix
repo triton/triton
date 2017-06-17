c556fa29097082fd0452b1d9685d37df81b85b7c
William A. Kennington III <william@wkennington.com>
2017-06-17 10:09:43 -0700

    libmypaint: 1.3.0-beta.1 -> 1.3.0

--- pkgs/all-pkgs/l/libmypaint/default.nix
+++ pkgs/all-pkgs/l/libmypaint/default.nix
@@ -8,15 +8,19 @@
 }:
 
 let
-  version = "1.3.0-beta.1";
+  version = "1.3.0";
+
+  baseUrls = [
+    "https://github.com/mypaint/libmypaint/releases/download/v${version}/libmypaint-${version}"
+  ];
 in
 stdenv.mkDerivation rec {
   name = "libmypaint-${version}";
 
   src = fetchurl {
-    url = "https://github.com/mypaint/libmypaint/releases/download/v${version}/${name}.tar.xz";
+    urls = map (n: "${n}.tar.xz") baseUrls;
     hashOutput = false;
-    sha256 = "d39138b21b9057376138e64f09f4c4741a14a7baed71d6f0ba9bc2504d69f9ee";
+    sha256 = "6a07d9d57fea60f68d218a953ce91b168975a003db24de6ac01ad69dcc94a671";
   };
 
   nativeBuildInputs = [
@@ -39,7 +43,7 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;
-      pgpsigUrls = map (n: "${n}.asc") src.urls;
+      sha256Urls = map (n: "${n}.sha256.asc") baseUrls;
       pgpKeyFingerprint = "C023 91F4 BBA4 F0E2 B27C  6BFF 6E30 37E1 2878 B299";
       inherit (src) urls outputHash outputHashAlgo;
     };

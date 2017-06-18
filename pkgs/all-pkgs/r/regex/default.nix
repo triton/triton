809578dc6e822c9466353abb435da8c3ba461622
Cody W. Opel <codyopel@gmail.com>
2017-06-18 02:28:20 -0400

    regex: 2016.11.21 -> 2017.06.07

--- pkgs/all-pkgs/r/regex/default.nix
+++ pkgs/all-pkgs/r/regex/default.nix
@@ -1,19 +1,22 @@
 { stdenv
 , buildPythonPackage
 , fetchPyPi
+, lib
 }:
 
+let
+  version = "2017.06.07";
+in
 buildPythonPackage rec {
   name = "regex-${version}";
-  version = "2016.11.21";
 
   src = fetchPyPi {
     package = "regex";
     inherit version;
-    sha256 = "245258012db0792838718c67fc33107f8b940196e29aa628341956d3d903ed1f";
+    sha256 = "e45784bbe5a0ce4a954fbc5e0f72909798257241147271d4906bc617fd59261b";
   };
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Alternative regular expression module, to replace re";
     homepage = https://bitbucket.org/mrabarnett/mrab-regex;
     license = licenses.free; # python sfl

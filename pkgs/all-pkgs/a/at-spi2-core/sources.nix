90d014b939ebca4fdc85efffcd5a8c3971463394
Cody W. Opel <codyopel@gmail.com>
2017-06-19 01:01:45 -0400

    at-spi2-core: 2.24.0 -> 2.24.1

--- pkgs/all-pkgs/a/at-spi2-core/sources.nix
+++ pkgs/all-pkgs/a/at-spi2-core/sources.nix
@@ -1,7 +1,7 @@
 { }:
 {
   "2.24" = {
-    version = "2.24.0";
-    sha256 = "6179f314fbd903b3cd3477a57b6b1fecca88b76ce769ce777fda0d3e5f61afcd";
+    version = "2.24.1";
+    sha256 = "1e90d064b937aacfe79a96232ac7e63d28d716e85bd9ff4333f865305a959b5b";
   };
 }

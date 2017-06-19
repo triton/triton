4fc84390f229b2938f50c40d4f69afd8dd41b125
William A. Kennington III <william@wkennington.com>
2017-06-19 12:56:07 -0700

    expat: 2.2.0 -> 2.2.1

--- pkgs/all-pkgs/e/expat/default.nix
+++ pkgs/all-pkgs/e/expat/default.nix
@@ -1,28 +1,14 @@
 { stdenv
 , fetchurl
-, fetchTritonPatch
 }:
 
 stdenv.mkDerivation rec {
-  name = "expat-2.2.0";
+  name = "expat-2.2.1";
 
   src = fetchurl {
     url = "mirror://sourceforge/expat/${name}.tar.bz2";
-    multihash = "QmXyog231KWB6xwMbPjpv51akeJZj1jHsdkVrWNkMxzVgX";
-    sha256 = "d9e50ff2d19b3538bd2127902a89987474e1a4db8e43a66a4d1a712ab9a504ff";
+    sha256 = "1868cadae4c82a018e361e2b2091de103cd820aaacb0d6cfa49bd2cd83978885";
   };
-
-  patchFlags = [
-    "-p2"
-  ];
-
-  patches = [
-    (fetchTritonPatch {
-      rev = "7968def6dcf2836760c1015f62b9afdde149217d";
-      file = "expat/2016-0718-regression.patch";
-      sha256 = "e64ff17753e601f23a6825beeb930aef1bec17b7eec7dce4e8c465b3c0cd66ff";
-    })
-  ];
 
   meta = with stdenv.lib; {
     description = "A stream-oriented XML parser library written in C";

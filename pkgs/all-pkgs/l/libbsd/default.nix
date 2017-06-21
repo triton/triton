293e0e992b26b1b335bce156f0e505cdd89cb9c0
Cody W. Opel <codyopel@gmail.com>
2017-06-21 02:01:51 -0400

    libbsd: 0.8.3 -> 0.8.4

--- pkgs/all-pkgs/l/libbsd/default.nix
+++ pkgs/all-pkgs/l/libbsd/default.nix
@@ -16,9 +16,9 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     urls = tarballUrls version;
-    multihash = "QmPbTzgi8XGkgMY6tXQ1wmdVDdqTqYurG5HsMS2WxFodtB";
+    multihash = "QmU3yw1wwbghZWsL51HzEbKMssZzvpcGHbp7pWjJ8St1Bx";
     hashOutput = false;
-    sha256 = "934b634f4dfd865b6482650b8f522c70ae65c463529de8be907b53c89c3a34a8";
+    sha256 = "217af1642915703151ea6df077311633dd6ad7bfa3c7e07dd9329a93f642cab3";
   };
 
   buildInputs = [
@@ -35,11 +35,11 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
-      urls = tarballUrls "0.8.3";
+      urls = tarballUrls "0.8.4";
       pgpsigUrls = map (n: "${n}.asc") urls;
       pgpKeyFingerprint = "4F3E 74F4 3605 0C10 F569  6574 B972 BF3E A4AE 57A3";
       inherit (src) outputHashAlgo;
-      outputHash = "934b634f4dfd865b6482650b8f522c70ae65c463529de8be907b53c89c3a34a8";
+      outputHash = "217af1642915703151ea6df077311633dd6ad7bfa3c7e07dd9329a93f642cab3";
     };
   };
 

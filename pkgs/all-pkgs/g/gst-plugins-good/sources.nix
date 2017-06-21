59bd79db6c2e1e0d29ce8682ef1920df2462a141
Cody W. Opel <codyopel@gmail.com>
2017-06-21 00:55:05 -0400

    gst-plugins-good(1.12): 1.12.0 -> 1.12.1

--- pkgs/all-pkgs/g/gst-plugins-good/sources.nix
+++ pkgs/all-pkgs/g/gst-plugins-good/sources.nix
@@ -5,7 +5,7 @@
     sha256 = "8a86c61434a8c44665365bd0b3557a040937d1f44bf69caee4e9ea816ce74d7e";
   };
   "1.12" = {
-    version = "1.12.0";
-    sha256 = "8a1d734db7338e00c28b794a7f0a5a9e67d1c5c8b0074075b50638207d372ebc";
+    version = "1.12.1";
+    sha256 = "121e8e46a7f0e622f09ec9be012607b89d737dd72d48b0f2f0680821ae2cf54b";
   };
 }

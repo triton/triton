630011634d569a2d0577d97a96352122cb9640d3
Cody W. Opel <codyopel@gmail.com>
2017-06-21 01:59:08 -0400

    libpwquality: 1.3.0 -> 1.4.0

--- pkgs/all-pkgs/l/libpwquality/default.nix
+++ pkgs/all-pkgs/l/libpwquality/default.nix
@@ -7,12 +7,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "libpwquality-1.3.0";
+  name = "libpwquality-1.4.0";
 
   src = fetchurl {
     url = "https://github.com/libpwquality/libpwquality/releases/download/"
       + "${name}/${name}.tar.bz2";
-    sha256 = "74d2ea90e103323c1f2d6a6cc9617cdae6877573eddb31aaf31a40f354cc2d2a";
+    sha256 = "1de6ff046cf2172d265a2cb6f8da439d894f3e4e8157b056c515515232fade6b";
   };
 
   buildInputs = [

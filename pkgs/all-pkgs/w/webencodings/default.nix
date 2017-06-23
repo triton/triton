35ec1aaede459b11149afe45e78c4ba1acdbde4f
Cody W. Opel <codyopel@gmail.com>
2017-06-23 04:44:43 -0400

    webencodings: 0.5 -> 0.5.1

--- pkgs/all-pkgs/w/webencodings/default.nix
+++ pkgs/all-pkgs/w/webencodings/default.nix
@@ -1,10 +1,11 @@
 { stdenv
 , buildPythonPackage
 , fetchPyPi
+, lib
 }:
 
 let
-  version = "0.5";
+  version = "0.5.1";
 in
 buildPythonPackage rec {
   name = "webencodings-${version}";
@@ -12,10 +13,10 @@ buildPythonPackage rec {
   src = fetchPyPi {
     package = "webencodings";
     inherit version;
-    sha256 = "a5c55ee93b24e740fe951c37b5c228dccc1f171450e188555a775261cce1b904";
+    sha256 = "b36a1c245f2d304965eb4e0a82848379241dc04b865afcc4aab16748587e1923";
   };
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Character encoding for the web";
     homepage = https://github.com/gsnedders/python-webencodings;
     license = licenses.bsd3;

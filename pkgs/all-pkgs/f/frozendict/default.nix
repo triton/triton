e7b9dcd4b15cfdf912d1a15681edde90829a9e1b
Cody W. Opel <codyopel@gmail.com>
2017-06-23 03:25:15 -0400

    frozendict: 1.0 -> 1.2

--- pkgs/all-pkgs/f/frozendict/default.nix
+++ pkgs/all-pkgs/f/frozendict/default.nix
@@ -1,10 +1,11 @@
 { stdenv
 , buildPythonPackage
 , fetchPyPi
+, lib
 }:
 
 let
-  version = "1.0";
+  version = "1.2";
 in
 buildPythonPackage {
   name = "frozendict-${version}";
@@ -12,10 +13,10 @@ buildPythonPackage {
   src = fetchPyPi {
     package = "frozendict";
     inherit version;
-    sha256 = "4852b8d74173f69bfaaca2dc77b69b0ae85ceddbead80d1954aa250d90e66f32";
+    sha256 = "774179f22db2ef8a106e9c38d4d1f8503864603db08de2e33be5b778230f6e45";
   };
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     maintainers = with maintainers; [
       wkennington
     ];

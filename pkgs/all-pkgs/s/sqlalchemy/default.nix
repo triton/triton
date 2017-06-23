1f79d0af597141bdd25e430dde50f7bcd4de7735
Cody W. Opel <codyopel@gmail.com>
2017-06-23 02:11:50 -0400

    sqlalchemy: 1.1.6 -> 1.1.11

--- pkgs/all-pkgs/s/sqlalchemy/default.nix
+++ pkgs/all-pkgs/s/sqlalchemy/default.nix
@@ -5,7 +5,7 @@
 }:
 
 let
-  version = "1.1.6";
+  version = "1.1.11";
 in
 buildPythonPackage rec {
   name = "sqlalchemy-${version}";
@@ -13,7 +13,7 @@ buildPythonPackage rec {
   src = fetchPyPi {
     package = "SQLAlchemy";
     inherit version;
-    sha256 = "815924e3218d878ddd195d2f9f5bf3d2bb39fabaddb1ea27dace6ac27d9865e4";
+    sha256 = "76f76965e9a968ba3aecd2a8bc0d991cea04fd9a182e6c95c81f1551487b0211";
   };
 
   meta = with lib; {

1619f48bf28f9a8630cdc1d074b19a3f4a47c8f0
Cody W. Opel <codyopel@gmail.com>
2017-06-23 04:16:28 -0400

    python-tvrage: refactor

--- pkgs/all-pkgs/p/python-tvrage/default.nix
+++ pkgs/all-pkgs/p/python-tvrage/default.nix
@@ -1,17 +1,17 @@
 { stdenv
 , buildPythonPackage
 , fetchPyPi
+, isPy3k
+, lib
 
-, pythonPackages
+, beautifulsoup
 }:
 
 let
-  inherit (pythonPackages)
-    isPy3k;
+  version = "0.4.1";
 in
 buildPythonPackage rec {
   name = "tvrage-${version}";
-  version = "0.4.1";
 
   src = fetchPyPi {
     package = "python-tvrage";
@@ -19,20 +19,19 @@ buildPythonPackage rec {
     sha256 = "f8a530376c5cf1bc573d1945a8504c3394b228c731a3eff5100c705997a72063";
   };
 
-  postPatch =
-    /* Fix setup.py detection of beautifulsoup */ ''
-      sed -i setup.py \
-        -e 's/\["BeautifulSoup"\]/\["beautifulsoup4"\]/'
-    '';
+  postPatch = /* Fix setup.py detection of beautifulsoup */ ''
+    sed -i setup.py \
+      -e 's/\["BeautifulSoup"\]/\["beautifulsoup4"\]/'
+  '';
 
   buildInputs = [
-    pythonPackages.beautifulsoup
+    beautifulsoup
   ];
 
   disabled = isPy3k;
   doCheck = true;
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Client interface for tvrage.com's XML-based api feeds";
     homepage = https://github.com/ckreutzer/python-tvrage;
     license = licenses.bsd3;

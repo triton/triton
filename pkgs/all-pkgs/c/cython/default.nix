ccc4a80c03916c6eb227ff5f4f4d4d3d0647204d
Cody W. Opel <codyopel@gmail.com>
2017-06-23 03:07:48 -0400

    cython: refactor

--- pkgs/all-pkgs/c/cython/default.nix
+++ pkgs/all-pkgs/c/cython/default.nix
@@ -1,13 +1,14 @@
 { stdenv
 , buildPythonPackage
 , fetchPyPi
+, lib
 }:
 
 let
   version = "0.25.2";
 in
 buildPythonPackage {
-  name = "Cython-${version}";
+  name = "cython-${version}";
 
   src = fetchPyPi {
     package = "Cython";
@@ -15,7 +16,7 @@ buildPythonPackage {
     sha256 = "f141d1f9c27a07b5a93f7dc5339472067e2d7140d1c5a9e20112a5665ca60306";
   };
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "An optimising static compiler for the Python and Cython programming languages";
     homepage = http://cython.org;
     license = licenses.asl20;

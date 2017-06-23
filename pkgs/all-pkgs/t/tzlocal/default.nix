6d517df704e868ed454ab1a553548720186c34e9
Cody W. Opel <codyopel@gmail.com>
2017-06-23 04:42:19 -0400

    tzlocal: 1.2.2 -> 1.4

--- pkgs/all-pkgs/t/tzlocal/default.nix
+++ pkgs/all-pkgs/t/tzlocal/default.nix
@@ -1,27 +1,30 @@
 { stdenv
 , buildPythonPackage
 , fetchPyPi
+, lib
 
-, pythonPackages
+, pytz
 }:
 
+let
+  version = "1.4";
+in
 buildPythonPackage rec {
   name = "tzlocal-${version}";
-  version = "1.2.2";
 
   src = fetchPyPi {
     package = "tzlocal";
     inherit version;
-    sha256 = "cbbaa4e9d25c36386f12af9febe315139fdd39317b91abcb42d782a5e93e525d";
+    sha256 = "05a2908f7fb1ba8843f03b2360d6ad314dbf2bce4644feb702ccd38527e13059";
   };
 
   buildInputs = [
-    pythonPackages.pytz
+    pytz
   ];
 
   doCheck = true;
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "tzinfo object for the local timezone";
     homepage = https://github.com/regebro/tzlocal;
     license = licenses.free; # CC0 1.0 Universal

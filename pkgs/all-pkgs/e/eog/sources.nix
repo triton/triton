7f70fa93322d189ea63b94f221a815c52a48aec2
Cody W. Opel <codyopel@gmail.com>
2017-06-24 08:51:09 -0400

    eog: 3.24.0 -> 3.24.1

--- pkgs/all-pkgs/e/eog/sources.nix
+++ pkgs/all-pkgs/e/eog/sources.nix
@@ -1,7 +1,7 @@
 { }:
 {
   "3.24" = {
-    version = "3.24.0";
-    sha256 = "c640bb76f202d06858e4d82be626d83256326f9cedfb46ae3b450a82ff3f11cc";
+    version = "3.24.1";
+    sha256 = "f51ee116e2c159fac1719e003a32e1cb6cbff51914fd1f6409f461a790ff27e7";
   };
 }

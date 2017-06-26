42b2a89ad7778df6ec12f2a9484f072f0812257d
Cody W. Opel <codyopel@gmail.com>
2017-06-26 07:51:14 -0400

    apr: 1.5.2 -> 1.6.2

--- pkgs/all-pkgs/a/apr/default.nix
+++ pkgs/all-pkgs/a/apr/default.nix
@@ -1,14 +1,15 @@
 { stdenv
 , fetchurl
+, lib
 }:
 
 stdenv.mkDerivation rec {
-  name = "apr-1.5.2";
+  name = "apr-1.6.2";
 
   src = fetchurl {
     url = "mirror://apache/apr/${name}.tar.bz2";
     hashOutput = false;
-    sha256 = "0ypn51xblix5ys9xy7da3ngdydip0qqh9rdq8nz54w9aq8lys0vx";
+    sha256 = "09109cea377bab0028bba19a92b5b0e89603df9eab05c0f7dbd4dd83d48dcebd";
   };
 
   preFixup = ''
@@ -19,12 +20,17 @@ stdenv.mkDerivation rec {
     srcVerification = fetchurl {
       failEarly = true;
       pgpsigUrls = map (n: "${n}.asc") src.urls;
-      pgpKeyFingerprint = "5B51 81C2 C0AB 13E5 9DA3  F7A3 EC58 2EB6 39FF 092C";
+      pgpKeyFingerprints = [
+        # Jeff Trawick
+        "5B51 81C2 C0AB 13E5 9DA3  F7A3 EC58 2EB6 39FF 092C"
+        # Nick Kew
+        "B1B9 6F45 DFBD CCF9 7401  9235 193F 180A B55D 9977"
+      ];
       inherit (src) urls outputHash outputHashAlgo;
     };
   };
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "The Apache Portable Runtime library";
     homepage = https://apr.apache.org/;
     license = licenses.asl20;

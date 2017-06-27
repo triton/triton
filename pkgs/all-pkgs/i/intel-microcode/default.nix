0a1933b68e5fe43c66ad373e3714d40941bce7a2
William A. Kennington III <william@wkennington.com>
2017-06-27 14:28:49 -0700

    intel-microcode: 2016-11-04 -> 2017-05-11

--- pkgs/all-pkgs/i/intel-microcode/default.nix
+++ pkgs/all-pkgs/i/intel-microcode/default.nix
@@ -4,17 +4,18 @@
 }:
 
 let
-  version = "2016-11-04";
+  version = "2017-05-11";
   version' = stdenv.lib.replaceStrings ["-"] [""] version;
-  id = "26400";
+  id = "26798";
 in
 stdenv.mkDerivation rec {
   name = "intel-microcode-${version}";
 
   src = fetchurl {
     url = "https://downloadmirror.intel.com/${id}/eng/microcode-${version'}.tgz";
-    md5Confirm = "6720d5c2673d79ca8e4ac8e66d8597bf";
-    sha256 = "70154ca62ff9b3da6291dfdecc90daaeb399d7290c0d308d719df16dff5ee3d1";
+    multihash = "QmUcrJ45CsJ83YoyMgGTE9g54H2yzkHUUArJDVnvknczDP";
+    hashOutput = false;
+    sha256 = "2f77fd2d87403b754d01a66c78a36a8b8ffc16dc3c50fb7aa2c4cd4da7f681a3";
   };
 
   nativeBuildInputs = [
@@ -39,6 +40,14 @@ stdenv.mkDerivation rec {
     echo 'kernel/x86/microcode/GenuineIntel.bin' | \
       bsdcpio -o -H newc -R 0:0 > "$out/intel-ucode.img"
   '';
+
+  passthru = {
+    srcVerification = fetchurl {
+      failEarly = true;
+      md5Confirm = "167e6e1ff234567291f067f48e11d740";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "Microcode for Intel processors";

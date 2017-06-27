1957a8c2df189259045779ff7ee03b23287281a3
William A. Kennington III <william@wkennington.com>
2017-06-26 22:55:39 -0700

    apr-util: 1.5.4 - 1.6.0

--- pkgs/all-pkgs/a/apr-util/default.nix
+++ pkgs/all-pkgs/a/apr-util/default.nix
@@ -7,17 +7,21 @@
 , expat
 , db
 , gnused
+, mariadb-connector-c
 , openldap
 , openssl
+, postgresql
+, sqlite
+, unixODBC
 }:
 
 stdenv.mkDerivation rec {
-  name = "apr-util-1.5.4";
+  name = "apr-util-1.6.0";
 
   src = fetchurl {
     url = "mirror://apache/apr/${name}.tar.bz2";
     hashOutput = false;
-    sha256 = "0bn81pfscy9yjvbmyx442svf43s6dhrdfcsnkpxz43fai5qk5kx6";
+    sha256 = "8474c93fa74b56ac6ca87449abe3e155723d5f534727f3f33283f6631a48ca4c";
   };
 
   nativeBuildInputs = [
@@ -28,29 +32,24 @@ stdenv.mkDerivation rec {
     apr
     db
     expat
+    mariadb-connector-c
     openldap
     openssl
+    postgresql
+    sqlite
+    unixODBC
   ];
-
-  patches = [
-		(fetchTritonPatch {
-      rev = "a29a999dbbcc6a7965e65c31ab9bc2938f738f2c";
-      file = "a/apr-util/openssl-1.1.patch";
-      sha256 = "88225f93632ee4fb2fa1e0c5ee7a177b1299e1b55f644c567b4763f8c8acbe54";
-    })
-  ];
-
-  postPatch = ''
-    sed -i 's,BN_init,BN_new,g' configure
-  '';
 
   configureFlags = [
     "--with-apr=${apr}"
     "--with-expat=${expat}"
     "--with-crypto"
     "--with-openssl=${openssl}"
+    "--with-dbm=db53"
     "--with-berkeley-db=${db}"
+    "--with-mysql=${mariadb-connector-c}"
     "--with-ldap=ldap"
+    "--with-odbc=${unixODBC}"
   ];
 
   preFixup = ''
@@ -70,7 +69,11 @@ stdenv.mkDerivation rec {
     srcVerification = fetchurl {
       failEarly = true;
       pgpsigUrls = map (n: "${n}.asc") src.urls;
-      pgpKeyFingerprint = "5B51 81C2 C0AB 13E5 9DA3  F7A3 EC58 2EB6 39FF 092C";
+      pgpKeyFingerprints = [
+        "5B51 81C2 C0AB 13E5 9DA3  F7A3 EC58 2EB6 39FF 092C"
+        # Nick Kew
+        "3CE3 BAC2 EB7B BC62 4D1D  22D8 F3B9 D88C B87F 79A9"
+      ];
       inherit (src) urls outputHash outputHashAlgo;
     };
   };

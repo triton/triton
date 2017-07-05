40301ffbd6c7a6bc4ff46e8b9def8b68b3fc687c
Cody W. Opel <codyopel@gmail.com>
2017-07-04 20:15:44 -0400

    deluge: 2017-06-24 -> 2017-06-29

--- pkgs/all-pkgs/d/deluge/default.nix
+++ pkgs/all-pkgs/d/deluge/default.nix
@@ -38,7 +38,7 @@ let
   inherit (lib)
     optionals;
 
-  version = "2017-06-24";
+  version = "2017-06-29";
   # Using an invalid version breaks compatibility with some trackers
   versionSpoof = "1.3.15";
 in
@@ -46,11 +46,11 @@ buildPythonPackage rec {
   name = "deluge-${version}";
 
   src = fetchgit {
-    version = 2;
+    version = 3;
     url = "git://git.deluge-torrent.org/deluge";
-    rev = "d250e0a48602671430729717c91e366e30728e15";
+    rev = "51b99caf24a3c2e30ed8f0a78fbef8db46d74494";
     branchName = "develop";
-    sha256 = "38c83c60ae60d149e1f35ceac0fa22c6802576f7f4557318689afa60ae2b31bc";
+    sha256 = "ec92127534424ff2764767dfb8a154e9b70cda54f1d9e9329a5865356e6ae63f";
   };
 
   nativeBuildInputs = [

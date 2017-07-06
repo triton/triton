2b5a9e21f5f506fedb23f958d97e5599a2dc510c
William A. Kennington III <william@wkennington.com>
2017-07-06 14:19:18 -0700

    spl: 2017-05-25 -> 2017-06-28

--- pkgs/all-pkgs/s/spl/sources.nix
+++ pkgs/all-pkgs/s/spl/sources.nix
@@ -6,10 +6,10 @@
     sha256 = "3f05a385bb6a26ce48daf980b4ce0daa2510a4d26db9decf7d2299466683ff2e";
   };
   "dev" = {
-    date = "2017-05-25";
-    rev = "8f87971e1fd11e3bca034dff2cbc0e884381c350";
+    date = "2017-06-28";
+    rev = "7a35f2b49581894cde93c356fd9372ff3246d344";
     maxKernelVersion = "4.13";
     fetchzipVersion = 3;
-    sha256 = "82489b27bd5cafc2fd944ba22aed59d085386903335e47d7b23c8bef8a401107";
+    sha256 = "07344c802957cd3eb12b0cbaaea100e168f65df1e9845dc5947074651ccc6888";
   };
 }

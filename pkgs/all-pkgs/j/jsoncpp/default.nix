9a62988cd72eab8c16157e05b60fc06ce35fbfb4
William A. Kennington III <william@wkennington.com>
2017-07-12 13:52:08 -0700

    jsoncpp: 1.8.0 -> 1.8.1

--- pkgs/all-pkgs/j/jsoncpp/default.nix
+++ pkgs/all-pkgs/j/jsoncpp/default.nix
@@ -6,17 +6,17 @@
 }:
 
 let
-  version = "1.8.0";
+  version = "1.8.1";
 in
 stdenv.mkDerivation rec {
   name = "jsoncpp-${version}";
 
   src = fetchFromGitHub {
-    version = 2;
+    version = 3;
     owner = "open-source-parsers";
     repo = "jsoncpp";
     rev = version;
-    sha256 = "fc200ae69c693acd725e29490ff2b3b94fe470dc3feaedeffef0c1143b344277";
+    sha256 = "a3691ee58b1cda92a4c2a15945b3877a1ebb12be4be89b78fc0752ca747e0eb4";
   };
 
   nativeBuildInputs = [

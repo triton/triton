fa7656a4e3288f7d3eceaf3f9546340b682c08fe
William A. Kennington III <william@wkennington.com>
2017-07-13 13:37:52 -0700

    mcpp: Add patches

--- pkgs/all-pkgs/m/mcpp/default.nix
+++ pkgs/all-pkgs/m/mcpp/default.nix
@@ -1,4 +1,5 @@
 { stdenv
+, fetchTritonPatch
 , fetchurl
 }:
 
@@ -12,6 +13,19 @@ stdenv.mkDerivation rec {
     url = "mirror://sourceforge/mcpp/mcpp/V.${version}/${name}.tar.gz";
     sha256 = "0r48rfghjm90pkdyr4khxg783g9v98rdx2n69xn8f6c5i0hl96rv";
   };
+
+  patches = [
+    (fetchTritonPatch {
+      rev = "853aa0890d644aaaf37aaa27534f091571dd936e";
+      file = "m/mcpp/fs28284.patch";
+      sha256 = "27e42d8cae06327370cf0f9a8118d23a2f9368b87f24b38a9cb9c0c4eaeadb4e";
+    })
+    (fetchTritonPatch {
+      rev = "853aa0890d644aaaf37aaa27534f091571dd936e";
+      file = "m/mcpp/namlen.patch";
+      sha256 = "ee8bf97c42150d2424a5984baec8227a44538e15c23cec93aabfc65daf9a6081";
+    })
+  ];
 
   configureFlags = [
     "--enable-mcpplib"

996bb49074b79a5314ab21ca5ff7dbf8aee41faa
William A. Kennington III <william@wkennington.com>
2017-07-14 16:12:07 -0700

    intltool: Fix perl compat

--- pkgs/all-pkgs/i/intltool/default.nix
+++ pkgs/all-pkgs/i/intltool/default.nix
@@ -1,4 +1,5 @@
 { stdenv
+, fetchTritonPatch
 , fetchurl
 
 , gettext
@@ -16,6 +17,19 @@ stdenv.mkDerivation rec {
     multihash = "QmYHDppGQTJfhnQzENFcLPD7iRmqWM9rbqZVtVeHjpZZVu";
     sha256 = "1karx4sb7bnm2j67q0q74hspkfn6lqprpy5r99vkn5bb36a4viv7";
   };
+
+  patches = [
+    (fetchTritonPatch {
+      rev = "e04c105d75d64c7dc9b99b21e843318fc1de43b7";
+      file = "i/intltool/0001-perl-compat-5.26.patch";
+      sha256 = "3a02adba6d81b55834934ff2b8cefd45c5461e2693d711b1ba7a633fc3b748a7";
+    })
+    (fetchTritonPatch {
+      rev = "e04c105d75d64c7dc9b99b21e843318fc1de43b7";
+      file = "i/intltool/0002-create-cache-file-atomically.patch";
+      sha256 = "13bd6deb65dc94933f132919d4eea4c24354d7c1c1c9e5930cb6e70c75703763";
+    })
+  ];
 
   # Most packages just expect these to be propagated
   propagatedBuildInputs = [

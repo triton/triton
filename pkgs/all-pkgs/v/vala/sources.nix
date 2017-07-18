279f49bd0f37e5d8c04ae252874a24d747a234e3
William A. Kennington III <william@wkennington.com>
2017-07-18 14:13:23 -0700

    vala: 0.36.3 -> 0.36.4

--- pkgs/all-pkgs/v/vala/sources.nix
+++ pkgs/all-pkgs/v/vala/sources.nix
@@ -5,7 +5,7 @@
     sha256 = "36662f77e36abf9ce6f46e3015c4512276e6361553bdcc2d75566ed83a1da19d";
   };
   "0.36" = {
-    version = "0.36.3";
-    sha256 = "ac8a4ecd01f62d0c5f62ba50b7290d8c5a1edb308eec772a65b8e79be68f061c";
+    version = "0.36.4";
+    sha256 = "e9f23ce711c1a72ce664d10946fbc5953f01b0b7f2a3562e7a01e362d86de059";
   };
 }

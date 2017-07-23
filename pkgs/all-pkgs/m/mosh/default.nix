9d5cca6ad5ba11e130bda2772b442ab11015bc57
William A. Kennington III <william@wkennington.com>
2017-07-22 21:23:22 -0700

    mosh: 1.3.0 -> 1.3.2

--- pkgs/all-pkgs/m/mosh/default.nix
+++ pkgs/all-pkgs/m/mosh/default.nix
@@ -10,12 +10,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "mosh-1.3.0";
+  name = "mosh-1.3.2";
 
   src = fetchurl {
     url = "https://mosh.mit.edu/${name}.tar.gz";
-    multihash = "QmcoCg9yhSfoBVv9n4CJiNh7uQB85kgb4F5YmAAic4EMa2";
-    sha256 = "320e12f461e55d71566597976bd9440ba6c5265fa68fbf614c6f1c8401f93376";
+    multihash = "QmY8gYBKr77eATh4i1LZFAz39Tb4USELiQ2Neev7nYodyf";
+    sha256 = "da600573dfa827d88ce114e0fed30210689381bbdcff543c931e4d6a2e851216";
   };
 
   nativeBuildInputs = [

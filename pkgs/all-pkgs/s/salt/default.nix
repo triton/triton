432cf11f4a1e2ddf1b62f6faa2a6cb2ca24b283a
Cody W. Opel <codyopel@gmail.com>
2017-07-26 08:14:22 -0400

    salt(2017-7): 2017.7.0rc1 -> 2017.7.0

--- pkgs/all-pkgs/s/salt/default.nix
+++ pkgs/all-pkgs/s/salt/default.nix
@@ -46,7 +46,29 @@
 }:
 
 let
-  source = (import ./sources.nix { })."${channel}";
+  # https://docs.saltstack.com/en/latest/topics/releases/index.html
+  # https://saltstack.com/product-support-lifecycle/
+  sources = {
+    "2016.3" = {
+      version = "2016.3.6";
+      sha256 = "4cc45f48255b7e46631807b071674552872c322ab77f398c9beefbf14d6a212f";
+    };
+    "2016.11" = {
+      version = "2016.11.6";
+      sha256 = "9031af68d31d0416fe3161526ef122a763afc6182bd63fe48b6c4d0a16a0703a";
+    };
+    "2017.7" = {
+      version = "2017.7.0";
+      sha256 = "aaf45f8dcf95a20b56072e620100432d058ab297210c4902dc90e197b236b064";
+    };
+    head = {
+      fetchzipversion = 2;
+      version = "2017-02-17";
+      rev = "deba6d26554720953409d2280e366621f40f5162";
+      sha256 = "bcfd9417a3a37909c4835dc401d57d6eb3c90b89e30526f4e76bf8d7df177afd";
+    };
+  };
+  source = sources."${channel}";
 in
 buildPythonPackage rec {
   name = "salt-${source.version}";

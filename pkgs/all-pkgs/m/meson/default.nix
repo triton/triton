540b70f0a23a7156aa13b9d2898aade0a0345014
William A. Kennington III <william@wkennington.com>
2017-07-26 10:25:17 -0700

    meson: Never mangle rpaths

--- pkgs/all-pkgs/m/meson/default.nix
+++ pkgs/all-pkgs/m/meson/default.nix
@@ -16,6 +16,12 @@ buildPythonPackage {
     sha256 = "ad1707717987fe8b7b65392b8327580105fcbdd5f2032bf3b7232b647284c95c";
   };
 
+  # Never mangle our RPATHS
+  postPatch = ''
+    grep -q 'def fix_rpath(' mesonbuild/scripts/depfixer.py
+    sed -i '/def fix_rpath(/a\        return' mesonbuild/scripts/depfixer.py
+  '';
+
   setupHook = ./setup-hook.sh;
 
   meta = with lib; {

ba9a0be12cd071d5b590a3066d6b27050ac27205
Cody W. Opel <codyopel@gmail.com>
2017-07-27 12:22:55 -0400

    faac: 1.28 -> 1.29.3

--- pkgs/all-pkgs/f/faac/default.nix
+++ pkgs/all-pkgs/f/faac/default.nix
@@ -1,9 +1,8 @@
 { stdenv
+, autoreconfHook
 , fetchTritonPatch
 , fetchurl
 , lib
-
-, mp4v2
 
 # Digital Radio Mondiale
 , drmSupport ? false
@@ -11,57 +10,27 @@
 
 let
   inherit (lib)
-    boolEn
-    boolWt;
+    boolEn;
+
+  channel = "1.29";
+  version = "${channel}.3";
 in
 stdenv.mkDerivation rec {
   name = "faac-${version}";
-  version = "1.28";
 
   src = fetchurl {
-    url = "mirror://sourceforge/faac/${name}.tar.gz";
-    sha256 = "1pqr7nf6p2r283n0yby2czd3iy159gz8rfinkis7vcfgyjci2565";
+    url = "mirror://sourceforge/faac/faac-src/faac-${channel}/${name}.tar.gz";
+    sha256 = "cef2897843baf366983ad29f471cd1c4dcc95762b86d283a925514bcc5cf5a3f";
   };
 
-  buildInputs = [
-    mp4v2
+  nativeBuildInputs = [
+    autoreconfHook
   ];
-
-  patches = [
-    (fetchTritonPatch {
-      rev = "11df088db92a272a4a8eef4fd3883812d05dcdc4";
-      file = "faac/faac-1.28-external-libmp4v2.patch";
-      sha256 = "8e0bfe501acb7f31a701ab2c797c2ace539ccd60e5f5b3c0ae538bcd5719c1df";
-    })
-    (fetchTritonPatch {
-      rev = "11df088db92a272a4a8eef4fd3883812d05dcdc4";
-      file = "faac/faac-1.28-altivec.patch";
-      sha256 = "6e836dc6cb5967a17f24cc236938452714b814a26a556d39d0a08a307835691a";
-    })
-    (fetchTritonPatch {
-      rev = "11df088db92a272a4a8eef4fd3883812d05dcdc4";
-      file = "faac/faac-1.28-libmp4v2_r479_compat.patch";
-      sha256 = "6d18ff5f5ad8e7e1717a7afcf5c7465637fc8866a213aeba44f5448de8e2ce8b";
-    })
-    (fetchTritonPatch {
-      rev = "11df088db92a272a4a8eef4fd3883812d05dcdc4";
-      file = "faac/faac-1.28-inttypes.patch";
-      sha256 = "9ab0e1eb8f489012e07a59c7ca8129b2a962fabdba7a1194155a68c7321070be";
-    })
-  ];
-
-  postPatch = /* Fix reference to obsolete macro */ ''
-    sed -i configure.in \
-      -e 's:AM_CONFIG_HEADER:AC_CONFIG_HEADERS:'
-  '';
 
   configureFlags = [
     "--${boolEn drmSupport}-drm"
     "--enable-largefile"
-    "--${boolWt (mp4v2 != null)}-mp4v2"
   ];
-
-  CXXFLAGS = "-std=c++03";
 
   meta = with lib; {
     description = "Open source MPEG-4 and MPEG-2 AAC encoder";

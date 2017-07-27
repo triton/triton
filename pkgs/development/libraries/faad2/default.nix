c6dd22b264277e15377ec2fa6aa4d248454df344
Cody W. Opel <codyopel@gmail.com>
2017-07-27 12:36:23 -0400

    faad2: 2.7 -> 2.8.1

--- pkgs/development/libraries/faad2/default.nix
+++ pkgs/development/libraries/faad2/default.nix
@@ -1,24 +1,42 @@
 { stdenv
+, autoreconfHook
 , fetchurl
-, drmSupport ? false # Digital Radio Mondiale
+, lib
+
+# Digital Radio Mondiale
+, drmSupport ? false
 }:
 
+let
+  inherit (lib)
+    boolWt;
+
+  channel = "2.8";
+  version = "${channel}.1";
+in
 stdenv.mkDerivation rec {
-  name = "faad2-2.7";
+  name = "faad2-${version}";
 
   src = fetchurl {
-    url = "mirror://sourceforge/faac/${name}.tar.bz2";
-    sha256 = "1db37ydb6mxhshbayvirm5vz6j361bjim4nkpwjyhmy4ddfinmhl";
+    url = "mirror://sourceforge/faac/faad2-src/faad2-${channel}.0/"
+      + "${name}.tar.gz";
+    sha256 = "133270a9be0c9ab8fea18017703ab4a94f9eddbb45a8aa6a511a1469fa413591";
   };
 
-  configureFlags = [
-    (if drmSupport then "--with-drm" else null)
+  nativeBuildInputs = [
+    autoreconfHook
   ];
 
-  meta = with stdenv.lib; {
+  configureFlags = [
+    #"--with-xmms"
+    "--${boolWt drmSupport}-drm"
+    #"--with-mpeg4ip"
+  ];
+
+  meta = with lib; {
     description = "An open source MPEG-4 and MPEG-2 AAC decoder";
-    homepage    = http://www.audiocoding.com/faad2.html;
-    license     = licenses.gpl2;
+    homepage = http://www.audiocoding.com/faad2.html;
+    license = licenses.gpl2;
     maintainers = with maintainers; [
       codyopel
     ];

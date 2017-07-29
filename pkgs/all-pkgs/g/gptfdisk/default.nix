1cbe09e4c1f359a01dd1165d50c98f45866f1279
Cody W. Opel <codyopel@gmail.com>
2017-07-28 23:25:30 -0400

    gptfdisk: 1.0.1 -> 1.0.3

--- pkgs/all-pkgs/g/gptfdisk/default.nix
+++ pkgs/all-pkgs/g/gptfdisk/default.nix
@@ -1,5 +1,6 @@
-{ fetchurl
-, stdenv
+{ stdenv
+, fetchurl
+, lib
 
 , icu
 , ncurses
@@ -8,7 +9,7 @@
 }:
 
 let
-  version = "1.0.1";
+  version = "1.0.3";
 in
 stdenv.mkDerivation rec {
   name = "gptfdisk-${version}";
@@ -17,7 +18,7 @@ stdenv.mkDerivation rec {
     # http://www.rodsbooks.com/gdisk/${name}.tar.gz also works, but the home
     # page clearly implies a preference for using SourceForge's bandwidth:
     url = "mirror://sourceforge/gptfdisk/gptfdisk/${version}/${name}.tar.gz";
-    sha256 = "1izazbyv5n2d81qdym77i8mg9m870hiydmq4d0s51npx5vp8lk46";
+    sha256 = "89fd5aec35c409d610a36cb49c65b442058565ed84042f767bba614b8fc91b5c";
   };
 
   buildInputs = [
@@ -28,15 +29,13 @@ stdenv.mkDerivation rec {
   ];
 
   installPhase = ''
-    mkdir -p $out/sbin
-    mkdir -p $out/share/man/man8
     for prog in gdisk sgdisk fixparts cgdisk; do
-      install -v -m755 $prog $out/sbin
-      install -v -m644 $prog.8 $out/share/man/man8
+      install -D -v -m755 $prog $out/bin
+      install -D -v -m644 $prog.8 $out/share/man/man8
     done
   '';
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Set of text-mode partitioning tools for Globally Unique Identifier (GUID) Partition Table (GPT) disks";
     license = licenses.gpl2;
     homepage = http://www.rodsbooks.com/gdisk/;

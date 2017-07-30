ea32dee08efda2969b88da5cd61d98a0f2f82a2f
Cody W. Opel <codyopel@gmail.com>
2017-07-29 22:51:39 -0400

    fdk-aac(head): 2017-06-10 -> 2017-06-22

--- pkgs/all-pkgs/f/fdk-aac/default.nix
+++ pkgs/all-pkgs/f/fdk-aac/default.nix
@@ -14,7 +14,21 @@ let
     boolEn
     optionals;
 
-  source = (import ./sources.nix { })."${channel}";
+  sources = {
+    "stable" = rec {
+      fetchzipverion = 2;
+      version = "0.1.5";
+      rev = "v${version}";
+      sha256 = "e2fdeb17d045e15cf57cd2689deca4ff7d0ad18cd4cdf9e400ff2664f3c91cf8";
+    };
+    "head" = {
+      fetchzipverion = 3;
+      version = "2017-06-22";
+      rev = "af5863a78efdfccd003dd6bea68c4a2cd2ad9f37";
+      sha256 = "2c0d90dd79d12fceda6f7fccfc0d0d4033a3c110a071f97e02898dea61db7cd0";
+    };
+  };
+  source = sources."${channel}";
 in
 stdenv.mkDerivation rec {
   name = "fdk-aac-${source.version}";

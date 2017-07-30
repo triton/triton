a8f2dfe789851ea760b5a9042ca5700da881c773
Cody W. Opel <codyopel@gmail.com>
2017-07-30 00:55:09 -0400

    zzliplib: 0.13.58 -> 0.13.67

--- pkgs/development/libraries/zziplib/default.nix
+++ pkgs/development/libraries/zziplib/default.nix
@@ -1,39 +1,53 @@
-{ fetchurl, stdenv, perl, python, zip, xmlto, zlib }:
+{ stdenv
+, fetchFromGitHub
+, lib
 
+, perl
+, python
+, xmlto
+, zip
+, zlib
+}:
+
+let
+  version = "0.13.67";
+in
 stdenv.mkDerivation rec {
-  name = "zziplib-0.13.58";
+  name = "zziplib-${version}";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/zziplib/${name}.tar.bz2";
-    sha256 = "13j9f6i8rx0qd5m96iwrcha78h34qpfk5qzi7cv098pms6gq022m";
+  src = fetchFromGitHub {
+    version = 3;
+    owner = "gdraheim";
+    repo = "zziplib";
+    rev = "v${version}";
+    sha256 = "ea26e0f197138a30318955a62c644503e48fe8ef4385325152434ef386fd1385";
   };
 
-  patchPhase = ''
-    sed -i -e s,--export-dynamic,, configure
-  '';
+  buildInputs = [
+    perl
+    python
+    xmlto
+    zip
+    zlib
+  ];
 
-  buildInputs = [ perl python zip xmlto zlib ];
+  postPatch = ''
+    sed -i configure \
+      -e 's/--export-dynamic//'
+  '';
 
   doCheck = true;
   parallelCheck = false;
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Library to extract data from files archived in a zip file";
-
-    longDescription = ''
-      The zziplib library is intentionally lightweight, it offers the ability
-      to easily extract data from files archived in a single zip
-      file.  Applications can bundle files into a single zip archive and
-      access them.  The implementation is based only on the (free) subset of
-      compression with the zlib algorithm which is actually used by the
-      zip/unzip tools.
-    '';
-
-    license = with licenses; [ lgpl2Plus mpl11 ];
-
     homepage = http://zziplib.sourceforge.net/;
-
-    maintainers = [ ];
-    platforms = python.meta.platforms;
+    license = with licenses; [
+      lgpl2Plus
+      mpl11
+    ];
+    maintainers = with maintainers; [ ];
+    platforms = with platforms;
+      x86_64-linux;
   };
 }

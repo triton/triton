de5d2b20eecc7edd43c30296edc65a9b79812bc8
William A. Kennington III <william@wkennington.com>
2017-08-02 02:41:25 -0700

    nmap: 7.50 -> 7.60

--- pkgs/all-pkgs/n/nmap/default.nix
+++ pkgs/all-pkgs/n/nmap/default.nix
@@ -13,13 +13,13 @@ let
   ];
 in
 stdenv.mkDerivation rec {
-  name = "nmap-7.50";
+  name = "nmap-7.60";
 
   src = fetchurl {
     urls = map (n: "${n}/${name}.tar.bz2") baseUrls;
-    multihash = "Qmbq72FwwZv2vNZwL8wvMgzqVWo6EVDWzAjGuvUhtoSzWW";
+    multihash = "QmSd9BAjL53EXssomcQAJp7GD9vyWDPmMe5MEzqisKAMRC";
     hashOutput = false;
-    sha256 = "e9a96a8e02bfc9e80c617932acc61112c23089521ee7d6b1502ecf8e3b1674b2";
+    sha256 = "a8796ecc4fa6c38aad6139d9515dc8113023a82e9d787e5a5fb5fa1b05516f21";
   };
 
   nativeBuildInputs = [

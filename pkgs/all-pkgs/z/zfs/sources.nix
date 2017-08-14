148356f60940308bc503c3e51fc4726a5c8a2a8e
William A. Kennington III <william@wkennington.com>
2017-08-14 14:55:15 -0700

    zfs: 0.7.0 -> 0.7.1

--- pkgs/all-pkgs/z/zfs/sources.nix
+++ pkgs/all-pkgs/z/zfs/sources.nix
@@ -1,9 +1,9 @@
 {
   "stable" = {
-    version = "0.7.0";
+    version = "0.7.1";
     maxKernelVersion = "4.13";
     fetchzipVersion = 3;
-    sha256 = "d48436d7ff0ac6bce34478f27d056e0b97d548fa0d7663edacff2a45505ecbb8";
+    sha256 = "bad781ebf14acaa70404fcbc40ce7f3f3efed5ec27a9e8e21a4239d9fc3587e7";
   };
   "dev" = {
     date = "2017-08-14";

31bf586fa513f543a639f9275cffbab68e908811
William A. Kennington III <william@wkennington.com>
2017-08-14 20:14:13 -0700

    rustc: Add 1.19.0 bootstrap

--- pkgs/all-pkgs/r/rustc/bootstrap.nix
+++ pkgs/all-pkgs/r/rustc/bootstrap.nix
@@ -6,12 +6,12 @@
 let
   sources = {
     "${stdenv.lib.head stdenv.lib.platforms.x86_64-linux}" = {
-      sha256 = "3eebd92512040baf3c0974c3ce61859646d8cf422cc515d724d857577da0dbd5";
+      sha256 = "4c8df3088d17c8e06bf58d453d39bd521487defcefc8193203b80f0fb797d6fe";
       platform = "x86_64-unknown-linux-gnu";
     };
   };
 
-  version = "1.17.0";
+  version = "1.19.0";
   
   inherit (sources."${stdenv.targetSystem}")
     platform
@@ -33,7 +33,7 @@ stdenv.mkDerivation rec {
     for file in "''${FILES[@]}"; do
       echo "Patching $file" >&2
       patchelf --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$file" || true
-      patchelf --set-rpath "$out/lib:${stdenv.cc.cc}/lib" "$file" || true
+      patchelf --set-rpath "$out/lib:${stdenv.cc.cc}/lib:${stdenv.cc.libc}/lib" "$file" || true
     done
   '';
 

9dafba6d320b640f8668d4925a494b2d678768d7
William A. Kennington III <william@wkennington.com>
2017-08-27 21:47:15 -0700

    go: 1.8 -> 1.9

--- pkgs/all-pkgs/g/go/sources.nix
+++ pkgs/all-pkgs/g/go/sources.nix
@@ -13,4 +13,18 @@
       }
     ];
   };
+  "1.9" = {
+    version = "1.9";
+    sha256 = "a4ab229028ed167ba1986825751463605264e44868362ca8e7accc8be057e993";
+    sha256Bootstrap = {
+      "x86_64-linux" = "d70eadefce8e160638a9a6db97f7192d8463069ab33138893ad3bf31b0650a79";
+    };
+    patches = [
+      {
+        rev = "2426b84827f78c72ffcb9da51d34b889fcb8b056";
+        file = "go/remove-tools.patch";
+        sha256 = "647282e43513a6d0a71aa406f54a0b13d3331f825bc60fedebaa32d757f0e483";
+      }
+    ];
+  };
 }

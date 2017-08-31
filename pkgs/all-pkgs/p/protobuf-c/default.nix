0f40d5a4bd99f22ed2f60314ea3c2543dd418d3b
William A. Kennington III <william@wkennington.com>
2017-08-31 12:26:27 -0700

    protobuf-c: 1.2.1 -> 1.3.0

--- pkgs/all-pkgs/p/protobuf-c/default.nix
+++ pkgs/all-pkgs/p/protobuf-c/default.nix
@@ -5,14 +5,14 @@
 }:
 
 let
-  version = "1.2.1";
+  version = "1.3.0";
 in
 stdenv.mkDerivation rec {
   name = "protobuf-c-${version}";
 
   src = fetchurl {
     url = "https://github.com/protobuf-c/protobuf-c/releases/download/v${version}/${name}.tar.gz";
-    sha256 = "846eb4846f19598affdc349d817a8c4c0c68fd940303e6934725c889f16f00bd";
+    sha256 = "5dc9ad7a9b889cf7c8ff6bf72215f1874a90260f60ad4f88acf21bb15d2752a1";
   };
 
   nativeBuildInputs = [

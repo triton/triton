715954a94be6a6cc7920d977a9bcdf76d0bf787e
Cody W. Opel <codyopel@gmail.com>
2017-09-02 14:37:18 -0400

    xfconf: refactor

--- pkgs/all-pkgs/x/xfconf/default.nix
+++ pkgs/all-pkgs/x/xfconf/default.nix
@@ -13,7 +13,14 @@
 }:
 
 let
-  source = (import ./sources.nix { })."${channel}";
+  sources = {
+    "4.12" = {
+      version = "4.12.1";
+      multihash = "QmexCEXyru5zBrZf1TevhiRUJysR5xhP1kwXXaU3JGTfZ2";
+      sha256 = "35f48564e5694faa54fdc180cd3268e16fc2352946a89a3c2fc12cbe400ada36";
+    };
+  };
+  source = sources."${channel}";
 in
 stdenv.mkDerivation rec {
   name = "xfconf-${source.version}";
--- pkgs/all-pkgs/x/xfconf/sources.nix
+++
@@ -1,8 +0,0 @@
-{ }:
-{
-  "4.12" = {
-    version = "4.12.1";
-    multihash = "QmexCEXyru5zBrZf1TevhiRUJysR5xhP1kwXXaU3JGTfZ2";
-    sha256 = "35f48564e5694faa54fdc180cd3268e16fc2352946a89a3c2fc12cbe400ada36";
-  };
-}

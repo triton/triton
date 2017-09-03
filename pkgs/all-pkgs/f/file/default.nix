3deb93987cababaecfb05176ce5bc256492b67ff
William A. Kennington III <william@wkennington.com>
2017-09-03 12:55:09 -0700

    file: 5.31 -> 5.32

--- pkgs/all-pkgs/f/file/default.nix
+++ pkgs/all-pkgs/f/file/default.nix
@@ -5,13 +5,13 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "file-5.31";
+  name = "file-5.32";
 
   src = fetchurl {
     url = "ftp://ftp.astron.com/pub/file/${name}.tar.gz";
-    multihash = "QmZukJ4ygPf8gB33rF2UxQTaBCJ1W9TEFojKgE2VjYXzz8";
+    multihash = "QmchzYpNV6HRqGer6eZkoEZdMTu3X7QZi6NeKBJ2QmWCtW";
     hashOutput = false;
-    sha256 = "09c588dac9cff4baa054f51a36141793bcf64926edc909594111ceae60fce4ee";
+    sha256 = "8639dc4d1b21e232285cd483604afc4a6ee810710e00e579dbe9591681722b50";
   };
 
   buildInputs = [

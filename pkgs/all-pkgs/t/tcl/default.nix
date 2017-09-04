eb312e590022f27e8b4435a9e675944c7a1eae9b
Cody W. Opel <codyopel@gmail.com>
2017-09-04 01:00:39 -0400

    tcl: remove multihashes

--- pkgs/all-pkgs/t/tcl/default.nix
+++ pkgs/all-pkgs/t/tcl/default.nix
@@ -11,12 +11,10 @@ let
   sources = {
     "8.5" = {
       version = "8.5.19";
-      multihash = "Qmf9EJZgbqQ1Yx1BUWJ2d1NzmUai9o3VXWnuwdYqR4W51g";
       sha256 = "d3f04456da873d17f02efc30734b0300fb6c3b85028d445fe284b83253a6db18";
     };
     "8.6" = {
       version = "8.6.7";
-      multihash = "";  # FIXME
       sha256 = "7c6b8f84e37332423cfe5bae503440d88450da8cc1243496249faa5268026ba5";
     };
   };
@@ -28,7 +26,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "mirror://sourceforge/tcl/Tcl/${source.version}/"
       + "tcl${source.version}-src.tar.gz";
-    inherit (source) multihash sha256;
+    inherit (source) sha256;
   };
 
   buildInputs = [

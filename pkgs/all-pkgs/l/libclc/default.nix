1df7b58ee8efd8542a4c304572e38d2a5e3475ec
William A. Kennington III <william@wkennington.com>
2017-09-07 22:31:30 -0700

    libclc: 2017-06-02 -> 2017-09-04

--- pkgs/all-pkgs/l/libclc/default.nix
+++ pkgs/all-pkgs/l/libclc/default.nix
@@ -7,8 +7,8 @@
 }:
 
 let
-  date = "2017-06-02";
-  rev = "1cb3fbf504e25d86d972e8b2af3e24571767046b";
+  date = "2017-09-04";
+  rev = "58f9eef531c3fe91ff9577fa3a91d01f3931e04d";
 in
 stdenv.mkDerivation {
   name = "libclc-${date}";
@@ -18,7 +18,7 @@ stdenv.mkDerivation {
     owner = "llvm-mirror";
     repo = "libclc";
     inherit rev;
-    sha256 = "ad68af6b4206533f22eae5d0add7619fc622795e62170954b98e6e98961c59bc";
+    sha256 = "fc6cd3a2206f4121c43917677627c63dc41f05bc948ebe5547741b9277702a04";
   };
 
   nativeBuildInputs = [

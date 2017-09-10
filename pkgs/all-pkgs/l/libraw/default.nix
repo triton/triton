f50b4824c874c22548e7c54a25f8b8f4452b2c13
William A. Kennington III <william@wkennington.com>
2017-09-10 13:48:31 -0700

    libraw: Fix multihash

--- pkgs/all-pkgs/l/libraw/default.nix
+++ pkgs/all-pkgs/l/libraw/default.nix
@@ -14,7 +14,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "http://www.libraw.org/data/LibRaw-${version}.tar.gz";
-    #multihash = "QmboFCaq5BGwrEgf87FRQBTcAqEtuNJo247avraarqbQDT";
+    multihash = "Qmc1Qw5nW69ewRHTTWRtZ7pDwmkUw1F76mHB9pdGpbK2JZ";
     sha256 = "57ba053f075e0b80f747f3102ed985687c16a8754d109e7c4d33633269a36aaa";
   };
 

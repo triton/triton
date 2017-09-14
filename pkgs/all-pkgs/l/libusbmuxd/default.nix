2101174e87b047ef702f028639b6f52892a093c0
William A. Kennington III <william@wkennington.com>
2017-09-14 11:28:54 -0700

    libusbmuxd: Cleanup
     - Add multihash
     - Remove unused dependency on inotify-tools (it just needs the kernel
     inotify headers)
     - Inotify autodetection for configure is fine

--- pkgs/all-pkgs/l/libusbmuxd/default.nix
+++ pkgs/all-pkgs/l/libusbmuxd/default.nix
@@ -2,31 +2,20 @@
 , fetchTritonPatch
 , fetchurl
 
-, inotify-tools
 , libplist
 }:
 
-let
-  inherit (stdenv)
-    targetSystem;
-  inherit (stdenv.lib)
-    elem
-    optionals
-    platforms
-    wtFlag;
-in
-
 stdenv.mkDerivation rec {
   name = "libusbmuxd-1.0.10";
+
   src = fetchurl {
     url = "http://www.libimobiledevice.org/downloads/${name}.tar.bz2";
+    multihash = "QmedFbCrXFgYoSZ9TujESJFgoNo9ZcX47yYmUie1iAYYzP";
     sha256 = "1wn9zq2224786mdr12c5hxad643d29wg4z6b7jn888jx4s8i78hs";
   };
 
   buildInputs = [
     libplist
-  ] ++ optionals (elem targetSystem platforms.linux) [
-    inotify-tools
   ];
 
   patches = [
@@ -35,14 +24,6 @@ stdenv.mkDerivation rec {
       file = "l/libusbmuxd/CVE-2016-5104.patch";
       sha256 = "9f3a84c8d0a32df13985f6574f5f0e86af435a67606612c0811df631070a97e3";
     })
-  ];
-
-  configureFlags = [
-    # Flag is not a boolean
-    (if elem targetSystem platforms.linux then
-       null
-     else
-       "--without-inotify")
   ];
 
   meta = with stdenv.lib; {

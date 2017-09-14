cf93016269bc740e464e0dec0b71ff9d66d4015d
William A. Kennington III <william@wkennington.com>
2017-09-14 15:48:32 -0700

    texinfo: Verify 6.5

--- pkgs/all-pkgs/t/texinfo/default.nix
+++ pkgs/all-pkgs/t/texinfo/default.nix
@@ -8,13 +8,19 @@
 let
   inherit (stdenv.lib)
     optionals;
-in
 
+  tarballUrls = version: [
+    "mirror://gnu/texinfo/texinfo-${version}.tar.xz"
+  ];
+
+  version = "6.4";
+in
 stdenv.mkDerivation rec {
-  name = "texinfo-6.4";
+  name = "texinfo-${version}";
 
   src = fetchurl {
-    url = "mirror://gnu/texinfo/${name}.tar.xz";
+    urls = tarballUrls version;
+    hashOutput = false;
     sha256 = "6ae2e61d87c6310f9af7c6f2426bd0470f251d1a6deb61fba83a3b3baff32c3a";
   };
 
@@ -34,6 +40,17 @@ stdenv.mkDerivation rec {
     "install"
     "install-tex"
   ];
+
+  passthru = {
+    srcVerification = fetchurl rec {
+      failEarly = true;
+      urls = tarballUrls "6.5";
+      pgpsigUrls = map (n: "${n}.sig") urls;
+      pgpKeyFingerprint = "EAF6 69B3 1E31 E1DE CBD1  1513 DDBC 579D AB37 FBA9";
+      inherit (src) outputHashAlgo;
+      outputHash = "77774b3f4a06c20705cc2ef1c804864422e3cf95235e965b1f00a46df7da5f62";
+    };
+  };
 
   meta = with stdenv.lib; {
     homepage = "http://www.gnu.org/software/texinfo/";

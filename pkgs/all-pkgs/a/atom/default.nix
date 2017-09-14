6c9355fa5329b01a31f2759417f40f650ebb3472
Cody W. Opel <codyopel@gmail.com>
2017-09-14 04:36:58 -0400

    atom: Updates

--- pkgs/all-pkgs/a/atom/default.nix
+++ pkgs/all-pkgs/a/atom/default.nix
@@ -1,5 +1,6 @@
 { stdenv
 , fetchurl
+, lib
 , makeWrapper
 
 , alsa-lib
@@ -13,15 +14,29 @@
 , gconf
 , gdk-pixbuf
 , gdk-pixbuf_unwrapped
+, git
 , glib
 , gtk_2
 , gvfs
+, libcap
 , libgnome-keyring
 , libgpg-error
 , libnotify
+, libx11
+, libxcb
+, libxcomposite
+, libxdamage
+, libxext
+, libxfixes
+#, libxi
+, libxrandr
+, libxrender
+, libxscrnsaver
 , nspr
 , nss
 , pango
+, python
+, shared-mime-info
 , systemd_lib
 , xorg
 , zlib
@@ -30,10 +45,22 @@
 }:
 
 let
-  inherit (stdenv.lib)
+  inherit (lib)
     makeSearchPath;
 
-  source = (import ./sources.nix { })."${channel}";
+  sources = {
+    "stable" = {
+      suffix = "";
+      version = "1.20.0";
+      sha256 = "4029f2d323cab956f9c007e1bf50ac1d991abe245c34c47a930d1d6f0951f7cb";
+    };
+    "beta" = {
+      suffix = "-beta";
+      version = "1.21.0-beta0";
+      sha256 = "951148e50c531c6f4ae0f25b54fde93cccd8db0dc56bf8d5b397b16dcfa9bf4e";
+    };
+  };
+  source = sources."${channel}";
 in
 stdenv.mkDerivation rec {
   name = "atom${source.suffix}-${source.version}";
@@ -64,25 +91,28 @@ stdenv.mkDerivation rec {
     glib
     gtk_2
     gvfs
+    libcap
     libgnome-keyring
     libgpg-error
     libnotify
+    libx11
+    libxcb
+    libxcomposite
+    xorg.libXcursor
+    libxdamage
+    libxext
+    libxfixes
+    xorg.libXi
+    xorg.libxkbfile
+    libxrandr
+    libxrender
+    libxscrnsaver
+    xorg.libXtst
     nspr
     nss
     pango
     stdenv.cc.cc
     systemd_lib
-    xorg.libX11
-    xorg.libXcomposite
-    xorg.libXcursor
-    xorg.libXdamage
-    xorg.libXext
-    xorg.libXfixes
-    xorg.libXi
-    xorg.libXrandr
-    xorg.libXrender
-    xorg.libXScrnSaver
-    xorg.libXtst
     zlib
   ];
 
@@ -92,7 +122,7 @@ stdenv.mkDerivation rec {
 
   unpackPhase = ''
     mkdir -pv "$out"
-    ar p $src data.tar.gz | tar -xz
+    ar p $src data.tar.xz | tar -xJ
   '';
 
   installPhase = ''
@@ -104,12 +134,20 @@ stdenv.mkDerivation rec {
     sed -i $out/share/applications/atom${source.suffix}.desktop \
       -e "s,/usr/share/atom${source.suffix},$out/bin,"
 
+    # Replace vendored git binary
+    rm -fv $out/share/atom/resources/app/node_modules/dugite/git/bin/git
+    ln -sv ${git}/bin/git \
+      $out/share/atom/resources/app/node_modules/dugite/git/bin/git
+
     # Fix beta detection
     sed -i $out/bin/atom${source.suffix} \
       -e 's,$(basename $0),atom${source.suffix},'
 
     wrapProgram $out/bin/atom${source.suffix} \
-      --prefix 'PATH' : "${gvfs}/bin"
+      --prefix 'PATH' : "${gvfs}/bin:${python}/bin" \
+      --prefix 'LD_LIBRARY_PATH' : "${libPath}" \
+      --prefix 'XDG_DATA_DIRS' : "${shared-mime-info}/share" \
+      --prefix 'XDG_DATA_DIRS' : "$out/share"
 
     patchelf \
       --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
@@ -119,14 +157,15 @@ stdenv.mkDerivation rec {
       --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
       --set-rpath "${libPath}" \
       $out/share/atom${source.suffix}/resources/app/apm/bin/node
-    patchelf \
-      --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-      $out/share/atom${source.suffix}/resources/app.asar.unpacked/node_modules/symbols-view/vendor/ctags-linux
+    #find $out/share/atom \
+    #  -name "*.node" \
+    #  -exec patchelf --set-rpath "${libPath}:$out/share/atom" {} \;
   '';
 
+  buildDirCheck = false;  # slow
   dontStrip = true;
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "A hackable text editor for the 21st Century";
     homepage = https://atom.io/;
     license = licenses.mit;

8c11a0f7666d31cfaedcf72acd207a4de693a940
Cody W. Opel <codyopel@gmail.com>
2017-09-16 20:58:00 -0400

    geoip: 1.6.10 -> 1.6.11

--- pkgs/all-pkgs/g/geoip/default.nix
+++ pkgs/all-pkgs/g/geoip/default.nix
@@ -4,17 +4,17 @@
 }:
 
 let
-  version = "1.6.10";
+  version = "1.6.11";
 in
 stdenv.mkDerivation rec {
   name = "geoip-${version}";
 
   src = fetchFromGitHub {
-    version = 1;
+    version = 3;
     owner = "maxmind";
     repo = "geoip-api-c";
     rev = "v${version}";
-    sha256 = "a4e9caaf51edbce6d28b38e40996e851a4a4650a737f70c8f388b1bb37728136";
+    sha256 = "40b289dc9ce7ad9a509fc3f4c5c9a5727ad591be28818f3f3173b24d9a317996";
   };
 
   nativeBuildInputs = [

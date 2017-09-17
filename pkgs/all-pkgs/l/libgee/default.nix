0c449a9a3d95289107c9afcd3d08f79b25e2d136
Cody W. Opel <codyopel@gmail.com>
2017-09-17 14:49:28 -0400

    libgee: refactor

--- pkgs/all-pkgs/l/libgee/default.nix
+++ pkgs/all-pkgs/l/libgee/default.nix
@@ -14,7 +14,17 @@ let
   inherit (lib)
     boolEn;
 
-  source = (import ./sources.nix { })."${channel}";
+  sources = {
+    "0.18" = {
+      version = "0.18.1";
+      sha256 = "99686bbe5a9c89ba9502c25f61314ee8efa0dd2a1159c9a639afc1458c2839a2";
+    };
+    "0.20" = {
+      version = "0.20.0";
+      sha256 = "21308ba3ed77646dda2e724c0e8d5a2f8d101fb05e078975a532d7887223c2bb";
+    };
+  };
+  source = sources."${channel}";
 in
 stdenv.mkDerivation rec {
   name = "libgee-${source.version}";

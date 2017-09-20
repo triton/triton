6efaddf4420b0b3ffe1f411fbcfec654dda8458c
William A. Kennington III <william@wkennington.com>
2017-09-20 13:47:31 -0700

    gnupg: 2.2.0 -> 2.2.1

--- pkgs/all-pkgs/g/gnupg/default.nix
+++ pkgs/all-pkgs/g/gnupg/default.nix
@@ -32,7 +32,7 @@ let
     "mirror://gnupg/gnupg/gnupg-${version}.tar.bz2"
   ];
 
-  version = "2.2.0";
+  version = "2.2.1";
 in
 stdenv.mkDerivation rec {
   name = "gnupg-${version}";
@@ -40,7 +40,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     urls = tarballUrls version;
     hashOutput = false;
-    sha256 = "d4514a0be0f7a1ff263193330019eb4b53c82f0f5e230af3c14df371271a45e6";
+    sha256 = "34d70cd65b9c95f3f2f90a9f5c1e0b6a0fe039a8d685e2d66d69c33d1cbf62fb";
   };
 
   nativeBuildInputs = [
@@ -97,7 +97,7 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
-      urls = tarballUrls "2.2.0";
+      urls = tarballUrls "2.2.1";
       pgpsigUrl = map (n: "${n}.sig") urls;
       pgpKeyFingerprints = [
         "D869 2123 C406 5DEA 5E0F  3AB5 249B 39D2 4F25 E3B6"
@@ -106,7 +106,7 @@ stdenv.mkDerivation rec {
         "D238 EA65 D64C 67ED 4C30  73F2 8A86 1B1C 7EFD 60D9"
         "46CC 7308 65BB 5C78 EBAB  ADCF 0437 6F3E E085 6959"
       ];
-      sha256 = "d4514a0be0f7a1ff263193330019eb4b53c82f0f5e230af3c14df371271a45e6";
+      sha256 = "34d70cd65b9c95f3f2f90a9f5c1e0b6a0fe039a8d685e2d66d69c33d1cbf62fb";
     };
   };
 

dc26a57c3168991b8aa807c8fc4b89c0c0a70536
William A. Kennington III <william@wkennington.com>
2017-09-21 15:24:04 -0700

    mariadb-connector-c: 2017-07-07 -> 2017-09-08

--- pkgs/all-pkgs/m/mariadb-connector-c/default.nix
+++ pkgs/all-pkgs/m/mariadb-connector-c/default.nix
@@ -11,8 +11,8 @@
 }:
 
 let
-  rev = "3f356c0598ad4785e9b4eff23e03fd662a701070";
-  date = "2017-07-07";
+  rev = "f9a6b8e0864fdb63707419639a34a3377f1b3091";
+  date = "2017-09-08";
 in
 stdenv.mkDerivation rec {
   name = "mariadb-connector-c-${date}";
@@ -22,7 +22,7 @@ stdenv.mkDerivation rec {
     owner = "MariaDB";
     repo = "mariadb-connector-c";
     inherit rev;
-    sha256 = "40749572ddae702b7522dd0bed864f3b5cadb6a4fec798fd4aacb6213d0d0685";
+    sha256 = "812b1a602ab307924f083e2bb8d48f8312a29f48b0ee37ac8d0e76e5a1095e2c";
   };
 
   nativeBuildInputs = [

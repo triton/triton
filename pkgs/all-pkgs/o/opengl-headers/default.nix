e07996a5467ad00e531730660b86afce32617e80
Cody W. Opel <codyopel@gmail.com>
2017-09-22 08:40:02 -0400

    opengl-headers: generate tarball, repo vendors pdf files

--- pkgs/all-pkgs/o/opengl-headers/default.nix
+++ pkgs/all-pkgs/o/opengl-headers/default.nix
@@ -1,4 +1,5 @@
 { stdenv
+, fetchurl
 , fetchFromGitHub
 , lib
 }:
@@ -11,12 +12,10 @@ in
 stdenv.mkDerivation rec {
   name = "opengl-headers-${version}";
 
-  src = fetchFromGitHub {
-    version = 3;
-    owner = "KhronosGroup";
-    repo = "OpenGL-Registry";
-    rev = "93e0595941ea275b95ba115e1f400283c652004d";
-    sha256 = "40b8204a8c97e95913c31d11dc58527780e866424c5dd577f9a8ee2209612209";
+  src = fetchurl {
+    url = "http://opengl-headers.tar.xz";
+    multihash = "QmZvWAAPb4FAB4psCuvfKZW1N638AFG7ZTLX1Jr4kRE8UB";
+    sha256 = "77172528da0549cebdaf4f5f129e020bfad722fdf9808f5e32d14766c2e81081";
   };
 
   configurePhase = ":";
@@ -24,7 +23,7 @@ stdenv.mkDerivation rec {
   buildPhase = ":";
 
   installPhase = ''
-    for api in api/GL{,ES{,2,3},SC{,2}}; do
+    for api in GL{,ES{,2,3},SC{,2}}; do
       pushd $api
         while read header; do
           install -D -m644 -v $header $out/include/$(basename "$api")/$header
@@ -32,6 +31,31 @@ stdenv.mkDerivation rec {
       popd
     done
   '';
+
+  passthru = {
+    generateDistTarball = stdenv.mkDerivation rec {
+      name = "opengl-headers-${version}";
+
+      src = fetchFromGitHub {
+        version = 3;
+        owner = "KhronosGroup";
+        repo = "OpenGL-Registry";
+        rev = "93e0595941ea275b95ba115e1f400283c652004d";
+        sha256 = "40b8204a8c97e95913c31d11dc58527780e866424c5dd577f9a8ee2209612209";
+      };
+
+      configurePhase = ":";
+
+      buildPhase = ''
+        tar -Jcvf opengl-headers-${version}.tar.xz api/
+      '';
+
+      installPhase = ''
+        install -D -m644 -v 'opengl-headers-${version}.tar.xz' \
+          "$out/opengl-headers-${version}.tar.xz"
+      '';
+    };
+  };
 
   meta = with lib; {
     description = "OpenGL, OpenGL ES, and OpenGL ES-SC API headers.";

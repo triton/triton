264afdc28ef1847729b981bd5e344f11994285de
Cody W. Opel <codyopel@gmail.com>
2017-09-27 10:59:42 -0400

    libbs2b: fix source url

--- pkgs/development/libraries/audio/libbs2b/default.nix
+++ pkgs/development/libraries/audio/libbs2b/default.nix
@@ -1,11 +1,13 @@
 { stdenv, fetchurl, pkgconfig, libsndfile }:
 
+let
+  version = "3.1.0";
+in
 stdenv.mkDerivation rec {
   name = "libbs2b-${version}";
-  version = "3.1.0";
 
   src = fetchurl {
-    url = "mirror://sourceforge/bs2b/${name}.tar.bz2";
+    url = "mirror://sourceforge/bs2b/libbs2b/${version}/${name}.tar.bz2";
     sha256 = "0vz442kkjn2h0dlxppzi4m5zx8qfyrivq581n06xzvnyxi5rg6a7";
   };
 

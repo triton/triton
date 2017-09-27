659702383c5167086537ca45c7b8d728bbbc9e81
Cody W. Opel <codyopel@gmail.com>
2017-09-27 16:00:27 -0400

    gn: fix build

--- pkgs/all-pkgs/g/gn/default.nix
+++ pkgs/all-pkgs/g/gn/default.nix
@@ -1,4 +1,5 @@
 { stdenv
+, fetchTritonPatch
 , fetchurl
 , lib
 , ninja
@@ -8,11 +9,10 @@
 }:
 
 let
-  # gn cannot be built out of the expected source tree, so we use the
-  # dev tarball as a simpler way of getting the necessary sources,
-  # while ensuring gn is up-to-date for all channels. Otherwise you
-  # would need chromium/src/base, chromium/src/build,
-  # chromium/src/build/config, chromium/testing/gtest, &
+  # gn cannot be built out of the chromium source tree, so we use the
+  # tarball for the chromium channel we are builing as a simpler way of
+  # getting the necessary sources. Otherwise you would need chromium/src/base,
+  # chromium/src/build, chromium/src/build/config, chromium/testing/gtest, &
   # chromium/src/third_patry/libevent.tar.gz in addition to
   # chromium/tools/gn.
   source = (import ../../c/chromium/sources.nix { })."${source-channel}";
@@ -32,8 +32,22 @@ stdenv.mkDerivation rec {
     python2
   ];
 
+  patches = [
+    (fetchTritonPatch {
+      rev = "81e065acf0b534fd119f6398d8ee8a941b740dc5";
+      file = "c/chromium/chromium-gcc-r1.patch";
+      sha256 = "8a2aee1382c0c689e9c5e34c9e392c7bdd01dafb98ef2ad623477bd8a339bc32";
+    })
+    (fetchTritonPatch {
+      rev = "8d6e9728038c063e2441134bfca735c013b9bd6e";
+      file = "c/chromium/chromium-gn-bootstrap-r14.patch";
+      sha256 = "9df1913b936339782261f5302612e319f2a371ed15a9028c1049964697b5a21a";
+    })
+  ];
+
   postPatch = ''
     patchShebangs build/write_buildflag_header.py
+    patchShebangs build/write_build_date_header.py
     patchShebangs tools/gn/bootstrap/bootstrap.py
   '';
 

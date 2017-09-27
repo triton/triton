0f20187e2117c007ef357ab10160479a1ddb7fa1
Cody W. Opel <codyopel@gmail.com>
2017-09-27 13:04:42 -0400

    djvulibre: fix source url

--- pkgs/applications/misc/djvulibre/default.nix
+++ pkgs/applications/misc/djvulibre/default.nix
@@ -1,10 +1,15 @@
 { stdenv, fetchurl, libjpeg, libtiff, librsvg }:
 
+# TODO: https://github.com/barak/djvulibre
+
+let
+  version = "3.5.27";
+in
 stdenv.mkDerivation rec {
-  name = "djvulibre-3.5.27";
+  name = "djvulibre-${version}";
 
   src = fetchurl {
-    url = "mirror://sourceforge/djvu/${name}.tar.gz";
+    url = "mirror://sourceforge/djvu/DjVuLibre/${version}/${name}.tar.gz";
     sha256 = "0psh3zl9dj4n4r3lx25390nx34xz0bg0ql48zdskhq354ljni5p6";
   };
 

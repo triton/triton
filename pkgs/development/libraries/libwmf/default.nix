a404f660499dd128e40ebae389738fd01836ddea
Cody W. Opel <codyopel@gmail.com>
2017-09-27 13:26:12 -0400

    libwmf: fix source url

--- pkgs/development/libraries/libwmf/default.nix
+++ pkgs/development/libraries/libwmf/default.nix
@@ -1,18 +1,22 @@
 { stdenv, fetchurl, zlib, imagemagick, libpng, pkgconfig, glib, freetype
 , libjpeg, libxml2 }:
 
-stdenv.mkDerivation {
-  name = "libwmf-0.2.8.4";
+let
+  version = "0.2.8.4";
+in
+stdenv.mkDerivation rec {
+  name = "libwmf-${version}";
 
   src = fetchurl {
-    url = mirror://sourceforge/wvware/libwmf-0.2.8.4.tar.gz;
+    url = "mirror://sourceforge/wvware/libwmf/${version}/${name}.tar.gz";
     sha256 = "1y3wba4q8pl7kr51212jwrsz1x6nslsx1gsjml1x0i8549lmqd2v";
   };
 
   buildInputs = [ zlib imagemagick libpng pkgconfig glib freetype libjpeg libxml2 ];
 
   patches = [
-    ./CVE-2006-3376.patch ./CVE-2009-1364.patch
+    ./CVE-2006-3376.patch
+    ./CVE-2009-1364.patch
     ./CVE-2015-0848+4588+4695+4696.patch
   ];
 

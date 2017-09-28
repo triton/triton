24b38815163b70c6d1b7c5a4c4f5c6dcad19d843
Cody W. Opel <codyopel@gmail.com>
2017-09-28 17:40:51 -0400

    faad2: 2.8.1 -> 2.8.3

--- pkgs/development/libraries/faad2/default.nix
+++ pkgs/development/libraries/faad2/default.nix
@@ -12,7 +12,7 @@ let
     boolWt;
 
   channel = "2.8";
-  version = "${channel}.1";
+  version = "${channel}.3";
 in
 stdenv.mkDerivation rec {
   name = "faad2-${version}";
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "mirror://sourceforge/faac/faad2-src/faad2-${channel}.0/"
       + "${name}.tar.gz";
-    sha256 = "133270a9be0c9ab8fea18017703ab4a94f9eddbb45a8aa6a511a1469fa413591";
+    sha256 = "a8a0e6818f436bb6f84f06b29248758b425327bba061428e6bce9522c613c5a7";
   };
 
   nativeBuildInputs = [

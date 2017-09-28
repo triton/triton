30e54c652dd00a7791dc240fa2ea7a4793b0e44f
Cody W. Opel <codyopel@gmail.com>
2017-09-28 17:16:16 -0400

    asn1crypto: 0.22.0 -> 0.23.0

--- pkgs/all-pkgs/a/asn1crypto/default.nix
+++ pkgs/all-pkgs/a/asn1crypto/default.nix
@@ -5,7 +5,7 @@
 }:
 
 let
-  version = "0.22.0";
+  version = "0.23.0";
 in
 buildPythonPackage rec {
   name = "asn1crypto-${version}";
@@ -13,7 +13,7 @@ buildPythonPackage rec {
   src = fetchPyPi {
     package = "asn1crypto";
     inherit version;
-    sha256 = "cbbadd640d3165ab24b06ef25d1dca09a3441611ac15f6a6b452474fdf0aed1a";
+    sha256 = "0874981329cfebb366d6584c3d16e913f2a0eb026c9463efcc4aaf42a9d94d70";
   };
 
   meta = with lib; {

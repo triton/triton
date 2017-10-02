2bfc4f48ac4ef8ee70437f503c1dd5285b58c116
William A. Kennington III <william@wkennington.com>
2017-10-02 13:01:03 -0700

    matrix-synapse-ldap3: 0.1.2 -> 0.1.3

--- pkgs/all-pkgs/m/matrix-synapse-ldap3/default.nix
+++ pkgs/all-pkgs/m/matrix-synapse-ldap3/default.nix
@@ -1,6 +1,6 @@
 { stdenv
 , buildPythonPackage
-, fetchFromGitHub
+, fetchPyPi
 
 , ldap3
 , service-identity
@@ -8,17 +8,15 @@
 }:
 
 let
-  version = "0.1.2";
+  version = "0.1.3";
 in
 buildPythonPackage {
   name = "matrix-synapse-ldap3-${version}";
   
-  src = fetchFromGitHub {
-    version = 2;
-    owner = "matrix-org";
-    repo = "matrix-synapse-ldap3";
-    rev = "v${version}";
-    sha256 = "b0b4d93d6b651f312cb93929f88af9fef936c900f88b1cde0a7c1dcb1151fb68";
+  src = fetchPyPi {
+    package = "matrix-synapse-ldap3";
+    inherit version;
+    sha256 = "f6cd04a901ef136cc45b50e68fadf5cf8cb947ee754166f66c4b81e8930f0d28";
   };
 
   propagatedBuildInputs = [

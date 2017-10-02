c5418ac3faed902b4e872a2cdd54be1143948ba6
William A. Kennington III <william@wkennington.com>
2017-10-02 12:52:45 -0700

    riot: 0.12.3 -> 0.12.6

--- pkgs/all-pkgs/r/riot/default.nix
+++ pkgs/all-pkgs/r/riot/default.nix
@@ -4,20 +4,30 @@
 }:
 
 let
-  version = "0.12.3";
+  version = "0.12.6";
 in
 stdenv.mkDerivation rec {
   name = "riot-${version}";
 
   src = fetchurl {
     url = "https://github.com/vector-im/riot-web/releases/download/v${version}/riot-v${version}.tar.gz";
-    sha256 = "631188da5af2ca72c2e4d1296b1901a53e9807a7a493468b8ef1e7a46c4e33ed";
+    hashOutput = false;
+    sha256 = "9bc62587818b91a02abf179ff3a388dfae1910a3a776433614c290aa2d941d02";
   };
 
   installPhase = ''
     mkdir -p "$out"/share/riot
     mv * "$out"/share/riot
   '';
+
+  passthru = {
+    srcVerification = fetchurl {
+      failEarly = true;
+      pgpsigUrls = map (n: "${n}.asc") src.urls;
+      pgpKeyFingerprint = "6FEB 6F83 D48B 9354 7E7D  FEDE E019 6452 48E8 F4A1";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
 
   meta = with lib; {
     maintainers = with maintainers; [

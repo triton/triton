ecea122062be860c1c9fbec9d8a36d24792f0cb7
William A. Kennington III <william@wkennington.com>
2017-10-10 10:44:33 -0700

    asciidoc: Fix url

--- pkgs/all-pkgs/a/asciidoc/default.nix
+++ pkgs/all-pkgs/a/asciidoc/default.nix
@@ -4,11 +4,14 @@
 , pythonPackages
 }:
 
+let
+  version = "8.6.9";
+in
 stdenv.mkDerivation rec {
-  name = "asciidoc-8.6.9";
+  name = "asciidoc-${version}";
 
   src = fetchurl {
-    url = "mirror://sourceforge/asciidoc/${name}.tar.gz";
+    url = "mirror://sourceforge/asciidoc/asciidoc/${version}/${name}.tar.gz";
     sha256 = "78db9d0567c8ab6570a6eff7ffdf84eadd91f2dfc0a92a2d0105d323cab4e1f0";
   };
 

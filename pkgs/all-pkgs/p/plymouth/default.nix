d675aceeccd203bca4e7a5a4c146de0c37f31a2a
William A. Kennington III <william@wkennington.com>
2017-10-09 19:58:13 -0700

    plymouth: 0.9.2 -> 0.9.3

--- pkgs/all-pkgs/p/plymouth/default.nix
+++ pkgs/all-pkgs/p/plymouth/default.nix
@@ -9,11 +9,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "plymouth-0.9.2";
+  name = "plymouth-0.9.3";
 
   src = fetchurl {
-    url = "https://www.freedesktop.org/software/plymouth/releases/${name}.tar.bz2";
-    sha256 = "2f0ce82042cf9c7eadd2517a1f74c8a85fa8699781d9f294a06eade29fbed57f";
+    url = "https://www.freedesktop.org/software/plymouth/releases/${name}.tar.xz";
+    multihash = "QmVttu23B6GoS2AcEwr7RKTZZUp3KetFa2FG32iDQCXFEa";
+    sha256 = "9f8dd08a90ceaf6228dcd8c27759adf18fc9482f15b6c56dcbcced268b4e4a74";
   };
 
   buildInputs = [
@@ -29,19 +30,15 @@ stdenv.mkDerivation rec {
     "--localstatedir=/var"
     "--enable-pango"
     "--enable-gtk"
-    "--enable-drm"
+    "--enable-drm-renderer"
     "--disable-documentation"
     "--disable-tracing"
     "--enable-gdm-transition"
     "--disable-upstart-monitoring"
     "--enable-systemd-integration"
+    "--with-udev"
     "--without-system-root-install"
   ];
-
-  #preBuild = ''
-  #  cat Makefile
-  #  exit 1
-  #'';
 
   preInstall = ''
     installFlagsArray+=(

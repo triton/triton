133021b151e71aec29c93de14c88068e4cc8f303
William A. Kennington III <william@wkennington.com>
2017-10-19 13:11:21 -0700

    scons: 2.5.1 -> 3.0.0

--- pkgs/all-pkgs/s/scons/default.nix
+++ pkgs/all-pkgs/s/scons/default.nix
@@ -4,15 +4,23 @@
 }:
 
 let
-  version = "2.5.1";
+  version = "3.0.0";
 in
 buildPythonPackage rec {
   name = "scons-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/scons/scons/${version}/${name}.tar.gz";
-    sha256 = "0b25218ae7b46a967db42f2a53721645b3d42874a65f9552ad16ce26d30f51f2";
+    sha256 = "0f532f405b98c60b731d231b3c503ab5bf47d89a6f66f70cb62c9249e9f45216";
   };
+
+  # Fix for python2 compatability
+  postPatch = ''
+    grep -q 'from __future__ import print_function' \
+      engine/SCons/Script/SConscript.py
+    sed -i '/from __future__ import print_function/d' \
+      engine/SCons/Script/SConscript.py
+  '';
 
   setupHook = ./setup-hook.sh;
 

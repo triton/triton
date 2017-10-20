c2cca86c83c33155f9bdf8459469364f73681ec8
Cody W. Opel <codyopel@gmail.com>
2017-10-20 12:11:32 -0400

    Revert "chromium: Updates"
    This reverts commit a9b94d524b6c5fd8aa03bf2d2c09f81083f2dd13.

--- pkgs/all-pkgs/c/chromium/sources.nix
+++ pkgs/all-pkgs/c/chromium/sources.nix
@@ -2,16 +2,16 @@
 { }:
 {
   "stable" = {
-    version = "62.0.3202.62";
-    sha256 = "e8df3150386729ddcb4971636627e54815ad447be5f122201e310f5bb0bcc362";
+    version = "61.0.3163.100";
+    sha256 = "4135968cac6623c1d2b224494600cd274098cce41c298f8c3908b354a34c281b";
   };
   "beta" = {
-    version = "62.0.3202.62";
-    sha256 = "e8df3150386729ddcb4971636627e54815ad447be5f122201e310f5bb0bcc362";
+    version = "62.0.3202.29";
+    sha256 = "c8d4485650ec31144c7e3825de8c7910c94207448df396df1cf4f978a2b01831";
   };
   "dev" = {
-    version = "63.0.3239.9";
-    sha256 = "aba3a9abf285abaf0cc85290410b01a8d4cf18f313d8c01ed1cf5e1564b2a937";
+    version = "63.0.3223.8";
+    sha256 = "bced462e16a28ff0a3896539e6c7f72e76cb7ba16640a6cac9a533ec4b8f5ee5";
   };
 }
 

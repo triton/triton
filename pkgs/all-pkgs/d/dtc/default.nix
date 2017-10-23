0f2eb1821f4ded6f7fad8d5ba93969ee5e1c1db6
William A. Kennington III <william@wkennington.com>
2017-10-23 00:15:42 -0700

    dtc: 1.4.4 -> 1.4.5

--- pkgs/all-pkgs/d/dtc/default.nix
+++ pkgs/all-pkgs/d/dtc/default.nix
@@ -1,25 +1,32 @@
 { stdenv
 , bison
-, fetchzip
+, fetchurl
 , flex
 }:
 
 let
-  version = "1.4.4";
+  version = "1.4.5";
+
+  tarballUrls = [
+    "mirror://kernel/software/utils/dtc/dtc-${version}.tar"
+  ];
 in
 stdenv.mkDerivation rec {
   name = "dtc-${version}";
 
-  src = fetchzip {
-    version = 2;
-    url = "https://git.kernel.org/pub/scm/utils/dtc/dtc.git/snapshot/v${version}.tar.xz";
-    multihash = "Qmc7rFTTDpbv62vAZk5eroWu7koMJpuiMGfDbtG43BZheb";
-    sha256 = "e2bc26528eb95e728c6b67d7f69d05eae468587498f6e18f5e731914ef0c3ebc";
+  src = fetchurl {
+    urls = map (n: "${n}.xz") tarballUrls;
+    hashOutput = false;
+    sha256 = "042c7164806af34069d13ede59d85b8156d09f179f721c516dc37712d3a0f621";
   };
 
   nativeBuildInputs = [
     flex
     bison
+  ];
+
+  makeFlags = [
+    "NO_PYTHON=yes"
   ];
 
   preInstall = ''
@@ -28,6 +35,16 @@ stdenv.mkDerivation rec {
       "PREFIX=$out"
     )
   '';
+
+  passthru = {
+    srcVerification = fetchurl {
+      failEarly = true;
+      pgpsigUrls = map (n: "${n}.sign") tarballUrls;
+      pgpDecompress = true;
+      pgpKeyFingerprint = "75F4 6586 AE61 A66C C44E  87DC 6C38 CACA 20D9 B392";
+      inherit (src) urls outputHash outputHashAlgo;
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "Device Tree Compiler";

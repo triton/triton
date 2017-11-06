3a9925ba2697def35c1d66b4b87f1539533e87f6
Cody W. Opel <codyopel@gmail.com>
2017-11-06 12:09:11 -0500

    libgdiplus: add package

---
+++ pkgs/all-pkgs/l/libgdiplus/default.nix
@@ -0,0 +1,57 @@
+{ stdenv
+, fetchurl
+, lib
+
+, cairo
+, giflib
+, glib
+, libexif
+, libpng
+, libtiff
+, libx11
+, pango
+, xproto
+, zlib
+}:
+
+stdenv.mkDerivation rec {
+  name = "libgdiplus-4.2";
+
+  src = fetchurl {
+    url = "https://download.mono-project.com/sources/libgdiplus/${name}.tar.gz";
+    sha256 = "f332b9b8b44fd1c50b8d8d01a7296360b806c790b8297614739b3de1edbadfeb";
+  };
+
+  buildInputs = [
+    cairo
+    giflib
+    glib
+    libexif
+    libpng
+    libtiff
+    libx11
+    pango
+    xproto
+    zlib
+  ];
+
+  configureFlags = [
+    "--disable-maintainer-mode"
+    "--with-pango"
+    "--with-libexif"
+    "--with-libjpeg"
+    "--with-libtiff"
+    "--with-libgif"
+  ];
+
+  meta = with lib; {
+    description = "C-based implementation of the GDI+ API";
+    homepage = https://github.com/mono/libgdiplus;
+    license = licenses.lgpl21;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = with platforms;
+      x86_64-linux;
+  };
+}

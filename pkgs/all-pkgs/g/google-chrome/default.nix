b828552cdd5960742dd1ff1f5198f5a15cff70d2
Cody W. Opel <codyopel@gmail.com>
2017-11-07 08:53:32 -0500

    Revert "google-chrome: add upower runtime dependency"
    This reverts commit f18665a96ae778e4ab869ad0e7247ef81a186083.

--- pkgs/all-pkgs/g/google-chrome/default.nix
+++ pkgs/all-pkgs/g/google-chrome/default.nix
@@ -39,7 +39,6 @@
 , pango
 , shared-mime-info
 , systemd_lib
-, upower
 , xdg-utils
 , xorg
 
@@ -205,7 +204,6 @@ stdenv.mkDerivation rec {
       --prefix 'XDG_DATA_DIRS' : "$GSETTINGS_SCHEMAS_PATH" \
       --prefix 'XDG_DATA_DIRS' : "$out/share" \
       --prefix 'XDG_DATA_DIRS' : "${shared-mime-info}/share" \
-      --prefix 'XDG_DATA_DIRS' : "${upower}/share" \
       --prefix 'XDG_DATA_DIRS' : "$XDG_ICON_DIRS"
   '';
 

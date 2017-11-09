389c92f37ca16a533d19c596500a8d25c436bf58
Cody W. Opel <codyopel@gmail.com>
2017-11-09 09:02:40 -0500

    gnome-menus: refactor

--- pkgs/all-pkgs/g/gnome-menus/default.nix
+++ pkgs/all-pkgs/g/gnome-menus/default.nix
@@ -15,7 +15,13 @@ let
   inherit (lib)
     boolEn;
 
-  source = (import ./sources.nix { })."${channel}";
+  sources = {
+    "3.13" = {
+      version = "3.13.3";
+      sha256 = "7c446eb8ff381df52b8ba04e5886497595e84d1bc46caf7af764d894736c654e";
+    };
+  };
+  source = sources."${channel}";
 in
 stdenv.mkDerivation rec {
   name = "gnome-menus-${source.version}";

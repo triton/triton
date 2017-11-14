9abeed1c0922d5b4b09cdd1fba7a10af1218554e
Cody W. Opel <codyopel@gmail.com>
2017-11-13 23:59:44 -0500

    pip(bootstrap): remove vendored dependencies

--- pkgs/all-pkgs/p/pip/bootstrap.nix
+++ pkgs/all-pkgs/p/pip/bootstrap.nix
@@ -1,60 +1,46 @@
 { stdenv
-, pythonPackages
+, fetchFromGitHub
+, python
 
+, pip
+, setuptools
 , unzip
+, wheel
 , wrapPython
 }:
 
 let
-  wheel_source = pythonPackages.fetchPyPi {
-    package = "wheel";
-    inherit (pythonPackages.wheel) version;
-    type = "-py2.py3-none-any.whl";
-    sha256 = "ea8033fc9905804e652f75474d33410a07404c1a78dd3c949a66863bd1050ebd";
-  };
-
-  setuptools_source = pythonPackages.fetchPyPi {
-    package = "setuptools";
-    inherit (pythonPackages.setuptools) version;
-    type = "-py2.py3-none-any.whl";
-    sha256 = pythonPackages.setuptools.bootstrapSha256;
-  };
-
-  pip_source = pythonPackages.fetchPyPi {
-    package = "pip";
-    inherit (pythonPackages.pip) version;
-    type = "-py2.py3-none-any.whl";
-    sha256 = "690b762c0a8460c303c089d5d0be034fb15a5ea2b75bdf565f40421f542fefb0";
-  };
+  inherit (pip)
+    version;
 in
-
 stdenv.mkDerivation rec {
-  name = "python-${pythonPackages.python.version}-pip-bootstrap-${version}";
-  inherit (pythonPackages.pip) version;
+  name = "python-${python.version}-pip-bootstrap-${version}";
 
-  src = pip_source;
+  inherit (pip) src;
 
   nativeBuildInputs = [
+    python
+    setuptools
+    wheel
     wrapPython
     unzip
   ];
 
-  unpackPhase = ''
-    mkdir -p $out/${pythonPackages.python.sitePackages}
-    unzip -d $out/${pythonPackages.python.sitePackages} $src
-    unzip -d $out/${pythonPackages.python.sitePackages} ${setuptools_source}
-    unzip -d $out/${pythonPackages.python.sitePackages} ${wheel_source}
-  '';
-
-  patchPhase = ''
-    mkdir -p $out/bin
+  buildPhase = ''
+    ${python.interpreter} setup.py bdist_wheel
   '';
 
   installPhase = ''
-    # install pip binary
-    echo '${pythonPackages.python.interpreter} -m pip "$@"' > $out/bin/pip
-    chmod +x $out/bin/pip
+    pushd dist
+      mkdir -pv $out/${python.sitePackages}
+      unzip -d $out/${python.sitePackages} pip-*.whl
+      mkdir -pv $out/bin
+      echo '${python.interpreter} -m pip "$@"' > $out/bin/pip
+      chmod +x $out/bin/pip
+    popd
+  '';
 
+  preFixup = ''
     wrapPythonPrograms $out/bin
   '';
 }

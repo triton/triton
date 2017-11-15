a4001bb064331437af809ee00ba54d06169ab274
Cody W. Opel <codyopel@gmail.com>
2017-11-14 21:04:11 -0500

    functools32: disable for Python 3.x

--- pkgs/all-pkgs/f/functools32/default.nix
+++ pkgs/all-pkgs/f/functools32/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , buildPythonPackage
 , fetchPyPi
+, isPy3
 }:
 
 let
@@ -14,6 +15,9 @@ buildPythonPackage {
     inherit version;
     sha256 = "f6253dfbe0538ad2e387bd8fdfd9293c925d63553f5813c4e587745416501e6d";
   };
+
+  # Python 3 backport to Python 2.
+  disabled = isPy3;
 
   meta = with stdenv.lib; {
     maintainers = with maintainers; [

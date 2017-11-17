baedb1a6482cd4dba882908dba18e8059bed0d85
William A. Kennington III <william@wkennington.com>
2017-11-17 14:39:48 -0800

    protobuf-cpp: 3.4.1 -> 3.5.0

--- pkgs/all-pkgs/p/protobuf-cpp/default.nix
+++ pkgs/all-pkgs/p/protobuf-cpp/default.nix
@@ -5,14 +5,14 @@
 }:
 
 let
-  version = "3.4.1";
+  version = "3.5.0";
 in
 stdenv.mkDerivation rec {
   name = "protobuf-cpp-${version}";
 
   src = fetchurl {
     url = "https://github.com/google/protobuf/releases/download/v${version}/${name}.tar.gz";
-    sha256 = "2bb34b4a8211a30d12ef29fd8660995023d119c99fbab2e5fe46f17528c9cc78";
+    sha256 = "8dd6b051c2b39ab95dfe1f53cc5e1662ceb852d856db57d85751696859698551";
   };
 
   buildInputs = [

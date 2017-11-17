f52fe25866f7c95e1dbe29f0c8bdc17d17cbf84f
Cody W. Opel <codyopel@gmail.com>
2017-11-17 15:25:23 -0500

    borgbackup: prevent build from checking system paths

--- pkgs/all-pkgs/b/borgbackup/default.nix
+++ pkgs/all-pkgs/b/borgbackup/default.nix
@@ -23,9 +23,6 @@ buildPythonPackage rec {
     sha256 = "097b2d92d51f570aaea82ab6632481b8235b78d7bbaac9d8164bdfa6bd5b5502";
   };
 
-  BORG_LZ4_PREFIX = lz4;
-  BORG_OPENSSL_PREFIX = openssl;
-
   nativeBuildInputs = [
     setuptools-scm
   ];
@@ -41,7 +38,16 @@ buildPythonPackage rec {
     llfuse
   ];
 
-  disabled = pythonOlder "3.4";
+  postPatch = ''
+    sed -i setup.py \
+      -e 's,/usr,/non-existant-path,g' \
+      -e 's,/opt,/non-existant-path,g'
+  '';
+
+  BORG_LZ4_PREFIX = lz4;
+  BORG_OPENSSL_PREFIX = openssl;
+
+  disabled = pythonOlder "3.5";
 
   meta = with stdenv.lib; {
     maintainers = with maintainers; [

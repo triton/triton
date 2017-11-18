821fc286238ca4853e975dfcaf42a36814cfa188
Cody W. Opel <codyopel@gmail.com>
2017-11-18 11:32:09 -0500

    pycparser: disable buildDirCheck

--- pkgs/all-pkgs/p/pycparser/default.nix
+++ pkgs/all-pkgs/p/pycparser/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , buildPythonPackage
 , fetchPyPi
+, lib
 }:
 
 let
@@ -8,14 +9,16 @@ let
 in
 buildPythonPackage {
   name = "pycparser-${version}";
-  
+
   src = fetchPyPi {
     package = "pycparser";
     inherit version;
     sha256 = "0aac31e917c24cb3357f5a4d5566f2cc91a19ca41862f6c3c22dc60a629673b6";
   };
 
-  meta = with stdenv.lib; {
+  buildDirCheck = false;
+
+  meta = with lib; {
     maintainers = with maintainers; [
       wkennington
     ];

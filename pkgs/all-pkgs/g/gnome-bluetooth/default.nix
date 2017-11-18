1b28a81a50aa018ec20552962426a5579e6376a6
Cody W. Opel <codyopel@gmail.com>
2017-11-18 01:54:05 -0500

    gnome-bluetooth: refactor

--- pkgs/all-pkgs/g/gnome-bluetooth/default.nix
+++ pkgs/all-pkgs/g/gnome-bluetooth/default.nix
@@ -32,7 +32,13 @@ let
     boolEn
     replaceStrings;
 
-  source = (import ./sources.nix { })."${channel}";
+  sources = {
+    "3.20" = {
+      version = "3.20.1";
+      sha256 = "ea9050b5f3b94ef279febae4cb789c2500a25344c784f650fe7f932fcd6798fe";
+    };
+  };
+  source = sources."${channel}";
 in
 stdenv.mkDerivation rec {
   name = "gnome-bluetooth-${source.version}";

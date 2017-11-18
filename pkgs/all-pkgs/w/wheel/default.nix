98e367a06340487603e1f083b46626d1273f0894
Cody W. Opel <codyopel@gmail.com>
2017-11-18 11:35:15 -0500

    wheel: remove old bootstrap hack

--- pkgs/all-pkgs/w/wheel/default.nix
+++ pkgs/all-pkgs/w/wheel/default.nix
@@ -2,14 +2,9 @@
 , buildPythonPackage
 , fetchPyPi
 , lib
-, python
 }:
 
 let
-  inherit (lib)
-    makeSearchPath
-    optionals;
-
   version = "0.30.0";
 in
 buildPythonPackage rec {
@@ -20,31 +15,6 @@ buildPythonPackage rec {
     inherit version;
     sha256 = "9515fe0a94e823fd90b08d22de45d7bde57c90edce705b22f5e1ecf7e1b653c8";
   };
-
-  installPhase = ''
-    # Unpack into a tmp directory because `pip --upgrade` will try to remove
-    # the files.
-    ${python.interpreter} -c "
-    import fnmatch
-    import os
-    import zipfile
-    for file in os.listdir('unique_dist_dir/'):
-      if fnmatch.fnmatch(file, '*.whl'):
-        zipfile.ZipFile('unique_dist_dir/' + file).extractall('bootstrap_source_unpack')
-    "
-
-    # Use --upgrade to prevent pip from failing silently due to dependency
-    # already satisfied.
-    PYTHONPATH="bootstrap_source_unpack/:$PYTHONPATH" \
-      ${python.interpreter} -m pip -v \
-        install unique_dist_dir/*.whl \
-        --upgrade \
-        --no-index \
-        --prefix="$out" \
-        --no-cache \
-        --build pipUnpackTmp \
-        --no-compile
-  '';
 
   passthru = {
     inherit version;

5727cbcd92af022e35608c1bec9e2c70c8640553
Cody W. Opel <codyopel@gmail.com>
2017-11-17 20:16:55 -0500

    setuptools(bootstrap wheel): fix duplicate sources

--- pkgs/all-pkgs/s/setuptools/bootstrap_wheel.nix
+++ pkgs/all-pkgs/s/setuptools/bootstrap_wheel.nix
@@ -1,5 +1,6 @@
 { stdenv
 , fetchPyPi
+, setuptools
 , unzip
 , wrapPython
 
@@ -7,16 +8,18 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "${python.executable}-setuptools-${version}";
-  # Make sure to update pkgs/p/pip/bootstrap.nix setuptools hash when updating
-  version = "32.3.1";
+  name = "${python.executable}-setuptools-${setuptools.version}";
+  # # Make sure to update pkgs/p/pip/bootstrap.nix setuptools hash when updating
+  # version = "32.3.1";
+  #
+  # src = fetchPyPi {
+  #   package = "setuptools";
+  #   inherit version;
+  #   type = ".zip";
+  #   sha256 = "806bae0840429c13f6e6e44499f7c0b87f3b269fdfbd815d769569c1daa7c351";
+  # };
 
-  src = fetchPyPi {
-    package = "setuptools";
-    inherit version;
-    type = ".zip";
-    sha256 = "806bae0840429c13f6e6e44499f7c0b87f3b269fdfbd815d769569c1daa7c351";
-  };
+  inherit (setuptools) meta src;
 
   nativeBuildInputs = [
     unzip
@@ -31,18 +34,7 @@ stdenv.mkDerivation rec {
     dst=$out/${python.sitePackages}
     mkdir -pv $dst
     export PYTHONPATH="$dst:$PYTHONPATH"
-    ${python.interpreter} setup.py install --prefix=$out
+    ${python.interpreter} setup.py install --prefix=$out --no-compile
     wrapPythonPrograms
   '';
-
-  meta = with stdenv.lib; {
-    description = "Utilities to facilitate the installation of Python packages";
-    homepage = http://pypi.python.org/pypi/setuptools;
-    license = licenses.zpt20;
-    maintainers = with maintainers; [
-      codyopel
-    ];
-    platforms = with platforms;
-      x86_64-linux;
-  };
 }

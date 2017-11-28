9db8ec30fbef1a4bd05559fe63953c143ea7a598
Cody W. Opel <codyopel@gmail.com>
2017-11-28 13:31:52 -0500

    libao: 1.2.0 -> 1.2.2

--- pkgs/all-pkgs/l/libao/default.nix
+++ pkgs/all-pkgs/l/libao/default.nix
@@ -1,34 +1,64 @@
 { stdenv
-, fetchurl
+, autoreconfHook
+, fetchFromGitHub
+, lib
 
 , alsa-lib
+, libice
+, libsm
+, libx11
+, libxau
 , pulseaudio_lib
-, xorg
+, xproto
 }:
 
+let
+  version = "1.2.2";
+in
 stdenv.mkDerivation rec {
-  name = "libao-1.2.0";
+  name = "libao-${version}";
 
-  src = fetchurl {
-    url = "mirror://xiph/ao/${name}.tar.gz";
-    multihash = "QmQGVMEhh5eEhJc5tQFWcDZzNf9rMwLrQxqWLVwqDtEwYo";
-    hashOutput = false;  # Hashes are at https://xiph.org/downloads/
-    sha256 = "03ad231ad1f9d64b52474392d63c31197b0bc7bd416e58b1c10a329a5ed89caf";
+  src = fetchFromGitHub {
+    version = 3;
+    owner = "xiph";
+    repo = "libao";
+    rev = "${version}";
+    sha256 = "fa6ebe7d8c84db0ffbd375bfd856ec20e12461b29902f987fdd4910caad3e19a";
   };
+
+  nativeBuildInputs = [
+    autoreconfHook
+  ];
 
   buildInputs = [
     alsa-lib
+    libice
+    libsm
+    libx11
+    libxau
     pulseaudio_lib
-    xorg.libICE
-    xorg.libX11
-    xorg.libXau
-    xorg.xproto
+    xproto
   ];
 
-  meta = with stdenv.lib; {
+  configureFlags = [
+    "--disable-maintainer-mode"
+    #"--enable-roar-default-slp"
+    "--disable-wmm"  # Windows
+    "--disable-esd"
+    "--disable-esdtest"
+    "--enable-alsa"
+    "--enable-alsa-mmap"
+    "--disable-broken-oss"
+    "--disable-arts"
+    "--disable-nas"
+    "--enable-pulse"
+  ];
+
+  meta = with lib; {
     homepage = http://xiph.org/ao/;
     license = licenses.gpl2;
     maintainers = with maintainers; [
+      codyopel
       wkennington
     ];
     platforms = with platforms;

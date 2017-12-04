00e50f316c46131037b72544cb8a083a9e8067be
William A. Kennington III <william@wkennington.com>
2017-12-03 19:26:00 -0800

    rdma-core: 14 -> 15.1

--- pkgs/all-pkgs/r/rdma-core/default.nix
+++ pkgs/all-pkgs/r/rdma-core/default.nix
@@ -9,14 +9,14 @@
 }:
 
 let
-  version = "14";
+  version = "15.1";
 in
 stdenv.mkDerivation rec {
   name = "rdma-core-${version}";
 
   src = fetchurl {
     url = "https://github.com/linux-rdma/rdma-core/releases/download/v${version}/${name}.tar.gz";
-    sha256 = "6d2202fcd5aa8f70358d99936944d3e1189a34a08861423cadcba49948fb0370";
+    sha256 = "927ee00ebb3144e19d5f9c0fa4d1be05616f59309a3e1732a24e215fd818f597";
   };
 
   nativeBuildInputs = [

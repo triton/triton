c98de4eaa082807432961bb071d8f3c0881c6c56
Cody W. Opel <codyopel@gmail.com>
2017-12-05 02:17:06 -0500

    dht: 0.24 -> 2017-09-30

--- pkgs/all-pkgs/d/dht/default.nix
+++ pkgs/all-pkgs/d/dht/default.nix
@@ -1,17 +1,19 @@
 { stdenv
 , cmake
+, fetchFromGitHub
 , ninja
-, fetchzip
 , lib
 }:
 
 stdenv.mkDerivation rec {
-  name = "dht-0.24";
+  name = "dht-2017-09-30";
 
-  src = fetchzip {
-    version = 2;
-    url = "https://github.com/jech/dht/archive/${name}.tar.gz";
-    sha256 = "a63327239eb81e97c4c78460f798de794fa910c7c1e66d7ecb7e4dc85329c06e";
+  src = fetchFromGitHub {
+    version = 3;
+    owner = "jech";
+    repo = "dht";
+    rev = "902fa6a57a5a5ae0adde7e94f4b9eb5f0b457901";
+    sha256 = "9ac0ac2a668c87a03d88cea06cf7024f3b5b87265607a6e142953aa0162e0b54";
   };
 
   postPatch = ''

cc932c17d025732928251684dfbd552331c0ce22
Cody W. Opel <codyopel@gmail.com>
2017-12-08 05:42:47 -0500

    python: cleanup

--- pkgs/all-pkgs/r/recursive-pth-loader/default.nix
+++ pkgs/all-pkgs/r/recursive-pth-loader/default.nix
@@ -1,22 +1,25 @@
-{ stdenv, python }:
+{ stdenv
+, python
+}:
 
 stdenv.mkDerivation rec {
   name = "python-recursive-pth-loader-1.0";
 
   unpackPhase = "true";
 
-  buildInputs = [ python ];
+  buildInputs = [
+    python
+  ];
 
-  patchPhase = "cat ${./sitecustomize.py} > sitecustomize.py";
+  buildPhase = "cat ${./sitecustomize.py} > sitecustomize.py";
 
-  buildPhase = "${python}/bin/${python.executable} -m compileall .";
+  #buildPhase = "${python.interpreter} -m compileall .";
 
-  installPhase =
-    ''
-      dst=$out/lib/${python.libPrefix}/site-packages
-      mkdir -p $dst
-      cp sitecustomize.* $dst/
-    '';
+  installPhase = ''
+    dst=$out/lib/${python.sitePackages}/site-packages
+    mkdir -p $dst
+    cp sitecustomize.* $dst/
+  '';
 
   meta = {
       description = "Enable recursive processing of pth files anywhere in sys.path";

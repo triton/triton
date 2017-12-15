6585a153474716c2305015f8d97fd3e2ca64d178
William A. Kennington III <william@wkennington.com>
2017-12-15 00:03:19 -0800

    spl: Update fetchzip

--- pkgs/all-pkgs/s/spl/sources.nix
+++ pkgs/all-pkgs/s/spl/sources.nix
@@ -9,7 +9,7 @@
     date = "2017-11-15";
     rev = "ed19bccfb651843fa208232b3a2d3d22a4152bc8";
     maxLinuxVersion = "4.14";
-    fetchzipVersion = 3;
-    sha256 = "a3aa120c3bb79a373e72d2c3a7f6efe52e45bc04336c641bc293807edac62779";
+    fetchzipVersion = 4;
+    sha256 = "13521520e12e41bb26718efe8c8f30bad599a55804dfaa0befad633d92927208";
   };
 }

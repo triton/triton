34ad8ab7b6bbabdbf0427db49593311b3024db8d
William A. Kennington III <william@wkennington.com>
2017-12-14 23:00:43 -0800

    icu: 60.1 -> 60.2

--- pkgs/all-pkgs/i/icu/default.nix
+++ pkgs/all-pkgs/i/icu/default.nix
@@ -10,16 +10,16 @@ let
     "http://download.icu-project.org/files/icu4c/${v}/icu4c-${replaceChars ["."] ["_"] v}-src.tgz"
   ];
 
-  version = "60.1";
+  version = "60.2";
 in
 stdenv.mkDerivation rec {
   name = "icu4c-${version}";
 
   src = fetchurl {
     urls = tarballUrls version;
-    multihash = "QmR4qHjWV6KGoQz5qGhaBeV6uX6A9GfnmkqcSzD5kqPTxd";
+    multihash = "QmRH8sAQ7AW4VqSbfeoBgN71N5SW7CjRgYTAi69QrNvBo5";
     hashOutput = false;
-    sha256 = "f8f5a6c8fbf32c015a467972bdb1477dc5f5d5dfea908b6ed218715eeb5ee225";
+    sha256 = "f073ea8f35b926d70bb33e6577508aa642a8b316a803f11be20af384811db418";
   };
 
   postUnpack = ''
@@ -36,10 +36,10 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
-      urls = tarballUrls "60.1";
+      urls = tarballUrls "60.2";
       pgpsigUrls = map (n: "${n}.asc") urls;
       pgpKeyFingerprint = "9731 166C D8E2 3A83 BEE7  C6D3 ACA5 DBE1 FD8F ABF1";
-      outputHash = "f8f5a6c8fbf32c015a467972bdb1477dc5f5d5dfea908b6ed218715eeb5ee225";
+      outputHash = "f073ea8f35b926d70bb33e6577508aa642a8b316a803f11be20af384811db418";
       inherit (src) outputHashAlgo;
     };
   };

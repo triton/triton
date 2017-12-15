9739eabbbcb05b8b8c0991073f6570269ebe4f20
William A. Kennington III <william@wkennington.com>
2017-12-14 23:34:27 -0800

    thrift: 0.10.0 -> 0.11.0

--- pkgs/all-pkgs/t/thrift/default.nix
+++ pkgs/all-pkgs/t/thrift/default.nix
@@ -8,7 +8,7 @@
 }:
 
 let
-  version = "0.10.0";
+  version = "0.11.0";
 in
 stdenv.mkDerivation rec {
   name = "thrift-${version}";
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "mirror://apache/thrift/${version}/${name}.tar.gz";
     hashOutput = false;
-    sha256 = "2289d02de6e8db04cbbabb921aeb62bfe3098c4c83f36eec6c31194301efa10b";
+    sha256 = "c4ad38b6cb4a3498310d405a91fef37b9a8e79a50cd0968148ee2524d2fa60c2";
   };
 
   buildInputs = [
@@ -28,15 +28,20 @@ stdenv.mkDerivation rec {
 
   configureFlags = [
     "--disable-tests"
+    "--enable-plugin"
     "--disable-tutorial"
-    "--disable-coverage"
   ];
 
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;
       pgpsigUrls = map (n: "${n}.asc") src.urls;
-      pgpKeyFingerprint = "9782 694B 8B54 B4AD D345  E52A BB06 368F 66B7 78F9";
+      pgpKeyFingerprints = [
+        # Jake Farrell
+        "9782 694B 8B54 B4AD D345  E52A BB06 368F 66B7 78F9"
+        # Jens Geyer
+        "8CD8 7F18 6F06 E958 EFCA  963D 76BD 340F C4B7 5865"
+      ];
       inherit (src) urls outputHash outputHashAlgo;
     };
   };

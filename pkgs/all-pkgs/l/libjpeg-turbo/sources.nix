081ba8b8943bacace31b2dfc3e175c93e4a03cb0
William A. Kennington III <william@wkennington.com>
2017-12-20 09:47:55 -0800

    libjpeg-turbo: 1.5.2 -> 1.5.3

--- pkgs/all-pkgs/l/libjpeg-turbo/sources.nix
+++ pkgs/all-pkgs/l/libjpeg-turbo/sources.nix
@@ -4,7 +4,7 @@
     sha256 = "521bb5d3043e7ac063ce3026d9a59cc2ab2e9636c655a2515af5f4706122233e";
   };
   "1.5" = {
-    versionPatch = "2";
-    sha256 = "9098943b270388727ae61de82adec73cf9f0dbb240b3bc8b172595ebf405b528";
+    versionPatch = "3";
+    sha256 = "b24890e2bb46e12e72a79f7e965f409f4e16466d00e1dd15d93d73ee6b592523";
   };
 }

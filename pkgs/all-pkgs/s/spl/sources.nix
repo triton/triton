e113aac7cccf82a76e5d30a7abfc570328ac2550
William A. Kennington III <william@wkennington.com>
2017-12-19 23:42:20 -0800

    spl: 0.7.4 -> 0.7.5

--- pkgs/all-pkgs/s/spl/sources.nix
+++ pkgs/all-pkgs/s/spl/sources.nix
@@ -1,15 +1,15 @@
 {
   "stable" = {
-    version = "0.7.4";
+    version = "0.7.5";
     maxLinuxVersion = "4.14";
-    fetchzipVersion = 4;
-    sha256 = "c78f5ef2d476fb3cac9adc87dded16f22ef55d8194867a79b9cfc4f1a77b3349";
+    fetchzipVersion = 5;
+    sha256 = "7a0cc669b792ad4c9a7009d1e6cd49cd841a7736cc067dd2a7ef4668ee3d4c26";
   };
   "dev" = {
     date = "2017-11-15";
     rev = "ed19bccfb651843fa208232b3a2d3d22a4152bc8";
     maxLinuxVersion = "4.14";
-    fetchzipVersion = 4;
+    fetchzipVersion = 5;
     sha256 = "13521520e12e41bb26718efe8c8f30bad599a55804dfaa0befad633d92927208";
   };
 }

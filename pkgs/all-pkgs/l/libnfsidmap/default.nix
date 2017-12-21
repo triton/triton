4d04b6af451b57ccebb87d953dc5b782c5bfcc2b
William A. Kennington III <william@wkennington.com>
2017-12-21 15:06:39 -0800

    libnfsidmap: 0.26 -> 0.27

--- pkgs/all-pkgs/l/libnfsidmap/default.nix
+++ pkgs/all-pkgs/l/libnfsidmap/default.nix
@@ -5,13 +5,16 @@
 , openldap
 }:
 
-stdenv.mkDerivation {
-  name = "libnfsidmap-0.26";
+let
+  version = "0.27";
+in
+stdenv.mkDerivation rec {
+  name = "libnfsidmap-${version}";
 
   src = fetchurl {
-    url = "https://fedorapeople.org/~steved/libnfsidmap/0.26/libnfsidmap-0.26.tar.bz2";
-    multihash = "QmTMDVFBZ2buVwiZkS3FJeQDB44Joie37X4yVwnaZQWd85";
-    sha256 = "391cd35a8aa48bcba1678b483c3e2525d0990eca963bb035962fcf1e3ee2a8bf";
+    url = "https://fedorapeople.org/~steved/libnfsidmap/${version}/${name}.tar.bz2";
+    multihash = "Qme1VnaXZZ3bjcEpc8mxFnpPvCZKvfh6GDHhasxkxAa1NY";
+    sha256 = "25a285b649e519e7437571f3437b837b7d2d51d6da8d6b5770950812235be22d";
   };
 
   nativeBuildInputs = [

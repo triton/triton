f99e6c4fd3489ed49194769710db2631c242d993
Cody W. Opel <codyopel@gmail.com>
2017-12-23 23:46:06 -0500

    aomedia: 2017-07-03 -> 2017-12-22

--- pkgs/all-pkgs/a/aomedia/default.nix
+++ pkgs/all-pkgs/a/aomedia/default.nix
@@ -2,36 +2,36 @@
 , cmake
 , fetchgit
 , lib
+, nasm
 , ninja
 , perl
-, yasm
 }:
 
 let
-  version = "2017-07-03";
+  version = "2017-12-22";
 in
 stdenv.mkDerivation rec {
   name = "aomedia-${version}";
 
   src = fetchgit {
-    version = 3;
+    version = 5;
     url = "https://aomedia.googlesource.com/aom";
-    rev = "7e55571ec483795c90adc1ce664f3da89c51636c";
-    sha256 = "1f1ab3900990b32a8cda3577dba962776e11ca43df2a0229fc31fa80b0e5185a";
+    rev = "94e3fe3b2f21d4c821336fd85d89bf07f4144d55";
+    sha256 = "405cfdc8393d8e34f61bd4401ee38768d98652ac65cfd2a8beb675a85b719078";
   };
 
   nativeBuildInputs = [
     cmake
+    nasm
     ninja
     perl
-    yasm
   ];
 
   cmakeFlags = [
     "-DENABLE_CCACHE=OFF"
     "-DENABLE_DISTCC=OFF"
     "-DENABLE_DOCS=OFF"
-    "-DENABLE_NASM=OFF"
+    "-DENABLE_NASM=ON"
     "-DENABLE_IDE_TEST_HOSTING=OFF"
     "-DBUILD_SHARED_LIBS=ON"
   ];

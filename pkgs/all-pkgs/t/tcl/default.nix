4ce9dc7288bb8ceffed88040058b3dbf7708c203
Cody W. Opel <codyopel@gmail.com>
2017-12-30 18:55:00 -0500

    tcl(8.6): 8.6.7 -> 8.6.8

--- pkgs/all-pkgs/t/tcl/default.nix
+++ pkgs/all-pkgs/t/tcl/default.nix
@@ -14,8 +14,8 @@ let
       sha256 = "d3f04456da873d17f02efc30734b0300fb6c3b85028d445fe284b83253a6db18";
     };
     "8.6" = {
-      version = "8.6.7";
-      sha256 = "7c6b8f84e37332423cfe5bae503440d88450da8cc1243496249faa5268026ba5";
+      version = "8.6.8";
+      sha256 = "c43cb0c1518ce42b00e7c8f6eaddd5195c53a98f94adc717234a65cbcfd3f96a";
     };
   };
   source = sources."${channel}";

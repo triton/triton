5fa068b52129846053327f637f2d773755ba99db
William A. Kennington III <william@wkennington.com>
2018-01-08 17:11:17 -0800

    pin-to-ipfs: Move can_use_ipfs to the common lib

--- maintainers/scripts/ipfs-common.sh
+++ maintainers/scripts/ipfs-common.sh
@@ -7,3 +7,23 @@ RO_GATEWAYS=(
   "https://hardbin.com"
   "https://ipfs.wak.io"
 )
+
+can_use_ipfs() {
+  if ! ipfs >/dev/null 2>&1; then
+    echo "Missing ipfs command" >&2
+    return 1
+  fi
+
+  if ! ipfs swarm peers >/dev/null 2>&1; then
+    echo "Ipfs daemon is not running" >&2
+    return 1
+  fi
+
+  if ipfs pin ls -t direct | grep -q 'api version mismatch'; then
+    echo "Got ipfs version mismatch" >&2
+    return 1
+  fi
+
+  return 0
+}
+
--- maintainers/scripts/pin-to-ipfs.sh
+++ maintainers/scripts/pin-to-ipfs.sh
@@ -5,20 +5,7 @@ set -o pipefail
 cd "$(dirname "$(readlink -f "$0")")"
 source ./ipfs-common.sh
 
-if ! ipfs >/dev/null 2>&1; then
-  echo "Missing ipfs command"
-  exit 1
-fi
-
-if ! ipfs swarm peers >/dev/null 2>&1; then
-  echo "Ipfs daemon is not running"
-  exit 1
-fi
-
-if ipfs pin ls -t direct | grep -q 'api version mismatch'; then
-  echo "Got ipfs version mismatch" >&2
-  exit 1
-fi
+can_use_ipfs
 
 cleanup() {
   CODE=$?

ef7397a58ad7999a0af15b475b3bcbb90ac356b3
William A. Kennington III <william@wkennington.com>
2018-01-08 16:14:46 -0800

    inotify-tools: 2014-11-06 -> 3.20.1

--- pkgs/all-pkgs/i/inotify-tools/default.nix
+++ pkgs/all-pkgs/i/inotify-tools/default.nix
@@ -3,24 +3,23 @@
 , fetchFromGitHub
 }:
 
+let
+  version = "3.20.1";
+in
 stdenv.mkDerivation rec {
-  name = "inotify-tools-2014-11-06";
+  name = "inotify-tools-${version}";
 
   src = fetchFromGitHub {
     version = 5;
     owner = "rvoicilas";
     repo = "inotify-tools";
-    rev = "1df9af4d6cd0f4af4b1b19254bcf056aed4ae395";
-    sha256 = "f26ea3b1b95f6ab428ee225455421c1a72532dd30d0c9845ab793624d9a88235";
+    rev = version;
+    sha256 = "e107ac8d3e8b9cd1729d35d661b9ec6d0c159aaf15e87852ce44f6b626314039";
   };
 
   nativeBuildInputs = [
     autoreconfHook
   ];
-
-  postPatch = ''
-    sed -i 's, -Werror,,g' src/Makefile.am
-  '';
 
   meta = with stdenv.lib; {
     homepage = https://github.com/rvoicilas/inotify-tools/wiki;

1b449358f1f070d5950775aa4c9d6edac3a4c562
Cody W. Opel <codyopel@gmail.com>
2018-01-21 23:39:09 -0500

    babel: 2.5.1 -> 2.5.3

--- pkgs/all-pkgs/b/babel/default.nix
+++ pkgs/all-pkgs/b/babel/default.nix
@@ -7,7 +7,7 @@
 }:
 
 let
-  version = "2.5.1";
+  version = "2.5.3";
 in
 buildPythonPackage {
   name = "Babel-${version}";
@@ -15,7 +15,7 @@ buildPythonPackage {
   src = fetchPyPi {
     package = "Babel";
     inherit version;
-    sha256 = "6007daf714d0cd5524bbe436e2d42b3c20e68da66289559341e48d2cd6d25811";
+    sha256 = "8ce4cb6fdd4393edd323227cba3a077bceb2a6ce5201c902c65e730046f41f14";
   };
 
   propagatedBuildInputs = [

483b393b5098ac9ed56154bbd3387d88484911c9
Cody W. Opel <codyopel@gmail.com>
2018-01-23 01:59:50 -0500

    sakura: 3.3.4 -> 3.5.0

--- pkgs/all-pkgs/s/sakura/default.nix
+++ pkgs/all-pkgs/s/sakura/default.nix
@@ -8,20 +8,22 @@
 , adwaita-icon-theme
 , gdk-pixbuf
 , glib
-, gtk3
+, gtk_3
+, libx11
+, shared-mime-info
 , vte
 }:
 
 let
-  version = "3.3.4";
+  version = "3.5.0";
 in
 stdenv.mkDerivation rec {
   name = "sakura-${version}";
 
   src = fetchurl {
-    url = "http://launchpad.net/sakura/trunk/${version}/+download/"
+    url = "https://launchpad.net/sakura/trunk/${version}/+download/"
       + "${name}.tar.bz2";
-    sha256 = "1fnkrkzf2ysav1ljgi4y4w8kvbwiwgmg1462xhizlla8jqa749r7";
+    sha256 = "eb30ede41083fda2b07c6a3b7393150f48867e81d28ef70b15824740cab00c3a";
   };
 
   nativeBuildInputs = [
@@ -32,15 +34,16 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     adwaita-icon-theme
-    gdk-pixbuf
     glib
-    gtk3
+    gtk_3
+    libx11
     vte
   ];
 
   preFixup = ''
     wrapProgram $out/bin/sakura \
       --set GDK_PIXBUF_MODULE_FILE "${gdk-pixbuf.loaders.cache}" \
+      --prefix XDG_DATA_DIRS : "${shared-mime-info}/share" \
       --prefix XDG_DATA_DIRS : "$XDG_ICON_DIRS"
   '';
 

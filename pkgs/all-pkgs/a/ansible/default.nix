58a09d103a84e229efc5e97987546b9e515e42b4
Cody W. Opel <codyopel@gmail.com>
2018-02-02 14:45:39 -0500

    ansible: 2.4.2.0 -> 2.4.3.0

--- pkgs/all-pkgs/a/ansible/default.nix
+++ pkgs/all-pkgs/a/ansible/default.nix
@@ -16,7 +16,7 @@
 }:
 
 let
-  version = "2.4.2.0";
+  version = "2.4.3.0";
 in
 buildPythonPackage rec {
   name = "ansible-${version}";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
   src = fetchPyPi {
     package = "ansible";
     inherit version;
-    sha256 = "315f1580b20bbc2c2f1104f8b5e548c6b4cac943b88711639c5e0d4dfc4d7658";
+    sha256 = "0e98b3a56928d03979d5f8e7ae5d8e326939111b298729b03f00b3ad8f998a3d";
   };
 
   propagatedBuildInputs = [

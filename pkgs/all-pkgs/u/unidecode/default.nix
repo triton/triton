512fbddfadf6f361df1ad46823e518bcaf44905c
Cody W. Opel <codyopel@gmail.com>
2018-02-12 19:08:22 -0500

    unidecode: 0.04.21 -> 1.0.22

--- pkgs/all-pkgs/u/unidecode/default.nix
+++ pkgs/all-pkgs/u/unidecode/default.nix
@@ -7,7 +7,7 @@
 }:
 
 let
-  version = "0.04.21";
+  version = "1.0.22";
 in
 buildPythonPackage rec {
   name = "Unidecode-${version}";
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   src = fetchPyPi {
     package = "Unidecode";
     inherit version;
-    sha256 = "280a6ab88e1f2eb5af79edff450021a0d3f0448952847cd79677e55e58bad051";
+    sha256 = "8c33dd588e0c9bc22a76eaa0c715a5434851f726131bd44a6c26471746efabf5";
   };
 
   buildInputs = [

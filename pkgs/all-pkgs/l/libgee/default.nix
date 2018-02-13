7b2a543e61cd6fc6c310b1146a1dda22ed5f96fb
Cody W. Opel <codyopel@gmail.com>
2018-02-12 19:05:56 -0500

    libgee(0.20): 0.20.0 -> 0.20.1

--- pkgs/all-pkgs/l/libgee/default.nix
+++ pkgs/all-pkgs/l/libgee/default.nix
@@ -20,8 +20,8 @@ let
       sha256 = "99686bbe5a9c89ba9502c25f61314ee8efa0dd2a1159c9a639afc1458c2839a2";
     };
     "0.20" = {
-      version = "0.20.0";
-      sha256 = "21308ba3ed77646dda2e724c0e8d5a2f8d101fb05e078975a532d7887223c2bb";
+      version = "0.20.1";
+      sha256 = "bb2802d29a518e8c6d2992884691f06ccfcc25792a5686178575c7111fea4630";
     };
   };
   source = sources."${channel}";

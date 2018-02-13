900b4a1d6c587448b2e77798e9dfcd510adae9ea
Cody W. Opel <codyopel@gmail.com>
2018-02-12 21:48:34 -0500

    msgpack-python: 0.5.1 -> 0.5.4

--- pkgs/all-pkgs/m/msgpack-python/default.nix
+++ pkgs/all-pkgs/m/msgpack-python/default.nix
@@ -5,7 +5,7 @@
 }:
 
 let
-  version = "0.5.1";
+  version = "0.5.4";
 in
 buildPythonPackage {
   name = "msgpack-python-${version}";
@@ -13,7 +13,7 @@ buildPythonPackage {
   src = fetchPyPi {
     package = "msgpack-python";
     inherit version;
-    sha256 = "69aa1eb0e13be1d3bd495ca937eae66df4431126f5cfd5491dc40370e5644853";
+    sha256 = "c1f3f8d02206f84258a3b4f99fbc0a4e3c849721c9361196c3bfd5243e4304cd";
   };
 
   meta = with lib; {

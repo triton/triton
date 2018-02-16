68748808587cdbfd32a36a291ba21507753c4e36
William A. Kennington III <william@wkennington.com>
2018-02-15 17:16:12 -0800

    wiredtiger: 2017-12-07 -> 3.0.0

--- pkgs/all-pkgs/w/wiredtiger/default.nix
+++ pkgs/all-pkgs/w/wiredtiger/default.nix
@@ -1,8 +1,5 @@
 { stdenv
-, autoconf
-, automake
-, fetchFromGitHub
-, libtool
+, fetchurl
 
 , bzip2
 , db
@@ -15,43 +12,27 @@
 }:
 
 let
-  rev = "d8f244717b6338063e0c20628bfa4bb65a821e0c";
-  date = "2017-12-06";
+  version = "3.0.0";
 in
 stdenv.mkDerivation rec {
-  name = "wiredtiger-${date}";
+  name = "wiredtiger-${version}";
 
-  src = fetchFromGitHub {
-    version = 5;
-    owner = "wiredtiger";
-    repo = "wiredtiger";
-    inherit rev;
-    sha256 = "90b9be2a1baccd57b194e51af18d58cd6edbff1173e7d1c92427967badff708d";
+  src = fetchurl {
+    url = "https://github.com/wiredtiger/wiredtiger/releases/download/${version}/${name}.tar.bz2";
+    sha256 = "a6662ecedc824ed61895c34821a1f9adbf5d3cf04630fa3d3881cb2b9573a304";
   };
-
-  nativeBuildInputs = [
-    autoconf
-    automake
-    libtool
-  ];
 
   buildInputs = [
     bzip2
     db
     gperftools
-    leveldb
     lz4
     snappy
     zlib
     zstd
   ];
 
-  preConfigure = ''
-    ./autogen.sh
-  '';
-
   configureFlags = [
-    "--enable-leveldb"
     "--enable-tcmalloc"
     "--with-builtins=lz4,snappy,zlib,zstd"
     "--with-berkeleydb=${db}"

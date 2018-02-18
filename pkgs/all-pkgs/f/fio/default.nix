0fe1b1ad8e8e7dff0092049f680de66db7296850
William A. Kennington III <william@wkennington.com>
2018-02-18 15:07:46 -0800

    fio: 3.3 -> 3.4

--- pkgs/all-pkgs/f/fio/default.nix
+++ pkgs/all-pkgs/f/fio/default.nix
@@ -12,14 +12,14 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "fio-3.3";
+  name = "fio-3.4";
 
   src = fetchFromGitHub {
     version = 5;
     owner = "axboe";
     repo = "fio";
     rev = name;
-    sha256 = "adf4c7e3f1917239b72776333d5acbd48c79b52617e5b8614e7b5a539d2e1a18";
+    sha256 = "df962c5a2e92f4172e1329e0581d8c3304fef5c22c333dc00bfde35c6669c1ee";
   };
 
   nativeBuildInputs = [

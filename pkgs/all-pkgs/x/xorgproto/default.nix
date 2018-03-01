eff618a660e9d4d17faa474b553e5004fd3f8021
Cody W. Opel <codyopel@gmail.com>
2018-03-01 14:24:24 -0500

    xorgproto: 2018.2 -> 2018.4

--- pkgs/all-pkgs/x/xorgproto/default.nix
+++ pkgs/all-pkgs/x/xorgproto/default.nix
@@ -7,12 +7,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "xorgproto-2018.2";
+  name = "xorgproto-2018.4";
 
   src = fetchurl {
     url = "mirror://xorg/individual/proto/${name}.tar.bz2";
     hashOutput = false;
-    sha256 = "9709c08b65d8371637605db97247782d1f0fa0bfd2111e37999088bb11996e64";
+    sha256 = "fee885e0512899ea5280c593fdb2735beb1693ad170c22ebcc844470eec415a0";
   };
 
   nativeBuildInputs = [
@@ -34,6 +34,7 @@ stdenv.mkDerivation rec {
       pgpsigUrls = map (n: "${n}.sig") src.urls;
       pgpKeyFingerprints = [
         # Adam Jackson
+        "DD38 563A 8A82 2453 7D1F  90E4 5B8A 2D50 A0EC D0D3"
         "995E D5C8 A613 8EB0 961F  1847 4C09 DD83 CAAA 50B2"
       ];
       failEarly = true;

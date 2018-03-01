c3151b1a613684f0ca4a6b731089bf07455a983e
Cody W. Opel <codyopel@gmail.com>
2018-03-01 18:48:36 -0500

    libqmi: 1.18.0 -> 1.20.0

--- pkgs/all-pkgs/l/libqmi/default.nix
+++ pkgs/all-pkgs/l/libqmi/default.nix
@@ -1,5 +1,6 @@
 { stdenv
 , fetchurl
+, lib
 , python
 
 , glib
@@ -8,12 +9,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "libqmi-1.18.0";
+  name = "libqmi-1.20.0";
 
   src = fetchurl {
     url = "https://www.freedesktop.org/software/libqmi/${name}.tar.xz";
     multihash = "QmZZs9nTRFqkwNHcjxU2XJuWJAF9okzF9PxBxEcnRSU93y";
-    sha256 = "a0a42c55935e75a630208e2f70840bd4407f56fe1c5258f5b0f6c0aaedf88cec";
+    sha256 = "21428cd3749c56246565123f707fee51238651a22c60bdc85ebce97388626eb4";
   };
 
   nativeBuildInputs = [
@@ -30,7 +31,7 @@ stdenv.mkDerivation rec {
     patchShebangs .
   '';
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     homepage = http://www.freedesktop.org/wiki/Software/libqmi/;
     description = "Modem protocol helper library";
     license = licenses.gpl2;

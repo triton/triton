06c6ebea1cafae0a37f4a65f68dddc5b596c361a
Cody W. Opel <codyopel@gmail.com>
2018-03-01 13:56:39 -0500

    zita-resampler: 1.3.0 -> 1.6.0

--- pkgs/all-pkgs/z/zita-resampler/default.nix
+++ pkgs/all-pkgs/z/zita-resampler/default.nix
@@ -1,17 +1,18 @@
 { stdenv
 , fetchurl
+, lib
 }:
 
 let
-  version = "1.3.0";
+  version = "1.6.0";
 in
 stdenv.mkDerivation rec {
   name = "zita-resampler-${version}";
 
   src = fetchurl {
     url = "http://kokkinizita.linuxaudio.org/linuxaudio/downloads/${name}.tar.bz2";
-    multihash = "Qmb3z4bagfp3h9zuwh1GvgRdJNrEngVZS9BvxcR4cnDv6e";
-    sha256 = "98034c8c77b03ad1093f7ca0a83ccdfad5a36040a5a95bd4dac80fa68bcf2a65";
+    multihash = "QmTgZe1MUMAgHNAhLav71jehHHLKjJ74SC5yjAS4nhQunw";
+    sha256 = "10888d76299d8072990939be45d6fc5865f5a45d766d7690819c5899d2a588f0";
   };
 
   postUnpack = ''
@@ -35,7 +36,7 @@ stdenv.mkDerivation rec {
       $out/lib/libzita-resampler.so.1
   '';
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Library for resampling audio signals";
     homepage = "http://kokkinizita.linuxaudio.org/linuxaudio/zita-resampler/resampler.html";
     license = licenses.gpl3;

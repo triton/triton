5c92c21449fd25d7ff15fbde1091fc70b1236f52
Cody W. Opel <codyopel@gmail.com>
2018-03-01 02:54:48 -0500

    cython: 0.27.3 -> 0.28b1

--- pkgs/all-pkgs/c/cython/default.nix
+++ pkgs/all-pkgs/c/cython/default.nix
@@ -1,19 +1,29 @@
 { stdenv
 , buildPythonPackage
-, fetchPyPi
+, fetchFromGitHub
+#, fetchPyPi
 , lib
 }:
 
 let
-  version = "0.27.3";
+  version = "0.28b1";
 in
 buildPythonPackage {
   name = "cython-${version}";
 
-  src = fetchPyPi {
-    package = "Cython";
-    inherit version;
-    sha256 = "6a00512de1f2e3ce66ba35c5420babaef1fe2d9c43a8faab4080b0dbcc26bc64";
+  # src = fetchPyPi {
+  #   package = "Cython";
+  #   inherit version;
+  #   sha256 = "6a00512de1f2e3ce66ba35c5420babaef1fe2d9c43a8faab4080b0dbcc26bc64";
+  # };
+
+  # Temporary until 0.28 is released, needed for python 3.7 & gevent compatibility.
+  src = fetchFromGitHub {
+    version = 5;
+    owner = "cython";
+    repo = "cython";
+    rev = version;
+    sha256 = "f73cd82d52762041f20f3cb4a217eaa1d4377b5a2c33853e7a308f861218c06c";
   };
 
   meta = with lib; {

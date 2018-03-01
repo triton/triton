a8dc879e36e56570216091746f5879e0ebf20ebb
Cody W. Opel <codyopel@gmail.com>
2018-03-01 18:45:25 -0500

    libwps: 0.4.7 -> 0.4.8

--- pkgs/all-pkgs/l/libwps/default.nix
+++ pkgs/all-pkgs/l/libwps/default.nix
@@ -8,11 +8,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "libwps-0.4.7";
+  name = "libwps-0.4.8";
 
   src = fetchurl {
     url = "mirror://sourceforge/libwps/libwps/${name}/${name}.tar.xz";
-    sha256 = "2f2cab630bceace24f9dbb7d187cd6cd1f4c9f8a7b682c5f7e49c1e2cb58b217";
+    sha256 = "e478e825ef33f6a434a19ff902c5469c9da7acc866ea0d8ab610a8b2aa94177e";
   };
 
   buildInputs = [

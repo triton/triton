700882e2dc286101ee1b8391478100d5ec2b9925
Cody W. Opel <codyopel@gmail.com>
2018-03-05 21:01:43 -0500

    nftables: 0.8.2 -> 0.8.3

--- pkgs/all-pkgs/n/nftables/default.nix
+++ pkgs/all-pkgs/n/nftables/default.nix
@@ -4,6 +4,7 @@
 , docbook_xml_dtd_45
 , fetchurl
 , flex
+, lib
 
 , gmp
 , iptables
@@ -13,13 +14,13 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "nftables-0.8.2";
+  name = "nftables-0.8.3";
 
   src = fetchurl {
     url = "http://netfilter.org/projects/nftables/files/${name}.tar.bz2";
-    multihash = "Qmf8DA2sbUY6J6WXAfaauaj1TSqj4DdrrBm9yku9D5bj1K";
+    multihash = "QmX4Q9t5gfPRzU3q97q4s6Kfdjnm4byxE1b5iVnqWmnsoF";
     hashOutput = false;
-    sha256 = "675f0aaf88f11e7eacef63dc89cb65d207d9e09c3ea6d518f0ebbb013f0767ec";
+    sha256 = "d16be1f5db88e95d29fc0b0e4df88acd079f3ee8e2b872ec7673f9a0d5d95e38";
   };
 
   nativeBuildInputs = [
@@ -58,7 +59,7 @@ stdenv.mkDerivation rec {
     };
   };
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "the project that aims to replace the existing {ip,ip6,arp,eb}tables framework";
     homepage = http://netfilter.org/projects/nftables;
     license = licenses.gpl2;

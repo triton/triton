8af764eedd2d7431e8c0ac3755b2c42cb59b8a50
Cody W. Opel <codyopel@gmail.com>
2018-03-05 21:19:09 -0500

    xcb-proto: 1.12 -> 1.13

--- pkgs/all-pkgs/x/xcb-proto/default.nix
+++ pkgs/all-pkgs/x/xcb-proto/default.nix
@@ -5,16 +5,31 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "xcb-proto-1.12";
+  name = "xcb-proto-1.13";
 
   src = fetchurl {
     url = "mirror://xorg/individual/xcb/${name}.tar.bz2";
-    sha256 = "5922aba4c664ab7899a29d92ea91a87aa4c1fc7eb5ee550325c3216c480a4906";
+    sha256 = "7b98721e669be80284e9bbfeab02d2d0d54cd11172b72271e47a2fe875e2bde1";
   };
 
   nativeBuildInputs = [
     python
   ];
+
+  passthru = {
+    srcVerification = fetchurl {
+      inherit (src)
+        outputHash
+        outputHashAlgo
+        urls;
+      pgpsigUrls = map (n: "${n}.sig") src.urls;
+      pgpKeyFingerprints = [
+        # Daniel Stone
+        "A66D 805F 7C93 29B4 C5D8  2767 CCC4 F07F AC64 1EFF"
+      ];
+      failEarly = true;
+    };
+  };
 
   meta = with lib; {
     description = "X C-language Bindings protocol headers";

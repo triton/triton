334b86999215c130db26c06303e3a520960e95aa
Cody W. Opel <codyopel@gmail.com>
2018-03-09 21:48:20 -0500

    vala: fix make flag being passed to ninja

--- pkgs/all-pkgs/v/vala/setup-hook.sh
+++ pkgs/all-pkgs/v/vala/setup-hook.sh
@@ -1,3 +1,6 @@
-installFlagsArray+=(
-  "vapidir=${out}/share/vala/vapi"
-)
+# FIXME: Use hack until makeFlags and ninjaFlags are completely separated.
+if [ -n "${setVapidirInstallFlag-true}" ]; then
+  installFlagsArray+=(
+    "vapidir=${out}/share/vala/vapi"
+  )
+fi

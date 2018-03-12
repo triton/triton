01994d21d18756124f1d30e573dd2b0c42992f2b
William A. Kennington III <william@wkennington.com>
2018-03-12 13:17:30 -0700

    spl_dev: Supports kernel 4.16

--- pkgs/all-pkgs/s/spl/sources.nix
+++ pkgs/all-pkgs/s/spl/sources.nix
@@ -8,7 +8,7 @@
   "dev" = {
     date = "2018-02-24";
     rev = "3673d032850c3b54b8b2cc74cf1782a75cc9b2a9";
-    maxLinuxVersion = "4.15";
+    maxLinuxVersion = "4.16";
     fetchzipVersion = 5;
     sha256 = "a61e53c2ae0e38378d1f8065da30b63707b187080a490efffd51c89856609c51";
   };

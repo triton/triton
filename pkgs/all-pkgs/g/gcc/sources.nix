93fa42dc2a6dd20b03142f1be4adfa80ee2fdd8a
William A. Kennington III <william@wkennington.com>
2018-03-13 15:16:00 -0700

    Fixes

--- pkgs/all-pkgs/g/gcc/sources.nix
+++ pkgs/all-pkgs/g/gcc/sources.nix
@@ -5,14 +5,19 @@
     sha256 = "832ca6ae04636adbb430e865a1451adf6979ab44ca1c8374f61fba65645ce15c";
     patches = [
       (fetchTritonPatch {
-        rev = "8d29376d9dbe106435e0f58523fef8617da47972";
-        file = "g/gcc/0001-libcpp-Remove-path-impurities.7.1.0.patch";
-        sha256 = "10ed16616d7ed59d4c215367a63b6a1646b8be94be81737bd48403f6ff26d083";
+        rev = "8e60932cc6002332b7d031568bdb72ec2b494f1c";
+        file = "g/gcc/0001-libcpp-Determine-if-we-should-enforce-purity.7.3.0.patch";
+        sha256 = "f09d4135ed4b3a6ec5e82334667d1925ac65fc0ceecd0875ecca2c747cb0375e";
       })
       (fetchTritonPatch {
-        rev = "8d29376d9dbe106435e0f58523fef8617da47972";
-        file = "g/gcc/0002-libcpp-Enforce-purity-for-time-functions.7.1.0.patch";
-        sha256 = "616d16c4586a6ae4823a2e780a0655bf45f07caaacdc5886b73b41a3f5b9ab3d";
+        rev = "8e60932cc6002332b7d031568bdb72ec2b494f1c";
+        file = "g/gcc/0002-libcpp-Remove-path-impurities.7.3.0.patch";
+        sha256 = "5b1350e80ff5d661dbcfb46ad9b429bb0d9de7ade9071fc4ad5f56f86cc9335f";
+      })
+      (fetchTritonPatch {
+        rev = "8e60932cc6002332b7d031568bdb72ec2b494f1c";
+        file = "g/gcc/0003-libcpp-Enforce-purity-for-time-functions.7.3.0.patch";
+        sha256 = "7bea633c39650f2d3434f7cff52641ad6230989b98bd20ee908cc27250ce6572";
       })
     ];
   };
--- pkgs/stdenv/generic/lib/00-preamble.sh
+++ pkgs/stdenv/generic/lib/00-preamble.sh
@@ -1,5 +1,2 @@
 set -e
 set -o pipefail
-
-# All builds should guarantee pure build outputs
-export NIX_ENFORCE_PURITY=1

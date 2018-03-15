246176ca1fe74b4c6d6ad5b1d62b0cac088446dc
Cody W. Opel <codyopel@gmail.com>
2018-03-15 06:18:18 -0400

    cython: 2018-03-03 -> 0.28

--- pkgs/all-pkgs/c/cython/default.nix
+++ pkgs/all-pkgs/c/cython/default.nix
@@ -1,29 +1,19 @@
 { stdenv
 , buildPythonPackage
-, fetchFromGitHub
-#, fetchPyPi
+, fetchPyPi
 , lib
 }:
 
 let
-  version = "2018-03-03";
+  version = "0.28";
 in
 buildPythonPackage {
   name = "cython-${version}";
 
-  # src = fetchPyPi {
-  #   package = "Cython";
-  #   inherit version;
-  #   sha256 = "6a00512de1f2e3ce66ba35c5420babaef1fe2d9c43a8faab4080b0dbcc26bc64";
-  # };
-
-  # Temporary until 0.28 is released, needed for python 3.7 & gevent compatibility.
-  src = fetchFromGitHub {
-    version = 5;
-    owner = "cython";
-    repo = "cython";
-    rev = "e8d309655fe0b80d8e10e35b852ccf9bf3045483";
-    sha256 = "0d9ed34ae5f3eb73950fd8df6eb4b8db3246e8f6318af7287a9f6ac82e248d3b";
+  src = fetchPyPi {
+    package = "Cython";
+    inherit version;
+    sha256 = "518f7e22da54109661e483a91a63045203caf9fd78da4a69185a7622f759965f";
   };
 
   meta = with lib; {
@@ -31,6 +21,7 @@ buildPythonPackage {
     homepage = http://cython.org;
     license = licenses.asl20;
     maintainers = with maintainers; [
+      codyopel
       wkennington
     ];
     platforms = with platforms;

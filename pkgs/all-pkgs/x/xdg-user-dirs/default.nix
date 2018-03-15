ab0e05a8691349a2b65dd8e9b52b8fde4664d609
Cody W. Opel <codyopel@gmail.com>
2018-03-15 06:08:44 -0400

    xdg-user-dirs: 0.15 -> 0.17

--- pkgs/all-pkgs/x/xdg-user-dirs/default.nix
+++ pkgs/all-pkgs/x/xdg-user-dirs/default.nix
@@ -1,17 +1,18 @@
 { stdenv
 , docbook-xsl
 , fetchurl
+, lib
 , libxslt
 , makeWrapper
 }:
 
 stdenv.mkDerivation rec {
-  name = "xdg-user-dirs-0.15";
+  name = "xdg-user-dirs-0.17";
 
   src = fetchurl {
-    url = "http://user-dirs.freedesktop.org/releases/${name}.tar.gz";
-    multihash = "QmTS4GA6yAUYFDQhV27FZ131qMrN7aMs6NKuFVZfa4gFNp";
-    sha256 = "20b4a751f41d0554bce3e0ce5e8d934be98cc62d48f0b90a894c3e1916552786";
+    url = "https://user-dirs.freedesktop.org/releases/${name}.tar.gz";
+    multihash = "QmT2dLoRbs3NSta4D2Ttv5MuHujjAaD3qmySzbtbLNtrUT";
+    sha256 = "2a07052823788e8614925c5a19ef5b968d8db734fdee656699ea4f97d132418c";
   };
 
   nativeBuildInputs = [
@@ -25,7 +26,7 @@ stdenv.mkDerivation rec {
       --prefix XDG_CONFIG_DIRS : "$out/etc/xdg"
   '';
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "A tool to help manage well known user directories";
     homepage = http://freedesktop.org/wiki/Software/xdg-user-dirs;
     license = licenses.gpl2;
@@ -33,7 +34,6 @@ stdenv.mkDerivation rec {
       codyopel
     ];
     platforms = with platforms;
-      i686-linux
-      ++ x86_64-linux;
+      x86_64-linux;
   };
 }

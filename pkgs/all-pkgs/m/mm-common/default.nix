0da3893a6c1b4e6804184473aca57a06daff3f9f
Cody W. Opel <codyopel@gmail.com>
2018-03-19 21:17:04 -0400

    mm-common: 0.9.10 -> 0.9.11

--- pkgs/all-pkgs/m/mm-common/default.nix
+++ pkgs/all-pkgs/m/mm-common/default.nix
@@ -1,31 +1,31 @@
 { stdenv
 , fetchurl
+, lib
 }:
 
 let
-  major = "0.9";
-  patch = "10";
-
-  version = "${major}.${patch}";
+  channel = "0.9";
+  version = "${channel}.11";
 in
 stdenv.mkDerivation rec {
   name = "mm-common-${version}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/mm-common/${major}/${name}.tar.xz";
+    url = "mirror://gnome/sources/mm-common/${channel}/${name}.tar.xz";
     hashOutput = false;
-    sha256 = "16c0e2bc196b67fbc145edaecb5dbe5818386504fe5703de27002d77140fa217";
+    sha256 = "20d1e7466ca4c83c92e29f9e8dfcc8e5721fdf1337f53157bed97be3b73b32a8";
   };
 
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;
-      sha256Url = "https://download.gnome.org/sources/mm-common/${major}/${name}.sha256sum";
+      sha256Url = "https://download.gnome.org/sources/mm-common/${channel}/"
+        + "${name}.sha256sum";
       inherit (src) urls outputHash outputHashAlgo;
     };
   };
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     maintainers = with maintainers; [
       wkennington
     ];

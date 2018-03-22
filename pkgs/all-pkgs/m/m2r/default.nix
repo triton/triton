a50dbe95e56dc2b88138119d72d09f6ed6a4117d
William A. Kennington III <william@wkennington.com>
2018-03-22 11:13:08 -0700

    m2r: 0.1.13 -> 0.1.14

--- pkgs/all-pkgs/m/m2r/default.nix
+++ pkgs/all-pkgs/m/m2r/default.nix
@@ -8,7 +8,7 @@
 }:
 
 let
-  version = "0.1.13";
+  version = "0.1.14";
 in
 buildPythonPackage {
   name = "m2r-${version}";
@@ -16,7 +16,7 @@ buildPythonPackage {
   src = fetchPyPi {
     package = "m2r";
     inherit version;
-    sha256 = "b19e3703a3a897859f01ff6a068ee9a0eea8e8fdf75e896e00e88b3476a12f87";
+    sha256 = "a14635cdeedb125f0f85e014eb5898fd634e2da358a160c124818e9c9f851add";
   };
 
   buildInputs = [

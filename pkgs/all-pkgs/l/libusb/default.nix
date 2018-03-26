c5485b5de832bb27fe80c7b9e205a10e4cb6d186
William A. Kennington III <william@wkennington.com>
2018-03-26 15:15:53 -0700

    libusb: 1.0.21 -> 1.0.22

--- pkgs/all-pkgs/l/libusb/default.nix
+++ pkgs/all-pkgs/l/libusb/default.nix
@@ -8,7 +8,7 @@ let
   inherit (stdenv.lib)
     optional;
 
-  version = "1.0.21";
+  version = "1.0.22";
 in
 stdenv.mkDerivation rec {
   name = "libusb-${version}";
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "https://github.com/libusb/libusb/releases/download/v${version}/"
       + "${name}.tar.bz2";
-    sha256 = "7dce9cce9a81194b7065ee912bcd55eeffebab694ea403ffb91b67db66b1824b";
+    sha256 = "75aeb9d59a4fdb800d329a545c2e6799f732362193b465ea198f2aa275518157";
   };
 
   buildInputs = [
@@ -24,15 +24,8 @@ stdenv.mkDerivation rec {
   ];
 
   configureFlags = [
-    "--disable-maintainer-mode"
     "--enable-udev"
-    "--disable-usbdk"
-    #"--enable-timerfd"
-    "--enable-log"
-    "--disable-debug-log"
-    "--enable-system-log"
-    "--disable-examples-build"
-    "--disable-tests-build"
+    "--enable-timerfd"
   ];
 
   NIX_LDFLAGS = [

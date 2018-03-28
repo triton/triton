663b4e2dc1d0e8c3d6d7f8f2aba552c28cc6bf9c
William A. Kennington III <william@wkennington.com>
2018-03-28 15:13:19 -0700

    lxc: 2.1.1 -> 3.0.0

--- pkgs/all-pkgs/l/lxc/default.nix
+++ pkgs/all-pkgs/l/lxc/default.nix
@@ -1,55 +1,54 @@
 { stdenv
-, autoreconfHook
 , fetchurl
 , docbook2x
 , docbook_xml_dtd_45
 
-, cgmanager
-, dbus
 , gnutls
 , libcap
-, libnih
 , libselinux
 , libseccomp
+, pam
 }:
 
 let
-  version = "2.1.1";
+  version = "3.0.0";
 in
 stdenv.mkDerivation rec {
   name = "lxc-${version}";
 
   src = fetchurl {
     url = "https://linuxcontainers.org/downloads/lxc/lxc-${version}.tar.gz";
-    multihash = "QmS6WAPq5RrvtWuv2c49vzp1WdvE2QUSJdL4tymBcyB6sZ";
+    multihash = "QmSDiZSpg9GPHa9eXVYoSrwbWU7KLKEDyN3ix87j3axwQk";
     hashOutput = false;
-    sha256 = "68663a67450a8d6734e137eac54cc7077209fb15c456eec401a2c26e6386eff6";
+    sha256 = "6230224c27f050201b372b18a9f39cd220ed584899c5f0cf73c6b313dabc8d8a";
   };
 
   nativeBuildInputs = [
-    autoreconfHook
     docbook2x
   ];
 
   buildInputs = [
-    cgmanager
-    dbus
     gnutls
     libcap
-    libnih
     libseccomp
     libselinux
+    pam
   ];
 
   XML_CATALOG_FILES = "${docbook_xml_dtd_45}/xml/dtd/docbook/catalog.xml";
 
-  patches = [
-    ./support-db2x.patch
-  ];
-
   postPatch = ''
-    # Can't setuid in a nixbuild
-    sed -i 's,chmod u+s.*,true,' src/lxc/Makefile.{am,in}
+    # We never want to build static binaries
+    grep -q '@HAVE_STATIC_LIBCAP_TRUE@.*init.lxc.static' src/lxc/Makefile.in
+    sed -i 's,@HAVE_STATIC_LIBCAP_TRUE@,#,' src/lxc/Makefile.in
+
+    # Never setuid
+    grep -q 'chmod u+s' src/lxc/Makefile.in
+    sed -i '/chmod u+s/d' src/lxc/Makefile.in
+
+    # Fix docbook usage
+    grep -q 'Davenport' configure
+    sed -i 's,xdocbook2man,xno-such-program,' configure
   '';
 
   configureFlags = [
@@ -59,29 +58,30 @@ stdenv.mkDerivation rec {
     "--enable-rpath"
     "--enable-doc"
     "--disable-api-docs"
-    "--with-init-script=none"
-    "--with-distro=nixos" # just to be sure it is "unknown"
     "--disable-apparmor"
     "--enable-gnutls"
     "--enable-selinux"
     "--enable-seccomp"
-    "--enable-cgmanager"
     "--enable-capabilities"
     "--disable-examples"
-    "--disable-python"
-    "--disable-lua"
     "--disable-bash"
+    "--enable-pam"
     "--with-rootfs-path=/var/lib/lxc/rootfs"
+    "--with-init-script=systemd"
+    "--with-distro=triton" # just to be sure it is "unknown"
   ];
 
-  installFlags = [
-    "localstatedir=\${TMPDIR}"
-    "sysconfdir=\${out}/etc"
-    "sysconfigdir=\${out}/etc/default"
-    "bashcompdir=\${out}/share/bash_completion.d" # FIXME
-    "READMEdir=\${TMPDIR}/var/lib/lxc/rootfs"
-    "LXCPATH=\${TMPDIR}/var/lib/lxc"
-  ];
+  preInstall = ''
+    installFlagsArray+=(
+      "sysconfdir=$out/etc"
+      "sysconfigdir=$out/etc/default"
+      "configdir=$out/etc/lxc"
+      "localstatedir=$TMPDIR"
+      "SYSTEMD_UNIT_DIR=$out/lib/systemd/system"
+      "READMEdir=$TMPDIR"
+      "LXCPATH=$TMPDIR"
+    )
+  '';
 
   passthru = {
     srcVerification = fetchurl rec {

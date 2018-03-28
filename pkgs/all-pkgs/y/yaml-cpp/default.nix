b0ab924334535678de077ef602c256cb3a2462a5
William A. Kennington III <william@wkennington.com>
2018-03-28 11:09:35 -0700

    yaml-cpp: 0.6.1 -> 0.6.2

--- pkgs/all-pkgs/y/yaml-cpp/default.nix
+++ pkgs/all-pkgs/y/yaml-cpp/default.nix
@@ -5,17 +5,17 @@
 }:
 
 let
-  version = "0.6.1";
+  version = "0.6.2";
 in
 stdenv.mkDerivation rec {
   name = "yaml-cpp-${version}";
 
   src = fetchFromGitHub {
-    version = 5;
+    version = 6;
     owner = "jbeder";
     repo = "yaml-cpp";
     rev = "yaml-cpp-${version}";
-    sha256 = "264e3e3916509d5d2f32125dd0f884d2e35f15bdfbf914fe13ec8952f62d3713";
+    sha256 = "aaad876425ad99e1dd1aecf55a7b22a5c8bb5efab9739e89f39f98627f7d927a";
   };
 
   nativeBuildInputs = [

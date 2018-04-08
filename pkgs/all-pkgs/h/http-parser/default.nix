1846b2a0460a9fb614861399bcd540a3f5e6497a
William A. Kennington III <william@wkennington.com>
2018-04-08 16:34:49 -0700

    http-parser: 2018-02-09 -> 2018-03-30

--- pkgs/all-pkgs/h/http-parser/default.nix
+++ pkgs/all-pkgs/h/http-parser/default.nix
@@ -4,18 +4,18 @@
 }:
 
 let
-  date = "2018-02-09";
-  rev = "dd74753cf5cf8944438d6f49ddf46f9659993dfb";
+  date = "2018-03-30";
+  rev = "54f55a2f02a823e5f5c87abe853bb76d1170718d";
 in
 stdenv.mkDerivation {
   name = "http-parser-${date}";
 
   src = fetchFromGitHub {
-    version = 5;
+    version = 6;
     owner = "nodejs";
     repo = "http-parser";
     inherit rev;
-    sha256 = "91a55f9b7449d20d9357e9898b38b89a05ffd43ef5b71a188597eea77730b47e";
+    sha256 = "be29d861db71cbdb8e9723abee049945aa34b37104aa7237d192b363eb134e63";
   };
 
   preBuild = ''

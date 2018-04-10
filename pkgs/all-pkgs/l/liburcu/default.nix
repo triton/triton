2ea644d53b8fe16554a938edce39f6930f478c76
William A. Kennington III <william@wkennington.com>
2018-04-10 15:24:47 -0700

    liburcu: 0.9.3 -> 0.10.1

--- pkgs/all-pkgs/l/liburcu/default.nix
+++ pkgs/all-pkgs/l/liburcu/default.nix
@@ -3,21 +3,22 @@
 }:
 
 let
-  version = "0.9.3";
+  version = "0.10.1";
 in
 stdenv.mkDerivation rec {
   name = "liburcu-${version}";
 
   src = fetchurl {
-    url = "http://lttng.org/files/urcu/userspace-rcu-${version}.tar.bz2";
-    multihash = "QmYYrSQQYiHAzpo9BN4e88otE3F4ETRgbmvQC656856c2G";
+    url = "https://lttng.org/files/urcu/userspace-rcu-${version}.tar.bz2";
+    multihash = "Qmcu4rhao4yvbSvcodKsreGa38P7SSzHSZuXiBjd1hsKoY";
     hashOutput = false;
-    sha256 = "1bce32e6a6c967fef6d37adaadf33df19878d69673f9ef9d3f2470e0c6ed4006";
+    sha256 = "9c09220be4435dc27fcd22d291707b94b97f159e0c442fbcd60c168f8f79eb06";
   };
 
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;
+      md5Urls = map (n: "${n}.md5") src.urls;
       sha1Urls = map (n: "${n}.sha1") src.urls;
       pgpsigUrls = map (n: "${n}.asc") src.urls;
       pgpKeyFingerprint = "2A0B 4ED9 15F2 D3FA 45F5  B162 1728 0A97 8118 6ACF";

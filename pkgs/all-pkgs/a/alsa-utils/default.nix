0e244a7f78d410c9cf97b504dfcbf71321f284c2
William A. Kennington III <william@wkennington.com>
2018-04-10 00:19:26 -0700

    alsa-utils: 1.1.5 -> 1.1.6

--- pkgs/all-pkgs/a/alsa-utils/default.nix
+++ pkgs/all-pkgs/a/alsa-utils/default.nix
@@ -1,54 +1,45 @@
 { stdenv
-, docbook_xml_dtd_42
-, docbook-xsl
 , fetchurl
-, gettext
 , lib
-, libxslt
-, xmlto
 
 , alsa-lib
 , fftw_single
 , libsamplerate
 , ncurses
-, systemd_lib
+, systemd-dummy
 }:
 
 stdenv.mkDerivation rec {
-  name = "alsa-utils-1.1.5";
+  name = "alsa-utils-1.1.6";
 
   src = fetchurl {
     url = "mirror://alsa/utils/${name}.tar.bz2";
-    multihash = "QmaKgJYXDqGWmyDmLUyqLS83j8rUXrtPL6NmwfRNVqGkrN";
-    sha256 = "320bd285e91db6e7fd7db3c9ec6f55b02f35449ff273c7844780ac6a5a3de2e8";
+    multihash = "QmT8tfm2fMu3smutKygap17rgQfdXHRta2udkRcCDzQ3m8";
+    sha256 = "155caecc40b2220f686f34ba3655a53e3bdbc0586adb1056733949feaaf7d36e";
   };
-
-  nativeBuildInputs = [
-    docbook_xml_dtd_42
-    docbook-xsl
-    gettext
-    libxslt
-    xmlto
-  ];
 
   buildInputs = [
     alsa-lib
     fftw_single
     libsamplerate
     ncurses
+    systemd-dummy
   ];
-
-  preConfigure = ''
-    configureFlagsArray+=("--with-udev-rules-dir=$out/lib/udev/rules.d")
-  '';
 
   configureFlags = [
     "--sysconfdir=/etc"
     "--localstatedir=/var"
+    "--disable-alsatest"  # This is just a test case program
+    "--disable-alsaconf"  # Not needed with udev
+    "--disable-xmlto"     # Man pages are pre-generated
+    "--disable-rst2man"   # Man pages are pre-generated
   ];
 
   preInstall = ''
-    installFlagsArray+=("ASOUND_STATE_DIR=$TMPDIR")
+    installFlagsArray+=(
+      "systemdsystemunitdir=$out/lib/systemd/system"
+      "ASOUND_STATE_DIR=$TMPDIR"
+    )
   '';
 
   meta = with lib; {

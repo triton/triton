fb1e5dd452ae685251aaae92e0abdbfb31139ab7
William A. Kennington III <william@wkennington.com>
2018-04-15 00:28:19 -0700

    wlc: 2017-10-14 -> 2017-12-28

--- pkgs/all-pkgs/w/wlc/default.nix
+++ pkgs/all-pkgs/w/wlc/default.nix
@@ -20,18 +20,18 @@
 }:
 
 let
-  date = "2017-10-14";
-  rev = "e3abf0b2f322b9d376ced452190cd4dd9f6fcb8a";
+  date = "2017-12-28";
+  rev = "6542c16652df147523245fc547d2a5ff4088a0cb";
 in
 stdenv.mkDerivation rec {
   name = "wlc-${date}";
 
   src = fetchFromGitHub {
-    version = 3;
+    version = 6;
     owner = "Cloudef";
     repo = "wlc";
     inherit rev;
-    sha256 = "54aa8a91f26fad6c30ce2f26484b18a40b20aac86de24814b964e7b3d81c8fa8";
+    sha256 = "d1b00ef6e9d8a1e5dca095ac623b6daaa3715e0b2dd59e1a805216344bd927a9";
   };
 
   nativeBuildInputs = [

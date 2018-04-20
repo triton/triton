c62234c135d3897cd687c4e8fc9f8fb1096245d9
William A. Kennington III <william@wkennington.com>
2018-04-19 23:58:07 -0700

    libffi: Add multihash

--- pkgs/all-pkgs/l/libffi/default.nix
+++ pkgs/all-pkgs/l/libffi/default.nix
@@ -7,6 +7,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "ftp://sourceware.org/pub/libffi/${name}.tar.gz";
+    multihash = "QmYVaqFAiZJy8YHrRYLCtBZzhNGxLJRBxR2Pkk1hm4ZMkN";
     sha256 = "0dya49bnhianl0r65m65xndz6ls2jn1xngyn72gd28ls3n7bnvnh";
   };
 

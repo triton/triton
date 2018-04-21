8642075faa5c13c944c6457414462496532237e3
William A. Kennington III <william@wkennington.com>
2018-04-20 18:45:00 -0700

    llfuse: 1.3.2 -> 1.3.3

--- pkgs/all-pkgs/l/llfuse/default.nix
+++ pkgs/all-pkgs/l/llfuse/default.nix
@@ -10,7 +10,7 @@
 }:
 
 let
-  version = "1.3.2";
+  version = "1.3.3";
 in
 buildPythonPackage {
   name = "llfuse-${version}";
@@ -19,7 +19,7 @@ buildPythonPackage {
     package = "llfuse";
     inherit version;
     type = ".tar.bz2";
-    sha256 = "96252a286a2be25810904d969b330ef2a57c2b9c18c5b503bbfbae40feb2bb63";
+    sha256 = "e514fa390d143530c7395f640c6b527f4f80b03f90995c7b38ff0b2f86e11ce7";
   };
 
   nativeBuildInputs = [

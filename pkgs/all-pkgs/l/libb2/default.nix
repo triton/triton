9181bfbd86fa2d383aa34aeeec3ed40d9ec644fe
William A. Kennington III <william@wkennington.com>
2018-04-20 18:44:49 -0700

    libb2: 2017-12-25 -> 2018-03-27

--- pkgs/all-pkgs/l/libb2/default.nix
+++ pkgs/all-pkgs/l/libb2/default.nix
@@ -5,18 +5,18 @@
 }:
 
 let
-  rev = "60ea749837362c226e8501718f505ab138e5c19d";
-  date = "2017-12-25";
+  rev = "e5f2bb51e580fd9e42da86d8943a35af3f19371e";
+  date = "2018-03-27";
 in
 stdenv.mkDerivation {
   name = "libb2-${date}";
 
   src = fetchFromGitHub {
-    version = 5;
+    version = 6;
     owner = "BLAKE2";
     repo = "libb2";
     inherit rev;
-    sha256 = "a5cda2c53afc336ed8fb85ac1d6c46c39740b981e30ad60c26cb1ea5cc44d991";
+    sha256 = "277e5ce9ef1ce40f21180ab264f10220b47f4394bf6042b0eec34a943ad10c0c";
   };
 
   nativeBuildInputs = [

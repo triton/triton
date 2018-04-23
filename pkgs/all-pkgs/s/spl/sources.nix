0cce224b68b437fbf94a4a8540f18380751ac4cb
William A. Kennington III <william@wkennington.com>
2018-04-22 20:54:21 -0700

    zfs/spl_dev: Allow kernel 4.17

--- pkgs/all-pkgs/s/spl/sources.nix
+++ pkgs/all-pkgs/s/spl/sources.nix
@@ -7,7 +7,7 @@
   "dev" = {
     date = "2018-04-09";
     rev = "9125f8f5bdb36bfbd2d816d30b6b29b9f89ae3d8";
-    maxLinuxVersion = "4.16";
+    maxLinuxVersion = "4.17";
     fetchzipVersion = 6;
     sha256 = "4d50628466e8997a27a8a1632da4ac4bbd5e468aa904cfc4a6e53717ff2f69f5";
   };
--- pkgs/all-pkgs/z/zfs/sources.nix
+++ pkgs/all-pkgs/z/zfs/sources.nix
@@ -7,7 +7,7 @@
   "dev" = {
     date = "2018-04-19";
     rev = "b4555c777a0be3c0dba29662d278c57099c60a87";
-    maxLinuxVersion = "4.16";
+    maxLinuxVersion = "4.17";
     fetchzipVersion = 6;
     sha256 = "fd13d59cd26393dd4c9a001ebe18e95b7d84794aea59099bb1784c30390cbec0";
   };

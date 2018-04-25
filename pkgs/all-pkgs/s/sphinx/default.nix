c6999b2fb772eb9d1bf0cded983f1367ff102f8f
William A. Kennington III <william@wkennington.com>
2018-04-24 17:08:09 -0700

    sphinx: 1.7.2 -> 1.7.3

--- pkgs/all-pkgs/s/sphinx/default.nix
+++ pkgs/all-pkgs/s/sphinx/default.nix
@@ -16,7 +16,7 @@
 }:
 
 let
-  version = "1.7.2";
+  version = "1.7.3";
 in
 buildPythonPackage {
   name = "Sphinx-${version}";
@@ -24,7 +24,7 @@ buildPythonPackage {
   src = fetchPyPi {
     package = "Sphinx";
     inherit version;
-    sha256 = "5a1c9a0fec678c24b9a2f5afba240c04668edb7f45c67ce2ed008996b3f21ae2";
+    sha256 = "9495a1f78c13d0a725ab8104e923e9663519ecc04552aa4a8f684c2da355443d";
   };
 
   propagatedBuildInputs = [

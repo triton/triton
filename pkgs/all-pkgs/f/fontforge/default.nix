6c8008402967ea1e223ff24d9daf1ac5f291333a
William A. Kennington III <william@wkennington.com>
2018-04-25 23:54:19 -0700

    fontforge: Cleanup

--- pkgs/all-pkgs/f/fontforge/default.nix
+++ pkgs/all-pkgs/f/fontforge/default.nix
@@ -1,24 +1,22 @@
 { stdenv
-, autoconf
-, automake
-, fetchzip
-, fetchpatch
+, fetchurl
 , gettext
-, gnulib
-, gnum4
 , lib
-, libtool
-, perl
 
+, cairo
 , freetype
 , giflib
 , glib
 , libjpeg
 , libpng
+, libspiro
 , libtiff
+, libtool
+, libuninameslist
 , libxml2
 , readline
 , uthash
+, zeromq
 , zlib
 }:
 
@@ -28,31 +26,14 @@ in
 stdenv.mkDerivation rec {
   name = "fontforge-${version}";
 
-  src = fetchzip {
-    version = 3;
-    url = "https://github.com/fontforge/fontforge/archive/${version}.tar.gz";
-    sha256 = "4e854db895ae1386a12a60af224d072664e3dc7b5d75145edc39566302730332";
+  src = fetchurl {
+    url = "https://github.com/fontforge/fontforge/releases/download/"
+      + "${version}/fontforge-dist-${version}.tar.xz";
+    sha256 = "840adefbedd1717e6b70b33ad1e7f2b116678fa6a3d52d45316793b9fd808822";
   };
 
-  patches = [
-    (fetchpatch {
-      name = "use-system-uthash.patch";
-      url = "http://pkgs.fedoraproject.org/cgit/fontforge.git/plain/"
-        + "fontforge-20140813-use-system-uthash.patch?id=8bdf933";
-      multihash = "QmcmrPTJafenDXyxb27fUwp4z9gQuwUFUyDQyH7xRmYiLP";
-      sha256 = "0n8i62qv2ygfii535rzp09vvjx4qf9zp5qq7qirrbzm1l9gykcjy";
-    })
-  ];
-
-  patchFlags = "-p0";
-
   nativeBuildInputs = [
-    autoconf
-    automake
     gettext
-    gnum4
-    libtool
-    perl
   ];
 
   buildInputs = [
@@ -61,20 +42,20 @@ stdenv.mkDerivation rec {
     glib
     libjpeg
     libpng
+    libspiro
     libtiff
+    libtool
+    libuninameslist
     libxml2
     readline
-    uthash
     zlib
   ];
 
-  preConfigure = ''
-    sed -i '/func_check_versions ()/{N;s/{/{ return 0/}' bootstrap
-
-    unpackFile "${gnulib.src}"
-    mv gnulib* gnulib
-
-    ./bootstrap --skip-git --gnulib-srcdir=./gnulib
+  # Remove vendoring
+  postPatch = ''
+    rm -r uthash
+    mkdir uthash
+    ln -sv '${uthash}'/include uthash/src
   '';
 
   configureFlags = [

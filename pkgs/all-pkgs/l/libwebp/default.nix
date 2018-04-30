063e2a6224fad5b52223db123911b9399f31eca0
William A. Kennington III <william@wkennington.com>
2018-04-30 14:38:46 -0700

    libwebp: 0.6.1 -> 1.0.0

--- pkgs/all-pkgs/l/libwebp/default.nix
+++ pkgs/all-pkgs/l/libwebp/default.nix
@@ -17,11 +17,11 @@ let
     optionals;
 in
 stdenv.mkDerivation rec {
-  name = "libwebp-0.6.1";
+  name = "libwebp-1.0.0";
 
   src = fetchurl {
     url = "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/${name}.tar.gz";
-    sha256 = "06503c782d9f151baa325591c3579c68ed700ffc62d4f5a32feead0ff017d8ab";
+    sha256 = "84259c4388f18637af3c5a6361536d754a5394492f91be1abc2e981d4983225b";
   };
 
   configureFlags = [

a8c62433c4b8363e7b7e90893bba732583b767eb
William A. Kennington III <william@wkennington.com>
2018-05-02 12:57:24 -0700

    hiredis: 0.13.3 -> 2018-01-06

--- pkgs/all-pkgs/h/hiredis/default.nix
+++ pkgs/all-pkgs/h/hiredis/default.nix
@@ -3,17 +3,18 @@
 }:
 
 let
-  version = "0.13.3";
+  date = "2018-01-06";
+  rev = "3d8709d19d7fa67d203a33c969e69f0f1a4eab02";
 in
 stdenv.mkDerivation rec {
-  name = "hiredis-${version}";
+  name = "hiredis-${date}";
 
   src = fetchFromGitHub {
-    version = 1;
+    version = 6;
     owner = "redis";
     repo = "hiredis";
-    rev = "v${version}";
-    sha256 = "20bfce29847c95118ed3a7280e8537036d192cefbd5fca91235b6923dc331a5d";
+    inherit rev;
+    sha256 = "314cfd4beff415ff8dffa0d5627a0fd1f675864ae0c671dbcebea9a39c354a5f";
   };
 
   preBuild = ''

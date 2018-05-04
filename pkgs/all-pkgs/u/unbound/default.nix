82d3440253aa89454f97397b1d6191ad22400e44
William A. Kennington III <william@wkennington.com>
2018-05-03 22:28:07 -0700

    unbound: 1.7.0 -> 1.7.1

--- pkgs/all-pkgs/u/unbound/default.nix
+++ pkgs/all-pkgs/u/unbound/default.nix
@@ -3,6 +3,7 @@
 
 , expat
 , fstrm
+, hiredis
 , libevent
 , libsodium
 , openssl
@@ -15,21 +16,22 @@ let
     "https://unbound.net/downloads/unbound-${version}.tar.gz"
   ];
 
-  version = "1.7.0";
+  version = "1.7.1";
 in
 stdenv.mkDerivation rec {
   name = "unbound-${version}";
 
   src = fetchurl {
     urls = tarballUrls version;
-    multihash = "QmZGAmDZz4SdvY3UiCDVuBofKwhBNXvSYWdYyDAQSSa3fs";
+    multihash = "QmcfW5KZRoV58V37i2QhSqbBGg3FDqzRHL9xkjXEE9equB";
     hashOutput = false;
-    sha256 = "94dd9071fb13d8ccd122a3ac67c4524a3324d0e771fc7a8a7c49af8abfb926a2";
+    sha256 = "56e085ef582c5372a20207de179d0edb4e541e59f87be7d4ee1d00d12008628d";
   };
 
   buildInputs = [
     expat
     fstrm
+    hiredis
     libevent
     libsodium
     openssl
@@ -38,9 +40,6 @@ stdenv.mkDerivation rec {
   ];
 
   configureFlags = [
-    "--with-ssl=${openssl}"
-    "--with-libexpat=${expat}"
-    "--with-libevent=${libevent}"
     "--localstatedir=/var"
     "--sysconfdir=/etc"
     "--enable-pie"
@@ -52,6 +51,11 @@ stdenv.mkDerivation rec {
     "--enable-dnstap"
     "--enable-dnscrypt"
     "--enable-cachedb"
+    "--enable-ipsecmod"
+    "--with-ssl=${openssl}"
+    "--with-libexpat=${expat}"
+    "--with-libevent=${libevent}"
+    "--with-libhiredis=${hiredis}"
     "--with-dnstap-socket-path=/run/dnstap.sock"
     "--with-pthreads"
   ];
@@ -63,11 +67,11 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
-      urls = tarballUrls "1.7.0";
+      urls = tarballUrls "1.7.1";
       pgpsigUrls = map (n: "${n}.asc") urls;
       pgpKeyFingerprint = "EDFA A3F2 CA4E 6EB0 5681  AF8E 9F6F 1C2D 7E04 5F8D";
       inherit (src) outputHashAlgo;
-      outputHash = "94dd9071fb13d8ccd122a3ac67c4524a3324d0e771fc7a8a7c49af8abfb926a2";
+      outputHash = "56e085ef582c5372a20207de179d0edb4e541e59f87be7d4ee1d00d12008628d";
     };
   };
 

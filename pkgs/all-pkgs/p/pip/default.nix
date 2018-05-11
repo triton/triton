9685760dccfa702cc462e37e3956930c04533fcc
Cody W. Opel <codyopel@gmail.com>
2018-05-11 17:55:55 -0400

    pip: 2018-03-01 -> 10.0.1

--- pkgs/all-pkgs/p/pip/default.nix
+++ pkgs/all-pkgs/p/pip/default.nix
@@ -1,31 +1,20 @@
 { stdenv
 , buildPythonPackage
-, fetchFromGitHub
-#, fetchPyPi
+, fetchPyPi
 , lib
 }:
 
 let
-  version = "2018-03-01";
+  version = "10.0.1";
 in
 buildPythonPackage rec {
   name = "pip-${version}";
 
-  # FIXME: Revert back to using versioned releases once 10.x is released.
-  # XXX: pip vendors outdated sources and a release has not been tagged since 2016.
-  src = fetchFromGitHub {
-    version = 5;
-    owner = "pypa";
-    repo = "pip";
-    rev = "1cb99c1a6a0161d11fc5396030d88ebd45e118d4";
-    sha256 = "afc0472d4fcbc69ee03ce44af87cbae81fec95976f5c788ff21c2b14ae58b8ee";
+  src = fetchPyPi {
+    package = "pip";
+    inherit version;
+    sha256 = "f2bd08e0cd1b06e10218feaf6fef299f473ba706582eb3bd9d52203fdbd7ee68";
   };
-
-  # src = fetchPyPi {
-  #   package = "pip";
-  #   inherit version;
-  #   sha256 = "09f243e1a7b461f654c26a725fa373211bb7ff17a9300058b205c61658ca940d";
-  # };
 
   passthru = {
     inherit version;

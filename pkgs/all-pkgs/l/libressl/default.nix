ce4128ca447e3edb24b6635f7c51087588b36c9f
William A. Kennington III <william@wkennington.com>
2018-05-14 00:17:54 -0700

    libressl: 2.7.2 -> 2.7.3

--- pkgs/all-pkgs/l/libressl/default.nix
+++ pkgs/all-pkgs/l/libressl/default.nix
@@ -7,12 +7,12 @@ let
 in
 
 stdenv.mkDerivation rec {
-  name = "libressl-2.7.2";
+  name = "libressl-2.7.3";
 
   src = fetchurl {
     url = "${baseUrl}/${name}.tar.gz";
     hashOutput = false;  # Upstream provides it directly
-    sha256 = "917a8779c342177ff3751a2bf955d0262d1d8916a4b408930c45cef326700995";
+    sha256 = "16c70d8fe1de6e9bedea0d67804b55f3894717693a05ed45e15e0e2f939c2795";
   };
 
   configureFlags = [

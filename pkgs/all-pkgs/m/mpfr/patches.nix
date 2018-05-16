a98899b6eaffde876adcca03bbcbe32b6eb91d4c
William A. Kennington III <william@wkennington.com>
2018-05-15 23:00:48 -0700

    mpfr: 4.0.1 -> 4.0.1p6

--- pkgs/all-pkgs/m/mpfr/patches.nix
+++ pkgs/all-pkgs/m/mpfr/patches.nix
@@ -1,2 +1,26 @@
 {
+  "patch01" = {
+    multihash = "QmQ26xu1DzuhYEBC2f52oQytNLmZcANukAhGfnMKLWS5Mp";
+    sha256 = "915855fdb5d4ea12ca010fb78949017e2cd3f201897db80e3214efa70f5b59bd";
+  };
+  "patch02" = {
+    multihash = "QmcNmRyzu6rM2FVYJuzBwKP1fd1mEPm8b9qG6uDBbFt7ou";
+    sha256 = "6d74b011dfe60634207ae25f23fcf9030b1b1a2d25c57149b9e95e4a521605bc";
+  };
+  "patch03" = {
+    multihash = "QmNwzFcdMj7uwbKdpZ4Rd4CR3bEEXQB7GWSXXe3AkBLsVd";
+    sha256 = "076f5ea9c594675bcf622a5728cac42903ba35a28e7e813abc4d2f8f66bc31c0";
+  };
+  "patch04" = {
+    multihash = "QmSSyXXTFWPK5YoU6zPribgyY2Rx9pY1nvGP6jdi4K1mnW";
+    sha256 = "e7bea6c5a7fa9256d86873a98d33b351f24619b72aff0273dab9009e1a251d3a";
+  };
+  "patch05" = {
+    multihash = "QmNqfqxCXHWAAEpPzcqznL2Ra7JsTMs427yfycCSr5B3Kj";
+    sha256 = "9a87a269ecba1f69903d363fa05bedc97af1b56237867b229fb980420c1e0b0a";
+  };
+  "patch06" = {
+    multihash = "QmXhP5XkAXQcLA6gqgZR8e7q8Db8ocHzR1ChQCtjH6nkLM";
+    sha256 = "b0f68329943a7518ab8619521713da5dc2d6436dc89fc3b74537c481ab44d4dd";
+  };
 }

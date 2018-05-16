80cc35dd40895a64d0c1f0e5c723634c17568735
William A. Kennington III <william@wkennington.com>
2018-05-15 21:33:17 -0700

    postgresql: 9.4.17 -> 9.4.18

--- pkgs/all-pkgs/p/postgresql/sources.nix
+++ pkgs/all-pkgs/p/postgresql/sources.nix
@@ -4,8 +4,8 @@
     sha256 = "1d981006dce3851e470b038e88bf496a80813c614c2e89ed7d2c7fb38e66f6cb";
   };
   "9.4" = {
-    version = "9.4.17";
-    sha256 = "7a320cd335052b840d209dc9688f09965763351c590e3cc7bf577591179fd7c6";
+    version = "9.4.18";
+    sha256 = "428337f2b2f5e3ea21b8a44f88eb89c99a07a324559b99aebe777c9abdf4c4c0";
   };
   "9.5" = {
     version = "9.5.12";

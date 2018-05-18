d91c2627940f3b47d929c00495114fdad02ee926
William A. Kennington III <william@wkennington.com>
2018-05-17 20:20:15 -0700

    intel-vaapi-driver: Add back src fields for stable versions

--- pkgs/all-pkgs/i/intel-vaapi-driver/default.nix
+++ pkgs/all-pkgs/i/intel-vaapi-driver/default.nix
@@ -12,6 +12,7 @@
 }:
 
 let
+  # Move this back to a stable release once this commit is integrated
   date = "2018-04-28";
   rev = "40b15a5c6c0103c23a5db810aef27cf75d0b6723";
 in
@@ -25,6 +26,13 @@ stdenv.mkDerivation rec {
     inherit rev;
     sha256 = "5ac5271ecd9918b3c95d265d0bdf27fd9cb5f5551757a4968a0600a42ff75761";
   };
+  /*
+  fetchurl {
+    url = "https://github.com/intel/intel-vaapi-driver/releases/download/${version}/${name}.tar.bz2";
+    hashOutput = false;
+    sha256 = "5ac5271ecd9918b3c95d265d0bdf27fd9cb5f5551757a4968a0600a42ff7576a";
+  };
+  */
 
   nativeBuildInputs = [
     meson
@@ -48,6 +56,17 @@ stdenv.mkDerivation rec {
     "-Dwith_wayland=yes"
     "-Denable_hybrid_codec=true"
   ];
+
+  passthru = {
+    srcVerification = fetchurl {
+      failEarly = true;
+      sha1Urls = map (n: "${n}.sha1sum") src.urls;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+    };
+  };
 
   meta = with lib; {
     description = "VA-API user mode driver for Intel GEN Graphics family";

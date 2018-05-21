e278d28f52d71b346120cc521f0cddd2f232b344
Cody W. Opel <codyopel@gmail.com>
2018-05-21 18:28:39 -0400

    botocore: 1.10.21 -> 1.10.24

--- pkgs/all-pkgs/b/botocore/default.nix
+++ pkgs/all-pkgs/b/botocore/default.nix
@@ -9,7 +9,7 @@
 }:
 
 let
-  version = "1.10.21";
+  version = "1.10.24";
 in
 buildPythonPackage rec {
   name = "botocore-${version}";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
   src = fetchPyPi {
     package = "botocore";
     inherit version;
-    sha256 = "be3e7067912342b4af144afd720d16cf5a0f24d773e202695dffaca96ab11d04";
+    sha256 = "b7a23361bcd6ce2d9cf56a3e5bc7c6b2e3233f3d902d41cb2dfb37472ea41986";
   };
 
   propagatedBuildInputs = [

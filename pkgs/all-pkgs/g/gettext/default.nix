e450451dbd4d8055f44a8397b1d1c3c3ae08ef1f
William A. Kennington III <william@wkennington.com>
2018-06-07 14:54:59 -0700

    gettext: Add srcVerification

--- pkgs/all-pkgs/g/gettext/default.nix
+++ pkgs/all-pkgs/g/gettext/default.nix
@@ -2,11 +2,18 @@
 , fetchurl
 }:
 
+let
+  tarballUrls = version: [
+    "mirror://gnu/gettext/gettext-${version}.tar.xz"
+  ];
+
+  version = "0.19.8.1";
+in
 stdenv.mkDerivation rec {
-  name = "gettext-0.19.8.1";
+  name = "gettext-${version}";
 
   src = fetchurl {
-    url = "mirror://gnu/gettext/${name}.tar.xz";
+    urls = tarballUrls version;
     hashOutput = false;
     sha256 = "105556dbc5c3fbbc2aa0edb46d22d055748b6f5c7cd7a8d99f8e7eb84e938be4";
   };
@@ -24,6 +31,17 @@ stdenv.mkDerivation rec {
       -e "/^  .\?gettext/ s,envsubst,$out/bin/\0,g" \
       -e "/^  .\?gettext/ s,^  ,\0$out/bin/,"
   '';
+
+  passthru = {
+    srcVerification = fetchurl rec {
+      failEarly = true;
+      urls = tarballUrls "0.19.8.1";
+      pgpsigUrls = map (n: "${n}.sig") urls;
+      pgpKeyFingerprint = "4622 25C3 B46F 3487 9FC8  496C D605 848E D7E6 9871";
+      inherit (src) outputHashAlgo;
+      outputHash = "105556dbc5c3fbbc2aa0edb46d22d055748b6f5c7cd7a8d99f8e7eb84e938be4";
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "Well integrated set of translation tools and documentation";

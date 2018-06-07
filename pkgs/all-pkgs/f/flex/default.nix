fd1dadf3360b2b1612e365dc98f46c4758333bb7
William A. Kennington III <william@wkennington.com>
2018-06-07 15:00:04 -0700

    flex: Add srcVerification

--- pkgs/all-pkgs/f/flex/default.nix
+++ pkgs/all-pkgs/f/flex/default.nix
@@ -5,13 +5,18 @@
 }:
 
 let
+  tarballUrls = version: [
+    "https://github.com/westes/flex/releases/download/v${version}/flex-${version}.tar.gz"
+  ];
+
   version = "2.6.4";
 in
 stdenv.mkDerivation rec {
   name = "flex-${version}";
 
   src = fetchurl {
-    url = "https://github.com/westes/flex/releases/download/v${version}/${name}.tar.gz";
+    urls = tarballUrls version;
+    hashOutput = false;
     sha256 = "e87aae032bf07c26f85ac0ed3250998c37621d95f8bd748b31f15b33c45ee995";
   };
 
@@ -28,6 +33,17 @@ stdenv.mkDerivation rec {
   ];
 
   disableStatic = false;
+
+  passthru = {
+    srcVerification = fetchurl rec {
+      failEarly = true;
+      urls = tarballUrls "2.6.4";
+      pgpsigUrls = map (n: "${n}.sig") urls;
+      pgpKeyFingerprint = "56C6 7868 E933 90AA 1039  AD1C E4B2 9C8D 6488 5307";
+      inherit (src) outputHashAlgo;
+      outputHash = "e87aae032bf07c26f85ac0ed3250998c37621d95f8bd748b31f15b33c45ee995";
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "A fast lexical analyser generator";

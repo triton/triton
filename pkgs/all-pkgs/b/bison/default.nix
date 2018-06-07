14a692ac3092d27a7829abc559a56d67a2e8bc5d
William A. Kennington III <william@wkennington.com>
2018-06-07 14:37:56 -0700

    bison: Add srcVerification

--- pkgs/all-pkgs/b/bison/default.nix
+++ pkgs/all-pkgs/b/bison/default.nix
@@ -1,14 +1,23 @@
 { stdenv
 , fetchurl
+, lib
 , m4
 , perl
 }:
 
+let
+  tarballUrls = version: [
+    "mirror://gnu/bison/bison-${version}.tar.xz"
+  ];
+
+  version = "3.0.4";
+in
 stdenv.mkDerivation rec {
-  name = "bison-3.0.4";
+  name = "bison-${version}";
 
   src = fetchurl {
     url = "mirror://gnu/bison/${name}.tar.gz";
+    hashOutput = false;
     sha256 = "b67fd2daae7a64b5ba862c66c07c1addb9e6b1b05c5f2049392cfd8a2172952e";
   };
 
@@ -23,7 +32,18 @@ stdenv.mkDerivation rec {
     m4
   ];
 
-  meta = with stdenv.lib; {
+  passthru = {
+    srcVerification = fetchurl rec {
+      failEarly = true;
+      urls = tarballUrls "3.0.5";
+      pgpsigUrls = map (n: "${n}.sig") urls;
+      pgpKeyFingerprint = "7DF8 4374 B1EE 1F97 64BB  E25D 0DDC AA32 78D5 264E";
+      inherit (src) outputHashAlgo;
+      outputHash = "075cef2e814642e30e10e8155e93022e4a91ca38a65aa1d5467d4e969f97f338";
+    };
+  };
+
+  meta = with lib; {
     description = "Yacc-compatible parser generator";
     homepage = "http://www.gnu.org/software/bison/";
     license = licenses.gpl3Plus;

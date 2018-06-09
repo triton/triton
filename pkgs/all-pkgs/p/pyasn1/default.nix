351d3af707a4f59ff0391a96a2eeeb1adb69a7dc
William A. Kennington III <william@wkennington.com>
2018-06-09 15:29:37 -0700

    pyasn1: 0.4.2 -> 0.4.3

--- pkgs/all-pkgs/p/pyasn1/default.nix
+++ pkgs/all-pkgs/p/pyasn1/default.nix
@@ -5,7 +5,7 @@
 }:
 
 let
-  version = "0.4.2";
+  version = "0.4.3";
 in
 buildPythonPackage {
   name = "pyasn1-${version}";
@@ -13,7 +13,7 @@ buildPythonPackage {
   src = fetchPyPi {
     package = "pyasn1";
     inherit version;
-    sha256 = "d258b0a71994f7770599835249cece1caef3c70def868c4915e6e5ca49b67d15";
+    sha256 = "fb81622d8f3509f0026b0683fe90fea27be7284d3826a5f2edf97f69151ab0fc";
   };
 
   meta = with lib; {

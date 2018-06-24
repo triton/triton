e703edbd10c70a91c9c08fb832a9dd05003780dd
William A. Kennington III <william@wkennington.com>
2018-06-23 20:01:22 -0700

    f2fs-tool: Remove unused attr

--- pkgs/all-pkgs/f/f2fs-tools/default.nix
+++ pkgs/all-pkgs/f/f2fs-tools/default.nix
@@ -3,7 +3,6 @@
 , autoreconfHook
 
 , acl
-, attr
 , libselinux
 , util-linux_lib
 }:
@@ -27,7 +26,6 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     acl
-    attr
     libselinux
     util-linux_lib
   ];

fdf0e6f9655646e745b07f676349c27be62899ed
William A. Kennington III <william@wkennington.com>
2018-06-24 22:42:04 -0700

    uthash: 2018-02-15 -> 2018-05-09

--- pkgs/all-pkgs/u/uthash/default.nix
+++ pkgs/all-pkgs/u/uthash/default.nix
@@ -3,8 +3,8 @@
 }:
 
 let
-  rev = "29fc26a8ee4116236230160b67ae693a11a9352f";
-  date = "2018-02-15";
+  rev = "49428699aa376f6668d2aedcfa9929480132b66e";
+  date = "2018-05-09";
 in
 stdenv.mkDerivation rec {
   name = "uthash-${date}";
@@ -14,7 +14,7 @@ stdenv.mkDerivation rec {
     owner = "troydhanson";
     repo = "uthash";
     inherit rev;
-    sha256 = "aad0a490b6bb075efcea2a1556ac0168c89e587915c89a7e0f89424cf4d44456";
+    sha256 = "71df8dd22626eb1b235fe7fbb06f9d93d63f0a7265b7b0352b96aeb87b68f970";
   };
 
   installPhase = ''

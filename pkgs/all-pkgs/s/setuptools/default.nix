e10f1c16273dbeaae7482562f0b31346266782ab
William A. Kennington III <william@wkennington.com>
2018-07-24 15:40:11 -0700

    setuptools: 39.2.0 -> 40.0.0

--- pkgs/all-pkgs/s/setuptools/default.nix
+++ pkgs/all-pkgs/s/setuptools/default.nix
@@ -15,7 +15,7 @@ let
   inherit (lib)
     makeSearchPath;
 
-  version = "39.2.0";
+  version = "40.0.0";
 in
 buildPythonPackage rec {
   name = "setuptools-${version}";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     package = "setuptools";
     inherit version;
     type = ".zip";
-    sha256 = "f7cddbb5f5c640311eb00eab6e849f7701fa70bf6a183fc8a2c33dd1d1672fb2";
+    sha256 = "012adb8e25fbfd64c652e99e7bab58799a3aaf05d39ab38561f69190a909015f";
   };
 
   nativeBuildInputs = [

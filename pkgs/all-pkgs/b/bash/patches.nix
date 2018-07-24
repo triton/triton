b344feccfad48a15a070082d948e047acf0d85f0
William A. Kennington III <william@wkennington.com>
2018-07-24 14:38:57 -0700

    bash: 4.4p19 -> 4.4p23

--- pkgs/all-pkgs/b/bash/patches.nix
+++ pkgs/all-pkgs/b/bash/patches.nix
@@ -18,4 +18,8 @@
   "bash44-017" = "0w6jpj2giakji1ir83rpkx1y7n7xqppah3j748m6dm38hywr0gvp";
   "bash44-018" = "1k58h4wxbsg7r4rwhrvzx5hfbapba2nxjysbhh6qp6ki5ys99i2v";
   "bash44-019" = "07n1i5610lbs672x1s8g82qn3qfj06s0ip3z80sri0g8vxp0s5r7";
+  "bash44-020" = "0b2jk5n1af1vh590qfc52hv65mafb4vl1xv26s8j5a3byb5y4h0q";
+  "bash44-021" = "1hblcd2xmqqlp0idnavw66570n7m0yv5rbbr873c2gkn982mk3xx";
+  "bash44-022" = "0yfbjzr79vzjs2hyi5m8iy2b38fq7vikdfa4zqdvjsp36q4iycs5";
+  "bash44-023" = "1dlism6qdx60nvzj0v7ndr7lfahl4a8zmzckp13hqgdx7xpj7v2g";
 }

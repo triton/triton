db350bc5096201326438a0d9e9653ec71e62ff40
William A. Kennington III <william@wkennington.com>
2018-07-24 16:47:35 -0700

    hiredis: 2018-01-06 -> 2018-05-31

--- pkgs/all-pkgs/h/hiredis/default.nix
+++ pkgs/all-pkgs/h/hiredis/default.nix
@@ -3,8 +3,8 @@
 }:
 
 let
-  date = "2018-01-06";
-  rev = "3d8709d19d7fa67d203a33c969e69f0f1a4eab02";
+  date = "2018-05-31";
+  rev = "a65537a672de845f3f4530050d1e7bd88d51ac67";
 in
 stdenv.mkDerivation rec {
   name = "hiredis-${date}";
@@ -14,7 +14,7 @@ stdenv.mkDerivation rec {
     owner = "redis";
     repo = "hiredis";
     inherit rev;
-    sha256 = "314cfd4beff415ff8dffa0d5627a0fd1f675864ae0c671dbcebea9a39c354a5f";
+    sha256 = "1eff991c1f689a14ef29f3906db8e89bdd36e86183709d09bce510ccc0aa7e6c";
   };
 
   preBuild = ''

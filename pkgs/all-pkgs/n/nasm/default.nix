cf71d026d6c6ecfd2cf77d9eb6a72290bb8d91a3
William A. Kennington III <william@wkennington.com>
2018-07-25 14:40:25 -0700

    nasm: Fix url

--- pkgs/all-pkgs/n/nasm/default.nix
+++ pkgs/all-pkgs/n/nasm/default.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
   name = "nasm-${version}";
 
   src = fetchurl {
-    url = "http://www.nasm.us/pub/nasm/releasebuilds/${version}/"
+    url = "https://www.nasm.us/pub/nasm/releasebuilds/${version}/"
       + "${name}.tar.xz";
     multihash = "QmaYeHp1JoEH5kGKr8Vo6C3aZpRTvMdA6JiSvGFmUx9h6e";
     sha256 = "812ecfb0dcbc5bd409aaa8f61c7de94c5b8752a7b00c632883d15b2ed6452573";

5feec932aca4b606f751edfa4aa5383d651d81ac
William A. Kennington III <william@wkennington.com>
2018-07-25 14:15:10 -0700

    setuptools-scm: Fix

--- pkgs/all-pkgs/s/setuptools-scm/default.nix
+++ pkgs/all-pkgs/s/setuptools-scm/default.nix
@@ -16,6 +16,12 @@ buildPythonPackage rec {
     sha256 = "113cea38b2edba8538b7e608b58cbd7e09bb71b16d968a9b97e36b4805e06d59";
   };
 
+  # Fix missing version
+  postPatch = ''
+    grep -q 'version=' setup.py
+    sed -i "/name=\"/a\    version='${version}'," setup.py
+  '';
+
   meta = with lib; {
     description = "Handles managing python package versions in scm metadata";
     homepage = https://bitbucket.org/pypa/setuptools_scm/;

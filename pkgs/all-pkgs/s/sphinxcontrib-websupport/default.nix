942c7bbf109b09bc327fd679c717a198aaf8f4dc
William A. Kennington III <william@wkennington.com>
2018-07-25 14:43:58 -0700

    sphinxcontrib-websupport: 1.0.1 -> 1.1.0

--- pkgs/all-pkgs/s/sphinxcontrib-websupport/default.nix
+++ pkgs/all-pkgs/s/sphinxcontrib-websupport/default.nix
@@ -5,7 +5,7 @@
 }:
 
 let
-  version = "1.0.1";
+  version = "1.1.0";
 in
 buildPythonPackage {
   name = "sphinxcontrib-websupport-${version}";
@@ -13,11 +13,8 @@ buildPythonPackage {
   src = fetchPyPi {
     package = "sphinxcontrib-websupport";
     inherit version;
-    sha256 = "7a85961326aa3a400cd4ad3c816d70ed6f7c740acd7ce5d78cd0a67825072eb9";
+    sha256 = "9de47f375baf1ea07cdb3436ff39d7a9c76042c10a769c52353ec46e4e8fc3b9";
   };
-
-  propagatedBuildInputs = [
-  ];
 
   meta = with lib; {
     maintainers = with maintainers; [

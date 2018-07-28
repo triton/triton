7b8d00f4c6b674cf099c9a425cf4a3ad65d06c5d
William A. Kennington III <william@wkennington.com>
2018-07-28 15:58:34 -0700

    volume_key: 0.3.9 -> 0.3.11

--- pkgs/all-pkgs/v/volume_key/default.nix
+++ pkgs/all-pkgs/v/volume_key/default.nix
@@ -1,55 +1,33 @@
 { stdenv
-, fetchpatch
 , fetchurl
-, python2
 
 , cryptsetup
 , glib
-, gnupg
 , gpgme
 , nss
 , nspr
+, python3
 , util-linux_lib
 }:
 
 stdenv.mkDerivation rec {
-  name = "volume_key-0.3.9";
+  name = "volume_key-0.3.11";
 
   src = fetchurl {
     url = "https://releases.pagure.org/volume_key/${name}.tar.xz";
-    multihash = "QmVUT6fW9bLZqFu1X2En8kpof9z2Ga9zN5ZZmfaemA2Dai";
-    sha256 = "450a54fe9bf56acec6850c1e71371d3e4913c9ca1ef0cdc3a517b4b6910412a6";
+    multihash = "QmUVdxheid5rjbZ7k7kRszh9T3DsVn4iHPa142rGYKz2b2";
+    sha256 = "e6b279c25ae477b555f938db2e41818f90c8cde942b0eec92f70b6c772095f6d";
   };
-
-  nativeBuildInputs = [
-    python2
-  ];
 
   buildInputs = [
     cryptsetup
     glib
-    gnupg
     gpgme
     nss
     nspr
+    python3
     util-linux_lib
   ];
-
-  patches = [
-    (fetchpatch {
-      url = "https://pagure.io/volume_key/c/ecef526a51c5a276681472fd6df239570c9ce518.patch";
-      sha256 = "b64ce41f959838165ab3608b9870fc04220533369657af194a10ab7918aa951e";
-    })
-  ];
-
-  postPatch = ''
-    grep -q '#include <config.h>' lib/libvolume_key.h
-    sed -i '/#include <config.h>/d' lib/libvolume_key.h
-  '';
-
-  preBuild = ''
-    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I$(find "$(dirname "$(type -tP python)")"/.. -name Python.h -exec dirname {} \;)"
-  '';
 
   configureFlags = [
     "--sysconfdir=/etc"

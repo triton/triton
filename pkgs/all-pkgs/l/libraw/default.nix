08cf318254a5f5aec8d825bd5160d862323642e1
William A. Kennington III <william@wkennington.com>
2018-07-28 19:16:57 -0700

    libraw: 0.18.12 -> 0.19.0

--- pkgs/all-pkgs/l/libraw/default.nix
+++ pkgs/all-pkgs/l/libraw/default.nix
@@ -8,21 +8,29 @@
 }:
 
 let
-  version = "0.18.12";
+  version = "0.19.0";
 in
 stdenv.mkDerivation rec {
   name = "libraw-${version}";
 
   src = fetchurl {
     url = "https://www.libraw.org/data/LibRaw-${version}.tar.gz";
-    multihash = "QmYMJHWPp2WoBjACEEEGiMLgGDfu3SW1V8GTxE6huRr42w";
-    sha256 = "57754d75a857e16ba1c2a429e4b5b4d79160a59eadaec715351fc9c8448653d4";
+    multihash = "QmRVsUUTDEy55LaiwBWv8niZ1xRVdfnVEMdEsWM3Je8pwb";
+    sha256 = "e83f51e83b19f9ba6b8bd144475fc12edf2d7b3b930d8d280bdebd8a8f3ed259";
   };
 
   buildInputs = [
     lcms2
     libjpeg
     jasper
+  ];
+
+  configureFlags = [
+    "--enable-openmp"
+    "--enable-jpeg"
+    "--enable-jasper"
+    "--enable-lcms"
+    "--disable-examples"
   ];
 
   meta = with lib; {

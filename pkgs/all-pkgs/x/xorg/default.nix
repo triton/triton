44fbafa84d70828d85af7aca2944c8701b5c9e8f
William A. Kennington III <william@wkennington.com>
2018-07-29 01:16:55 -0700

    libXxf86misc: 1.0.3 -> 1.0.4

--- pkgs/all-pkgs/x/xorg/default.nix
+++ pkgs/all-pkgs/x/xorg/default.nix
@@ -788,10 +788,10 @@ let
   }) // {inherit libX11 libXext xorgproto ;};
 
   libXxf86misc = (mkDerivation "libXxf86misc" {
-    name = "libXxf86misc-1.0.3";
+    name = "libXxf86misc-1.0.4";
     src = fetchurl {
-      url = mirror://xorg/individual/lib/libXxf86misc-1.0.3.tar.bz2;
-      sha256 = "0nvbq9y6k6m9hxdvg3crycqsnnxf1859wrisqcs37z9fhq044gsn";
+      url = mirror://xorg/individual/lib/libXxf86misc-1.0.4.tar.bz2;
+      sha256 = "a89c03e2b0f16239d67a2031b9003f31b5a686106bbdb3c797fb88ae472af380";
     };
     nativeBuildInputs = [ utilmacros ];
     buildInputs = [ libX11 libXext xorgproto ];

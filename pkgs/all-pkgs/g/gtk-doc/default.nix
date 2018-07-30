1c5135956b83e213d740640f53f7a3a641cdd10e
William A. Kennington III <william@wkennington.com>
2018-07-30 00:52:00 -0700

    gtk-doc: Cleanup

--- pkgs/all-pkgs/g/gtk-doc/default.nix
+++ pkgs/all-pkgs/g/gtk-doc/default.nix
@@ -1,24 +1,26 @@
 { stdenv
+, combine-xml-catalogs
+, docbook_xml_dtd_43
+, docbook-xsl
 , fetchurl
+, gettext
+, itstool
 , lib
-, which
+, libxml2
+, libxslt
 
 # TODO: reenable once texlive is fixed
 #, dblatex
-, docbook_xml_dtd_43
-, docbook-xsl
-, gettext
-, glib
-, gnome_doc_utils
-, itstool
-, libxml2
-, libxslt
-, perl
-, python3Packages
+, python2
 }:
 
 let
-  version = "1.26";
+  version = "1.28";
+
+  xmlcatalog = combine-xml-catalogs [
+    docbook-xsl
+    docbook_xml_dtd_43
+  ];
 in
 stdenv.mkDerivation rec {
   name = "gtk-doc-${version}";
@@ -26,49 +28,22 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "mirror://gnome/sources/gtk-doc/${version}/${name}.tar.xz";
     hashOutput = false;
-    sha256 = "bff3f44467b1d39775e94fad545f050faa7e8d68dc6a31aef5024ba3c2d7f2b7";
+    sha256 = "911e29e302252c96128965ee1f4067d5431a88e00ad1023a8bc1d6b922af5715";
   };
 
-  preConfigure =
-  /* maybe there is a better way to pass the needed dtd and xsl files
-     "-//OASIS//DTD DocBook XML V4.1.2//EN" and
-     "http://docbook.sourceforge.net/release/xsl/current/html/chunk.xsl" */ ''
-    mkdir -pv $out/nix-support
-    cat > $out/nix-support/catalog.xml << EOF
-    <?xml version="1.0"?>
-    <!DOCTYPE catalog PUBLIC "-//OASIS//DTD Entity Resolution XML Catalog V1.0//EN" "http://www.oasis-open.org/committees/entity/release/1.0/catalog.dtd">
-    <catalog xmlns="urn:oasis:names:tc:entity:xmlns:xml:catalog">
-      <nextCatalog  catalog="${docbook-xsl}/xml/xsl/docbook/catalog.xml" />
-      <nextCatalog  catalog="${docbook_xml_dtd_43}/xml/dtd/docbook/catalog.xml" />
-    </catalog>
-    EOF
-
-    configureFlagsArray+=(
-      "--with-xml-catalog=$out/nix-support/catalog.xml"
-      "--disable-scrollkeeper"
-    )
-  '';
-
   nativeBuildInputs = [
-    which
+    gettext
+    libxml2
+    libxslt
+    itstool
   ];
 
   buildInputs = [
-    #dblatex
-    docbook_xml_dtd_43
-    docbook-xsl
-    gettext
-    gnome_doc_utils
-    itstool
-    libxml2
-    libxslt
-    perl
-    python3Packages.python
+    python2
   ];
 
-  propagatedBuildInputs = [
-    glib
-    python3Packages.six
+  configureFlags = [
+    "--with-xml-catalog=${xmlcatalog}"
   ];
 
   passthru = {

a003cf5dd1bfa7c39419d27b96cb45da29754fbc
William A. Kennington III <william@wkennington.com>
2018-08-01 18:01:31 -0700

    egl-wayland: 2018-01-31 -> 2018-06-25

--- pkgs/all-pkgs/e/egl-wayland/default.nix
+++ pkgs/all-pkgs/e/egl-wayland/default.nix
@@ -13,7 +13,7 @@
 }:
 
 let
-  version = "2018-01-31";
+  version = "2018-06-25";
 in
 stdenv.mkDerivation rec {
   name = "egl-wayland-${version}";
@@ -22,8 +22,8 @@ stdenv.mkDerivation rec {
     version = 6;
     owner = "NVIDIA";
     repo = "egl-wayland";
-    rev = "68ffe6fff49fff7667e8bab5b743c3e6c1950a6f";
-    sha256 = "89f67cc9fd6082466d75449d3b496f2eae3e9dc0fe0f8b79d49c444e6acab9f2";
+    rev = "395ce9f609fbf66f6cab622aec3ded663e089f84";
+    sha256 = "8fdd323190a8a0cdc786c6414874a3eff0736cabba281b610e08b81dcca16c6b";
   };
 
   nativeBuildInputs = [
@@ -39,13 +39,6 @@ stdenv.mkDerivation rec {
     wayland
     xorgproto
   ];
-
-  postPatch = ''
-    # Fix list being cast to a string
-    sed -i meson.build \
-      -e '/EGL_EXTERNAL_PLATFORM_MAX_VERSION/ s/\[//' \
-      -e '/EGL_EXTERNAL_PLATFORM_MAX_VERSION/ s/\]//'
-  '';
 
   meta = with lib; {
     description = "Wayland EGL External Platform library";

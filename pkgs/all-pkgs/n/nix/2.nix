640bbbdea99ade00b9f580362baf583f896f2d1a
William A. Kennington III <william@wkennington.com>
2018-08-06 10:02:18 -0700

    nix(2): 2.0.2 -> 2.0.4

--- pkgs/all-pkgs/n/nix/2.nix
+++ pkgs/all-pkgs/n/nix/2.nix
@@ -1,5 +1,6 @@
 { stdenv
 , config
+, fetchTritonPatch
 , fetchurl
 , lib
 
@@ -19,12 +20,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "nix-2.0.2";
+  name = "nix-2.0.4";
 
   src = fetchurl {
     url = "https://nixos.org/releases/nix/${name}/${name}.tar.xz";
-    multihash = "QmecUivPEb8TNiXz64tE7Q2rNtK6m8DE5fyXGMasRdUGHT";
-    sha256 = "2d2984410f73d759485526e594ce41b9819fafa4676f4f85a93dbdd5352a1435";
+    multihash = "Qmdf1xu1r8ifDjVkq5gNbxUBS2c3JpJ8XxqBe1BvVacN15";
+    sha256 = "166540ff7b8bb41449586b67e5fc6ab9e25525f6724b6c6bcbfb0648fbd6496b";
   };
 
   buildInputs = [
@@ -40,8 +41,26 @@ stdenv.mkDerivation rec {
     xz
   ];
 
-  # Make sure we have a working busybox sh
+  patches = [
+    (fetchTritonPatch {
+      rev = "87c8a7e6ef87849eb89909dce74e0c83de571408";
+      file = "n/nix/0001-Include-ca-certs-in-fixed-outputs.patch";
+      sha256 = "30b1302bd4c32ed08f7bcf1ca7e5111bfa735cee77560a08ebd082d3245f736f";
+    })
+    (fetchTritonPatch {
+      rev = "87c8a7e6ef87849eb89909dce74e0c83de571408";
+      file = "n/nix/0002-Remove-nixpkgs-references.patch";
+      sha256 = "00dc9899fb324924529245ab7ae37deb1528f45592b7364626cac5ee68d4deab";
+    })
+    (fetchTritonPatch {
+      rev = "87c8a7e6ef87849eb89909dce74e0c83de571408";
+      file = "n/nix/0003-Remove-hardcoded-nixos.org-references.patch";
+      sha256 = "af0134f875ed431637184e58468072e1238d59a7aee87af6a437d53bc9a740de";
+    })
+  ];
+
   postPatch = ''
+    # Make sure we have a working busybox sh
     '${busybox}'/bin/busybox sh --help 2>&1 | grep -q 'Usage: sh'
   '';
 

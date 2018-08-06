f0a8557e6c9d5aea86ac5dd0f39b3026973245bb
William A. Kennington III <william@wkennington.com>
2018-08-06 13:51:08 -0700

    nix: Fixes

--- pkgs/all-pkgs/n/nix/2.nix
+++ pkgs/all-pkgs/n/nix/2.nix
@@ -43,19 +43,24 @@ stdenv.mkDerivation rec {
 
   patches = [
     (fetchTritonPatch {
-      rev = "6755e747916ce40339cda63422520813d1c2ed4f";
-      file = "n/nix/0001-Include-ca-certs-in-fixed-outputs.patch";
-      sha256 = "30b1302bd4c32ed08f7bcf1ca7e5111bfa735cee77560a08ebd082d3245f736f";
+      rev = "9f8cc90aa574e0259d8d44fb57ce6a0a8e299ccc";
+      file = "n/nix/0001-Configurable-fixed-output-paths.patch";
+      sha256 = "1e324269d592e10ae2e72d4b1efd490c82cbfdcb9d1f9cb5cec3296818fe49f8";
     })
     (fetchTritonPatch {
-      rev = "6755e747916ce40339cda63422520813d1c2ed4f";
+      rev = "9f8cc90aa574e0259d8d44fb57ce6a0a8e299ccc";
       file = "n/nix/0002-Remove-hardcoded-nixos.org-references.patch";
-      sha256 = "38d2d06ff311e40ce31d410565b25b871d2865e82039a1c49dd854bb3bc762cb";
+      sha256 = "88888e6d71c9fa07e7bdebbffb1d82c9e2ef1cc095de560ea44d20d6cca2e580";
     })
     (fetchTritonPatch {
-      rev = "6755e747916ce40339cda63422520813d1c2ed4f";
+      rev = "9f8cc90aa574e0259d8d44fb57ce6a0a8e299ccc";
       file = "n/nix/0003-Remove-nixpkgs-references.patch";
-      sha256 = "b1906ca56a149b54297000e3ce17494950817eb939d4aa772a1f05c9e55c10f9";
+      sha256 = "1c28d29385b3dfbf840b99efba9fe90ed6842371fc112b8f3602d36f7e904852";
+    })
+    (fetchTritonPatch {
+      rev = "9f8cc90aa574e0259d8d44fb57ce6a0a8e299ccc";
+      file = "n/nix/0004-Build-dir-should-be-unique.patch";
+      sha256 = "6e3ba2beaacf59ed6e2120b15bf833826596f0729c9568e0e36e24a3a73561d0";
     })
   ];
 
@@ -66,7 +71,10 @@ stdenv.mkDerivation rec {
 
   configureFlags = [
     "--sysconfdir=/etc"
-    "--localstatedir=/var"
+    # Ideally this would be /var but this is standard
+    # If we change this now we break the nix-daemon severely and
+    # we can't use upstream binaries
+    "--localstatedir=/nix/var"
     "--enable-gc"
     "--disable-init-state"
     "--with-store-dir=${storeDir}"

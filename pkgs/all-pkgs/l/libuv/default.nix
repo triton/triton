28b78cc8441216f30ca5f02762e7e3aa303fb4fe
William A. Kennington III <william@wkennington.com>
2018-08-17 16:21:22 -0700

    libuv: 1.22.0 -> 1.23.0

--- pkgs/all-pkgs/l/libuv/default.nix
+++ pkgs/all-pkgs/l/libuv/default.nix
@@ -7,7 +7,7 @@
 }:
 
 let
-  version = "1.22.0";
+  version = "1.23.0";
 in
 stdenv.mkDerivation rec {
   name = "libuv-${version}";
@@ -17,7 +17,7 @@ stdenv.mkDerivation rec {
     owner = "libuv";
     repo = "libuv";
     rev = "v${version}";
-    sha256 = "4dcef8b2aa5523a362390426c3960ef8c4d9782cc7fd0bbb4ab35c6931ca042b";
+    sha256 = "ac5ad39002b738bc66dae59370a9d2d5f94ad5f6700e32e3567aa77394ffb65f";
   };
 
   nativeBuildInputs = [

356bcba1f2337fd692fff389a0a21116d3feb82b
William A. Kennington III <william@wkennington.com>
2018-08-24 20:14:59 -0700

    expat: 2.2.5 -> 2.2.6

--- pkgs/all-pkgs/e/expat/default.nix
+++ pkgs/all-pkgs/e/expat/default.nix
@@ -6,14 +6,19 @@
 }:
 
 let
-  version = "2.2.5";
+  tarballUrls = version: [
+    "mirror://sourceforge/expat/expat/${version}/expat-${version}.tar.bz2"
+  ];
+
+  version = "2.2.6";
 in
 stdenv.mkDerivation rec {
   name = "expat-${version}";
 
   src = fetchurl {
-    url = "mirror://sourceforge/expat/expat/${version}/${name}.tar.bz2";
-    sha256 = "d9dc32efba7e74f788fcc4f212a43216fc37cf5f23f4c2339664d473353aedf6";
+    urls = tarballUrls version;
+    hashOutput = false;
+    sha256 = "17b43c2716d521369f82fc2dc70f359860e90fa440bea65b3b85f0b246ea81f2";
   };
 
   configureFlags = [
@@ -23,6 +28,20 @@ stdenv.mkDerivation rec {
   buildInputs = [
     libbsd
   ];
+
+  passthru = {
+    srcVerification = fetchurl rec {
+      failEarly = true;
+      inherit (src)
+        outputHashAlgo;
+      urls = tarballUrls "2.2.6";
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.asc") urls;
+        pgpKeyFingerprint = "3D7E 959D 89FA CFEE 3837  1921 B00B C66A 401A 1600";
+      };
+      outputHash = "17b43c2716d521369f82fc2dc70f359860e90fa440bea65b3b85f0b246ea81f2";
+    };
+  };
 
   meta = with lib; {
     description = "A stream-oriented XML parser library written in C";

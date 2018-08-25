b29e0d321221ad29a56d3e988215306754e0bd31
William A. Kennington III <william@wkennington.com>
2018-08-24 19:35:04 -0700

    openexr: 2.2.1 -> 2.3.0

--- pkgs/all-pkgs/o/openexr/default.nix
+++ pkgs/all-pkgs/o/openexr/default.nix
@@ -7,17 +7,14 @@
 }:
 
 let
-  version = "2.2.1";
+  version = "2.3.0";
 in
 stdenv.mkDerivation rec {
   name = "openexr-${version}";
 
   src = fetchurl {
-    urls = [
-      "https://github.com/openexr/openexr/releases/download/v${version}/${name}.tar.gz"
-      "mirror://savannah/openexr/${name}.tar.gz"
-    ];
-    sha256 = "8f9a5af6131583404261931d9a5c83de0a425cb4b8b25ddab2b169fbf113aecd";
+    url = "https://github.com/openexr/openexr/releases/download/v${version}/${name}.tar.gz";
+    sha256 = "fd6cb3a87f8c1a233be17b94c74799e6241d50fc5efd4df75c7a4b9cf4e25ea6";
   };
 
   buildInputs = [

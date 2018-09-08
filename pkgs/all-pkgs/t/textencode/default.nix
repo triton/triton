e02988775dd06bbbaff47c7327a757c5c3238a86
William A. Kennington III <william@wkennington.com>
2018-09-08 00:39:14 -0700

    textencode: Add url

--- pkgs/all-pkgs/t/textencode/default.nix
+++ pkgs/all-pkgs/t/textencode/default.nix
@@ -6,12 +6,14 @@
 let
   inherit (lib)
     boolEn;
+
+  version = "0.1";
 in
 stdenv.mkDerivation rec {
-  name = "textencode-0.1";
+  name = "textencode-${version}";
 
   src = fetchurl {
-    name = "${name}.tar.xz";
+    url = "https://github.com/triton/textencode/releases/download/v${version}/${name}.tar.xz";
     multihash = "QmWsThAGsQYUoo3foaAKWvLR8f76KnMwjEdN4t4xL46C7x";
     sha256 = "d3c47856f44a1c92f868223874c73da6ce6fdffe213fbf7297b6d77addb6f455";
   };

f527d472f23b8a5a0185d938bcacad2d86229574
William A. Kennington III <william@wkennington.com>
2018-09-09 14:57:33 -0700

    p11-kit: 0.23.12 -> 0.23.14

--- pkgs/all-pkgs/p/p11-kit/default.nix
+++ pkgs/all-pkgs/p/p11-kit/default.nix
@@ -6,7 +6,7 @@
 }:
 
 let
-  version = "0.23.12";
+  version = "0.23.14";
 
   tarballUrls = version: [
     "https://github.com/p11-glue/p11-kit/releases/download/${version}/p11-kit-${version}.tar.gz"
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     urls = tarballUrls version;
     hashOutput = false;
-    sha256 = "58bae22f19db1de1a1103e7ca4149eed6e303e727878c2cd5ea9e6fe445fd403";
+    sha256 = "1cb9fa6d237539f25f62f4c3d4ec71a1c8e0772957ec45ec5af92134129e0d70";
   };
 
   buildInputs = [
@@ -39,12 +39,14 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
-      urls = tarballUrls "0.23.12";
-      pgpsigUrls = map (n: "${n}.sig") urls;
-      pgpKeyFingerprints = [
-        "AED6 E2A1 85EE B379 F174  76D2 E012 D07A D0E3 CC30"
-        "4622 25C3 B46F 3487 9FC8  496C D605 848E D7E6 9871"
-      ];
+      urls = tarballUrls "0.23.14";
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.sig") urls;
+        pgpKeyFingerprints = [
+          "AED6 E2A1 85EE B379 F174  76D2 E012 D07A D0E3 CC30"
+          "4622 25C3 B46F 3487 9FC8  496C D605 848E D7E6 9871"
+        ];
+      };
       inherit (src) outputHashAlgo;
       outputHash = "58bae22f19db1de1a1103e7ca4149eed6e303e727878c2cd5ea9e6fe445fd403";
     };

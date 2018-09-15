2fe2c737fb72f3575a75f9fb0cca8225b5152e8a
William A. Kennington III <william@wkennington.com>
2018-09-14 22:27:17 -0700

    curl: 7.61.0 -> 7.61.1

--- pkgs/all-pkgs/c/curl/default.nix
+++ pkgs/all-pkgs/c/curl/default.nix
@@ -25,16 +25,16 @@ let
     "https://curl.haxx.se/download/curl-${version}.tar.bz2"
   ];
 
-  version = "7.61.0";
+  version = "7.61.1";
 in
 stdenv.mkDerivation rec {
   name = "curl-${version}";
 
   src = fetchurl {
     urls = tarballUrls version;
-    multihash = "QmXrHSWRuAWn4HPvrLsWFqLNM4gvPBs8QWM6zya2qvFFht";
+    multihash = "QmULjVZV6dEupUK42B6Wvs2g5YXCJohvTU62cBtCF6FC62";
     hashOutput = false;
-    sha256 = "5f6f336921cf5b84de56afbd08dfb70adeef2303751ffb3e570c936c6d656c9c";
+    sha256 = "a308377dbc9a16b2e994abd55455e5f9edca4e31666f8f8fcfe7a1a4aea419b9";
   };
 
   buildInputs = [
@@ -66,11 +66,13 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
-      urls = tarballUrls "7.61.0";
-      pgpsigUrls = map (n: "${n}.asc") urls;
-      pgpKeyFingerprint = "27ED EAF2 2F3A BCEB 50DB  9A12 5CC9 08FD B71E 12C2";
+      urls = tarballUrls "7.61.1";
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.asc") urls;
+        pgpKeyFingerprint = "27ED EAF2 2F3A BCEB 50DB  9A12 5CC9 08FD B71E 12C2";
+      };
       inherit (src) outputHashAlgo;
-      outputHash = "5f6f336921cf5b84de56afbd08dfb70adeef2303751ffb3e570c936c6d656c9c";
+      outputHash = "a308377dbc9a16b2e994abd55455e5f9edca4e31666f8f8fcfe7a1a4aea419b9";
     };
   };
 

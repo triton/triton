3df2348a0800dec447582c547cf1341c5bdebd80
William A. Kennington III <william@wkennington.com>
2018-10-05 00:28:26 -0700

    mtools: 4.0.18 -> 4.0.19

--- pkgs/all-pkgs/m/mtools/default.nix
+++ pkgs/all-pkgs/m/mtools/default.nix
@@ -3,15 +3,30 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "mtools-4.0.18";
+  name = "mtools-4.0.19";
 
   src = fetchurl {
     url = "mirror://gnu/mtools/${name}.tar.bz2";
-    sha256 = "119gdfnsxc6hzicnsf718k0fxgy2q14pxn7557rc96aki20czsar";
+    hashOutput = false;
+    sha256 = "1bc197199ea1c2c317f597879486eeb817f3199b526d94cc6841624768d910df";
   };
 
   # Fails to install correctly in parallel
   installParallel = false;
+
+  passthru = {
+    srcVerification = fetchurl {
+      failEarly = true;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.sig") src.urls;
+        pgpKeyFingerprint = "E151 F8F5 4AE4 F4E9 019F  037B C806 31B2 6F43 1961";
+      };
+    };
+  };
 
   meta = with stdenv.lib; {
     homepage = http://www.gnu.org/software/mtools/;

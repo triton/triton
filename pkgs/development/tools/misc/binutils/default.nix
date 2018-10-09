23e516cfbdcf452093a49679c553b358878fad34
William A. Kennington III <william@wkennington.com>
2018-10-08 23:00:58 -0700

    binutils: 2.23.1 -> 2.31.1

--- pkgs/development/tools/misc/binutils/default.nix
+++ pkgs/development/tools/misc/binutils/default.nix
@@ -2,6 +2,7 @@
 , bison
 , fetchTritonPatch
 , fetchurl
+, texinfo
 
 , zlib
 }:
@@ -13,39 +14,62 @@ let
     optionalString;
 in
 stdenv.mkDerivation rec {
-  name = "binutils-2.23.1";
+  name = "binutils-2.31.1";
 
   src = fetchurl {
-    url = "mirror://gnu/binutils/${name}.tar.bz2";
-    sha256 = "06bs5v5ndb4g5qx96d52lc818gkbskd1m0sz57314v887sqfbcia";
+    url = "mirror://gnu/binutils/${name}.tar.xz";
+    hashOutput = false;
+    sha256 = "5d20086ecf5752cc7d9134246e9588fa201740d540f7eb84d795b1f7a93bca86";
   };
 
   nativeBuildInputs = [
     bison
+    texinfo
   ];
 
   buildInputs = [
     zlib
   ];
 
+  patches = [
+    (fetchTritonPatch {
+      rev = "ee7246d6cd30ae3d8ba0f9531684f2cb0c40abeb";
+      file = "b/binutils/0000-2.31.1-2018-10-09.patch";
+      sha256 = "6246b9a14eebca5aeac7cc35f7aaa4351b5d1e3d5aca84d8b4b75bcf5b0359a0";
+    })
+  ];
+
   preConfigure = ''
     # Clear the default library search path.
+    grep -q 'NATIVE_LIB_DIRS=' ld/configure.tgt
     echo 'NATIVE_LIB_DIRS=' >> ld/configure.tgt
 
-    # Use symlinks instead of hard links to save space ("strip" in the
-    # fixup phase strips each hard link separately).
-    for i in $(find . -name Makefile.in); do
-        sed -i "$i" -e 's|ln |ln -s |'
-    done
+    # Symlink all tools instead of hardlinking to make sure
+    # we save space when packing the derivation
+    find . -name Makefile.in -exec sed -i 's,ln \$,ln -s $,' {} \;
   '';
 
   configureFlags = [
     "--enable-shared"
+    # Autodetection is not working for binutils because of how the nested
+    # configure system works
+    "--disable-static"
     "--enable-deterministic-archives"
-    "--disable-werror"
     "--enable-gold"
     "--enable-plugins"
+    "--with-system-zlib"
   ];
+
+  preBuild = ''
+    # Needed otherwise it defaults to $prefix/$archtriple
+    makeFlagsArray+=("tooldir=$out")
+  '';
+
+  # Libtool files reference intermediate static libraries in the build
+  # like libiberty. We don't need them anyway
+  postInstall = ''
+    rm "$out"/lib/*.la
+  '';
 
   meta = with stdenv.lib; {
     description = "Tools for manipulating binaries (linker, assembler, etc.)";

40f865a094f1671b52a5bd43c9c8aad1e226aabf
William A. Kennington III <william@wkennington.com>
2018-10-19 11:37:46 -0700

    re2c: 1.0.3 -> 1.1.1

--- pkgs/all-pkgs/r/re2c/default.nix
+++ pkgs/all-pkgs/r/re2c/default.nix
@@ -3,14 +3,14 @@
 }:
 
 let
-  version = "1.0.3";
+  version = "1.1.1";
 in
 stdenv.mkDerivation rec {
   name = "re2c-${version}";
 
   src = fetchurl {
     url = "https://github.com/skvadrik/re2c/releases/download/${version}/${name}.tar.gz";
-    sha256 = "cf56e0de3f335f6a22d3e8c06b8b450d858a4e7875ea1b01c9233e084b90cb52";
+    sha256 = "856597337ea00b24ce91f549f79e6eece1b92ba5f8b63292cad66c14ac7451cf";
   };
 
   meta = with stdenv.lib; {

0ad10b000d62f6c0ff199930bc6db180b468441f
William A. Kennington III <william@wkennington.com>
2018-10-20 16:55:20 -0700

    file: 5.34 -> 5.35

--- pkgs/all-pkgs/f/file/default.nix
+++ pkgs/all-pkgs/f/file/default.nix
@@ -6,13 +6,13 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "file-5.34";
+  name = "file-5.35";
 
   src = fetchurl {
     url = "ftp://ftp.astron.com/pub/file/${name}.tar.gz";
-    multihash = "QmZyLU7tkfswRTQL4CeYPYxWBzrSdkTdKcZs2W2NeD45Yp";
+    multihash = "QmbMKFj8GB1VC3rfTVE1jyZHogXAbfbtdWAo3hapAPdJeK";
     hashOutput = false;
-    sha256 = "f15a50dbbfa83fec0bd1161e8e191b092ec832720e30cd14536e044ac623b20a";
+    sha256 = "30c45e817440779be7aac523a905b123cba2a6ed0bf4f5439e1e99ba940b5546";
   };
 
   buildInputs = [
@@ -23,9 +23,14 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;
-      pgpsigUrls = map (n: "${n}.asc") src.urls;
-      pgpKeyFingerprint = "BE04 995B A8F9 0ED0 C0C1  76C4 7111 2AB1 6CB3 3B3A";
-      inherit (src) urls outputHash outputHashAlgo;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.asc") src.urls;
+        pgpKeyFingerprint = "BE04 995B A8F9 0ED0 C0C1  76C4 7111 2AB1 6CB3 3B3A";
+      };
     };
   };
 

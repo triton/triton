2bca70a884be51154f69e9f24614f2b594073786
William A. Kennington III <william@wkennington.com>
2018-10-20 16:07:19 -0700

    alsa-lib: 1.1.6 -> 1.1.7

--- pkgs/all-pkgs/a/alsa-lib/default.nix
+++ pkgs/all-pkgs/a/alsa-lib/default.nix
@@ -4,17 +4,31 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "alsa-lib-1.1.6";
+  name = "alsa-lib-1.1.7";
 
   src = fetchurl {
     url = "mirror://alsa/lib/${name}.tar.bz2";
-    multihash = "QmQiwxxHKouNYJeRE7tJyqrJ3M1zntxrSJevCNoVxSpwoU";
-    sha256 = "5f2cd274b272cae0d0d111e8a9e363f08783329157e8dd68b3de0c096de6d724";
+    multihash = "QmVVzgHiV5hYr8YHBHSHca5apWz94TgCJcRRgNFNnfJrMb";
+    hashOutput = false;
+    sha256 = "9d6000b882a3b2df56300521225d69717be6741b71269e488bb20a20783bdc09";
   };
 
   patches = [
     ./alsa-plugin-conf-multilib.patch
   ];
+
+  passthru = {
+    srcVerification = fetchurl {
+      failEarly = true;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+      fullOpts = {
+        insecureHashOutput = true;
+      };
+    };
+  };
 
   meta = with lib; {
     description = "ALSA, the Advanced Linux Sound Architecture libraries";

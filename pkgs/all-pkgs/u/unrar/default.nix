09f16f8a7337027f8e064d34a5f5d40468450674
William A. Kennington III <william@wkennington.com>
2018-10-20 15:59:24 -0700

    unrar: 5.6.6 -> 5.6.8

--- pkgs/all-pkgs/u/unrar/default.nix
+++ pkgs/all-pkgs/u/unrar/default.nix
@@ -4,15 +4,16 @@
 }:
 
 let
-  version = "5.6.6";
+  version = "5.6.8";
 in
 stdenv.mkDerivation rec {
   name = "unrar-${version}";
 
   src = fetchurl {
     url = "https://www.rarlab.com/rar/unrarsrc-${version}.tar.gz";
-    multihash = "QmeBCvw4pA5o4TATDYUBCvnMp2txxLUnVmzmW5HQRqZxba";
-    sha256 = "5dbdd3cff955c4bc54dd50bf58120af7cb30dec0763a79ffff350f26f96c4430";
+    multihash = "QmQyLXvzYx8HhYLKyxZh4NY5f5XbYwcK62Ahji8HgjWiBB";
+    hashOutput = false;
+    sha256 = "a4cc0ac14a354827751912d2af4a0a09e2c2129df5766576fa7e151791dd3dff";
   };
 
   setupHook = ./setup-hook.sh;
@@ -20,6 +21,17 @@ stdenv.mkDerivation rec {
   preBuild = ''
     makeFlags+=("DESTDIR=$out")
   '';
+
+  passthru = {
+    srcVerification = fetchurl {
+      failEarly = true;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+      fullOpts = { };
+    };
+  };
 
   meta = with lib; {
     description = "Utility for RAR archives";

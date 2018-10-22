c7188792463034884a6d94a5c96e4919806d10d7
William A. Kennington III <william@wkennington.com>
2018-10-22 16:51:32 -0700

    sshfs: 3.4.0 -> 3.5.0

--- pkgs/all-pkgs/s/sshfs/default.nix
+++ pkgs/all-pkgs/s/sshfs/default.nix
@@ -12,7 +12,7 @@
 }:
 
 let
-  version = "3.4.0";
+  version = "3.5.0";
 in
 stdenv.mkDerivation rec {
   name = "sshfs-${version}";
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "https://github.com/libfuse/sshfs/releases/download/${name}/${name}.tar.xz";
     hashOutput = false;
-    sha256 = "d27ccd35436d72755c40234cefa081e30fa529e092232a5b4abbff2178c2c22f";
+    sha256 = "c2035332489dea3be88b9328d0e1905f904a34f93eb87fb07cd5380329a3dfe5";
   };
 
   nativeBuildInputs = [
@@ -49,9 +49,14 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;
-      pgpsigUrls = map (n: "${n}.asc") src.urls;
-      pgpKeyFingerprint = "ED31 791B 2C5C 1613 AF38  8B8A D113 FCAC 3C4E 599F";
-      inherit (src) urls outputHash outputHashAlgo;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.asc") src.urls;
+        pgpKeyFingerprint = "ED31 791B 2C5C 1613 AF38  8B8A D113 FCAC 3C4E 599F";
+      };
     };
   };
 

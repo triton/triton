5113aee0a6e1ab852349cbbeae1d9a5f52ac5d8e
William A. Kennington III <william@wkennington.com>
2018-10-22 01:21:06 -0700

    pbr: 4.2.0 -> 5.0.0

--- pkgs/all-pkgs/p/pbr/default.nix
+++ pkgs/all-pkgs/p/pbr/default.nix
@@ -5,7 +5,7 @@
 }:
 
 let
-  version = "4.2.0";
+  version = "5.0.0";
 in
 buildPythonPackage {
   name = "pbr-${version}";
@@ -13,7 +13,7 @@ buildPythonPackage {
   src = fetchPyPi {
     package = "pbr";
     inherit version;
-    sha256 = "1b8be50d938c9bb75d0eaf7eda111eec1bf6dc88a62a6412e33bf077457e0f45";
+    sha256 = "ab94783019179bf48f5784edc63f5bc8328ec5ff93f33591567f266d21ac7323";
   };
 
   meta = with lib; {

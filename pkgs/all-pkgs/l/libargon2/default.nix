9d0a0e4ed50695736ceed73bf714d958e8d3516b
William A. Kennington III <william@wkennington.com>
2018-10-22 01:45:58 -0700

    libargon2: 2017-12-27 -> 2018-08-19

--- pkgs/all-pkgs/l/libargon2/default.nix
+++ pkgs/all-pkgs/l/libargon2/default.nix
@@ -3,18 +3,18 @@
 }:
 
 let
-  date = "2017-12-27";
-  rev = "670229c849b9fe882583688b74eb7dfdc846f9f6";
+  date = "2018-08-19";
+  rev = "b31aa322566a8559403d419b2e9cd3f57957e394";
 in
 stdenv.mkDerivation {
   name = "libargon2-${date}";
 
   src = fetchFromGitHub {
-    version = 5;
+    version = 6;
     owner = "P-H-C";
     repo = "phc-winner-argon2";
     inherit rev;
-    sha256 = "eb80bfece331bc4e657826e114dfd178e676376d1b2d9af19a97aa84520daaa6";
+    sha256 = "1cb3f9a1ae3180960f0141cb854021bb78b8cd7ed2ed09754f4d10b672568d98";
   };
 
   preBuild = ''

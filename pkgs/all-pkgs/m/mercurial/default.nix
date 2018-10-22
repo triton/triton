335798a77ea62666dbc0dfd3afe6cbcd57223cf4
William A. Kennington III <william@wkennington.com>
2018-10-22 01:11:31 -0700

    mercurial: 4.7 -> 4.7.2

--- pkgs/all-pkgs/m/mercurial/default.nix
+++ pkgs/all-pkgs/m/mercurial/default.nix
@@ -5,25 +5,30 @@
 }:
 
 buildPythonPackage rec {
-  name = "mercurial-4.7";
+  name = "mercurial-4.7.2";
 
   src = fetchurl {
     url = "https://www.mercurial-scm.org/release/${name}.tar.gz";
-    multihash = "QmScRD6dEfC7BLaqrYekby91zxKUReJDees2FJeRvPF8XV";
+    multihash = "QmSrKX4D9t5tHFAMk5FXgzvpSjDfhgb7ouTjxJJQLbthQn";
     hashOutput = false;
-    sha256 = "098cb1437f77fb7f75dc2f008742933c729ec0b63cfa3e4e2f0a8fbc3d0d349f";
+    sha256 = "97f0594216f2348a2e37b2ad8a56eade044e741153fee8c584487e9934ca09fb";
   };
 
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;
-      pgpsigUrls = map (n: "${n}.asc") src.urls;
-      pgpKeyFingerprints = [
-        "FAD6 1395 F642 FC2B 33C8  4B9A 2057 81AC 682A 2D72"
-        "3A81 5516 3D0E 20A5 30FC  B786 47A6 7FFA A346 AACE"
-        "2BCC E14F 5C67 25AA 2EA8  AEB7 B9C9 DC82 4AA5 BDD5"
-      ];
-      inherit (src) urls outputHash outputHashAlgo;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.asc") src.urls;
+        pgpKeyFingerprints = [
+          "FAD6 1395 F642 FC2B 33C8  4B9A 2057 81AC 682A 2D72"
+          "3A81 5516 3D0E 20A5 30FC  B786 47A6 7FFA A346 AACE"
+          "2BCC E14F 5C67 25AA 2EA8  AEB7 B9C9 DC82 4AA5 BDD5"
+        ];
+      };
     };
   };
 

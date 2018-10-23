b8684cc975e8505014773ee3125055ebd1a88772
William A. Kennington III <william@wkennington.com>
2018-10-22 18:27:59 -0700

    diffoscope: 96 -> 103

--- pkgs/all-pkgs/d/diffoscope/default.nix
+++ pkgs/all-pkgs/d/diffoscope/default.nix
@@ -7,7 +7,7 @@
 }:
 
 let
-  version = "96";
+  version = "103";
 in
 buildPythonPackage rec {
   name = "diffoscope-${version}";
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   src = fetchPyPi {
     package = "diffoscope";
     inherit version;
-    sha256 = "e2d067f540e11aa690f546bed1cafa8081a3d97d1192e05c7cc3c78aba70c6f4";
+    sha256 = "aea22460e46bf4c57d7e908a309f4a7c4b88aed9470603bcc109c05a5b6f411b";
   };
 
   postPatch = /* Fix invalid encoding in README */ ''

a5631fe05f15495fd8cbd70e29af598f6a7304cb
William A. Kennington III <william@wkennington.com>
2018-10-22 18:24:27 -0700

    tor: 0.3.3.7 -> 0.3.4.8

--- pkgs/all-pkgs/t/tor/default.nix
+++ pkgs/all-pkgs/t/tor/default.nix
@@ -16,13 +16,13 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "tor-0.3.3.7";
+  name = "tor-0.3.4.8";
 
   src = fetchurl {
     url = "https://www.torproject.org/dist/${name}.tar.gz";
-    multihash = "QmcJjfnd7Kqi88No4wasBUp5EhiCf9viETJGRnziJfAJj6";
+    multihash = "QmdtxgrkKdQe9qhoEb7MxMv4yyJYtS4apF7s1gAxrywV7t";
     hashOutput = false;
-    sha256 = "ea6bb512c4adfbc4e05b22e4c2d06bddff5b358a53de982273fec846b75bde0c";
+    sha256 = "826a4cb2c099a29c7cf91516ffffcfcb5aace7533b8853a8c8bddcfe2bfb1023";
   };
 
   nativeBuildInputs = [
@@ -66,12 +66,17 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;
-      pgpsigUrls = map (n: "${n}.asc") src.urls;
-      pgpKeyFingerprints = [
-        "B35B F85B F194 89D0 4E28  C33C 2119 4EBB 1657 33EA"
-        "2133 BC60 0AB1 33E1 D826  D173 FE43 009C 4607 B1FB"
-      ];
-      inherit (src) urls outputHash outputHashAlgo;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.asc") src.urls;
+        pgpKeyFingerprints = [
+          "B35B F85B F194 89D0 4E28  C33C 2119 4EBB 1657 33EA"
+          "2133 BC60 0AB1 33E1 D826  D173 FE43 009C 4607 B1FB"
+        ];
+      };
     };
   };
 

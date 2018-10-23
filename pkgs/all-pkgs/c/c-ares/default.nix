fd4bd2d46ecce9a06244dfe5fe6d65bc38c6f98f
William A. Kennington III <william@wkennington.com>
2018-10-23 16:23:57 -0700

    c-ares: 1.14.0 -> 1.15.0

--- pkgs/all-pkgs/c/c-ares/default.nix
+++ pkgs/all-pkgs/c/c-ares/default.nix
@@ -7,26 +7,28 @@ let
     "https://c-ares.haxx.se/download/c-ares-${version}.tar.gz"
   ];
 
-  version = "1.14.0";
+  version = "1.15.0";
 in
 stdenv.mkDerivation rec {
   name = "c-ares-${version}";
 
   src = fetchurl {
     urls = tarballUrls version;
-    multihash = "QmRtEZDegbvV7FBwQv9tADmkJRwEGXWcKbU1obQn1KXTSn";
+    multihash = "QmPSBCLotU8oPBJFvCAh3Tj2RkRta5MJVqD7CPKMJ6Pa5r";
     hashOutput = false;
-    sha256 = "45d3c1fd29263ceec2afc8ff9cd06d5f8f889636eb4e80ce3cc7f0eaf7aadc6e";
+    sha256 = "6cdb97871f2930530c97deb7cf5c8fa4be5a0b02c7cea6e7c7667672a39d6852";
   };
 
   passthru = {
     srcVerification = fetchurl rec {
       inherit (src) outputHashAlgo;
-      urls = tarballUrls "1.14.0";
-      pgpsigUrls = map (n: "${n}.asc") urls;
-      outputHash = "45d3c1fd29263ceec2afc8ff9cd06d5f8f889636eb4e80ce3cc7f0eaf7aadc6e";
-      pgpKeyFingerprint = "27ED EAF2 2F3A BCEB 50DB  9A12 5CC9 08FD B71E 12C2";
+      urls = tarballUrls "1.15.0";
+      outputHash = "6cdb97871f2930530c97deb7cf5c8fa4be5a0b02c7cea6e7c7667672a39d6852";
       failEarly = true;
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.asc") urls;
+        pgpKeyFingerprint = "27ED EAF2 2F3A BCEB 50DB  9A12 5CC9 08FD B71E 12C2";
+      };
     };
   };
 

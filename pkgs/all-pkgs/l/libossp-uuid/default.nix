eb9417e544dd8d11db0c788185d262fbbe5a1dfc
William A. Kennington III <william@wkennington.com>
2018-10-22 18:51:20 -0700

    libossp-uuid: Cleanup

--- pkgs/all-pkgs/l/libossp-uuid/default.nix
+++ pkgs/all-pkgs/l/libossp-uuid/default.nix
@@ -2,15 +2,18 @@
 , fetchurl
 }:
 
+let
+  version = "1.6.2";
+in
 stdenv.mkDerivation rec {
   name = "libossp-uuid-${version}";
-  version = "1.6.2";
 
   src = fetchurl {
     urls = [
       "ftp://ftp.ossp.org/pkg/lib/uuid/uuid-${version}.tar.gz"
       "http://www.mirrorservice.org/sites/ftp.ossp.org/pkg/lib/uuid/uuid-${version}.tar.gz"
     ];
+    multihash = "QmWwswUwjQnnLENKcnE44VzBBg9ngMutjbrpgpfE5NUMfS";
     sha256= "11a615225baa5f8bb686824423f50e4427acd3f70d394765bdff32801f0fd5b0";
   };
 

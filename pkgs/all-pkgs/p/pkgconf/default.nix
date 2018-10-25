8855b2aca0507a8caea2c2322a41a893e23b6f3e
William A. Kennington III <william@wkennington.com>
2018-10-25 15:13:37 -0700

    pkgconf: 1.5.3 -> 1.5.4

--- pkgs/all-pkgs/p/pkgconf/default.nix
+++ pkgs/all-pkgs/p/pkgconf/default.nix
@@ -3,12 +3,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "pkgconf-1.5.3";
+  name = "pkgconf-1.5.4";
 
   src = fetchurl {
     url = "https://distfiles.dereferenced.org/pkgconf/${name}.tar.xz";
-    multihash = "QmYYpMf3Jm1uv3CPqzDV34Boj6YCa11ZPYZPHDLZ9TX2nk";
-    sha256 = "d3468308553c94389dadfd10c4d1067269052b5364276a9d24a643c88485f715";
+    multihash = "QmXuakgijHTF2CYiAXCaUv7uaWtnNfpUy7daCWgDQmg6U6";
+    sha256 = "9c5864a4e08428ef52f05a41c948529555458dec6d283b50f8b7d32463c54664";
   };
 
   configureFlags = [

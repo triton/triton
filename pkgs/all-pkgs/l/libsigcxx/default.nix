51c7a05f2d95fe0cd1aca427e99c980cfa13bfe8
Cody W. Opel <codyopel@gmail.com>
2018-10-30 14:48:43 -0400

    libsigcxx: cleanup

--- pkgs/all-pkgs/l/libsigcxx/default.nix
+++ pkgs/all-pkgs/l/libsigcxx/default.nix
@@ -6,7 +6,13 @@
 }:
 
 let
-  source = (import ./sources.nix { })."${channel}";
+  sources = {
+    "2.10" = {
+      version = "2.10.0";
+      sha256 = "f843d6346260bfcb4426259e314512b99e296e8ca241d771d21ac64f28298d81";
+    };
+  };
+  source = sources."${channel}";
 in
 stdenv.mkDerivation rec {
   name = "libsigc++-${source.version}";
@@ -30,8 +36,10 @@ stdenv.mkDerivation rec {
         outputHash
         outputHashAlgo
         urls;
-      sha256Url = "https://download.gnome.org/sources/libsigc++/${channel}/"
-        + "${name}.sha256sum";
+      fullOpts = {
+        sha256Url = "https://download.gnome.org/sources/libsigc++/${channel}/"
+          + "${name}.sha256sum";
+      };
       failEarly = true;
     };
   };

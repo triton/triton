c02d402c7c808f0f7377df29190f525afaab944a
William A. Kennington III <william@wkennington.com>
2018-11-09 16:38:05 -0800

    jq: 2018-08-17 -> 1.6

--- pkgs/all-pkgs/j/jq/default.nix
+++ pkgs/all-pkgs/j/jq/default.nix
@@ -1,28 +1,16 @@
 { stdenv
-, autoreconfHook
-, fetchFromGitHub
+, fetchurl
 
 , oniguruma
 }:
 
-let
-  date = "2018-08-17";
-  rev = "46d1ce2667253f1a34cd389b6d00c0288ab0276f";
-in
 stdenv.mkDerivation rec {
-  name = "jq-${date}";
+  name = "jq-1.6";
 
-  src = fetchFromGitHub {
-    version = 6;
-    owner = "stedolan";
-    repo = "jq";
-    inherit rev;
-    sha256 = "fe0bd58edda2cd0fb2f4d071d42ce85ff2d1577193647075fd550a289719d5ba";
+  src = fetchurl {
+    url = "https://github.com/stedolan/jq/releases/download/${name}/${name}.tar.gz";
+    sha256 = "9625784cf2e4fd9842f1d407681ce4878b5b0dcddbcd31c6135114a30c71e6a8";
   };
-
-  nativeBuildInputs = [
-    autoreconfHook
-  ];
 
   buildInputs = [
     oniguruma

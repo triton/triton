f5b6683f7f64a9aff994490cca6cbccd0aa198e9
William A. Kennington III <william@wkennington.com>
2018-11-11 03:13:07 -0800

    pcituils: 3.6.1 -> 3.6.2

--- pkgs/all-pkgs/p/pciutils/default.nix
+++ pkgs/all-pkgs/p/pciutils/default.nix
@@ -8,7 +8,7 @@
 }:
 
 let
-  name = "pciutils-3.6.1"; # with database from 2017-11
+  name = "pciutils-3.6.2"; # with database from 2017-11
 
   tarballUrls = [
     "mirror://kernel/software/utils/pciutils/${name}.tar"
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     urls = map (n: "${n}.xz") tarballUrls;
     hashOutput = false;
-    sha256 = "fcc0431cc951c3563f1e4f946d27c8e2161cfd81f25316e6bd783fa6118469e0";
+    sha256 = "db452ec986edefd88af0d222d22f6102f8030a8633fdfe846c3ae4bde9bb93f3";
   };
 
   nativeBuildInputs = [
@@ -38,9 +38,9 @@ stdenv.mkDerivation rec {
   '';
 
   makeFlags = [
-    "SHARED=yes"
     "ZLIB=yes"
     "DNS=yes"
+    "SHARED=yes"
     "LIBKMOD=yes"
     "HWDB=yes"
   ];
@@ -58,10 +58,15 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;
-      pgpsigUrls = map (n: "${n}.sign") tarballUrls;
-      pgpDecompress = true;
-      pgpKeyFingerprint = "5558 F939 9CD7 8368 5055  3C6E C28E 7847 ED70 F82D";
-      inherit (src) urls outputHash outputHashAlgo;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.sign") tarballUrls;
+        pgpDecompress = true;
+        pgpKeyFingerprint = "5558 F939 9CD7 8368 5055  3C6E C28E 7847 ED70 F82D";
+      };
     };
   };
 

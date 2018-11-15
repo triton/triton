f8d87004a8b732769e52bc35cce0a4f5502b00f4
William A. Kennington III <william@wkennington.com>
2018-11-15 01:52:35 -0800

    asciinema: 2.0.0 -> 2.0.1

--- pkgs/all-pkgs/a/asciinema/default.nix
+++ pkgs/all-pkgs/a/asciinema/default.nix
@@ -4,7 +4,7 @@
 }:
 
 let
-  version = "2.0.0";
+  version = "2.0.1";
 in
 buildPythonPackage {
   name = "asciinema-${version}";
@@ -12,7 +12,7 @@ buildPythonPackage {
   src = fetchPyPi {
     package = "asciinema";
     inherit version;
-    sha256 = "be193e2513cd309dd8de5b92d22bd48752076fe269ee3fb56da1052e5acc3768";
+    sha256 = "8d48baa3a263cfb9536540ee545e5c95e43758520d68c6ebc81a092e20c2a4ea";
   };
 
   meta = with stdenv.lib; {

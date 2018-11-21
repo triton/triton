96114a2682403087771013231bd76a615e61398f
Cody W. Opel <codyopel@gmail.com>
2018-11-21 01:28:05 -0500

    gevent: 2018-02-28 -> 1.3.7

--- pkgs/all-pkgs/g/gevent/default.nix
+++ pkgs/all-pkgs/g/gevent/default.nix
@@ -1,8 +1,7 @@
 { stdenv
 , buildPythonPackage
 , cython
-, fetchFromGitHub
-#, fetchPyPi
+, fetchPyPi
 , isPy3
 , isPyPy
 , lib
@@ -18,25 +17,15 @@ let
   inherit (lib)
     optionals;
 
-  version = "2018-02-28";
-  #version = "1.2.2";
+  version = "1.3.7";
 in
 buildPythonPackage rec {
   name = "gevent-${version}";
 
-  # src = fetchPyPi {
-  #   package = "gevent";
-  #   inherit version;
-  #   sha256 = "4791c8ae9c57d6f153354736e1ccab1e2baf6c8d9ae5a77a9ac90f41e2966b2d";
-  # };
-
-  # Switch back to stable releases once 1.3 is released.
-  src = fetchFromGitHub {
-    version = 5;
-    owner = "gevent";
-    repo = "gevent";
-    rev = "a6844125377cdc7002166d9be00b17900674568a";
-    sha256 = "596a86b94817100c0143bf9bd71c7a8003ec7abbfcfdc4ceed5f9a35232325fe";
+  src = fetchPyPi {
+    package = "gevent";
+    inherit version;
+    sha256 = "3f06f4802824c577272960df003a304ce95b3e82eea01dad2637cc8609c80e2c";
   };
 
   nativeBuildInputs = [
@@ -57,8 +46,6 @@ buildPythonPackage rec {
   NIX_CFLAGS_COMPILE = [
     "-I${greenlet}/include/${python.executable}${if isPy3 then "m" else ""}"
   ];
-
-  buildDirCheck = false;  # FIXME
 
   meta = with lib; {
     description = "Coroutine-based networking library";

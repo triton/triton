a29d5fe682ce4c192aceeab01d0e239a1e2d1028
William A. Kennington III <william@wkennington.com>
2019-01-05 01:10:23 -0800

    groff: 1.22.3 -> 1.22.4

--- pkgs/all-pkgs/g/groff/default.nix
+++ pkgs/all-pkgs/g/groff/default.nix
@@ -3,16 +3,23 @@
 , perl
 }:
 
+let
+  tarballUrls = version: [
+    "mirror://gnu/groff/groff-${version}.tar.gz"
+  ];
+
+  version = "1.22.4";
+in
 stdenv.mkDerivation rec {
-  name = "groff-1.22.3";
+  name = "groff-${version}";
 
   src = fetchurl {
-    url = "mirror://gnu/groff/${name}.tar.gz";
+    urls = tarballUrls version;
     hashOutput = false;
-    sha256 = "1998v2kcs288d3y7kfxpvl369nqi06zbbvjzafyvyl3pr7bajj1s";
+    sha256 = "e78e7b4cb7dec310849004fa88847c44701e8d133b5d4c13057d876c1bad0293";
   };
 
-  nativeBuildInputs = [
+  buildInputs = [
     perl
   ];
 
@@ -27,6 +34,19 @@ stdenv.mkDerivation rec {
   '';
 
   buildParallel = false;
+
+  passthru = {
+    srcVerification = fetchurl rec {
+      failEarly = true;
+      urls = tarballUrls "1.22.4";
+      inherit (src) outputHashAlgo;
+      outputHash = "e78e7b4cb7dec310849004fa88847c44701e8d133b5d4c13057d876c1bad0293";
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.sig") urls;
+        pgpKeyFingerprint = "2D0C 08D2 B0AD 0D3D 8626  6702 72D2 3FBA C99D 4E75";
+      };
+    };
+  };
 
   meta = with stdenv.lib; {
     homepage = http://www.gnu.org/software/groff/;

7116ba9a6936f4b14a346d34c78ae2c70c109341
William A. Kennington III <william@wkennington.com>
2019-01-05 01:54:04 -0800

    libtiff: 4.0.9 -> 4.0.10

--- pkgs/all-pkgs/l/libtiff/default.nix
+++ pkgs/all-pkgs/l/libtiff/default.nix
@@ -6,18 +6,20 @@
 , libjpeg
 , xz
 , zlib
+, zstd
 }:
 
 let
-  version = "4.0.9";
+  version = "4.0.10";
 in
 stdenv.mkDerivation rec {
   name = "libtiff-${version}";
 
   src = fetchurl {
     url = "https://download.osgeo.org/libtiff/tiff-${version}.tar.gz";
-    multihash = "QmQCAPYk7dbLjNhJrQ9X8jvztSsLQ62hkqtEZqB8mBByVQ";
-    sha256 = "6e7bdeec2c310734e734d19aae3a71ebe37a4d842e0e23dbb1b8921c0026cfcd";
+    multihash = "QmfUyXXCpjnQkoxD2qsZycKyFKD9S1nH1tP89dztgrFDaM";
+    hashOutput = false;
+    sha256 = "2c52d11ccaf767457db0c46795d9c7d1a8d8f76f68b0b800a3dfe45786b996e4";
   };
 
   buildInputs = [
@@ -25,40 +27,22 @@ stdenv.mkDerivation rec {
     libjpeg
     xz
     zlib
+    zstd
   ];
 
-  patches = [
-    (fetchTritonPatch {
-      rev = "1573e5285d1efb2e011b02dc29cf545a2dfc0d23";
-      file = "l/libtiff/CVE-2017-9935.patch";
-      sha256 = "a18bd3e28931a723639e51bb3386ab6b710f5ca0930b6d85c7123e898190f31e";
-    })
-    (fetchTritonPatch {
-      rev = "34ab40a8883e1c64d26c5c1833e76fc605275a4c";
-      file = "l/libtiff/CVE-2017-18013.patch";
-      sha256 = "60fcb294a55617cb0619be21733e10226e6f2bdbf1ff216df7b083f0d4580e58";
-    })
-    (fetchTritonPatch {
-      rev = "34ab40a8883e1c64d26c5c1833e76fc605275a4c";
-      file = "l/libtiff/CVE-2018-10963.patch";
-      sha256 = "5324020b92212644b9a1d00e48c4992efbc94b83e4d72925ebe8d7774b61d1d7";
-    })
-    (fetchTritonPatch {
-      rev = "34ab40a8883e1c64d26c5c1833e76fc605275a4c";
-      file = "l/libtiff/CVE-2018-5784.patch";
-      sha256 = "2a2ac96ce07320dfd08ae59b11973512cfeff4cb77ef4135194830a31a6ce92e";
-    })
-    (fetchTritonPatch {
-      rev = "34ab40a8883e1c64d26c5c1833e76fc605275a4c";
-      file = "l/libtiff/CVE-2018-7456.patch";
-      sha256 = "a31f0ddc8ab7e2a95ccf7159b06d8da25e152852a0d327a741a1d8d6e5b67d89";
-    })
-    (fetchTritonPatch {
-      rev = "34ab40a8883e1c64d26c5c1833e76fc605275a4c";
-      file = "l/libtiff/CVE-2018-8905.patch";
-      sha256 = "4dba6d7fe8b2f224f874027c858aa15ec8f8aebacea65f0afec677691e629a89";
-    })
-  ];
+  passthru = {
+    srcVerification = fetchurl rec {
+      failEarly = true;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.sig") src.urls;
+        pgpKeyFingerprint = "EBDF DB21 B020 EE8F D151  A88D E301 047D E119 8975";
+      };
+    };
+  };
 
   meta = with stdenv.lib; {
     description = "Library and utilities for working with the TIFF image file format";

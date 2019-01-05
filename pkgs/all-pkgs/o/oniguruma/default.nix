1f2adc303d9ef5ad76dd6919dc8a61279c5100d1
William A. Kennington III <william@wkennington.com>
2019-01-05 01:54:52 -0800

    oniguruma: 6.9.0 -> 6.9.1

--- pkgs/all-pkgs/o/oniguruma/default.nix
+++ pkgs/all-pkgs/o/oniguruma/default.nix
@@ -3,14 +3,14 @@
 }:
 
 let
-  version = "6.9.0";
+  version = "6.9.1";
 in
 stdenv.mkDerivation rec {
   name = "oniguruma-${version}";
 
   src = fetchurl {
     url = "https://github.com/kkos/oniguruma/releases/download/v${version}/onig-${version}.tar.gz";
-    sha256 = "91bfb25e050ce3c301483204dd3f0d03a7c05472e20d48fe227a383d4534e7c9";
+    sha256 = "c7c3feb7be45a5cc9f2dec239b4a317a422e6ffea299cf91ffab1b926633ea12";
   };
 
   meta = with stdenv.lib; {

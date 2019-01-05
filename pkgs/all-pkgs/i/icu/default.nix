c115f2a848948dc40fc7e7994061d761187a6ab3
William A. Kennington III <william@wkennington.com>
2019-01-05 02:06:56 -0800

    icu: 62.1 -> 63.1

--- pkgs/all-pkgs/i/icu/default.nix
+++ pkgs/all-pkgs/i/icu/default.nix
@@ -10,16 +10,16 @@ let
     "http://download.icu-project.org/files/icu4c/${v}/icu4c-${replaceChars ["."] ["_"] v}-src.tgz"
   ];
 
-  version = "62.1";
+  version = "63.1";
 in
 stdenv.mkDerivation rec {
   name = "icu4c-${version}";
 
   src = fetchurl {
     urls = tarballUrls version;
-    multihash = "QmVYj1nbFT2pGp7WPvPGd9XryaBLuHLTf3RERsorXdH37Q";
+    multihash = "QmcwUadnqKHdhksCRvvgsV4WK4nqM18X3gKnGY5oPJ75nS";
     hashOutput = false;
-    sha256 = "3dd9868d666350dda66a6e305eecde9d479fb70b30d5b55d78a1deffb97d5aa3";
+    sha256 = "05c490b69454fce5860b7e8e2821231674af0a11d7ef2febea9a32512998cb9d";
   };
 
   postUnpack = ''
@@ -36,15 +36,19 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
-      urls = tarballUrls "62.1";
-      pgpsigUrls = map (n: "${n}.asc") urls;
-      pgpKeyFingerprints = [
-        "9731 166C D8E2 3A83 BEE7  C6D3 ACA5 DBE1 FD8F ABF1"
-        # Fredrik Roubert <fredrik@roubert.name>
-        "FFA9 129A 180D 765B 7A5B  EA1C 9B43 2B27 D1BA 20D7"
-      ];
-      outputHash = "3dd9868d666350dda66a6e305eecde9d479fb70b30d5b55d78a1deffb97d5aa3";
+      urls = tarballUrls "63.1";
       inherit (src) outputHashAlgo;
+      outputHash = "05c490b69454fce5860b7e8e2821231674af0a11d7ef2febea9a32512998cb9d";
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.asc") urls;
+        pgpKeyFingerprints = [
+          "9731 166C D8E2 3A83 BEE7  C6D3 ACA5 DBE1 FD8F ABF1"
+          # Fredrik Roubert <fredrik@roubert.name>
+          "FFA9 129A 180D 765B 7A5B  EA1C 9B43 2B27 D1BA 20D7"
+          # Steven R. Loomis (IBM) <srloomis@us.ibm.com>
+          "E409 8B78 AFC9 4394 F3F4  9AA9 0399 6C7C 83F1 2F11"
+        ];
+      };
     };
   };
 

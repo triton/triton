5dca4ae3dd8db4cc1b9f5dbe2a809050a77a52db
William A. Kennington III <william@wkennington.com>
2019-01-20 21:15:27 -0800

    thrify: 0.11.0 -> 0.12.0

--- pkgs/all-pkgs/t/thrift/default.nix
+++ pkgs/all-pkgs/t/thrift/default.nix
@@ -8,7 +8,7 @@
 }:
 
 let
-  version = "0.11.0";
+  version = "0.12.0";
 in
 stdenv.mkDerivation rec {
   name = "thrift-${version}";
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "mirror://apache/thrift/${version}/${name}.tar.gz";
     hashOutput = false;
-    sha256 = "c4ad38b6cb4a3498310d405a91fef37b9a8e79a50cd0968148ee2524d2fa60c2";
+    sha256 = "c336099532b765a6815173f62df0ed897528a9d551837d627c1f87fadad90428";
   };
 
   buildInputs = [
@@ -35,14 +35,21 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;
-      pgpsigUrls = map (n: "${n}.asc") src.urls;
-      pgpKeyFingerprints = [
-        # Jake Farrell
-        "9782 694B 8B54 B4AD D345  E52A BB06 368F 66B7 78F9"
-        # Jens Geyer
-        "8CD8 7F18 6F06 E958 EFCA  963D 76BD 340F C4B7 5865"
-      ];
-      inherit (src) urls outputHash outputHashAlgo;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.asc") src.urls;
+        pgpKeyFingerprints = [
+          # Jake Farrell
+          "9782 694B 8B54 B4AD D345  E52A BB06 368F 66B7 78F9"
+          # Jens Geyer
+          "8CD8 7F18 6F06 E958 EFCA  963D 76BD 340F C4B7 5865"
+          # James E King
+          "9348 F036 9A20 8184 00F8  7140 C6F2 B11B EDD0 2683"
+        ];
+      };
     };
   };
 

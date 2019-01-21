3a2aa6295c88d691a646969721b1d75333412f92
William A. Kennington III <william@wkennington.com>
2019-01-20 18:37:11 -0800

    libmicrohttpd: 0.9.59 -> 0.9.62

--- pkgs/all-pkgs/l/libmicrohttpd/default.nix
+++ pkgs/all-pkgs/l/libmicrohttpd/default.nix
@@ -6,12 +6,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "libmicrohttpd-0.9.59";
+  name = "libmicrohttpd-0.9.62";
 
   src = fetchurl {
     url = "mirror://gnu/libmicrohttpd/${name}.tar.gz";
     hashOutput = false;
-    sha256 = "9b9ccd7d0b11b0e179f1f58dc2caa3e0c62c8609e1e1dc7dcaadf941b67d923c";
+    sha256 = "bd3e097d703f5091a6a01b56c0464a90fdd17f5d50478cea50a346b25c88db49";
   };
 
   buildInputs = [
@@ -28,13 +28,18 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
-      pgpsigUrls = map (n: "${n}.sig") src.urls;
-      pgpKeyFingerprints = [
-        "D842 3BCB 326C 7907 0339  29C7 939E 6BE1 E29F C3CC"
-        # Evgeny Grin (Karlson2k)
-        "289F E99E 138C F6D4 73A3  F0CF BF7A C4A5 EAC2 BAF4"
-      ];
-      inherit (src) urls outputHash outputHashAlgo;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.sig") src.urls;
+        pgpKeyFingerprints = [
+          "D842 3BCB 326C 7907 0339  29C7 939E 6BE1 E29F C3CC"
+          # Evgeny Grin (Karlson2k)
+          "289F E99E 138C F6D4 73A3  F0CF BF7A C4A5 EAC2 BAF4"
+        ];
+      };
     };
   };
 

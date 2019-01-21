ac68d4f6b2bac0b428995b9f42b54792abf99154
William A. Kennington III <william@wkennington.com>
2019-01-21 00:26:18 -0800

    wget: 1.20 -> 1.20.1

--- pkgs/all-pkgs/w/wget/default.nix
+++ pkgs/all-pkgs/w/wget/default.nix
@@ -12,13 +12,9 @@
 , libunistring
 , lzip
 , openssl
-, pcre
+, pcre2_lib
 , util-linux_lib
 , zlib
-
-, perl
-, perlPackages
-, python3
 }:
 
 let
@@ -27,16 +23,17 @@ let
     optionalString;
 in
 stdenv.mkDerivation rec {
-  name = "wget-1.19.5";
+  name = "wget-1.20.1";
 
   src = fetchurl {
     url = "mirror://gnu/wget/${name}.tar.lz";
     hashOutput = false;
-    sha256 = "29fbe6f3d5408430c572a63fe32bd43d5860f32691173dfd84edc06869edca75";
+    sha256 = "0f63e84dd23dc53ab3ab6f483c3afff8301e54c165783f772101cdd9b1c64928";
   };
 
   nativeBuildInputs = [
     gettext
+    lzip
   ];
 
   buildInputs = [
@@ -46,63 +43,30 @@ stdenv.mkDerivation rec {
     libmetalink
     libpsl
     libunistring
-    lzip
     openssl
-    pcre
+    pcre2_lib
     util-linux_lib
     zlib
-  ] ++ optionals doCheck [
-    perl
-    perlPackages.IOSocketSSL
-    perlPackages.LWP
-    python3
   ];
-
-  # The configure.ac is bad and doesn't set the rpath
-  # even though it uses libidn2
-  NIX_LDFLAGS = "-rpath ${libidn2}/lib";
-
-  patches = [
-    (fetchTritonPatch {
-      rev = "fa395249911cd2e87c55ef493b7a2be43ec16abd";
-      file = "w/wget/fix-ptimer.patch";
-      sha256 = "fa155549e97376e7a9668a45aa3052536054f019abe4db9506dc6bfed41da4be";
-    })
-  ];
-
-  postPatch = ''
-    for i in "doc/texi2pod.pl" "util/rmold.pl" ; do
-      sed -i "$i" \
-        -e 's|/usr/bin.*perl|${perl}/bin/perl|g'
-    done
-  '' + optionalString doCheck ''
-    # Work around lack of DNS resolution in chroots.
-    for i in "tests/"*.pm "tests/"*.px ; do
-      sed -i "$i" \
-        -e 's/localhost/127.0.0.1/g'
-    done
-  '';
 
   configureFlags = [
-    "--disable-debug"
     "--with-ssl=openssl"
     "--with-metalink"
     "--with-cares"
     "--with-openssl"
   ];
 
-  makeFlags = [
-    "V=1"
-  ];
-
-  doCheck = false;
-
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;
-      pgpsigUrls = map (n: "${n}.sig") src.urls;
-      pgpKeyFingerprint = "1CB2 7DBC 9861 4B2D 5841  646D 0830 2DB6 A267 0428";
-      inherit (src) urls outputHash outputHashAlgo;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.sig") src.urls;
+        pgpKeyFingerprint = "1CB2 7DBC 9861 4B2D 5841  646D 0830 2DB6 A267 0428";
+      };
     };
   };
 

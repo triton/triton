3f62c9e9c38c1129f44ee26d0808a81dd96cecbe
William A. Kennington III <william@wkennington.com>
2019-01-21 00:31:05 -0800

    libraw: 0.19.0 -> 0.19.2

--- pkgs/all-pkgs/l/libraw/default.nix
+++ pkgs/all-pkgs/l/libraw/default.nix
@@ -8,14 +8,14 @@
 }:
 
 let
-  version = "0.19.0";
+  version = "0.19.2";
 in
 stdenv.mkDerivation rec {
   name = "libraw-${version}";
 
   src = fetchurl {
     url = "https://www.libraw.org/data/LibRaw-${version}.tar.gz";
-    multihash = "QmRVsUUTDEy55LaiwBWv8niZ1xRVdfnVEMdEsWM3Je8pwb";
+    multihash = "QmUBzUPTkFVdUkaLnSstahvobWRq4ovNaG3QhyNyJSAFrN";
     sha256 = "e83f51e83b19f9ba6b8bd144475fc12edf2d7b3b930d8d280bdebd8a8f3ed259";
   };
 

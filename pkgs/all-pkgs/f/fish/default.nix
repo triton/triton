af6a4b84ef2207d0fc99cd612c82e2892f845a57
William A. Kennington III <william@wkennington.com>
2019-01-20 17:52:29 -0800

    fish: 2.7.1 -> 3.0.0

--- pkgs/all-pkgs/f/fish/default.nix
+++ pkgs/all-pkgs/f/fish/default.nix
@@ -3,12 +3,12 @@
 , gettext
 
 , ncurses
-, pcre2
+, pcre2_lib
 , python3
 }:
 
 let
-  version = "2.7.1";
+  version = "3.0.0";
 in
 stdenv.mkDerivation rec {
   name = "fish-${version}";
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "https://github.com/fish-shell/fish-shell/releases/download/${version}/${name}.tar.gz";
     hashOutput = false;
-    sha256 = "e42bb19c7586356905a58578190be792df960fa81de35effb1ca5a5a981f0c5a";
+    sha256 = "ea9dd3614bb0346829ce7319437c6a93e3e1dfde3b7f6a469b543b0d2c68f2cf";
   };
 
   nativeBuildInputs = [
@@ -25,8 +25,14 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     ncurses
-    pcre2
+    pcre2_lib
   ];
+
+  postPatch = ''
+    # Hack around building the version file from the git tree
+    echo "FISH_BUILD_VERSION = '${version}'" >FISH-BUILD-VERSION-FILE
+    sed -i 's,^FISH-BUILD-VERSION-FILE:.*$,FISH-BUILD-VERSION-FILE:,' Makefile.in
+  '';
 
   configureFlags = [
     "--without-included-pcre2"
@@ -40,9 +46,14 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
-      pgpsigUrl = map (n: "${n}.asc") urls;
-      pgpKeyFingerprint = "0038 3798 6104 8788 35FA  516D 7A67 D962 D88A 709A ";
-      inherit (src) urls outputHash outputHashAlgo;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+      fullOpts = {
+        pgpsigUrl = map (n: "${n}.asc") urls;
+        pgpKeyFingerprint = "0038 3798 6104 8788 35FA  516D 7A67 D962 D88A 709A ";
+      };
     };
   };
 

c7c8c832d4fd0f135e0706c54c123b62177e5860
William A. Kennington III <william@wkennington.com>
2019-01-20 17:10:51 -0800

    bcachefs-tools: 2018-10-21 -> 2019-01-14

--- pkgs/all-pkgs/b/bcachefs-tools/default.nix
+++ pkgs/all-pkgs/b/bcachefs-tools/default.nix
@@ -14,8 +14,8 @@
 }:
 
 let
-  date = "2018-10-21";
-  rev = "67fb317a0706ec3d305f4b02b3fa4b75717cd848";
+  date = "2019-01-14";
+  rev = "8630059e6ac363bea39e54df3f8115da5a5c2c5d";
 in
 stdenv.mkDerivation {
   name = "bcachefs-tools-${date}";
@@ -23,8 +23,8 @@ stdenv.mkDerivation {
   src = fetchzip {
     version = 6;
     url = "https://evilpiepirate.org/git/bcachefs-tools.git/snapshot/bcachefs-tools-${rev}.tar.xz";
-    multihash = "QmSBFrirXfgtSvpRiNTH3wrqVAMRESBZSmA7kVDtY4cG1S";
-    sha256 = "49923eb0817cf3f8ea311680e77024882a508e77412708875d673aacfed7b9b5";
+    multihash = "QmZ55yYDPRW9ZhH8doR5LqDWrWFJmBBYrTZKdBWNPdj5Xh";
+    sha256 = "ad8f6433a18fa8525499ea4cb1a97fa008653ab3aeaf7b43022c03300f2e55a2";
   };
 
   buildInputs = [
@@ -39,11 +39,6 @@ stdenv.mkDerivation {
     zlib
     zstd
   ];
-
-  postPatch = ''
-    grep -q 'attr/xattr.h' cmd_migrate.c
-    sed -i 's,attr/xattr.h,sys/xattr.h,' cmd_migrate.c
-  '';
 
   preBuild = ''
     makeFlagsArray+=(

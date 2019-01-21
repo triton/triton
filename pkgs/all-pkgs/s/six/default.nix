9f4b931bdb510b2c85824951ce53b7d208dcfd57
William A. Kennington III <william@wkennington.com>
2019-01-20 17:42:16 -0800

    six: 1.11.0 -> 1.12.0

--- pkgs/all-pkgs/s/six/default.nix
+++ pkgs/all-pkgs/s/six/default.nix
@@ -5,7 +5,7 @@
 }:
 
 let
-  version = "1.11.0";
+  version = "1.12.0";
 in
 buildPythonPackage rec {
   name = "six-${version}";
@@ -13,7 +13,7 @@ buildPythonPackage rec {
   src = fetchPyPi {
     package = "six";
     inherit version;
-    sha256 = "70e8a77beed4562e7f14fe23a786b54f6296e34344c23bc42f07b15018ff98e9";
+    sha256 = "d16a0141ec1a18405cd4ce8b4613101da75da0e9a7aec5bdd4fa804d0e0eba73";
   };
 
   passthru = {

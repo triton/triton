955795f9c218a2c43df41d291b40c46e4ced1e12
William A. Kennington III <william@wkennington.com>
2019-01-20 18:40:32 -0800

    liburcu: 0.10.1 -> 0.10.2

--- pkgs/all-pkgs/l/liburcu/default.nix
+++ pkgs/all-pkgs/l/liburcu/default.nix
@@ -3,26 +3,28 @@
 }:
 
 let
-  version = "0.10.1";
+  version = "0.10.2";
 in
 stdenv.mkDerivation rec {
   name = "liburcu-${version}";
 
   src = fetchurl {
     url = "https://lttng.org/files/urcu/userspace-rcu-${version}.tar.bz2";
-    multihash = "Qmcu4rhao4yvbSvcodKsreGa38P7SSzHSZuXiBjd1hsKoY";
+    multihash = "QmYqRUfnbKPNZ2V3hmVqvy58vKzAD3iQatkKqWAwKXX8FK";
     hashOutput = false;
-    sha256 = "9c09220be4435dc27fcd22d291707b94b97f159e0c442fbcd60c168f8f79eb06";
+    sha256 = "b3f6888daf6fe02c1f8097f4a0898e41b5fe9975e121dc792b9ddef4b17261cc";
   };
 
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;
-      md5Urls = map (n: "${n}.md5") src.urls;
-      sha1Urls = map (n: "${n}.sha1") src.urls;
-      pgpsigUrls = map (n: "${n}.asc") src.urls;
-      pgpKeyFingerprint = "2A0B 4ED9 15F2 D3FA 45F5  B162 1728 0A97 8118 6ACF";
       inherit (src) urls outputHash outputHashAlgo;
+      fullOpts = {
+        md5Urls = map (n: "${n}.md5") src.urls;
+        sha1Urls = map (n: "${n}.sha1") src.urls;
+        pgpsigUrls = map (n: "${n}.asc") src.urls;
+        pgpKeyFingerprint = "2A0B 4ED9 15F2 D3FA 45F5  B162 1728 0A97 8118 6ACF";
+      };
     };
   };
 

3c237897b1ddff9614ed068278d3ed76a1625c19
William A. Kennington III <william@wkennington.com>
2019-02-11 18:55:59 -0800

    libidn2: 2.1.0 -> 2.1.1

--- pkgs/all-pkgs/l/libidn2/default.nix
+++ pkgs/all-pkgs/l/libidn2/default.nix
@@ -3,7 +3,7 @@
 }:
 
 let
-  version = "2.1.0";
+  version = "2.1.1";
 
   tarballUrls = version: [
     "mirror://gnu/libidn/libidn2-${version}.tar.gz"
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     urls = tarballUrls version;
     hashOutput = false;
-    sha256 = "032398dbaa9537af43f51a8d94e967e3718848547b1b2a4eb3138b20cad11d32";
+    sha256 = "95416080329298a13269e13175041b530cec3d98b54cafae9424b8dfd22078b1";
   };
 
   configureFlags = [
@@ -25,9 +25,9 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
-      urls = tarballUrls "2.1.0";
+      urls = tarballUrls "2.1.1";
       inherit (src) outputHashAlgo;
-      outputHash = "032398dbaa9537af43f51a8d94e967e3718848547b1b2a4eb3138b20cad11d32";
+      outputHash = "95416080329298a13269e13175041b530cec3d98b54cafae9424b8dfd22078b1";
       fullOpts = {
         pgpsigUrls = map (n: "${n}.sig") urls;
         pgpKeyFingerprint = "1CB2 7DBC 9861 4B2D 5841  646D 0830 2DB6 A267 0428";

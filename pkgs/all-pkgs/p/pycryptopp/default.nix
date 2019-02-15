97d5f3572005b5ee54c1aa58f6d3dfb8595463b8
William A. Kennington III <william@wkennington.com>
2019-02-14 19:25:44 -0800

    pycryptopp: Add cryptopp support

--- pkgs/all-pkgs/p/pycryptopp/default.nix
+++ pkgs/all-pkgs/p/pycryptopp/default.nix
@@ -1,6 +1,8 @@
 { stdenv
 , buildPythonPackage
 , fetchPyPi
+
+, cryptopp
 }:
 
 let
@@ -14,6 +16,15 @@ buildPythonPackage {
     inherit version;
     sha256 = "08ad57a1a39b7ed23c173692281da0b8d49d98ad3dcc09f8cca6d901e142699f";
   };
+
+  buildInputs = [
+    #cryptopp
+  ];
+
+  configureFlags = [
+    # TODO: Make work with cryptopp 8.0
+    #"--disable-embedded-cryptopp"
+  ];
 
   meta = with stdenv.lib; {
     maintainers = with maintainers; [

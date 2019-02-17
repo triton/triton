63f424d24addb483d62205dc238812557559f6df
Cody W. Opel <codyopel@gmail.com>
2019-02-16 22:19:17 -0500

    amrwb: amrwb_3gpp Rel-14.1 -> Rel-16

--- pkgs/all-pkgs/a/amrwb/default.nix
+++ pkgs/all-pkgs/a/amrwb/default.nix
@@ -8,13 +8,12 @@ stdenv.mkDerivation rec {
   name = "amrwb-11.0.0.0";
 
   # http://www.3gpp.org/DynaReport/26204.htm
-  # NOTE: When updating amrwb-3gpp, update every instance of 26204-e10 to the
-  #       updated file name.
+  amrwb_3gpp_version = "26204-g00";  # Rel-16
   amrwb_3gpp = fetchurl {
-    # Rel-14.1
-    url = http://www.3gpp.org/ftp/Specs/archive/26_series/26.204/26204-e10.zip;
-    multihash = "QmeAnGzpXFmjkowDLJCMPfG6Tfa3yUw5QyyECamJEvKgV2";
-    sha256 = "68cfe06d38eda2ab12a194175d62ef1cee9327a99e98b24dbad9e84545bd7f2b";
+    url = "http://www.3gpp.org/ftp/Specs/archive/26_series/26.204/"
+      + "${amrwb_3gpp_version}.zip";
+    multihash = "QmRouou9GMpSEPdW3MCs9qiJVVE1Q3fNLm7H5LKCDpZ23d";
+    sha256 = "453cb352be4535abf72842f571e739a251210ddfecdb0d4d1272422ace0b24ce";
   };
 
   src = fetchurl {
@@ -31,7 +30,13 @@ stdenv.mkDerivation rec {
     sed -i Makefile.{in,am} \
       -i configure{,.ac} \
       -i prepare_sources.sh.in \
-      -e 's/26204-b00/26204-e10/g'
+      -e 's/26204-b00/${amrwb_3gpp_version}/g'
+  '' + /* Fix extracted path */ ''
+    sed -i prepare_sources.sh.in \
+      -e '/@UNZIP@ ${amrwb_3gpp_version}_ANSI/a mv -v ${amrwb_3gpp_version}_ANSI-C_source_code/c-code/ c-code/'
+  '' + /* Remove obsolete patch */ ''
+    sed -i prepare_sources.sh.in \
+      -e '/amrwb-intsizes.patch/d'
   '';
 
   configureFlags = [
@@ -39,7 +44,7 @@ stdenv.mkDerivation rec {
   ];
 
   postConfigure = ''
-    cp -v $amrwb_3gpp 26204-e10.zip
+    cp -v "$amrwb_3gpp" '${amrwb_3gpp_version}.zip'
   '';
 
   meta = with lib; {

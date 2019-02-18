980fad7909ed1155b4568fdbfdacef7e3befce16
Cody W. Opel <codyopel@gmail.com>
2019-02-18 02:30:33 -0500

    shntool: add wrapper

--- pkgs/all-pkgs/s/shntool/default.nix
+++ pkgs/all-pkgs/s/shntool/default.nix
@@ -1,34 +1,43 @@
 { stdenv
 , fetchurl
+, lib
+, makeWrapper
+
 #, alac-decoder
 , flac
-#, mac
+, mac
 #, shorten
 , sox
 , wavpack
 }:
 
-stdenv.mkDerivation rec {
+let
   version = "3.0.10";
+in
+stdenv.mkDerivation rec {
   name = "shntool-${version}";
 
   src = fetchurl {
-    url = "http://www.etree.org/shnutils/shntool/dist/src/${name}.tar.gz";
+    url = "http://shnutils.freeshell.org/shntool/dist/src/${name}.tar.gz";
+    multihash = "QmXkBizEKhTxjqK2effFa6roHV8E4mJQHUyELwqMeCVBgc";
     sha256 = "74302eac477ca08fb2b42b9f154cc870593aec8beab308676e4373a5e4ca2102";
   };
 
-  buildInputs = [
-    #alac-decoder
-    flac
-    #mac
-    #shorten
-    sox
-    wavpack
+  nativeBuildInputs = [
+    makeWrapper
   ];
 
-  meta = with stdenv.lib; {
-    description = "Multi-purpose WAVE data processing and reporting utility";
-    homepage = http://www.etree.org/shnutils/shntool/;
+  postInstall = ''
+    wrapProgram "$out"/bin/shntool \
+      --prefix PATH : "${flac}/bin" \
+      --prefix PATH : "${mac}/bin" \
+      --prefix PATH : "${sox}/bin" \
+      --prefix PATH : "${wavpack}/bin"
+  '';
+
+  meta = with lib; {
+    description = "WAVE data processing and reporting utility";
+    homepage = http://shnutils.freeshell.org/shntool/;
     license = licenses.gpl2Plus;
     maintainers = with maintainers; [
       codyopel

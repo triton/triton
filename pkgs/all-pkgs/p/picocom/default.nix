7ad720a40a35f06ed5aa57e228ab61db94e4893d
William A. Kennington III <william@wkennington.com>
2019-02-19 13:42:32 -0800

    picocom: 2.2 -> 2018-04-12

--- pkgs/all-pkgs/p/picocom/default.nix
+++ pkgs/all-pkgs/p/picocom/default.nix
@@ -3,17 +3,18 @@
 }:
 
 let
-  version = "2.2";
+  date = "2018-04-12";
+  rev = "1acf1ddabaf3576b4023c4f6f09c5a3e4b086fb8";
 in
 stdenv.mkDerivation rec {
-  name = "picocom-${version}";
+  name = "picocom-${date}";
 
   src = fetchFromGitHub {
-    version = 2;
+    version = 6;
     owner = "npat-efault";
     repo = "picocom";
-    rev = version;
-    sha256 = "8e03bbacc32f48dc94703335b4375e257c48b0166f9d32b2a5009977a7554f17";
+    inherit rev;
+    sha256 = "00d5678dd17c066aae71908556690c3bfa45b468704c6638fb82a34e704ad35d";
   };
   
   installPhase = ''

a81acb2b0d88018bda8796e24bb0f745410f25a0
Cody W. Opel <codyopel@gmail.com>
2019-02-20 17:43:21 -0500

    bs1770gain: 0.4.12 -> 0.5.2

--- pkgs/all-pkgs/b/bs1770gain/default.nix
+++ pkgs/all-pkgs/b/bs1770gain/default.nix
@@ -7,29 +7,20 @@
 }:
 
 let
-  version = "0.4.12";
+  version = "0.5.2";
 in
 stdenv.mkDerivation rec {
   name = "bs1770gain-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/bs1770gain/bs1770gain/${version}/${name}.tar.gz";
-    sha256 = "cafc5440cf4940939c675e98c8dbeb839f4965d60f74270a37d4ee70559b3a59";
+    sha256 = "73e5738786b57afb89582333ed18206fd2c6d5245717d3b24ace7f7670f9dedc";
   };
 
   buildInputs = [
     ffmpeg
     sox
   ];
-
-  postPatch = ''
-    #ls -al ; return 1
-    sed -i libffsox-2/ffsox_frame_writer.c \
-      -i libffsox-2/ffsox_frame.c \
-      -i libffsox-2/ffsox_packet_writer.c \
-      -e 's/CODEC_FLAG_GLOBAL_HEADER/AV_CODEC_FLAG_GLOBAL_HEADER/g' \
-      -e 's/CODEC_CAP_VARIABLE_FRAME_SIZE/AV_CODEC_CAP_VARIABLE_FRAME_SIZE/'
-  '';
 
   meta = with lib; {
     description = "A audio/video loudness scanner implementing ITU-R BS.1770";

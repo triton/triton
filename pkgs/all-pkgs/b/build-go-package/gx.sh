e1cf154e4fe91d4774d91efe53b5a48b14cbfc9b
William A. Kennington III <william@wkennington.com>
2019-02-20 15:52:45 -0800

    build-go-package: GX should be rewritten in parallel

--- pkgs/all-pkgs/b/build-go-package/gx.sh
+++ pkgs/all-pkgs/b/build-go-package/gx.sh
@@ -1,3 +1,34 @@
+gxRewrite() {
+  local pkg="$1"
+
+  if [[ "$pkg" == *example* ]]; then
+    return 0
+  fi
+
+  local url
+  if ! url="$(jq -r '.gx.dvcsimport' "$pkg/package.json")"; then
+    return 0
+  fi
+  if [ "$url" = "null" ]; then
+    return 0
+  fi
+
+  local name="${pkg#go/src/}"
+  echo "Rewriting $name"
+
+  # Patch go files for dependencies
+  ln -sr go/src "$pkg/vendor"
+  pushd "$pkg" >/dev/null
+  gx-go rewrite
+  popd >/dev/null
+  rm "$pkg/vendor"
+
+  # Rewrite imports for self
+  # We can't just symlink in case we need non-versioned imports to work
+  find "$pkg" -type f -name \*.go -exec sed -i "s#\"$url\\(/\\|\"\\)#\"$name\\1#" {} \;
+}
+export -f gxRewrite
+
 gxConfigure() {
   # Move all gx dependencies out of the vendor directories
   local gxdir
@@ -15,34 +46,8 @@ gxConfigure() {
   fi
 
   # Find each gx package
-  local pkg
-  for pkg in $(find go/src -name package.json -exec dirname {} \;); do
-    if [[ "$pkg" == *example* ]]; then
-      continue
-    fi
-
-    local url
-    if ! url="$(jq -r '.gx.dvcsimport' "$pkg/package.json")"; then
-      continue
-    fi
-    if [ "$url" = "null" ]; then
-      continue
-    fi
-
-    local name="${pkg#go/src/}"
-    echo "Rewriting $name"
-
-    # Patch go files for dependencies
-    ln -sr go/src "$pkg/vendor"
-    pushd "$pkg" >/dev/null
-    gx-go rewrite
-    popd >/dev/null
-    rm "$pkg/vendor"
-
-    # Rewrite imports for self
-    # We can't just symlink in case we need non-versioned imports to work
-    find "$pkg" -type f -name \*.go -exec sed -i "s#\"$url\\(/\\|\"\\)#\"$name\\1#" {} \;
-  done
+  find go/src -name package.json -exec dirname {} \; | \
+    parallel -j "$NIX_BUILD_CORES" gxRewrite
 }
 
 buildFlagsArray+=(

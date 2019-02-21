1b821576f2cd68c449dd5b07d4cb6ee4ac1f6c13
Cody W. Opel <codyopel@gmail.com>
2019-02-20 19:43:37 -0500

    colorama: 0.4.0 -> 0.4.1

--- pkgs/all-pkgs/c/colorama/default.nix
+++ pkgs/all-pkgs/c/colorama/default.nix
@@ -6,7 +6,7 @@
 }:
 
 let
-  version = "0.4.0";
+  version = "0.4.1";
 in
 buildPythonPackage rec {
   name = "colorama-${version}";
@@ -14,8 +14,7 @@ buildPythonPackage rec {
   src = fetchPyPi {
     package = "colorama";
     inherit version;
-    type = ".zip";
-    sha256 = "c9b54bebe91a6a803e0772c8561d53f2926bfeb17cd141fbabcb08424086595c";
+    sha256 = "05eed71e2e327246ad6b38c540c4a3117230b19679b875190486ddd2d721422d";
   };
 
   nativeBuildInputs = [

bb98b8b63d13b8e54a87661a87574b672ab9917f
Cody W. Opel <codyopel@gmail.com>
2019-02-21 03:05:10 -0500

    babelfish: cleanup

--- pkgs/all-pkgs/b/babelfish/default.nix
+++ pkgs/all-pkgs/b/babelfish/default.nix
@@ -16,8 +16,6 @@ buildPythonPackage rec {
     sha256 = "8380879fa51164ac54a3e393f83c4551a275f03617f54a99d70151358e444104";
   };
 
-  doCheck = true;
-
   meta = with lib; {
     description = "A module to work with countries and languages";
     homepage = https://github.com/Diaoul/babelfish;

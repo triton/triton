1bc760ad4c6f8ed389113b2101f3f3c40383e609
Cody W. Opel <codyopel@gmail.com>
2019-02-21 03:05:37 -0500

    backports-abc: cleanup

--- pkgs/all-pkgs/b/backports-abc/default.nix
+++ pkgs/all-pkgs/b/backports-abc/default.nix
@@ -19,7 +19,6 @@ buildPythonPackage rec {
 
   # Backport of Python 3.5 "collections.abc"
   disabled = pythonAtLeast "3.5";
-  doCheck = true;
 
   meta = with lib; {
     description = "Backport of recent additions to the 'collections.abc' module";

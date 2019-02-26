4001ca1da5b3c323fbac75e5af35338bca549500
William A. Kennington III <william@wkennington.com>
2019-02-25 23:38:34 -0800

    btrfs-progs: 4.20.1 -> 4.20.2

--- pkgs/all-pkgs/b/btrfs-progs/default.nix
+++ pkgs/all-pkgs/b/btrfs-progs/default.nix
@@ -15,7 +15,7 @@
 }:
 
 let
-  version = "4.20.1";
+  version = "4.20.2";
 
   tarballUrls = [
     "mirror://kernel/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v${version}.tar"
@@ -27,7 +27,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     urls = map (n: "${n}.xz") tarballUrls;
     hashOutput = false;
-    sha256 = "562f5d1ff1d17867c4c2be2768c653b62f1f257c42f9bb3e1a36380c02ec4fcd";
+    sha256 = "890f8b7e162f2bbfaa5c7b23e8b6f791fd3f325239a0510871fa4b45e4a80e7c";
   };
 
   nativeBuildInputs = [

a6c9ac0e379f80528915b9e1cda1a3bc6357fe6f
William A. Kennington III <william@wkennington.com>
2019-02-27 22:30:15 -0800

    openssl: 1.1.1a -> 1.1.1b

--- pkgs/all-pkgs/o/openssl/default.nix
+++ pkgs/all-pkgs/o/openssl/default.nix
@@ -24,9 +24,9 @@ let
       ];
     };
     "1.1.1" = {
-      version = "1.1.1a";
-      multihash = "QmRhKw3UoTf6qniPZUuNwhhWednuAqGyyhCbSSCbJ85Tvk";
-      sha256 = "fc20130f8b7cbd2fb918b2f14e2f429e109c31ddd0fb38fc5d71d9ffed3f9f41";
+      version = "1.1.1b";
+      multihash = "Qma8xeEeCAkmC1wL6Mqg8q3k8cjoEEQoyP1GV1zmhpVwM3";
+      sha256 = "5c557b023230413dfb0756f3137a13e6d726838ccd1430888ad15bfb2b43ea4b";
       patches = [
         (fetchTritonPatch {
           rev = "29569cdc2793ba0e4902c2134fa3f3bbe9eb6a9f";

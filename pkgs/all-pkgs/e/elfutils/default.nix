93efcbfac5ab6c31ce237274a9e0ff4b632d6d0b
William A. Kennington III <william@wkennington.com>
2019-03-02 10:48:38 -0800

    elfutils: 0.175 -> 0.176

--- pkgs/all-pkgs/e/elfutils/default.nix
+++ pkgs/all-pkgs/e/elfutils/default.nix
@@ -8,15 +8,17 @@
 }:
 
 let
-  inherit (import ./common.nix { inherit fetchurl; })
-    version
-    src
-    srcVerification;
+  version = "0.176";
 in
 stdenv.mkDerivation rec {
   name = "elfutils-${version}";
 
-  inherit src;
+  src = fetchurl {
+    url = "https://sourceware.org/elfutils/ftp/${version}/${name}.tar.bz2";
+    multihash = "QmaaRXyFKYMEvvH7ctZMBqsYCHe5nbCotaNmMv2pTxbr19";
+    hashOutput = false;
+    sha256 = "eb5747c371b0af0f71e86215a5ebb88728533c3a104a43d4231963f308cd1023";
+  };
 
   nativeBuildInputs = [
     gnum4
@@ -34,9 +36,18 @@ stdenv.mkDerivation rec {
   ];
 
   passthru = {
-    inherit
-      version
-      srcVerification;
+    inherit version;
+    srcVerification = fetchurl rec {
+      failEarly = true;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.sig") urls;
+        pgpKeyFingerprint = "47CC 0331 081B 8BC6 D0FD  4DA0 8370 665B 5781 6A6A";
+      };
+    };
   };
 
   meta = with stdenv.lib; {

74e3da80e6dfa04cad2fd99fb039747b053f78d5
Cody W. Opel <codyopel@gmail.com>
2019-03-05 21:11:18 -0500

    gmime: 3.2.0 -> 3.2.3

--- pkgs/all-pkgs/g/gmime/default.nix
+++ pkgs/all-pkgs/g/gmime/default.nix
@@ -1,19 +1,18 @@
 { stdenv
 , fetchurl
 , lib
+, vala
 
 , glib
 , gobject-introspection
 , libgpg-error
+, libidn
 , zlib
 }:
 
 let
-  inherit (lib)
-    boolEn;
-
   channel = "3.2";
-  version = "${channel}.0";
+  version = "${channel}.3";
 in
 stdenv.mkDerivation rec {
   name = "gmime-${version}";
@@ -21,32 +20,26 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "mirror://gnome/sources/gmime/${channel}/${name}.tar.xz";
     hashOutput = false;
-    sha256 = "75ec6033f9192488ff37745792c107b3d0ab0a36c2d3e4f732901a771755d7e0";
+    sha256 = "6a0875eeb552ab447dd54853a68ced62217d863631048737dd97eaa2713e7311";
   };
+
+  nativeBuildInputs = [
+    vala
+  ];
 
   buildInputs = [
     glib
     gobject-introspection
     libgpg-error
+    libidn
     zlib
   ];
 
   configureFlags = [
     "--disable-maintainer-mode"
-    "--disable-gtk-doc"
-    "--disable-gtk-doc-html"
-    "--disable-gtk-doc-pdf"
-    "--disable-profiling"
-    "--disable-warnings"
-    "--disable-glibtest"
-    "--enable-largefile"
-    "--${boolEn (libgpg-error != null)}-crypto"
-    "--${boolEn (gobject-introspection != null)}-introspection"
-    "--disable-vala"
-    "--disable-coverage"
-    #--with-libiconv=
-    #--with-gpgme-prefix=
-    #--with-libidn=
+    "--enable-crypto"
+    "--enable-introspection"
+    "--enable-vala"
   ];
 
   passthru = {
@@ -55,8 +48,10 @@ stdenv.mkDerivation rec {
         outputHash
         outputHashAlgo
         urls;
-      sha256Url = "https://download.gnome.org/sources/gmime/${channel}/"
-        + "${name}.sha256sum";
+      fullOpts = {
+        sha256Urls =
+          map (u: lib.replaceStrings ["tar.xz"] ["sha256sum"] u) src.urls;
+      };
       failEarly = true;
     };
   };

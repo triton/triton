f1a537bbde091edaa78a76e044f75b92a3119609
Cody W. Opel <codyopel@gmail.com>
2019-03-06 14:35:24 -0500

    libxau: 1.0.8 -> 1.0.9

--- pkgs/all-pkgs/l/libxau/default.nix
+++ pkgs/all-pkgs/l/libxau/default.nix
@@ -7,11 +7,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "libXau-1.0.8";
+  name = "libXau-1.0.9";
 
   src = fetchurl {
     url = "mirror://xorg/individual/lib/${name}.tar.bz2";
-    sha256 = "fdd477320aeb5cdd67272838722d6b7d544887dfe7de46e1e7cc0c27c2bea4f2";
+    hashOutput = false;
+    sha256 = "ccf8cbf0dbf676faa2ea0a6d64bcc3b6746064722b606c8c52917ed00dcb73ec";
   };
 
   nativeBuildInputs = [
@@ -25,10 +26,26 @@ stdenv.mkDerivation rec {
   configureFlags = [
     "--enable-selective-werror"
     "--disable-strict-compilation"
-    "--enable-xthreads"
     "--disable-lint-library"
     "--without-lint"
   ];
+
+  passthru = {
+    srcVerification = fetchurl {
+      inherit (src)
+        outputHash
+        outputHashAlgo
+        urls;
+      fullOpts = {
+        pgpsigUrls = map (u: "${u}.sig") src.urls;
+        pgpKeyFingerprints = [
+          # Alan Coopersmith
+          "4A19 3C06 D35E 7C67 0FA4  EF0B A2FB 9E08 1F2D 130E"
+        ];
+      };
+      failEarly = true;
+    };
+  };
 
   meta = with lib; {
     description = "X.Org X authorization library";

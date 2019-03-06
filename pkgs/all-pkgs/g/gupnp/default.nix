c9bfdd61afeeea9b8954e81b413a7697d5d14f51
Cody W. Opel <codyopel@gmail.com>
2019-03-05 22:47:58 -0500

    gupnp: support netlink context manager

--- pkgs/all-pkgs/g/gupnp/default.nix
+++ pkgs/all-pkgs/g/gupnp/default.nix
@@ -1,29 +1,35 @@
 { stdenv
 , fetchurl
+, lib
+#, meson
+#, ninja
 
 , glib
 , gobject-introspection
 , gssdp
 , libsoup
 , libxml2
+, linux-headers
 , util-linux_lib
 , vala
 }:
 
 let
-  major = "1.0";
-  version = "${major}.3";
+  channel = "1.0";
+  version = "${channel}.3";
 in
 stdenv.mkDerivation rec {
   name = "gupnp-${version}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gupnp/${major}/${name}.tar.xz";
+    url = "mirror://gnome/sources/gupnp/${channel}/${name}.tar.xz";
     hashOutput = false;
     sha256 = "794b162ee566d85eded8c3f3e8c9c99f6b718a6b812d8b56f0c2ed72ac37cbbb";
   };
 
   nativeBuildInputs = [
+    #meson
+    #ninja
     vala
   ];
 
@@ -33,13 +39,19 @@ stdenv.mkDerivation rec {
     gssdp
     libsoup
     libxml2
+    linux-headers
     util-linux_lib
   ];
 
   configureFlags = [
     "--disable-maintainer-mode"
-    #--with-context-manager=[network-manager/connman/unix/linux]
+    "--with-context-manager=linux"
   ];
+
+  #mesonFlags = [
+  #  "-Dcontext_manager=linux"
+  #  "-Dexamples=false"
+  #];
 
   passthru = {
     srcVerification = fetchurl {
@@ -47,13 +59,15 @@ stdenv.mkDerivation rec {
         outputHash
         outputHashAlgo
         urls;
-      sha256Url = "https://download.gnome.org/sources/gupnp/${major}/"
-        + "${name}.sha256sum";
+      fullOpts = {
+        sha256Urls =
+          map (u: lib.replaceStrings ["tar.xz"] ["sha256sum"] u) src.urls;
+      };
       failEarly = true;
     };
   };
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "An implementation of the UPnP specification";
     homepage = http://www.gupnp.org/;
     license = licenses.gpl2;

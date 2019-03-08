225e08d128f11513b8a424228c5153d7691a0184
Cody W. Opel <codyopel@gmail.com>
2019-03-08 15:25:14 -0500

    eglexternalplatform: 2018-03-14 -> 2018-08-16

--- pkgs/all-pkgs/e/eglexternalplatform/default.nix
+++ pkgs/all-pkgs/e/eglexternalplatform/default.nix
@@ -4,8 +4,7 @@
 }:
 
 let
-  date = "2018-03-14";
-  rev = "e3b182e3253f92bdbb03a71fdbd958bfb69cf3e3";
+  date = "2018-08-16";
 in
 stdenv.mkDerivation rec {
   name = "eglexternalplatform-${date}";
@@ -14,8 +13,8 @@ stdenv.mkDerivation rec {
     version = 6;
     owner = "NVIDIA";
     repo = "eglexternalplatform";
-    inherit rev;
-    sha256 = "2f09000182bb8f0935cf13c23742bee66fc054df88dc9c77e1ea71757661667d";
+    rev = "7c8f8e2218e46b1a4aa9538520919747f1184d86";
+    sha256 = "33d77361ad622351753b4162d130445d1b6e53e51c40eace940ac3058af2299f";
   };
 
   postPatch = ''

2bfad736872c5176fc631b3a2bb8cd6795bd3868
Cody W. Opel <codyopel@gmail.com>
2019-03-08 15:28:35 -0500

    libglvnd: generate dummy pkgconfig files

--- pkgs/all-pkgs/l/libglvnd/default.nix
+++ pkgs/all-pkgs/l/libglvnd/default.nix
@@ -3,6 +3,7 @@
 , egl-headers
 , fetchFromGitHub
 , lib
+, mesa-headers
 , opengl-headers
 , python2
 
@@ -54,6 +55,37 @@ stdenv.mkDerivation rec {
 
   NIX_CFLAGS_COMPILE = [
     "-Wno-array-bounds"
+  ];
+
+  postInstall = ''
+    # Generate dummy pkgconfig files
+    tolower() { echo "$@" | tr '[A-Z]' '[a-z]'; }
+    local -a opengl_libs=(
+      'GL'
+      'EGL'
+      'GLESv1_CM'
+      'GLESv2'
+    )
+    local opengl_lib
+    local opengl_lib_lower
+    for opengl_lib in "''${opengl_libs[@]}"; do
+      opengl_lib_lower="$(tolower "$opengl_lib")"
+      cat > "''${opengl_lib_lower}.pc" <<EOF
+    Name: $opengl_lib_lower
+    Description: Dummy $opengl_lib library
+    Version: ${date}
+    Libs: -L$out/lib -l$opengl_lib
+    Cflags: -I${opengl-headers}/include -I${egl-headers}/include -I${mesa-headers}/include
+
+    EOF
+      install -D -m644 -v "''${opengl_lib_lower}.pc" \
+        "$dummypc"/lib/pkgconfig/"''${opengl_lib_lower}.pc"
+    done
+  '';
+
+  outputs = [
+    "out"
+    "dummypc"
   ];
 
   meta = with lib; {

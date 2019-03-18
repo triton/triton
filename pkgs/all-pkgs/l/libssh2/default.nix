80a12483227836b7fd002afee18d5d7ba4afad5a
William A. Kennington III <william@wkennington.com>
2019-03-18 15:26:56 -0700

    libssh2: 1.8.0 -> 1.8.1

--- pkgs/all-pkgs/l/libssh2/default.nix
+++ pkgs/all-pkgs/l/libssh2/default.nix
@@ -6,13 +6,13 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "libssh2-1.8.0";
+  name = "libssh2-1.8.1";
 
   src = fetchurl {
     url = "https://www.libssh2.org/download/${name}.tar.gz";
-    multihash = "QmY6PpE6gRK4vY5i277niCrMvev4pxqpxoqBNbXwXkUrzH";
+    multihash = "QmScmpHweA1xmEa1rv3UtekFXVMGjcW8MCBi6dvDmtuTbi";
     hashOutput = false;
-    sha256 = "39f34e2f6835f4b992cafe8625073a88e5a28ba78f83e8099610a7b3af4676d4";
+    sha256 = "40b517f35b1bb869d0075b15125c7a015557f53a5a3a6a8bffb89b69fd70f159";
   };
 
   buildInputs = [
@@ -29,9 +29,14 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;
-      pgpsigUrls = map (n: "${n}.asc") src.urls;
-      pgpKeyFingerprint = "27ED EAF2 2F3A BCEB 50DB  9A12 5CC9 08FD B71E 12C2";
-      inherit (src) urls outputHash outputHashAlgo;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.asc") src.urls;
+        pgpKeyFingerprint = "27ED EAF2 2F3A BCEB 50DB  9A12 5CC9 08FD B71E 12C2";
+      };
     };
   };
 

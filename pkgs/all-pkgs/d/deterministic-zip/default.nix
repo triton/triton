d3f99487db8426f7c588925e9affbf851c30d2e8
William A. Kennington III <william@wkennington.com>
2019-03-18 16:19:19 -0700

    deterministic-zip: Add dist compression

--- pkgs/all-pkgs/d/deterministic-zip/default.nix
+++ pkgs/all-pkgs/d/deterministic-zip/default.nix
@@ -3,6 +3,7 @@
 
 , brotli_1-0-3
 , gnutar_1-30
+, xz_5-2-4
 
 , version ? 6
 }:
@@ -12,33 +13,62 @@ let
     "6" = {
       brotli = brotli_1-0-3;
       tar = gnutar_1-30;
+      xz = xz_5-2-4;
     };
   };
 
   inherit (versions."${toString version}")
     brotli
-    tar;
+    tar
+    xz;
 
-  brotliFlags = [ "-6" "-c" ];
+  inherit (lib)
+    concatStringsSep;
 
-  cmd = ''
-    ${tar}/bin/tar \
-      --sort=name --owner=0 --group=0 --numeric-owner \
-      --no-acls --no-selinux --no-xattrs \
-      --mode=go=rX,u+rw,a-s \
-      --clamp-mtime --mtime=@"''${SOURCE_DATE_EPOCH:-1}" \
-      -c "$@" | ${brotli}/bin/brotli ${lib.concatStringsSep " " brotliFlags}
-  '';
+  tarCmd = concatStringsSep " " [
+    "${tar}/bin/tar"
+    "--sort=name"
+    "--owner=0"
+    "--group=0"
+    "--numeric-owner"
+    "--no-acls"
+    "--no-selinux"
+    "--no-xattrs"
+    "--mode=go=rX,u+rw,a-s"
+    "--clamp-mtime"
+    "--mtime=@\${SOURCE_DATE_EPOCH:-1}"
+    "-c"
+    "\"$@\""
+  ];
+
+  brotliCmd = concatStringsSep " " [
+    "${brotli}/bin/brotli"
+    "-6"
+    "-c"
+  ];
+
+  xzCmd = concatStringsSep " " [
+    "${xz}/bin/xz"
+    "-9"
+    "-e"
+    "-c"
+  ];
 in
 stdenv.mkDerivation {
   name = "deterministic-zip-${toString version}";
 
   buildCommand = ''
     mkdir -p "$out"/bin
+
     echo '#! ${stdenv.shell} -e' >>"$out"/bin/deterministic-zip-${toString version}
-    echo '${cmd}' >>"$out"/bin/deterministic-zip-${toString version}
+    echo '${tarCmd} | ${brotliCmd}' >>"$out"/bin/deterministic-zip-${toString version}
     chmod +x "$out"/bin/deterministic-zip-${toString version}
     ln -sv deterministic-zip-${toString version} "$out"/bin/deterministic-zip
+
+    echo '#! ${stdenv.shell} -e' >>"$out"/bin/deterministic-zip-dist-${toString version}
+    echo '${tarCmd} | ${xzCmd}' >>"$out"/bin/deterministic-zip-dist-${toString version}
+    chmod +x "$out"/bin/deterministic-zip-dist-${toString version}
+    ln -sv deterministic-zip-dist-${toString version} "$out"/bin/deterministic-zip-dist
   '';
 
   passthru = {

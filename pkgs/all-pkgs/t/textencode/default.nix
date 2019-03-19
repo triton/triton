d3982ea99bcda224ccb7f66ab8f07a1f41ac3e95
William A. Kennington III <william@wkennington.com>
2019-03-19 01:08:15 -0700

    textencode: 0.1.1 -> 0.1.2

--- pkgs/all-pkgs/t/textencode/default.nix
+++ pkgs/all-pkgs/t/textencode/default.nix
@@ -7,15 +7,15 @@ let
   inherit (lib)
     boolEn;
 
-  version = "0.1.1";
+  version = "0.1.2";
 in
 stdenv.mkDerivation rec {
   name = "textencode-${version}";
 
   src = fetchurl {
     url = "https://github.com/triton/textencode/releases/download/v${version}/${name}.tar.xz";
-    multihash = "QmP9MUrj2V3B5PuN4qmuDYo2LowMZYJJmLwGpZUnWYaZWq";
-    sha256 = "f4584eb7bb725dee1aef745fd53846a8599fc5192f6c2c6e2416e59607b0f12a";
+    multihash = "Qma56mUtqDVoTqUjmEmUbkTJLdSUoXpp6i4Tv1GYWJjQf4";
+    sha256 = "c67e1c80eb5a58c5878bf88d56e98ae2ad1109e475e37da309e56e971b7ad532";
   };
 
   configureFlags = [

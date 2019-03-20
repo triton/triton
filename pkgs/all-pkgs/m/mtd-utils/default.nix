612b006896b95040fbe5dc66fc6daabeea807143
William A. Kennington III <william@wkennington.com>
2019-03-19 18:11:09 -0700

    mtd-utils: 2.0.2 -> 2.1.0

--- pkgs/all-pkgs/m/mtd-utils/default.nix
+++ pkgs/all-pkgs/m/mtd-utils/default.nix
@@ -4,23 +4,25 @@
 
 , acl
 , lzo
+, openssl
 , util-linux_lib
 , zlib
 }:
 
 stdenv.mkDerivation rec {
-  name = "mtd-utils-2.0.2";
+  name = "mtd-utils-2.1.0";
 
   src = fetchurl {
     url = "ftp://ftp.infradead.org/pub/mtd-utils/${name}.tar.bz2";
-    multihash = "QmS2YuAJ38SAkBX3jL5wSqVgHshBiGsAzW4xczH15B2bhj";
+    multihash = "QmbQoz2cDharUvwpHQ8yNZ4abHYz5xEyoQLcY48RWarBj3";
     hashOutput = false;
-    sha256 = "fb3de61be8e932abb424e8ea3c30298f553d5f970ad158a737bb303bbf9660b8";
+    sha256 = "b4b995b06d93aee4125e8e44c05a1cae6eea545ca5a6e8a377405ee8aa454bd2";
   };
   
   buildInputs = [
     acl
     lzo
+    openssl
     util-linux_lib
     zlib
   ];
@@ -32,9 +34,14 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;
-      pgpsigUrls = map (n: "${n}.asc") src.urls;
-      pgpKeyFingerprint = "1306 3F72 3C9E 584A EACD  5B9B BCE5 DC3C 741A 02D1";
-      inherit (src) urls outputHash outputHashAlgo;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.asc") src.urls;
+        pgpKeyFingerprint = "1306 3F72 3C9E 584A EACD  5B9B BCE5 DC3C 741A 02D1";
+      };
     };
   };
 

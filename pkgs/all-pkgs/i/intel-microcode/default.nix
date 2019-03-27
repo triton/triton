1465f721618d35c0e82ef29de769aa62d1e93beb
William A. Kennington III <william@wkennington.com>
2019-03-26 19:49:29 -0700

    intel-microcode: 2018-08-07a -> 2019-03-12

--- pkgs/all-pkgs/i/intel-microcode/default.nix
+++ pkgs/all-pkgs/i/intel-microcode/default.nix
@@ -1,35 +1,28 @@
 { stdenv
-, fetchurl
+, fetchFromGitHub
 , iucode-tool
 }:
 
 let
-  version = "2018-08-07a";
-  version' = stdenv.lib.replaceStrings ["-"] [""] version;
-  id = "28087";
+  date = "2019-03-12";
+  rev = "7febfb94da92a71ca52628b940a8ebbe15bf4e09";
 
   initrdPath = "share/intel-microcode/intel-microcode-initrd.img";
 in
 stdenv.mkDerivation rec {
-  name = "intel-microcode-${version}";
+  name = "intel-microcode-${date}";
 
-  src = fetchurl {
-    url = "https://downloadmirror.intel.com/${id}/eng/microcode-${version'}.tgz";
-    multihash = "QmaEb3xGPG33EBU2cPog3c5BW1YG39r5S9xxgfKuEAQY5W";
-    hashOutput = false;
-    sha256 = "46ab18699ec42eb6cc01ee1846ec4d7ca979766dee2156f92d69e2f6df548137";
+  src = fetchFromGitHub {
+    version = 6;
+    owner = "intel";
+    repo = "Intel-Linux-Processor-Microcode-Data-Files";
+    inherit rev;
+    sha256 = "0986d562df13e701deda00ffb0e3d8de9b0a3502620c65ea785634774bbc7158";
   };
 
   nativeBuildInputs = [
     iucode-tool
   ];
-
-  srcRoot = ".";
-
-  preUnpack = ''
-    mkdir src
-    cd src
-  '';
 
   buildPhase = ''
     iucode_tool --write-earlyfw=initrd intel-ucode*/
@@ -38,20 +31,6 @@ stdenv.mkDerivation rec {
   installPhase = ''
     install -D -m755 -v initrd "$out"/'${initrdPath}'
   '';
-
-  passthru = {
-    srcVerification = fetchurl {
-      failEarly = true;
-      inherit (src)
-        urls
-        outputHash
-        outputHashAlgo;
-      fullOpts = {
-        md5Confirm = "b12f8680d87c81a302e8c85712ed1a80";
-      };
-    };
-    inherit initrdPath;
-  };
 
   meta = with stdenv.lib; {
     description = "Microcode for Intel processors";

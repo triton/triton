45e9a034011ab0304f5a1a168a72dfa341b258e6
Cody W. Opel <codyopel@gmail.com>
2019-03-28 00:34:56 -0400

    giflib: 5.1.4 -> 5.1.8

--- pkgs/all-pkgs/g/giflib/default.nix
+++ pkgs/all-pkgs/g/giflib/default.nix
@@ -1,16 +1,17 @@
 { stdenv
 , fetchurl
+, lib
 }:
 
 stdenv.mkDerivation rec {
-  name = "giflib-5.1.4";
+  name = "giflib-5.1.8";
 
   src = fetchurl {
-    url = "mirror://sourceforge/giflib/${name}.tar.bz2";
-    sha256 = "df27ec3ff24671f80b29e6ab1c4971059c14ac3db95406884fc26574631ba8d5";
+    url = "mirror://sourceforge/giflib/${name}.tar.gz";
+    sha256 = "d105a905df34a7822172d13657cdae3d4b0c8e8c7067ccf05e39a40044f8ca53";
   };
-  
-  meta = with stdenv.lib; {
+
+  meta = with lib; {
     description = "A library for reading and writing gif images";
     license = licenses.mit;
     maintainers = with stdenv.lib.maintainers; [

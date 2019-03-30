69a4f1653c6d2ca6975df03ade7cc2d2cbf8b95c
William A. Kennington III <william@wkennington.com>
2019-03-29 18:29:57 -0700

    rdma-core: 22.1 -> 23

--- pkgs/all-pkgs/r/rdma-core/default.nix
+++ pkgs/all-pkgs/r/rdma-core/default.nix
@@ -10,7 +10,7 @@
 }:
 
 let
-  version = "22.1";
+  version = "23";
 in
 stdenv.mkDerivation rec {
   name = "rdma-core-${version}";
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "https://github.com/linux-rdma/rdma-core/releases/download/v${version}/${name}.tar.gz";
     hashOutput = false;
-    sha256 = "d2ba34326c828ebeff26b300761d3c45ffceb76e0a804e9c612d1baf96ad673a";
+    sha256 = "ee4de492c8ba6a646b1cea4e3eb865657d481d9db97de55c4e87fa17ed57ee05";
   };
 
   nativeBuildInputs = [

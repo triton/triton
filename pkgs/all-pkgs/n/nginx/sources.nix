5763ea216dd0a7f64bb89a9415e2f44a0cc16ba3
William A. Kennington III <william@wkennington.com>
2019-04-29 03:34:10 -0700

    nginx: 1.14.2 -> 1.16.0

--- pkgs/all-pkgs/n/nginx/sources.nix
+++ pkgs/all-pkgs/n/nginx/sources.nix
@@ -1,13 +1,13 @@
 {
   "stable" = {
-    version = "1.14.2";
-    multihash = "QmWxvE8Jt8PWLwUGy4drtnsxqmZzEcHms1ysMEmQQJPCVX";
-    sha256 = "002d9f6154e331886a2dd4e6065863c9c1cf8291ae97a1255308572c02be9797";
+    version = "1.16.0";
+    multihash = "QmVZotY4Y8qsPsQYY3Q4KXGi6gDxXuyohzYXrnyBpdA54g";
+    sha256 = "4fd376bad78797e7f18094a00f0f1088259326436b537eb5af69b01be2ca1345";
   };
 
   "unstable" = {
-    version = "1.15.10";
-    multihash = "QmW3xyrHUXGYzcLmmXbBj9k29u8FE9WMeCuUTKLKCq1yVc";
-    sha256 = "b865743abd52bce4745d0f7e7fedde3cafbaaab617b022c105e3e4e456537c3c";
+    version = "1.16.0";
+    multihash = "QmVZotY4Y8qsPsQYY3Q4KXGi6gDxXuyohzYXrnyBpdA54g";
+    sha256 = "4fd376bad78797e7f18094a00f0f1088259326436b537eb5af69b01be2ca1345";
   };
 }

7caefeca7804075d1b4246aeeb3da12acb26ec40
William A. Kennington III <william@wkennington.com>
2019-05-01 22:44:55 -0700

    go: Backport `go build -trimpath`

--- pkgs/all-pkgs/g/go/sources.nix
+++ pkgs/all-pkgs/g/go/sources.nix
@@ -11,6 +11,16 @@
         file = "g/go/0001-Get-TOOLDIR-from-the-environment.patch";
         sha256 = "72695d3dbe3ce401f8c69a3e433ebea7f5fb207a8d3ed1e8f65c58aea3906148";
       }
+      {
+        rev = "04f84dd74a9b337749b74795024be9cebbd262fc";
+        file = "g/go/0001-cmd-internal-objabi-expand-trimpath-syntax.patch";
+        sha256 = "6665db24b0280bc009b155c899bcb6e3d82349a64f4e96803e8a2263e4fc3dd8";
+      }
+      {
+        rev = "04f84dd74a9b337749b74795024be9cebbd262fc";
+        file = "g/go/0002-cmd-go-add-trimpath-build-flag.patch";
+        sha256 = "c02ded43dd30a240ab44ca977131ddc8b2ccdc9704d3b3a7c82da64d8da02e24";
+      }
     ];
   };
 }

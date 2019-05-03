39ace05aa54481d674006a3aa800d59a819eeff4
William A. Kennington III <william@wkennington.com>
2019-05-02 17:08:58 -0700

    zfs(dev): 2019-03-19 -> 2019-05-02

--- pkgs/all-pkgs/z/zfs/common.nix
+++ pkgs/all-pkgs/z/zfs/common.nix
@@ -37,18 +37,18 @@ in
     ];
   };
   "dev" = {
-    date = "2019-03-19";
+    date = "2019-05-02";
     maxLinuxVersion = "5.1";
     src = fetcher {
       fetchzipVersion = 6;
-      rev = "73c25a78e6b420ff37d703d2e1911c17cf449caa";
-      sha256 = "4fe9854e47dd0fa0fdb04e34e24319629846d8b0130b9817719b13cce0ad81c8";
+      rev = "b3b60984ee191bdbb00b91842053c83581854db7";
+      sha256 = "23386d1fc70c8beee280bdcef984eb2bb2ddbb522e76ae50ee43839366424e57";
     };
     patches = [
       (fetchTritonPatch {
-        rev = "8c2686354b8307407391b0434ce48aef322ab3b3";
+        rev = "af81e3b365a91a63b44e468b2dc0c770686dfa6e";
         file = "z/zfs/0001-Fix-binary-paths.patch";
-        sha256 = "bc4ad5b4da1ba0f1ae72f78195f958229d8e4dd617db812babf10ae1902e5720";
+        sha256 = "8009b1df288109d4f386842210c39e9e1ba3f0734d719dc6eb56deb3d29d2963";
       })
     ];
   };

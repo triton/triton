b56dac4518bb7ef267fd3c203744936246fda58b
William A. Kennington III <william@wkennington.com>
2019-05-06 19:04:39 -0700

    ndisc6: 1.0.3 -> 1.0.4

--- pkgs/all-pkgs/n/ndisc6/default.nix
+++ pkgs/all-pkgs/n/ndisc6/default.nix
@@ -3,14 +3,18 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "ndisc6-1.0.3";
+  name = "ndisc6-1.0.4";
   
   src = fetchurl {
-    url = "http://www.remlab.net/files/ndisc6/${name}.tar.bz2";
+    url = "https://www.remlab.net/files/ndisc6/${name}.tar.bz2";
+    multihash = "QmYqLavHVPbYNaXCKvdxmx8WwgMtUZEBCbu2vz4Ge6sgPP";
     hashOutput = false;
-    multihash = "QmYXNRRL2YtRRZiuw8VnBR3KAfdsQEs82oRa2xq1s2NTmQ";
-    sha256 = "0f41d6caf5f2edc1a12924956ae8b1d372e3b426bd7b11eed7d38bc974eec821";
+    sha256 = "abb1da4a98d94e5abe1dd7b1c975de540306b0581cbbd36aff035118b2f25c1f";
   };
+
+  postPatch = ''
+    sed -i '/ch\(mod\|own\)/d' Makefile.in
+  '';
 
   configureFlags = [
     "--sysconfdir=/etc"
@@ -27,9 +31,11 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;
-      pgpsigUrls = map (n: "${n}.asc") src.urls;
-      pgpKeyFingerprint = "9480 5833 53E3 34D2 F03F  E80C C3EC 6DBE DD6D 12BD";
       inherit (src) urls outputHash outputHashAlgo;
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.asc") src.urls;
+        pgpKeyFingerprint = "9480 5833 53E3 34D2 F03F  E80C C3EC 6DBE DD6D 12BD";
+      };
     };
   };
 

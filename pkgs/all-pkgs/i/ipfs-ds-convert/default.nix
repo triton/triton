46b362ca9ad3151c441c2a4a1c8b6dde9d92eee8
William A. Kennington III <william@wkennington.com>
2019-05-17 17:06:50 -0700

    ipfs-ds-convert: 2019-03-29 -> 2019-05-14

--- pkgs/all-pkgs/i/ipfs-ds-convert/default.nix
+++ pkgs/all-pkgs/i/ipfs-ds-convert/default.nix
@@ -1,7 +1,6 @@
 { lib
 , buildGo
 , fetchGo
-, fetchTritonPatch
 }:
 
 let
@@ -19,14 +18,6 @@ buildGo {
     gosum = ./go.sum;
     sourceJSON = ./source.json;
   };
-
-  patches = [
-    (fetchTritonPatch {
-      rev = "6c3aef9b462db920e9ed47141d1eb94561e01080";
-      file = "i/ipfs-ds-convert/fix.patch";
-      sha256 = "e3b27dd7a0ce47b18836527de66b5c6821f6e5787c76b42ad3ce464d2929c2f8";
-    })
-  ];
 
   installedSubmodules = [
     "."

3571a9c27966287dacba40b537dbdf61b6021a02
Cody W. Opel <codyopel@gmail.com>
2019-05-25 21:09:14 -0400

    cppunit: Add patch for compatibility with GCC 9.1

--- pkgs/all-pkgs/c/cppunit/default.nix
+++ pkgs/all-pkgs/c/cppunit/default.nix
@@ -1,4 +1,5 @@
 { stdenv
+, fetchTritonPatch
 , fetchurl
 }:
 
@@ -10,6 +11,15 @@ stdenv.mkDerivation rec {
     multihash = "QmUdPCbTGq4fuyXUV7MoMBPQuW2shm4W9cWvMxfJe47koq";
     sha256 = "3d569869d27b48860210c758c4f313082103a5e58219a7669b52bfd29d674780";
   };
+
+  patches = [
+    # Fix compat with GCC 9.1, remove for 1.15+
+    (fetchTritonPatch {
+      rev = "e8619d4113073eb9ecf0a4adac7e65243d2a86dd";
+      file = "c/cppunit/cppunit-fix.gcc.9.1.patch";
+      sha256 = "a87312d818ba28e6a9a0d5294ebafde7df9de5096276b32905e9404700dd1020";
+    })
+  ];
 
   doCheck = true;
 

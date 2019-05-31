fea0bf03b66bc5d27a7f0b8492043b8fd66638b9
William A. Kennington III <william@wkennington.com>
2019-05-31 01:28:49 -0700

    meson: Fix formatting

--- pkgs/all-pkgs/m/meson/setup-hook.sh
+++ pkgs/all-pkgs/m/meson/setup-hook.sh
@@ -1,67 +1,67 @@
 mesonConfigurePhase() {
-    eval "$preConfigure"
+  runHook 'preConfigure'
 
-    if [ -n "${addPrefix-true}" ]; then
-      mesonFlagsArray+=(
-        "--prefix" "$prefix"
-      )
-    fi
-
-    # Configure the source and build directories
-    if [ -z "$mesonSrcDir" ]; then
-      mesonSrcDir="$(pwd)"
-    fi
-    if [ -z "$mesonBuildDir" ]; then
-      if [ -n "${createMesonBuildDir-true}" ]; then
-        mkdir -p "$TMPDIR"/build
-        cd "$TMPDIR"/build
-      fi
-      mesonBuildDir="$(pwd)"
-    fi
-
-    local LTOFlag="false"
-    if [ -n "${buildLTO-1}" ]; then
-      LTOFlag="true"
-    fi
-
+  if [ -n "${addPrefix-true}" ]; then
     mesonFlagsArray+=(
-      # Build always Release, to ensure optimisation flags
-      "--buildtype" "${mesonBuildType-release}"
-      # Prefer using Link-Time Optimization
-      "-Db_lto=$LTOFlag"
+      "--prefix" "$prefix"
     )
+  fi
 
-    echo "meson flags: $mesonFlags ${mesonFlagsArray[@]}"
+  # Configure the source and build directories
+  if [ -z "$mesonSrcDir" ]; then
+    mesonSrcDir="$(pwd)"
+  fi
+  if [ -z "$mesonBuildDir" ]; then
+    if [ -n "${createMesonBuildDir-true}" ]; then
+      mkdir -p "$TMPDIR"/build
+      cd "$TMPDIR"/build
+    fi
+    mesonBuildDir="$(pwd)"
+  fi
 
-    "$meson" setup $mesonFlags "${mesonFlagsArray[@]}" \
-      "${mesonSrcDir}" "${mesonBuildDir}"
+  local LTOFlag="false"
+  if [ -n "${buildLTO-1}" ]; then
+    LTOFlag="true"
+  fi
 
-    eval "$postConfigure"
+  mesonFlagsArray+=(
+    # Build always Release, to ensure optimisation flags
+    "--buildtype" "${mesonBuildType-release}"
+    # Prefer using Link-Time Optimization
+    "-Db_lto=$LTOFlag"
+  )
+
+  echo "meson flags: $mesonFlags ${mesonFlagsArray[@]}"
+
+  "$meson" setup $mesonFlags "${mesonFlagsArray[@]}" \
+    "${mesonSrcDir}" "${mesonBuildDir}"
+
+  runHook 'postConfigure'
 }
 
 mesonCheckPhase() {
-    runHook 'preCheck'
+  runHook 'preCheck'
 
-    local actualMakeFlags
-    actualMakeFlags=($checkFlags "${checkFlagsArray[@]}")
-    printMakeFlags 'check'
-    "$meson" test "${actualMakeFlags[@]}"
+  local actualMakeFlags
+  actualMakeFlags=($checkFlags "${checkFlagsArray[@]}")
+  printMakeFlags 'check'
+  "$meson" test "${actualMakeFlags[@]}"
 
-    runHook 'postCheck'
+  runHook 'postCheck'
 }
 
 mesonInstallPhase() {
-    runHook 'preInstall'
+  runHook 'preInstall'
 
-    mkdir -p "$prefix"
+  mkdir -p "$prefix"
 
-    local actualMakeFlags
-    actualMakeFlags=($installFlags "${installFlagsArray[@]}")
-    actualMakeFlags+=('--no-rebuild')
-    printMakeFlags 'install'
-    "$meson" install "${actualMakeFlags[@]}"
+  local actualMakeFlags
+  actualMakeFlags=($installFlags "${installFlagsArray[@]}")
+  actualMakeFlags+=('--no-rebuild')
+  printMakeFlags 'install'
+  "$meson" install "${actualMakeFlags[@]}"
 
-    runHook 'postInstall'
+  runHook 'postInstall'
 }
 
 mesonFixup() {

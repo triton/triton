e26144cc480c67f0c578fa947702c4dcf4947ffa
William A. Kennington III <william@wkennington.com>
2019-06-02 15:02:31 -0700

    libatomic_ops: 7.6.8 -> 7.6.10

--- pkgs/all-pkgs/l/libatomic_ops/default.nix
+++ pkgs/all-pkgs/l/libatomic_ops/default.nix
@@ -3,14 +3,14 @@
 }:
 
 let
-  version = "7.6.8";
+  version = "7.6.10";
 in
 stdenv.mkDerivation rec {
   name = "libatomic_ops-${version}";
 
   src = fetchurl {
     url = "https://github.com/ivmai/libatomic_ops/releases/download/v${version}/${name}.tar.gz";
-    sha256 = "1d6a279edf81767e74d2ad2c9fce09459bc65f12c6525a40b0cb3e53c089f665";
+    sha256 = "587edf60817f56daf1e1ab38a4b3c729b8e846ff67b4f62a6157183708f099af";
   };
 
   meta = with stdenv.lib; {

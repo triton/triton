f2cd525d8e9b6f8ed3f03a4120a9b0d37e2bba97
William A. Kennington III <william@wkennington.com>
2019-06-22 17:07:15 -0700

    linux: 4.19.51 -> 4.19.55

--- pkgs/all-pkgs/l/linux/default.nix
+++ pkgs/all-pkgs/l/linux/default.nix
@@ -31,9 +31,9 @@ let
 
   sources = {
     "4.19" = {
-      version = "4.19.51";
+      version = "4.19.55";
       baseSha256 = "0c68f5655528aed4f99dae71a5b259edc93239fa899e2df79c055275c21749a1";
-      patchSha256 = "d5026e8cdbe80238a87f90499cfd5e3065eec9981efd8ffc86c9ca09b4c87ce4";
+      patchSha256 = "6b572393d79379cc7d7e9bd55170b2d4fc76745ad9f15c0b893a6749167f63f5";
     };
     "5.1" = {
       version = "5.1.10";

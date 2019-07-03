e2ff06f293884527ce3f5eaca02acf67fc376de4
William A. Kennington III <william@wkennington.com>
2019-07-03 15:48:47 -0700

    fido2: 0.6.0 -> 0.7.0

--- pkgs/all-pkgs/f/fido2/default.nix
+++ pkgs/all-pkgs/f/fido2/default.nix
@@ -5,7 +5,7 @@
 }:
 
 let
-  version = "0.6.0";
+  version = "0.7.0";
 in
 buildPythonPackage {
   name = "fido2-${version}";
@@ -13,7 +13,7 @@ buildPythonPackage {
   src = fetchPyPi {
     package = "fido2";
     inherit version;
-    sha256 = "7541edad31967d23f5006ffeccc54536ab9934dd981d65d29620d9dfb54566bf";
+    sha256 = "47b02852780849bb4bb698b9727d61970ee77a83eb25715fe7c6235ebd648d87";
   };
 
   meta = with lib; {

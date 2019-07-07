0ec51a623311715c5924e04acbff8de4fcf7a3c2
William A. Kennington III <william@wkennington.com>
2019-07-06 18:38:22 -0700

    protobuf-c : 1.3.1 -> 1.3.2

--- pkgs/all-pkgs/p/protobuf-c/default.nix
+++ pkgs/all-pkgs/p/protobuf-c/default.nix
@@ -5,14 +5,14 @@
 }:
 
 let
-  version = "1.3.1";
+  version = "1.3.2";
 in
 stdenv.mkDerivation rec {
   name = "protobuf-c-${version}";
 
   src = fetchurl {
     url = "https://github.com/protobuf-c/protobuf-c/releases/download/v${version}/${name}.tar.gz";
-    sha256 = "51472d3a191d6d7b425e32b612e477c06f73fe23e07f6a6a839b11808e9d2267";
+    sha256 = "53f251f14c597bdb087aecf0b63630f434d73f5a10fc1ac545073597535b9e74";
   };
 
   nativeBuildInputs = [
@@ -22,12 +22,6 @@ stdenv.mkDerivation rec {
   buildInputs = [
     protobuf-cpp
   ];
-
-  postPatch = ''
-    # Fix namespace for 3.7.0+
-    grep -q 'google::protobuf::Message::Reflection' t/generated-code2/cxx-generate-packed-data.cc
-    sed -i 's,google::protobuf::Message::Reflection,google::protobuf::Reflection,' t/generated-code2/cxx-generate-packed-data.cc
-  '';
 
   meta = with stdenv.lib; {
     maintainers = with maintainers; [

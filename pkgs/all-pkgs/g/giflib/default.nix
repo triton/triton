7a59b411748f993b1f27cc99aae40b3456f7edcb
William A. Kennington III <william@wkennington.com>
2019-07-07 04:42:26 -0700

    giflib: 5.1.9 -> 5.2.1

--- pkgs/all-pkgs/g/giflib/default.nix
+++ pkgs/all-pkgs/g/giflib/default.nix
@@ -4,11 +4,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "giflib-5.1.9";
+  name = "giflib-5.2.1";
 
   src = fetchurl {
     url = "mirror://sourceforge/giflib/${name}.tar.gz";
-    sha256 = "36ccab06aa43e5d608cdd74902f89c47fd55c348009798434ba5798967454057";
+    sha256 = "31da5562f44c5f15d63340a09a4fd62b48c45620cd302f77a6d9acf0077879bd";
   };
 
   preBuild = ''

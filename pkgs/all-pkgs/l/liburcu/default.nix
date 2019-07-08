04494d0c86b4d9b01a8a425d975d37ffe5f93f27
William A. Kennington III <william@wkennington.com>
2019-07-07 23:03:50 -0700

    liburcu: 0.11.0 -> 0.11.1

--- pkgs/all-pkgs/l/liburcu/default.nix
+++ pkgs/all-pkgs/l/liburcu/default.nix
@@ -3,16 +3,16 @@
 }:
 
 let
-  version = "0.11.0";
+  version = "0.11.1";
 in
 stdenv.mkDerivation rec {
   name = "liburcu-${version}";
 
   src = fetchurl {
     url = "https://lttng.org/files/urcu/userspace-rcu-${version}.tar.bz2";
-    multihash = "Qmc1R98UF5EN1wcBpUFjSgLrq2j1cekerAxBPupk8SQ7mn";
+    multihash = "QmaG2YwLkf7THJy2Px6x8G3DqyjAMAY4J8213w946pVRdc";
     hashOutput = false;
-    sha256 = "1af5694c4f6266f4eba5eb4b832daee600d1e7055fce6da5d514d735d72eb3e7";
+    sha256 = "92b9971bf3f1c443edd6c09e7bf5ff3b43531e778841f16377a812c8feeb3350";
   };
 
   passthru = {
@@ -21,7 +21,7 @@ stdenv.mkDerivation rec {
       inherit (src) urls outputHash outputHashAlgo;
       fullOpts = {
         md5Urls = map (n: "${n}.md5") src.urls;
-        sha1Urls = map (n: "${n}.sha1") src.urls;
+        sha256Urls = map (n: "${n}.sha256") src.urls;
         pgpsigUrls = map (n: "${n}.asc") src.urls;
         pgpKeyFingerprint = "2A0B 4ED9 15F2 D3FA 45F5  B162 1728 0A97 8118 6ACF";
       };

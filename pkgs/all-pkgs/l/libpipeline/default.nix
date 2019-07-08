8a91951599bcc379d4a61f23ad4d81f3fd183a2f
William A. Kennington III <william@wkennington.com>
2019-07-08 00:13:36 -0700

    libpipeline: 1.5.0 -> 1.5.1

--- pkgs/all-pkgs/l/libpipeline/default.nix
+++ pkgs/all-pkgs/l/libpipeline/default.nix
@@ -3,20 +3,25 @@
 }:
  
 stdenv.mkDerivation rec {
-  name = "libpipeline-1.5.0";
+  name = "libpipeline-1.5.1";
   
   src = fetchurl {
     url = "mirror://savannah/libpipeline/${name}.tar.gz";
     hashOutput = false;
-    sha256 = "0d72e12e4f2afff67fd7b9df0a24d7ba42b5a7c9211ac5b3dcccc5cd8b286f2b";
+    sha256 = "d633706b7d845f08b42bc66ddbe845d57e726bf89298e2cee29f09577e2f902f";
   };
 
   passthru = {
     srcVerification = fetchurl {
       failEarly = true;
-      pgpsigUrls = map (n: "${n}.sig") src.urls;
-      pgpKeyFingerprint = "AC0A 4FF1 2611 B6FC CF01  C111 3935 87D9 7D86 500B";
-      inherit (src) urls outputHash outputHashAlgo;
+      inherit (src)
+        urls
+        outputHash
+        outputHashAlgo;
+      fullOpts = {
+        pgpsigUrls = map (n: "${n}.asc") src.urls;
+        pgpKeyFingerprint = "AC0A 4FF1 2611 B6FC CF01  C111 3935 87D9 7D86 500B";
+      };
     };
   };
 

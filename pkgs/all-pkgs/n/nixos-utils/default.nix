490fb59e10ccaf9d26f1eecf1bcf0aa629256f48
William A. Kennington III <william@wkennington.com>
2019-07-14 15:08:54 -0700

    nixos-utils: Customizable ids

--- pkgs/all-pkgs/n/nixos-utils/default.nix
+++ pkgs/all-pkgs/n/nixos-utils/default.nix
@@ -1,10 +1,15 @@
 { stdenv
+, base_uid ? null
+, base_gid ? null
 }:
 
 let
   ids = (import ../../../../nixos/modules/misc/ids.nix) {
     inherit (stdenv) lib;
   };
+
+  base_uid' = if base_uid != null then base_uid else ids.config.ids.uids.nixbld;
+  base_gid' = if base_gid != null then base_gid else ids.config.ids.gids.nixbld;
 in
 stdenv.mkDerivation {
   name = "nixos-utils";
@@ -12,8 +17,8 @@ stdenv.mkDerivation {
   buildCommand = ''
     set -x
     sed ${./nss.c.in} \
-      -e "s,@BASE_UID@,${toString ids.config.ids.uids.nixbld},g" \
-      -e "s,@BASE_GID@,${toString ids.config.ids.gids.nixbld},g" \
+      -e "s,@BASE_UID@,${toString base_uid'},g" \
+      -e "s,@BASE_GID@,${toString base_gid'},g" \
       > nss.c
     gcc -shared -fPIC -Wall -Werror -O2 -o libnss_nixos.so.2 nss.c
     mkdir -p "$out"/lib

85df14a2d0ddbe3b3e470f9aebc40edfd9df464e
Cody W. Opel <codyopel@gmail.com>
2019-07-18 03:24:32 -0400

    flask-restplus: 0.10.1 -> 0.12.1

--- pkgs/all-pkgs/f/flask-restplus/default.nix
+++ pkgs/all-pkgs/f/flask-restplus/default.nix
@@ -8,41 +8,18 @@
 , jsonschema
 , pytz
 , six
-
-, blinker
-, flask-restful
-, mock
-, nose
-#, rednose
-, tzlocal
-
-, channel ? "latest"
 }:
 
 let
-  inherit (lib)
-    optionals
-    versionOlder;
-
-  sources = {
-    "0.8" = {
-      version = "0.8.6";
-      sha256 = "3bb76cc156b9a09da62396d82b29fa31e4f27cccf79528538fe7155cf2785593";
-    };
-    "latest" = {
-      version = "0.10.1";
-      sha256 = "129767ba6087a6f0a6d1bd901f4c11192b4d33fe80c38cf2a8a0b5f8dd6049e3";
-    };
-  };
-
-  source = sources."${channel}";
+  version = "0.12.1";
 in
 buildPythonPackage rec {
-  name = "flask-restplus-${source.version}";
+  name = "flask-restplus-${version}";
 
   src = fetchPyPi {
     package = "flask-restplus";
-    inherit (source) sha256 version;
+    inherit version;
+    sha256 = "3fad697e1d91dfc13c078abcb86003f438a751c5a4ff41b84c9050199d2eab62";
   };
 
   propagatedBuildInputs = [
@@ -51,17 +28,7 @@ buildPythonPackage rec {
     jsonschema
     pytz
     six
-  ] ++ optionals (versionOlder source.version "0.9.0") [
-    flask-restful
-  ] ++ optionals doCheck [
-    blinker
-    mock
-    nose
-    #rednose
-    tzlocal
   ];
-
-  doCheck = false;
 
   meta = with lib; {
     description = "Framework for fast, easy and documented API development";

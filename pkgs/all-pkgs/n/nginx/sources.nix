554a79ec06cb77b6126e35b01cb75d09d020aa75
William A. Kennington III <william@wkennington.com>
2019-07-25 23:46:00 -0700

    nginx: 1.16.0 -> 1.17.2

--- pkgs/all-pkgs/n/nginx/sources.nix
+++ pkgs/all-pkgs/n/nginx/sources.nix
@@ -6,8 +6,8 @@
   };
 
   "unstable" = {
-    version = "1.16.0";
-    multihash = "QmVZotY4Y8qsPsQYY3Q4KXGi6gDxXuyohzYXrnyBpdA54g";
-    sha256 = "4fd376bad78797e7f18094a00f0f1088259326436b537eb5af69b01be2ca1345";
+    version = "1.17.2";
+    multihash = "QmeKFJCxp3nhEB24zVEpd37G7NvDYBphzJ9ioLTR5FuJFd";
+    sha256 = "5e333687464e1d6dfb86fc22d653b99a6798dda40093b33186eeeec5a97e69ec";
   };
 }

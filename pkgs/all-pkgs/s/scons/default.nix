774e4c1ff7b283e733e49fbed5b308d1c7fc7d0f
William A. Kennington III <william@wkennington.com>
2019-08-09 13:43:52 -0700

    scons: 3.0.4 -> 3.1.1

--- pkgs/all-pkgs/s/scons/default.nix
+++ pkgs/all-pkgs/s/scons/default.nix
@@ -5,14 +5,14 @@
 }:
 
 let
-  version = "3.0.4";
+  version = "3.1.1";
 in
 buildPythonPackage rec {
   name = "scons-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/scons/scons/${version}/${name}.tar.gz";
-    sha256 = "e2b8b36e25492720a05c0f0a92a219b42d11ce0c51e3397a1e8296dfea1d9b1a";
+    sha256 = "4cea417fdd7499a36f407923d03b4b7000b0f9e8fd7b31b316b9ce7eba9143a5";
   };
 
   setupHook = ./setup-hook.sh;

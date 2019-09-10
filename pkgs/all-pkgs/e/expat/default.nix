618195ac1c185a8bdc8b0aae2d9a4dc1b9eb61bb
William A. Kennington III <william@wkennington.com>
2019-09-10 15:47:07 -0700

    expat: 2.2.6 -> 2.2.7

--- pkgs/all-pkgs/e/expat/default.nix
+++ pkgs/all-pkgs/e/expat/default.nix
@@ -1,8 +1,6 @@
 { stdenv
 , fetchurl
 , lib
-
-, libbsd
 }:
 
 let
@@ -10,7 +8,7 @@ let
     "mirror://sourceforge/expat/expat/${version}/expat-${version}.tar.bz2"
   ];
 
-  version = "2.2.6";
+  version = "2.2.7";
 in
 stdenv.mkDerivation rec {
   name = "expat-${version}";
@@ -18,28 +16,20 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     urls = tarballUrls version;
     hashOutput = false;
-    sha256 = "17b43c2716d521369f82fc2dc70f359860e90fa440bea65b3b85f0b246ea81f2";
+    sha256 = "cbc9102f4a31a8dafd42d642e9a3aa31e79a0aedaa1f6efd2795ebc83174ec18";
   };
-
-  configureFlags = [
-    "--with-libbsd"
-  ];
-
-  buildInputs = [
-    libbsd
-  ];
 
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
       inherit (src)
         outputHashAlgo;
-      urls = tarballUrls "2.2.6";
+      urls = tarballUrls "2.2.7";
       fullOpts = {
         pgpsigUrls = map (n: "${n}.asc") urls;
         pgpKeyFingerprint = "3D7E 959D 89FA CFEE 3837  1921 B00B C66A 401A 1600";
       };
-      outputHash = "17b43c2716d521369f82fc2dc70f359860e90fa440bea65b3b85f0b246ea81f2";
+      outputHash = "cbc9102f4a31a8dafd42d642e9a3aa31e79a0aedaa1f6efd2795ebc83174ec18";
     };
   };
 

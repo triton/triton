869b32992f574187d98bad4d4952922166ee9934
William A. Kennington III <william@wkennington.com>
2019-09-10 15:16:47 -0700

    libuv: 1.30.1 -> 1.32.0

--- pkgs/all-pkgs/l/libuv/default.nix
+++ pkgs/all-pkgs/l/libuv/default.nix
@@ -7,16 +7,16 @@
 }:
 
 let
-  version = "1.30.1";
+  version = "1.32.0";
 in
 stdenv.mkDerivation rec {
   name = "libuv-${version}";
 
   src = fetchurl {
     url = "https://dist.libuv.org/dist/v${version}/libuv-v${version}.tar.gz";
-    multihash = "QmbCWbJ7AqvmpPrqbgf2rHz7z619E6MhoB7ut9Jq431Bdq";
+    multihash = "QmVCerwwfx8m4Wo7pGpepjGUewfJ9gAaJWs6KKREJbma5z";
     hashOutput = false;
-    sha256 = "468316fa841d114114f167b45d1f43d46a2a1852d8464336a4abbbf5b88b478b";
+    sha256 = "203927683d53d1b82eee766c8ffecfa8ed0e392679c15d5ad3a23504eda0ed1f";
   };
 
   nativeBuildInputs = [
@@ -38,7 +38,10 @@ stdenv.mkDerivation rec {
         outputHashAlgo;
       fullOpts = {
         pgpsigUrls = map (n: "${n}.sign") src.urls;
-        pgpKeyFingerprint = "AED6 E2A1 85EE B379 F174  76D2 E012 D07A D0E3 CC30";
+        pgpKeyFingerprints = [
+          "AED6 E2A1 85EE B379 F174  76D2 E012 D07A D0E3 CC30"
+          "5735 3E0D BDAA A7E8 39B6  6A1A FF47 D5E4 AD8B 4FDC"
+        ];
       };
     };
   };

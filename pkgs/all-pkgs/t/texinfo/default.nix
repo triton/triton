53d4017c5782fa33c028e6323417876831344bdb
William A. Kennington III <william@wkennington.com>
2019-11-18 02:30:20 -0800

    texinfo: multiple-outputs

--- pkgs/all-pkgs/t/texinfo/default.nix
+++ pkgs/all-pkgs/t/texinfo/default.nix
@@ -13,7 +13,7 @@ let
     "mirror://gnu/texinfo/texinfo-${version}.tar.xz"
   ];
 
-  version = "6.6";
+  version = "6.7";
 in
 stdenv.mkDerivation rec {
   name = "texinfo-${version}";
@@ -21,15 +21,12 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     urls = tarballUrls version;
     hashOutput = false;
-    sha256 = "9bb9ca00da53f26a7e5725eee49689cd4a1e18d25d5b061ac8b2053018d93d66";
+    sha256 = "988403c1542d15ad044600b909997ba3079b10e03224c61188117f3676b02caa";
   };
-
-  nativeBuildInputs = [
-    perl
-  ];
 
   buildInputs = [
     ncurses
+    perl
   ];
 
   configureFlags = [
@@ -39,7 +36,7 @@ stdenv.mkDerivation rec {
   ];
 
   preInstall = ''
-    installFlagsArray+=("TEXMF=$out/share")
+    installFlagsArray+=("TEXMF=$bin/share")
   '';
 
   installTargets = [
@@ -47,12 +44,21 @@ stdenv.mkDerivation rec {
     "install-tex"
   ];
 
+  postFixup = ''
+    rm -rv "$bin"/share/info
+  '';
+
+  outputs = [
+    "bin"
+    "man"
+  ];
+
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
-      urls = tarballUrls "6.6";
+      urls = tarballUrls "6.7";
       inherit (src) outputHashAlgo;
-      outputHash = "9bb9ca00da53f26a7e5725eee49689cd4a1e18d25d5b061ac8b2053018d93d66";
+      outputHash = "988403c1542d15ad044600b909997ba3079b10e03224c61188117f3676b02caa";
       fullOpts = {
         pgpsigUrls = map (n: "${n}.sig") urls;
         pgpKeyFingerprint = "EAF6 69B3 1E31 E1DE CBD1  1513 DDBC 579D AB37 FBA9";

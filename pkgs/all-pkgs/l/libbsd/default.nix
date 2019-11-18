28b041645a90dba6e7ed881e5be10ac666fd38a2
William A. Kennington III <william@wkennington.com>
2019-11-18 03:31:38 -0800

    libbsd: multiple-outputs

--- pkgs/all-pkgs/l/libbsd/default.nix
+++ pkgs/all-pkgs/l/libbsd/default.nix
@@ -26,14 +26,30 @@ stdenv.mkDerivation rec {
       -i Makefile.in
   '';
 
+  postInstall = ''
+    mkdir -p "$lib"/lib
+    mv "$dev"/lib*/*.so* "$lib"/lib
+    ln -sv "$lib"/lib/* "$dev"/lib
+  '';
+
+  postFixup = ''
+    rm -rv "$dev"/share
+  '';
+
+  outputs = [
+    "dev"
+    "lib"
+    "man"
+  ];
+
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
-      urls = tarballUrls "0.9.1";
+      urls = tarballUrls "0.10.0";
       pgpsigUrls = map (n: "${n}.asc") urls;
       pgpKeyFingerprint = "4F3E 74F4 3605 0C10 F569  6574 B972 BF3E A4AE 57A3";
       inherit (src) outputHashAlgo;
-      outputHash = "56d835742327d69faccd16955a60b6dcf30684a8da518c4eca0ac713b9e0a7a4";
+      outputHash = "34b8adc726883d0e85b3118fa13605e179a62b31ba51f676136ecb2d0bc1a887";
     };
   };
 

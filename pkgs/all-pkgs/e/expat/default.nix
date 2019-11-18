eeba0fd511650584feb8aeaeeec63478f9937816
William A. Kennington III <william@wkennington.com>
2019-11-18 02:33:22 -0800

    expat: multiple-outputs

--- pkgs/all-pkgs/e/expat/default.nix
+++ pkgs/all-pkgs/e/expat/default.nix
@@ -8,7 +8,7 @@ let
     "mirror://sourceforge/expat/expat/${version}/expat-${version}.tar.bz2"
   ];
 
-  version = "2.2.7";
+  version = "2.2.9";
 in
 stdenv.mkDerivation rec {
   name = "expat-${version}";
@@ -16,20 +16,39 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     urls = tarballUrls version;
     hashOutput = false;
-    sha256 = "cbc9102f4a31a8dafd42d642e9a3aa31e79a0aedaa1f6efd2795ebc83174ec18";
+    sha256 = "f1063084dc4302a427dabcca499c8312b3a32a29b7d2506653ecc8f950a9a237";
   };
+
+  postInstall = ''
+    mkdir -p "$bin"
+    mv -v "$dev"/bin "$bin"
+
+    mkdir -p "$lib"/lib
+    mv -v "$dev"/lib/*.so* "$lib"/lib
+    ln -sv "$lib"/lib/* "$dev"/lib
+  '';
+
+  postFixup = ''
+    rm -rv "$dev"/share
+  '';
+
+  outputs = [
+    "dev"
+    "bin"
+    "lib"
+  ];
 
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
       inherit (src)
         outputHashAlgo;
-      urls = tarballUrls "2.2.7";
+      urls = tarballUrls "2.2.9";
       fullOpts = {
         pgpsigUrls = map (n: "${n}.asc") urls;
         pgpKeyFingerprint = "3D7E 959D 89FA CFEE 3837  1921 B00B C66A 401A 1600";
       };
-      outputHash = "cbc9102f4a31a8dafd42d642e9a3aa31e79a0aedaa1f6efd2795ebc83174ec18";
+      outputHash = "f1063084dc4302a427dabcca499c8312b3a32a29b7d2506653ecc8f950a9a237";
     };
   };
 

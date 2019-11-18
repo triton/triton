fca57d87f3e1e4a46a9740a57004afc3f774c563
William A. Kennington III <william@wkennington.com>
2019-11-18 02:25:42 -0800

    libtool: multiple-outputs

--- pkgs/all-pkgs/l/libtool/default.nix
+++ pkgs/all-pkgs/l/libtool/default.nix
@@ -3,7 +3,6 @@
 , help2man
 , gnum4
 , makeWrapper
-, perl
 }:
 
 stdenv.mkDerivation rec {
@@ -16,15 +15,40 @@ stdenv.mkDerivation rec {
   
   nativeBuildInputs = [
     help2man
-    gnum4
+    gnum4.bin
     makeWrapper
-    perl
   ];
 
-  preFixup = ''
-    wrapProgram $out/bin/libtoolize \
-      --prefix PATH : "${gnum4}/bin"
+  prefix = placeholder "bin";
+
+  postInstall = ''
+    mkdir -p "$dev"
+    mv -v "$bin"/{include,lib} "$dev"
+
+    mkdir -p "$lib"/lib
+    mv -v "$dev"/lib*/*.so* "$lib"/lib
+    ln -sv "$lib"/lib/* "$dev"/lib
   '';
+
+  preFixup = ''
+    wrapProgram $bin/bin/libtoolize \
+      --prefix PATH : "${gnum4.bin}/bin"
+  '';
+
+  postFixup = ''
+    mkdir -p "$bin"/share2/libtool
+    mv -v "$bin"/share/aclocal "$bin"/share2
+    mv -v "$bin"/share/libtool/build-aux "$bin"/share2/libtool
+    rm -rv "$bin"/share
+    mv "$bin"/share2 "$bin"/share
+  '';
+
+  outputs = [
+    "dev"
+    "bin"
+    "lib"
+    "man"
+  ];
 
   meta = with stdenv.lib; {
     description = "Generic library support script";

06c2305cc9faf5e11ac922089100e51fbf1aef3c
William A. Kennington III <william@wkennington.com>
2020-02-17 01:26:43 -0800

    socat: 1.7.3.3 -> 1.7.3.4

--- pkgs/all-pkgs/s/socat/default.nix
+++ pkgs/all-pkgs/s/socat/default.nix
@@ -7,12 +7,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "socat-1.7.3.3";
+  name = "socat-1.7.3.4";
 
   src = fetchurl {
     url = "http://www.dest-unreach.org/socat/download/${name}.tar.bz2";
-    multihash = "QmSVx8mHJMdfiHXh6gYE6GeHW3BhWb5ZB5gujFrMQcwoxv";
-    sha256 = "0dd63ffe498168a4aac41d307594c5076ff307aa0ac04b141f8f1cec6594d04a";
+    multihash = "QmTbLx3SsQahGwcDBdaLXjoVFy5xhvMw7DJpLh73D9VyTm";
+    sha256 = "972374ca86f65498e23e3259c2ee1b8f9dbeb04d12c2a78c0c9b5d1cb97dfdfc";
   };
   
   buildInputs = [

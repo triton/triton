0f25c498ecc52d32721ba622f1d1bce14241f18e
William A. Kennington III <william@wkennington.com>
2020-03-22 17:22:17 -0700

    wireguard: 2020-02-15 -> 2020-03-18

--- pkgs/all-pkgs/w/wireguard/kernel.nix
+++ pkgs/all-pkgs/w/wireguard/kernel.nix
@@ -8,8 +8,8 @@
 }:
 
 let
-  rev = "d05a993b76af758b4bd998999aea633d30e88ce4";
-  date = "2020-02-15";
+  rev = "cf877de9a8b38422aef6beb514b688a0eedae4d8";
+  date = "2020-03-18";
 in
 stdenv.mkDerivation {
   name = "wireguard-linux-compat-${date}-${kernel.version}";
@@ -17,8 +17,8 @@ stdenv.mkDerivation {
   src = fetchzip {
     version = 6;
     url = "https://git.zx2c4.com/wireguard-linux-compat/snapshot/wireguard-linux-compat-${rev}.tar.xz";
-    multihash = "QmQxbVPTAYtiMem2YuXoQ1i58E7eGfM93h5oiTqA5iJzEo";
-    sha256 = "2d95351d5f7efa5deef7839c89c6816bb0e2ca19afc37c798ed8ce898d88306a";
+    multihash = "QmebYStGVwA5YVy8YEjJyN93iwhfUv8pbG1ECR2ctc8wTt";
+    sha256 = "2ce9fec119538d652bca15a986cf37085d51593732ab52f1308c3483f421c3f4";
   };
 
   nativeBuildInputs = [

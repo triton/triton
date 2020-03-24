da6e756f64007e5a079d16c7759b63d9876e6fa5
William A. Kennington III <william@wkennington.com>
2020-03-23 20:15:20 -0700

    libxslt: 1.1.33 -> 1.1.34

--- pkgs/all-pkgs/l/libxslt/default.nix
+++ pkgs/all-pkgs/l/libxslt/default.nix
@@ -11,16 +11,16 @@ let
     "http://xmlsoft.org/sources/libxslt-${version}.tar.gz"
   ];
 
-  version = "1.1.33";
+  version = "1.1.34";
 in
 stdenv.mkDerivation rec {
   name = "libxslt-${version}";
 
   src = fetchurl {
     urls = tarballUrls version;
-    multihash = "QmfQAJ5t9uqJjmNvGmSi2dLRwbp3iQfSiQauygZdqGxsRT";
+    multihash = "QmUpf2GX77PjxXQkkhsSuozWPwGJZC7cmjZ9x9Cf9pJEGF";
     hashOutput = false;
-    sha256 = "8e36605144409df979cab43d835002f63988f3dc94d5d3537c12796db90e38c8";
+    sha256 = "98b1bd46d6792925ad2dfe9a87452ea2adebf69dcb9919ffd55bf926a7f93f7f";
   };
 
   buildInputs = [
@@ -43,9 +43,9 @@ stdenv.mkDerivation rec {
   passthru = {
     srcVerification = fetchurl rec {
       failEarly = true;
-      urls = tarballUrls "1.1.33";
+      urls = tarballUrls "1.1.34";
       inherit (src) outputHashAlgo;
-      outputHash = "8e36605144409df979cab43d835002f63988f3dc94d5d3537c12796db90e38c8";
+      outputHash = "98b1bd46d6792925ad2dfe9a87452ea2adebf69dcb9919ffd55bf926a7f93f7f";
       fullOpts = {
         pgpsigUrls = map (n: "${n}.asc") urls;
         pgpKeyFingerprint = "C744 15BA 7C9C 7F78 F02E  1DC3 4606 B8A5 DE95 BC1F";

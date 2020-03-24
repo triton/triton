d53153278e0dd12e79d2ea9419ad0de561a4aaec
William A. Kennington III <william@wkennington.com>
2020-03-23 20:14:53 -0700

    libtirpc: 1.1.4 -> 1.2.5

--- pkgs/all-pkgs/l/libtirpc/default.nix
+++ pkgs/all-pkgs/l/libtirpc/default.nix
@@ -6,14 +6,14 @@
 }:
 
 let
-  version = "1.1.4";
+  version = "1.2.5";
 in
 stdenv.mkDerivation rec {
   name = "libtirpc-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/libtirpc/libtirpc/${version}/${name}.tar.bz2";
-    sha256 = "2ca529f02292e10c158562295a1ffd95d2ce8af97820e3534fe1b0e3aec7561d";
+    sha256 = "f3b6350c7e9c3cd9c58fc7a5e5f8e6be469cc571bb5eb31eb9790b3e675186ca";
   };
 
   propagatedBuildInputs = [
